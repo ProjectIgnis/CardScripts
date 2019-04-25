2719b841f5374eaacdf6b9db0c63ce1bdfb15a0a
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-04-25 19:18:16 -0300

    Updated "Naturia Sacred Tree"
    Update to remove unecessary parts now handled by the core.

--- c3734202.lua
+++ c3734202.lua
@@ -1,11 +1,11 @@
 --ナチュルの神星樹
+--Naturia Sacred Tree
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -42,60 +42,6 @@ function s.initial_effect(c)
 	e4:SetTarget(s.thtg)
 	e4:SetOperation(s.thop)
 	c:RegisterEffect(e4)
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	e:SetLabel(1)
-	return true
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		if e:GetLabel()==1 then e:SetLabel(0) end
-		return true
-	end
-	local b1=s.spcost1(e,tp,eg,ep,ev,re,r,rp,0) and s.sptg1(e,tp,eg,ep,ev,re,r,rp,0)
-	local b2=s.spcost2(e,tp,eg,ep,ev,re,r,rp,0) and s.sptg2(e,tp,eg,ep,ev,re,r,rp,0)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if Duel.GetFlagEffect(tp,id)==0 and ft>-1
-		and (b1 or b2) and Duel.SelectYesNo(tp,94) then
-		local opt=0
-		if b1 and b2 then
-			opt=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
-		elseif b1 then
-			opt=Duel.SelectOption(tp,aux.Stringid(id,0))
-		else
-			opt=Duel.SelectOption(tp,aux.Stringid(id,1))+1
-		end
-		if opt==0 then
-			s.spcost1(e,tp,eg,ep,ev,re,r,rp,1)
-		else
-			s.spcost2(e,tp,eg,ep,ev,re,r,rp,1)
-		end
-		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		e:SetLabel(opt+1)
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-	else
-		e:SetCategory(0)
-		e:SetLabel(0)
-	end
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local opt=e:GetLabel()
-	if opt==0 or not e:GetHandler():IsRelateToEffect(e) then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if opt==1 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,RACE_PLANT)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,RACE_INSECT)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
-	end
 end
 function s.cfilter(c,race,ft,tp)
 	return c:IsRace(race) and c:IsAttribute(ATTRIBUTE_EARTH) and (ft>0 or (c:GetSequence()<5 and c:IsControler(tp))) and (c:IsFaceup() or c:IsControler(tp))

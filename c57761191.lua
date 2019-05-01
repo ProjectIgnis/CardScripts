0000372b33e058b496c7c85f1c70ae82fe505468
edo9300 <edoardo762@gmail.com>
2019-05-01 15:53:26 +0200

    Updated Metalron XII to use extra tribute, changed tribute string to say card instead of monster

--- c57761191.lua
+++ c57761191.lua
@@ -1,30 +1,30 @@
 --真竜機兵ダースメタトロン
 local s,id=GetID()
 function s.initial_effect(c)
-	--summon with 3 tribute
+	--summon with s/t
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCondition(s.ttcon)
-	e1:SetOperation(s.ttop)
-	e1:SetValue(SUMMON_TYPE_ADVANCE)
+	e1:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
+	e1:SetTargetRange(LOCATION_SZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_CONTINUOUS))
+	e1:SetValue(POS_FACEUP)
 	c:RegisterEffect(e1)
+	--summon with 3 tribute
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_LIMIT_SET_PROC)
-	e2:SetCondition(s.setcon)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_LIMIT_SUMMON_PROC)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCondition(s.ttcon)
+	e2:SetOperation(s.ttop)
+	e2:SetValue(SUMMON_TYPE_ADVANCE)
 	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
-    	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-    	e3:SetType(EFFECT_TYPE_SINGLE)
-    	e3:SetCode(EFFECT_LIMIT_SUMMON_PROC)
-	e3:SetDescription(aux.Stringid(id,1))
-    	e3:SetCondition(s.ttcon2)
-    	e3:SetOperation(s.ttop2)
-    	e3:SetValue(SUMMON_TYPE_ADVANCE)
-    	c:RegisterEffect(e3)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_LIMIT_SET_PROC)
+	e3:SetCondition(s.setcon)
+	c:RegisterEffect(e3)
 	--tribute check
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
@@ -52,80 +52,11 @@ function s.initial_effect(c)
 	e6:SetOperation(s.spop)
 	c:RegisterEffect(e6)
 end
-function s.otfilter(c)
-	return c:IsType(TYPE_CONTINUOUS) and c:IsReleasable()
-end
-function s.exfilter(c,g,sc)
-	if not c:IsReleasable() or g:IsContains(c) or c:IsHasEffect(EFFECT_EXTRA_RELEASE) then return false end
-	local rele=c:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-	if rele then
-		local remct,ct,flag=rele:GetCountLimit()
-		if remct<=0 then return false end
-	else return false end
-	local sume={c:GetCardEffect(EFFECT_UNRELEASABLE_SUM)}
-	for _,te in ipairs(sume) do
-		if type(te:GetValue())=='function' then
-			if te:GetValue()(te,sc) then return false end
-		else return false end
-	end
-	return true
-end
-function s.val(c,sc,ma)
-	local eff3={c:GetCardEffect(EFFECT_TRIPLE_TRIBUTE)}
-	if ma>=3 then
-		for _,te in ipairs(eff3) do
-			if type(te:GetValue())~='function' or te:GetValue()(te,sc) then return 0x30001 end
-		end
-	end
-	local eff2={c:GetCardEffect(EFFECT_DOUBLE_TRIBUTE)}
-	for _,te in ipairs(eff2) do
-		if type(te:GetValue())~='function' or te:GetValue()(te,sc) then return 0x20001 end
-	end
-	return 1
-end
-function s.req(c)
-	return c:IsType(TYPE_CONTINUOUS) and c:IsLocation(LOCATION_SZONE)
-end
-function s.unreq(c,tp)
-	return c:IsControler(1-tp) and not c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsHasEffect(EFFECT_EXTRA_RELEASE_SUM)
-end
-function s.rescon(sg,e,tp,mg)
-	local c=e:GetHandler()
-	if not sg:IsExists(s.req,1,nil) or not aux.ChkfMMZ(1)(sg,e,tp,mg) 
-		or sg:FilterCount(s.unreq,nil,tp)>1 then return false end
-	local ct=#sg
-	return sg:CheckWithSumEqual(s.val,3,ct,ct,c,3)
-end
 function s.ttcon(e,c,minc)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(s.otfilter,tp,LOCATION_SZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(s.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	return minc<=3 and Duel.GetLocationCount(tp,LOCATION_MZONE)>-3 and aux.SelectUnselectGroup(g,e,tp,1,3,s.rescon,0)
-end
-function s.ttop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(s.otfilter,tp,LOCATION_SZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(s.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	local sg=aux.SelectUnselectGroup(g,e,tp,1,3,s.rescon,1,tp,HINTMSG_RELEASE,s.rescon)
-	local remc=sg:Filter(s.unreq,nil,tp):GetFirst()
-	if remc then
-		local rele=remc:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-		rele:Reset()
-	end
-	c:SetMaterial(sg)
-	Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
-end
-function s.ttcon2(e,c,minc)
 	if c==nil then return true end
 	return minc<=3 and Duel.CheckTribute(c,3)
 end
-function s.ttop2(e,tp,eg,ep,ev,re,r,rp,c)
+function s.ttop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.SelectTribute(tp,c,3,3)
 	c:SetMaterial(g)
 	Duel.Release(g,REASON_SUMMON+REASON_MATERIAL)

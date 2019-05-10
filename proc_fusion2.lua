ae82c50b7058a9b15f3d889967d74ff421a4ca47
edo9300 <edoardo762@gmail.com>
2019-05-11 00:22:42 +0200

    Further fixes in fusion summon effs

--- proc_fusion2.lua
+++ proc_fusion2.lua
@@ -7,10 +7,11 @@
 --extraop		function called right before sending the monsters to the graveyard as material
 --gc			mandatory card or function returning a group to be used (for effects like Soprano)
 --stage2		function called after the monster has been summoned
+--exactcount	
 --location		location where to summon fusion monsters from (default LOCATION_EXTRA)
 --chkf			FUSPROC flags for the fusion summon
 
-function Fusion.AddSummonEff(c,fusfilter,matfilter,desc,extrafil,extraop,gc,stage2,location,chkf)
+function Fusion.AddSummonEff(c,fusfilter,matfilter,desc,extrafil,extraop,gc,stage2,exactcount,location,chkf)
 	local e1=Effect.CreateEffect(c)
 	if desc then
 		e1:SetDescription(desc)
@@ -18,8 +19,8 @@ function Fusion.AddSummonEff(c,fusfilter,matfilter,desc,extrafil,extraop,gc,stag
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(Fusion.SummonEffTG(fusfilter,matfilter,extrafil,extraop,gc,stage2,location,chkf))
-	e1:SetOperation(Fusion.SummonEffOP(fusfilter,matfilter,extrafil,extraop,gc,stage2,location,chkf))
+	e1:SetTarget(Fusion.SummonEffTG(fusfilter,matfilter,extrafil,extraop,gc,stage2,exactcount,location,chkf))
+	e1:SetOperation(Fusion.SummonEffOP(fusfilter,matfilter,extrafil,extraop,gc,stage2,exactcount,location,chkf))
 	c:RegisterEffect(e1)
 	return e1
 end
@@ -27,7 +28,7 @@ function Fusion.SummonEffFilter(c,fusfilter,e,tp,mg,gc,chkf)
 	return c:IsType(TYPE_FUSION) and (not fusfilter or fusfilter(c)) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
 			and c:CheckFusionMaterial(mg,gc,chkf)
 end
-function Fusion.SummonEffTG(fusfilter,matfilter,extrafil,extraop,gc,stage2,location,chkf)
+function Fusion.SummonEffTG(fusfilter,matfilter,extrafil,extraop,gc,stage2,exactcount,location,chkf)
 	return	function(e,tp,eg,ep,ev,re,r,rp,chk)
 				location = location or LOCATION_EXTRA
 				chkf = chkf and chkf|tp or tp
@@ -37,8 +38,9 @@ function Fusion.SummonEffTG(fusfilter,matfilter,extrafil,extraop,gc,stage2,locat
 						mg1:Merge(extrafil(e,tp,eg,ep,ev,re,r,rp,chk):Filter(Card.IsCanBeFusionMaterial,nil))
 					end
 					if matfilter then
-						mg1:Filter(matfilter,nil,e,tp,eg,ep,ev,re,r,rp)
+						mg1=mg1:Filter(matfilter,nil,e,tp,eg,ep,ev,re,r,rp,0)
 					end
+					Auxiliary.FCheckExact=exactcount
 					local res=Duel.IsExistingMatchingCard(Fusion.SummonEffFilter,tp,location,0,1,nil,fusfilter,e,tp,mg1,type(gc)=="function" and gc(e,tp,eg,ep,ev,re,r,rp,chk) or gc,chk)
 					if not res then
 						for _,ce in ipairs({Duel.GetPlayerEffect(tp,EFFECT_CHAIN_MATERIAL)}) do
@@ -46,10 +48,12 @@ function Fusion.SummonEffTG(fusfilter,matfilter,extrafil,extraop,gc,stage2,locat
 							local mg=fgroup(ce,e,tp)
 							local mf=ce:GetValue()
 							if Duel.IsExistingMatchingCard(Fusion.SummonEffFilter,tp,location,0,1,nil,aux.AND(mf,fusfilter or aux.TRUE),e,tp,mg,type(gc)=="function" and gc(e,tp,eg,ep,ev,re,r,rp,chk) or gc,chkf) then
-								return true
+								res=true
+								break
 							end
 						end		
 					end
+					Auxiliary.FCheckExact=nil
 					return res
 				end
 				Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,location)
@@ -72,15 +76,11 @@ function Fusion.ChainMaterialPrompt(effswithgroup,fieldID,tp,e)
 	if #effs == 1 then return effs[1] end
 	local desctable = {}
 	for _,index in ipairs(effs) do
-		if index == 1 then
-			table.insert(desctable,e:GetDescription())
-		else
-			table.insert(desctable,effswithgroup[index][1]:GetDescription())
-		end
+		table.insert(desctable,effswithgroup[index][1]:GetDescription())
 	end
 	return effs[Duel.SelectOption(tp,false,table.unpack(desctable)) + 1]
 end
-function Fusion.SummonEffOP(fusfilter,matfilter,extrafil,extraop,gc,stage2,location,chkf)
+function Fusion.SummonEffOP(fusfilter,matfilter,extrafil,extraop,gc,stage2,exactcount,location,chkf)
 	return	function(e,tp,eg,ep,ev,re,r,rp)
 				location = location or LOCATION_EXTRA
 				chkf = chkf and chkf|tp or tp
@@ -89,20 +89,23 @@ function Fusion.SummonEffOP(fusfilter,matfilter,extrafil,extraop,gc,stage2,locat
 					mg1:Merge(extrafil(e,tp,eg,ep,ev,re,r,rp,chk):Filter(Card.IsCanBeFusionMaterial,nil))
 				end
 				if matfilter then
-					mg1:Filter(matfilter,nil,e,tp,eg,ep,ev,re,r,rp)
+					mg1=mg1:Filter(matfilter,nil,e,tp,eg,ep,ev,re,r,rp,1)
 				end
-				mg1:Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
+				mg1=mg1:Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
+				Debug.Message(#mg1)
+				Auxiliary.FCheckExact=exactcount
 				local effswithgroup={}
-				local sg1=Duel.GetMatchingGroup(Fusion.SummonEffFilter,tp,LOCATION_EXTRA,0,nil,ffilter,e,tp,mg,type(gc)=="function" and gc(e,tp,eg,ep,ev,re,r,rp,chk) or gc,chkf)
+				local sg1=Duel.GetMatchingGroup(Fusion.SummonEffFilter,tp,location,0,nil,ffilter,e,tp,mg1,type(gc)=="function" and gc(e,tp,eg,ep,ev,re,r,rp,chk) or gc,chkf)
 				local extraeffs = {Duel.GetPlayerEffect(tp,EFFECT_CHAIN_MATERIAL)}
 				if #sg1 > 0 then
+					Debug.Message("count "..#sg1)
 					table.insert(effswithgroup,{e,aux.GrouptoFieldid(sg1)})
 				end
 				for _,ce in ipairs(extraeffs) do
 					local fgroup=ce:GetTarget()
 					local mg2=fgroup(ce,e,tp)
 					local mf=ce:GetValue()
-					local sg2=Duel.GetMatchingGroup(Fusion.SummonEffFilter,tp,location,0,nil,aux.AND(mf,fusfilter or aux.TRUE),e,tp,mg,type(gc)=="function" and gc(e,tp,eg,ep,ev,re,r,rp,chk) or gc,chkf)
+					local sg2=Duel.GetMatchingGroup(Fusion.SummonEffFilter,tp,location,0,nil,aux.AND(mf,fusfilter or aux.TRUE),e,tp,mg2,type(gc)=="function" and gc(e,tp,eg,ep,ev,re,r,rp,chk) or gc,chkf)
 					if #sg2 > 0 then
 						table.insert(effswithgroup,{ce,aux.GrouptoFieldid(sg2)})
 						sg1:Merge(sg2)
@@ -119,7 +122,7 @@ function Fusion.SummonEffOP(fusfilter,matfilter,extrafil,extraop,gc,stage2,locat
 						backupmat=mat1:Clone()
 						tc:SetMaterial(mat1)
 						if extraop then
-							extraop(mat1,tc,e,tp)
+							extraop(e,tc,tp,mat1)
 						end
 						if #mat1>0 then
 							Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
@@ -132,8 +135,9 @@ function Fusion.SummonEffOP(fusfilter,matfilter,extrafil,extraop,gc,stage2,locat
 					end
 					tc:CompleteProcedure()
 					if stage2 then
-						stage2(backupmat,tc,e,tp)
+						stage2(e,tc,tp,backupmat)
 					end
 				end
+				Auxiliary.FCheckExact=nil
 			end
 end

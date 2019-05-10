2eef10c5feced9a5ccdafe0992d976eb35181714
edo9300 <edoardo762@gmail.com>
2019-05-11 00:23:38 +0200

    Use new fusion effect with ultra polymerization

--- c76647978.lua
+++ c76647978.lua
@@ -1,16 +1,18 @@
 --超越融合
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	local e1=Fusion.AddSummonEff(c,nil,Card.IsOnField,aux.Stringid(id,0),nil,nil,nil,s.register,2)
 	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
-	c:RegisterEffect(e1)
+	local tg=e1:GetTarget()
+	e1:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk)
+					if chk==0 then
+						return tg(e,tp,eg,ep,ev,re,r,rp,chk)
+					end
+					tg(e,tp,eg,ep,ev,re,r,rp,chk) 
+					if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+						Duel.SetChainLimit(aux.FALSE)
+					end
+				end)
 	if not UltraPolyTable then UltraPolyTable={} end
 	if not s.global_check then
 		s.global_check=true
@@ -21,105 +23,34 @@ function s.initial_effect(c)
 		Duel.RegisterEffect(ge2,0)
 	end
 end
-function s.clear(e,tp,eg,ep,ev,re,r,rp)
-	for c,e2 in ipairs(UltraPolyTable) do
-		local g=e2:GetLabelObject()
-		g:Remove(aux.FilterEqualFunction(Card.GetFlagEffect,0,id),nil)
-		if #g<=0 then
-			g:DeleteGroup()
-			UltraPolyTable[c]=nil
-		end
-	end
-end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,2000) end
 	Duel.PayLPCost(tp,2000)
 end
-function s.filter2(c,e,tp,m,f)
-	return c:IsType(TYPE_FUSION) and (not f or f(c))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,tp)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil)
-		Fusion.CheckExact=2
-		local res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil)
-		if not res then
-			local ce=Duel.GetChainMaterial(tp)
-			if ce~=nil then
-				local fgroup=ce:GetTarget()
-				local mg2=fgroup(ce,e,tp)
-				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf)
-			end
-		end
-		Fusion.CheckExact=nil
-		return res
+function s.register(e,tc,tp,sg)
+	tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+	local c=e:GetHandler()
+	local g
+	if UltraPolyTable[c]==nil then
+		g=Group.CreateGroup()
+		g:KeepAlive()
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,1))
+		e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		e2:SetType(EFFECT_TYPE_IGNITION)
+		e2:SetRange(LOCATION_GRAVE)
+		e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e2:SetCost(s.spcost)
+		e2:SetTarget(s.sptg)
+		e2:SetOperation(s.spop)
+		e2:SetLabelObject(g)
+		e2:SetReset(RESET_EVENT+RESET_TODECK)
+		c:RegisterEffect(e2)
+		UltraPolyTable[c]=e2
+	else
+		g=UltraPolyTable[c]:GetLabelObject()
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		Duel.SetChainLimit(aux.FALSE)
-	end
-end
-function s.filter0(c,e)
-	return c:IsOnField() and not c:IsImmuneToEffect(e)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local mg1=Duel.GetFusionMaterial(tp):Filter(s.filter0,nil,e)
-	Fusion.CheckExact=2
-	local sg1=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf)
-	end
-	if #sg1>0 or (sg2~=nil and #sg2>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,tp)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,tp)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-		local c=e:GetHandler()
-		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
-		tc:CompleteProcedure()
-		local g
-		if UltraPolyTable[c]==nil then
-			g=Group.CreateGroup()
-			g:KeepAlive()
-			local e2=Effect.CreateEffect(c)
-			e2:SetDescription(aux.Stringid(id,1))
-			e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-			e2:SetType(EFFECT_TYPE_IGNITION)
-			e2:SetRange(LOCATION_GRAVE)
-			e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-			e2:SetCost(s.spcost)
-			e2:SetTarget(s.sptg)
-			e2:SetOperation(s.spop)
-			e2:SetLabelObject(g)
-			e2:SetReset(RESET_EVENT+RESET_TODECK)
-			c:RegisterEffect(e2)
-			UltraPolyTable[c]=e2
-		else
-			g=UltraPolyTable[c]:GetLabelObject()
-		end
-		g:AddCard(tc)
-	end
-	Fusion.CheckExact=nil
+	g:AddCard(tc)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end

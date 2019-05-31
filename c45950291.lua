7c3568ec42fb330289fad73145cc7c412d827b7c
Naim <naimsantos45@gmail.com>
2019-05-30 21:34:44 -0300

    EFFECT_FLAG_CLIENT_HINT update
    Rank-Up-Magic Astral Force
    Speedroid Taketomborg
    Parallel World Fusion
    Eidos the Underworld Squire
    Dinomists Howling

--- c45950291.lua
+++ c45950291.lua
@@ -1,4 +1,5 @@
 --RUM－アストラル・フォース
+--Rank-Up-Magic Astral Force
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -77,6 +78,12 @@ function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetTargetRange(1,0)
+	Duel.RegisterEffect(e2,tp)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not se:GetHandler():IsCode(id)
--- c60675348.lua
+++ c60675348.lua
@@ -1,4 +1,5 @@
 --ダイナミスト・ハウリング
+--Dinomists Howling
 local s,id=GetID()
 function s.initial_effect(c)
 	--activate
@@ -43,8 +44,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetDescription(aux.Stringid(id,2))
 		e1:SetTargetRange(1,0)
 		e1:SetTarget(s.splimit)
 		e1:SetReset(RESET_PHASE+PHASE_END,2)

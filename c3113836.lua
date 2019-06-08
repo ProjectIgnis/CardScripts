ea33b3fd5e288a9b847ca4fe7bfe8a1e5b281cbf
Naim <naimsantos45@gmail.com>
2019-06-08 20:13:07 -0300

    Fixed "Gem-Knight Seraphinite"'s strings

--- c3113836.lua
+++ c3113836.lua
@@ -1,4 +1,5 @@
 --ジェムナイト・セラフィ
+--Gem-Knight Seraphinite
 local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
@@ -16,6 +17,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
 	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetRange(LOCATION_MZONE)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()

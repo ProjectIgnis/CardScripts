3cc7bce5c91f3a6da82587a0f00556f882fb2a40
Naim <naimsantos45@gmail.com>
2019-08-22 13:19:59 -0300

    Changes from master

--- c300.lua
+++ c300.lua
@@ -150,46 +150,43 @@ if not DeckMasters then
 			Duel.MoveSequence(c,6)
 		end
 	end
-
-	--register
-	local e1=Effect.GlobalEffect()
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PREDRAW)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetCondition(DeckMasters.con)
-	e1:SetOperation(DeckMasters.op)
-	Duel.RegisterEffect(e1,0)
-	--Deck Masters Zone implementation
-	local e2=Effect.GlobalEffect()
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetTargetRange(0xff,0xff)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsLocation,0x400))
-	Duel.RegisterEffect(e2,0)
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
-	Duel.RegisterEffect(e3,0)
-	local e4=e2:Clone()
-	e4:SetCode(EFFECT_UNRELEASABLE_SUM)
-	e4:SetValue(DeckMasters.sumval)
-	Duel.RegisterEffect(e4,0)
-	local e5=e4:Clone()
-	e5:SetCode(EFFECT_UNRELEASABLE_NONSUM)
-	Duel.RegisterEffect(e5,0)
-	local e6=e2:Clone()
-	e6:SetCode(EFFECT_IMMUNE_EFFECT)
-	e6:SetValue(DeckMasters.efilter)
-	Duel.RegisterEffect(e6,0)
+	local function finish_setup() 
+		--register
+		local e1=Effect.GlobalEffect()
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_STARTUP)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetOperation(DeckMasters.op)
+		Duel.RegisterEffect(e1,0)
+		--Deck Masters Zone implementation
+		local e2=Effect.GlobalEffect()
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+		e2:SetTargetRange(0xff,0xff)
+		e2:SetTarget(aux.TargetBoolFunction(Card.IsLocation,0x400))
+		Duel.RegisterEffect(e2,0)
+		local e3=e2:Clone()
+		e3:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+		Duel.RegisterEffect(e3,0)
+		local e4=e2:Clone()
+		e4:SetCode(EFFECT_UNRELEASABLE_SUM)
+		e4:SetValue(DeckMasters.sumval)
+		Duel.RegisterEffect(e4,0)
+		local e5=e4:Clone()
+		e5:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+		Duel.RegisterEffect(e5,0)
+		local e6=e2:Clone()
+		e6:SetCode(EFFECT_IMMUNE_EFFECT)
+		e6:SetValue(DeckMasters.efilter)
+		Duel.RegisterEffect(e6,0)
+	end
 	
 	function DeckMasters.sumval(e,c)
 		return e:GetHandler():IsDeckMaster(true)
 	end
 	function DeckMasters.efilter(e,te)
 		return te:GetHandler()~=e:GetOwner()
-	end
-	function DeckMasters.con(e,tp,eg,ep,ev,re,r,rp)
-		return Duel.GetTurnCount()==1
 	end
 	function DeckMasters.op(e,tp,eg,ep,ev,re,r,rp)
 		local g1=getmatchg(function(c) return c.dm and not c.dm_no_activable end,0,0xff,0,nil)
@@ -247,9 +244,6 @@ if not DeckMasters then
 				dm[i]:RegisterEffect(e1)
 			end
 			dm[i]:RegisterFlagEffect(300,0,EFFECT_FLAG_CLIENT_HINT,1,0,63)
-			if dm[i]:GetPreviousLocation()==LOCATION_HAND then
-					Duel.Draw(i,1,REASON_RULE)
-			end
 		end
 		DeckMasters.VictoryEffects(dm[0])
 	end
@@ -311,7 +305,7 @@ if not DeckMasters then
 		elseif c1==0 and c2==0 and f2==0 and f1==0  then
 			Duel.Win(PLAYER_NONE,WIN_REASON_DM)
 		elseif c1>0 and c2==0 and f2==0 then
-			Duel.Win(tp,WIN_REASON_DM)  
+			Duel.Win(tp,WIN_REASON_DM)
 		end
 	end
 
@@ -415,6 +409,8 @@ if not DeckMasters then
 			Duel.ResetFlagEffect(tp,300+1)
 		end
 	end
+	
+	finish_setup()
 
 
 	tableDm = {
--- c946.lua
+++ c946.lua
@@ -1,4 +1,3 @@
---Generic Token
 local s,id=GetID()
 function s.initial_effect(c)
 end

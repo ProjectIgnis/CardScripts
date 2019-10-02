b21153063885d87168b8841e1c9b43dbff11022a
NaimSantos <naimsantos45@gmail.com>
2019-10-02 19:40:41 -0300

    Added script: "Judgment Arrows"

---
+++ c511009503.lua
@@ -0,0 +1,85 @@
+--Judgment Arrows
+local s,id=GetID()
+function s.initial_effect(c)
+    Card.SetUniqueOnField(c,1,0,id,LOCATION_SZONE)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetProperty(EFFECT_FLAG_LIMIT_ZONE)
+    e1:SetValue(s.zones)
+    c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetCondition(s.atkcon)
+	e2:SetTarget(s.atktg)
+	e2:SetValue(s.adval)
+	c:RegisterEffect(e2)
+	--linked group
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_LEAVE_FIELD_P)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(s.linkcon)
+	e3:SetOperation(s.linkop)
+	c:RegisterEffect(e3)
+	--destroy
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetCondition(s.descon)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
+	e4:SetLabelObject(e3)
+	c:RegisterEffect(e4)
+end
+function s.zones(e,tp,eg,ep,ev,re,r,rp)
+    return (Duel.GetLinkedZone(tp)>>8) & 0xff
+end
+function s.atkcon(e)
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL 
+end
+function s.atktg(e,c)
+	local lg=e:GetHandler():GetLinkedGroup()
+	return lg:IsContains(c) 
+end
+function s.adval(e,c)
+	return c:GetAttack()
+end
+function s.linkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetLinkedGroup():GetCount()>0
+end
+function s.linkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetHandler():GetLinkedGroup()
+	if #g>0 then
+		e:SetLabelObject(g)
+		e:SetLabel(1)
+		local tc=g:GetFirst()
+		while tc do
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+			tc=g:GetNext()
+		end
+	end
+end
+function s.desfilter(c)
+	return c:GetFlagEffect(id)>0
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	return #g>0 and not c:IsLocation(LOCATION_DECK)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.Destroy(g,REASON_EFFECT)
+end

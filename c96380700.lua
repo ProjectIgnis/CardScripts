d23325629a4557640ee3f4eb54c9b1bcf430c232
AlphaKretin <deewhydeeecks@gmail.com>
2019-10-13 21:41:33 +1030

    Fix "Talkback Lancer"
    Percy script did not work due to missing function. Based on Fluo script modified to use Percy procs

--- c96380700.lua
+++ c96380700.lua
@@ -1,6 +1,5 @@
 --トークバック・ランサー
 --Talkback Lancer
---scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
 	--link summon
@@ -14,40 +13,44 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
+	e1:SetCost(s.spcost)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
+s.listed_series={0x101}
 function s.matfilter(c,lc,sumtype,tp)
 	return c:IsLevelBelow(2) and c:IsRace(RACE_CYBERSE,lc,sumtype,tp)
 end
-function s.spfilter(c,e,tp,zone,code)
-	if zone==0 then zone=0xff end
-	return c:IsSetCard(0x101) and not c:IsOriginalCode(code)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
+function s.cfilter(c,e,tp,zone)
+	return c:IsRace(RACE_CYBERSE) and c:IsType(TYPE_MONSTER) and Duel.GetMZoneCount(tp,c,tp,LOCATION_REASON_TOFIELD,zone)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,c)
 end
-function s.spcfilter(c,g,zone,e,tp)
-	return c:IsRace(RACE_CYBERSE) and (zone~=0 or g:IsContains(c))
-		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone,c:GetOriginalCode()) 
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local zone=c:GetLinkedZone(tp)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,s.cfilter,1,c,e,tp,zone) end
+	local g=Duel.SelectReleaseGroup(tp,s.cfilter,1,1,c,e,tp,zone)
+	Duel.Release(g,REASON_COST)
+	e:SetLabelObject(g:GetFirst())
+end
+function s.spfilter(c,e,tp,rc)
+	return c:IsSetCard(0x101) and c:IsType(TYPE_MONSTER) and not c:IsOriginalCodeRule(rc:GetOriginalCodeRule()) 
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local c=e:GetHandler()
-	local lg=c:GetLinkedGroup()
-	local zone=c:GetFreeLinkedZone()&0x1f
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp,zone) end
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.spcfilter,1,false,nil,c,lg,zone,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local tc=Duel.SelectReleaseGroupCost(tp,s.spcfilter,1,1,false,nil,c,lg,zone,e,tp)
-	Duel.Release(tc,REASON_COST)
+	local cc=e:GetLabelObject()
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp)
+		and s.spfilter(chkc,e,tp,cc) end
+	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,c:GetFreeLinkedZone()&0x1f,tc:GetFirst():GetOriginalCode())
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,LOCATION_GRAVE)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,cc)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local zone=e:GetHandler():GetFreeLinkedZone()&0x1f
+	local zone=e:GetHandler():GetLinkedZone(tp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and zone~=0 then
+	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP,zone)
 	end
 end
-

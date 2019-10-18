2742bfefa8be7a57dc11eb7f6407e63c77338af7
NaimSantos <naimsantos45@gmail.com>
2019-10-18 19:09:30 -0300

    Minor style fix

--- c21159309.lua
+++ c21159309.lua
@@ -1,12 +1,12 @@
 --救世竜 セイヴァー・ドラゴン
 local s,id=GetID()
 function s.initial_effect(c)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetValue(s.synlimit)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetValue(s.synlimit)
+	c:RegisterEffect(e1)
 end
 function s.synlimit(e,c)
 	if not c then return false end

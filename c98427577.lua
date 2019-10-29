48d2d4efac29d105de143d17d529610be3c57f9f
AlphaKretin <deewhydeeecks@gmail.com>
2019-10-29 21:36:18 +1030

    Fix Trap Cards that set themselves (#35)
    * Update "Scrap-Iron Scarecrow"
    * Further update Scrap-Iron Scarecrow
    Missed that the cost could be removed entirely
    * Update "Scrap-Iron Statue"
    * Update "Subterror Final Battle"
    * Update "Subterror Succession"
    It does not seem to have inherited the same issue as other self-setting Traps, but did have a minor mistake using "IsCanTurnSet" instead of "IsSSetable"
    * Fix
    Two of the files were missing `c:CancelToGrave`

--- c98427577.lua
+++ c98427577.lua
@@ -1,4 +1,5 @@
 --くず鉄のかかし 
+--Scrap-Iron Scarecrow
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -7,19 +8,12 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetCondition(s.condition)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer()
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		aux.RemainFieldCost(e,tp,eg,ep,ev,re,r,rp,1)
-	end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local tg=Duel.GetAttacker()
@@ -30,12 +24,11 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateAttack()
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) or c:IsStatus(STATUS_LEAVE_CONFIRMED) then return end
+	if not c:IsRelateToEffect(e) then return end
 	if c:IsSSetable(true) and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
 		Duel.BreakEffect()
+		c:CancelToGrave()
 		Duel.ChangePosition(c,POS_FACEDOWN)
 		Duel.RaiseEvent(c,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
-	else
-		c:CancelToGrave(false)
 	end
 end

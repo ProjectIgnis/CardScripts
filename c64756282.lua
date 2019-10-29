861bf62184e66187573f732bf0e8f4138d739596
Naim <naimsantos45@gmail.com>
2019-10-29 12:35:03 -0300

    Update c64756282.lua

--- c64756282.lua
+++ c64756282.lua
@@ -57,7 +57,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.filter(c)
 	return c:IsSetCard(0x128) and c:IsType(TYPE_SPELL) and c:IsAbleToRemoveAsCost()
-		and ~=nil and c:CheckActivateEffect(false,true,false):GetOperation()~=nil
+		and c:CheckActivateEffect(false,true,false)~=nil and c:CheckActivateEffect(false,true,false):GetOperation()~=nil
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()

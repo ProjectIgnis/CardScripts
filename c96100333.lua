640a945990f2edb399bde29344c29720356c6393
Naim <naimsantos45@gmail.com>
2019-10-31 14:14:27 -0300

    Update c96100333.lua

--- c96100333.lua
+++ c96100333.lua
@@ -8,7 +8,6 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)

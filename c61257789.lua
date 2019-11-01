499564b247daa8ec5b2558c95c1b047462cfbe48
NaimSantos <naimsantos45@gmail.com>
2019-11-01 07:36:56 -0300

    Patch 1

--- c61257789.lua
+++ c61257789.lua
@@ -1,4 +1,5 @@
 --スターダスト・ドラゴン／バスター
+--Stardust Dragon/Assault Mode
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
--- c78661338.lua
+++ c78661338.lua
@@ -1,5 +1,5 @@
 --幻創龍ファンタズメイ
---Illusion Dragon Phantazmei
+--Fantastical Dragon Phantazmay
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)

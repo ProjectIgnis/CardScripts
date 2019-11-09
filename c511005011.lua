639080e641c5ce32f4259aff1b1f8caa54ec384e
NaimSantos <naimsantos45@gmail.com>
2019-11-09 19:28:38 -0300

    Fixes 3

--- c511005011.lua
+++ c511005011.lua
@@ -1,30 +1,25 @@
 --Negative Energy
---  By Shad3
-
-local scard=s
-
-function scard.initial_effect(c)
+--original script by Shad3
+local s,id=GetID()
+function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetTarget(scard.act_tg)
-	e1:SetOperation(scard.act_op)
+	e1:SetTarget(s.act_tg)
+	e1:SetOperation(s.act_op)
 	c:RegisterEffect(e1)
 end
-
-function scard.act_fil(c)
+function s.act_fil(c)
 	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DARK)
 end
-
-function scard.act_tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(scard.act_fil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(scard.act_fil,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+function s.act_tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.act_fil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.act_fil,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,0,0,0)
 end
-
-function scard.act_op(e,tp,eg,ep,ev,re,r,rp)
-	local tg=Duel.GetMatchingGroup(scard.act_fil,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+function s.act_op(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetMatchingGroup(s.act_fil,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	if #tg>0 then
 		local tc=tg:GetFirst()
 		local c=e:GetHandler()
--- c511005019.lua
+++ c511005019.lua
@@ -1,37 +1,31 @@
 --Incornceivable
---  By Shad3
-
-local scard=s
-
-function scard.initial_effect(c)
+--original script by Shad3
+local s,id=GetID()
+function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetCondition(scard.cd)
-	e1:SetTarget(scard.tg)
-	e1:SetOperation(scard.op)
+	e1:SetCondition(s.cd)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 end
 s.listed_names={511005044}
-
-function scard.fil(c,e,tp)
+function s.fil(c,e,tp)
 	return c:IsCode(511005044) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-
-function scard.cd(e,tp,eg,ep,ev,re,r,rp)
+function s.cd(e,tp,eg,ep,ev,re,r,rp)
 	local ac=Duel.GetAttacker()
 	return ac:IsControler(1-tp) and ac:GetAttack()>=2500
 end
-
-function scard.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(scard.fil,tp,LOCATION_DECK,0,1,nil,e,tp) end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.fil,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
-
-function scard.op(e,tp,eg,ep,ev,re,r,rp)
+function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,scard.fil,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.fil,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
 	if not tc then return end
 	if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) then
 		Duel.ChangeAttackTarget(tc)

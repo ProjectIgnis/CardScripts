9639b63b5b677fde44927a7cb0d8d4a4537b83bd
NaimSantos <naimsantos45@gmail.com>
2019-11-09 20:19:50 -0300

    Fixes 4

--- c150000068.lua
+++ c150000068.lua
@@ -1,13 +1,14 @@
 --Action Card - Tenacity
-function c150000069.initial_effect(c)
+local s,id=GetID()
+function s.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_CONTROL)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c150000069.target)
-	e1:SetOperation(c150000069.activate)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--become action card
 	local e2=Effect.CreateEffect(c)
@@ -20,7 +21,7 @@ function c150000069.initial_effect(c)
 	e3:SetValue(TYPE_QUICKPLAY)
 	c:RegisterEffect(e3)
 end
-function c150000069.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
 	local tc=g:GetFirst()
 	if chkc then return chkc:IsControler(tp) and g:IsContains(chkc) and chkc:IsCanBeEffectTarget(e) end
@@ -28,7 +29,7 @@ function c150000069.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetTargetCard(g)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
 end
-function c150000069.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())

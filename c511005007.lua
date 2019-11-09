1b8f8b96d9d739df09efee58e1657757a8ec859a
NaimSantos <naimsantos45@gmail.com>
2019-11-09 19:09:19 -0300

    More fixes

--- c511005007.lua
+++ c511005007.lua
@@ -1,35 +1,29 @@
 --Guidance to Salvation
---  By Shad3
-
-local scard=s
-
-function scard.initial_effect(c)
+--original script by Shad3
+local s,id=GetID()
+function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetCondition(scard.cd)
-	e1:SetTarget(scard.tg)
-	e1:SetOperation(scard.op)
+	e1:SetCondition(s.cd)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 end
-
-function scard.cd(e,tp,eg,ep,ev,re,r,rp)
+function s.cd(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetLP(tp)<=1500
 end
-
-function scard.fil(c,e,tp)
+function s.fil(c,e,tp)
 	return c:IsType(TYPE_TUNER) and c:IsAttackBelow(1500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-
-function scard.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(scard.fil,tp,LOCATION_DECK,0,1,nil,e,tp) end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.fil,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
-
-function scard.op(e,tp,eg,ep,ev,re,r,rp)
+function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,scard.fil,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.fil,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
 	if tc then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end

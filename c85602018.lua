22eb5c4c7b1239f102e8be358f711ca1988833f8
edo9300 <edoardo762@gmail.com>
2019-11-25 19:05:29 +0100

    Updated THIS IS MY LAST RIPPLE JOJO!!!!

--- c85602018.lua
+++ c85602018.lua
@@ -5,120 +5,68 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetOperation(s.operation)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	aux.GlobalCheck(s,function()
-		s[0]=true
-		s[1]=true
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_TO_GRAVE)
 		ge1:SetOperation(s.checkop)
 		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetOperation(s.clear)
-		Duel.RegisterEffect(ge2,0)
 	end)
 end
 function s.cfilter(c,tp)
-	return c:IsControler(tp) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
+	return c:IsControler(tp) and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local ct1=eg:FilterCount(s.cfilter,nil,tp)
-	local ct2=eg:FilterCount(s.cfilter,nil,1-tp)
-	if ct1>0 then
-		s[tp]=true
-	end
-	if ct2>0 then
-		s[1-tp]=true
+	for p=0,1 do
+		if eg:IsExists(s.cfilter,1,nil,p) then
+			Duel.RegisterFlagEffect(p,id,RESET_PHASE+PHASE_END,0,1)
+		end
 	end
 end
-function s.clear(e,tp,eg,ep,ev,re,r,rp)
-	s[0]=false
-	s[1]=false
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if Duel.GetFlagEffect(tp,id)~=0 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+		e:SetLabel(1)
+	else
+		e:SetCategory(0)
+		e:SetLabel(0)
+	end
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--spsummon
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(id)
-	e1:SetLabel(1)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCondition(s.spcon)
-	e2:SetOperation(s.spop)
-	e2:SetLabelObject(e1)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-	local e3=e2:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCondition(s.spcon2)
-	Duel.RegisterEffect(e3,tp)
-	local e4=e2:Clone()
-	e4:SetCode(EVENT_CHAIN_END)
-	Duel.RegisterEffect(e4,tp)
-	local e5=e2:Clone()
-	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
-	Duel.RegisterEffect(e5,tp)
-	local e6=e2:Clone()
-	e6:SetCode(EVENT_DAMAGE_STEP_END)
-	Duel.RegisterEffect(e6,tp)
-	local e7=e2:Clone()
-	e7:SetCode(EVENT_PHASE+PHASE_END)
-	e7:SetCountLimit(1)
-	Duel.RegisterEffect(e7,tp)
-	if Duel.GetTurnPlayer()==tp then
-		local e8=Effect.CreateEffect(c)
-		e8:SetType(EFFECT_TYPE_FIELD)
-		e8:SetCode(EFFECT_SPSUMMON_PROC_G)
-		e8:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_BOTH_SIDE+EFFECT_FLAG_SET_AVAILABLE)
-		e8:SetRange(0xff)
-		e8:SetLabel(tp)
-		e8:SetLabelObject(e1)
-		e8:SetCondition(s.spcon3)
-		e8:SetOperation(s.spop3)
-		e8:SetReset(RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e8)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	else
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.spcon)
+		e1:SetOperation(s.spop)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
 end
 function s.spfilter(c,e,tp)
 	return c:IsAttackBelow(1500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():GetLabel()==1 and s[tp]
-end
-function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():GetLabel()==1 and Duel.GetCurrentChain()==0 and s[tp]
+	return Duel.GetFlagEffect(tp,id)~=0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabelObject():GetLabel()~=1 then return end
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		e:GetLabelObject():SetLabel(0)
-		Duel.Hint(HINT_CARD,0,id)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	end
-end
-function s.spcon3(e,c,og)
-	if c==nil then return true end
-	local tp=e:GetLabel()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetLabelObject():GetLabel()==1 and s[tp]
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-end
-function s.spop3(e,tp,eg,ep,ev,re,r,rp,c,og)
 	Duel.Hint(HINT_CARD,0,id)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	og:Merge(g)
-	e:GetLabelObject():SetLabel(0)
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 end

509d336271e3ea35153ee0ceea3dc1e5025392c0
NaimSantos <naimsantos45@gmail.com>
2019-11-29 20:59:57 -0300

    Patch 2: krawlers

--- c10424147.lua
+++ c10424147.lua
@@ -90,7 +90,7 @@ function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0
 		and g:GetClassCount(Card.GetCode)>2
 		and Duel.IsExistingMatchingCard(s.spfilter4,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-	local eg=aux.SelectUnselectGroup(g,e,tp,1,3,s.rescon,1,tp,HINTMSG_XMATERIAL)
+	local eg=aux.SelectUnselectGroup(g,e,tp,3,3,s.rescon,1,tp,HINTMSG_XMATERIAL)
 	Duel.SetTargetCard(eg)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end

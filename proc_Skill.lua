41013515e285d7f7ffca4250d2ed3477a365b58d
edo9300 <edoardo762@gmail.com>
2020-01-23 23:21:32 +0100

    Draw 1 less handling

--- proc_Skill.lua
+++ proc_Skill.lua
@@ -2,6 +2,9 @@ HINT_SKILL = 200
 HINT_SKILL_COVER = 201
 HINT_SKILL_FLIP  = 202
 -- HINT_SKILL_REMOVE = 203 (need to check with edo for name)
+aux.DrawlessToken={}
+aux.DrawlessToken[0]=nil
+aux.DrawlessToken[1]=nil
 function Auxiliary.AddFieldSkillProcedure(c,coverid,drawless)
 	c:Cover(coverid)
 	--place on field
@@ -24,36 +27,69 @@ function Auxiliary.AddFieldSkillProcedure(c,coverid,drawless)
 	c:RegisterEffect(e2)
 	
 	--If the card have an "You draw 1 less card at the beginning of the Duel" condition
-	-- if drawless==true then
-	-- local e1=Effect.CreateEffect(c)	
-	-- e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	-- e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	-- e1:SetCode(EVENT_STARTUP)
-	-- e1:SetCountLimit(1)
-	-- e1:SetRange(0x5f)
-	-- e1:SetOperation(Auxiliary.drawlessop)
-	-- c:RegisterEffect(e1)
-	-- end
-	
-	
+	if drawless then
+		local e1=Effect.CreateEffect(c)
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_STARTUP)
+		e1:SetCountLimit(1)
+		e1:SetRange(0x5f)
+		e1:SetOperation(Auxiliary.drawlessop)
+		c:RegisterEffect(e1)
+	end
+	if not aux.DrawlessGlobal then
+		aux.DrawlessGlobal=true
+		local e1=Effect.GlobalEffect()
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCountLimit(1)
+		e1:SetOperation(Auxiliary.drawlessreset)
+		Duel.RegisterEffect(e1,0)
+	end
 end
 function Auxiliary.fieldop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==0 then
+		Duel.DisableShuffleCheck(e:GetHandlerPlayer())
 		p=e:GetHandler():GetControler()
 		Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_SZONE,POS_FACEDOWN,true)
 		Duel.ChangePosition(e:GetHandler(),POS_FACEUP)
 	end
-		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
-			Duel.Draw(p,1,REASON_RULE)
-		end
+	if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
+		Duel.Draw(p,1,REASON_RULE)
+	end
 	e:SetLabel(1)
+	e:Reset()
 end
 function Auxiliary.drawlessop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local sg=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	local dg=sg:RandomSelect(tp,1)
-	if #dg==0 then return end
-	Duel.SendtoDeck(dg,nil,2,REASON_RULE)
+	if aux.DrawlessToken[e:GetHandlerPlayer()]==nil then
+		aux.DrawlessToken[e:GetHandlerPlayer()]=Duel.CreateToken(e:GetHandlerPlayer(),1)
+		Duel.SendtoDeck(aux.DrawlessToken[e:GetHandlerPlayer()],nil,0,REASON_RULE)
+	end
+	e:Reset()
+end
+function Auxiliary.drawlessreset(e,tp,eg,ep,ev,re,r,rp)
+	if aux.DrawlessToken[0] then
+		local wasinhand=aux.DrawlessToken[0]:IsLocation(LOCATION_HAND)
+		if not wasinhand then
+			Duel.DisableShuffleCheck(0)
+		end
+		Duel.SendtoDeck(aux.DrawlessToken[0],nil,-2,REASON_RULE)
+		if wasinhand then
+			Duel.ShuffleHand(0)
+		end
+	end
+	if aux.DrawlessToken[1] then
+		local wasinhand=aux.DrawlessToken[1]:IsLocation(LOCATION_HAND)
+		if not wasinhand then
+			Duel.DisableShuffleCheck(1)
+		end
+		Duel.SendtoDeck(aux.DrawlessToken[1],nil,-2,REASON_RULE)
+		if wasinhand then
+			Duel.ShuffleHand(1)
+		end
+	end
+	e:Reset()
 end
 
 

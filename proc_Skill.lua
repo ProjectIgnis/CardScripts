2507972f63b7f2251e1a6af1eddeb19a60995366
edo9300 <edoardo762@gmail.com>
2020-01-24 18:07:07 +0100

    Cleanup drawless registration

--- proc_Skill.lua
+++ proc_Skill.lua
@@ -5,6 +5,51 @@ HINT_SKILL_FLIP  = 202
 aux.DrawlessToken={}
 aux.DrawlessToken[0]=nil
 aux.DrawlessToken[1]=nil
+
+function aux.RegisterDrawless(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetOperation(Auxiliary.drawlessop)
+	c:RegisterEffect(e1)
+	if not aux.DrawlessGlobal then
+		aux.DrawlessGlobal=true
+		local e1=Effect.GlobalEffect()
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCountLimit(1)
+		e1:SetOperation(Auxiliary.drawlessreset)
+		Duel.RegisterEffect(e1,0)
+	end
+end
+
+function Auxiliary.drawlessop(e,tp,eg,ep,ev,re,r,rp)
+	if aux.DrawlessToken[e:GetHandlerPlayer()]==nil then
+		aux.DrawlessToken[e:GetHandlerPlayer()]=Duel.CreateToken(e:GetHandlerPlayer(),1)
+		Duel.SendtoDeck(aux.DrawlessToken[e:GetHandlerPlayer()],nil,0,REASON_RULE)
+	end
+	e:Reset()
+end
+function Auxiliary.drawlessreset(e,tp,eg,ep,ev,re,r,rp)
+	for i=0,1 do
+		if aux.DrawlessToken[i] then
+			local wasinhand=aux.DrawlessToken[i]:IsLocation(LOCATION_HAND)
+			if not wasinhand then
+				Duel.DisableShuffleCheck(true)
+			end
+			Duel.SendtoDeck(aux.DrawlessToken[i],nil,-2,REASON_RULE)
+			if wasinhand then
+				Duel.ShuffleHand(i)
+			end
+		end
+	end
+	e:Reset()
+end
+
 function Auxiliary.AddFieldSkillProcedure(c,coverid,drawless)
 	c:Cover(coverid)
 	--place on field
@@ -28,66 +73,16 @@ function Auxiliary.AddFieldSkillProcedure(c,coverid,drawless)
 	
 	--If the card have an "You draw 1 less card at the beginning of the Duel" condition
 	if drawless then
-		local e1=Effect.CreateEffect(c)
-		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_STARTUP)
-		e1:SetCountLimit(1)
-		e1:SetRange(0x5f)
-		e1:SetOperation(Auxiliary.drawlessop)
-		c:RegisterEffect(e1)
-	end
-	if not aux.DrawlessGlobal then
-		aux.DrawlessGlobal=true
-		local e1=Effect.GlobalEffect()
-		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_PREDRAW)
-		e1:SetCountLimit(1)
-		e1:SetOperation(Auxiliary.drawlessreset)
-		Duel.RegisterEffect(e1,0)
+		aux.RegisterDrawless(c)
 	end
 end
 function Auxiliary.fieldop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==0 then
-		Duel.DisableShuffleCheck(e:GetHandlerPlayer())
-		p=e:GetHandler():GetControler()
-		Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_SZONE,POS_FACEDOWN,true)
-		Duel.ChangePosition(e:GetHandler(),POS_FACEUP)
-	end
+	Duel.DisableShuffleCheck(true)
+	p=e:GetHandler():GetControler()
+	Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_SZONE,POS_FACEDOWN,true)
+	Duel.ChangePosition(e:GetHandler(),POS_FACEUP)
 	if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
 		Duel.Draw(p,1,REASON_RULE)
-	end
-	e:SetLabel(1)
-	e:Reset()
-end
-function Auxiliary.drawlessop(e,tp,eg,ep,ev,re,r,rp)
-	if aux.DrawlessToken[e:GetHandlerPlayer()]==nil then
-		aux.DrawlessToken[e:GetHandlerPlayer()]=Duel.CreateToken(e:GetHandlerPlayer(),1)
-		Duel.SendtoDeck(aux.DrawlessToken[e:GetHandlerPlayer()],nil,0,REASON_RULE)
-	end
-	e:Reset()
-end
-function Auxiliary.drawlessreset(e,tp,eg,ep,ev,re,r,rp)
-	if aux.DrawlessToken[0] then
-		local wasinhand=aux.DrawlessToken[0]:IsLocation(LOCATION_HAND)
-		if not wasinhand then
-			Duel.DisableShuffleCheck(0)
-		end
-		Duel.SendtoDeck(aux.DrawlessToken[0],nil,-2,REASON_RULE)
-		if wasinhand then
-			Duel.ShuffleHand(0)
-		end
-	end
-	if aux.DrawlessToken[1] then
-		local wasinhand=aux.DrawlessToken[1]:IsLocation(LOCATION_HAND)
-		if not wasinhand then
-			Duel.DisableShuffleCheck(1)
-		end
-		Duel.SendtoDeck(aux.DrawlessToken[1],nil,-2,REASON_RULE)
-		if wasinhand then
-			Duel.ShuffleHand(1)
-		end
 	end
 	e:Reset()
 end
@@ -100,27 +95,15 @@ end
 function Auxiliary.AddContinuousSkillProcedure(c,coverid,drawless,flip)
 	c:Cover(coverid)
 	--activate
-	if flip==true then
-		local e1=Effect.CreateEffect(c)	
-		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_STARTUP)
-		e1:SetCountLimit(1)
-		e1:SetRange(0x5f)
-		e1:SetLabel(0)
-		e1:SetOperation(Auxiliary.continuousFlipOp)
-		c:RegisterEffect(e1)
-	else
-		local e1=Effect.CreateEffect(c)	
-		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_STARTUP)
-		e1:SetCountLimit(1)
-		e1:SetRange(0x5f)
-		e1:SetLabel(0)
-		e1:SetOperation(Auxiliary.continuousOp)
-		c:RegisterEffect(e1)
-	end
+	local e1=Effect.CreateEffect(c)	
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(Auxiliary.continuousOp(flip))
+	c:RegisterEffect(e1)
 	--cannot to deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
@@ -129,17 +112,22 @@ function Auxiliary.AddContinuousSkillProcedure(c,coverid,drawless,flip)
 	e2:SetCode(EFFECT_CANNOT_TO_DECK)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-end
-function Auxiliary.continuousFlipOp(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==0 then
-		p=e:GetHandler():GetControler()
-		Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_SZONE,POS_FACEDOWN,true,(1 << 2))
-		Duel.ChangePosition(e:GetHandler(),POS_FACEUP)
+	if drawless then
+		aux.RegisterDrawless(c)
 	end
+end
+function Auxiliary.continuousOp(flip)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		p=e:GetHandlerPlayer()
+		Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_SZONE,POS_FACEDOWN,true,(1 << 2))
+		if flip then
+			Duel.ChangePosition(e:GetHandler(),POS_FACEUP)
+		end
 		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
 			Duel.Draw(p,1,REASON_RULE)
 		end
-	e:SetLabel(1)
+		e:Reset()
+	end
 end
 
 
@@ -169,22 +157,25 @@ function Auxiliary.AddSkillProcedure(c,coverid,drawless,skillcon,skillop)
 	e1:SetCondition(skillcon)
 	e1:SetOperation(skillop)
 	Duel.RegisterEffect(e1,tp)
+	if drawless then
+		aux.RegisterDrawless(c)
+	end
 end
 
 -- Duel.Hint(HINT_SKILL_COVER,1,coverID|(BackEntryID<<32))
 -- Duel.Hint(HINT_SKILL,1,FrontID)
 function Auxiliary.SetSkillOp(e,tp,eg,ep,ev,re,r,rp)
-local coverid = e:GetLabel()
-		local c=e:GetHandler()
-		if e:GetLabel()>0 then
-			--generate the skill in the "skill zone"
-			Duel.Hint(HINT_SKILL_COVER,c:GetControler(),coverid|(coverid<<32))
-			Duel.Hint(HINT_SKILL,c:GetControler(),c:GetCode())			
-			--send to limbo then draw 1 if the skill was in the hand
-			Duel.SendtoDeck(c,tp,-2,REASON_RULE)
-			if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
-				Duel.Draw(p,1,REASON_RULE)
-			end
+	local coverid = e:GetLabel()
+	local c=e:GetHandler()
+	if e:GetLabel()>0 then
+		--generate the skill in the "skill zone"
+		Duel.Hint(HINT_SKILL_COVER,c:GetControler(),coverid|(coverid<<32))
+		Duel.Hint(HINT_SKILL,c:GetControler(),c:GetCode())			
+		--send to limbo then draw 1 if the skill was in the hand
+		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
+		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
+			Duel.Draw(p,1,REASON_RULE)
 		end
-		e:SetLabel(0)
+	end
+	e:SetLabel(0)
 end

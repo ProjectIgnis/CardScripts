7715628de31f8491c37df20a4dddc57826c8b09c
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-25 21:23:16 +0100

    Updated Skill
    - Changed the script name to fit the new cdb
    - added Aroma Strategy

---
+++ skill/c300202007.lua
@@ -0,0 +1,48 @@
+-- Aroma Strategy
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)	
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+end
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+		if e:GetLabel()==0 then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PREDRAW)
+			e1:SetCondition(s.flipcon)
+			e1:SetOperation(s.flipop)
+			Duel.RegisterEffect(e1,tp)
+		end
+	e:SetLabel(1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	--aroma effect
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetOperation(s.operation)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetDecktopGroup(e:GetHandler():GetControler(),1)
+	if g:GetFirst():GetFlagEffect(id)==0 then
+		Duel.ConfirmDecktop(e:GetHandler():GetControler(),1)
+		g:GetFirst():RegisterFlagEffect(id,RESET_EVENT+0x1fe0000,0,1)
+	end
+end

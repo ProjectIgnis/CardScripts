ec75f6e744ed617e3834c48501c259b89ee4fb06
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-26 23:20:58 +0100

    added Two new Skill
    The World's Greatest Fisherman and Pal-O'Mine-zation!

---
+++ skill/c300202006.lua
@@ -0,0 +1,35 @@
+--Pal-O'Mine-zation!
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)==0 then return end
+	--condition
+	return aux.CanActivateSkill(tp) 
+	and Duel.IsExistingMatchingCard(Card.isType,c:GetControler(),LOCATION_HAND,1,nil,TYPE_NORMAL)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil)
+	local tc=g:GetFirst()
+	Duel.ConfirmCards(1-tp,tc)
+	Duel.ShuffleHand(tp)
+	
+	--fusion substitute
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_FUSION_SUBSTITUTE)
+	e1:SetCondition(s.subcon)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END)
+	tc:RegisterEffect(e1)
+	
+end
+function s.subcon(e)
+	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+end

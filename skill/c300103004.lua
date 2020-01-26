7173965e9d772b5f4464503e2f59c23e5e9c26ed
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-26 13:34:41 +0100

    added Servants of the Fallen King

---
+++ skill/c300103004.lua
@@ -0,0 +1,26 @@
+--Servants of the Fallen King
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opt check
+	if Duel.GetFlagEffect(ep,id)==0 then return end
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
+	and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	--opt register
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
+	
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
+function s.tgfilter(c)
+	return c:IsRace(RACE_ZOMBIE) and c:IsType(TYPE_NORMAL) and c:IsLevelBelow(3) and c:IsAbleToGrave()
+end

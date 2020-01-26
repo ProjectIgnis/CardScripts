d1fb0e8e6270a48525d5628aed95ee2855dc9a47
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-26 10:55:58 -0500

    Added Draw Change Skills
    - Destiny Draw
    - Gravekeeper's Lot
    - Flight of the Harpies

---
+++ skill/c300201002.lua
@@ -0,0 +1,67 @@
+-- Destiny Draw
+local s,id=GetID()
+function s.initial_effect(c)
+	--skill
+	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop)
+	if not s.global_check then
+		s.global_check=true
+		s[0]=nil
+		s[1]=nil
+		s[2]=0
+		s[3]=0
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if not s[tp] then s[tp]=Duel.GetLP(tp) end
+    if s[tp]>Duel.GetLP(tp) then
+        s[2+tp]=s[2+tp]+(s[tp]-Duel.GetLP(tp))
+        s[tp]=Duel.GetLP(tp)
+    end
+end
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and tp==Duel.GetTurnPlayer() and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0
+		and Duel.GetDrawCount(tp)>0 and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+		and s[2+tp]>=2000 
+	
+end
+function s.thfilter(c)
+	return c:IsAbleToHand()
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--ask if you want to activate the skill or not
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end 
+	
+	--draw replace
+	local dt=Duel.GetDrawCount(tp)
+	if dt~=0 then
+		_replace_count=0
+		_replace_max=dt
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_DRAW_COUNT)
+		e1:SetTargetRange(1,0)
+		e1:SetReset(RESET_PHASE+PHASE_DRAW)
+		e1:SetValue(0)
+		Duel.RegisterEffect(e1,tp)
+	end
+	
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g~=0 then
+		if Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 then
+			Duel.Hint(HINT_SKILL_FLIP,0,id|(2<<32))
+		end
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

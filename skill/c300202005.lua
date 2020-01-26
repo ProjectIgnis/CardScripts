84325166b5f34eb537110881eba26f9715341f7e
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-26 17:07:12 +0100

    Test adding Last Gamble

---
+++ skill/c300202005.lua
@@ -0,0 +1,35 @@
+-- Last Gamble
+local s,id=GetID()
+function s.initial_effect(c)
+	--skill
+	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==5 
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	--ask if you want to activate the skill or not
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,2)) then return end 
+	
+		Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+		Duel.Hint(HINT_CARD,0,id)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCondition(s.con)
+		e1:SetOperation(s.op)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.skillcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetLP(tp)>100 and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,nil)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.SetLP(tp,100)
+	Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
+	local dc=Duel.TossDice(tp,1)
+	Duel.Draw(tp,dc,REASON_EFFECT)
+end

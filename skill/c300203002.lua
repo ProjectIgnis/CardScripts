c914127b5ec15a0d03d3e8f2efe171c546a2f334
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-26 18:54:20 +0100

    Added Nightmare Sonic Blast!

---
+++ skill/c300203002.lua
@@ -0,0 +1,54 @@
+--Nightmare Sonic Blast!
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
+end
+function s.flipconfilter(c)
+	return c:IsFaceup() and c:IsCode(66516792)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)==0 then return end
+	--condition
+	return aux.CanActivateSkill(tp) 
+	and Duel.IsExistingMatchingCard(s.flipconfilter,c:GetControler(),LOCATION_ONFIELD,0,1,nil)
+	and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>3
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
+	
+	Duel.ConfirmDecktop(tp,4)
+	local g=Duel.GetDecktopGroup(tp,4)
+	local ct=g:FilterCount(Card.IsType,nil,TYPE_MONSTER)
+	if ct=1
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:FilterSelect(tp,Card.IsAbleToHand,1,1,nil)
+		Duel.DisableShuffleCheck()
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.ShuffleHand(tp)
+		Duel.SortDecktop(tp,tp,3)
+	elseif ct=2
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+		Duel.HintSelection(dg)
+		Duel.Destroy(dg,REASON_EFFECT)
+		Duel.SortDecktop(tp,tp,4)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:FilterSelect(tp,Card.IsAbleToHand,1,1,nil)
+		Duel.DisableShuffleCheck()
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.ShuffleHand(tp)
+		
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local dg=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+		Duel.HintSelection(dg)
+		Duel.Destroy(dg,REASON_EFFECT)
+		Duel.SortDecktop(tp,tp,3)
+	end
+end

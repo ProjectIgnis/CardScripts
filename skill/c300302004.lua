c35ce7ea55f73ba87aa5144f7b2ed1bf8cd01b1e
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-26 17:31:13 +0100

    Adding Digging for Gold

---
+++ skill/c300302004.lua
@@ -0,0 +1,30 @@
+--Digging for Gold
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
+end
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)==0 then return end
+	--condition
+	return aux.CanActivateSkill(tp) and Duel.GetMatchingGroupCount(Card.IsAbleToRemove,c:GetControler(),LOCATION_GRAVE,0,nil,POS_FACEUP)>=3
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	local g
+	if Duel.IsPlayerAffectedByEffect(tp,69832741) then
+		g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,0,3,3,nil)
+	else
+		g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_GRAVE,0,3,3,nil)
+	end
+	if #g>0 then
+		if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)~=0 then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end

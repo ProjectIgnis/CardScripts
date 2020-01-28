e46b62b875c64d609b08ee1c135a54528566c999
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-28 16:53:34 +0100

    Power Up fix
    Fixed the activation condition
    a monster must be face up on the field to activate this skill

--- skill/c300301001.lua
+++ skill/c300301001.lua
@@ -3,14 +3,12 @@ local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
 end
-
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--opd check
-	if Duel.GetFlagEffect(ep,id)==0 then return end
+	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
-	return aux.CanActivateSkill(tp)
+	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
-
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)

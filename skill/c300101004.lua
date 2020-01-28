a1f5d1b48e7513d0af6fe2a8b6671d4f0f95f460
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-28 16:37:31 +0100

    Royal Flush Fix
    Fixed the activation condition
    removed useless OPT check
    Improved the "King and Jack cannot attack directly" check

--- skill/c300101004.lua
+++ skill/c300101004.lua
@@ -1,18 +1,14 @@
 -- Royal Flush
---Skill activation
--- Flip this card over when you activate this Skill.
--- Effect
--- Once per turn, if you control "Queen's Knight", you can pay 1000 LP to Normal Summon 1 "King's Knight" in addition to your Normal Summon or Set. "King's Knight" and "Jack's Knight" you control cannot attack directly the turn you use this Skill.
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)	
 end
-
+function s.cfilter(c,code, code2)
+	return (c:IsCode(code) or c:IsCode(code2)) and c:IsDirectAttacked()
+end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--opt check
-	if Duel.GetFlagEffect(ep,id)==0 then return end
 	--"cost" check
-	if not (Duel.CheckLPCost(tp,1000) and Duel.GetCustomActivityCount(id,tp,ACTIVITY_ATTACK)==0) then return false end
+	if not (Duel.CheckLPCost(tp,1000) and not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,64788463,90876561)) then return false end
 	--condition
 	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()==tp and (Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2)
 	and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,25652259),tp,LOCATION_ONFIELD,0,1,nil)
@@ -21,8 +17,6 @@ end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
-	--opt register
-	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
 	--cost
 	Duel.PayLPCost(tp,1000)
 	local e1=Effect.CreateEffect(e:GetHandler())

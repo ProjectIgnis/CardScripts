b58128c5ad61b9114c6844603fc19b1913c4c2b7
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-28 17:10:08 +0100

    Switcheroo Fix
    removed useless OPT flag
    Fixed the "Shuffle into deck then draw" that was inverted
    Added "player can draw" check

--- skill/c300104005.lua
+++ skill/c300104005.lua
@@ -1,31 +1,25 @@
 --Switcheroo
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)	
 end
-
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--twice per duel check
-	if Duel.GetFlagEffect(ep,id)<2 then return end
-	--opt check
-	if Duel.GetFlagEffect(ep,id+100)>0 then return end
+	if Duel.GetFlagEffect(ep,id)>1 then return end
 	--condition
-	return aux.CanActivateSkill(tp)
+	return aux.CanActivateSkill(tp) and Duel.IsPlayerCanDraw(tp,1)
 end
-
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
 	--used skill flag register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
-	--opt register
-	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
 	local c=e:GetHandler()
-	
-	local ct=Duel.Draw(tp,1,REASON_EFFECT)
-	if ct~=0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_HAND,0,1,1,nil)
-		Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 and Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
+		Duel.ShuffleDeck(tp)
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
 	end
+	
 end

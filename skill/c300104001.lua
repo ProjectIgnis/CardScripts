9f2027790b23b85396e085d52c779a53a16abb4c
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-28 19:43:46 +0100

    Zombie master Fix
    Fixed the activation condition
    Added a check on "you control a level 4- zombie"
    Added the "normal monster become effect" rule

--- skill/c300104001.lua
+++ skill/c300104001.lua
@@ -3,14 +3,12 @@ local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
 end
-
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--opd check
-	if Duel.GetFlagEffect(ep,id)==0 then return end
+	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
-	return aux.CanActivateSkill(tp)
+	return aux.CanActivateSkill(tp)  and Duel.IsExistingMatchingCard(s.ZMfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
@@ -32,12 +30,19 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetOperation(s.spop)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
+		if not tc:IsType(TYPE_EFFECT) then
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_ADD_TYPE)
+			e2:SetValue(TYPE_EFFECT)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2,true)
+		end
 	end
 end
 function s.ZMfilter(c)
 	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_ZOMBIE)
 end
-
 function s.costfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost()
 end

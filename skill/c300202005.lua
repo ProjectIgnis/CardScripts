bb6cced290b55ac5c833c443a8fc10a4b6222a14
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-01-29 12:49:38 +0100

    Last Gamble Fix
    Fixed both condition

--- skill/c300202005.lua
+++ skill/c300202005.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
-	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==5 
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==5
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--ask if you want to activate the skill or not
@@ -14,7 +14,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	
 		Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 		Duel.Hint(HINT_CARD,0,id)
-		local e1=Effect.CreateEffect(c)
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_FREE_CHAIN)
 		e1:SetCondition(s.con)
@@ -23,11 +23,16 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.con(e,tp,eg,ep,ev,re,r,rp)
 	--condition
-	return Duel.GetCurrentChain()==0 and Duel.GetLP(tp)>100 and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,nil)
+	return aux.CanActivateSkill(tp) and Duel.GetLP(tp)>100 and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,nil) and Duel.GetFlagEffect(ep,id)==0
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,id)
 	Duel.SetLP(tp,100)
+	
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
+	
+	
 	Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
 	local dc=Duel.TossDice(tp,1)
 	Duel.Draw(tp,dc,REASON_EFFECT)

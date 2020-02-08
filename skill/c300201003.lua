dba274a72b80d0cdde05474bf8c09ddd701a30b8
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-02-08 13:35:10 +0100

    Added Final Draw Prototype
    Atm, work only with the regular draw phase and missing the lose effect
    need a way to change duel.draw behavior

---
+++ skill/c300201003.lua
@@ -0,0 +1,63 @@
+--Final Draw
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)	
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return aux.CanActivateSkill(tp)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	local c=e:GetHandler()
+	
+	if Duel.GetFlagEffect(tp,id)==0 then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+		e2:SetProperty(EFFECT_FLAG_DELAY)
+		e2:SetCode(EVENT_PREDRAW)
+		e2:SetCondition(s.searchcon)
+		e2:SetTarget(s.searchtg)
+		e2:SetOperation(s.searchop)
+		Duel.RegisterEffect(e2,tp)
+	end
+	--1 flag = 1 counter
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	Debug.Message(Duel.GetFlagEffect(tp,id))
+end
+function s.searchcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer()
+		and Duel.GetFlagEffect(tp,id)>2
+		and Duel.GetDrawCount(tp)>0
+end
+function s.searchfilter(c)
+	return c:IsAbleToHand()
+end
+function s.searchtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	local dt=Duel.GetDrawCount(tp)
+	if dt~=0 then
+		_replace_count=0
+		_replace_max=dt
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_DRAW_COUNT)
+		e1:SetTargetRange(1,0)
+		e1:SetReset(RESET_PHASE+PHASE_DRAW)
+		e1:SetValue(0)
+		Duel.RegisterEffect(e1,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.searchop(e,tp,eg,ep,ev,re,r,rp)
+	_replace_count=_replace_count+1
+	if _replace_count>_replace_max then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g~=0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

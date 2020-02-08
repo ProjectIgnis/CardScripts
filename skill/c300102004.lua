76fd9fa5da5b288d41380a4a3f0d3892a788dd1a
edo9300 <edoardo762@gmail.com>
2020-02-08 20:15:53 +0100

    Use new con dice effect with it's my lucky day

--- skill/c300102004.lua
+++ skill/c300102004.lua
@@ -5,7 +5,7 @@ local register=function(what)
 	return function(...)
 		local params={...}
 		local tp=params[1]
-		if Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		if Duel.CheckLPCost(tp,1000) and Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 			Duel.SetFlagEffectLabel(tp,id,1)
 		end
 		return what(...)
@@ -27,25 +27,41 @@ function s.initial_effect(c)
 	aux.GlobalCheck(s,function()
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EFFECT_TOSS_DICE_REPLACE)
+		ge1:SetCode(EFFECT_TOSS_DICE_CHOOSE)
 		ge1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)return Duel.GetFlagEffect(ep,id)>0 and Duel.GetFlagEffectLabel(ep,id)>0 end)
-		ge1:SetOperation(s.repop(Duel.GetDiceResult,Duel.SetDiceResult,function(tp) return Duel.AnnounceNumber(tp,1,2,3,4,5,6,7) end))
-		Duel.RegisterEffect(ge1,0)	
+		ge1:SetOperation(s.repop("dice",Duel.GetDiceResult,Duel.SetDiceResult,function(tp) Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3)) return Duel.AnnounceNumber(tp,1,2,3,4,5,6) end))
+		Duel.RegisterEffect(ge1,0)
 		local ge2=ge1:Clone()
-		ge2:SetCode(EFFECT_TOSS_COIN_REPLACE)
-		ge2:SetOperation(s.repop(Duel.GetCoinResult,Duel.SetCoinResult,function(tp) return 1-Duel.AnnounceCoin(tp) end))
-		Duel.RegisterEffect(ge2,0)	
+		ge2:SetCode(EFFECT_TOSS_COIN_CHOOSE)
+		ge2:SetOperation(s.repop("coin",Duel.GetCoinResult,Duel.SetCoinResult,function(tp) Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,4)) return 1-Duel.AnnounceCoin(tp) end))
+		Duel.RegisterEffect(ge2,0)
 	end)
 end
-function s.repop(func1,func2,func3)
+function s.repop(typ,func1,func2,func3)
 	return function(e,tp,eg,ep,ev,re,r,rp)
+		Duel.PayLPCost(ep,1000)
 		Duel.ResetFlagEffect(ep,id)
 		Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 		Duel.Hint(HINT_CARD,0,id)
 		local dc={func1()}
 		local ct=(ev&0xff)+(ev>>16)
-		for i=1,ct do
-			dc[i]=func3(ep)
+		local ac=1
+		if ct>1 then
+			if typ=="dice" then
+				Duel.Hint(HINT_SELECTMSG,ep,aux.Stringid(id,1))
+				local val,idx=Duel.AnnounceNumber(ep,table.unpack(dc,1,ct))
+				ac=idx+1
+			else
+				local tab={}
+				for i=1,ct do
+					table.insert(tab,60+(1-dc[i]))
+				end
+				Duel.Hint(HINT_SELECTMSG,ep,aux.Stringid(id,2))
+				Duel.SelectOption(ep,table.unpack(tab))
+			end
+			dc[ac]=func3(ep)
+		else
+			dc[1]=func3(ep)
 		end
 		func2(table.unpack(dc))
 	end

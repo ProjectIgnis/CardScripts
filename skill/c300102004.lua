f29e6a47a87134add476fc3068b70c702a58df7a
edo9300 <edoardo762@gmail.com>
2020-02-08 19:15:03 +0100

    It's My Lucky Day! more cleanup, only use 1 flag effect

--- skill/c300102004.lua
+++ skill/c300102004.lua
@@ -1,24 +1,20 @@
 --It's My Lucky Day!
 --credit: edo9300
 local s,id=GetID()
-local tossc=Duel.TossDice
-Duel.TossDice=function(...)
-	local params={...}
-	local tp=params[1]
-	if Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.RegisterFlagEffect(tp,id+1,0,0,0)
+local register=function(what)
+	return function(...)
+		local params={...}
+		local tp=params[1]
+		if Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.SetFlagEffectLabel(tp,id,1)
+		end
+		return what(...)
 	end
-	return tossc(...)
 end
+local tossd=Duel.TossDice
+Duel.TossDice=register(tossd)
 local tossc=Duel.TossCoin
-Duel.TossCoin=function(...)
-	local params={...}
-	local tp=params[1]
-	if Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.RegisterFlagEffect(tp,id+1,0,0,0)
-	end
-	return tossc(...)
-end
+Duel.TossCoin=register(tossc)
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false)
 	local e1=Effect.CreateEffect(c)	
@@ -32,7 +28,7 @@ function s.initial_effect(c)
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EFFECT_TOSS_DICE_REPLACE)
-		ge1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)return Duel.GetFlagEffect(ep,id+1)>0 end)
+		ge1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)return Duel.GetFlagEffect(ep,id)>0 and Duel.GetFlagEffectLabel(ep,id)>0 end)
 		ge1:SetOperation(s.repop(Duel.GetDiceResult,Duel.SetDiceResult,function(tp) return Duel.AnnounceNumber(tp,1,2,3,4,5,6,7) end))
 		Duel.RegisterEffect(ge1,0)	
 		local ge2=ge1:Clone()
@@ -43,7 +39,6 @@ function s.initial_effect(c)
 end
 function s.repop(func1,func2,func3)
 	return function(e,tp,eg,ep,ev,re,r,rp)
-		Duel.ResetFlagEffect(ep,id+1)
 		Duel.ResetFlagEffect(ep,id)
 		Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 		Duel.Hint(HINT_CARD,0,id)

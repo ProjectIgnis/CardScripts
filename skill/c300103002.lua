3486e524d9ffe3d2884011c4cf95393477fb7b4e
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-02-08 20:35:28 +0100

    Spell Proof Armor Fix
    Fixed the activation and improved the effect being reseted when flipped down

--- skill/c300103002.lua
+++ skill/c300103002.lua
@@ -6,7 +6,7 @@ end
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--opt check
-	if Duel.GetFlagEffect(ep,id)==0 then return end
+	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
 	return aux.CanActivateSkill(tp)
 end
@@ -22,7 +22,6 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_IMMUNE_EFFECT)
 		e1:SetTargetRange(LOCATION_MZONE,0)
 		e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_MACHINE))
-		e1:SetCondition(s.con2)
 		e1:SetValue(s.efilter)
 		Duel.RegisterEffect(e1,tp)
 		--decrease tribute
@@ -31,8 +30,8 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetCode(EFFECT_DECREASE_TRIBUTE)
 		e2:SetTargetRange(LOCATION_HAND,0)
 		e2:SetTarget(s.atktg)
-		e2:SetCondition(s.con2)
 		e2:SetValue(0x10001)
+		e2:SetLabelObject(e1)
 		Duel.RegisterEffect(e2,tp)
 		--flip back if non machine in GY
 		local e3=Effect.CreateEffect(e:GetHandler())
@@ -40,6 +39,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 		e3:SetCode(EVENT_ADJUST)
 		e3:SetCondition(s.con2)
 		e3:SetOperation(s.op2)
+		e3:SetLabelObject(e2)
 		Duel.RegisterEffect(e3,tp)
 end
 function s.con1(e,tp,eg,ep,ev,re,r,rp)
@@ -55,9 +55,13 @@ function s.cfilter(c)
 	return c:IsFaceup() and not c:IsRace(RACE_MACHINE)
 end
 function s.con2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(ep,id)~=0 and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil)
 end
 function s.op2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(2<<32))
-	Duel.ResetFlagEffect(tp, id)
+	local e1=e:GetLabelObject():GetLabelObject()
+	local e2=e:GetLabelObject()
+	e1:Reset()
+	e2:Reset()
+	e:Reset()
 end

6db7e92b052affd44bf8d1b6a3ddc9fe7a7d9463
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-02-08 20:21:23 +0100

    Prescience Fix
    Forgot the LP check

--- skill/c300201004.lua
+++ skill/c300201004.lua
@@ -7,7 +7,7 @@ end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
-	return aux.CanActivateSkill(tp)
+	return aux.CanActivateSkill(tp) and Duel.GetLP(tp)<=(Duel.GetLP(1-tp)/2)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	
@@ -21,11 +21,11 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetDecktopGroup(tp,1)
 	local g2=Duel.GetDecktopGroup(1-tp,1)
 	
-	if g:GetFirst():GetFlagEffect(id)==0 then
+	if #g>0 and g:GetFirst():GetFlagEffect(id)==0 then
 		Duel.ConfirmDecktop(tp,1)
 		g:GetFirst():RegisterFlagEffect(id,RESET_EVENT+0x1fe0000,0,1)
 	end
-	if g2:GetFirst():GetFlagEffect(id)==0 then
+	if #g2>0 and g2:GetFirst():GetFlagEffect(id)==0 then
 		Duel.ConfirmDecktop(1-e:GetHandler():GetControler(),1)
 		g2:GetFirst():RegisterFlagEffect(id,RESET_EVENT+0x1fe0000,0,1)
 	end
@@ -41,11 +41,11 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetDecktopGroup(e:GetHandler():GetControler(),1)
 	local g2=Duel.GetDecktopGroup(1-e:GetHandler():GetControler(),1)
 	
-	if g:GetFirst():GetFlagEffect(id)==0 then
+	if #g>0 and g:GetFirst():GetFlagEffect(id)==0 then
 		Duel.ConfirmDecktop(tp,1)
 		g:GetFirst():RegisterFlagEffect(id,RESET_EVENT+0x1fe0000,0,1)
 	end
-	if g2:GetFirst():GetFlagEffect(id)==0 then
+	if #g2>0 and g2:GetFirst():GetFlagEffect(id)==0 then
 		Duel.ConfirmDecktop(tp,1)
 		g2:GetFirst():RegisterFlagEffect(id,RESET_EVENT+0x1fe0000,0,1)
 	end

dcf3abf58c8eb548314f0c0fbad102e7a5f6f95a
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-02-09 13:22:27 +0100

    The Greatest Fisherman! Fix
    Fixed activation condition and filter

--- skill/c300102001.lua
+++ skill/c300102001.lua
@@ -1,38 +1,37 @@
 --The World's Greatest Fisherman
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)	
 end
-
 function s.cfilter(c)
-	local lv=c:GetOriginalLevel()
+	local lvl=c:GetOriginalLevel()
 	return c:GetLevel()>0 and c:IsAttribute(ATTRIBUTE_WATER) and c:IsDiscardable()
-	and Duel.IsExistingTarget(s.filter2,tp,LOCATION_GRAVE,0,1,nil,lv) 
+	and Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil,lvl,c:GetCode()) 
 end
-function s.filter2(c,lv)
-	return c:IsFaceup() and c:IsLevelBelow(lv*2)  and c:IsAbleToHand() and c:IsAttribute(ATTRIBUTE_WATER)
+function s.thfilter(c,lvl,code)
+	return c:IsFaceup() and c:IsLevelBelow(lvl*2)  and c:IsAbleToHand() and c:IsAttribute(ATTRIBUTE_WATER)
+	and not c:IsCode(code)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--opt check
-	if Duel.GetFlagEffect(ep,id)==0 then return end
+	if Duel.GetFlagEffect(ep,id)>0 then return end
 	--condition
 	return aux.CanActivateSkill(tp) 
-	and Duel.IsExistingMatchingCard(s.cfilter,c:GetControler(),LOCATION_HAND,0,1,nil)
+	and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil)
 	and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>=2
-	and Duel.CheckLPCost(tp,100)
+	and Duel.CheckLPCost(tp,500)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
-	--opt register
-	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,0)
 	
 	Duel.PayLPCost(tp,500)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
 	local lvl=g:GetFirst():GetLevel()
+	local code=g:GetFirst():GetCode()
 	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
-	local g2=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_GRAVE,0,1,1,nil,lvl)
-	if Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 then
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil,lvl,code)
+	if Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 then
 		Duel.Hint(HINT_SKILL_FLIP,0,id|(2<<32))
 	end
 end

af9f3bcb633c6a304f9433c6bd0fe93477604503
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-02-09 15:22:28 +0100

    Catch of the Day Rework and fixes

--- skill/c300102003.lua
+++ skill/c300102003.lua
@@ -1,62 +1,92 @@
 --Catch of the Day
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)	
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+	
+	local e1=Effect.CreateEffect(c)	
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.flipop2)
+	c:RegisterEffect(e1)
 end
-
+function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
+	--immune
+	
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_BATTLE_DAMAGE)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	Duel.RegisterEffect(e1,tp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_BATTLE_DESTROYING)
+	e1:SetCondition(s.spcon2)
+	e1:SetOperation(s.spop)
+	Duel.RegisterEffect(e1,tp)
+end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--opd check
-	if Duel.GetFlagEffect(ep,id)==0 then return end
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.ffilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil,tp)
+
 	--condition
 	return aux.CanActivateSkill(tp) 
-	
+	and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+	and #g>0
 end
-function s.filter(c,tp)
+function s.cfilter(c)
+	return c:IsCode(3643300) and c:IsFaceup()
+end
+function s.ffilter(c,tp)
 	return c:IsCode(CARD_UMI) and c:GetActivateEffect() and c:GetActivateEffect():IsActivatable(tp,true)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
 	Duel.Hint(HINT_CARD,0,id)
-	--opt register
+	--opd register
 	Duel.RegisterFlagEffect(ep,id,0,0,0)
 	
-	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.filter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil,tp)
-	if Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_HAND,0,1,nil,3643300) and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.ffilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil,tp)
+
+	if Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_ONFIELD,0,1,nil,3643300) and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 		local tc=g:Select(tp,1,1,nil):GetFirst()
 		aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
 	end
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_BATTLE_DESTROYING)
-	e2:SetCondition(s.con)
-	e2:SetOperation(s.operation)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_BATTLE_DAMAGE)
-	e2:SetCondition(s.con2)
-	e2:SetOperation(s.op)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
 end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local rc=eg:GetFirst()
-	return rc:IsRelateToBattle() and rc:IsStatus(STATUS_OPPO_BATTLE)
-		and rc:IsFaceup() and rc:IsCode(3643300) and rc:IsControler(tp) 
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
-end
-function s.con2(e,tp,eg,ep,ev,re,r,rp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
-	return ep~=tp and tc:IsControler(tp) and tc:IsCode(3643300) 
+	if Duel.GetFlagEffect(ep,id+1)>0 then return end
+	return ep~=tp and tc:IsControler(tp) and tc:IsCode(3643300)
+	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.afilter(c,tp)
+	return c:IsControler(tp) and c:IsCode(3643300)
+end
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp,chk)
+	return eg:IsExists(s.afilter,1,nil,tp) 
+	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) 
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:GetLevel()<=4 and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end 
+	Duel.Hint(HINT_SKILL_FLIP,0,id|(1<<32))
+	Duel.Hint(HINT_CARD,0,id)
+	-- opt register
+	Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,0)
+	
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

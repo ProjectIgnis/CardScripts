5b390315a1a57ffbf13aa5abcfd685e62e72e720
NaimSantos <naimsantos45@gmail.com>
2020-02-22 16:58:30 -0300

    Fix "Sublime Knight - Expaladin"
    Replaced incorrect function name

--- official/c54485355.lua
+++ official/c54485355.lua
@@ -107,7 +107,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=g:GetFirst()
 	for tc in aux.Next(g) do
 		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-			tc:EnableDualState()
+			tc:EnableGeminiState()
 			tc:RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,64)
 		end
 	end

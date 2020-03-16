213a7f9fe97fa029ea54986b2d877038f50306fa
NaimSantos <naimsantos45@gmail.com>
2020-03-16 09:30:07 -0300

    Fix "Sea Stealth Attack"
    Should now call the correct strings

--- official/c19089195.lua
+++ official/c19089195.lua
@@ -37,7 +37,7 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.filter),tp,LOCATION_HAND+LOCATION_GRAVE,0,nil,tp)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 		local tc=g:Select(tp,1,1,nil):GetFirst()
 		aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)

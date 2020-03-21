11e921a29fe02a8e7148ceb60152ea0242d05a2f
edo9300 <edoardo762@gmail.com>
2020-03-21 17:02:04 +0100

    Fixed battle damaze zeroing for last turn

--- official/c28566710.lua
+++ official/c28566710.lua
@@ -41,7 +41,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
 		e1:SetTargetRange(1,1)
-		e1:SetReset(RESET_CHAIN)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
 		Duel.RegisterEffect(e1,tp)
 		if tc then Duel.ForceAttack(sc,tc) end
 	end

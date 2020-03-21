3ac5eddd3adc361aab6e58624254b3212e4105fe
edo9300 <edoardo762@gmail.com>
2020-03-21 15:29:21 +0100

    Correct function typo fix for caspar

--- official/c32841045.lua
+++ official/c32841045.lua
@@ -85,7 +85,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetCardID(e:GetLabel())
+	local tc=Duel.GetCardFromCardID(e:GetLabel())
 	if not tc then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,e:GetLabelObject(),tc:GetCode())

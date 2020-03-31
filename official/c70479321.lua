6db9b0d47f0f745f53f6d3989f7ad3f5a45a8624
NaimSantos <naimsantos45@gmail.com>
2020-03-31 20:42:43 -0300

    Fix "Performapal Drummerilla"

--- official/c70479321.lua
+++ official/c70479321.lua
@@ -1,47 +1,51 @@
 --EMドラミング・コング
+--Performapal Drummerilla
 local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
 	--atk
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCountLimit(1)
-	e2:SetCondition(s.atkcon)
-	e2:SetTarget(s.atktg)
-	e2:SetOperation(s.atkop)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.atkcon)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
 	--summon with no tribute
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e3:SetCode(EFFECT_SUMMON_PROC)
-	e3:SetCondition(s.ntcon)
-	c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_SUMMON_PROC)
+	e2:SetCondition(s.ntcon)
+	c:RegisterEffect(e2)
 	--change level
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_SUMMON_COST)
-	e4:SetOperation(s.lvop)
-	c:RegisterEffect(e4)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_SUMMON_COST)
+	e3:SetOperation(s.lvop)
+	c:RegisterEffect(e3)
 	--atk
-	local e5=e2:Clone()
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetOperation(s.atkop2)
-	c:RegisterEffect(e5)
+	local e4=e2:Clone()
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetOperation(s.atkop2)
+	c:RegisterEffect(e4)
 end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
 	if d and a:GetControler()~=d:GetControler() then
-		if a:IsControler(tp) then e:SetLabelObject(a)
-		else e:SetLabelObject(d) end
-		return true
+		if a:IsControler(tp) then
+			e:SetLabelObject(a)
+		else
+			e:SetLabelObject(d)
+		end
+	return true
 	else return false end
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)

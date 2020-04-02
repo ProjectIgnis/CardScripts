52ad1cb174195ec929359b50643a9a0dba39aa84
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-02 01:06:01 -0400

    Updated cards from "Structure Deck: Sacred Beasts of Chaos"
    Chaos Core
    * Should not be able to place counters or avoid battle damage if the sent cards did not end up in GY.
    
    Imperial Custom
    * Updated it to match with current errata (no longer protects continuous traps from self-destruction, like maintenance cost).

--- official/c9995766.lua
+++ official/c9995766.lua
@@ -1,22 +1,27 @@
 --宮廷のしきたり
+--Imperial Custom
+
 local s,id=GetID()
 function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
-	--activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--indestructable
+	--Continuous traps cannot be destroyed by battle or card effects
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_INDESTRUCTABLE)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
 	e2:SetTarget(s.infilter)
-	e2:SetValue(1)
+	e2:SetValue(indesval)
 	c:RegisterEffect(e2)
 end
 function s.infilter(e,c)
 	return (c:GetType()&0x20004)==0x20004 and c:GetCode()~=id
+end
+function s.indesval(e,re,r,rp)
+	return (r&REASON_RULE)==0
 end

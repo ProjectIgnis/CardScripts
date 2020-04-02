2ee2c9eaa200c7d8ab51d4d4b0b251ea7d3f9f98
NaimSantos <naimsantos45@gmail.com>
2020-04-02 20:55:19 -0300

    Added "Imperial Custom" pre-errata

---
+++ pre-errata/c9995767.lua
@@ -0,0 +1,23 @@
+--宮廷のしきたり
+--Imperial Custom (pre-errata)
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Continuous traps cannot be destroyed
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
+	e2:SetTarget(s.infilter)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+end
+function s.infilter(e,c)
+	return (c:GetType()&0x20004)==0x20004 and c:GetCode()~=id
+end

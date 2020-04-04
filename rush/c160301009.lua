9946b75ec3be0172690b1eae4c079f5c6d5b82a4
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-04 16:24:43 -0400

    Updated "Luminous Shaman"
    Was using the wrong script; rewrote it entirely.

--- rush/c160301009.lua
+++ rush/c160301009.lua
@@ -1,7 +1,9 @@
---Phoenix Dragon - フェニックス・ドラゴン
+--ルミナス・シャーマン
+--Luminous Shaman
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--to hand
+	--Let a level 4 or lower spellcaster attack directly
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_IGNITION)
@@ -12,24 +14,32 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
+	--Check if this card can be sent to GY
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+
 end
-function s.thfilter(c)
-	return c:IsRace(RACE_DRAGON) and c:IsLevelAbove(5) and  c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+	--Check for level 4 or lower spellcaster
+function s.filter(c)
+	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_SPELLCASTER)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE) 
+	--Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 end
+	--Send this card to the GY to let a level 4 or lower spellcaster attack directly
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	-- requirement
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-	-- effect
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+	--Requirement
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+	--Effect
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
 	end
 end

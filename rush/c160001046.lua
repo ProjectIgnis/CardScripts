88e3cee73123e7fef901495521b2a7597bc24a32
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-04 15:50:22 -0400

    Added "Counter Bomb" (Rush Duel)

---
+++ rush/c160001046.lua
@@ -0,0 +1,45 @@
+--カウンター・ボム
+--Counter Bomb
+
+--Substitute ID
+local s,id=GetID()
+function s.initial_effect(c)
+	--Inflict damage equal to opponent's attacking monster level x 200
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+	--Check for card in hand to send to GY
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end	
+end
+	--Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return true end
+	local tg=Duel.GetAttacker()
+	local lv=tg:GetLevel()
+	e:SetLabel(lv)
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(lv*200)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,lv*200)
+	Duel.SetChainLimit(s.chlimit)
+end
+function s.chlimit(e,ep,tp)
+	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+	--Send 1 card from hand to GY to inflict damage equal to opponent's attacking monster level x 200
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	--Effect
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end

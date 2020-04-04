eb0e3ac68b4d1b729fdd8f04491b4291827fd08d
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-04-04 20:19:35 +0200

    added new rush script
    Prima Guitarna of the Colorful Light

---
+++ rush/c160001028.lua
@@ -0,0 +1,42 @@
+--彩光のプリマギターナ
+--Prima Guitarna of the Colorful Light
+local s,id=GetID()
+function s.initial_effect(c)
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+if chk==0 then return Duel.CheckLPCost(tp,1000) end
+end
+function s.filter(c)
+	return c:IsCanChangePosition()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--requirement
+	Duel.PayLPCost(tp,1000)	
+	
+	--effect
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(ct*300)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end

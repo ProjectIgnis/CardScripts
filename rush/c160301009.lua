9f585a42078db4da90cc2a5110c4eaa084dde5c9
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-04 16:28:15 -0400

    Updated "Luminous Shaman"
    Removed leftover (and wrong) category for the effect.

--- rush/c160301009.lua
+++ rush/c160301009.lua
@@ -5,7 +5,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Let a level 4 or lower spellcaster attack directly
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)

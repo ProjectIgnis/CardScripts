44f1fcaa2967c995739cc2be6f49e51b87265af5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-05 21:49:32 +0300

    "Fortune Vision" fix
    The effect that makes battle damage 0 should now work properly.

--- official/c91407982.lua
+++ official/c91407982.lua
@@ -67,10 +67,12 @@ function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg and eg:IsExists(s.damfilter,1,nil,tp)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetCountLimit(1)
-	e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end

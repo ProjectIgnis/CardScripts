fbf2f5f5b5db94b9c7717a7e3f8ed37ccd881a6f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-06 04:18:40 +0300

    "Salamangreat Circle" fix

--- official/c52155219.lua
+++ official/c52155219.lua
@@ -10,30 +10,8 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
-	--recarnation check
-	aux.GlobalCheck(s,function()
-		local e1=Effect.GlobalEffect()
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_MATERIAL_CHECK)
-		e1:SetValue(s.val)
-		local ge1=Effect.GlobalEffect()
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
-		ge1:SetLabelObject(e1)
-		ge1:SetTargetRange(0xff,0xff)
-		ge1:SetTarget(s.gtg)
-		Duel.RegisterEffect(ge1,0)
-	end)
 end
 s.listed_series={0x119}
-function s.gtg(e,c)
-	return c:IsLinkMonster() and c:IsSetCard(0x119)
-end
-function s.val(e,c)
-	local g=c:GetMaterial()
-	if g:IsExists(Card.IsSummonCode,1,nil,c,SUMMON_TYPE_LINK,c:GetControler(),c:GetCode()) and c:IsSummonType(SUMMON_TYPE_LINK) then
-		c:RegisterFlagEffect(41463181,RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD-RESET_LEAVE-RESET_TEMP_REMOVE,0,1)
-	end
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.immfilter(chkc) end
 	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
@@ -70,7 +48,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.immfilter(c)
-	return c:IsLinkMonster() and c:IsSetCard(0x119) and c:GetFlagEffect(41463181)~=0
+	return c:IsLinkMonster() and c:IsSetCard(0x119) and c:IsReincarnationSummoned()
 end
 function s.immop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
@@ -89,4 +67,3 @@ end
 function s.efilter(e,te)
 	return te:IsActiveType(TYPE_MONSTER) and te:GetOwner()~=e:GetOwner()
 end
-

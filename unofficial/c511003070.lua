c06bc162c938fd09d26a674d4ad8bda820a5b0cd
NaimSantos <naimsantos45@gmail.com>
2020-04-06 17:37:14 -0300

    Updated "Luminous Dragon Ritual (Anime)" to use new Ritual Procedure

--- unofficial/c511003070.lua
+++ unofficial/c511003070.lua
@@ -1,5 +1,6 @@
 --踊りによる誘発
+--Luminous Dragon Ritual (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	Ritual.AddProcGreaterCode(c,85346853)
+	Ritual.AddProcGreaterCode(c,4,nil,85346853)
 end

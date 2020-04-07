3aa1ac2f4aec2f132330562f27766f361dcf880f
NaimSantos <naimsantos45@gmail.com>
2020-04-07 11:28:54 -0300

    Make "Absorb Fusion"'s fusion effect optional

--- official/c71422989.lua
+++ official/c71422989.lua
@@ -41,7 +41,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.operation(oldop,oldtg)
+function s.operation(oldtg,oldop)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)

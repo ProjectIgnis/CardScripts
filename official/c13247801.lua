db0321e688b5b8d4e2b986dbb1f3779eb88fbeae
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-09 01:02:45 -0400

    Updated "Machina Defense Region"
    Should not continue to the effect if there is no longer a valid target.

--- official/c13247801.lua
+++ official/c13247801.lua
@@ -73,7 +73,7 @@ function s.tohandtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.tohandop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end

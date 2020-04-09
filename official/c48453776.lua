eaeca5955a1a10a10c11a0b35b97855742dc1d2c
senpaizuri3 <senpaizuri3@gmail.com>
2020-04-08 23:59:25 -0400

    Small fix "Sky Scourge Norleras"
    Now you should banish monsters to Special Summon it.

--- official/c48453776.lua
+++ official/c48453776.lua
@@ -17,6 +17,7 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 	--to grave

30e4e9aaf1ffedceb95b7d34a01cb2293465d12e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-11 01:23:18 +0300

    "Apostle of the Nadir" fixes
    Should not be able to search "Dragma" Spells/Traps. Also, should prevent you from Special Summoning from the Extra Deck.

--- pre-release/c101101052.lua
+++ pre-release/c101101052.lua
@@ -1,5 +1,5 @@
 --天底の使徒
---Apostle of the Nadir
+--Disciple of the Nadir
 --Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
@@ -19,7 +19,7 @@ function s.tgfilter(c,e,tp)
 	return c:IsAbleToGrave() and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,c:GetAttack(),e,tp,c)
 end
 function s.thfilter(c,atk)
-	return (c:IsCode(CARD_ALBUS) or c:IsSetCard(0x244)) and c:GetAttack()<=atk and c:IsAbleToHand()
+	return (c:IsCode(CARD_ALBUS) or (c:IsType(TYPE_MONSTER) and c:IsSetCard(0x244))) and c:GetAttack()<=atk and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
@@ -38,4 +38,22 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoHand(g2,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g2)
 	end
+	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_OATH)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetTargetRange(1,0)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return c:IsLocation(LOCATION_EXTRA)
 end

2a24e88bf3957ce4cfe1ab2c6cb73d03571bcf50
NaimSantos <naimsantos45@gmail.com>
2020-04-10 13:54:24 -0300

    Removed duplicated function definition from "A.I. Shadow (Anime)"

--- unofficial/c511600314.lua
+++ unofficial/c511600314.lua
@@ -34,16 +34,6 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 s.listed_series={0x135}
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g>0 and Duel.SelectYesNo(tp,94) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:Select(tp,1,1,nil)
-		Duel.SendtoHand(sg,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,sg)
-	end
-end
 function s.tgfilter(c)
 	return c:IsControler(tp) and c:IsSetCard(0x135) and c:IsFaceup(0x135)
 end

c9eae206f8eae920d0ac3ae6f6ca7ff692427f0c
edo9300 <edoardo762@gmail.com>
2020-04-11 12:34:22 +0200

    Fix Albus's Nullius Filius
    Was allowint to continue the fusion summon even after leaving the field mid chain

--- pre-release/c101101011.lua
+++ pre-release/c101101011.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--fusion summon
-	local params = {nil,aux.FALSE,s.fextra,nil,Fusion.ForcedHandler}
+	local params = {nil,Fusion.ChechWithHandler(aux.FALSE),s.fextra,nil,Fusion.ForcedHandler}
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
@@ -21,9 +21,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.fextra(e,tp,mg)
-	local g=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil)
-	g:AddCard(e:GetHandler())
-	return g
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end

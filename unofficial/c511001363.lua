ca81dacd166f25c890403801e92110bdcef79724
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-11 04:28:42 +0300

    "CXyz Barian Hope (Anime)" fix
    Typo

--- unofficial/c511001363.lua
+++ unofficial/c511001363.lua
@@ -41,7 +41,7 @@ function s.initial_effect(c)
 end
 s.listed_series={0x1048}
 function s.cfilter(c)
-	return c:IsHasEffect(511002571) and #{c:GetCardEffect(id0)}==0
+	return c:IsHasEffect(511002571) and #{c:GetCardEffect(id)}==0
 end
 function s.op(e)
 	local g=Duel.GetMatchingGroup(s.cfilter,0,0xff,0xff,nil)
@@ -81,7 +81,7 @@ function s.op(e)
 			c:RegisterEffect(e1,true)
 			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(id0)
+			e2:SetCode(id)
 			e2:SetProperty(prop1,prop2)
 			if resetflag and resetcount then
 				e2:SetReset(resetflag,resetcount)

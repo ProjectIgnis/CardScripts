3ec08d797fe06e1a43915abd29ec667061bb4541
NaimSantos <naimsantos45@gmail.com>
2020-04-11 18:59:43 -0300

    Updated "Rokket Recharger"
    Prevent the script from executing all the operation if the target is nil

--- official/c5969957.lua
+++ official/c5969957.lua
@@ -64,7 +64,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+	if tc and tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,tc,e,tp)
 		if #g>0 then

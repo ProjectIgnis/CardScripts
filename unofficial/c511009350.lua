d774f4f0f244be7bbe27bb2904f1f0c95c821764
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-12 11:06:05 +0300

    "Predaplant Chimera Rafflesia (Anime)" fixes
    Added a check to prevent a script error and rewrote part of the script to remove redundancies.

--- unofficial/c511009350.lua
+++ unofficial/c511009350.lua
@@ -1,4 +1,5 @@
---Predator plant Chimere rafflesia
+--捕食植物キメラフレシア (Manga)
+--Predaplant Chimerafflesia (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
@@ -16,57 +17,32 @@ end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
-	return (a:GetControler()==tp and a:IsRelateToBattle() and d:GetAttack()~=d:GetBaseAttack())
+	return (a:GetControler()==tp and a:IsRelateToBattle() and d and d:GetAttack()~=d:GetBaseAttack())
 		or (d and d:GetControler()==tp and d:IsRelateToBattle() and a:GetAttack()~=a:GetBaseAttack())
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local atk=0
+	local bc
 	if e:GetHandler()==Duel.GetAttacker() then
-	local bc=Duel.GetAttackTarget()	
-	
-	if(bc:GetBaseAttack()>bc:GetAttack()) then
-	atk=bc:GetBaseAttack()-bc:GetAttack()
+		bc=Duel.GetAttackTarget()
 	else
-	atk=bc:GetAttack()>bc:GetBaseAttack()
+		bc=Duel.GetAttacker()
 	end
-	
+	if(bc:GetBaseAttack()>bc:GetAttack()) then
+		atk=bc:GetBaseAttack()-bc:GetAttack()
+	else
+		atk=bc:GetAttack()-bc:GetBaseAttack()
+	end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	e1:SetValue(bc:GetBaseAttack())
 	bc:RegisterEffect(e1)
-	
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
 	e2:SetValue(atk)
 	e:GetHandler():RegisterEffect(e2)
-	
-	else
-	
-	local bc=Duel.GetAttacker()	
-	
-	if(bc:GetBaseAttack()>bc:GetAttack()) then
-	atk=bc:GetBaseAttack()-bc:GetAttack()
-	else
-	atk=bc:GetAttack()>bc:GetBaseAttack()
-	end
-	
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e1:SetValue(bc:GetBaseAttack())
-	bc:RegisterEffect(e1)
-	
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
-	e2:SetValue(atk)
-	e:GetHandler():RegisterEffect(e2)
-	
-end
 end

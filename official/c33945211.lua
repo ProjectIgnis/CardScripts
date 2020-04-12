66f87cd1f0067e2422d024057ca76fcf7e3911b1
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-12 06:58:45 +0300

    "Marincess Basilalima" fix
    The search effect should now work for the 2nd player.

--- official/c33945211.lua
+++ official/c33945211.lua
@@ -40,9 +40,9 @@ function s.cfilter(c,tp)
 end
 s.listed_series={0x12b}
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 	e:SetLabel(g:GetFirst():GetCode())
 end

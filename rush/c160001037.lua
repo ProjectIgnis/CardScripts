35a1d0fd2317aa514b747d17f14dfef90d191441
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-04-14 14:36:06 +0200

    "Piercing!" Fix
    You cannot activate it if you cannot enter the battle phase

--- rush/c160001037.lua
+++ rush/c160001037.lua
@@ -5,9 +5,13 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end

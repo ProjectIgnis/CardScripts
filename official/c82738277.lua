cea1ecc83c59c8792f0ebe5a679c8eaf00a065d0
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-15 14:12:40 -0400

    Updated "Traptrix Mantis"
    Should not be able to set a card from hand, if the targeted card did not end up in the hand. Also no longer reveal the card set by its quick effect.

--- official/c82738277.lua
+++ official/c82738277.lua
@@ -1,7 +1,9 @@
 --ランカの蟲惑魔
+--Traptrix Mantis
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--immune
+	--Unaffected by "Hole" normal trap cards
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -9,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
 	e1:SetValue(s.efilter)
 	c:RegisterEffect(e1)
-	--search
+	--Upon normal summon, add 1 "Traptrix" monster
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -18,7 +20,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	--return
+	--Return 1 set S/T, then can set 1 S/T from hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TOHAND)
@@ -64,14 +66,14 @@ function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFacedown() and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+	if tc:IsRelateToEffect(e) and tc:IsFacedown() and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) then
 		local g=Duel.GetMatchingGroup(Card.IsSSetable,tp,LOCATION_HAND,0,nil)
 		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 			Duel.ShuffleHand(tp)
 			Duel.BreakEffect()
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 			local sg=g:Select(tp,1,1,nil)
-			Duel.SSet(tp,sg:GetFirst())
+			Duel.SSet(tp,sg:GetFirst(),tp,false)
 		end
 	end
 end

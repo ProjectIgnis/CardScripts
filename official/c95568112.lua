5fb73cae58f804746e2db596d546ce24c4e50413
NaimSantos <naimsantos45@gmail.com>
2020-04-17 12:19:03 -0300

    Updated  "Rain Bozu"

--- official/c95568112.lua
+++ official/c95568112.lua
@@ -3,7 +3,6 @@
 --
 local s,id=GetID()
 function s.initial_effect(c)
-	--pendulum summon
 	Pendulum.AddProcedure(c)
 	--atk
 	local e1=Effect.CreateEffect(c)
@@ -17,7 +16,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.atktg1)
 	e1:SetOperation(s.atkop1)
 	c:RegisterEffect(e1)
-	--atk/def
+	--atk/def while in the monster zone
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)

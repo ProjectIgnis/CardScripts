0184e8bdcec4d7db8f6e1497dc189c4913786f9e
AlphaKretin <deewhydeeecks@gmail.com>
2020-04-20 19:24:10 +0930

    Rescript "Darkness 2"

--- unofficial/c100000592.lua
+++ unofficial/c100000592.lua
@@ -1,44 +1,36 @@
 --ダークネス ２
 --Darkness 2
+--Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(TIMING_DAMAGE_STEP)
-	e1:SetCondition(s.con)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 end
-s.listed_names={100000591,id+1}
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
-end
-function s.cfilter(c)
-	return c:IsFaceup() and (c:IsCode(100000591) or c:IsCode(id) or c:IsCode(id+1))
-end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:GetControler()==tp and chkc:IsFaceup() end
-	if chk==0 then return true end
-	local ct=Duel.GetMatchingGroupCount(c100000591.cfilter,tp,LOCATION_ONFIELD,0,nil)
-	if ct>0 and Duel.GetFlagEffect(tp,100000590)~=0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
-	end
-end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	local ct=Duel.GetMatchingGroupCount(c100000591.cfilter,tp,LOCATION_ONFIELD,0,nil)
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetValue(ct*1000)
-		tc:RegisterEffect(e1)
+s.listed_names={100000594,100000595}
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:GetFlagEffect(100000594)~=0 then
+		if Duel.GetCurrentChain()==1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTACK)
+			local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+			local ac=g:Select(tp,1,1,nil):GetFirst()
+			if ac:UpdateAttack(1000,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,c)==1000 then
+				local ct=0
+				if Duel.GetFlagEffect(tp,100000591)~=0 then ct=ct+1 end
+				if Duel.GetFlagEffect(tp,100000593)~=0 then ct=ct+1 end
+				if ct>0 then
+					Duel.BreakEffect()
+					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTACK)
+					local ac2=dg:Select(tp,ct,ct,nil):GetFirst()
+					ac2:UpdateAttack(ct*1000)
+				end
+			end
+		else
+			Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,0)
+		end
 	end
 end

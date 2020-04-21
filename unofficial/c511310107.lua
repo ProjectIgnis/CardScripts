062dbd254f5f220aa1a32e8f3a35c5c11809cceb
AlphaKretin <deewhydeeecks@gmail.com>
2020-04-21 23:22:19 +0930

    Clean up "Darkness Bramble"

--- unofficial/c511310107.lua
+++ unofficial/c511310107.lua
@@ -2,44 +2,45 @@
 --Darkness Bramble
 local s,id=GetID()
 function s.initial_effect(c)
-	--lp4000
+	--Reveal cards
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_REPEAT)
-	e1:SetCountLimit(1)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.condition)
-	e1:SetOperation(s.operation)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.rvtg)
+	e1:SetOperation(s.rvop)
 	c:RegisterEffect(e1)
-	--battle indestructable
+	--lp4000
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e2:SetValue(1)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCountLimit(1)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.lptg)
+	e2:SetOperation(s.lpop)
 	c:RegisterEffect(e2)
+	--battle indestructable
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetOperation(s.op)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetValue(1)
 	c:RegisterEffect(e3)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetLP(tp)<4000
+function s.rvtg(e,tp,ev,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,LOCATION_SZONE,0,1,nil) end
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local p=e:GetHandler():GetControler()
-	Duel.SetLP(tp,4000,REASON_EFFECT)
+function s.rvop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	Duel.SelectMatchingCard(tp,Card.IsFacedown,tp,LOCATION_SZONE,0,1,1,nil)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	--Def
-	local e3=Effect.CreateEffect(e:GetHandler())
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(100000703)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(LOCATION_ONFIELD,0)
-	e3:SetReset(RESET_PHASE+PHASE_END)
-	e:GetHandler():RegisterEffect(e3)
+function s.lptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(tp)<4000 end
+	Duel.SetTargetPlayer(tp)
+end
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.SetLP(p,4000)
 end

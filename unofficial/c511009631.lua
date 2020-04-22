c9a3cb9c15b4a8f1e48140facade8732b835a31b
NaimSantos <naimsantos45@gmail.com>
2020-04-22 08:06:48 -0300

    Fix "Speedroid Hexasaucer (anime)"
    This updates the handling used for pendulum zone that would cause the Special Summon effect to not trigger sometimes. It also makes so the card no longer misses the activation timing

--- unofficial/c511009631.lua
+++ unofficial/c511009631.lua
@@ -1,13 +1,14 @@
---Speedroid Hexasaucer
+--Speedroid Hexasaucer (anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
 	--Special Summon (P.Zone)
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(33656832,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_DESTROYED)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
@@ -20,11 +21,10 @@ function s.initial_effect(c)
 	e2:SetCondition(s.rdcon)
 	e2:SetOperation(s.rdop)
 	c:RegisterEffect(e2)
-	
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousLocation(LOCATION_SZONE) and (c:GetPreviousSequence()==6 or c:GetPreviousSequence()==7)
+	return c:IsPreviousLocation(LOCATION_PZONE)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end

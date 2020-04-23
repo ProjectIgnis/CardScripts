894e3e32da47fccf0706b84fce98eb824cb1c8d9
mercury233 <me@mercury233.me>
2020-04-23 20:23:02 +0800

    Fix "Nemesys Keystone"
    Fix it can't return to hand if it is disabled when banished

--- pre-release/c44440058.lua
+++ pre-release/c44440058.lua
@@ -17,6 +17,7 @@ function s.initial_effect(c)
 	--register banished
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_REMOVE)
 	e1:SetOperation(s.regop)
 	c:RegisterEffect(e1)

5daf32b00640fa7d34fee24897be32304f035dff
edo9300 <edoardo762@gmail.com>
2020-04-24 19:52:55 +0200

    Remove workaround from Traptrix Rafflesia

--- official/c6511113.lua
+++ official/c6511113.lua
@@ -40,21 +40,10 @@ function s.initial_effect(c)
 	e5:SetCode(EVENT_FREE_CHAIN)
 	e5:SetHintTiming(0x3c0)
 	e5:SetCountLimit(1)
-	e5:SetCondition(s.condition)
 	e5:SetCost(s.cost)
 	e5:SetTarget(s.target)
 	e5:SetOperation(s.operation)
 	c:RegisterEffect(e5,false,REGISTER_FLAG_DETACH_XMAT)
-	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,1))
-	e6:SetType(EFFECT_TYPE_QUICK_O)
-	e6:SetRange(LOCATION_MZONE)
-	e6:SetCode(EVENT_CHAINING)
-	e6:SetCountLimit(1)
-	e6:SetCost(s.cost)
-	e6:SetTarget(s.target2)
-	e6:SetOperation(s.operation)
-	c:RegisterEffect(e6,false,REGISTER_FLAG_DETACH_XMAT)
 end
 s.listed_series={0x108a,0x4c,0x89}
 function s.imcon(e)
@@ -66,15 +55,11 @@ end
 function s.imtg(e,c)
 	return c:IsSetCard(0x108a) and not c:IsCode(id)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.CheckEvent(EVENT_CHAINING)
-end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
-	if chk==0 then return e:GetHandler():GetFlagEffect(id)==0 end
-	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	return true
 end
-function s.filter1(c)
+function s.filter(c)
 	return c:GetType()==TYPE_TRAP and (c:IsSetCard(0x4c) or c:IsSetCard(0x89)) and c:IsAbleToGraveAsCost()
 		and c:CheckActivateEffect(false,true,false)~=nil
 end
@@ -83,11 +68,11 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		if e:GetLabel()==0 then return false end
 		e:SetLabel(0)
 		return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
-			and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
 	end
 	e:SetLabel(0)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	local te,ceg,cep,cev,cre,cr,crp=g:GetFirst():CheckActivateEffect(false,true,true)
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 	Duel.SendtoGrave(g,REASON_COST)
@@ -104,58 +89,4 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	e:SetLabelObject(te:GetLabelObject())
 	local op=te:GetOperation()
 	if op then op(e,tp,eg,ep,ev,re,r,rp) end
-end
-function s.filter2(c,e,tp,eg,ep,ev,re,r,rp)
-	if c:GetType()==TYPE_TRAP and (c:IsSetCard(0x4c) or c:IsSetCard(0x89)) and c:IsAbleToGraveAsCost() then
-		if c:CheckActivateEffect(false,true,false)~=nil then return true end
-		local te=c:GetActivateEffect()
-		if te:GetCode()~=EVENT_CHAINING then return false end
-		local con=te:GetCondition()
-		if con and not con(e,tp,eg,ep,ev,re,r,rp) then return false end
-		local tg=te:GetTarget()
-		if tg and not tg(e,tp,eg,ep,ev,re,r,rp,0) then return false end
-		return true
-	else return false end
-end
-function s.filter3(c)
-	return c:GetFlagEffect(id+1) > 0
-end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		if e:GetLabel()==0 then return false end
-		e:SetLabel(0)
-		local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil,e,tp,eg,ep,ev,re,r,rp)
-		if #g>0 then
-			for tc in aux.Next(g) do
-				tc:RegisterFlagEffect(id+1,RESET_CHAIN,0,1)
-			end
-			return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
-		else
-			return false
-		end
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.filter3,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	local te,ceg,cep,cev,cre,cr,crp
-	local fchain=s.filter1(tc)
-	if fchain then
-		te,ceg,cep,cev,cre,cr,crp=tc:CheckActivateEffect(false,true,true)
-	else
-		te=tc:GetActivateEffect()
-	end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-	Duel.SendtoGrave(g,REASON_COST)
-	e:SetProperty(te:GetProperty())
-	local tg=te:GetTarget()
-	if tg then
-		if fchain then
-			tg(e,tp,ceg,cep,cev,cre,cr,crp,1)
-		else
-			tg(e,tp,eg,ep,ev,re,r,rp,1)
-		end
-	end
-	te:SetLabelObject(e:GetLabelObject())
-	e:SetLabelObject(te)
-	Duel.ClearOperationInfo(0)
 end

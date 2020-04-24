8759f254544c0097690c64f6d91f8c08cbd0972b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-25 02:42:20 +0300

    "Phantom Scales" update
    Re-scripted it to bring it up to modern standards and also fix an issue with a parameter not being passed properly.

--- unofficial/c511005051.lua
+++ unofficial/c511005051.lua
@@ -1,45 +1,39 @@
+--幻影鱗粉
 --Phantom Scales
 --original script by Shad3
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.sel_fil(c,e)
-	return c:IsFaceup() and c:IsRace(RACE_INSECT) and c:IsCanBeEffectTarget(e)
+function s.tgfilter(c,e,tp)
+	return c:IsFaceup() and c:IsRace(RACE_INSECT) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
 end
-function s.sum_fil(c,e,p,co)
-	return c:IsCode(co) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter(c,e,tp,code)
+	return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsFaceup() and chkc:IsOnField() end
-	local g=Duel.GetMatchingGroup(s.sel_fil,tp,LOCATION_MZONE,0,nil,e)
-	local tg=g:Clone()
-	local c=g:GetFirst()
-	while c do
-		if Duel.GetMatchingGroupCount(s.sum_fil,tp,LOCATION_DECK,0,nil,e,tp,c:GetCode())==0 then
-			tg:RemoveCard(c)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc:GetCode())
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
-		c=g:GetNext()
 	end
-	if chk==0 then return #tg>0 end
-	local tc=tg:Select(tp,1,1,nil)
-	Duel.SetTargetCard(tc)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,tp,0)
-end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-  local tc=Duel.GetFirstTarget()
-  if tc:IsRelateToEffect(e) then
-	local ttc=Duel.SelectMatchingCard(tp,s.sum_fil,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc:GetCode()):GetFirst()
-	if ttc then
-	  Duel.SpecialSummon(ttc,0,tp,tp,false,false,POS_FACEUP)
-	end
-  end
 end

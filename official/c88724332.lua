98a98c5c663aea09893d4203845fb1fa11c61893
NaimSantos <naimsantos45@gmail.com>
2020-04-25 13:47:29 -0300

    Fix "Night Dragolich"

--- official/c88724332.lua
+++ official/c88724332.lua
@@ -1,7 +1,8 @@
 --ナイト・ドラゴリッチ
+--Night Dragolich
 local s,id=GetID()
 function s.initial_effect(c)
-	--pos change
+	--position change
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SET_POSITION)
@@ -10,25 +11,18 @@ function s.initial_effect(c)
 	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e1:SetValue(POS_FACEUP_DEFENSE)
 	c:RegisterEffect(e1)
-	--def
+	--defense decrease
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(s.deftg)
+	e2:SetTarget(s.target)
 	e2:SetValue(s.defval)
 	c:RegisterEffect(e2)
 end
 function s.target(e,c)
-	return c:IsFaceup() and not c:IsRace(RACE_WYRM)
-		and c:IsSummonType(SUMMON_TYPE_SPECIAL)
-		and (c:GetSummonLocation()&LOCATION_DECK+LOCATION_EXTRA)~=0
-end
-function s.deftg(e,c)
-	return c:IsFaceup() and not c:IsRace(RACE_WYRM)
-		and c:IsSummonType(SUMMON_TYPE_SPECIAL)
-		and not (c:IsSummonLocation(LOCATION_DECK) or c:IsSummonLocation(LOCATION_EXTRA))
+	return c:IsFaceup() and not c:IsRace(RACE_WYRM) and c:IsSummonType(SUMMON_TYPE_SPECIAL) and (c:GetSummonLocation()&LOCATION_DECK+LOCATION_EXTRA)~=0
 end
 function s.defval(e,c)
 	return -c:GetBaseDefense()

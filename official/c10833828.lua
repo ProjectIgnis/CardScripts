fe8d83f22b1ee328a40304a4187f3bcba3c563b4
NaimSantos <naimsantos45@gmail.com>
2020-04-27 14:47:25 -0300

    Fix "Forbidden Dark Contract with the Swamp King"
    Fixed a bug where the player would be unable to activate the effect to Fusion Summon

--- official/c10833828.lua
+++ official/c10833828.lua
@@ -18,9 +18,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 	--fusion summon
-	local params = {aux.FilterBoolFunction(Card.IsRace,RACE_FIEND),nil,function(e,tc,mg)
-					return nil,s.extrafil(e:GetHandler():GetCardTarget():Filter(Card.IsControler,nil,tp))
-					end}
+	local params = {aux.FilterBoolFunction(Card.IsRace,RACE_FIEND),nil,function(e,tc,mg) return nil,s.extrafil(e:GetHandler():GetCardTarget()) end}
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)

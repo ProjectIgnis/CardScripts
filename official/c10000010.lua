45f3f603899b7f45c155cc6668d13edf7dd22a07
NaimSantos <naimsantos45@gmail.com>
2020-04-29 08:28:51 -0300

    Fix "Galaxy-Eyes Afterglow Dragon"
    Fixed a bug where the effect would not trigger if the Galaxy-Eyes Afterglow Dragon was detached but banished, due to Macro Cosmo-like effects

--- official/c10000010.lua
+++ official/c10000010.lua
@@ -1,97 +1,95 @@
---ラーの翼神竜
---The Winged Dragon of Ra
+--銀河眼の残光竜
+--Galaxy-Eyes Afterglow Dragon
+--Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
-	--summon with 3 tribute
-	local e1=aux.AddNormalSummonProcedure(c,true,false,3,3)
-	local e2=aux.AddNormalSetProcedure(c)
-	--summon
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	--special summon from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--special summon or attach
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCondition(s.atcon)
+	e2:SetTarget(s.attg)
+	e2:SetOperation(s.atop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_REMOVE)
 	c:RegisterEffect(e3)
-	--summon success
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e4:SetCode(EVENT_SUMMON_SUCCESS)
-	e4:SetOperation(s.sumsuc)
-	c:RegisterEffect(e4)
-	--cannot special summon
-	local e5=Effect.CreateEffect(c)
-	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e5:SetType(EFFECT_TYPE_SINGLE)
-	e5:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e5:SetValue(s.spval)
-	c:RegisterEffect(e5)
-	--change ATK
-	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,0))
-	e6:SetCategory(CATEGORY_ATKCHANGE)
-	e6:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
-	e6:SetCode(EVENT_SUMMON_SUCCESS)
-	e6:SetCost(s.atkcost)
-	e6:SetOperation(s.atkop)
-	c:RegisterEffect(e6)
-	--destroy
-	local e7=Effect.CreateEffect(c)
-	e7:SetDescription(aux.Stringid(id,1))
-	e7:SetCategory(CATEGORY_DESTROY)
-	e7:SetType(EFFECT_TYPE_IGNITION)
-	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetCost(s.descost)
-	e7:SetTarget(s.destg)
-	e7:SetOperation(s.desop)
-	c:RegisterEffect(e7)
 end
-function s.genchainlm(c)
-	return	function (e,rp,tp)
-				return e:GetHandler()==c
-			end
+s.listed_series={0x107b,0x48}
+s.listed_names={CARD_GALAXYEYES_P_DRAGON}
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x107b),tp,LOCATION_MZONE,0,1,nil)
 end
-function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(s.genchainlm(e:GetHandler()))
-end
-function s.spval(e,se,sp,st)
-	return se and se:GetHandler():IsCode(83764718) and Duel.IsPlayerAffectedByEffect(sp,100424006)
-end
-function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLP(tp)>100 end
-	local lp=Duel.GetLP(tp)
-	e:SetLabel(lp-100)
-	Duel.PayLPCost(tp,lp-100)
-end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(e:GetLabel())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		c:RegisterEffect(e2)
-	end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
-function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,1000) end
-	Duel.PayLPCost(tp,1000)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
-	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+function s.atcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_COST) and re:IsHasType(0x7e0) and re:IsActiveType(TYPE_MONSTER)
+		and c:IsPreviousLocation(LOCATION_OVERLAY)
 end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		Duel.Destroy(tc,REASON_EFFECT)
+function s.cfilter2(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+end
+function s.cfilter3(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x48)
+end
+function s.attg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,CARD_GALAXYEYES_P_DRAGON) end
+end
+function s.atop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ph=Duel.GetCurrentPhase()
+	local tg=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_MZONE,0,1,nil,e)
+	local sg=Duel.GetMatchingGroup(s.cfilter3,tp,LOCATION_MZONE,0,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,CARD_GALAXYEYES_P_DRAGON)
+	local tc=g:GetFirst()
+	local op=0
+		if #tg>0 and (tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0) then
+		op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
+		elseif #tg>0 then op=0
+		else op=1 end
+		if op==0 then
+			local oc=tg:Select(tp,1,1,nil,e):GetFirst()
+			if oc:IsImmuneToEffect(e) then Duel.SendtoGrave(tc,REASON_RULE) end
+			if oc and oc:IsFaceup() then
+				Duel.Overlay(oc,tc)
+			end
+		else 
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	if ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE then
+		for sc in aux.Next(sg) do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(sc:GetAttack()*2)
+			sc:RegisterEffect(e1)
+		end
 	end
 end

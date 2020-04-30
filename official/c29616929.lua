10580df5595348711dce9e8bc63f9194050743b2
NaimSantos <naimsantos45@gmail.com>
2020-04-30 16:18:25 -0300

    Fix "Traptrix Trap Hole Nightmare"
    Check for the status of the triggering card instead of raising a global effect for flag registration

--- official/c29616929.lua
+++ official/c29616929.lua
@@ -11,25 +11,11 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	aux.GlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-	end)
-end
-function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	for tc in aux.Next(eg) do
-		if tc:GetFlagEffect(id)==0 then
-			tc:RegisterFlagEffect(id,RESET_PHASE+PHASE_END,0,1)
-		end
-	end
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local loc=Duel.GetChainInfo(0,CHAININFO_TRIGGERING_LOCATION)
-	return ep~=tp and loc==LOCATION_MZONE and re:GetHandler():GetFlagEffect(id)>0 and Duel.IsChainDisablable(ev)
+	return re:GetHandler():IsControler(1-tp) and re:GetHandler():IsStatus(STATUS_SPSUMMON_TURN)
+		and loc==LOCATION_MZONE and Duel.IsChainDisablable(ev)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

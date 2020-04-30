5323ceeeb68ea5127e79327d8099be59443c9c60
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-04-30 13:41:23 +0300

    "The Phantom Knights of Break Sword (Anime)" fix
    Added an additional check to prevent an error. Also, now properly checks if all the monsters can be Special Summoned.

--- unofficial/c511001051.lua
+++ unofficial/c511001051.lua
@@ -1,3 +1,4 @@
+--幻影騎士団ブレイクソード (Anime)
 --The Phantom Knights of Break Sword (Anime)
 --rescripted by Naim
 local s,id=GetID()
@@ -31,23 +32,23 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	e:SetLabelObject(g)
 end
 function s.filter(c,e,tp)
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=e:GetLabelObject()
-	g=g:Filter(s.filter,nil,e,tp)
+	local g1=e:GetLabelObject()
+	local g2
+	if g1 then g2=g1:Filter(s.filter,nil,e,tp) end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	if chk==0 then return #g>0 and ft>#g-1 end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,tp,nil)
+	if chk==0 then return g1 and g2 and #g1==#g2 and ft>=#g2 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g2,#g2,tp,nil)
 end
 function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 	local g=e:GetLabelObject()
 	g=g:Filter(s.filter,nil,e,tp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	if ft<=#g-1 then return end
-	local tc=g:GetFirst()
+	if ft<#g then return end
 	for tc in aux.Next(g) do
 		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
 		local e1=Effect.CreateEffect(e:GetHandler())

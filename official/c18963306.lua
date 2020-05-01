3f07e2d26e712b053d9ac408d4bfea04e02429ef
NaimSantos <naimsantos45@gmail.com>
2020-05-01 08:59:18 -0300

    Remove client hint from "Galaxy-Eyes Cipher Dragon"
    Given that the effect is applied to monsters and not to the player itself, it should not use the client hint

--- official/c18963306.lua
+++ official/c18963306.lua
@@ -42,9 +42,8 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetLabel(e:GetHandler():GetFieldID())
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,1),nil)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.GetControl(tc,tp,PHASE_END,1) then
+	if tc and tc:IsRelateToEffect(e) and Duel.GetControl(tc,tp,PHASE_END,1) then
 		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)

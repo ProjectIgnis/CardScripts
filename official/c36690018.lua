1cfed88ca3846720a5a04c2c2defaa62da4faaba
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-05-03 23:55:28 +0300

    "Reversal of Fate" fix
    Added an archetype check.

--- official/c36690018.lua
+++ official/c36690018.lua
@@ -1,4 +1,5 @@
 --逆転する運命
+--Reversal of Fate
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -11,7 +12,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.filter(c)
-	return c:GetFlagEffect(id)~=0
+	return c:IsSetCard(0x5) and c:GetFlagEffect(id)>0
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end

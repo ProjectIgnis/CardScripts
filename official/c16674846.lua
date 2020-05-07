cca80e52c828db0dded05ef123b79534839c826f
NaimSantos <naimsantos45@gmail.com>
2020-05-07 08:16:48 -0300

    Fix "Spirit Force"
    Fixed a bug where the effect would last for more than the Damage Calculation

--- official/c16674846.lua
+++ official/c16674846.lua
@@ -23,6 +23,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
 	Duel.RegisterEffect(e1,tp)
 	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,nil)
 	if #g~=0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then

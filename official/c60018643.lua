0007e65f7735de93d922d33e8521fd849b92d316
NaimSantos <naimsantos45@gmail.com>
2020-05-07 08:42:56 -0300

    Fix "Cynet Codec"
    Fixed a bug where the player would be unable to select which "Cynet Codec" to trigger if multiples were face-up on the field

--- official/c60018643.lua
+++ official/c60018643.lua
@@ -10,6 +10,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--search
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_CUSTOM+id)
@@ -23,6 +24,7 @@ function s.initial_effect(c)
 	e2:SetLabelObject(g)
 	--mass register
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -44,10 +46,10 @@ end
 s.listed_series={0x101}
 function s.thcfilter(c,e,tp)
 	local attr=c:GetAttribute()
-	return c:IsSetCard(0x101) and c:IsControler(tp) 
+	return c:IsSetCard(0x101) and c:IsControler(tp)
 		and c:IsLocation(LOCATION_MZONE) and c:IsPreviousLocation(LOCATION_EXTRA)
 		and s.attr_list[tp]&attr==0
-		and c:IsCanBeEffectTarget(e)		
+		and c:IsCanBeEffectTarget(e)
 		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,attr)
 end
 function s.thfilter(c,attr)
@@ -60,16 +62,14 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.GetCurrentChain()==0 then g:Clear() end
 		g:Merge(tg)
 		e:GetLabelObject():SetLabelObject(g)
-		if Duel.GetFlagEffect(tp,id)==0 then
-			Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
-			Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+id,e,0,tp,tp,0)
-		end
+		Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+id,e,0,tp,tp,0)
 	end
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=e:GetLabelObject():Filter(s.thcfilter,nil,e,tp)
 	if chkc then return g:IsContains(chkc) and s.thcfilter(chkc,e,tp) end
-	if chk==0 then return #g>0 end
+	if chk==0 then return #g>0 and Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
 	if #g==1 then
 		Duel.SetTargetCard(g:GetFirst())
 	else
@@ -104,6 +104,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
+	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,2),nil)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsRace(RACE_CYBERSE) and c:IsLocation(LOCATION_EXTRA)

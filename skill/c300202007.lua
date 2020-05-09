2eb6e7555bc86ad717035ebb086d4f6fb0ba6a54
NaimSantos <naimsantos45@gmail.com>
2020-05-09 20:56:58 -0300

    Fix "Aroma Strategy"
    Prevent the script from returning an error message if the player has no card in the deck

--- skill/c300202007.lua
+++ skill/c300202007.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	aux.AddSkillProcedure(c,2,false,nil,nil)
-	local e1=Effect.CreateEffect(c)	
+	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
@@ -40,8 +40,10 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetDecktopGroup(e:GetHandler():GetControler(),1)
-	if g:GetFirst():GetFlagEffect(id)==0 then
-		Duel.ConfirmDecktop(e:GetHandler():GetControler(),1)
-		g:GetFirst():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+	if g and #g>0 then
+		if g:GetFirst():GetFlagEffect(id)==0 then
+			Duel.ConfirmDecktop(e:GetHandler():GetControler(),1)
+			g:GetFirst():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+		end
 	end
 end

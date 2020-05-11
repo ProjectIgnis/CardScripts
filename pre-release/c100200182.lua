3eae6ab6489933702a549ab146e0f0334c0529cf
EerieCode <eeriecode@gmail.com>
2020-05-11 21:25:42 +0200

    Added new cards
    Hundred Dragon
    Time Magus
    Harpie Lady - Scratch Clash
    Jinzo - Layer

---
+++ pre-release/c100200182.lua
@@ -0,0 +1,60 @@
+--百年竜
+--Hundred Dragon
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--level/atk increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_LVCHANGE+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--protection
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.tg)
+	e2:SetOperation(s.op)
+	c:RegisterEffect(e2)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local count=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
+		if c:UpdateLevel(count,RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)==count then
+			return  c:UpdateAttack(count*100,RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		end
+	end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSummonLocation(LOCATION_EXTRA)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsFaceup() and chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCountLimit(1)
+		e1:SetValue(s.valcon)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.valcon(e,re,r,rp)
+	return (r&REASON_BATTLE+REASON_EFFECT)~=0
+end

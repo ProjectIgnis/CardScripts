f1cc6e6a618a438f4f1b593a0c201f65baca1eeb
NaimSantos <naimsantos45@gmail.com>
2020-05-12 08:24:28 -0300

    Updated reset values for "Hundred Dragon"'s second effect

--- pre-release/c100200182.lua
+++ pre-release/c100200182.lua
@@ -51,7 +51,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCountLimit(1)
 		e1:SetValue(s.valcon)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
 end

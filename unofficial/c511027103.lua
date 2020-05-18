fa1f7cf1fa7d555dac07e4766b7541ed575c85b6
42Rundas <38040866+42Rundas@users.noreply.github.com>
2020-05-18 23:17:03 +0200

    Update c511027103.lua

--- unofficial/c511027103.lua
+++ unofficial/c511027103.lua
@@ -3,25 +3,25 @@
 --Script by Rundas
 local s,id=GetID()
 function s.initial_effect(c)
-	--equip
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
-	e1:SetTarget(s.eqtg)
-	e1:SetOperation(s.eqop)
-	c:RegisterEffect(e1)
-	--protecc
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCondition(s.con)
-	e2:SetValue(s.val)
-	c:RegisterEffect(e2)
+    --equip
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCategory(CATEGORY_EQUIP)
+    e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
+    e1:SetTarget(s.eqtg)
+    e1:SetOperation(s.eqop)
+    c:RegisterEffect(e1)
+    --protecc
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2:SetRange(LOCATION_SZONE)
+    e2:SetCondition(s.con)
+    e2:SetValue(s.val)
+    c:RegisterEffect(e2)
     --destroy replace
     local e3=Effect.CreateEffect(c)
     e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -92,16 +92,16 @@ end
 --desrep
 
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp,e) end
+    if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,e) end
     return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
 end
 function s.repval(e,c)
-    return s.repfilter(c,e:GetHandlerPlayer(),e)
+    return s.repfilter(c,e)
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
     Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
 end
 
-function s.repfilter(c,tp,e)
+function s.repfilter(c,e)
     return c==e:GetHandler():GetEquipTarget() and c:IsReason(REASON_EFFECT)
 end

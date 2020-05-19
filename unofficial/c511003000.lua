0a098fee57032ef48b38d69af219718ffa78bcb0
edo9300 <edoardo762@gmail.com>
2020-05-19 13:08:57 +0200

    Fix pendulum xyz (legal and manga)

--- unofficial/c511003000.lua
+++ unofficial/c511003000.lua
@@ -12,35 +12,44 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.spfilter(c,mg,lv)
-	mg:ForEach(function(mc)
-		mc:AssumeProperty(ASSUME_TYPE,mc:GetOriginalType())
-		mc:AssumeProperty(ASSUME_LEVEL,lv)
-	end)
-	return c:IsXyzSummonable(nil,mg,2,2)
+local function reglevel(c,tc,lv)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_XYZ_LEVEL)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(lv)
+	tc:RegisterEffect(e1,true)
+	return e1
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local pg=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
-		if #pg~=2 then return false end
-		local lv1=pg:GetFirst():GetLevel()
-		local lv2=pg:GetNext():GetLevel()
-		return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,pg,lv1) 
-			or Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,pg,lv2)
+		local g=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
+		if #g~=2 then return false end
+		local tc1=g:GetFirst()
+		local tc2=g:GetNext()
+		local e1=reglevel(e:GetHandler(),tc1,tc2:GetLevel())
+		local e2=reglevel(e:GetHandler(),tc2,tc1:GetLevel())
+		local res=Duel.IsExistingMatchingCard(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,1,nil,g,g)
+		if e1 then e1:Reset() end
+		if e2 then e2:Reset() end
+		return res
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local pg=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
 	if #pg~=2 then return false end
-	local lv1=pg:GetFirst():GetLevel()
-	local lv2=pg:GetNext():GetLevel()
-	local g1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,pg,lv1)
-	local g2=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,pg,lv2)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local xyz=(g1+g2):Select(tp,1,1,nil):GetFirst()
-	if xyz then
-		if not s.spfilter(xyz,pg,lv1) then s.spfilter(xyz,pg,lv2) end
-		Duel.XyzSummon(tp,xyz,nil,pg)
+	local tc1=pg:GetFirst()
+	local tc2=pg:GetNext()
+	local e1=reglevel(e:GetHandler(),tc1,tc2:GetLevel())
+	local e2=reglevel(e:GetHandler(),tc2,tc1:GetLevel())
+	local g=Duel.GetMatchingGroup(Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,nil,pg,pg)
+	if g and #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local xyz=g:Select(tp,1,1,nil):GetFirst()
+		Duel.XyzSummon(tp,xyz,pg,pg)
+	else
+		if e1 then e1:Reset() end
+		if e2 then e2:Reset() end
 	end
 end

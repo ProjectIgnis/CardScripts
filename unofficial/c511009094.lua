7b2aa9f462ca0f5a7eb3ff38a36c68e5ea6c4b5c
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-19 18:14:05 +0800

    Update c511009094.lua

--- unofficial/c511009094.lua
+++ unofficial/c511009094.lua
@@ -6,7 +6,6 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
 	--negate attack
 	local e2=Effect.CreateEffect(c)

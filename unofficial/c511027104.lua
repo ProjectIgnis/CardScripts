47f9e682744df50d2357e1cce6ebb96247999833
Larry126 <28985622+larry126@users.noreply.github.com>
2020-05-20 09:14:49 +0800

    Update c511027104.lua

--- unofficial/c511027104.lua
+++ unofficial/c511027104.lua
@@ -1,5 +1,5 @@
---ZW－阿修羅副腕
---ZW - Asura Strike (anime)
+--ZW－阿修羅副腕 (Anime)
+--ZW - Asura Strike (Anime)
 --Script by Rundas
 local s,id=GetID()
 function s.initial_effect(c)
@@ -34,8 +34,7 @@ end
 
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	if c:IsLocation(LOCATION_MZONE) and c:IsFacedown() then return end
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
 	local tc=Duel.GetFirstTarget()
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or tc:GetControler()~=tp or tc:IsFacedown() or not tc:IsRelateToEffect(e) then
 		Duel.SendtoGrave(c,REASON_EFFECT)

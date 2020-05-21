62c62ecb25e6397ffee7a4a00909561a9617718e
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-05-21 17:05:54 +0200

    added New Skill (part 2)
    added Knight of Legend

---
+++ skill/c300204002.lua
@@ -0,0 +1,104 @@
+--Knight of Legend
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+s.listed_names={CARD_GAIA_CHAMPION}
+s.listed_series={0xbd}
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opt check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	
+	local c=e:GetHandler()
+	--decrease tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetTargetRange(LOCATION_HAND,0)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.ntcon)
+	e1:SetTarget(aux.FieldSummonProcTg(s.nttg))
+	Duel.RegisterEffect(e1,tp)
+	--cannot activate set card
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_TRIGGER)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetTargetRange(LOCATION_SZONE,0)
+	e3:SetTarget(s.distg)
+	e3:SetCondition(s.effcon)
+	Duel.RegisterEffect(e3,tp)
+	
+	
+	--draw 2
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_DRAW)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_BATTLE_DAMAGE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.drcon)
+	e4:SetTarget(s.drtg)
+	e4:SetOperation(s.drop)
+	Duel.RegisterEffect(e4,tp)
+	--pierce
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_PIERCE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xbd))
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_PIERCE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsCode,CARD_GAIA_CHAMPION))
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+function s.nttg(e,c)
+	return c:IsLevelAbove(5) and c:IsCode(6368038)
+end
+function s.effilter(c)
+	return c:IsFaceup() and (c:IsSetCard(0xbd) or c:IsCode(CARD_GAIA_CHAMPION))
+end
+function s.effcon(e)
+	return Duel.IsExistingMatchingCard(s.effilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.distg(e,c)
+	return c:IsFacedown()
+end
+--draw 2
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	local bc=tc:GetBattleTarget()
+	return tc:IsControler(tp) and bc and bc:IsDefensePos() and (tc:IsSetCard(0xbd) or tc:IsCode(CARD_GAIA_CHAMPION))
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return end
+	Duel.Hint(HINT_CARD,tp,id)
+	if Duel.Draw(tp,2,REASON_EFFECT)==2 then
+		Duel.ShuffleHand(tp)
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT+REASON_DISCARD)
+	end
+end

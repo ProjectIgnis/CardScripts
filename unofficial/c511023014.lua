46b4056d4a351a8accc0247c0f1bff8df234f202
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-22 02:59:02 +0800

    Update 2 unofficial cards
    Updated Elemental HERO Necroshade (Anime)
    Updated Sabatiel (Anime)

--- unofficial/c511023014.lua
+++ unofficial/c511023014.lua
@@ -1,10 +1,11 @@
---Elemental HERO Necroshade
+--E・HERO ネクロダークマン (Anime)
+--Elemental HERO Necroshade (Anime)
 --fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
 	--summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(21524779,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_GRAVE)
@@ -18,11 +19,11 @@ function s.filter(c,ec)
 	if not c:IsSetCard(0x3008) then return false end
 	if c:IsSummonable(true,nil) then return true end
 	local e1=Effect.CreateEffect(ec)
-	e1:SetDescription(aux.Stringid(78651105,0))
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCondition(s.ntcon)
-	e1:SetReset(RESET_CHAIN)
+	e1:SetReset(RESET_CHAIN+RESET_EVENT+RESETS_STANDARD)
 	c:RegisterEffect(e1)
 	local res=c:IsSummonable(true,nil)
 	e1:Reset()
@@ -41,13 +42,18 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
 	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e:GetHandler()):GetFirst()
 	if tc then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetDescription(aux.Stringid(78651105,0))
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SUMMON_PROC)
-		e1:SetCondition(s.ntcon)
-		e1:SetReset(RESET_CHAIN)
-		tc:RegisterEffect(e1)
-		Duel.Summon(tp,tc,true,nil)
+		if tc:IsSummonable(true,nil) then Duel.Summon(tp,tc,true,nil)
+		else
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetDescription(aux.Stringid(id,1))
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
+			e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+			e1:SetCondition(s.ntcon)
+			e1:SetReset(RESET_CHAIN+RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			Duel.Summon(tp,tc,true,nil)
+			e1:Reset()
+		end
 	end
 end

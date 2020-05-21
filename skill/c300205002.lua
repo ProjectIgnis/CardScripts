0cbe5ace1dbd057b82032e7e567775e25d283237
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-05-21 17:07:22 +0200

    added New Skill (part 3)
    Added Into the Darkness Below

---
+++ skill/c300205002.lua
@@ -0,0 +1,49 @@
+--Into the Darkness Below
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil)
+end
+s.listed_names={15259703}
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	Duel.DiscardHand(tp,s.filter,1,1,REASON_COST+REASON_DISCARD)
+	local g1=Duel.IsPlayerCanDraw(tp,2)
+	
+	local g=Duel.GetMatchingGroup(s.cfilter,c:GetControler(),LOCATION_GRAVE,0,nil)
+	local ct=g:GetClassCount(Card.GetCode)
+	local g2=(Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and ct>3)
+
+	local opt=0
+	if g1 and g2 then
+		opt=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
+	elseif g1 then
+		opt=Duel.SelectOption(tp,aux.Stringid(id,1))
+	elseif g2 then
+		opt=Duel.SelectOption(tp,aux.Stringid(id,2))+1
+	else return end
+	if opt==0 then
+		Duel.Draw(tp,1,REASON_EFFECT)
+	else
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+end
+function s.filter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsLevelBelow(2) and c:IsDiscardable()
+end
+function s.cfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsType(TYPE_MONSTER) and c:IsRace(RACE_FIEND)
+end
+function s.thfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end

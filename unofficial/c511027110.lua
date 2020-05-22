0c13baa01fe6c7246ed5293246b7b0fcbaf78d92
Larry126 <28985622+larry126@users.noreply.github.com>
2020-05-22 20:30:22 +0800

    A few updates/fixes
    I noticed that you accidentally put the ```excludeGroup/Card``` in ```xxxMatchingCard``` functions in the wrong place, so I fixed them.

--- unofficial/c511027110.lua
+++ unofficial/c511027110.lua
@@ -19,18 +19,19 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,nil,c)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,c)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 	end
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_HAND,0,c)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,1,nil,c)
-	if #g==0 then return end
-	if Duel.SendtoGrave(g,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,1,c)
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

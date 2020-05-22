e6da4fa1cda0cfa870238080aec10ab65cee5438
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-22 11:18:54 +0800

    Fix 5ds anime cards
    Sneak Giant's NS proc fixed
    Star Excursion overhaul

--- unofficial/c511000873.lua
+++ unofficial/c511000873.lua
@@ -27,79 +27,89 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local at=Duel.GetAttackTarget()
 	local g=Group.FromCards(a,at)
-	if Duel.Remove(g,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+	if Duel.Remove(g,0,REASON_EFFECT+REASON_TEMPORARY)~=0 and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
 		local og=Duel.GetOperatedGroup()
-		local oc=og:GetFirst()
-		while oc do
+		for oc in aux.Next(og) do
 			if oc:IsControler(tp) then
 				oc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,0,4)
 			else
 				oc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE+RESET_SELF_TURN,0,4)
 			end
-			oc=og:GetNext()
 		end
-		if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
 		og:KeepAlive()
 		local c=e:GetHandler()
-		local res
+		local res=Duel.GetTurnPlayer()~=tp and 4 or 3
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
-		if Duel.GetTurnPlayer()~=tp then
-			res=4
-			e1:SetLabel(-1)
-		else
-			res=3
-			e1:SetLabel(0)
-		end
-		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetCountLimit(1)
+		e1:SetLabel(0)
 		e1:SetLabelObject(og)
-		e1:SetCondition(s.retcon)
-		e1:SetOperation(s.retop)
+		e1:SetCondition(s.tccon)
+		e1:SetOperation(s.tcop)
+		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
 		Duel.RegisterEffect(e1,tp)
-		local descnum=tp==c:GetOwner() and 0 or 1
 		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,0))
 		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetDescription(aux.Stringid(4931121,descnum))
 		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
 		e2:SetCode(1082946)
 		e2:SetLabelObject(e1)
 		e2:SetOwnerPlayer(tp)
 		e2:SetOperation(s.reset)
-		e1:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
+		e2:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
 		c:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_PHASE+PHASE_BATTLE)
+		e3:SetCountLimit(1)
+		e3:SetLabelObject(e1)
+		e3:SetCondition(s.retcon)
+		e3:SetOperation(s.retop)
+		e3:SetReset(RESET_PHASE+PHASE_BATTLE+RESET_OPPO_TURN,res)
+		Duel.RegisterEffect(e3,tp)
+		Duel.RegisterFlagEffect(0,e1:GetFieldID()+id,RESET_PHASE+PHASE_END,0,1)
 	end
 end
 function s.reset(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetLabelObject() then e:Reset() return end
-	s.retop(e:GetLabelObject(),tp,eg,ep,ev,e,r,rp)
-end
-function s.retcon(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetLabelObject()
-	if not g:IsExists(s.retfilter,1,nil) then
-		g:DeleteGroup()
-		e:Reset()
-		return false
-	else return Duel.GetTurnPlayer()~=tp end
+	s.tcop(e:GetLabelObject(),tp,eg,ep,ev,e,r,rp)
 end
 function s.retfilter(c)
 	return c:GetFlagEffect(id)~=0
 end
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
+function s.tccon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(s.retfilter,1,nil) or e:GetLabel()==2 and Duel.GetTurnPlayer()~=tp then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return Duel.GetTurnPlayer()~=tp and Duel.GetFlagEffect(0,e:GetFieldID()+id)==0 end
+end
+function s.tcop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()+1
 	e:SetLabel(ct)
-	e:GetHandler():SetTurnCounter(ct)
-	if ct>=3 then
-		local g=e:GetLabelObject()
-		local sg=g:Filter(s.retfilter,nil)
+	e:GetOwner():SetTurnCounter(ct)
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetLabelObject() then e:Reset() return false end
+	local g=e:GetLabelObject():GetLabelObject()
+	if not g:IsExists(s.retfilter,1,nil) then
 		g:DeleteGroup()
-		local tc=sg:GetFirst()
-		while tc do
-			Duel.ReturnToField(tc)
-			tc:SetStatus(STATUS_SUMMON_TURN+STATUS_FORM_CHANGED,false)
-			tc=sg:GetNext()
-		end
-		if re then re:Reset() end
+		e:Reset()
+		return false
+	else return Duel.GetTurnPlayer()~=tp and e:GetLabelObject():GetLabel()==2 end
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetLabelObject() then e:Reset() return false end
+	local g=e:GetLabelObject():GetLabelObject()
+	local sg=g:Filter(s.retfilter,nil)
+	for tc in aux.Next(sg) do
+		Duel.ReturnToField(tc)
+		tc:SetStatus(STATUS_SUMMON_TURN+STATUS_FORM_CHANGED,false)
 	end
+	if re then re:Reset() end
+	g:DeleteGroup()
+	e:Reset()
+	e:GetLabelObject():Reset()
 end

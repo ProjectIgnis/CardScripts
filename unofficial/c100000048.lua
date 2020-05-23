85d37804db7bfa54f338cc6d55653009e51c9c1c
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-24 01:29:02 +0800

    Anime cards fixes
    - Fixed Destiny Overlay
    - Fixed and cleaned up TF6 Meklord Components

--- unofficial/c100000048.lua
+++ unofficial/c100000048.lua
@@ -1,9 +1,10 @@
---ワイゼルＡ３
---Wisel Attack 3
+--ワイゼルＡ３ (TF6)
+--Wisel Attack 3 (TF6)
 local s,id=GetID()
 function s.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
@@ -51,5 +52,5 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	g:DeleteGroup()
 end
 function s.sdcon(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x3013),tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler())
+	return not Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x3013),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
--- unofficial/c100000050.lua
+++ unofficial/c100000050.lua
@@ -1,8 +1,10 @@
---機皇帝ワイゼル∞
+--機皇帝ワイゼル∞ (TF6)
+--Meklord Emperor Wisel ∞ (TF6)
 Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--atkup
+	c:SetUniqueOnField(1,0,aux.FilterBoolFunction(Card.IsSetCard,0x3013),LOCATION_MZONE)
+	--atk/def up
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -10,7 +12,6 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_UPDATE_DEFENSE)
 	e1:SetValue(s.val)
 	c:RegisterEffect(e1)
-	--defup
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	c:RegisterEffect(e2)
@@ -24,55 +25,34 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 	--equip
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetCategory(CATEGORY_EQUIP)
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
+	e4:SetCondition(s.eqcon)
 	e4:SetTarget(s.eqtg)
 	e4:SetOperation(s.eqop)
 	c:RegisterEffect(e4)
-	aux.AddEREquipLimit(c,nil,s.eqval,s.equipop,e4)
-	--only 1 can exists
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE)
-	e5:SetCode(EFFECT_CANNOT_SUMMON)
-	e5:SetCondition(s.excon)
-	c:RegisterEffect(e5)
-	local e6=e5:Clone()
-	e6:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
-	c:RegisterEffect(e6)
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_SINGLE)
-	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e7:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e7:SetValue(s.splimit)
-	c:RegisterEffect(e7)
-	local e8=Effect.CreateEffect(c)
-	e8:SetType(EFFECT_TYPE_SINGLE)
-	e8:SetCode(EFFECT_SELF_DESTROY)
-	e8:SetCondition(s.descon)
-	c:RegisterEffect(e8)
+	aux.AddEREquipLimit(c,s.eqcon,function(ec,_,tp) return ec:IsControler(1-tp) and ec:IsFaceup() and ec:IsType(TYPE_SYNCHRO) end,s.equipop,e4)
 end
-function s.eqval(ec,c,tp)
-	return ec:IsControler(1-tp) and ec:IsType(TYPE_SYNCHRO)
-end
+s.listed_series={0x3013}
 function s.filter(c)
 	return c:IsFaceup() and (c:IsWisel() or c:IsGranel() or c:IsSkiel())
 end
 function s.val(e,c)
-	local wup=0
-	local wg=Duel.GetMatchingGroup(s.filter,c:GetControler(),LOCATION_MZONE,0,c)
-	local wbc=wg:GetFirst()
-	while wbc do
-		wup=wup+wbc:GetAttack()
-		wbc=wg:GetNext()
-	end
-	return wup
+	return Duel.GetMatchingGroup(s.filter,c:GetControler(),LOCATION_MZONE,0,c):GetSum(Card.GetAttack)
 end
 function s.effval(e,re)
 	return re:GetHandlerPlayer()~=e:GetHandlerPlayer()
+end
+function s.eqlfilter(c)
+	return c:GetFlagEffect(id)~=0 
+end
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetHandler():GetEquipGroup():Filter(s.eqlfilter,nil)
+	return #g==0
 end
 function s.eqfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsAbleToChangeControler()
@@ -88,38 +68,21 @@ end
 function s.equipop(c,e,tp,tc)
 	local atk=tc:GetTextAttack()
 	if atk<0 then atk=0 end
-	if not aux.EquipByEffectAndLimitRegister(c,e,tp,tc) then return end
+	if not aux.EquipByEffectAndLimitRegister(c,e,tp,tc,id) then return end
 	if atk>0 then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_EQUIP)
-		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OWNER_RELATE)
-		e2:SetCode(EFFECT_UPDATE_ATTACK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e2:SetValue(atk)
-		tc:RegisterEffect(e2)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_EQUIP)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OWNER_RELATE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(atk)
+		tc:RegisterEffect(e1)
 	end
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		if c:IsFaceup() and c:IsRelateToEffect(e) then
-			s.equipop(c,e,tp,tc)
-		else Duel.SendtoGrave(tc,REASON_EFFECT) end
+	if tc and tc:IsRelateToEffect(e) then
+		s.equipop(c,e,tp,tc)
 	end
-end
-function s.exfilter(c,fid)
-	return c:IsFaceup() and c:IsSetCard(0x3013) and (fid==nil or c:GetFieldID()<fid)
-end
-function s.excon(e)
-	local c=e:GetHandler()
-	return Duel.IsExistingMatchingCard(s.exfilter,c:GetControler(),LOCATION_ONFIELD,0,1,nil)
-end
-function s.splimit(e,se,sp,st,spos,tgp)
-	if (spos&POS_FACEDOWN)~=0 then return true end
-	return not Duel.IsExistingMatchingCard(s.exfilter,tgp,LOCATION_ONFIELD,0,1,nil)
-end
-function s.descon(e)
-	local c=e:GetHandler()
-	return Duel.IsExistingMatchingCard(s.exfilter,c:GetControler(),LOCATION_ONFIELD,0,1,nil,c:GetFieldID())
 end

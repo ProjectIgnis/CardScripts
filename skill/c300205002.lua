f37241f907189789a82a5bd10f07c70a0b2eedb1
NaimSantos <naimsantos45@gmail.com>
2020-05-24 06:35:46 -0300

    Added count limit to "Into the Darkness Below""

--- skill/c300205002.lua
+++ skill/c300205002.lua
@@ -1,7 +1,7 @@
 --Into the Darkness Below
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition

40da7431dabfc6e601edfe7143ab7910b9cf4d8f
larry126 <28985622+larry126@users.noreply.github.com>
2020-05-24 12:49:38 +0800

    Update Ultimate Machine Union
    Using new fusion proc

--- unofficial/c100000390.lua
+++ unofficial/c100000390.lua
@@ -1,15 +1,22 @@
---超機合体 (Anime)
---Overload Fusion (Anime)
+--超機合体
+--Ultimate Machine Union
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE),Fusion.OnFieldMat(Card.IsAbleToRemove),s.fextra,Fusion.BanishMaterial)
+	--activate
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x16),matfilter=aux.FALSE,extrafil=s.fextra,extraop=Fusion.BanishMaterial})
 	c:RegisterEffect(e1)
 	if not GhostBelleTable then GhostBelleTable={} end
 	table.insert(GhostBelleTable,e1)
 end
+s.listed_series={0x16}
+function s.filter(c,p)
+	return c:IsAbleToRemove() and c:IsSetCard(0x16,nil,SUMMON_TYPE_FUSION,p)
+end
 function s.fextra(e,tp,mg)
 	if not Duel.IsPlayerAffectedByEffect(tp,69832741) then
-		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(s.filter,tp),tp,LOCATION_GRAVE,0,nil)
+	else
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(s.filter,tp),tp,LOCATION_ONFIELD,0,nil)
 	end
 	return nil
 end

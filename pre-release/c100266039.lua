c9a5578819c7bcb3e022099a1b2edf7a9ba3a348
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-05-26 15:22:41 +0300

    "Appliancer Kappa Scale" fix
    HOPT was overlapping with "Appliancer Vacculephant".

--- pre-release/c100266039.lua
+++ pre-release/c100266039.lua
@@ -30,12 +30,13 @@ function s.initial_effect(c)
 	--Special Summon if not co-linked
 	local e3=e2:Clone()
 	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCountLimit(1,id+1)
+	e3:SetCountLimit(1,id+100)
 	e3:SetCondition(aux.NOT(s.spcon))
 	e3:SetLabel(0)
 	c:RegisterEffect(e3)
 end
 s.listed_series={0x244}
+s.listed_names={id}
 function s.lkcon(e)
 	local c=e:GetHandler()
 	return c:IsStatus(STATUS_SPSUMMON_TURN) and c:IsSummonType(SUMMON_TYPE_LINK)

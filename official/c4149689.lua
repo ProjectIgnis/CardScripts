702a52895facbb50266a46dc674bab84a6792bce
NaimSantos <naimsantos45@gmail.com>
2020-05-29 11:01:56 -0300

    Rescript "Goblin Fan"
    The effect should be continuously applied and not trigger-like

--- official/c4149689.lua
+++ official/c4149689.lua
@@ -1,7 +1,7 @@
 --天狗のうちわ
+--Goblin Fan
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -17,26 +17,22 @@ function s.initial_effect(c)
 	--destroy
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetTarget(s.destg)
 	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
 end
 function s.aclimit(e,re,tp)
 	local rc=re:GetHandler()
-	return re:IsActiveType(TYPE_MONSTER) and rc:IsSummonType(SUMMON_TYPE_FLIP) and rc:IsLevelBelow(2)
-end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg:GetFirst():IsLevelBelow(2) end
-	Duel.SetTargetCard(eg)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	return re:IsActiveType(TYPE_MONSTER) and re:GetActivateLocation()==LOCATION_MZONE and rc:GetFlagEffect(id)~=0
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsLevelBelow(2) then
-		Duel.Destroy(tc,REASON_EFFECT)
+	if eg then
+		local tc=eg:GetFirst()
+		if tc:IsLevelBelow(2) then
+			Duel.Destroy(tc,REASON_EFFECT)
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+		end
 	end
 end

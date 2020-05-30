2b1de7dbc016801e527cf510870e93ecd0ccd0d9
edo9300 <edoardo762@gmail.com>
2020-05-30 23:02:57 +0200

    Nimble momonga, summon in defense

--- official/c22567609.lua
+++ official/c22567609.lua
@@ -38,7 +38,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			else
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 				local sg=g:Select(tp,1,ft,nil)
-				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
 			end
 		end
 	end

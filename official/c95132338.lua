09cd2f12a49ad717b8a1e1b89ad67229569e741a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-06-08 03:10:19 +0300

    "Aqua Chorus" fix
    Should be able to activate it during the Damage Step (up until before damage calculation).

--- official/c95132338.lua
+++ official/c95132338.lua
@@ -1,12 +1,14 @@
 --アクアの合唱
+--Aqua Chorus
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--
+	--Increase ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
@@ -19,9 +21,6 @@ function s.initial_effect(c)
 	e3:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e3)
 end
-function s.cfilter(c,code)
-	return c:IsFaceup() and c:IsCode(code)
-end
 function s.target(e,c)
-	return Duel.IsExistingMatchingCard(s.cfilter,0,LOCATION_MZONE,LOCATION_MZONE,1,c,c:GetCode())
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,c:GetCode()),0,LOCATION_MZONE,LOCATION_MZONE,1,c)
 end

31de985d8e37a686ccbe1cdcb8fc72fb14dca147
Naim <naimsantos45@gmail.com>
2020-06-08 12:01:02 -0300

    Minor fix for "Teamwork of the Possessed"
    Fixed a bug where it would be possible to activate the GY effect even if the player didnt have another "Possessed" Spell/Trap in the GY

--- pre-release/c100339031.lua
+++ pre-release/c100339031.lua
@@ -68,9 +68,9 @@ end
 function s.tftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tffilter(chkc,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(s.tffilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
+		and Duel.IsExistingTarget(s.tffilter,tp,LOCATION_GRAVE,0,1,e:GetHandler(),tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local g=Duel.SelectTarget(tp,s.tffilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
+	local g=Duel.SelectTarget(tp,s.tffilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler(),tp)
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
 end
 function s.tfop(e,tp,eg,ep,ev,re,r,rp)

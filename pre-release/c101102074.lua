910c1333b6c68b5901fa98818c59d04de87906f0
Naim <naimsantos45@gmail.com>
2020-06-09 16:54:13 -0300

    Fix "Souten's Duty"
    The player should only be able to activate it in a Main Phase

--- pre-release/c101102074.lua
+++ pre-release/c101102074.lua
@@ -9,12 +9,16 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_DRAW_PHASE+TIMINGS_CHECK_MONSTER_E,TIMING_DRAW_PHASE+TIMINGS_CHECK_MONSTER_E)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x247}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase()
+end
 function s.filter(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0x247) and not c:IsCode(id) 
 		and c:IsAbleToRemoveAsCost() and c:CheckActivateEffect(false,true,false)~=nil

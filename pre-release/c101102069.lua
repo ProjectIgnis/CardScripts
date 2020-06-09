e96a41fa983b0a4509aac42be7de6e2f59af8503
NaimSantos <naimsantos45@gmail.com>
2020-06-09 06:27:29 -0300

    Fix "Raidraptor - Phantom Knights' Claw"
    Updated the archetype verification when it detaches a monster

--- pre-release/c101102069.lua
+++ pre-release/c101102069.lua
@@ -32,12 +32,13 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return #g>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVEXYZ)
 	local sc=g:Select(tp,1,1,nil):GetFirst()
-	if sc:IsSetCard(0x2073) or sc:IsSetCard(0x10db) or sc:IsSetCard(0xba) then
+	Duel.SendtoGrave(sc,REASON_COST)
+	local og=Duel.GetOperatedGroup():GetFirst()
+	if og:IsSetCard(0x2073) or og:IsSetCard(0x10db) or og:IsSetCard(0xba) then
 		e:SetLabel(1)
 	else
 		e:SetLabel(0)
 	end
-	Duel.SendtoGrave(sc,REASON_COST)
 	Duel.RaiseSingleEvent(e:GetHandler(),EVENT_DETACH_MATERIAL,e,0,0,0,0)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)

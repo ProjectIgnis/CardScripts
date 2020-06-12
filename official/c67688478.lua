d6615b6391f756dac99d21f7ed5742d41ce3ce05
NaimSantos <naimsantos45@gmail.com>
2020-06-12 15:32:04 -0300

    Fix a typo

--- official/c67688478.lua
+++ official/c67688478.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	ux.DoubleSnareValidity(c,LOCATION_MZONE)
+	aux.DoubleSnareValidity(c,LOCATION_MZONE)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	if ep==tp or (not re:IsHasType(EFFECT_TYPE_ACTIVATE) and not re:IsActiveType(TYPE_MONSTER))

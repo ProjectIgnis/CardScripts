142d36486fb583e3e015a0a3f705deac3e08142f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-06-12 18:06:05 +0300

    "Gravity Press Dragon" update
    Wrong effect category.

--- rush/c160302002.lua
+++ rush/c160302002.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--ATK/DEF decrease
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)

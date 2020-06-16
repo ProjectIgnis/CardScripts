db8f23dfa34deafd46f72488e14c2f0eecbca6a8
NaimSantos <naimsantos45@gmail.com>
2020-06-16 10:42:27 -0300

    Added pre-release scripts

---
+++ pre-release/c100200186.lua
@@ -0,0 +1,47 @@
+--輪廻竜サンサーラ
+--Samsara Dragon
+--Scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Double tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DOUBLE_TRIBUTE)
+	e1:SetValue(s.condition)
+	c:RegisterEffect(e1)
+	--Add 1 Dragon to the hand, then Normal Summon
+	local e2=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+function s.condition(e,c)
+	return c:IsRace(RACE_DRAGON)
+end
+function s.thfilter(c)
+	return c:IsRace(RACE_DRAGON) and c:IsLevelAbove(5) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+		if tc:IsSummonable(true,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.BreakEffect()
+			Duel.Summon(tp,tc,true,nil)
+		end
+	end
+end

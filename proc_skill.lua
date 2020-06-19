60430f9a637aff440745ef1cf5e600ea41141ff6
larry126 <28985622+larry126@users.noreply.github.com>
2020-06-19 15:43:27 +0800

    Added Vrains Skill proc

--- proc_skill.lua
+++ proc_skill.lua
@@ -1,9 +1,12 @@
 HINT_SKILL = 200
 HINT_SKILL_COVER = 201
 HINT_SKILL_FLIP  = 202
--- HINT_SKILL_REMOVE = 203 (need to check with edo for name)
+HINT_SKILL_REMOVE = 203
 
-SKILL_COVER=300000000
+SKILL_COVER =   300000000
+VRAINS_SKILL_COVER  =   300000001
+
+EFFECT_NEGATE_SKILL =   EVENT_CUSTOM+300000000
 --c: the card you want the cover (card)
 --coverNum: the number of the cover, for ex, for the second cover, you pass 2 as a parameter (int)
 function Auxiliary.GetCover(c,coverNum)
@@ -71,7 +74,7 @@ end
 function Auxiliary.AddFieldSkillProcedure(c,coverNum,drawless)
 	c:Cover(Auxiliary.GetCover(c,coverNum))
 	--place in field zone
-	local e1=Effect.CreateEffect(c)	
+	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
@@ -113,7 +116,7 @@ end
 function Auxiliary.AddContinuousSkillProcedure(c,coverNum,drawless,flip)
 	c:Cover(Auxiliary.GetCover(c,coverNum))
 	--place in correct zone, then if required, flip the skill
-	local e1=Effect.CreateEffect(c)	
+	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
@@ -157,7 +160,7 @@ end
 -- flipOp: operation related to the skill activation (function)
 function Auxiliary.AddSkillProcedure(c,coverNum,drawless,skillcon,skillop,countlimit)
 	--activate
-	local e1=Effect.CreateEffect(c)	
+	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
@@ -190,7 +193,7 @@ function Auxiliary.SetSkillOp(coverNum,drawless,skillcon,skillop,countlimit,efft
 		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
 		--generate the skill in the "skill zone"
 		Duel.Hint(HINT_SKILL_COVER,c:GetControler(),coverid|(coverid<<32))
-		Duel.Hint(HINT_SKILL,c:GetControler(),c:GetCode())			
+		Duel.Hint(HINT_SKILL,c:GetControler(),c:GetCode())		  
 		--send to limbo then draw 1 if the skill was in the hand
 		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
 			Duel.Draw(p,1,REASON_RULE)
@@ -206,7 +209,7 @@ end
 -- flip con: condition to activate the skill (function)
 -- flipOp: operation related to the skill activation (function)
 function Auxiliary.AddPreDrawSkillProcedure(c,coverNum,drawless,skillcon,skillop,countlimit)
-	local e1=Effect.CreateEffect(c)	
+	local e1=Effect.CreateEffect(c) 
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
@@ -216,4 +219,65 @@ function Auxiliary.AddPreDrawSkillProcedure(c,coverNum,drawless,skillcon,skillop
 	if drawless then
 		aux.RegisterDrawless(c)
 	end
+end
+
+--Proc for Vrains Skills
+--flip con: condition to activate the skill (function)
+--flipOp: operation related to the skill activation (function)
+--efftype: Event to trigger the Skill, default to EVENT_FREE_CHAIN. Additionally accept EFFECT_NEGATE_SKILL for Anti Skill (int)
+function Auxiliary.AddVrainsSkillProcedure(c,skillcon,skillop,efftype)
+	efftype=efftype or EVENT_FREE_CHAIN
+	--activate
+	local e1=Effect.CreateEffect(c) 
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetRange(0x5f)
+	e1:SetOperation(Auxiliary.SetVrainsSkillOp(skillcon,skillop,efftype))
+	c:RegisterEffect(e1)
+end
+function Auxiliary.SetVrainsSkillOp(skillcon,skillop,efftype)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		local c=e:GetHandler()
+		if skillop~=nil then
+			local e1=Effect.CreateEffect(c)
+			if efftype~=EFFECT_NEGATE_SKILL then
+				e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			else
+				e1:SetType(EFFECT_TYPE_FIELD)
+				e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+				e1:SetTargetRange(1,0)
+			end
+			e1:SetCode(efftype)
+			e1:SetCondition(skillcon)
+			e1:SetOperation(skillop)
+			Duel.RegisterEffect(e1,e:GetHandlerPlayer())
+			if efftype==EVENT_FREE_CHAIN then
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+				e1:SetCode(EVENT_CHAIN_END)
+				e1:SetCondition(skillcon)
+				e1:SetOperation(skillop)
+				Duel.RegisterEffect(e1,e:GetHandlerPlayer())
+			end
+		end
+		Duel.DisableShuffleCheck(true)
+		Duel.SendtoDeck(c,tp,-2,REASON_RULE)
+		Duel.Hint(HINT_SKILL_COVER,c:GetControler(),VRAINS_SKILL_COVER)
+		Duel.Hint(HINT_SKILL,c:GetControler(),c:GetCode())	
+		if e:GetHandler():IsPreviousLocation(LOCATION_HAND) then 
+			Duel.Draw(p,1,REASON_RULE)
+		end
+		e:Reset()
+	end
+end
+
+--Function to check whether the Skill would be negated by Anti Skill
+function Auxiliary.CheckSkillNegation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(1-tp,EFFECT_NEGATE_SKILL) then
+		local eff=Duel.GetPlayerEffect(1-tp,EFFECT_NEGATE_SKILL)
+		if eff:GetCondition()(e,1-tp,eg,ep,ev,re,r,rp) then
+			return eff:GetOperation()(e,1-tp,eg,ep,ev,re,r,rp)
+		else return false end
+	else return false end
 end

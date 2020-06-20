2d54ee72615e8e1ad2fd3802a29129e1909f8083
Naim <naimsantos45@gmail.com>
2020-06-20 12:34:57 -0300

    CP20 official release script updates

---
+++ pre-release/c5846183.lua
@@ -0,0 +1,80 @@
+--遮断機塊ブレイカーバンクル
+--Appliancer Breakerbuncle
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	--battle Damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.indcon)
+	e1:SetCost(s.indcost)
+	e1:SetOperation(s.indop)
+	c:RegisterEffect(e1)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE+LOCATION_MZONE)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	e2:SetOperation(s.repop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x14a}
+function s.indcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if not d or a:GetControler()==d:GetControler() then return false end
+	local tc=a
+	if d:IsControler(tp) then
+		tc=d
+	end
+	if not (tc:IsFaceup() and tc:IsSetCard(0x14a)) then return false end
+	e:SetLabelObject(tc)
+	return true
+end
+function s.indcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.indop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	Duel.RegisterEffect(e1,tp)
+	local tc=e:GetLabelObject()
+	if tc and tc:IsRelateToBattle() and tc:IsFaceup() then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		e2:SetValue(1)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x14a) and c:IsLocation(LOCATION_MZONE)
+		and c:IsControler(tp) and c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and not e:GetHandler():IsStatus(STATUS_DESTROY_CONFIRMED)
+		and eg:IsExists(s.repfilter,1,nil,tp) end
+	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
+		return true
+	else
+		return false
+	end
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end

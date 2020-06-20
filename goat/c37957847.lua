d9a0c638b16307c4806f85436100342bfccecc3f
edo9300 <edoardo762@gmail.com>
2020-06-20 12:02:17 +0200

    Insect Princess (GOAT)

--- goat/c37957847.lua
+++ goat/c37957847.lua
@@ -1,4 +1,6 @@
 --インセクト・プリンセス
+--Insect Princess (GOAT)
+--Activation timing
 local s,id=GetID()
 function s.initial_effect(c)
 	--Pos Change
@@ -12,23 +14,25 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--atkup
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_BATTLED)
+	e2:SetCondition(s.condition)
 	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local other=e:GetHandler():GetBattleTarget()
+	return other and other:IsBattleDestroyed() and other:IsRace(RACE_INSECT)
 end
 function s.target(e,c)
 	return c:IsRace(RACE_INSECT)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local bc=c:GetBattleTarget()
-	if bc and bc:IsBattleDestroyed() and bc:IsRace(RACE_INSECT) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(500)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
-	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(500)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
 end

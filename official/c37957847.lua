cfab9a9dbd2f82b2abb9e3887b9c745dbdcf4d46
edo9300 <edoardo762@gmail.com>
2020-06-20 11:56:34 +0200

    Update Insect Princess
    Its effect is the same as zombyra the dark, a forced trigger, not a continuous effect

--- official/c37957847.lua
+++ official/c37957847.lua
@@ -1,4 +1,5 @@
 --インセクト・プリンセス
+--Insect Princess
 local s,id=GetID()
 function s.initial_effect(c)
 	--Pos Change
@@ -12,8 +13,8 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--atkup
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_BATTLED)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
 	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
 end
@@ -22,13 +23,10 @@ function s.target(e,c)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local bc=c:GetBattleTarget()
-	if bc and bc:IsStatus(STATUS_BATTLE_DESTROYED) and bc:IsRace(RACE_INSECT) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(500)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
-	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(500)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
 end

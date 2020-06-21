de857c9d695271a7fcb45865fd321254e537cfc1
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-06-21 14:17:01 +0300

    "Appliancer Electrilyrical World (Anime)" update

--- unofficial/c511030054.lua
+++ unofficial/c511030054.lua
@@ -35,8 +35,9 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 s.listed_series={0x14a}
+s.listed_names={3875465}
 function s.thfilter(c)
-	return c:IsType(TYPE_SPELL) and c:IsSetCard(0x14a) and c:IsAbleToHand() and not c:IsCode(c:Alias())
+	return c:IsType(TYPE_SPELL) and c:IsSetCard(0x14a) and c:IsAbleToHand() and not c:IsCode(3875465)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end

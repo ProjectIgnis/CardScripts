f284c244cd2d03f5de1eaf61f718e4fa7e66ef41
ahtelel <32773013+ahtelel@users.noreply.github.com>
2020-06-22 16:39:33 +0700

    Fix "Borreload Dragon"
    During the End Phase of the turn the monster (whose control was changed by Borreload Dragon's effect) is supposed to be sent to the GY, if it has returned to the field by Interdimensional Matter Transporter's effect, control of that monster will return to its owner, and it will not be sent to the GY. Closes #202 .

--- official/c31833038.lua
+++ official/c31833038.lua
@@ -83,7 +83,7 @@ function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and tc then
 		local zone=c:GetLinkedZone()&0x1f
-		if Duel.GetControl(tc,tp,0,0,zone)~=0 then
+		if Duel.GetControl(tc,tp,PHASE_END,2,zone)~=0 then
 			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)

26fe295365198fe6d7a09c240e91e5e2f260bd18
NaimSantos <naimsantos45@gmail.com>
2020-06-22 08:47:44 -0300

    batch 4: c5XX range

--- official/c50823978.lua
+++ official/c50823978.lua
@@ -1,19 +1,15 @@
 --軍隊ピラニア
+--Piranha Army
 local s,id=GetID()
 function s.initial_effect(c)
 	--deepen damage
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
 	e1:SetCondition(s.dcon)
-	e1:SetOperation(s.dop)
+	e1:SetValue(aux.ChangeBattleDamage(1,DOUBLE_DAMAGE))
 	c:RegisterEffect(e1)
 end
-function s.dcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return ep~=tp and c==Duel.GetAttacker() and Duel.GetAttackTarget()==nil
-end
-function s.dop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.DoubleBattleDamage(ep)
+function s.dcon(e)
+	return Duel.GetAttackTarget()==nil
 end

8c4d15abee1cd9b0dc17013e839a8d77e5d6dd4d
NaimSantos <naimsantos45@gmail.com>
2020-06-23 09:35:10 -0300

    Remove DoublePiercingDamage and HalfBattleDamage auxiliary functions

--- official/c28427869.lua
+++ official/c28427869.lua
@@ -39,7 +39,6 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local dam=Duel.GetBattleDamage(tp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)

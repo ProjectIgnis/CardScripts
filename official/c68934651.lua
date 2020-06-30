a36b5fe10dad9e3cede45a08c68658066d8cab79
NaimSantos <naimsantos45@gmail.com>
2020-06-30 15:26:33 -0300

    Fix "Firewall Dragon Darkfluid"

--- official/c68934651.lua
+++ official/c68934651.lua
@@ -81,7 +81,7 @@ end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetAttacker()
-	if Duel.NegateActivation(ev) and c==tc and c:CanChainAttack() then
+	if Duel.NegateActivation(ev) and c==tc and c:CanChainAttack(9999) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_DAMAGE_STEP_END)

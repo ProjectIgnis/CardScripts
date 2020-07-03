9faf38bdeb1badd76c33bd54c408dfdafddf836c
larry126 <28985622+larry126@users.noreply.github.com>
2020-07-03 08:43:06 +0800

    Fix Rank-Up-Magic - The Seventh One (Anime)

--- unofficial/c513000008.lua
+++ unofficial/c513000008.lua
@@ -1,4 +1,6 @@
---RUM－七皇の剣
+--RUM－七皇の剣 (Anime)
+--Rank-Up-Magic - The Seventh One (Anime)
+Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -11,28 +13,28 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x48,0x1048,0x1073}
-s.listed_names={511000296}
+s.listed_series={0x48,0x50e}
 function s.filter1(c,e,tp)
 	local m = c:GetMetatable(true)
 	if not m then return false end
 	local no, rk=tonumber(m.xyz_number), c:GetRank()
 	if not no or no < 101 or no > 107 or not c:IsSetCard(0x48) or rk <= 0 then return false end
-	return (c:IsLocation(LOCATION_MZONE) and c:IsFaceup())
+	return (c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsCanBeEffectTarget(e))
 		or (c:IsLocation(LOCATION_GRAVE|LOCATION_EXTRA) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 		and ((c:IsLocation(LOCATION_GRAVE) and Duel.GetLocationCount(tp,LOCATION_MZONE) > 0)
 		or (c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp,tp,nil,c) > 0)))
 end
 function s.filter2(c,e,tp,mc,rk)
-	return (c:IsSetCard(0x1048) or c:IsSetCard(0x1073) or c:IsCode(511000296)) and mc:IsCanBeXyzMaterial(c) and c:IsRank(rk)
+	return c:IsC() and mc:IsCanBeXyzMaterial(c) and c:IsRank(rk)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc,c) > 0
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE|LOCATION_GRAVE|LOCATION_EXTRA) and s.filter1(chkc,e,tp) end
-	if chk == 0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE|LOCATION_GRAVE|LOCATION_EXTRA,0,1,nil,e,tp) end
+	if chk == 0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_MZONE|LOCATION_GRAVE|LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE|LOCATION_GRAVE|LOCATION_EXTRA,0,1,1,nil,e,tp)
-	if not e:GetHandler():IsLocation(LOCATION_MZONE) then
+	local tc = Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_MZONE|LOCATION_GRAVE|LOCATION_EXTRA,0,1,1,nil,e,tp):GetFirst()
+	Duel.SetTargetCard(tc)
+	if not tc:IsLocation(LOCATION_MZONE) then
 		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,e:GetHandler():GetLocation())
 	end
 end

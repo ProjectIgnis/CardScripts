d2a0768672dc511e78c48eefdb7e1cc8d2c03086
NaimSantos <naimsantos45@gmail.com>
2020-07-10 15:20:58 -0300

    Update PHRA scripts
    Check for the location to summon from the Extra deck

--- pre-release/c101102007.lua
+++ pre-release/c101102007.lua
@@ -48,15 +48,16 @@ end
 function s.rmfilter(c)
 	return c:IsRace(RACES_BEAST_BWARRIOR_WINGB) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true,false)
 end
-function s.spfilter(c,e,tp,ct)
+function s.spfilter(c,e,tp,ct,g)
 	return c:IsRace(RACES_BEAST_BWARRIOR_WINGB) and c:IsType(TYPE_LINK) and c:IsLink(ct)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+		and Duel.GetLocationCountFromEx(tp,tp,g,c)>0
 end
 function s.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
 	local nums={}
 	for i=1,#g do
-		if Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,i) then
+		if Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,i,g) then
 			table.insert(nums,i)
 		end
 	end

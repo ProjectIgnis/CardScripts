2570498741e114a9bb971b5cce10591f53b5a3ac
Naim <naimsantos45@gmail.com>
2020-07-11 18:00:59 -0300

    Fix "Datascape Princess - Niangniang"
    The card should be shuffled into the Deck, not sent to the bottom

--- official/c8736823.lua
+++ official/c8736823.lua
@@ -96,6 +96,6 @@ end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
-		Duel.SendtoDeck(tc,nil,1,REASON_EFFECT)
+		Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 	end
 end

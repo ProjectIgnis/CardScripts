c326be6ea3a1d4e804cb367cbc508120cdc4bb2a
NaimSantos <naimsantos45@gmail.com>
2020-07-18 18:27:18 -0300

    Fix incorrect effect type used by "Temple of the Mind's Eye"

--- official/c92481084.lua
+++ official/c92481084.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--Change battle damage
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS)
+	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)

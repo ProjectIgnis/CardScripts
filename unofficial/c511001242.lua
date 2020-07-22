1181fefff2b1a25cd28e56b39775a044dcc1bc69
NaimSantos <naimsantos45@gmail.com>
2020-07-22 17:01:24 -0300

    Fix a typo

--- unofficial/c511001242.lua
+++ unofficial/c511001242.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.condition()
-	return Duel.IsBattePhase()
+	return Duel.IsBattlePhase()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end

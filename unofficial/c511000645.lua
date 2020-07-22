e068d842556bebcdcd2271b33857bd98da217f9d
NaimSantos <naimsantos45@gmail.com>
2020-07-22 16:52:08 -0300

    batch 2

--- unofficial/c511000645.lua
+++ unofficial/c511000645.lua
@@ -1,3 +1,4 @@
+--サービスエース
 --Service Ace
 local s,id=GetID()
 function s.initial_effect(c)
@@ -12,15 +13,16 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:GetLocation()==LOCATION_HAND and chkc:GetControler()==tp and chkc:IsAbleToRemove() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,nil) end
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_HAND,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+	Duel.SetTargetCard(g)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc and tc:IsRelateToEffect(e) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
 		local op=Duel.SelectOption(1-tp,70,71,72)
 		Duel.ConfirmCards(1-tp,tc)

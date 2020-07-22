3ed2b7ab34d882a7ebe04317ff37bd1f8c43cae6
Naim <naimsantos45@gmail.com>
2020-07-21 21:16:03 -0300

    Fix  "Crimson Nova the Dark Cubic Lord"
    Fixed a bug where the the second attack would reset if the effect of "Crimson Nova the Dark Cubic Lord" was negated

--- official/c30270176.lua
+++ official/c30270176.lua
@@ -97,6 +97,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToBattle() then return end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EFFECT_EXTRA_ATTACK)
 	e1:SetValue(1)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)

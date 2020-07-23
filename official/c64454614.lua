9da8b6e2d93de44870fa95c5419f8fe83dd94b57
NaimSantos <naimsantos45@gmail.com>
2020-07-23 08:20:05 -0300

    Update cards that negate activation
    This fixes cards that negate activations without performing a second action, like destroying.

--- official/c64454614.lua
+++ official/c64454614.lua
@@ -2,7 +2,7 @@
 --Arcana Extra Joker
 local s,id=GetID()
 function s.initial_effect(c)
-	--link summon
+	--Link summon procedure
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),3,3,s.lcheck)
 	c:EnableReviveLimit()
 	--Negate
@@ -59,7 +59,9 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp,chk)
-	Duel.NegateActivation(ev)
+	if Duel.NegateActivation(ev) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SendtoGrave(eg,REASON_EFFECT)
+	end
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()

91613a0ad90890cca6030eef7ea24212779aa11e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-26 18:16:41 +0300

    "Road Magic - Diastrophism" fix
    Small typos.

--- rush/c160002036.lua
+++ rush/c160002036.lua
@@ -27,7 +27,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup(),tp,0,LOCATION_MZONE,1,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
 		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,1,nil,TYPE_MONSTER) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
@@ -36,11 +36,13 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		--Effect
 		local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_MONSTER)
 		if #g==0 then return end
-		local tc=Duel.SelectMatchingCard(tp,Card.IsType,tp,0,LOCATION_MZONE,1,1,nil,TYPE_MONSTER):GetFirst()
+		local sg=Duel.SelectMatchingCard(tp,Card.IsType,tp,0,LOCATION_MZONE,1,1,nil,TYPE_MONSTER)
+		Duel.HintSelection(sg)
+		local tc=sg:GetFirst()
 		local gc=g:GetMaxGroup(Card.GetAttack):GetFirst()
 		if tc and gc then
 			local atk=gc:GetAttack()
-			local e1=Effect.CreateEffect(c)
+			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)

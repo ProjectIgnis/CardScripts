b6a3004f4892dc52384448d2f6df1acd75cecf09
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-07-26 21:37:48 +0300

    "Numeron Calling" fix
    Fixed its interaction with "Blue-Eyes Spirit Dragon".

--- official/c77402960.lua
+++ official/c77402960.lua
@@ -33,7 +33,9 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_EXTRA,0,nil,e,tp)
-	local sg=aux.SelectUnselectGroup(g,e,tp,1,4,s.check,1,tp,HINTMSG_SPSUMMON)
+	local ct=4
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,ct,s.check,1,tp,HINTMSG_SPSUMMON)
 	if #sg>0 then
 		local fid=c:GetFieldID()
 		for tc in aux.Next(sg) do

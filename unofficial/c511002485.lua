ce77c4425df1055811a35a13e758b2a6c7d458fc
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2020-07-27 19:10:51 -0700

    Fix for "Soprano the Melodious Songstress (Anime)" (#358)
    Fixed monster being unable to activate Fusion Summon effect even with valid targets in Extra Deck.

--- unofficial/c511002485.lua
+++ unofficial/c511002485.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--perform a fusion summon
-	local params = {nil,Fusion.CheckWithHandler(nil,nil,nil,Fusion.ForcedHandler)}
+	local params = {nil,nil,nil,nil,Fusion.ForcedHandler}
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)

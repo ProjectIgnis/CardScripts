97cc0aaeb6aa48b7c5a5916c4c391ffec3d5307b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-08-02 23:46:39 +0300

    Update c99666430.lua

--- official/c99666430.lua
+++ official/c99666430.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
 	--Tribute 1 monster and destroy 1 card on the field
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)

95c5652b33561296ab794348eebb1630ee8ca3b6
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-08-09 19:01:45 +0300

    Update c34568403.lua

--- official/c34568403.lua
+++ official/c34568403.lua
@@ -80,8 +80,6 @@ function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	--Tails
 	if c:GetFlagEffectLabel(36690018)==0 and c:GetFlagEffectLabel(id)==0 then
 		c:SetFlagEffectLabel(id,1)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-		local zone=Duel.SelectDisableField(tp,1,0,LOCATION_MZONE,0)>>16
-		Duel.GetControl(c,1-tp,0,0,zone)
+		Duel.GetControl(c,1-tp,0,0)
 	end
 end

77bb7b87db432fd14d2e2c21b5135a56cfa7ab75
Larry126 <28985622+larry126@users.noreply.github.com>
2020-08-14 20:55:44 +0800

    Unofficial cards update
    - Ancient Gear Scrap Fusion cleaned up
    - Magical Mallet (Anime) cleaned up
    - Banish Reactor fix

--- unofficial/c511002823.lua
+++ unofficial/c511002823.lua
@@ -1,30 +1,31 @@
+--古代の機械廃棄融合
 --Ancient Gear Scrap Fusion
 --rescripted by Naim to match the Fusion Summon procedure
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x7),matfilter=aux.FALSE,extrafil=s.extrafilter,stage2=s.stage2})
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x7),matfilter=aux.FALSE,extrafil=s.extrafilter,stage2=s.stage2,extraop=s.extraop})
 	c:RegisterEffect(e1)
-end
-function s.filter1(c,e)
-	return c:IsType(TYPE_MONSTER) and c:IsCanBeFusionMaterial() and (not e or not c:IsImmuneToEffect(e))
 end
 function s.extrafilter(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,e)
 end
-function s.stage2(e,tc,tp,sg,chk)
-	if chk==1 and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		local c=e:GetHandler()
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1,true)
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e2,true)
-		Duel.SpecialSummonComplete()
-		tc:CompleteProcedure()
+function s.extraop(e,tc,tp,sg)
+	for sc in aux.Next(sg) do
+		sc:SetReason(sc:GetReason()|REASON_MATERIAL|REASON_FUSION)
+		sc:SetReasonCard(tc)
 	end
+	sg:Clear()
+end
+function s.stage2(e,tc,tp,sg,chk)
+	if chk~=0 then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e1,true)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e2,true)
 end

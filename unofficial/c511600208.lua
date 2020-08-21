7d838bf1176887eaedcd350fc8839c858c94fbb1
Naim <naimsantos45@gmail.com>
2020-08-21 18:35:45 -0300

    Fix "Morley's Shield"

--- unofficial/c511600208.lua
+++ unofficial/c511600208.lua
@@ -29,13 +29,11 @@ function s.dmcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.dmop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e1:SetOperation(s.damop)
-	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
 	Duel.RegisterEffect(e1,tp)
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	Duel.ChangeBattleDamage(tp,0)
 end

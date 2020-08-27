8abb65aab184077054363cd5345fffbde2ffc90f
Larry126 <28985622+larry126@users.noreply.github.com>
2020-08-27 16:40:41 +0800

    Added failsafe message

--- unofficial/c153000006.lua
+++ unofficial/c153000006.lua
@@ -3,6 +3,10 @@
 --Scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
+	if not DeckMaster then
+		Debug.Message("You are not using "..math.floor(id).." properly!")
+		return
+	end
 	--Deck Master Effect
 	local dme1=Effect.CreateEffect(c)
 	dme1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
--- unofficial/c153000013.lua
+++ unofficial/c153000013.lua
@@ -3,6 +3,10 @@
 --Scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
+	if not DeckMaster then
+		Debug.Message("You are not using "..math.floor(id).." properly!")
+		return
+	end
 	--Deck Master Ability
 	local dme1=Effect.CreateEffect(c)
 	dme1:SetType(EFFECT_TYPE_FIELD)

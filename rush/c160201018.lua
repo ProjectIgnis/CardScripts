1d82f1df3138b553fc6a16d936cd05183fc6e132
Naim <naimsantos45@gmail.com>
2020-09-02 15:02:12 -0300

    Fix  "Royal Demon's Shout"

--- rush/c160201018.lua
+++ rush/c160201018.lua
@@ -8,10 +8,14 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
+	e1:SetCostn(s.cost)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
 end
 function s.filter(c)
 	return c:IsFaceup() and c:IsLevelAbove(7)
@@ -24,7 +28,11 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,1-tp,2)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	--Effect
-	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND):RandomSelect(tp,2)
-	Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	if Duel.SendtoGrave(g,REASON_COST)~=0 then
+		--Effect
+		local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND):RandomSelect(tp,2)
+		Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)
+	end
 end

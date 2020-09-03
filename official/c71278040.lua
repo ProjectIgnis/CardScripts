8729be16656b09bf733c6cf8fc1638a80895b6ae
Naim <naimsantos45@gmail.com>
2020-09-03 14:28:14 -0300

    Update c71278040.lua

--- official/c71278040.lua
+++ official/c71278040.lua
@@ -59,7 +59,7 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local zones=s.getLinkedZones(eg,tp)
-	if c:IsRelateToEffect(e) and zones~=0 then
+	if c:IsRelateToEffect(e) and zones~=0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zones) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP,zones)
 	end
 end

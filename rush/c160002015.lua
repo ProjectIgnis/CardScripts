4cf5c586149f85d0e071431cb861d34343bf73e5
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-09-04 20:21:32 +0200

    added Royal's Demon Invasion

---
+++ rush/c160002015.lua
@@ -0,0 +1,43 @@
+--ロイヤルデモンズ・インヴェイジョン
+--Royal Demon’s Invasion
+local s,id=GetID()
+function s.initial_effect(c)
+	--mat check
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_MATERIAL_CHECK)
+	e0:SetValue(s.valcheck)
+	c:RegisterEffect(e0)
+	--Destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetLabelObject(e0)
+	e1:SetCondition(s.descond)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+end
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	local flag=0
+	if g:IsExists(Card.IsLevelAbove,2,nil,5) then flag=1 end
+	e:SetLabel(flag)
+end
+function s.descond(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsStatus(STATUS_SUMMON_TURN) and e:GetLabelObject():GetLabel()~=0
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsLevelBelow,4),tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsLevelBelow,4),tp,0,LOCATION_MZONE,nil)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end

cd6b9a30aa02d3d0352ca4f49b806d63fe306756
Sanity Sierota <47074795+that-hatter@users.noreply.github.com>
2020-09-08 05:35:17 +0800

    Fix "Climax Finale" (#404)
    Fixed cost variable assignment and usage

--- rush/c160201038.lua
+++ rush/c160201038.lua
@@ -12,8 +12,8 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_GRAVE,0,nil,RACE_PSYCHIC)
-	local cost=#ct*100
+	local ct=Duel.GetMatchingGroupCount(Card.IsRace,e:GetHandler():GetControler(),LOCATION_GRAVE,0,nil,RACE_PSYCHIC)
+	local cost=ct*100
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_PSYCHIC),tp,LOCATION_MZONE,0,1,nil) and Duel.CheckLPCost(tp,cost) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
@@ -21,8 +21,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	--requirement
 	local ct=Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_GRAVE,0,nil,RACE_PSYCHIC)
-	local cost=#ct*100
-	Duel.PayLPCost(tp,1000)
+	local cost=ct*100
+	Duel.PayLPCost(tp,cost)
    	--effect
 	local g=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.IsRace,RACE_PSYCHIC),tp,LOCATION_MZONE,0,1,1,nil)
 	if #g>0 then

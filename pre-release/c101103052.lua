33830fc5dbee1833a4c95f9912dfeddb37c4694b
NaimSantos <naimsantos45@gmail.com>
2020-09-09 16:51:23 -0300

    Update c101103052.lua

--- pre-release/c101103052.lua
+++ pre-release/c101103052.lua
@@ -45,7 +45,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		aux.ToHandOrElse(sc,tp,function(c)
 						return sc:IsCanBeSpecialSummoned(e,0,tp,true,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end,
 						function(c)
-							if Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP)~=0 then
+							if Duel.SpecialSummonStep(sc,0,tp,tp,true,false,POS_FACEUP)~=0 then
 								local e1=Effect.CreateEffect(c)
 								e1:SetDescription(aux.Stringid(id,2))
 								e1:SetType(EFFECT_TYPE_SINGLE)

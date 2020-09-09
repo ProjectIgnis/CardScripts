0b9588d307ace6e1d10a4f281a0492c13811ea0a
NaimSantos <naimsantos45@gmail.com>
2020-09-09 16:26:19 -0300

    Optmization to "Armed Dragon LV10 - White" to prevent lag

--- pre-release/c101103005.lua
+++ pre-release/c101103005.lua
@@ -50,7 +50,9 @@ s.listed_names={49306994}
 s.LVnum=10
 s.LVset=0x111
 function s.rescon(sg,tp)
-	return aux.ChkfMMZ(1)(sg,nil,tp) and sg:GetSum(Card.GetLevel)==10
+	if not aux.ChkfMMZ(1)(sg,nil,tp) then return false end
+	local sum=sg:GetSum(Card.GetLevel)
+	return sum==10,sum>10
 end
 function s.cfilter(c)
 	return c:IsAbleToRemoveAsCost() and c:HasLevel() and c:IsSetCard(0x111) and aux.SpElimFilter(c,true,true)

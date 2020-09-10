f0b29379a34d7a3348c5e9d84e55797b6812b0a5
Naim <naimsantos45@gmail.com>
2020-09-10 10:06:08 -0300

    Update c101103002.lua

--- pre-release/c101103002.lua
+++ pre-release/c101103002.lua
@@ -35,7 +35,7 @@ function s.initial_effect(c)
 	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
-s.listed_names={59464593}
+s.listed_names={73879377}
 s.listed_series={0x111}
 s.LVnum=7
 s.LVset=0x111

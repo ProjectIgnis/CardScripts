095a854c49f38316b595276834e4c9caad11be57
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-09-13 11:44:44 +0200

    Script Clean up
    Updated the eX value and replaced the effect in fossil by Auxiliary.addLizardCheck

--- official/c12015000.lua
+++ official/c12015000.lua
@@ -7,12 +7,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_ROCK),s.ffilter)
 	--lizard check
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(CARD_CLOCK_LIZARD)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetValue(1)
-	c:RegisterEffect(e0)
+	Auxiliary.addLizardCheck(c)
 	--Special summon condition
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)

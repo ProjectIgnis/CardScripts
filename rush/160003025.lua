fb00c872ae16565fcc53793a1702d610fffb78e1
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-09-14 13:09:14 +0200

    Added new script
    - Fantastrike Dragon Miragears
    - Return of Dragon

---
+++ rush/160003025.lua
@@ -0,0 +1,69 @@
+-- 幻撃竜ミラギアス
+--Fantastrike Dragon Miragears
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate (summon)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_DRAGON) and c:IsLevelAbove(7) and c:IsAbleToDeckAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.atkfilter(c)
+	return c:IsType(TYPE_MONSTER) and Ic:IsFaceup() and c:GetAttack()>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.atkfilter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tg=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	if Duel.SendtoGrave(tg,nil,REASON_COST)==1 then
+		local g=Duel.GetMatchingGroup(s.atkfilter,tp,0,LOCATION_MZONE,nil)
+		if #g>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+			local g2=Duel.SelectMatchingCard(tp,s.atkfilter,tp,0,LOCATION_MZONE,1,2,nil)
+			if #g2>0 then
+				for tc in aux.Next(g2) do
+					local e1=Effect.CreateEffect(c)
+					e1:SetType(EFFECT_TYPE_SINGLE)
+					e1:SetCode(EFFECT_UPDATE_ATTACK)
+					e1:SetValue(-1500)
+					e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+					tc:RegisterEffect(e1)
+				end
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+				e1:SetCondition(s.macon)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+				e1:SetValue(1)
+				c:RegisterEffect(e1)
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+				e2:SetCode(EVENT_BATTLE_DESTROYING)
+				e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e2:SetCondition(aux.bdcon)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+				e2:SetOperation(s.bdop)
+				c:RegisterEffect(e2)
+			end
+		end
+	end
+end
+function s.macon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():GetFlagEffect(id+1)>0
+end
+function s.bdop(e,tp,eg,ep,ev,re,r,rp)
+    e:GetHandler():RegisterFlagEffect(id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end

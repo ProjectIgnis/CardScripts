db4e70eb30b7078c57de13bf38e573466e43b626
lrhel <lrhel@users.noreply.github.com>
2020-09-22 03:48:42 +0200

    Fix "Luminous Parrot"
    - removing the wrong category
    - make the card mill the deck instead of of discarding from hand
    - make the card select an opponent card
    - adjust the hint message
    - removing a function never being called

--- rush/c160003036.lua
+++ rush/c160003036.lua
@@ -13,9 +13,6 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.costfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToGraveAsCost()
-end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
 end
@@ -27,16 +24,13 @@ function s.attfilter(c)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.attfilter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	--requirement
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local tg=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
-	if Duel.SendtoGrave(tg,nil,REASON_COST)==1 then
+	if Duel.DiscardDeck(tp,1,REASON_COST)==1 then
 		--effect
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.attfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
+		local g=Duel.SelectMatchingCard(tp,s.attfilter,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
 		if #g>0 then
 			local tc=g:GetFirst()
 			local e1=Effect.CreateEffect(e:GetHandler())

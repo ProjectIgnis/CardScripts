708e6f2fc35ddaabcbdb10408985d89c30f0b696
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-10-07 22:38:53 +0200

    added new script
    Lightsky Maha Vailo and Ojama Pink

---
+++ pre-release/c101103036.lua
@@ -0,0 +1,58 @@
+--ジャマ・ピンク
+--Ojama Pink
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Draw and discard
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0xf}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) or e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,PLAYER_ALL,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local h1=Duel.Draw(tp,1,REASON_EFFECT)
+	local h2=Duel.Draw(1-tp,1,REASON_EFFECT)
+	if h1>0 or h2>0 then Duel.BreakEffect() end
+	local sealbool=false
+	if h1>0 then
+		Duel.ShuffleHand(tp)
+		if Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)>0 then
+			local dc=Duel.GetOperatedGroup():GetFirst()
+			if dc:IsSetCard(0xf) then sealbool=true end
+		end
+	end
+	if h2>0 then 
+		Duel.ShuffleHand(1-tp)
+		Duel.DiscardHand(1-tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
+	end
+	if sealbool and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
+	and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.BreakEffect()
+		local zone=Duel.SelectDisableField(tp,1,0,LOCATION_MZONE,0)
+		if tp==1 then
+				zone=((zone&0xffff)<<16)|((zone>>16)&0xffff)
+		end
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_DISABLE_FIELD)
+		e1:SetValue(zone)
+		e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		Duel.RegisterEffect(e1,tp)
+	end
+end

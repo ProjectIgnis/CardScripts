033589b1116496106052f7415d1ce7417da29218
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-10-09 20:16:15 -0400

    Updated "Counter Shield"
    Opponent's attacking monster should be losing 1000 ATK, not your attacked monster gaining 1000 ATK.

--- rush/c160003056.lua
+++ rush/c160003056.lua
@@ -19,7 +19,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tc and tc:IsFaceup() and tc:IsControler(tp) and tc:IsType(TYPE_NORMAL)
 end
 function s.cfilter(c)
-	return tc:IsType(TYPE_NORMAL) and c:IsType(TYPE_MONSTER) and c:IsDefenseBelow(1000) and c:IsAbleToDeckAsCost()
+	return c:IsType(TYPE_NORMAL) and c:IsType(TYPE_MONSTER) and c:IsDefenseAbove(1000) and c:IsAbleToDeckAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
@@ -38,12 +38,12 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)>0 then
 		--Effect
-		local tc=Duel.GetAttackTarget()
+		local tc=Duel.GetAttacker()
 		if tc and tc:IsRelateToBattle() and tc:IsFaceup() then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(1000)
+			e1:SetValue(-1000)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			tc:RegisterEffect(e1)
 		end

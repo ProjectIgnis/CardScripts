682ba9f6b662fac14657d26758ad01d5beb06005
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-10-10 22:18:11 +0200

    Added new scripts
    OCG:
    - Divine Treebeast High Peryton
    
    Rush:
    - Throne of Darkness
    - Dayu the Dark King of Chairs
    - 3K Bolt

---
+++ pre-release/c101103047.lua
@@ -0,0 +1,81 @@
+--神樹獣ハイペリュトン
+--Divine Treebeast High Peryton
+--scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,9,2)
+	c:EnableReviveLimit()
+	--attach
+	local e1=Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id,0))
+  e1:SetCategory(CATEGORY_LEAVE_GRAVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetCountLimit(1,id)
+  e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_CARD_TARGET)
+  e1:SetCondition(s.attcon)
+  e1:SetTarget(s.atttg)
+	e1:SetOperation(s.attop)
+  c:RegisterEffect(e1)
+  --negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCondition(s.negcon)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+  aux.DoubleSnareValidity(c,LOCATION_MZONE)
+end
+function s.attcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and rp==tp and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+end
+function s.atttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  local rt=re:GetActiveType()&(TYPE_SPELL|TYPE_MONSTER|TYPE_TRAP)
+  if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and chkc:IsType(rt) end
+  if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,LOCATION_GRAVE,0,1,nil,rt) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,Card.IsType,tp,LOCATION_GRAVE,0,1,1,nil,rt)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+end
+function s.attop(e,tp,eg,ep,ev,re,r,rp)
+  local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local g=Duel.GetTargetCards(e)
+		if #g>0 then
+			Duel.Overlay(c,g)
+		end
+	end
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and Duel.IsChainNegatable(ev) and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then 
+    local rt=re:GetActiveType()&(TYPE_SPELL|TYPE_MONSTER|TYPE_TRAP)
+    return e:GetHandler():GetOverlayGroup():IsExists(Card.IsType,1,nil,rt)
+  end
+  Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+  if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+  local rt=re:GetActiveType()&(TYPE_SPELL|TYPE_MONSTER|TYPE_TRAP)
+  local ov=e:GetHandler():GetOverlayGroup():Filter(Card.IsType,nil,rt)
+  if #ov<1 then return end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+  local sg=ov:Select(tp,1,1,true,nil)
+  if #sg>0 and Duel.SendtoGrave(sg,REASON_EFFECT)>0 then
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+      Duel.Destroy(eg,REASON_EFFECT)
+    end
+  end
+end
---
+++ rush/c160202100.lua
@@ -0,0 +1,41 @@
+--イスの冥王ダユー
+--Dayu the Dark King of Chairs
+local s,id=GetID()
+function s.initial_effect(c)
+	--ATK increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+	Duel.DiscardDeck(tp,1,REASON_COST)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsAttack(0) and c:IsLevelAbove(1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(g:GetFirst():GetLevel()*500)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			g:GetFirst():RegisterEffect(e1)
+		end
+	end
+end

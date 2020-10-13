71175204d5b06fcd365a7fe830e8be5b04268180
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-12 20:21:05 -0400

    Small fix "Clear Ice Dragon"
    Now the effect can choose up to 2 monsters in the opponent's GY of any specification.

--- rush/c160003022.lua
+++ rush/c160003022.lua
@@ -21,7 +21,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,nil) end
 end
 function s.shfilter(c)
-	return c:IsLevelBelow(5) and c:IsRace(RACE_DRAGON)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeck()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.shfilter,tp,0,LOCATION_GRAVE,1,nil) end

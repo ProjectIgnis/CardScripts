e4137f4862b35d9326b0ce2bc2fff60a06ce9dbc
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2020-10-15 22:27:04 -0700

    Add "Zero Potential"

---
+++ unofficial/c511003209.lua
@@ -0,0 +1,34 @@
+--ゼロ・ポテンシャル
+--Zero Potential (Manga)
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--0 Battle Damage/Draw 1 card
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetBattleDamage(tp)>0 and Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Draw(1-tp,1,REASON_EFFECT)
+	Duel.Draw(tp,1,REASON_EFFECT)
+	local dg=Duel.GetOperatedGroup():GetFirst()
+	if dg:IsSetCard(0x10af) then
+		Duel.ConfirmCards(1-tp,dg)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+		e1:SetOperation(s.damop)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(tp,0)
+end

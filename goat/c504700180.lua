67a3b89b5b109cbc5cdf8a93d6de296edf42472c
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-16 11:10:14 -0400

    Add script "Altar for Tribute (GOAT)"

---
+++ goat/c504700180.lua
@@ -0,0 +1,45 @@
+--生贄の祭壇
+--Altar for Tribute
+--Tokens can be selected to send for cost (but no LP gain occurs)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsAbleToGraveAsCost() or c:IsType(TYPE_TOKEN)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	local atk=(tc:IsType(TYPE_TOKEN) and 0) or tc:GetTextAttack()
+	if atk<0 then atk=0 end
+	e:SetLabel(atk)
+	Duel.SendtoGrave(tc,REASON_COST)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local res=e:GetLabel()~=0
+		e:SetLabel(0)
+		return res
+	end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(e:GetLabel())
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,e:GetLabel())
+	e:SetLabel(0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end

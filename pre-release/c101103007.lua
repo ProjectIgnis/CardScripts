3fc1cc3c339b995eb4d26113e0b9f219c11de3a7
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-17 16:28:08 -0400

    Small fix "Sprigguns Pede"
    Now the description for the effects should be accurate

--- pre-release/c101103007.lua
+++ pre-release/c101103007.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--Special summon 1 "Spriggun" monster from GY
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)

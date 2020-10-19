14f8d2b546888eac770863649e72c387f303e408
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-18 20:25:34 -0400

    Small fix "Battlewasp - Dart the Hunter"
    Check that the opponent owns the monster

--- official/c91283212.lua
+++ official/c91283212.lua
@@ -34,6 +34,7 @@ function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	local bc=tc:GetBattleTarget()
 	return tc:IsRelateToBattle() and tc:IsStatus(STATUS_OPPO_BATTLE) and tc:IsControler(tp) and tc:IsSetCard(0x12f)
 		and bc:IsLocation(LOCATION_GRAVE) and bc:IsReason(REASON_BATTLE) and bc:GetPreviousControler()~=tp and bc:GetBaseAttack()>0
+		and bc:GetOwner()==1-tp
 end
 function s.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsDiscardable() end

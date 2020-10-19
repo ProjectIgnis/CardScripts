d91fc1485c4fd545c7d0c02bbefdf59a56bc3c02
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-18 22:25:23 -0400

    Add script "Red-Eyes Spirit (Anime)"

---
+++ unofficial/c511017003.lua
@@ -0,0 +1,31 @@
+--レッドアイズ・スピリッツ
+--Red-Eyes Spirit
+--scripted by senpaizuri
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x3b}
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x3b) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+		and c:IsReason(REASON_DESTROY) and c:GetTurnID()==Duel.GetTurnCount()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if tc then Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP) end
+end

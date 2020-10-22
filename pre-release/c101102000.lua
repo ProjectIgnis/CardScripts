59e9885edd61695cefa663501f1759d703efab99
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-10-22 16:10:09 -0400

    Updated "Myutant Cry"
    Should only be able to activate during the main phase.

--- pre-release/c101102000.lua
+++ pre-release/c101102000.lua
@@ -9,6 +9,7 @@ function s.initial_effect(c)
 	--By shuffling fusion materials from field/GY/face-up banished into deck
 	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x259),Fusion.OnFieldMat(Card.IsAbleToDeck),s.fextra,Fusion.ShuffleMaterial)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
 	e1:SetHintTiming(0,TIMING_MAIN_END)
 	c:RegisterEffect(e1)
 	if not GhostBelleTable then GhostBelleTable={} end
@@ -19,4 +20,8 @@ s.listed_series={0x259}
 
 function s.fextra(e,tp,mg)
 	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(aux.NecroValleyFilter(Card.IsFaceup,Card.IsAbleToDeck)),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
+end
+	--Check if it's the main phase
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2
 end

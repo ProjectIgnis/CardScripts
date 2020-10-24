9c2466956aaeca2d466d75947018a0814015a57a
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-24 00:57:53 -0400

    Small fix "Speed World 2"
    Now the debug occurs and the script doesn't error.

--- unofficial/c110000000.lua
+++ unofficial/c110000000.lua
@@ -6,6 +6,6 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_STARTUP)
 	e1:SetCountLimit(1)
-	e1:SetOperation(function() Debug.Message("Turbo Duel undergoing overhaul"))
+	e1:SetOperation(function() Debug.Message("Turbo Duel undergoing overhaul") end)
 	Duel.RegisterEffect(e1,0)
 end

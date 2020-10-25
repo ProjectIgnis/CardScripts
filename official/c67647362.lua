77de5099ee52a0ed83eba907c420508badcc2927
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-10-25 16:47:43 -0400

    Updated "Blackbeard, the Plunder Patroll Captain"
    Its effect is now classified as an effect that equips and draws.

--- official/c67647362.lua
+++ official/c67647362.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	--Special summon 1 "Plunder Patroll" monster from extra deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)

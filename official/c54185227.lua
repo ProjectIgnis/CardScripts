2772ace64a05af06e00ed5d5ff6bdafe7091cb37
senpaizuri3 <senpaizuri3@gmail.com>
2020-10-26 19:58:29 -0400

    Small fix "Vision HERO Gravito"
    Now the effect can only activate if you have 2 or more Vision HERO Monsters in your Spell/Trap Zone (rather than any 2 Spells/Traps)

--- official/c54185227.lua
+++ official/c54185227.lua
@@ -52,13 +52,13 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function s.spfilter(c,e,sp)
-	return c:IsFaceup() and c:IsSetCard(0x5008) and c:IsCanBeSpecialSummoned(e,0,sp,true,false)
+	return c:IsFaceup() and c:IsSetCard(0x5008) and c:GetSequence()<5 and c:IsCanBeSpecialSummoned(e,0,sp,true,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
-	if chk==0 then return ft>1 and Duel.IsExistingTarget(s.filter,tp,LOCATION_SZONE,0,2,nil,e,tp)
+	if chk==0 then return ft>1 and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_SZONE,0,2,nil,e,tp)
 		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_SZONE,0,2,2,nil,e,tp)

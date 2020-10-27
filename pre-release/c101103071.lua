d055d4a1c1350e3a596ac5e03901112463467b5c
edo9300 <edoardo762@gmail.com>
2020-10-27 11:26:19 +0100

    Fix Security Force Specimen
    Was allowing any mosnter to be targeted to move

--- pre-release/c101103071.lua
+++ pre-release/c101103071.lua
@@ -59,9 +59,9 @@ end
 function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-							and Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,0,1,nil,0x25a) end
+							and Duel.IsExistingTarget(aux.FilterFaceupFunction(Card.IsSetCard,0x25a),tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
-	Duel.SelectTarget(tp,nil,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.IsSetCard,0x25a),tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.seqop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()

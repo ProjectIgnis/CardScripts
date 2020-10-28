b892c7e0e466efa1ffff2d640808541d7ea9a868
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-10-28 15:26:44 +0100

    added new scripts
    - Darkness Crested Hawk
    - Counter Heart

---
+++ rush/c160003057.lua
@@ -0,0 +1,42 @@
+--カウンター・ハート
+--Counter Heart
+
+--Substitute ID
+local s,id=GetID()
+function s.initial_effect(c)
+	--Opponent's attacking monster loses ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_ATKCHANGE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttackTarget()
+	return tc and tc:IsFaceup() and tc:IsControler(tp) and tc:IsRace(RACE_WINGEDBEAST) and tc:IsPosition(POS_FACEUP_ATTACK)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetAttackTarget()
+	if chk==0 then return tg:IsControler(tp) and tg:IsOnField() end
+	Duel.SetChainLimit(s.chlimit)
+end
+function s.chlimit(e,ep,tp)
+	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	local tc=Duel.GetAttacker()
+	local atk=tc:GetLevel()*100
+	if tc and tc:IsRelateToBattle() and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end

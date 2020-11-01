e5b7b13ed67b4348e120b42745dc699f1ec34f6c
edo9300 <edoardo762@gmail.com>
2020-11-02 00:51:57 +0100

    Update security force archetype for auxiliary function

--- cards_specific_functions.lua
+++ cards_specific_functions.lua
@@ -604,5 +604,5 @@ function Auxiliary.SecurityTarget(e,c)
 	local g2=og:GetMaxGroup(Card.GetSequence)
 	local c1=g1:GetFirst()
 	local c2=g2:GetFirst()
-	return c1 and c2==c and c1:IsFaceup() and c1:IsSetCard(0x25a)
+	return c1 and c2==c and c1:IsFaceup() and c1:IsSetCard(0x15a)
 end

0bb97972e02d196ccd523f399364be8766d051f6
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-06 18:33:18 +0800

    Update "Afterglow (Anime)"

--- unofficial/c511050013.lua
+++ unofficial/c511050013.lua
@@ -6,60 +6,57 @@ function s.initial_effect(c)
 	alias=c:Alias()
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCategory(CATEGORY_TODECK)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_names={alias}
+function s.filter(c)
+	return c:IsCode(alias) and c:IsAbleToRemove()
+end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_DECK,0,nil,alias)
-	if chk==0 then return #g>0 and g:FilterCount(Card.IsAbleToRemove,nil)==#g end
+	if chk==0 then return e:GetHandler():IsAbleToDeck() end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,tp,LOCATION_SZONE)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_DECK,0,nil,alias)
-	local ct=Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
-	if ct>0 and c:IsRelateToEffect(e) then
+	Duel.Remove(Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil),POS_FACEUP,REASON_EFFECT)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.BreakEffect()
+		local c=e:GetHandler()
 		c:CancelToGrave()
 		Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
 		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-		e1:SetProperty(EFFECT_FLAG_DELAY)
-		e1:SetCategory(CATEGORY_DAMAGE)
+		e1:SetDescription(aux.String(id,0))
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_DRAW)
-		e1:SetCountLimit(1)
 		e1:SetCondition(s.con)
-		e1:SetTarget(s.tg)
 		e1:SetOperation(s.op)
+		e1:SetOwnerPlayer(tp)
 		if Duel.GetTurnPlayer()==tp and Duel.GetCurrentPhase()==PHASE_DRAW then
 			e1:SetLabel(Duel.GetTurnCount())
-			e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN,2)
+			e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN+RESET_EVENT+RESETS_STANDARD-RESET_TOHAND,2)
 		else
 			e1:SetLabel(0)
-			e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN)
+			e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN+RESET_EVENT+RESETS_STANDARD-RESET_TOHAND)
 		end
 		c:RegisterEffect(e1)
 	end
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(4000)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,4000)
-end
 function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and (r&REASON_RULE)~=0 and Duel.GetTurnPlayer()==tp and Duel.GetTurnCount()~=e:GetLabel()
-		and Duel.GetCurrentPhase()==PHASE_DRAW
+	return ep==e:GetOwnerPlayer() and Duel.GetTurnPlayer()==e:GetOwnerPlayer()
+		and Duel.GetCurrentPhase()==PHASE_DRAW and Duel.GetTurnCount()~=e:GetLabel()
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
+	if Duel.SelectEffectYesNo(tp,e:GetHandler()) then
 		Duel.Hint(HINT_CARD,0,id)
-		Duel.ConfirmCards(1-tp,c)
+		Duel.ConfirmCards(1-tp,e:GetHandler())
 		Duel.Damage(1-tp,4000,REASON_EFFECT)
+		Duel.ShuffleHand(tp)
 	end
-	Duel.ShuffleHand(tp)
 end

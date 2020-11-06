628d1c1d1f1b87343da20cfb2aa5129ef31b7518
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-06 10:59:48 +0800

    Unofficial cards script updates
    - "Level Down!" effect correction
    - "Celestial Tuner" ATK effect fixed

--- unofficial/c100000250.lua
+++ unofficial/c100000250.lua
@@ -1,31 +1,33 @@
 --レベルダウン！
+--Level Down!
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_LVCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.tg)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 function s.filter(c)
-	return c:IsSetCard(0x41) and c:GetLevel()>2
+	return c:IsSetCard(0x41) and c:GetLevel()>2 and not c:IsPublic()
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil)
-	if #g>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-		local sg=g:Select(tp,1,1,nil)
-		local tc=sg:GetFirst()
-		Duel.ConfirmCards(1-tp,sg)
-		if tc:GetLocation()==LOCATION_HAND then Duel.ShuffleHand(tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.ConfirmCards(1-tp,tc)
+		Duel.ShuffleHand(tp)
 		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id,1))
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END)
 		e1:SetValue(-2)
 		tc:RegisterEffect(e1)

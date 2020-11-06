d0c579e5539a53e439cf149816384dcdd6ae9edf
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-06 16:05:39 +0100

    added new script
    Savage Claw Tiger
    Rear Misfire

---
+++ rush/c160003019.lua
@@ -0,0 +1,38 @@
+--サベージ・クロー・タイガー
+--Savage Claw Tiger
+local s,id=GetID()
+function s.initial_effect(c)
+	--ATK increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,1,nil,TYPE_MONSTER) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)>0 then
+		--effect
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			local atk=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsLevel,7),tp,LOCATION_MZONE,0,nil)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+			e1:SetValue(atk*400)
+			c:RegisterEffect(e1)
+		end
+	end
+end
---
+++ rush/c160003060.lua
@@ -0,0 +1,45 @@
+--背誤射撃
+--Rear Misfire
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsPreviousLocation(LOCATION_SZONE) and c:IsPreviousControler(tp)
+		and (c:GetReason()&(REASON_EFFECT+REASON_DESTROY))==(REASON_EFFECT+REASON_DESTROY)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and eg:IsExists(s.filter,1,nil,tp)
+end
+function s.desfilter(c)
+	return c:IsType(TYPE_EFFECT) and c:IsLevelBelow(6)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and s.desfilter(chkc) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_ONFIELD,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_ONFIELD,e:GetHandler())
+	if #g>0 then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	end
+	Duel.SetChainLimit(s.chlimit)
+end
+function s.chlimit(e,ep,tp)
+	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--effect
+	local tc=Duel.SelectMatchingCard(tp,s.desfilter,tp,0,LOCATION_ONFIELD,1,1,nil):GetFirst()
+	if tc then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end

4eadc27e706a002eed8198d6e9e63feac4f49a7d
Edoardo Lolletti <edoardo762@gmail.com>
2020-11-10 11:56:23 +0100

    Fix Kesshin the Decided Death Demon General

--- rush/c160003026.lua
+++ rush/c160003026.lua
@@ -19,6 +19,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(p,LOCATION_HAND,0)
 	Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)
 	if #g>0 then
+		local c=e:GetHandler()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)

4ea25c87bbb2fdf8d54506391eab4277b161eef8
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-12 21:45:58 +0100

    Added new Script (Rush)
    Long Shield Gardna
    Sea Dragon King Granganoth
    Great Turtle of Greed
    Triple 3

---
+++ rush/c160003058.lua
@@ -0,0 +1,30 @@
+-- 強欲な大亀 Gouyoku na Ookame (Great Turtle of Greed)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp,chk)
+	return ep==1-tp
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	 if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1)
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	Duel.SetChainLimit(s.chlimit)
+end
+function s.chlimit(e,ep,tp)
+	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end

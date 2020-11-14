1b81b654b85620adbe2879ba57f2d5b8b619ee7e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2020-11-14 12:01:46 -0800

    Update "Thunder Unicorn (Anime)"
    ATK change was resetting during EP when card proxy was not indicating such a reset existed.

--- unofficial/c511002706.lua
+++ unofficial/c511002706.lua
@@ -67,7 +67,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(atk)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
 end

b2755ec02ff111fd804215cd50ae56a88fd6b72f
edo9300 <edoardo762@gmail.com>
2020-11-15 16:44:12 +0100

    Fix Bascule the Moving Fortress
    Was summoning all the valid targets rather than the selected monster

--- rush/c160402005.lua
+++ rush/c160402005.lua
@@ -40,7 +40,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
 			Duel.HintSelection(Group.FromCards(sg))
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
 end

dfa98ba083ea784c4c4d65c898718b7634c64c98
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-16 14:28:08 +0800

    Update c511027455.lua

--- unofficial/c511027455.lua
+++ unofficial/c511027455.lua
@@ -1,13 +1,15 @@
---Edge Imp Frightfuloid (anime)
+--エッジインプ・ＤＴモドキ (Anime)
+--Edge Imp Frightfuloid (Anime)
 --Scripted By Sever666
 local s,id=GetID()
 function s.initial_effect(c)
 	--code
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_ADD_SETCODE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
-	e3:SetValue(0xad)
-	c:RegisterEffect(e3)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_ADD_SETCODE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e1:SetValue(0xad)
+	c:RegisterEffect(e1)
 end
+s.listed_series={0xad}

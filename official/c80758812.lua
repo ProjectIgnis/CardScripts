c691abec50b16b76dc8a22f33ed94bef755ebaaf
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-11-17 16:52:20 -0500

    Updated "Gemini Ablation"
    Wasn't letting the player special summon a FIRE warrior monster from hand. Cost wasn't handling if the only FIRE warrior monster in hand, the player shouldn't be able to select it.

--- official/c80758812.lua
+++ official/c80758812.lua
@@ -1,6 +1,8 @@
 --デュアル・アブレーション
 --Gemini Ablation
---original script by Logical Nonsense, rescripted by Naim
+--Logical Nonsense
+
+--Substitute ID
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -9,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	c:RegisterEffect(e1)
-	--Special Summon
+	--Special summon 1 gemini monster from deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -17,34 +19,38 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.spcond)
-	e2:SetCost(s.spcost)
+	e2:SetCondition(function()return Duel.IsMainPhase()end)
+	e2:SetCost(s.discost)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	c:RegisterEffect(e2)
+	--Special summon 1 FIRE warrior monster from hand or deck
 	local e3=e2:Clone()
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY+CATEGORY_RELEASE)
+	e3:SetCost(s.spcost)
 	e3:SetTarget(s.sptg2)
 	e3:SetOperation(s.spop2)
 	c:RegisterEffect(e3)
 end
-function s.spcond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2
-end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	--Discard 1 card as cost
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_DISCARD+REASON_COST,nil)
 end
+	--Check for a gemini monster
 function s.spfilter(c,e,tp)
 	return c:IsType(TYPE_GEMINI) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
+	--Activation legality
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+	--Special summon 1 gemini monster from deck, it gains its effects
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
@@ -58,19 +64,47 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummonComplete()
 	end
 end
+	--"Cost"
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	return true
+end
+function s.costfilter(c,e,tp)
+	return c:IsDiscardable() and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND+LOCATION_DECK,0,1,c,e,tp)
+end
+	--Check for a FIRE warrior monster
 function s.spfilter2(c,e,tp)
 	return c:IsRace(RACE_WARRIOR) and c:IsType(TYPE_MONSTER) and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
+	--Activation legality
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	if chk==0 then 
+		if e:GetLabel()~=0 then
+			e:SetLabel(0)
+			--If you only have 1 FIRE warrior monster and it's in your hand, don't discard it
+			return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+				and Duel.IsExistingMatchingCard(s.releasefil,tp,LOCATION_MZONE,0,1,nil)
+		else
+			return Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp)
+				and Duel.IsExistingMatchingCard(s.releasefil,tp,LOCATION_MZONE,0,1,nil)
+		end
+	end
+	if e:GetLabel()~=0 then
+		e:SetLabel(0)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+		local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+		Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
+	--Check for a gemini monster to tribute
 function s.releasefil(c)
 	return c:IsFaceup() and c:IsType(TYPE_GEMINI) and c:IsReleasableByEffect()
 end
+	--Tribute 1 gemini monster, special summon a FIRE warrior, destroy a card if the tributed was in gemini state
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if not Duel.IsExistingMatchingCard(s.releasefil,tp,LOCATION_MZONE,0,1,nil) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local g=Duel.SelectMatchingCard(tp,s.releasefil,tp,LOCATION_MZONE,0,1,1,nil)
 	if #g>0 and Duel.Release(g,REASON_EFFECT)>0 then

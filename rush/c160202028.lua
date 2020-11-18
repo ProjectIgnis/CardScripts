de437144223a36c74311be55fc122587623a86c5
Edoardo Lolletti <edoardo762@gmail.com>
2020-11-18 22:26:46 +0100

    Fix Cat's Eyes (Rush)
    It wasn't actually checking the milled card and was milling the player

--- rush/c160202028.lua
+++ rush/c160202028.lua
@@ -13,14 +13,13 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_BEAST),e:GetHandlerPlayer(),LOCATION_MZONE,0,nil)==3 end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2),aux.Stringid(id,3))
-	local tc=Duel.GetOperatedGroup():GetFirst()
-	if Duel.DiscardDeck(tp,1,REASON_EFFECT)>0 then
-		if (op==0 and tc:IsType(TYPE_MONSTER)) or (op==1 and tc:IsType(TYPE_SPELL)) or (op==2 and tc:IsType(TYPE_TRAP)) then 
+	local op=Duel.SelectOption(1-tp,aux.Stringid(id,1),aux.Stringid(id,2),aux.Stringid(id,3))
+	if Duel.DiscardDeck(1-tp,1,REASON_EFFECT)>0 then
+		local tc=Duel.GetOperatedGroup():GetFirst()
+		if tc and tc:IsLocation(LOCATION_GRAVE) and
+			((op==0 and tc:IsType(TYPE_MONSTER)) or (op==1 and tc:IsType(TYPE_SPELL)) or (op==2 and tc:IsType(TYPE_TRAP))) then
 			local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-			local tc=g:GetFirst()
 			for tc in aux.Next(g) do
 				local e1=Effect.CreateEffect(e:GetHandler())
 				e1:SetType(EFFECT_TYPE_SINGLE)

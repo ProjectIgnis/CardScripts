23a6b28be858c53460c1aafa8bc6c65eaf8d65ae
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-18 18:16:59 +0800

    Fix "Junk Dealer"

--- unofficial/c511002646.lua
+++ unofficial/c511002646.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_HANDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)

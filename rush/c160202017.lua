bd69e6ab708633c5d79d4d8d0198160ad418627f
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-11-18 17:23:45 -0500

    Updated "Yoctron"
    Was not returning the 3 monsters in GY to deck as cost.

--- rush/c160202017.lua
+++ rush/c160202017.lua
@@ -1,12 +1,13 @@
 --ヨクトロン 
 --Yoctron
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon
+	--Send itself to GY, draw 3, then send 3 cards from hand to GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW+CATEGORY_TOGRAVE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
@@ -15,6 +16,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_names={160202014,160202015,160202016}
+
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
 end
@@ -29,11 +31,21 @@ if chk==0 then return Duel.IsPlayerCanDraw(tp,3) end
 	Duel.SetTargetParam(3)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
 end
+function s.ctfilter(c)
+	return c:IsCode(160202014,160202015,160202016) and c:IsAbleToDeckAsCost()
+end
+function s.ctcheck(sg,e,tp)
+	return sg:GetClassCount(Card.GetCode)==#sg
+end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	--Requirement
+	local g=Duel.GetMatchingGroup(s.ctfilter,tp,LOCATION_GRAVE,0,nil)
+	local sg=aux.SelectUnselectGroup(g,e,tp,3,3,s.ctcheck,1,tp,HINTMSG_TODECK)
+	Duel.SendtoDeck(sg,nil,SEQ_DECKBOTTOM,REASON_COST)
+	Duel.SortDeckbottom(tp,tp,3)
 	local c=e:GetHandler()
+	--Effect
 	if Duel.SendtoGrave(c,REASON_EFFECT)>0 then
-		--effect
 		if Duel.Draw(tp,3,REASON_EFFECT)>0 then
 			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
 			local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,3,3,nil)

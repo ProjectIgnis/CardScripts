29fc62aabdbb4825f0c260762d1aa4f5796f333d
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-11-21 17:34:20 -0500

    Updated "Damage Diet"
    Should halve battle damage as well for its first effect.

--- official/c95448692.lua
+++ official/c95448692.lua
@@ -1,14 +1,15 @@
 --ダメージ・ダイエット
 --Damage Diet
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Halve all damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--effect damage
+	--Halve effect damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
@@ -24,7 +25,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
-	e1:SetValue(s.val)
+	e1:SetValue(s.val1)
 	e1:SetReset(RESET_PHASE+PHASE_END,1)
 	Duel.RegisterEffect(e1,tp)
 end
@@ -35,12 +36,17 @@ function s.activate2(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
-	e1:SetValue(s.val)
+	e1:SetValue(s.val2)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
 end
-function s.val(e,re,dam,r,rp,rc)
+function s.val1(e,re,dam,r,rp,rc)
+	if r&(REASON_BATTLE+REASON_EFFECT)~=0 then
+		return dam/2
+	else return dam end
+end
+function s.val2(e,re,dam,r,rp,rc)
 	if (r&REASON_EFFECT)~=0 then
 		return dam/2
 	else return dam end

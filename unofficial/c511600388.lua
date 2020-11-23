6f3a9d180d235d7c7ae82fff1ddc005f772b79ce
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-23 12:41:16 +0800

    Re-added "Magician's Selection" script

---
+++ unofficial/c511600388.lua
@@ -0,0 +1,36 @@
+--マジシャンズセレクト
+--Magician's Selection
+--Scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.GetAttacker():IsControler(tp) and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_SPELLCASTER),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local tg=g:GetMinGroup(Card.GetAttack)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateAttack() then
+		local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+		local tg=g:GetMinGroup(Card.GetAttack)
+		if #tg>1 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local sg=tg:Select(tp,1,1,nil)
+			Duel.HintSelection(sg)
+			Duel.Destroy(sg,REASON_EFFECT)
+		else Duel.Destroy(tg,REASON_EFFECT) end
+	end
+end

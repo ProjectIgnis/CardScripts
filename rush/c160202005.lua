cfbf62179b911b1f3f722ca846d42056b78261c0
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-11-24 22:26:16 +0100

    Added new scripts
    - Kuribottriple
    - Picock Hightron

---
+++ rush/c160202005.lua
@@ -0,0 +1,39 @@
+--クリボットリプル
+--Kuribottriple
+local s,id=GetID()
+function s.initial_effect(c)
+	--Shuffle 3 spellcasters into the deck to draw 1
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160001017}
+function s.filter(c)
+	return c:IsCode(160001017) and c:IsAbleToDeckAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,3,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	--Requirement
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,3,3,nil)
+	if Duel.SendtoDeck(g,nil,3,REASON_COST)>0 then
+		Duel.ShuffleDeck(tp)
+		--Effect
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Draw(p,d,REASON_EFFECT)
+	end
+end

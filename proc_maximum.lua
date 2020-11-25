e05c485fbb9178feeb2b33353f27fdd3115c3e84
edo9300 <edoardo762@gmail.com>
2020-11-25 21:02:45 +0100

    Maximum: other minor cleanups
    Removed named arg declaration of maximum funciton, due to the variadic nature of the arguments it doesn't really make much sense
    Remove some redoundancy with lua tables

--- proc_maximum.lua
+++ proc_maximum.lua
@@ -11,9 +11,9 @@ if not Maximum then
 	Maximum = aux.MaximumProcedure
 end
 --Maximum Summon
-Maximum.AddProcedure = aux.FunctionWithNamedArgs(
-function(c,desc,...)
-	c:GetMetatable().MaximumSet={...}
+function Maximum.AddProcedure(c,desc,...)
+	local mats={...}
+	c:GetMetatable().MaximumSet=mats
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	if desc then
@@ -24,8 +24,8 @@ function(c,desc,...)
 	e1:SetCode(EFFECT_SPSUMMON_PROC_G)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(Maximum.Condition())
-	e1:SetOperation(Maximum.Operation())
+	e1:SetCondition(Maximum.Condition(mats))
+	e1:SetOperation(Maximum.Operation(mats))
 	e1:SetValue(SUMMON_TYPE_MAXIMUM)
 	c:RegisterEffect(e1)
 	--cannot be changed to def
@@ -61,14 +61,12 @@ function(c,desc,...)
 	e4:SetCondition(Maximum.battlecon)
     e4:SetOperation(Maximum.battleop)
     c:RegisterEffect(e4)
-	
-end,"handler","desc","filter1","filter2","filter3","filter4")
+end
 --that function check if you can maximum summon the monster and its other part(s)
-function Maximum.Condition()
+function Maximum.Condition(mats)
+	local ct=#mats
 	return  function(e,c,og)
 		if c==nil then return true end
-		local filters=c.MaximumSet
-		local ct=#filters
 		local tp=c:GetControler()
 		local g=nil
 		if og then
@@ -76,25 +74,22 @@ function Maximum.Condition()
 		else
 			g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
 		end
-		return aux.SelectUnselectGroup(g,e,tp,ct,ct,Maximum.spcheck(table.unpack(filters)),0)
+		return aux.SelectUnselectGroup(g,e,tp,ct,ct,Maximum.spcheck(mats),0)
 	end
 end
-function Maximum.spcheck(...)
-	local filters={...}
+function Maximum.spcheck(filters)
 	return function(sg,e,tp,mg)
-		local ct=#filters
-		for i=1,ct do
-			if not sg:IsExists(filters[i],1,nil) then return false end
+		for _,filter in ipairs(filters) do
+			if not sg:IsExists(filter,1,nil) then return false end
 		end
-		return #sg==ct
+		return true
 	end
 end
 --operation that do the maximum summon
-function Maximum.Operation()
-	return  function(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
+function Maximum.Operation(mats)
+	local ct=#mats
+	return function(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
 		if c==nil then return true end
-		local filters=c.MaximumSet
-		local ct=#filters
 		local tp=c:GetControler()
 		local g=nil
 		if og then
@@ -103,7 +98,7 @@ function Maximum.Operation()
 			g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
 		end
 		--select side monsters
-		local tg=aux.SelectUnselectGroup(g,e,tp,ct,ct,Maximum.spcheck(table.unpack(filters)),1,tp,HINTMSG_SPSUMMON,nil,nil,true)
+		local tg=aux.SelectUnselectGroup(g,e,tp,ct,ct,Maximum.spcheck(mats),1,tp,HINTMSG_SPSUMMON,nil,nil,true)
 		if #tg==0 then return end
 		--adding the "maximum mode" flag
 		--center

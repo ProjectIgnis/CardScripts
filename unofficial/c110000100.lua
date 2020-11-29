27729041c75d7e58e01aa4701d065c866ba9c4f1
Larry126 <28985622+larry126@users.noreply.github.com>
2020-11-29 16:07:44 +0800

    Updated unofficial card scripts
    - Fix Lunalight Cat Dancer (Anime) & Lunalight Panther Dancer (Anime)
    - Fix Orichalcos Deuteros and Orichalcos Tritos
    - Minor improvements to c421

--- unofficial/c110000100.lua
+++ unofficial/c110000100.lua
@@ -1,68 +1,66 @@
 --オレイカルコス・デウテロス
+--Orichalcos Deuteros
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(s.atcon)
+	e1:SetCondition(s.condition)
 	c:RegisterEffect(e1)
-	--
+	--Recover
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_RECOVER)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
 	c:RegisterEffect(e2)
-	--DESTROY
+	--Negate attack
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCondition(s.atkcon)
-	e3:SetCost(s.cost)
+	e3:SetCost(s.atkcost)
 	e3:SetTarget(s.atktg)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
-	--selfdes
+	--Gain effect
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e4:SetCode(EVENT_ADJUST)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetCondition(s.sdcon2)
-	e4:SetOperation(s.sdop)
+	e4:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+	e4:SetCountLimit(1)
+	e4:SetOperation(s.op)
 	c:RegisterEffect(e4)
 end
 s.listed_names={48179391}
-function s.sdcon2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(id)==0
+function s.condition(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetFieldCard(e:GetHandlerPlayer(),LOCATION_SZONE,5)
+	return tc and tc:IsFaceup() and tc:IsCode(48179391)
 end
-function s.sdop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():CopyEffect(48179391,RESET_EVENT+RESETS_STANDARD)
-	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
-end
-function s.atcon(e)
-	local tc=Duel.GetFieldCard(e:GetHandler():GetControler(),LOCATION_SZONE,5)	
-	return tc~=nil and tc:IsFaceup() and tc:IsCode(48179391)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)>0 end
 	Duel.SetTargetPlayer(tp)
 	local rec=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)*500
 	Duel.SetTargetParam(rec)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,rec)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local rec=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)*500
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
 	Duel.Recover(p,rec,REASON_EFFECT)
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroup(tp,nil,1,nil) end
 	local g=Duel.SelectReleaseGroup(tp,nil,1,1,nil)
 	Duel.Release(g,REASON_COST)
@@ -78,8 +76,12 @@ function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,1,0,0)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetAttacker()
 	if tc and tc:IsRelateToEffect(e) and tc:CanAttack() and Duel.NegateAttack() then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():CopyEffect(48179391,RESET_EVENT+RESETS_STANDARD)
 end

37e901efaba46a18fecdf6c3672503e7328380ec
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-12-11 15:39:31 -0500

    Updated "Echoing Whispers" Should only be able to select monster cards. Also added hint to show which cards were selected.

--- rush/c160202009.lua
+++ rush/c160202009.lua
@@ -1,8 +1,9 @@
--- 拡がるささやき
+--拡がるささやき
 --Echoing Whispers
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Shuffle up to 5 monsters from any GY to the deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -13,6 +14,9 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+function s.filter(c)
+	return c:IsAbleToDeck() and c:IsType(TYPE_MONSTER)
+end
 function s.cfilter(c,tp)
 	return c:IsSummonPlayer(1-tp) and c:IsPreviousLocation(LOCATION_HAND)
 end
@@ -20,11 +24,15 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and s.rmfilter(chkc) end
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
-	
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.rmfilter(chkc) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+	Duel.SetChainLimit(s.chlimit)
+end
+function s.chlimit(e,ep,tp)
+	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,5,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,5,nil)
+	Duel.HintSelection(g)
 	Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
 end

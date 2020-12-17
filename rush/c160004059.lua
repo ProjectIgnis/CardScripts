8d6bff4ac20b13a8ad6ef70d1efe89a7cb7830c1
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2020-12-16 19:38:09 -0800

    Fix for "Jewelry Trap Hole"
    Should not shuffle from GY into Deck and removed extraneous filter

--- rush/c160004059.lua
+++ rush/c160004059.lua
@@ -22,9 +22,6 @@ end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.filter1,1,nil,e,tp) and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_AQUA),tp,LOCATION_MZONE,0,3,nil)
 end
-function s.filter(c,e,tp)
-	return c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
 function s.desfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsLevelBelow(8) and not c:IsMaximumModeSide()
 end
@@ -38,18 +35,12 @@ function s.chlimit(e,ep,tp)
 end
 	--Destroy 1 of opponent's monsters
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	--Requirement
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,4,4,nil)
-	Duel.HintSelection(g)
-	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)>0 then
-		--Effect
-		local dg=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
-		local sg=dg:Select(tp,1,1,nil)
-		if #sg>0 then
-			Duel.HintSelection(sg)
-			sg=sg:AddMaximumCheck()
-			Duel.Destroy(sg,REASON_EFFECT)
-		end
+	--Effect
+	local dg=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
+	local sg=dg:Select(tp,1,1,nil)
+	if #sg>0 then
+		Duel.HintSelection(sg)
+		sg=sg:AddMaximumCheck()
+		Duel.Destroy(sg,REASON_EFFECT)
 	end
 end

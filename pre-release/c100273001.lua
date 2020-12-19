2c264dc114b2cf5add70b9b927943e0bc7738eb6
lrhel <lrhel@users.noreply.github.com>
2020-12-19 15:21:21 +0100

    Fix "Thunderforce Attack"
    https://www.db.yugioh-card.com/yugiohdb/faq_search.action?ope=5&fid=23160&request_locale=ja

--- pre-release/c100273001.lua
+++ pre-release/c100273001.lua
@@ -25,7 +25,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
 end
 function s.filter(c,p)
-	return c:IsControler(p) and c:IsLocation(LOCATION_GRAVE)
+	return c:IsControler(p) and c:IsLocation(LOCATION_GRAVE) and c:IsType(TYPE_MONSTER)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local sg=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)

260d8238a296fda31dfa2c112eb9dd415d9656d6
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-12-20 17:29:16 -0500

    Updated various cards * Fossil Dragon Skullgar - ignition effect should be once per turn * Drilling Mandrill - minor typos

--- official/c12015000.lua
+++ official/c12015000.lua
@@ -1,14 +1,16 @@
 --新生代化石竜 スカルガ
 --Fossil Dragon Skullgar
 --Scripted by AlphaKretin
+
 local s,id=GetID()
 function s.initial_effect(c)
-	--Fusion material
-	c:EnableReviveLimit()
+	--Fusion summon procedure
 	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_ROCK),s.ffilter)
-	--lizard check
+	--Must be properly summoned before reviving
+	c:EnableReviveLimit()
+	--Clock Lizard check
 	Auxiliary.addLizardCheck(c)
-	--Special summon condition
+	--Must first be special summoned with "Fossil Fusion"
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SINGLE_RANGE)
@@ -16,22 +18,24 @@ function s.initial_effect(c)
 	e0:SetRange(LOCATION_EXTRA)
 	e0:SetValue(aux.FossilLimit)
 	c:RegisterEffect(e0)
-	--Piercing damage
+	--Inflict piercing damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e1)
-	--Add "Fossil Fusion" to hand
+	--Add 1 "Fossil Fusion" from deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={CARD_FOSSIL_FUSION}
+
 function s.ffilter(c,fc,sumtype,tp)
 	return c:IsLevelBelow(4) and c:IsLocation(LOCATION_GRAVE) and c:IsControler(1-tp)
 end
--- rush/c160004031.lua
+++ rush/c160004031.lua
@@ -1,8 +1,9 @@
 --ドリリング・マンドリル
 --Drilling Mandrill
+
 local s,id=GetID()
 function s.initial_effect(c)
-	
+	--Change opponent's face-down monsters to face-up attack
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_POSITION+CATEGORY_ATKCHANGE)
@@ -18,22 +19,20 @@ function s.poscond(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsStatus(STATUS_SUMMON_TURN+STATUS_SPSUMMON_TURN)
 end
 function s.filter(c)
-    return c:IsFacedown() and c:IsDefensePos()
+	return c:IsFacedown() and c:IsDefensePos()
 end
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_MACHINE),tp,LOCATION_MZONE,0,nil)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc) end
-    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) and ct>0 end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEDOWNDEFENSE)
-    Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) and ct>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEDOWNDEFENSE)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_MACHINE),tp,LOCATION_MZONE,0,nil)
-    local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,ct,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,ct,nil)
 	Duel.HintSelection(g)
 	if #g>0 then
-		 -- Duel.ChangePosition(tc,0,0,0,POS_FACEUP_ATTACK,true)
-		 Duel.ChangePosition(g,0,0,POS_FACEUP_ATTACK,0)
+		Duel.ChangePosition(g,POS_FACEUP_ATTACK)
 		local tc=g:GetFirst()
 		for tc in aux.Next(g) do
 			local e1=Effect.CreateEffect(e:GetHandler())

5b79aa8a40e98871425a04b8d267e4d48226282c
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-12-21 15:20:53 -0500

    Updated "Sprigguns Booty"
    Minor typo.

--- pre-release/c101104054.lua
+++ pre-release/c101104054.lua
@@ -57,7 +57,7 @@ function s.caop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetDescription(3302)
 		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CANNOT_TRIGGER+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CANNOT_TRIGGER)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1,true)
 	end

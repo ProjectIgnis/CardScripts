4bf640b1babea53f9a3da99a44632b2cdb3c08d4
senpaizuri3 <senpaizuri3@gmail.com>
2020-12-29 07:23:47 -0500

    Small fix "Arsenal Summoner (GOAT)"
    now it can search a card

--- goat/c504700146.lua
+++ goat/c504700146.lua
@@ -20,7 +20,7 @@ function s.filter(c)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.GoatDeckSearch(s.filter),tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)

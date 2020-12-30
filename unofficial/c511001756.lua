dfbec6bb1c6de4d291699af9564a4f9a5681f05d
edo9300 <edoardo762@gmail.com>
2020-12-31 00:55:02 +0100

    Fix missing parameter in Big Eater

--- unofficial/c511001756.lua
+++ unofficial/c511001756.lua
@@ -15,7 +15,7 @@ end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local rg=Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,c:GetControler(),LOCATION_HAND,0,e:GetHandler())
-	return aux.SelectUnselectGroup(rg,e,tp,1,2,aux.ChkfMMZ(1),0)
+	return aux.SelectUnselectGroup(rg,e,c:GetControler(),1,2,aux.ChkfMMZ(1),0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
 	local rg=Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,e:GetHandler())

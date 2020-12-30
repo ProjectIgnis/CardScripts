7828e3dfccfe955ff2f3accbf8118b755344a2bc
senpaizuri3 <senpaizuri3@gmail.com>
2020-12-29 23:16:24 -0500

    Small fix "Infernoble Knight Oliver"
    Now the level modulation occurs.

--- official/c58346901.lua
+++ official/c58346901.lua
@@ -58,7 +58,6 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
 		e1:SetValue(1)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)

7206eaf0096b6b7c270fffa2e32a6edcd79adb69
edo9300 <edoardo762@gmail.com>
2021-01-06 16:53:22 +0100

    Fix Number 92: Heart-eartH Dragon and Amazzoness Swords Woman
    Weren't properly applying the reflect damage effect

--- official/c94004268.lua
+++ official/c94004268.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--reflect battle dam
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
--- official/c97403510.lua
+++ official/c97403510.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--battle
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)

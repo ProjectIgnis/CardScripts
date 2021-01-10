64502216a3c432b46d5da28c668c06094b0fbf91
edo9300 <edoardo762@gmail.com>
2021-01-11 00:30:37 +0100

    fix Geological Survey
    Wasn't counting spell/traps and didn't allow to keep teh cards on top of the deck

--- rush/c160004043.lua
+++ rush/c160004043.lua
@@ -12,15 +12,19 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	local ct=Duel.GetFieldGroupCountRush(tp,0,LOCATION_ONFIELD)
 	if chk==0 then return ct>0 and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=ct end
 	Duel.SetTargetPlayer(tp)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	local ct=Duel.GetFieldGroupCountRush(tp,0,LOCATION_ONFIELD)
 	Duel.ConfirmDecktop(p,ct)
 	local g=Duel.GetDecktopGroup(p,ct)
-	Duel.MoveToDeckBottom(g)
-	Duel.SortDeckbottom(p,p,#g)
+	if Duel.SelectOption(p,aux.Stringid(id,0),aux.Stringid(id,1))==1 then
+		Duel.MoveToDeckBottom(g)
+		Duel.SortDeckbottom(p,p,#g)
+	else
+		Duel.SortDecktop(p,p,#g)
+	end
 end

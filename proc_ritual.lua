5db47ba65c4d09ed6da93a9d56ce7e8affd9f6c8
edo9300 <edoardo762@gmail.com>
2021-01-11 15:36:41 +0100

    prud ritual: don't use select functions if overlaid cards are present

--- proc_ritual.lua
+++ proc_ritual.lua
@@ -198,7 +198,7 @@ function(filter,_type,lv,extrafil,extraop,matfilter,stage2,location,forcedselect
 						if tc.mat_filter then
 							mg=mg:Filter(tc.mat_filter,tc,tp)
 						end
-						if ft>0 and not forcedselection then
+						if not mg:IsExists(Card.IsLocation,1,nil,LOCATION_OVERLAY) and ft>0 and not forcedselection then
 							Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 							if _type==RITPROC_EQUAL then
 								mat=mg:SelectWithSumEqual(tp,requirementfunc or Card.GetRitualLevel,lv,1,#mg,tc)

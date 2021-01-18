ed302c718c5b1cb5d71f43d2e8cb0ca6defed037
Edoardo Lolletti <edoardo762@gmail.com>
2021-01-18 17:41:19 +0100

    Fix Yamoimori
    Properly check for situations where not all the targets for the effect are satisfied (for example it was possible activate its effect while controlling a link reptile with the opponent only controlling a 0 atk monster)

--- official/c51474037.lua
+++ official/c51474037.lua
@@ -19,49 +19,62 @@ function s.initial_effect(c)
 end
 s.listed_names={id}
 --Banish from GY + choice effect
-function s.mfilter(c)
-	return c:IsRace(RACE_REPTILE) and c:IsFaceup() and (c:IsCanChangePosition() or c:IsDestructable())
+function s.mfilter(c,e,tp)
+	return c:IsCanBeEffectTarget(e) and c:IsMonster() and (c:IsControler(1-tp) or c:IsRace(RACE_REPTILE))
 end
-function s.mfilter2(c)
-	return c:IsMonster() and c:IsFaceup() and (c:IsCanChangePosition() or c:GetAttack()>0)
+function s.mfilter2(c,chk1,chk2,g2)
+	return (chk1 and c:IsCanChangePosition() and g2:IsExists(Card.IsCanChangePosition,1,nil)) or (chk2 and c:IsDestructable() and g2:IsExists(Card.IsAttackAbove,1,nil,1))
 end
-function s.btg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(s.mfilter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingTarget(s.mfilter2,tp,0,LOCATION_MZONE,1,nil) end
+function s.mfilter3(c,chk1,chk2)
+	return (chk1 and c:IsCanChangePosition()) or (chk2 and c:IsAttackAbove(1))
+end
+function s.btg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local g1,g2=Duel.GetMatchingGroup(aux.FilterFaceupFunction(s.mfilter,e,tp),tp,LOCATION_MZONE,LOCATION_MZONE,nil):Split(Card.IsControler,nil,tp)
+	if chk==0 and #g1+#g2==0 then return false end
+	local check1=g1:IsExists(Card.IsCanChangePosition,1,nil) and g2:IsExists(Card.IsCanChangePosition,1,nil)
+	local check2=g1:IsExists(Card.IsDestructable,1,nil) and g2:IsExists(Card.IsAttackAbove,1,nil,1)
+	if chk==0 then return check1 or check2 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local tc1=Duel.SelectTarget(tp,s.mfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	local tc1=g1:FilterSelect(tp,s.mfilter2,1,1,nil,check1,check2,g2):GetFirst()
+	Duel.SetTargetCard(tc1)
+	check1=check1 and tc1:IsCanChangePosition()
+	check2=check2 and tc1:IsDestructable()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local tc2=Duel.SelectTarget(tp,s.mfilter2,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
-	local pos,des,choice=tc1:IsCanChangePosition() and tc2:IsCanChangePosition(),tc1:IsDestructable() and tc2:GetAttack()>0,-1
-	if pos and des then 
-		choice=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-		elseif pos then choice=Duel.SelectOption(tp,aux.Stringid(id,1))
-			elseif des then choice=Duel.SelectOption(tp,aux.Stringid(id,2))+1
-			else return
+	local tc2=g2:FilterSelect(tp,s.mfilter3,1,1,nil,check1,check2):GetFirst()
+	Duel.SetTargetCard(tc2)
+	check1=check1 and tc2:IsCanChangePosition()
+	check2=check2 and tc2:GetAttack()>0
+	local t={}
+	if not check1 and not check2 then return end
+	if check1 then table.insert(t,aux.Stringid(id,1)) end
+	if check2 then table.insert(t,aux.Stringid(id,2)) end
+	local choice=Duel.SelectOption(tp,table.unpack(t))+(check1 and 0 or 1)
+	if choice==0 then
+		Duel.SetOperationInfo(0,CATEGORY_POSITION,Group.FromCards(tc1,tc2),2,tp,LOCATION_MZONE)
+	elseif choice==1 then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc1,1,tp,LOCATION_MZONE)
+		Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,tc2,1,tp,LOCATION_MZONE)
 	end
-	if choice==0 then Duel.SetOperationInfo(0,CATEGORY_POSITION,Group.FromCards(tc1,tc2),2,tp,LOCATION_MZONE)
-		elseif choice==1 then Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc1,1,tp,LOCATION_MZONE)
-								Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,tc2,1,tp,LOCATION_MZONE)
-			else return
-	end
-	local a={}
-	a[1],a[2],a[3]=tc1,tc2,choice
-	e:SetLabelObject(a)
+	e:SetLabel(choice)
 end
 function s.bop(e,tp,eg,ep,ev,re,r,rp)
-	local tc1,tc2,choice=e:GetLabelObject()[1],e:GetLabelObject()[2],e:GetLabelObject()[3]
-	if not tc1 or not tc2 or not tc1:IsRelateToEffect(e) or not tc2:IsRelateToEffect(e) or choice==-1 then return end
-	if (choice==0 and (not tc1:IsCanChangePosition() or not tc2:IsCanChangePosition())) or (choice==1 and (not tc1:IsDestructable() or tc2:GetAttack()==0)) then return end
+	local g=Duel.GetTargetCards(e)
+	if #g~=2 then return end
+	local choice=e:GetLabel()
 	if choice==0 then
-		Duel.ChangePosition(Group.FromCards(tc1,tc2),POS_FACEDOWN_DEFENSE)
-	end
-	if choice==1 then
-		if Duel.Destroy(tc1,REASON_EFFECT)>0 then
+		if g:FilterCount(Card.IsCanChangePosition,nil)~=2 then return end
+		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+	elseif choice==1 then
+		local g1,g2=g:Split(Card.IsControler,nil,tp)
+		if not g1:GetFirst():IsDestructable() or not g2:GetFirst():IsAttackAbove(1) then return end
+		if Duel.Destroy(g1,REASON_EFFECT)>0 then
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_SET_ATTACK)
 			e1:SetValue(0)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc2:RegisterEffect(e1)
+			g2:GetFirst():RegisterEffect(e1)
 		end
 	end
 end

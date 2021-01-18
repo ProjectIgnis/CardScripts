f626f4f41edeef4fd38ac9e22ba63660f490ee49
senpaizuri3 <senpaizuri3@gmail.com>
2021-01-18 10:02:48 -0500

    Small fixes "A-Team: Trap Disposal Unit"
    OCG version: It can be used during the Damage Step
    Goat version: Its ruling is now more accurately defined regarding damage step. Also, it can be destroyed by Double Snare using Sorcerer of Dark Magic as precedent.

--- goat/c504700019.lua
+++ goat/c504700019.lua
@@ -1,26 +1,27 @@
 --トラップ処理班 Aチーム
 --A-Team: Trap Disposal Unit (GOAT)
---Usable in the damage step
+--Usable in the damage step only if it doesn't relate to battle
 local s,id=GetID()
 function s.initial_effect(c)
 	--Negate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	aux.DoubleSnareValidity(c,LOCATION_MZONE)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return Duel.GetAttacker()~=c and Duel.GetAttackTarget()~=c and
-		not c:IsStatus(STATUS_BATTLE_DESTROYED) and rp~=tp
+	return not (Duel.GetCurrentPhase()==PHASE_DAMAGE and (Duel.GetAttacker()==c or Duel.GetAttackTarget()==c))
+		and not c:IsStatus(STATUS_BATTLE_DESTROYED) and rp~=tp
 		and re:IsActiveType(TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)

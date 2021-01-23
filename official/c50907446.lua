d7c2decc5e6eea15d10838e1b67a2f1277701e2c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-01-23 04:02:46 +0200

    "El Shaddoll Apkallone" update
    Trap Monster negation handling.

--- official/c50907446.lua
+++ official/c50907446.lua
@@ -72,6 +72,14 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp,chk)
 		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
+		if tc:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e3)
+		end
 	end
 end
 function s.thfilter(c)

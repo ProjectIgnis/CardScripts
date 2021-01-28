844aff9cd374c6396476889a66b38adef3f04d36
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-01-28 22:51:15 +0100

    Basoon Fix
    forgot to remove the face-up check in the condition

--- rush/c160004029.lua
+++ rush/c160004029.lua
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsPosition,POS_DEFENSE),tp,0,LOCATION_MZONE,1,nil)
+	return Duel.IsExistingMatchingCard(Card.IsPosition,POS_DEFENSE,tp,0,LOCATION_MZONE,1,nil)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end

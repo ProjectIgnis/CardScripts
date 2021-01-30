76ded8e2b5221ead632a43db15450e5baba76e14
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-01-30 22:46:43 +0200

    "Roxrose Dragon" fix
    Its second effect was activating in incorrect scenarios.

--- official/c93708824.lua
+++ official/c93708824.lua
@@ -17,24 +17,19 @@ function s.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	--To hand
+	--Return to hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DAMAGE_STEP)
 	e3:SetCode(EVENT_DESTROYED)
 	e3:SetRange(LOCATION_GRAVE)
 	e3:SetCountLimit(1,id+1)
-	e3:SetCondition(s.con)
-	e3:SetTarget(s.thtg2)
-	e3:SetOperation(s.thop2)
+	e3:SetCondition(s.rthcon)
+	e3:SetTarget(s.rthtg)
+	e3:SetOperation(s.rthop)
 	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DAMAGE_STEP)
-	e4:SetCode(EVENT_BATTLE_DESTROYED)
-	e4:SetCondition(s.con2)
-	c:RegisterEffect(e4)
 end
 s.listed_names={id,CARD_BLACK_ROSE_DRAGON}
 s.listed_series={0x1123}
@@ -54,24 +49,23 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
---To hand
-function s.filter(c,tp)
-	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
-		and ((c:GetPreviousSetCard()&0x1123)~=0 or ((c:GetPreviousRaceOnField()&RACE_PLANT)~=0 and (c:GetPreviousTypeOnField()()&TYPE_SYNCHRO)~=0))
+--Return to hand
+function s.rthfilter(c,tp)
+	return (c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp))
+		and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
+		and (c:IsPreviousSetCard(0x1123) or (c:GetPreviousRaceOnField()&RACE_PLANT~=0 and c:GetPreviousTypeOnField()&TYPE_SYNCHRO~=0))
 end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return rp==1-tp and eg:IsExists(aux.NecroValleyFilter(s.filter),1,e:GetHandler(),tp)
+function s.rthcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return not eg:IsContains(c) and eg:IsExists(s.rthfilter,1,c,tp)
 end
-function s.con2(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(aux.NecroValleyFilter(s.filter),1,e:GetHandler(),tp)
-end
-function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rthtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToHand() end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,tp,LOCATION_GRAVE)
 end
-function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+function s.rthop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and not Duel.IsPlayerAffectedByEffect(tp,CARD_NECROVALLEY) then
+	if c:IsRelateToEffect(e) then
 		Duel.SendtoHand(c,nil,REASON_EFFECT)
 	end
 end

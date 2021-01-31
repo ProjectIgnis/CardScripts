d54de89c11a29ad40a5a7d5727c044bbc379a799
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-01-31 20:02:09 +0100

    Darkness Approach fix
    You should not be able to use the effect on a Maximum Mode monster

--- rush/c160001043.lua
+++ rush/c160001043.lua
@@ -17,7 +17,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,e:GetHandler()) end
 end
 function s.filter(c)
-	return c:IsFaceup() and c:IsCanTurnSet()
+	return c:IsFaceup() and c:IsCanTurnSet() and not c:IsMaximumMode()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end

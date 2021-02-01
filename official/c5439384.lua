472cb1a08b239629996e14e21a84a0d449041f2e
Mercury233 <me@mercury233.me>
2021-02-01 10:20:34 +0800

    typo Explosive Birth of the Flame Champion (#548)

--- official/c5439384.lua
+++ official/c5439384.lua
@@ -45,6 +45,5 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,lv)
 		if #sg>0 then Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP) end
-		end
 	end
 end

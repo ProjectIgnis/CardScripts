b0ff47817676ad2e048c4fb35bbfc0a8680963b2
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-02-05 22:17:57 +0100

    Removed unnecessary double reveals from "Eldlich" cards (#550)

--- official/c94224458.lua
+++ official/c94224458.lua
@@ -69,6 +69,5 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SSet(tp,g)
-		Duel.ConfirmCards(1-tp,g)
 	end
 end

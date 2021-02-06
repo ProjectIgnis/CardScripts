27db441719a2035c2aa2ccb1082995c700a041b2
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-02-06 15:04:01 -0800

    Update "Karma Cut"
    Should be able to be interacted with by Ghost Belle.

--- official/c71587526.lua
+++ official/c71587526.lua
@@ -1,4 +1,5 @@
 --因果切断
+--Karma Cut
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -12,6 +13,8 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end

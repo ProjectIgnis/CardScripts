6a5c09724ff7e8c8b6775a83fcd25c733498aec9
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-07 18:41:51 +0200

    "Paleozoic Dinomischus" fix
    Should banish the target even if it's face-down during resolution.

--- official/c38761908.lua
+++ official/c38761908.lua
@@ -1,6 +1,5 @@
 --バージェストマ・ディノミスクス
 --Paleozoic Dinomischus
-
 local s,id=GetID()
 function s.initial_effect(c)
 	--Banish 1 card on the field
@@ -11,7 +10,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	c:RegisterEffect(e1)
 	--Special summon itself from GY as a monster
 	local e2=Effect.CreateEffect(c)
@@ -42,8 +41,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if Duel.DiscardHand(tp,s.filter1,1,1,REASON_EFFECT+REASON_DISCARD,nil)~=0
-		and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+	if Duel.DiscardHand(tp,s.filter1,1,1,REASON_EFFECT+REASON_DISCARD,nil)>0 and tc:IsRelateToEffect(e) then
 		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 	end
 end

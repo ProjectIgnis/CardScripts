6a9716d5449e4e9fbfdd460304a942e6071ce0b0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-11 01:23:27 +0200

    "Amunesia, Queen of the Abhyss" fix
    Was not checking if you have an available MMZ for its last effect.

--- pre-release/c100416008.lua
+++ pre-release/c100416008.lua
@@ -83,7 +83,8 @@ function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_REPTILE) and (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
 end
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)

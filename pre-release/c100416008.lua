b51a04c9550881eb6829e00fa939db2854212083
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-11 18:46:51 +0200

    "Amunesia, Queen of the Abhyss" fix
    Incorrect filter name.

--- pre-release/c100416008.lua
+++ pre-release/c100416008.lua
@@ -84,7 +84,7 @@ function s.spfilter(c,e,tp)
 end
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.spop2(e,tp,eg,ep,ev,re,r,rp)

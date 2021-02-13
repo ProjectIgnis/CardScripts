ba6d3ff354566e25b9916affb9ddff5daf6a84ea
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-13 22:54:03 +0200

    "Stardust Illuminate" fix
    Was able to SS if your Synchro that lists "Stardust Dragon" was face-down.

--- pre-release/c101105106.lua
+++ pre-release/c101105106.lua
@@ -29,7 +29,7 @@ s.listed_names={id,CARD_STARDUST_DRAGON}
 s.listed_series={0xa3}
 --To Grave
 function s.ssfilter(c)
-	return c:IsFaceup() and c:IsCode(CARD_STARDUST_DRAGON) or (aux.IsCodeListed(c,CARD_STARDUST_DRAGON) and c:IsType(TYPE_SYNCHRO))
+	return c:IsFaceup() and (c:IsCode(CARD_STARDUST_DRAGON) or (aux.IsCodeListed(c,CARD_STARDUST_DRAGON) and c:IsType(TYPE_SYNCHRO)))
 end
 function s.tgfilter(c,e,tp,ss,mz)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xa3) and (c:IsAbleToGrave() or (ss and mz and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))

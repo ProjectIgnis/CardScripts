fb424423bd3db7f5cdfcce4d46a0cc177ff5fe00
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-02-13 21:43:41 +0100

    Update c160004051.lua

--- rush/c160004051.lua
+++ rush/c160004051.lua
@@ -16,7 +16,7 @@ function s.filter(c,e,tp)
 	return c:IsSummonPlayer(1-tp) and c:IsLocation(LOCATION_MZONE)
 end
 function s.cfilter(c,tp)
-	return c:IsAbleToDeck() and Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_GRAVE,0,2,c:GetRace())
+	return c:IsAbleToDeck() and Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_GRAVE,0,2,c,c:GetRace())
 end
 function s.cfilter2(c,race)
 	return c:IsAbleToDeck() and c:IsRace(race)

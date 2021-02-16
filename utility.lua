95a0c2cbaa4b2490c9d045bbca6f95ae93d020fc
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-16 19:21:06 +0200

    Added new Pendulum Scale-related functions

--- utility.lua
+++ utility.lua
@@ -1534,6 +1534,26 @@ function Auxiliary.ChangeBattleDamage(player,value)
 		end
 end
 
+function Card.GetScale(c)
+	if not c:IsType(TYPE_PENDULUM) then return 0 end
+	local sc=0
+	if c:IsLocation(LOCATION_PZONE) then
+		local seq=c:GetSequence()
+		if seq==0 then sc=c:GetLeftScale() else sc=c:GetRightScale() end
+	else
+		sc=c:GetLeftScale()
+	end
+	return sc
+end
+function Card.IsOddScale(c)
+	if not c:IsType(TYPE_PENDULUM) then return false end
+	return c:GetScale() % 2 ~= 0
+end
+function Card.IsEvenScale(c)
+	if not c:IsType(TYPE_PENDULUM) then return false end
+	return c:GetScale() % 2 == 0
+end
+
 Duel.LoadScript("cards_specific_functions.lua")
 Duel.LoadScript("proc_fusion.lua")
 Duel.LoadScript("proc_fusion_spell.lua")

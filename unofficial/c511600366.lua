73f2695e9266fc14d11404aa4231d12a520f3439
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-17 19:23:52 +0200

    "Starving Venemy Lethal Dose Dragon (Manga)" fix
    Should not be able to use the last effect if the opponent controls no monsters.

--- unofficial/c511600366.lua
+++ unofficial/c511600366.lua
@@ -75,7 +75,8 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and aux.disfilter1(chkc) end
-	if chk==0 then return Duel.GetTargetCount(aux.disfilter1,tp,0,LOCATION_MZONE,nil)==Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE) end
+	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	if chk==0 then return Dct>0 and Duel.GetTargetCount(aux.disfilter1,tp,0,LOCATION_MZONE,nil)==ct end
 	local g=Duel.GetMatchingGroup(aux.disfilter1,tp,0,LOCATION_MZONE,nil)
 	Duel.SetTargetCard(g)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)

1557d8c5b2c2d50731a104b5a3619bc59b261356
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-20 17:37:35 +0200

    "Endymion, the Master Magician" fix
    Player param not passed to a filter properly.

--- official/c40732515.lua
+++ official/c40732515.lua
@@ -46,7 +46,7 @@ end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD,0,nil)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD,0,nil,tp)
 	local ct=0
 	for i in aux.Next(g) do
 		ct=ct+i:GetCounter(COUNTER_SPELL)
@@ -54,7 +54,7 @@ function s.spcon(e,c)
 	return ct>=6 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD,0,nil)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD,0,nil,tp)
 	local ct=0
 	while ct<6 do
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)

0854aa8a42d2ce6303e009449bc9794d03bd4ea8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-02-20 23:00:03 +0200

    "Armed Dragon Blitz" fix
    The restriction about Special Summoning Dragons should check both before and after activating the card.

--- official/c61606250.lua
+++ official/c61606250.lua
@@ -8,21 +8,42 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,s.counterfilter)
 end
-function s.opfilter(c,code,ft,e,tp)
-	return c:IsSetCard(0x111) and c:IsCode(code)
-		and (c:IsAbleToHand() or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)))
+function s.counterfilter(c)
+	return c:IsRace(RACE_DRAGON)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+	--Cannot Special Summon monsters this turn, except Dragon monsters
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.sumlimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,3),nil)
+end
+function s.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return not c:IsRace(RACE_DRAGON)
 end
 function s.filter(c,ft,e,tp)
 	return c:IsFaceup() and c:IsSetCard(0x111)
 		and Duel.IsExistingMatchingCard(s.opfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,c:GetCode(),ft,e,tp)
+end
+function s.opfilter(c,code,ft,e,tp)
+	return c:IsSetCard(0x111) and c:IsCode(code)
+		and (c:IsAbleToHand() or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
@@ -58,18 +79,5 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 								Duel.SpecialSummonComplete()
 							end
 						end,aux.Stringid(id,1))
-		end
-	--Cannot special summon monsters this turn, except dragon monsters
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
-	e2:SetTargetRange(1,0)
-	e2:SetTarget(s.splimit)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,3),nil)
-end
-function s.splimit(e,c)
-	return not c:IsRace(RACE_DRAGON)
+	end
 end

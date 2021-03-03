8df2cd1419091287b0042c66cda1b7d3a2f06a65
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-03 21:24:06 +0200

    "Majestic Star/Red" update
    Updated the check for whether the card can be returned to the Extra Deck or not.

--- official/c7841112.lua
+++ official/c7841112.lua
@@ -134,7 +134,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:GetOriginalType()&0x802040~=0 and Duel.SendtoDeck(c,nil,0,REASON_EFFECT)~=0
+	if c:IsRelateToEffect(e) and c:IsAbleToExtra() and Duel.SendtoDeck(c,nil,0,REASON_EFFECT)~=0
 		and c:IsLocation(LOCATION_EXTRA) and tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end

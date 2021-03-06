b18a20ff083c551d17bc08ddaa4e395ec0331e1b
mercury233 <me@mercury233.me>
2021-03-06 21:57:17 +0800

    Minor fix the 2 Dreg of the Abhyss
    Missing hint

--- pre-release/c100416004.lua
+++ pre-release/c100416004.lua
@@ -67,6 +67,7 @@ function s.drop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
 	Duel.SendtoGrave(g,REASON_COST)
 end

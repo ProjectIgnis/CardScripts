e36861f2a7bc4ec8b69123e2e230ee7fcc0445ad
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-07 22:24:18 +0200

    "Hydradrive Booster" fix
    Undeclared param was causing a script error. Also general script polish.

--- unofficial/c511009708.lua
+++ unofficial/c511009708.lua
@@ -1,7 +1,8 @@
---High Drive Booster
+--ハイドライブ・ブースター
+--Hydradrive Booster
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Special Summon procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
@@ -10,10 +11,7 @@ function s.initial_effect(c)
 	e1:SetCondition(s.hspcon)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c)
-	return c:GetSequence()<5
-end
 function s.hspcon(e,c)
 	if c==nil then return true end
-	return not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	return not Duel.IsExistingMatchingCard(Card.IsInMainMZone,0,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end

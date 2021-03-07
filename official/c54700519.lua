289cea1d85e9bef22a83cc3da08a6e4500337d19
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-07 23:08:04 +0200

    Update and rename pre-release/c100416031.lua to official/c54700519.lua

--- official/c54700519.lua
+++ official/c54700519.lua
@@ -12,17 +12,17 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCountLimit(1,id+100)
+	e2:SetCountLimit(1,id+1)
 	e2:SetCondition(s.poscon)
 	e2:SetTarget(s.postg)
 	e2:SetOperation(s.posop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={id}
-s.listed_series={0x25b}
+s.listed_series={0x165}
 --Change Position
 function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x25b)
+	return c:IsFaceup() and c:IsSetCard(0x165)
 end
 function s.poscon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())

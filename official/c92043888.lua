bb20a2db8198c6b535ea92dc1f72204b6ed7632f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-07 23:03:50 +0200

    Update and rename pre-release/c100416026.lua to official/c92043888.lua

--- official/c92043888.lua
+++ official/c92043888.lua
@@ -14,10 +14,10 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x261}
+s.listed_series={0x164}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return rp==1-tp and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE))
-		and Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_PZONE,0,1,nil,0x261)
+		and Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_PZONE,0,1,nil,0x164)
 end
 function s.disfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsAbleToDeck()

c4ecc46a7dd73dbe7b3c98454ba48965bc502a4f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-07 22:59:31 +0200

    Update and rename pre-release/c100416022.lua to official/c27870337.lua

--- official/c27870337.lua
+++ official/c27870337.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x261}
+s.listed_series={0x164}
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1 = s.dtoptg(e,tp,eg,ep,ev,re,r,rp,0)
 	local b2 = s.htoetg(e,tp,eg,ep,ev,re,r,rp,0)
@@ -51,7 +51,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function s.filter(c)
-	return c:IsSetCard(0x261) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden() 
+	return c:IsSetCard(0x164) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden() 
 end
 function s.dtoptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return (Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1))

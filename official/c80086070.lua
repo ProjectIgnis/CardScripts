c78130688c0f0cab68d430ef02b917371beae669
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-07 23:11:44 +0200

    Update and rename pre-release/c100416035.lua to official/c80086070.lua

--- official/c80086070.lua
+++ official/c80086070.lua
@@ -45,7 +45,7 @@ function s.initial_effect(c)
 	e4:SetOperation(s.disop)
 	c:RegisterEffect(e4)
 end
-s.listed_series={0x25b}
+s.listed_series={0x165}
 function s.sprfilter(c)
 	return c:IsFaceup() and c:IsAbleToGraveAsCost() and c:HasLevel()
 end
@@ -105,7 +105,7 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 end
 --negate
 function s.disfilter(c,tp)
-	return c:IsFaceup() and c:IsLocation(LOCATION_ONFIELD) and c:IsControler(tp) and c:IsSetCard(0x25b)
+	return c:IsFaceup() and c:IsLocation(LOCATION_ONFIELD) and c:IsControler(tp) and c:IsSetCard(0x165)
 end
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end

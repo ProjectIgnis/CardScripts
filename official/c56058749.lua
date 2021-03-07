d9f4b092455c27e499f7c36f37c6ec824b88cfc1
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-07 23:03:16 +0200

    Update and rename pre-release/c100416025.lua to official/c56058749.lua

--- official/c56058749.lua
+++ official/c56058749.lua
@@ -11,9 +11,9 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x261}
+s.listed_series={0x164}
 function s.cfilter(c,f)
-	return c:IsFaceup() and c:IsSetCard(0x261) and c:IsOriginalType(TYPE_PENDULUM) and c:IsOriginalType(TYPE_MONSTER) and f(c)
+	return c:IsFaceup() and c:IsSetCard(0x164) and c:IsOriginalType(TYPE_PENDULUM) and c:IsOriginalType(TYPE_MONSTER) and f(c)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc) end

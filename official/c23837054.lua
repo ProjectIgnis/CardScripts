8e32dad1ec5fa13f87fd2430ee235c6744b93b33
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-07 22:42:28 +0200

    Update and rename pre-release/c100416013.lua to official/c23837054.lua

--- official/c23837054.lua
+++ official/c23837054.lua
@@ -1,6 +1,6 @@
--- 溟界の呼び蛟
--- Invasion of the Abhyss
--- scripted by Hatter
+--溟界の呼び蛟
+--Invasion of the Abhyss
+--scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -16,19 +16,19 @@ function s.initial_effect(c)
 	if not GhostBelleTable then GhostBelleTable={} end
 	table.insert(GhostBelleTable,e1)
 end
-s.listed_series={0x260}
+s.listed_series={0x163}
 function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_REPTILE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.setfitler(c)
-	return c:IsSetCard(0x260) and c:IsType(TYPE_MONSTER)
+	return c:IsSetCard(0x163) and c:IsType(TYPE_MONSTER)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
-		local g=Duel.GetMatchingGroup(s.setfitler,tp,LOCATION_GRAVE,0,nil,0x260)
+		local g=Duel.GetMatchingGroup(s.setfitler,tp,LOCATION_GRAVE,0,nil,0x163)
 		local gysummon=g:GetClassCount(Card.GetCode)>=8 and sg:GetClassCount(Card.GetCode)>=2
-		local tksummon=Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0x260,TYPES_TOKEN,0,0,2,RACE_REPTILE,ATTRIBUTE_DARK)
+		local tksummon=Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x163,TYPES_TOKEN,0,0,2,RACE_REPTILE,ATTRIBUTE_DARK)
 		return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1 and (tksummon or gysummon)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
@@ -37,9 +37,9 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
 	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,nil,e,tp)
-	local g=Duel.GetMatchingGroup(s.setfitler,tp,LOCATION_GRAVE,0,nil,0x260)
+	local g=Duel.GetMatchingGroup(s.setfitler,tp,LOCATION_GRAVE,0,nil,0x163)
 	local gysummon=g:GetClassCount(Card.GetCode)>=8 and sg:GetClassCount(Card.GetCode)>=2
-	local tksummon=Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0x260,TYPES_TOKEN,0,0,2,RACE_REPTILE,ATTRIBUTE_DARK)
+	local tksummon=Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x163,TYPES_TOKEN,0,0,2,RACE_REPTILE,ATTRIBUTE_DARK)
 	-- player choice
 	local choice
 	if tksummon and gysummon then
@@ -50,7 +50,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	-- apply effect
 	if choice==0 then
 		for i=1,2 do
-			local token=Duel.CreateToken(tp,id+100)
+			local token=Duel.CreateToken(tp,id+1)
 			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
 		end
 		Duel.SpecialSummonComplete()

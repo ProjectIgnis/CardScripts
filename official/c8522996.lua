12faee65841cb4df64c3eb08b14b1186614c9de1
Edoardo Lolletti <edoardo762@gmail.com>
2021-03-12 19:37:38 +0100

    The first monarch

--- official/c8522996.lua
+++ official/c8522996.lua
@@ -1,4 +1,5 @@
 --始源の帝王
+--The first monarch
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -55,9 +56,8 @@ function s.chcost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.chtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.Hint(HINT_SELECTMSG,tp,0)
-	local aat=Duel.AnnounceAttribute(tp,1,0x7f)
-	e:SetLabel(aat)
+	local att=c:AnnounceAnotherAttribute(tp)
+	e:SetLabel(att)
 end
 function s.chop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()

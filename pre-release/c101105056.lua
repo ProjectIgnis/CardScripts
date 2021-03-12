e28173c7147fe0b342c7c021203cd4ba837d6403
Edoardo Lolletti <edoardo762@gmail.com>
2021-03-12 14:46:16 +0100

    Update Magikey - Maphteah
    You should choose if performing the ritual or fusion summon during resolution, not at activation

--- pre-release/c101105056.lua
+++ pre-release/c101105056.lua
@@ -3,18 +3,19 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	--Fusion
-	local e1=Fusion.CreateSummonEff({handler=c,desc=aux.Stringid(id,0),fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x262),extrafil=s.fextra})
+	--Fusion/Ritual summon
+	local fparams={fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x262),extrafil=s.fextra}
+	local rparams={filter=aux.FilterBoolFunction(Card.IsSetCard,0x262),lvtype=RITPROC_GREATER,extrafil=s.rextra,extraop=s.extraop,forcedselection=s.rcheck}
+	local fustg,fusop,rittg,ritop=Fusion.SummonEffTG(fparams),Fusion.SummonEffOP(fparams),Ritual.Target(rparams),Ritual.Operation(rparams)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target(fustg,rittg))
+	e1:SetOperation(s.operation(fustg,fusop,rittg,ritop))
 	c:RegisterEffect(e1)
-	--Ritual
-	local e2=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,desc=aux.Stringid(id,1),filter=aux.FilterBoolFunction(Card.IsSetCard,0x262),extrafil=s.rextra,extraop=s.extraop,forcedselection=s.rcheck})
-	c:RegisterEffect(e2)
 end
 s.listed_series={0x262}
-function s.costhint(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
-end
 function s.fextra(e,tp,mg)
 	if Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_NORMAL),tp,LOCATION_MZONE,0,1,nil) then
 		local sg=Duel.GetMatchingGroup(s.fexfilter,tp,LOCATION_DECK,0,nil)
@@ -47,4 +48,27 @@ function s.extraop(mg,e,tp,eg,ep,ev,re,r,rp)
 	mg:Sub(mat2)
 	Duel.ReleaseRitualMaterial(mg)
 	Duel.SendtoGrave(mat2,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+end
+function s.target(fustg,rittg)
+	return function(e,tp,eg,ep,ev,re,r,rp,chk)
+		if chk==0 then return fustg(e,tp,eg,ep,ev,re,r,rp,chk) or rittg(e,tp,eg,ep,ev,re,r,rp,chk) end
+	end
+end
+function s.operation(fustg,fusop,rittg,ritop)
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		local tc=Duel.GetFirstTarget()
+		local fus=fustg(e,tp,eg,ep,ev,re,r,rp,0)
+		local rit=rittg(e,tp,eg,ep,ev,re,r,rp,0)
+		if fus or rit then
+			local sel={}
+			if fus then table.insert(sel,aux.Stringid(id,0)) end
+			if rit then table.insert(sel,aux.Stringid(id,1)) end
+			local res=Duel.SelectOption(tp,table.unpack(sel))
+			if res==0 and fus then
+				fusop(e,tp,eg,ep,ev,re,r,rp)
+			else
+				ritop(e,tp,eg,ep,ev,re,r,rp)
+			end
+		end
+	end
 end

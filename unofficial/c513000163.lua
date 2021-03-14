e0d6685c5e75dd724728d7e30b7053f0770452cc
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-03-13 23:12:31 -0800

    Fix "Arcana Force XVII - The Hangman (Anime)"
    Effect should not trigger twice if it was already used (still needs to trigger in EP if not), along with script clean-up.

--- unofficial/c513000163.lua
+++ unofficial/c513000163.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--coin
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(511005634,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_COIN)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -36,24 +36,24 @@ end
 function s.arcanareg(c,coin)
 	--coin effect
 	local e0=Effect.CreateEffect(c)
-	e0:SetDescription(aux.Stringid(511005634,2))
+	e0:SetDescription(aux.Stringid(id,2))
 	e0:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e0:SetType(EFFECT_TYPE_IGNITION)
-	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_SINGLE_RANGE)
-	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_SINGLE_RANGE)
 	e0:SetRange(LOCATION_MZONE)
 	e0:SetCountLimit(1)
 	e0:SetCondition(s.descon)
-	e0:SetTarget(s.destg555)
-	e0:SetOperation(s.desop555)
+	e0:SetTarget(s.destg)
+	e0:SetOperation(s.desop)
 	e0:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 	c:RegisterEffect(e0)
-	--coin effect
+	--coin effect if not used in Main Phase
 	local e1=e0:Clone()
-	e1:SetDescription(aux.Stringid(511005634,1))
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCondition(s.epcon)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	c:RegisterEffect(e1)
 	c:RegisterFlagEffect(36690018,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,coin,63-coin)
@@ -61,38 +61,38 @@ end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return tp==Duel.GetTurnPlayer()
 end
-function s.filter555(c)
-	return c:IsDestructable() 
+function s.epcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp=Duel.GetTurnPlayer() and Duel.GetFlagEffect(tp,id)==0
 end
-function s.destg555(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	--self
 	local heads=e:GetHandler():GetFlagEffectLabel(36690018)==1
 	if heads then
-		if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+		if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) end
 		local c=e:GetHandler()
 		if chk==0 then return true end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local g=Duel.SelectTarget(tp,s.filter555,tp,LOCATION_MZONE,0,1,1,nil)
+		local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 		local atk=g:GetFirst():GetAttack()
 		Duel.SetTargetPlayer(tp)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,atk)
-		Duel.RegisterFlagEffect(tp,511005634,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
+		Duel.RegisterFlagEffect(tp,id,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
 	else
 	--opponents
-		if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc) end
+		if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
 		local c=e:GetHandler()
 		if chk==0 then return Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE)>0 end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local g=Duel.SelectTarget(tp,s.filter555,tp,0,LOCATION_MZONE,1,1,nil)
+		local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
 		local atk=g:GetFirst():GetAttack()
 		Duel.SetTargetPlayer(1-tp)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
-		Duel.RegisterFlagEffect(tp,511005634,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
+		Duel.RegisterFlagEffect(tp,id,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
 	end
 end
-function s.desop555(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
 	local atk=tc:GetAttack()

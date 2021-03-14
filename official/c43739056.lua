89abd8122c6a1385c998428e1a286be87622967b
Edoardo Lolletti <edoardo762@gmail.com>
2021-03-15 00:26:24 +0100

    Update c43739056.lua

--- official/c43739056.lua
+++ official/c43739056.lua
@@ -32,7 +32,7 @@ function s.aclimit(e,re,tp)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
-	return Duel.GetTurnPlayer()==e:GetHandler():GetOwner() and (ph==PHASE_MAIN1 or ph==PHASE_MAIN2)
+	return Duel.GetTurnPlayer()==e:GetHandler():GetOwner() and Duel.IsMainPhase()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToChangeControler() end

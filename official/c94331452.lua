63d60f54f2f52d723abcc225da164b304fc77160
Edoardo Lolletti <edoardo762@gmail.com>
2021-03-14 23:53:20 +0100

    Update c94331452.lua

--- official/c94331452.lua
+++ official/c94331452.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(s.target1)
+	e1:SetTarget(s.target(LOCATION_MZONE))
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCost(aux.bfgcost)
-	e2:SetTarget(s.target2)
+	e2:SetTarget(s.target(0))
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
@@ -30,25 +30,17 @@ function s.diffattfil(c,att)
 	local _att=c:GetAttribute()
 	return (_att&att)~=_att
 end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) and s.diffattfil(chkc,e:GetLabel()) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(aux.AND(s.filter,Card.IsCanBeEffectTarget),tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
-	local att=aux.AnnounceAnotherAttribute(g,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local sel=g:FilterSelect(tp,s.diffattfil,1,1,nil,att)
-	Duel.SetTargetCard(sel)
-	e:SetLabel(att)
-end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) and s.diffattfil(chkc,e:GetLabel()) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-	local g=Duel.GetMatchingGroup(aux.AND(s.filter,Card.IsCanBeEffectTarget),tp,LOCATION_MZONE,0,nil,e)
-	local att=aux.AnnounceAnotherAttribute(g,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local sel=g:FilterSelect(tp,s.diffattfil,1,1,nil,att)
-	Duel.SetTargetCard(sel)
-	e:SetLabel(att)
+function s.target(oppo)
+	return function (e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+		if chkc then return chkc:IsLocation(LOCATION_MZONE) and (oppo==0 or chkc:IsControler(tp)) and s.filter(chkc) and s.diffattfil(chkc,e:GetLabel()) end
+		if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,oppo,1,nil) end
+		local g=Duel.GetMatchingGroup(aux.AND(s.filter,Card.IsCanBeEffectTarget),tp,LOCATION_MZONE,oppo,nil,e)
+		local att=aux.AnnounceAnotherAttribute(g,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local sel=g:FilterSelect(tp,s.diffattfil,1,1,nil,att)
+		Duel.SetTargetCard(sel)
+		e:SetLabel(att)
+	end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()

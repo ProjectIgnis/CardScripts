24126bf7e3614952b59df6c08103a40c9ebea5ae
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-03-14 16:43:28 +0100

    added new rush script
    Moisturize

---
+++ rush/c160005047.lua
@@ -0,0 +1,37 @@
+--水分補給 
+--Moisturize
+local s,id=GetID()
+function s.initial_effect(c)
+	--heal and destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_RECOVER+CATEGORY_DESTROY)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.reccon)
+	e1:SetTarget(s.rectg)
+	e1:SetOperation(s.recop)
+	c:RegisterEffect(e1)
+end
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_AQUA),tp,LOCATION_MZONE,0,2,nil)
+end
+function s.filter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
+end
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Recover(p,d,REASON_EFFECT)>0 then return end
+	local dg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_ONFIELD,nil)
+	if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		local sg=dg:Select(tp,1,1,nil)
+		Duel.HintSelection(sg)
+		Duel.Destroy(sg,REASON_EFFECT)
+	end
+end

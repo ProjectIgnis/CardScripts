9dce3ae4303ba5fb07203d3449cefa2f7389e1c7
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-03-14 02:21:41 +0200

    Added new cards' scripts

---
+++ pre-release/c101105020.lua
@@ -0,0 +1,73 @@
+--驚楽園の助手 <Delia>
+--Amazement Assistant <Delia>
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCost(s.setcost)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)	
+end
+s.listed_series={0x15f}
+function s.costfilter(c)
+	return c:IsSetCard(0x15f) and c:IsType(TYPE_TRAP) and not c:IsPublic()
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.ShuffleHand(tp)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.cfilter(c,tp)
+	if not (c:IsSetCard(0x15f) and c:IsType(TYPE_TRAP) and c:IsAbleToGraveAsCost()) then return false end
+	if not c:IsLocation(LOCATION_SZONE) then
+		return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
+	else
+		return c:IsFaceup() and Duel.GetLocationCount(tp,LOCATION_SZONE)>-1 
+			and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,true)
+	end
+end
+function s.filter(c,ignore)
+	return c:IsSetCard(0x15f) and c:IsType(TYPE_TRAP) and c:IsSSetable(ignore)
+end
+function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil,tp)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g:GetFirst())
+	end
+end
---
+++ pre-release/c101105076.lua
@@ -0,0 +1,61 @@
+--Ａ・Ɐ・ＶＶ
+--Amaze Ɐttraction Vanquish Viking
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	--From cards_specific_functions.lua
+	aux.AddAttractionEquipProc(c)
+	--You
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCategory(CATEGORY_CONTROL)
+	e1:SetCondition(aux.AttractionEquipCon(true))
+	e1:SetCondition(s.negcon)
+	e1:SetTarget(s.negtg)
+	e1:SetOperation(s.negop)
+	c:RegisterEffect(e1)
+	--Your opponent
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(aux.AttractionEquipCon(false))
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return tp==1-Duel.GetTurnPlayer()
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ec=e:GetHandler():GetEquipTarget()
+	local tg=Duel.GetAttacker()
+	if chk==0 then return tg and tg:IsOnField() and ec:IsControlerCanBeChanged() end
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,ec,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local ec=e:GetHandler():GetEquipTarget()
+	if Duel.NegateAttack() and ec:IsControlerCanBeChanged() then
+		Duel.GetControl(ec,1-ec:GetControler(),PHASE_BATTLE,1)
+	end
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:GetHandler()==e:GetHandler():GetEquipTarget()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ec=e:GetHandler():GetEquipTarget()
+	if chk==0 then return ec:IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,ec,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local ec=e:GetHandler():GetEquipTarget()
+	if ec then
+		Duel.SendtoHand(ec,nil,REASON_EFFECT)
+	end
+end

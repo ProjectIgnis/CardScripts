fb4ea0053cc4611bb2cacfe1a8f214a58911e0ba
Sanity Sierota <47074795+that-hatter@users.noreply.github.com>
2021-03-15 19:41:35 +0800

    Various fixes (mainly operation infos) (#553)
    * Fix "Sprigguns Watch"
    
    removed incorrect operation info and added category
    
    * Fix "Doomking Balerdroch"
    
    fixed filters, removed incorrect operation infos and enforced ghost belle interaction via GhostBelle table
    
    * Fix "Mahaama the Fairy Dragon"
    
    removed incorrect operation infos
    
    * Fix "Sky Striker Maneuver - Scissors Cross"
    
    fixed categories and removed incorrect operation infos

--- official/c46271408.lua
+++ official/c46271408.lua
@@ -1,10 +1,10 @@
---閃刀術式－シザーズクロス 
+--閃刀術式－シザーズクロス
 --Sky Striker Maneuver - Scissors Cross
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_LEAVE_GRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -12,6 +12,8 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
 end
 s.listed_series={0x1115}
 function s.cfilter(c)
@@ -21,40 +23,28 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.filter(c,e,tp,spchk)
-	return  c:IsSetCard(0x1115) and c:IsType(TYPE_MONSTER) and c:IsLevel(4) and (c:IsAbleToHand() or (spchk and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+	return c:IsSetCard(0x1115) and c:IsType(TYPE_MONSTER) and c:IsLevel(4) and (c:IsAbleToHand() or (spchk and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local spchk=false
-	if Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3 then
-		spchk=true
-	end
+	local spchk=Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(c,e,tp,spchk) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
-	local c=g:GetFirst()
-	if spchk and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsAbleToHand() then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-	elseif not (spchk and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) and c:IsAbleToHand() then
-		e:SetCategory(CATEGORY_TOHAND)
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-	else 
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-	end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,spchk) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,spchk)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
-		aux.ToHandOrElse(tc,tp,function(c)
-								return Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3
-										and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-										and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-				end,function(c)
-					Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-					end,1152)
+		aux.ToHandOrElse(tc,tp,
+			function(c)
+				return Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3
+					and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+					and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			end,
+			function(c)
+				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+			end,
+		1152)
 	end
 end
-
-

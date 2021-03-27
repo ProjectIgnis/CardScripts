325134bde898c747ab4496687d5203aa04ef35ce
lrhel <lrhel@users.noreply.github.com>
2021-03-27 14:54:58 +0000

    fix "Crusadia Testament"

--- official/c87497553.lua
+++ official/c87497553.lua
@@ -60,7 +60,7 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(e:GetLabelObject():GetLink())
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,0,0,tp,ct)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local rc=e:GetLabelObject()

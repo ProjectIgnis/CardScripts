69325e2c1f12771cb090a95cbd319cb348a9cfd4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-03-27 22:07:23 -0700

    Fix "Fluffal Octopus (Anime)"
    Wrong category in opinfo

--- unofficial/c511050003.lua
+++ unofficial/c511050003.lua
@@ -14,14 +14,14 @@ function s.initial_effect(c)
 end
 s.listed_series={0xa9,0xc3}
 function s.filter(c)
-	return c:IsSetCard(0xa9) or c:IsSetCard(0xc3) and c:IsAbleToHand()
+	return (c:IsSetCard(0xa9) or c:IsSetCard(0xc3)) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_HAND,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()

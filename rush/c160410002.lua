6158b5ddba4645ddf4fe94ac35f076c9d294e42e
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-03-29 18:24:38 +0200

    added new scripts
    - Royal Demon's Command
    - alt art for SRM and Dragias

---
+++ rush/c160410002.lua
@@ -0,0 +1,55 @@
+--連撃竜ドラギアス
+--Rush Dragon Dragears
+local s,id=GetID()
+function s.initial_effect(c)
+	--Attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.atkcon)
+	e1:SetCost(s.atkcost)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP()
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local con=c:GetFlagEffect(id)==0
+	if chk==0 then return con end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	--requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--effect
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END,0,0)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EXTRA_ATTACK)
+		e1:SetCondition(s.macon)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+		e1:SetValue(1)
+		c:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_BATTLE_DESTROYING)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetCondition(aux.bdcon)
+		e2:SetOperation(s.bdop)
+		c:RegisterEffect(e2)
+	end
+end
+function s.macon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id+1)>0
+end
+function s.bdop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+end

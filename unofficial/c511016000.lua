c35f00933dc1ef32bc770eb521d9b879ae5adcca
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-04-04 13:05:08 -0700

    Fix "Arcana Force VII - The Chariot (Anime)"
    Changed to GetFirstTarget.

--- unofficial/c511016000.lua
+++ unofficial/c511016000.lua
@@ -80,7 +80,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
+	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end

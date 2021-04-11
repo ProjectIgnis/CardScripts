2044caed1b01c3d1cce3a8c2b59ed1bfe4afdb06
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-04-11 22:06:20 +0300

    "Chronomaly Megala Globe" fix
    Was also checking the hand for valid monsters to Special Summon during resolution of the effect.

--- pre-release/c101105013.lua
+++ pre-release/c101105013.lua
@@ -45,7 +45,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
 	end

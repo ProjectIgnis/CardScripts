fcaff59cec885859e4b67fa5e370171bbe10727d
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-04-11 14:53:43 +0200

    Avian Spell Strategy fix
    fixed to not count Maximum monsters

--- rush/c160005053.lua
+++ rush/c160005053.lua
@@ -11,8 +11,11 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+function s.filter(c)
+	return c:IsRace(RACE_WARRIOR+RACE_WINGEDBEAST) and c:IsFaceup()
+end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_WARRIOR+RACE_WINGEDBEAST),tp,LOCATION_MZONE,0,2,nil)
+	return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,0,2,nil)
 end
 function s.filter2(c)
 	return c:IsType(TYPE_SPELL+TYPE_TRAP)

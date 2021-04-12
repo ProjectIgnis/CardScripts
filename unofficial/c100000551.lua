74cbca7ad03da7dbdc83cbe62bae190e70624a9d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-04-12 17:29:36 +0300

    "Contact Soul" update
    Removed the targeting flag property from its effect since it does not target.

--- unofficial/c100000551.lua
+++ unofficial/c100000551.lua
@@ -5,7 +5,6 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)

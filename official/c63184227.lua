adffba1f5697c2f0979b727713a62ef29ddbd2b6
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-04-19 23:11:35 +0300

    Update and rename pre-release/c101105102.lua to official/c63184227.lua

--- official/c63184227.lua
+++ official/c63184227.lua
@@ -22,7 +22,7 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BE_MATERIAL)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,id+100)
+	e2:SetCountLimit(1,id+1)
 	e2:SetCondition(s.stcon)
 	e2:SetTarget(s.sttg)
 	e2:SetOperation(s.stop)
@@ -63,14 +63,14 @@ function s.stcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.sttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and
-			Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0xa3,TYPES_TOKEN,0,0,1,RACE_DRAGON,ATTRIBUTE_LIGHT)
+			Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0xa3,TYPES_TOKEN,0,0,1,RACE_DRAGON,ATTRIBUTE_LIGHT)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
 function s.stop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and
-			Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0xa3,TYPES_TOKEN,0,0,1,RACE_DRAGON,ATTRIBUTE_LIGHT) then
+			Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0xa3,TYPES_TOKEN,0,0,1,RACE_DRAGON,ATTRIBUTE_LIGHT) then
 		local token=Duel.CreateToken(tp,id+100)
 		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
 	end

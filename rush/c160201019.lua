7cc9aadd20302f1fcbbb158b361c3c4753e405b7
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-04-22 14:16:14 +0200

    Gargoyle of the Palace update
    updated to use the "UpdateLevel" function

--- rush/c160201019.lua
+++ rush/c160201019.lua
@@ -26,12 +26,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		--Effect
 		local c=e:GetHandler()
 		if c:IsFaceup() and c:IsRelateToEffect(e) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_LEVEL)
-			e1:SetValue(1)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-			c:RegisterEffect(e1)
+			c:UpdateLevel(1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,c)
 		end
 	end
 end

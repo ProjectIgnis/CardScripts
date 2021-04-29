34813bde5cdbc662708f28b3aeea3bb8b87c8c06
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-04-28 19:44:46 -0700

    Fix "Hell Spider"
    Ending BP handling

--- unofficial/c511000794.lua
+++ unofficial/c511000794.lua
@@ -1,3 +1,4 @@
+--地獄蜘蛛
 --Hell Spider
 local s,id=GetID()
 function s.initial_effect(c)
@@ -14,5 +15,5 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SkipPhase(Duel.GetTurnPlayer(),PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE,1)
+	Duel.SkipPhase(Duel.GetTurnPlayer(),PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
 end

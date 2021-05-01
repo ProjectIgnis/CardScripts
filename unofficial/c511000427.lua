bd343c5a365b2c4b56e19fe0f12f0c273f426480
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-02 02:53:56 +0300

    "Aid to the Doomed" script update
    Updated its script in relation with its text update.

--- unofficial/c511000427.lua
+++ unofficial/c511000427.lua
@@ -10,36 +10,41 @@ function s.initial_effect(c)
 	e1:SetCost(s.cost)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--act qp in hand
+	--Can activate from hand during opponent's turn
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
 	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(s.actcon)
+	e2:SetLabel(0)
 	c:RegisterEffect(e2)
+	e1:SetLabelObject(e2)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(Card.IsPreviousControler,1,nil,tp)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,2,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,2,2,e:GetHandler())
-	Duel.SendtoGrave(g,REASON_EFFECT)
+	local label=e:GetLabelObject():GetLabel()
+	if chk==0 then
+		if label==1 then
+			return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,e:GetHandler())
+		elseif label==0 then
+			return true
+		end
+	end
+	if label==1 then
+		Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD,e:GetHandler())
+	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local sk=Duel.GetTurnPlayer()
-	Duel.BreakEffect()
-	Duel.SkipPhase(sk,PHASE_DRAW,RESET_PHASE+PHASE_END,1)
-	Duel.SkipPhase(sk,PHASE_STANDBY,RESET_PHASE+PHASE_END,1)
-	Duel.SkipPhase(sk,PHASE_MAIN1,RESET_PHASE+PHASE_END,1)
-	Duel.SkipPhase(sk,PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
-	Duel.SkipPhase(sk,PHASE_MAIN2,RESET_PHASE+PHASE_MAIN2,1)
-	Duel.SkipPhase(sk,PHASE_END,RESET_PHASE+PHASE_END,1)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_BP)
-	e1:SetTargetRange(1,1)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
+	Duel.SkipPhase(Duel.GetTurnPlayer(),PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
+end
+function s.actcon(e)
+	if Duel.IsExistingMatchingCard(Card.IsDiscardable,e:GetHandlerPlayer(),LOCATION_HAND,0,2,e:GetHandler()) then
+		e:SetLabel(1)
+		return true
+	else
+		e:SetLabel(0)
+		return false
+	end
 end

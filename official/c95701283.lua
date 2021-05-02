5d6bb45db8132ad00a6455e99cd907be5366b7ab
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-02 04:00:37 +0300

    "Majestic Mech - Goryu" fix
    The effect to send itself to the GY should not be activated if the effect of "Kaiser Sea Horse" is applied for its Tribute Summon.

--- official/c95701283.lua
+++ official/c95701283.lua
@@ -2,47 +2,27 @@
 --Majestic Mech - Goryu
 local s,id=GetID()
 function s.initial_effect(c)
-	--summon with 1 tribute
-	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0))
-	--tribute check
+	--Tribute Summon with 1 Tribute
+	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),nil,s.sumop)
+	--Piercing
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_MATERIAL_CHECK)
-	e2:SetValue(s.valcheck)
+	e2:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e2)
-	--sent to graev in ep only after summon with 1 tribute
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_SUMMON_COST)
-	e3:SetOperation(s.facechk)
-	e3:SetLabelObject(e2)
-	c:RegisterEffect(e3)
-	--pierce
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_PIERCE)
-	c:RegisterEffect(e4)
 end
-function s.valcheck(e,c)
-	local g=c:GetMaterialCount()
-	if e:GetLabel()==1 and g==1 then
-		e:SetLabel(0)
-		--to grave
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(id,1))
-		e1:SetCategory(CATEGORY_TOGRAVE)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetCountLimit(1)
-		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetTarget(s.tgtg)
-		e1:SetOperation(s.tgop)
-		e1:SetReset(RESET_EVENT|(RESETS_STANDARD|RESET_MSCHANGE|RESET_OVERLAY)&~(RESET_TOFIELD|RESET_LEAVE|RESET_TEMP_REMOVE))
-		c:RegisterEffect(e1)
-	end
-end
-function s.facechk(e,tp,eg,ep,ev,re,r,rp)
-	e:GetLabelObject():SetLabel(1)
+function s.sumop(g,e,tp,eg,ep,ev,re,r,rp,c,minc,zone,relzone,exeff)
+	--Send to GY during the End Phase
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.tgtg)
+	e1:SetOperation(s.tgop)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+	c:RegisterEffect(e1)
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

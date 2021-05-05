3fb64e517e8086d05d3c96db376cf404cee53331
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-05 23:30:01 +0300

    "Dragged Down into the Grave" fix
    Should not be able to activate it if either player cannot draw 1 card.

--- official/c16435215.lua
+++ official/c16435215.lua
@@ -1,4 +1,5 @@
 --墓穴の道連れ
+--Dragged Down into the Grave
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -12,15 +13,15 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsLocation(LOCATION_HAND) then
-		return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>1
-	else
-		return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0
-	end
+	if Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)<=0 then return false end
+	local ct=0
+	if e:GetHandler():IsLocation(LOCATION_HAND) then ct=1 end
+	return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>ct
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,PLAYER_ALL,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)==0 or Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0 then return end

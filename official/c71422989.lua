82dc1573390665137c614db1a818dbfce6f6eb55
Edoardo Lolletti <edoardo762@gmail.com>
2021-05-06 18:39:13 +0200

    Fix for absorb fusion not checking for fusion materials to be banishable

--- official/c71422989.lua
+++ official/c71422989.lua
@@ -3,7 +3,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local params={aux.FilterBoolFunction(Card.IsSetCard,0x1047),nil,nil,Fusion.BanishMaterial}
+	local params={aux.FilterBoolFunction(Card.IsSetCard,0x1047),Card.IsAbleToRemove,nil,Fusion.BanishMaterial}
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)

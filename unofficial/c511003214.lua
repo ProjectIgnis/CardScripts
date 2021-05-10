13942d65e87269f2e12bc5aa1b6e9a0e0fc78795
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-05-09 18:54:37 -0700

    Add "Galaxy Dragon (Manga) and "Dragon Buster"

---
+++ unofficial/c511003214.lua
@@ -0,0 +1,49 @@
+--ギャラクシー・ドラグーン (Manga)
+--Galaxy Dragon (Manga)
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Increase ATK by 1000 if Galaxy-Eyes Photon Dragon is on the field
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCondition(s.atkcon)
+	e1:SetValue(1000)
+	c:RegisterEffect(e1)
+	--Negate effects if it attacks GEPD
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	c:RegisterEffect(e3)
+end
+s.listed_names={CARD_GALAXYEYES_P_DRAGON}
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsCode,tp,0,LOCATION_MZONE,1,nil,CARD_GALAXYEYES_P_DRAGON)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	if bc and bc:IsCode(CARD_GALAXYEYES_P_DRAGON) then
+		c:CreateRelation(bc,RESET_EVENT+RESETS_STANDARD)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetCondition(s.discon)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+		bc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		bc:RegisterEffect(e2)
+	end
+end
+function s.discon(e)
+	return e:GetOwner():IsRelateToCard(e:GetHandler())
+end

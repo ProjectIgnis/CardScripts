faa7bf9927501adb80627013903f5edfd5869efc
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-05-09 18:26:48 -0700

    Add "Infernity Patriach (Manga)"

---
+++ unofficial/c511003213.lua
@@ -0,0 +1,51 @@
+--インフェルニティ・ビショップ (Manga)
+--Infernity Patriarch (Manga)
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Make 1 monster you control unable to be destroyed by battle or card effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0 and c:GetFlagEffect(id)==0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc then
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD-RESET_TOGRAVE,0,0)
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3000)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetCondition(s.indcon)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetLabelObject(c)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetDescription(3001)
+		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.indcon(e,tp,eg,ep,ev,re,r,rp)
+	 local c=e:GetLabelObject()
+	 return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0 and c:GetFlagEffect(id)>0
+end

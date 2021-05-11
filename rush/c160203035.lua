573f9e02f97fb224d2c387b00b12e01f8a3a4649
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-05-11 21:18:16 +0200

    added new scripts
    Miraculous Coevolution
    Horn Wan Yue the Crescent Music Maiden

---
+++ rush/c160203035.lua
@@ -0,0 +1,41 @@
+--弧楽姫ホルン・ワン・ユエ
+--Horn Wan Yue the Crescent Music Maiden
+local s,id=GetID()
+function s.initial_effect(c)
+	--ATK increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_TOHAND)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsStatus(STATUS_SUMMON_TURN)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+end
+function s.thfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsRace(RACE_WARRIOR) and c:IsAbleToHand()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.DiscardDeck(tp,1,REASON_EFFECT)
+	local g=Duel.GetOperatedGroup()
+	local ct=g:GetFirst()
+	if ct then
+		--If it was a monster, make 1 warrior monster gain ATK equal to sent monster's level x 300 and take damage equal to that amount
+		if ct:IsType(TYPE_MONSTER) and ct:IsRace(RACE_WARRIOR) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+			if #g>0 then
+				Duel.SendtoHand(g,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,g)
+			end
+		end
+	end
+end

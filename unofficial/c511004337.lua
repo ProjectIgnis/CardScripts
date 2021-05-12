679fdb9d39c4c295d389e9789f9d6619cb27dc03
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-05-11 20:55:48 -0700

    Update "Dark Auction"
    Updated script to reflect text.

--- unofficial/c511004337.lua
+++ unofficial/c511004337.lua
@@ -8,6 +8,7 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetTarget(s.tg)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
@@ -15,18 +16,22 @@ end
 function s.tfilter(c)
 	return c:GetCounter(0x1107)~=0 and c:IsAbleToHand()
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.tfilter,tp,LOCATION_ONFIELD,0,1,nil) end
-	local g=Duel.GetMatchingGroup(s.tfilter,tp,LOCATION_ONFIELD,0,nil)
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then chkc:IsLocation(LOCATION_ONFIELD) and s.tfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.tfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	local dam=g:GetFirst():GetAttack()
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(dam)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)	
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.tfilter,tp,LOCATION_ONFIELD,0,1,1,nil)
-	local tc=g:GetFirst()
-	local dam=tc:GetAttack()
-	if tc then
+	local tc=Duel.GetFirstTarget()
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if tc and tc:IsRelateToeEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.Damage(1-tp,dam,REASON_EFFECT)
+		Duel.Damage(p,d,REASON_EFFECT)
 	end
 end

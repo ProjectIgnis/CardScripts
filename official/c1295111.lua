05cbdf5a75b38eee93dab35598a775af5dc5b70d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-16 22:57:30 +0300

    "Salamangreat Sanctuary" fix
    Should not gain LP if the target's ATK doesn't change to 0.

--- official/c1295111.lua
+++ official/c1295111.lua
@@ -107,7 +107,8 @@ function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
+		--Change ATK to 0
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)

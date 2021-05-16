143f9e2ae952b990817dbabd60942b913fe88bc8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-16 23:46:03 +0300

    "Pile Armed Dragon" fix
    Missing reset in the ATK increasing effect.

--- official/c19153590.lua
+++ official/c19153590.lua
@@ -74,7 +74,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(e:GetLabel())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 	--Can only attack with 1 monster

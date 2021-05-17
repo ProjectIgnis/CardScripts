6e6e05edef966df05029572561fac52dbbba33d9
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-17 04:42:04 +0300

    "Enemy Controller (Anime)" fix
    Was missing the targeting flag.

--- unofficial/c511000604.lua
+++ unofficial/c511000604.lua
@@ -1,9 +1,11 @@
+--エネミーコントローラー (Anime)
 --Enemy Controller (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_BATTLE_PHASE+TIMING_STANDBY_PHASE,TIMING_BATTLE_PHASE)
 	e1:SetCost(s.cost)

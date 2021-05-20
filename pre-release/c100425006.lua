2232bfc983bee8c4def364dfb1004d80c3965304
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-21 00:27:05 +0300

    Added new card's script

---
+++ pre-release/c100425006.lua
@@ -0,0 +1,63 @@
+--SRルーレット
+--Speedroid Roulette
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DICE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.roll_dice=true
+s.listed_series={0x2016}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x2016) and c:HasLevel() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.check(lv)
+	return  function(sg,e,tp,mg)
+				return sg:GetSum(Card.GetLevel)==lv and Duel.GetLocationCount(tp,LOCATION_MZONE)>=#sg
+			end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local dc=Duel.TossDice(tp,1)
+	local flag=false
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,nil,e,tp)
+		local ft=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),2)
+		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+		local sg=aux.SelectUnselectGroup(g,e,tp,1,ft,s.check(dc),1,tp,HINTMSG_SPSUMMON)
+		if #sg>0 then
+			for tc in ~sg do
+				if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+					--Negate their effects
+					local e1=Effect.CreateEffect(e:GetHandler())
+					e1:SetType(EFFECT_TYPE_SINGLE)
+					e1:SetCode(EFFECT_DISABLE)
+					e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+					e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+					tc:RegisterEffect(e1)
+					local e2=e1:Clone()
+					e2:SetCode(EFFECT_DISABLE_EFFECT)
+					tc:RegisterEffect(e2)
+					flag=true
+				end
+			end
+			Duel.SpecialSummonComplete()
+		end
+	end
+	if not flag then Duel.SetLP(tp,Duel.GetLP(tp)-dc*500) end
+end

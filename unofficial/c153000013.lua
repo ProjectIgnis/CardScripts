a8cf36b3195120042684891ad179857b31d42b6e
Larry126 <28985622+larry126@users.noreply.github.com>
2021-05-22 23:25:09 +0800

    Removed debug messages

--- unofficial/c153000013.lua
+++ unofficial/c153000013.lua
@@ -3,10 +3,6 @@
 --Scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	if not DeckMaster then
-		Debug.Message("You are not using "..math.floor(id).." properly!")
-		return
-	end
 	--Deck Master Ability
 	local dme1=Effect.CreateEffect(c)
 	dme1:SetType(EFFECT_TYPE_FIELD)

59dbe34975eac2178789f8f7bc0183c373c2ab28
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-05-23 20:16:44 +0300

    "Thousand Knives (Rush)" fix
    Was using the targeting flag when the effect does not target.

--- rush/c160002040.lua
+++ rush/c160002040.lua
@@ -1,3 +1,4 @@
+--千本ナイフ (Rush)
 --Thousand Knives (Rush)
 local s,id=GetID()
 function s.initial_effect(c)
@@ -5,7 +6,6 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
@@ -16,15 +16,13 @@ s.listed_names={CARD_DARK_MAGICIAN}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,CARD_DARK_MAGICIAN),tp,LOCATION_ONFIELD,0,1,nil)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local dg=Duel.GetMatchingGroup(Card.IsNotMaximumSide,tp,0,LOCATION_MZONE,nil)
-	if chkc then return chkc:IsOnField() and chkc~=e:GetHandler() end
 	if chk==0 then return #dg>0 end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	-- requirement
-	
-	-- effect
+	--Requirement
+	--Effect
 	local dg=Duel.GetMatchingGroup(Card.IsNotMaximumSide,tp,0,LOCATION_MZONE,nil)
 	if dg and #dg>0 then
 		local sg=dg:Select(tp,1,1,nil)

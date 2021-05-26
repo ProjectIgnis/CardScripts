1a8977ac48d4daf670b3c9934ee4a1cb343936ef
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-05-26 09:00:53 -0700

    Add "Sea Lord's Amulet (Anime)" Script (#589)

---
+++ unofficial/c511003216.lua
@@ -0,0 +1,41 @@
+--水神の護符 (Anime)
+--Sea Lord's Amulet (Anime)
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.reg)
+	c:RegisterEffect(e1)
+	--WATER monsters cannot be targeted by opponents card effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER))
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--Destroys itself in End Phase
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+end
+function s.reg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	e:GetHandler():RegisterFlagEffect(id,RESET_PHASE+PHASE_END,EFFECT_FLAG_OATH,1)
+end
+function s.descon(e)
+	return e:GetHandler():GetFlagEffect(id)>0
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end

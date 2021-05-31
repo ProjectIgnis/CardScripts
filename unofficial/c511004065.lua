391eebf232d71396ed515c35a6746e89b8fbf92b
Kohana <80439628+XyleN5967@users.noreply.github.com>
2021-06-01 07:48:52 +0800

    Update c511004065.lua

--- unofficial/c511004065.lua
+++ unofficial/c511004065.lua
@@ -3,7 +3,6 @@
 --scripted by XyLeN
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableReviveLimit()
 	--Special Summon Procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)

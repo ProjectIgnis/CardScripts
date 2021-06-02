13f1f68fd113041e4fcb650cf02b82057b18098a
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-06-02 22:15:33 +0200

    Darkness Ri-chair-ual fix

--- rush/c160203027.lua
+++ rush/c160203027.lua
@@ -16,7 +16,7 @@ function s.costfilter(c)
 	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsFaceup() and c:IsAbleToGraveAsCost() and not c:IsMaximumModeSide()
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE,0,3,nil)
 	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 end
 function s.filter(c,e,tp)

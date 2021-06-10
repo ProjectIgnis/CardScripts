cf85c6905a6258148ba85aa178b4c2da75e6f59d
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-06-10 16:35:27 -0700

    Fix "Mirror Imagine Catadioptricker 7"
    Misspelled filter name allowed adding of non-Spell Cards.

--- unofficial/c511009644.lua
+++ unofficial/c511009644.lua
@@ -42,12 +42,12 @@ function s.sgfilter(sg,e,tp,mg)
 	return sg:GetClassCount(Card.GetCode)==1
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local mg=Duel.GetMatchingGroup(s.cilfter,tp,LOCATION_GRAVE,0,nil)
+	local mg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
 	if chk==0 then return aux.SelectUnselectGroup(mg,e,tp,3,3,s.sgfilter,0) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,3,tp,LOCATION_GRAVE)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetMatchingGroup(s.cilfter,tp,LOCATION_GRAVE,0,nil)
+	local mg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local sg=aux.SelectUnselectGroup(mg,e,tp,3,3,s.sgfilter,1,tp,HINTMSG_ATOHAND)
 	if #sg>0 then

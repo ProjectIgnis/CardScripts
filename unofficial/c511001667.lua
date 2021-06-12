ec154682c0a1411887c6a3b120b9a22fb1b21090
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-06-12 22:57:21 +0300

    Update c511001667.lua

--- unofficial/c511001667.lua
+++ unofficial/c511001667.lua
@@ -12,9 +12,9 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x266}
+s.listed_series={0x16b}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x266),tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x16b),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.filter(c)
 	return c:GetSequence()<5

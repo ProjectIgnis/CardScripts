7d402902364b2b4802872bb36d02390fde779daf
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-06-12 22:50:56 +0300

    Update and rename pre-release/c100278035.lua to official/c29114773.lua

--- official/c29114773.lua
+++ official/c29114773.lua
@@ -2,7 +2,7 @@
 --Speedroid Scratch
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)

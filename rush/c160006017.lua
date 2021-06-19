ecdd8ee5dabe249e61f800668d499db91929756d
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2021-06-19 17:42:10 -0400

    Updated "Metagias the Steelstriking Dragon"
    The effect to make itself unable to be destroyed by opponent's trap effects was not working.

--- rush/c160006017.lua
+++ rush/c160006017.lua
@@ -2,7 +2,7 @@
 --Metagias the Steelstriking Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-	--Attack
+	--Make itself unable to be destroyed by battle or opponent's traps, make a 2nd attack on monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
@@ -20,13 +20,13 @@ function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	--requirement
+	--Requirement
 	local tg=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
 	if Duel.SendtoGrave(tg,REASON_COST)==1 then
-		--effect
+		--Effect
 		if c:IsFaceup() and c:IsRelateToEffect(e) then
 			c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END,0,0)
-			--cannot be destroyed battle
+			--Cannot be destroyed by opponent's trap
 			local e1=Effect.CreateEffect(c)
 			e1:SetDescription(3012)
 			e1:SetType(EFFECT_TYPE_SINGLE)
@@ -36,14 +36,16 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetValue(s.efilter)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			c:RegisterEffect(e1)
+			--Cannot be destroyed battle
 			local e2=Effect.CreateEffect(c)
+			e2:SetDescription(3000)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
 			e2:SetValue(1)
 			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			c:RegisterEffect(e2)
-			--Attack up to thrice
+			--Attack up to twice
 			local e2=Effect.CreateEffect(c)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -54,4 +56,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 			c:RegisterEffect(e2)
 		end
 	end
+end
+function s.efilter(e,te)
+	return te:IsActiveType(TYPE_TRAP) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end

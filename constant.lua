298d460a5f7d69e5da87c600dbf95a858313557e
Edoardo Lolletti <edoardo762@gmail.com>
2021-06-25 20:05:34 +0200

    Remove unused constants

--- constant.lua
+++ constant.lua
@@ -1,7 +1,3 @@
---min/max values
-MIN_ID      = 1000      --4 digit, by DataManager::GetDesc()
-MAX_ID      = 268435455 --9 digit, by DataManager::GetDesc()
-MAX_COUNTER = 65535     --max number for adding/removing counters, by card::add_counter(), field::remove_counter()
 --Locations
 LOCATION_DECK    = 0x01
 LOCATION_HAND    = 0x02

a1615540c43e82f539e884bd407ec6d93fc63de5
Edoardo Lolletti <edoardo762@gmail.com>
2021-06-25 19:47:13 +0200

    Minor cleanup to Dimension Fusion Destruction
    Use new fusion procedure parameter

--- official/c89190953.lua
+++ official/c89190953.lua
@@ -4,19 +4,8 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--activate
-	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x145),extrafil=s.fextra,chkf=FUSPROC_NOTFUSION|FUSPROC_LISTEDMATS,stage2=s.stage2,extraop=Fusion.BanishMaterial})
-	local tg=e1:GetTarget()
-	e1:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk)
-					if chk==0 then
-						return tg(e,tp,eg,ep,ev,re,r,rp,chk)
-					end
-					tg(e,tp,eg,ep,ev,re,r,rp,chk)
-					if Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,6007213,32491822,69890967),tp,LOCATION_MZONE,0,1,nil)
-						and e:IsHasType(EFFECT_TYPE_ACTIVATE) and e:GetHandler()==e:GetOwner() then
-						Duel.SetChainLimit(s.chlimit)
-					end
-				end)
-	c:RegisterEffect(e1)
+	local e1=Fusion.RegisterSummonEff{handler=c,fusfilter=aux.FilterBoolFunction(Card.IsSetCard,0x145),extrafil=s.fextra,
+									chkf=FUSPROC_NOTFUSION|FUSPROC_LISTEDMATS,stage2=s.stage2,extraop=Fusion.BanishMaterial,extratg=s.extratg}
 	if not GhostBelleTable then GhostBelleTable={} end
 	table.insert(GhostBelleTable,e1)
 end
@@ -38,5 +27,11 @@ function s.stage2(e,tc,tp,mg,chk)
 		e1:SetValue(1)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1,true)
+	end
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) and e:GetHandler()==e:GetOwner()
+		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,6007213,32491822,69890967),tp,LOCATION_MZONE,0,1,nil) then
+		Duel.SetChainLimit(s.chlimit)
 	end
 end

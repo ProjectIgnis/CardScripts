dffc10ceab9ff36e661652a7cfab67696d69adbb
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-06-28 15:11:51 +0300

    "Crystal Abundance" fixes
    Should be able to activate it even without any "Crystal Beast" monsters in your GY to Special Summon. Additionally, was not checking that there are other cards on the field to send to the GY (excluding what you would send as the cost). Lastly, should be able to chain the effect of "Ghost Belle & Haunted Mansion" to it, and made sure this new implementation works with "Destiny HERO - Diamond Dude".

--- official/c72881007.lua
+++ official/c72881007.lua
@@ -1,4 +1,5 @@
 --宝玉の氾濫
+--Crystal Abundance
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -10,48 +11,55 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
 end
 s.listed_series={0x1034}
 function s.costfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsAbleToGraveAsCost()
+	return c:IsFaceup() and c:IsSetCard(0x1034) and c:IsAbleToGraveAsCost() and c:GetSequence()<5
+end
+function s.rescon(sg,e,tp,mg)
+	return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,sg+e:GetHandler())
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_SZONE,0,4,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_SZONE,0,4,4,nil)
+	local cg=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_SZONE,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(cg,e,tp,4,4,s.rescon,0) end
+	local g=aux.SelectUnselectGroup(cg,e,tp,4,4,s.rescon,1,tp,HINTMSG_TOGRAVE)
 	Duel.SendtoGrave(g,REASON_COST)
 end
-function s.filter(c,e,tp)
-	return c:IsSetCard(0x1034) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,0,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
 	if chk==0 then
 		if e:GetLabel()~=1 then
-			return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+			return #g>0
 		else
 			e:SetLabel(0)
-			return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_SZONE,0,1,nil,e,tp)
+			return true
 		end
 	end
 	e:SetLabel(0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,tp,0)
 end
 function s.ctfilter(c,tp)
-	return c:IsPreviousControler(tp) and c:IsLocation(LOCATION_GRAVE)
+	return c:IsPreviousControler(1-tp) and c:IsLocation(LOCATION_GRAVE)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x1034) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,0,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
 	Duel.SendtoGrave(g,REASON_EFFECT)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	g=Duel.GetOperatedGroup()
-	local ct=g:FilterCount(s.ctfilter,nil,1-tp)
+	local og=Duel.GetOperatedGroup()
+	local ct=og:FilterCount(s.ctfilter,nil,tp)
+	if ct==0 then return end
 	if ft>ct then ft=ct end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,ft,ft,nil,e,tp)
-	if #g>0 then
+	local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,ft,ft,nil,e,tp)
+	if #sg>0 then
 		Duel.BreakEffect()
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

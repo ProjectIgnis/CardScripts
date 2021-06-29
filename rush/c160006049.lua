b34041bff6cb9a2ff1eb96832d9679b1c0a2a6f5
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-06-29 19:59:33 +0200

    Burning Beast Quality fix

--- rush/c160006049.lua
+++ rush/c160006049.lua
@@ -6,12 +6,13 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 function s.tdfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_BEAST) and c:IsLevel(1) and c:IsAbleToDeckAsCost()
+	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_BEAST) and c:IsAbleToDeckAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end

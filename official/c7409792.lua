ad6bc6e41e9a83afd07ab6a7bb856b461a9e121b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-06-30 15:54:43 +0300

    "Dynatherium" update
    Script cleanup.

--- official/c7409792.lua
+++ official/c7409792.lua
@@ -13,14 +13,14 @@ function s.initial_effect(c)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 	--special summon when summoned
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCondition(s.condition)
-	e3:SetOperation(s.operation)
-	c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.condition)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -28,17 +28,17 @@ function s.spcon(e,c)
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+1)
 end
 function s.filter(c,e,tp)
-	return c:GetLevel()==4 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,1-tp,false,false)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.filter),1-tp,LOCATION_GRAVE,LOCATION_GRAVE,nil,e,1-tp)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.filter),0,LOCATION_GRAVE,LOCATION_GRAVE,nil,e,tp)
 	if #g>0 and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
 		and Duel.SelectYesNo(1-tp,aux.Stringid(id,1)) then
 		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
 		local sg=g:Select(1-tp,1,1,nil)
 		Duel.SpecialSummon(sg,0,1-tp,1-tp,false,false,POS_FACEUP)
-end
+	end
 end

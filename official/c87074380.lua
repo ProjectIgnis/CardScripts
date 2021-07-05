872a3a408057a0a4718c1491d139dc893ce01f5b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-07-05 13:43:29 +0300

    "Machina Citadel" fix
    Fixed some edge cases regarding the destroy effect.

--- official/c87074380.lua
+++ official/c87074380.lua
@@ -41,14 +41,15 @@ s.listed_names={}
 function s.splimit(e,se,sp,st)
 	return se:IsHasType(EFFECT_TYPE_ACTIONS)
 end
-function s.tgfilter(c)
-	return c:IsFaceup() and c:IsRace(RACE_MACHINE) and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsAttackBelow,c:GetAttack()),c:GetControler(),0,LOCATION_MZONE,1,nil)
+function s.tgfilter(c,tp)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE)
+		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsAttackBelow,c:GetAttack()),tp,0,LOCATION_MZONE,1,nil)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil) end
-	local tc=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	local tc=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,tp):GetFirst()
 	local dg=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsAttackBelow,tc:GetAttack()),tp,0,LOCATION_MZONE,nil)
 	dg:AddCard(tc)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,0,0)

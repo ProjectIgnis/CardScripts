c3eb3c9e8fb0869209b2683800c1390ab716040a
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-07-05 18:00:03 +0200

    Demonic Hermit Vessel Kachisuzaku fix

--- rush/c160006021.lua
+++ rush/c160006021.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.costfilter(c,e,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost() and c:IsAttribute(ATTRIBUTE_WATER)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToGraveAsCost() and c:IsLevelAbove(7)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
@@ -21,9 +21,11 @@ function s.filter(c)
 	return c:IsType(TYPE_NORMAL) and c:IsLevelBelow(6) and c:IsAbleToHand()
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	--Requirement
+	local c=e:GetHandler()
 	--requirement
-	local ct=Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST)
+	-- local ct=Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	local ct=Duel.SendtoGrave(g,REASON_COST)
 	if ct>0 then
 		--Effect
 		--atk change
@@ -33,12 +35,12 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(1000)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
-		if ct:GetFirst():IsRace(RACE_WARRIOR+RACE_WINGEDBEAST) then
+		if g:GetFirst():IsRace(RACE_WARRIOR+RACE_WINGEDBEAST) then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-			local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
-			if #g>0 then
-				Duel.SendtoHand(g,nil,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,g)
+			local g2=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+			if #g2>0 then
+				Duel.SendtoHand(g2,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,g2)
 			end
 		end
 	end

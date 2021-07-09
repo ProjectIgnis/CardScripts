1b2ba6af3e7ed91f08b0ad028f19afc725458228
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2021-07-08 21:38:34 -0700

    Update c513000126.lua
    Missing flageffectlabel caused effect to loop through group.

--- unofficial/c513000126.lua
+++ unofficial/c513000126.lua
@@ -26,65 +26,68 @@ function s.cointg(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
 end
 function s.coinop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-    local res=0
-    if c:IsHasEffect(73206827) then
-        res=1-Duel.SelectOption(tp,60,61)
-    else res=Duel.TossCoin(tp,1) end
-    s.arcanareg(c,res)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local res=0
+	if c:IsHasEffect(73206827) then
+		res=1-Duel.SelectOption(tp,60,61)
+	else 
+		res=Duel.TossCoin(tp,1) 
+	end
+	s.arcanareg(c,res)
 end
 function s.arcanareg(c,coin)
-    --coin effect
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_CONTROL)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-    e1:SetCode(EVENT_ADJUST)
-    e1:SetRange(LOCATION_MZONE) 
-    e1:SetOperation(s.ctop)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-    c:RegisterEffect(e1)
-    c:RegisterFlagEffect(36690018,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,coin,63-coin)
+	--coin effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_CONTROL)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_ADJUST)
+	e1:SetRange(LOCATION_MZONE) 
+	e1:SetOperation(s.ctop)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e1)
+	c:RegisterFlagEffect(36690018,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,coin,63-coin)
 	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,coin)
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    --heads effect
-    if c:GetFlagEffectLabel(36690018)==1 and c:GetFlagEffectLabel(id)==1 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
-        local g=Duel.SelectMatchingCard(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
-        local tc=g:GetFirst()
-        if tc then
-            Duel.HintSelection(g)
-            Duel.GetControl(tc,tp)
-        end
+	local c=e:GetHandler()
+    	--heads effect
+    	if c:GetFlagEffectLabel(36690018)==1 and c:GetFlagEffectLabel(id)==1 then
+		c:SetFlagEffectLabel(id,0)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+		local g=Duel.SelectMatchingCard(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
+		local tc=g:GetFirst()
+		if tc then
+			Duel.HintSelection(g)
+			Duel.GetControl(tc,tp)
+		end
 	end
-    --tails  effect
+	--tails  effect
 	if c:GetFlagEffectLabel(36690018)==0 and c:GetFlagEffectLabel(id)==0 then
 		c:SetFlagEffectLabel(id,1)
-        local g=Duel.GetMatchingGroup(Card.IsControlerCanBeChanged,tp,LOCATION_MZONE,0,c)
-        Duel.GetControl(g,1-tp)
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        e1:SetCode(EVENT_SUMMON_SUCCESS)
-        e1:SetRange(LOCATION_MZONE)
-        e1:SetTargetRange(LOCATION_MZONE,0)
-        e1:SetOperation(s.ctrlop)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-        c:RegisterEffect(e1)
-        local e2=e1:Clone()
-        e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-        c:RegisterEffect(e2)
-        local e3=e1:Clone()
-        e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-        c:RegisterEffect(e3)
-    end
+		local g=Duel.GetMatchingGroup(Card.IsControlerCanBeChanged,tp,LOCATION_MZONE,0,c)
+		Duel.GetControl(g,1-tp)
+        	local e1=Effect.CreateEffect(c)
+        	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        	e1:SetCode(EVENT_SUMMON_SUCCESS)
+        	e1:SetRange(LOCATION_MZONE)
+        	e1:SetTargetRange(LOCATION_MZONE,0)
+        	e1:SetOperation(s.ctrlop)
+        	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        	c:RegisterEffect(e1)
+        	local e2=e1:Clone()
+        	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+        	c:RegisterEffect(e2)
+        	local e3=e1:Clone()
+        	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+        	c:RegisterEffect(e3)
+	end
 end
 function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
-    local ft=Duel.GetLocationCount(1-tp,LOCATION_MZONE)
-    if ft>=0 then
-        Duel.GetControl(eg,1-tp)
-    else 
-        Duel.Destroy(eg,REASON_EFFECT)
-    end
+	local ft=Duel.GetLocationCount(1-tp,LOCATION_MZONE)
+    	if ft>=0 then
+        	Duel.GetControl(eg,1-tp)
+    	else 
+        	Duel.Destroy(eg,REASON_EFFECT)
+    	end
 end

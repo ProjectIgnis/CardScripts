357edc462a6c04393db6f3130438eed1f6d625fc
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-07-15 15:19:21 +0200

    added new scripts
    from Deck Modification Pack - Fierce Thunder Storm!!:
    - Betrayer of Peace
    
    from Saikyo Battle Pack
    - Saikyo Flag Beast Aim Eagle
    
    from Yu-Gi-Oh! Rush Duel Saikyo Battle Royale Winner's Spirit promotional card
    - Saikyo Flag Beast Surge Bicorn

---
+++ rush/c160415003.lua
@@ -0,0 +1,58 @@
+--最強旗獣エイムイーグル 
+--Saikyo Flag Beast Aim Eagle
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsFaceup() and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE,0,2,nil) end
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsLevelAbove(7)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,0,LOCATION_MZONE,1,nil)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_MACHINE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE,0,2,2,nil)
+	local ct=Duel.SendtoGrave(g,REASON_COST)
+	if ct>1 then
+		--Effect
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g2=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+		if #g2>0 then
+			Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
+			local tc=g2:GetFirst()
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e1:SetValue(1000)
+			tc:RegisterEffectRush(e1)
+		end
+	end
+end

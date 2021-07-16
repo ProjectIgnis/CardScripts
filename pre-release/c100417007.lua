fd215e789386fe4b894df91ed686e7f54ed0c433
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-07-16 09:52:54 +0200

    Fix for "Ukiyoe P.U.N.K. Carp Rising"
    The SS effect should also be usable, if the card is in the MMZ with no free zones.

--- pre-release/c100417007.lua
+++ pre-release/c100417007.lua
@@ -41,7 +41,7 @@ function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x26e) and not c:IsLevel(8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or not e:GetHandler():IsInExtraMZone(tp)
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end

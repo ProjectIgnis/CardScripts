82498a99388f71a71f21f8a60a70b0f5cd7fc394
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-07-18 23:31:25 +0200

    Fiendish Commander Current fix

--- rush/c160006042.lua
+++ rush/c160006042.lua
@@ -37,7 +37,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(tc:GetDefense())
+			e1:SetValue(g:GetFirst():GetBaseDefense())
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			g:GetFirst():RegisterEffectRush(e1)
 		end

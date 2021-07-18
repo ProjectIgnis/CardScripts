67152c66d4d800a19b8ec5c6c8fedaa5e13479ad
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-07-19 00:03:04 +0200

    bladesaurus string fix

--- rush/c160006037.lua
+++ rush/c160006037.lua
@@ -29,7 +29,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	c:RegisterEffect(e1)
 	--gy recover
 	local sg=Duel.GetMatchingGroup(s.sfilter,tp,LOCATION_GRAVE,0,nil)
-	if #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	if #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.BreakEffect()
 		local tg=aux.SelectUnselectGroup(sg,1,tp,1,ft,s.rescon,1,tp)
 		Duel.SendtoHand(tg,nil,REASON_EFFECT)

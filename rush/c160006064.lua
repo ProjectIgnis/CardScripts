d3f46a1e7d52cfb2784c38abaf7f9cadd744b15b
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-07-18 23:45:55 +0200

    Eternal Freeze fix

--- rush/c160006064.lua
+++ rush/c160006064.lua
@@ -7,9 +7,13 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.filter(c,tp,ep)
 	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsAttackAbove(2500)
@@ -21,8 +25,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetTargetCard(eg)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.SendtoDeck(tc,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+	local c=e:GetHandler()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,1,1,nil)
+	local ct=Duel.SendtoGrave(g,REASON_COST)
+	if ct>0 then
+		local tc=eg:GetFirst()
+		if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+			Duel.SendtoDeck(tc,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+		end
 	end
 end

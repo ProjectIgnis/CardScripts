eadf56e47764696e3af9a103215eb0a7c8eacc75
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-07-19 17:04:58 +0300

    "Tamtam the Melodious Diva" fix
    The target needs to lose exactly 500 ATK in order to deal 500 damage.

--- official/c79757784.lua
+++ official/c79757784.lua
@@ -1,4 +1,5 @@
 --幻奏の音女タムタム
+--Tamtam the Melodious Diva
 local s,id=GetID()
 function s.initial_effect(c)
 	--to hand
@@ -48,22 +49,20 @@ end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return r==REASON_FUSION and e:GetHandler():IsLocation(LOCATION_GRAVE)
 end
+function s.damfilter(c)
+	return s.cfilter(c) and c:IsAttackAbove(500)
+end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.cfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.damfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.damfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,s.damfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(-500)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsImmuneToEffect(e)
+		and tc:IsAttackAbove(500) and tc:UpdateAttack(-500,RESET_EVENT+RESETS_STANDARD,e:GetHandler())==-500 then
 		Duel.Damage(1-tp,500,REASON_EFFECT)
 	end
 end

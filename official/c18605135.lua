4667b6d500d1a5842abc49efb747b132bbd05507
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-07-19 17:27:41 +0300

    "Tornado Wall" fix
    Should prevent all battle damage to the player while "Umi" is on the field.

--- official/c18605135.lua
+++ official/c18605135.lua
@@ -1,11 +1,12 @@
 --竜巻海流壁
+--Tornado Wall
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(s.actcon)
+	e1:SetCondition(s.umicheck)
 	c:RegisterEffect(e1)
 	--avoid battle damage
 	local e2=Effect.CreateEffect(c)
@@ -14,7 +15,7 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetTargetRange(1,0)
-	e2:SetCondition(s.abdcon)
+	e2:SetCondition(s.umicheck)
 	c:RegisterEffect(e2)
 	--self destroy
 	local e3=Effect.CreateEffect(c)
@@ -22,17 +23,10 @@ function s.initial_effect(c)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCode(EFFECT_SELF_DESTROY)
-	e3:SetCondition(s.sdcon)
+	e3:SetCondition(aux.NOT(s.umicheck))
 	c:RegisterEffect(e3)
 end
 s.listed_names={CARD_UMI}
-function s.actcon(e,tp,eg,ep,ev,re,r,rp)
+function s.umicheck()
 	return Duel.IsEnvironment(CARD_UMI)
-end
-function s.abdcon(e)
-	local at=Duel.GetAttackTarget()
-	return Duel.IsEnvironment(CARD_UMI) and (at==nil or at:IsAttackPos() or Duel.GetAttacker():GetAttack()>at:GetDefense())
-end
-function s.sdcon(e)
-	return not Duel.IsEnvironment(CARD_UMI)
 end

88af078c495f8c0a4d75fb6c2c98b6f4f7effe4a
Hatter <sanity.sierota@protonmail.com>
2021-07-25 02:45:33 +0800

    Fix and update "Magicalibra"
    - level changes should only be applied until the end of turn
    - the player should select which card's level to reduce/increase during resolution, not activation
    - if one of the targets is no longer face-up on the field, the remaining target's level is reduced (if not Lv. 1)
    - style changes

--- official/c43857222.lua
+++ official/c43857222.lua
@@ -1,11 +1,11 @@
---ライブラの魔法秤
---Magical Scales of Libra
---scripted by Hatter
+-- ライブラの魔法秤
+-- Magicalibra
+-- Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	--Pendulum attributes
+	-- Pendulum properties
 	Pendulum.AddProcedure(c)
-	--Change levels
+	-- Change levels
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -15,37 +15,33 @@ function s.initial_effect(c)
 	e1:SetOperation(s.lvop)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,tp,lv)
-	return c:IsFaceup() and c:GetLevel()>lv and Duel.IsExistingTarget(aux.FilterFaceupFunction(Card.HasLevel),tp,LOCATION_MZONE,0,1,c)
+function s.lvfilter(c,e)
+	return c:IsFaceup() and c:HasLevel() and c:IsCanBeEffectTarget(e) 
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	local _,val=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil):GetMaxGroup(Card.GetLevel)
-	if not val or val<2 then return false end
-	local lv=math.min(val-1,6)
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,tp,lv) end
+	local g=Duel.GetMatchingGroup(s.lvfilter,tp,LOCATION_MZONE,0,nil,e)
+	local _,hlv=g:GetMaxGroup(Card.GetLevel)
+	if chk==0 then return #g>1 and hlv>1 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LVRANK)
-	local dlv=Duel.AnnounceLevel(tp,1,lv)
-	Duel.SetTargetParam(dlv)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-	local g1=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,tp,dlv)
-	e:SetLabelObject(g1:GetFirst())
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
-	local g2=Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.HasLevel),tp,LOCATION_MZONE,0,1,1,g1)
+	local lv=Duel.AnnounceLevel(tp,1,math.min(hlv-1,6))
+	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,function(sg)return sg:IsExists(Card.IsLevelAbove,1,nil,lv+1)end,1,tp,HINTMSG_FACEUP)
+	Duel.SetTargetCard(tg)
+	Duel.SetTargetParam(lv)
 end
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local tg=Duel.GetTargetCards(e):Filter(Card.IsFaceup,nil)
+	if not c:IsRelateToEffect(e) or #tg<1 then return end
 	local lv=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		local tc=e:GetLabelObject()
-		if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and tc:UpdateLevel(-lv,nil,c)~=0 then
-			local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-			g:RemoveCard(tc)
-			local tc2=g:GetFirst()
-			if tc2==tc then tc2=g:GetNext() end
-			if tc2 and tc2:IsFaceup() and tc2:IsRelateToEffect(e) then
-				tc2:UpdateLevel(lv,nil,c)
-			end
+	local reset=RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END
+	if #tg==1 then
+		tg:GetFirst():UpdateLevel(-lv,reset,c)
+	elseif #tg==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+		local rd=tg:FilterSelect(tp,Card.IsLevelAbove,1,1,nil,lv+1):GetFirst()
+		if rd and rd:UpdateLevel(-lv,reset,c)~=0 then
+			(tg-rd):GetFirst():UpdateLevel(lv,reset,c)
 		end
 	end
 end

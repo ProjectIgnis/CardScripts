e6edf0ee018aefe64534174b7ce0d077ff43816e
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2021-07-31 13:49:52 -0400

    Updated "Beetrooper Sting Lancer" Had an unnecessary check if your targeted monster was still an insect monster when resolving the effect. Also failed to shuffle the remaining target if one of them was moved elsewhere.

--- pre-release/c101105087.lua
+++ pre-release/c101105087.lua
@@ -2,7 +2,7 @@
 --Scripted by fiftyfour
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon
+	--Special Summon itself from hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--Search Beetrooper S/T
+	--Add 1 "Beetrooper" S/T from Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -59,8 +59,6 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetTargetCards(e)
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 and #g>0 then
-		local tc=g:Filter(Card.IsControler,nil,tp):GetFirst()
-		if not tc:IsRace(RACE_INSECT) then g:RemoveCard(tc) end
 		Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
 	end
 end

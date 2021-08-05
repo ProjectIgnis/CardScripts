9a7a9e39d991b1b5558f4cc070900a9372dcbd78
Edoardo Lolletti <edoardo762@gmail.com>
2021-08-05 17:45:52 +0200

    Fix Speed Spell - Takeover
    Wasn't properly gaining lp based on the counters on the destroyed card

--- unofficial/c511000978.lua
+++ unofficial/c511000978.lua
@@ -1,4 +1,5 @@
---ＳｐTakeover
+--Ｓｐ－テイク・オーバー
+--Speed Spell - Takeover
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -13,11 +14,18 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.con(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+	local tc=Duel.GetFieldCard(tp,LOCATION_FZONE,0)
 	return tc and tc:GetCounter(0x91)>3
 end
 function s.filter(c)
-	return c:IsFaceup() and c:GetCounter(0)~=0 and c:IsDestructable()
+	return c:IsFaceup() and c:HasCounters() and c:IsDestructable()
+end
+function s.countcounters(c)
+    local count=0
+    for counter,amount in pairs(c:GetAllCounters()) do
+        count=count+amount
+    end
+	return count
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and s.filter(chkc) end
@@ -25,46 +33,13 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,g:GetFirst():GetCounter(0)*100)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,s.countcounters(g:GetFirst())*100)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
-	local lp=tc:GetCounter(0)*100
-	if tc:GetCounter(0x3001)>0 then
-		lp=lp+(tc:GetCounter(0x3001)*100)
-	end
-	if tc:GetCounter(0x100)>0 then
-		lp=lp+(tc:GetCounter(0x100)*100)
-	end
-	if tc:GetCounter(0xe)>0 then
-		lp=lp+(tc:GetCounter(0xe)*100)
-	end
-	if tc:GetCounter(0x15)>0 then
-		lp=lp+(tc:GetCounter(0x15)*100)
-	end
-	if tc:GetCounter(0x19)>0 then
-		lp=lp+(tc:GetCounter(0x19)*100)
-	end
-	if tc:GetCounter(0xf)>0 then
-		lp=lp+(tc:GetCounter(0xf)*100)
-	end
-	if tc:GetCounter(0x1f)>0 then
-		lp=lp+(tc:GetCounter(0x1f)*100)
-	end
-	if tc:GetCounter(0x8)>0 then
-		lp=lp+(tc:GetCounter(0x8)*100)
-	end
-	if tc:GetCounter(0x10)>0 then
-		lp=lp+(tc:GetCounter(0x10)*100)
-	end
-	if tc:GetCounter(0x20)>0 then
-		lp=lp+(tc:GetCounter(0x20)*100)
-	end
-	if tc:GetCounter(0x99)>0 then
-		lp=lp+(tc:GetCounter(0x99)*100)
-	end
-	if Duel.Destroy(tc,REASON_EFFECT)>0 then
-		Duel.Recover(tp,lp,REASON_EFFECT)
-	end
+    local tc=Duel.GetFirstTarget()
+    if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
+	local lp=s.countcounters(tc)*100
+    if Duel.Destroy(tc,REASON_EFFECT)>0 then
+        Duel.Recover(tp,lp,REASON_EFFECT)
+    end
 end

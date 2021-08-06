5db3d38ef24b66cac8e6e97b8701dff7046b795d
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-08-06 13:52:58 +0200

    Ice Age Catapult and Thunder the lightning fix
    Added forgotten Yes/no for Ice age catapult
    fixed wrong registereffect for thunder the lightning

--- rush/c160001032.lua
+++ rush/c160001032.lua
@@ -37,7 +37,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(-300)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			g:GetFirst():RegisterEffect(e1)
+			g:GetFirst():RegisterEffectRush(e1)
 		end
 	end
 end

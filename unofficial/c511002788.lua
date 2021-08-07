e258920d4cf8881120f1c4d9a92eb4976bf04c00
Edoardo Lolletti <edoardo762@gmail.com>
2021-08-07 22:40:47 +0200

    Quiz Panel - Ra 30 minor cleanup

--- unofficial/c511002788.lua
+++ unofficial/c511002788.lua
@@ -16,12 +16,10 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local endtime=0
 	local check=true
 	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_DECK,0,1,3,nil)
-	local ct=Duel.GetMatchingGroupCount(s.filter,tp,0x13,0,nil,g)
+	local ct=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_DECK|LOCATION_HAND|LOCATION_GRAVE,0,nil,g)
 	local start=os.time()
 	Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(4006,8))
-	local tc=Duel.AnnounceNumber(1-tp,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,
-		44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,
-		93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120)
+	local tc=Duel.AnnounceNumberRange(1-tp,1,Duel.GetFieldGroupCount(tp,LOCATION_DECK|LOCATION_HAND|LOCATION_GRAVE,0))
 	endtime=os.time()-start
 	check=endtime<=10 and tc==ct
 	if e:GetHandler():IsRelateToEffect(e) then

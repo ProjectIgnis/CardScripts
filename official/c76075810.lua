079543d3cba0ebb15a70a778feca94a0cb9ae744
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-08-09 21:43:56 +0300

    "Throwstone Unit" updates

--- official/c76075810.lua
+++ official/c76075810.lua
@@ -2,7 +2,7 @@
 --Throwstone Unit
 local s,id=GetID()
 function s.initial_effect(c)
-	--destroy
+	--Destroy
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
@@ -12,26 +12,29 @@ function s.initial_effect(c)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 end
-function s.descfilter(c)
+function s.cfilter(c)
 	return c:IsRace(RACE_WARRIOR)
+end
+function s.desfilter(c,atk)
+	return c:IsFaceup() and c:IsDefenseBelow(atk)
 end
 function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c:GetAttack())
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.descfilter,1,false,aux.ReleaseCheckTarget,c,dg) end
+	local dg=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c:GetAttack())
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,aux.ReleaseCheckTarget,c,dg) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g=Duel.SelectReleaseGroupCost(tp,s.descfilter,1,1,false,aux.ReleaseCheckTarget,c,dg)
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,aux.ReleaseCheckTarget,c,dg)
 	Duel.Release(g,REASON_COST)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg-g,1,0,0)
 end
-function s.filter(c,atk)
-	return c:IsFaceup() and c:IsDefenseBelow(atk-1)
-end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local atk=c:GetAttack()
+	if not c:IsRelateToEffect(e) then atk=c:GetPreviousAttackOnField() end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,c:GetAttack()):GetFirst()
-	if tc then
-		Duel.Destroy(tc,REASON_EFFECT)
+	local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,atk)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		Duel.Destroy(g,REASON_EFFECT)
 	end
 end

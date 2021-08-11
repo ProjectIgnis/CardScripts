b1617c2d86214809d0bde7004250a59bf9410253
that-hatter <sanity.sierota@protonmail.com>
2021-08-11 21:54:51 +0800

    Add "Vendread Token" in listed names
    - Vendread Reorigin

--- official/c30650147.lua
+++ official/c30650147.lua
@@ -12,6 +12,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_names={30650148}
 s.listed_series={0x106}
 function s.filter(c,tp)
 	return c:IsFaceup() and c:GetOriginalLevel()>0 and c:IsReleasableByEffect()

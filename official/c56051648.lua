cf19da8985ae8d928bf589817bf88def8070eed8
that-hatter <sanity.sierota@protonmail.com>
2021-08-12 22:53:52 +0800

    Add "Speedroid Token" to listed names
    - Speedroid Block-n-Roll

--- official/c56051648.lua
+++ official/c56051648.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={56051649}
+s.listed_names={69550260}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer() and Duel.GetAttackTarget()==nil
 		and Duel.IsExistingMatchingCard(Card.IsRace,tp,LOCATION_GRAVE,0,3,nil,RACE_INSECT)

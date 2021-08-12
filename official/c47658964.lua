d6beab46ee253f3009c3d04b6c1746b0150797c7
that-hatter <sanity.sierota@protonmail.com>
2021-08-13 01:22:16 +0800

    Add "Laval Token" to listed names
    - Searing Fire Wall

--- official/c47658964.lua
+++ official/c47658964.lua
@@ -11,6 +11,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_names={47658965}
 s.listed_series={0x39}
 function s.cfilter(c)
 	return c:IsSetCard(0x39) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)

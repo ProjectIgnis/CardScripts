112addacfb07815c136a2fb245195fdabadffd46
that-hatter <sanity.sierota@protonmail.com>
2021-08-12 22:55:57 +0800

    Add "Soul Token" to listed names
    - Limit Impulse

--- official/c69550259.lua
+++ official/c69550259.lua
@@ -14,6 +14,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.tkop)
 	c:RegisterEffect(e1)
 end
+s.listed_names={69550260}
 function s.tkcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
 end

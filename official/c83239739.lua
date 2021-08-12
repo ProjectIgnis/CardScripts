c55fbd0835c6f6501e177c3abc22991d04bcff51
that-hatter <sanity.sierota@protonmail.com>
2021-08-13 00:25:32 +0800

    Add "Oyster Token" to listed names
    - Oyster Meister

--- official/c83239739.lua
+++ official/c83239739.lua
@@ -1,4 +1,5 @@
 --オイスターマイスター
+--Oyster Meister
 local s,id=GetID()
 function s.initial_effect(c)
 	--token
@@ -12,6 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
+s.listed_names={83239740}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetPreviousLocation()&LOCATION_ONFIELD>0
 		and not (r&REASON_BATTLE+REASON_DESTROY==REASON_BATTLE+REASON_DESTROY)

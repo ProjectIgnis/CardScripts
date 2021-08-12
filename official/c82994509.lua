7bbc7230a80a8c7a81c443ce168674c65afeba89
that-hatter <sanity.sierota@protonmail.com>
2021-08-13 02:11:08 +0800

    Add "Horseytail Token" to listed names
    - Horseytail

--- official/c82994509.lua
+++ official/c82994509.lua
@@ -1,4 +1,5 @@
 --ツクシー
+--Horseytail
 local s,id=GetID()
 function s.initial_effect(c)
 	--token
@@ -12,6 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
+s.listed_names={82994510}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
 end

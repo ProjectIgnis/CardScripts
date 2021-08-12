992880f4f92f51885b346c157207c7094f3b6989
that-hatter <sanity.sierota@protonmail.com>
2021-08-13 00:48:45 +0800

    Add "Noble Knight Token" to listed names
    - Noble Knight Iyvanne

--- official/c88923963.lua
+++ official/c88923963.lua
@@ -26,6 +26,7 @@ function s.initial_effect(c)
 	e2:SetValue(500)
 	c:RegisterEffect(e2)
 end
+s.listed_names={88923964}
 s.listed_series={0x107a,0x207a}
 function s.tkcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(Card.IsSetCard,1,nil,0x207a)

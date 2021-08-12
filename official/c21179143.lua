4f16e109f14e104405e798f998af655976c9fd0d
that-hatter <sanity.sierota@protonmail.com>
2021-08-12 23:55:26 +0800

    Add "Reptilianne Token" to listed names
    - Reptilianne Spawn
    - Reptilianne Recoil

--- official/c21179143.lua
+++ official/c21179143.lua
@@ -1,4 +1,5 @@
 --レプティレス・スポーン
+--Reptilianne Spawn
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -11,6 +12,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_names={TOKEN_REPTILIANNE}
 s.listed_series={0x3c}
 function s.cfilter(c)
 	return c:IsSetCard(0x3c) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
@@ -24,15 +26,15 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and (Duel.GetLocationCount(tp,LOCATION_MZONE)>1 
 		or (Duel.IsPlayerAffectedByEffect(tp,69832741) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0))
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x3c,TYPES_TOKEN,0,0,1,RACE_REPTILE,ATTRIBUTE_EARTH) end
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_REPTILIANNE,0x3c,TYPES_TOKEN,0,0,1,RACE_REPTILE,ATTRIBUTE_EARTH) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 
-		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x3c,TYPES_TOKEN,0,0,1,RACE_REPTILE,ATTRIBUTE_EARTH) then return end
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_REPTILIANNE,0x3c,TYPES_TOKEN,0,0,1,RACE_REPTILE,ATTRIBUTE_EARTH) then return end
 	for i=1,2 do
-		local token=Duel.CreateToken(tp,id+1)
+		local token=Duel.CreateToken(tp,TOKEN_REPTILIANNE)
 		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
 	end
 	Duel.SpecialSummonComplete()

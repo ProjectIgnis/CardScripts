b98dafadcd0cd1e9864a591836b28c7926db87b1
that-hatter <sanity.sierota@protonmail.com>
2021-08-13 03:16:51 +0800

    Add "Ceremonial Token" to listed names
    - Offering to the Immortals

--- official/c82340056.lua
+++ official/c82340056.lua
@@ -13,6 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_names={82340057}
 s.listed_series={0x21}
 
 function s.condition(e,tp,eg,ep,ev,re,r,rp)

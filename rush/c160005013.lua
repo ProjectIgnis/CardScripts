f09c94f0685240f0b1a56855b77a2c019662cad1
Hatter <sanity.sierota@protonmail.com>
2021-08-16 13:26:15 +0800

    Fix and update "Ice Age Catapult"
    - should ask the player if they want to use the atk/def decrease effect before selecting a card
    - fix mismatched string ID
    - style updates

--- rush/c160005013.lua
+++ rush/c160005013.lua
@@ -32,35 +32,32 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+	--Requirement
 	local tg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.HintSelection(tg)
-	if Duel.SendtoDeck(tg,nil,SEQ_DECKBOTTOM,REASON_COST)==1 then
-		--effect
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.rthfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
-		if #g>0 then
-			Duel.HintSelection(g)
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
+	if Duel.SendtoDeck(tg,nil,SEQ_DECKBOTTOM,REASON_COST)~=1 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.rthfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #g<1 then return end
+	Duel.HintSelection(g)
+	if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) 
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local g2=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+		if #g2>0 then
 			Duel.BreakEffect()
-			if Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,e:GetHandler()) then
-				local g2=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-				if #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-					Duel.HintSelection(g2)
-					local e1=Effect.CreateEffect(c)
-					e1:SetType(EFFECT_TYPE_SINGLE)
-					e1:SetCode(EFFECT_UPDATE_ATTACK)
-					e1:SetValue(-600)
-					e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-					g2:GetFirst():RegisterEffectRush(e1)
-					local e2=Effect.CreateEffect(c)
-					e2:SetType(EFFECT_TYPE_SINGLE)
-					e2:SetCode(EFFECT_UPDATE_DEFENSE)
-					e2:SetValue(-600)
-					e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-					g2:GetFirst():RegisterEffectRush(e2)
-				end
-			end
+			Duel.HintSelection(g2)
+			local c=e:GetHandler()
+			local tc=g2:GetFirst()
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-600)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffectRush(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			tc:RegisterEffectRush(e2)
 		end
 	end
 end

b5328bd0f1b32dcae464784b78e847cc6913ee9d
Edoardo Lolletti <edoardo762@gmail.com>
2021-08-21 19:12:53 +0200

    Typo in previous commit

--- official/c60800381.lua
+++ official/c60800381.lua
@@ -28,7 +28,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsLevelBelow,2),tp,LOCATION_MZONE,0,nil)
 	if #g>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
-		local atk=g::GetSum(Card.GetAttack)
+		local atk=g:GetSum(Card.GetAttack)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)

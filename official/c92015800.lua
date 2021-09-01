c21ba3d737c3ed0189d70ae7fed4defd7e92e4cc
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-09-01 13:10:16 +0300

    "Number 76: Harmonizer Gradielle" update
    Script polishing.

--- official/c92015800.lua
+++ official/c92015800.lua
@@ -1,12 +1,12 @@
 --Ｎｏ．７６ 諧調光師グラディエール
---Number 76: Gladiyell the Melody Lumaestra
+--Number 76: Harmonizer Gradielle
 --Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
+	--Xyz Summon
 	c:EnableReviveLimit()
 	Xyz.AddProcedure(c,nil,7,2)
-	--attribute
+	--Attribute change
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_ADD_ATTRIBUTE)
@@ -14,26 +14,24 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(s.attval)
 	c:RegisterEffect(e1)
-	--indes
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
+	--Cannot be destroyed
+	local e2=e1:Clone()
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e2:SetValue(s.btindes)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
+	local e3=e1:Clone()
 	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e3:SetValue(s.efindes)
 	c:RegisterEffect(e3)
-	--attach material
+	--Attach material
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetCountLimit(1,id)
 	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetHintTiming(0,TIMING_END_PHASE)
+	e4:SetCountLimit(1,id)
 	e4:SetTarget(s.mttg)
 	e4:SetOperation(s.mtop)
 	c:RegisterEffect(e4)
@@ -52,7 +50,7 @@ function s.btindes(e,c)
 end
 function s.efindes(e,re,rp)
 	local c=e:GetHandler()
-	return re:GetHandler():IsAttribute(c:GetAttribute()) and re:IsActivated() and rp~=c:GetControler()
+	return re:GetHandler():IsAttribute(c:GetAttribute()) and re:IsActivated() and rp==1-c:GetControler()
 end
 function s.mttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and chkc:IsType(TYPE_MONSTER) end

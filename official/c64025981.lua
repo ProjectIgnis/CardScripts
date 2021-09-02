daeab1d546636ced57593e80516752305f462a99
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-09-02 13:01:20 +0300

    "Primal Dragon the Primordial Dragon" fix
    Cannot use the last effect if the player cannot conduct their Battle Phase this turn.

--- official/c64025981.lua
+++ official/c64025981.lua
@@ -1,5 +1,5 @@
 --始原竜プライマル・ドラゴン
---Primal Dragon, the Primordial Dragon
+--Primal Dragon the Primordial Dragon
 --Logical Nonsense
 
 --Substitute ID
@@ -10,22 +10,24 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_NO_BATTLE_DAMAGE)
 	c:RegisterEffect(e1)
-	--Gains ATK/DEF equal to banished dragon monster's ATK
+	--Gains ATK/DEF equal to banished Dragon monster's ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetCountLimit(1,id)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
 	e2:SetCost(s.cost)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
-	--Targeted monster can make a second attack
+	--Targeted monster can make a 2nd attack
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_RELEASE)
 	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_RELEASE)
 	e3:SetCountLimit(1,id+1)
+	e3:SetCondition(s.atkcon)
 	e3:SetTarget(s.atktg)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
@@ -46,6 +48,7 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		--Increase ATK/DEF
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -56,6 +59,9 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetCode(EFFECT_UPDATE_DEFENSE)
 		c:RegisterEffect(e2)
 	end
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP() or Duel.IsBattlePhase()
 end
 	--Activation legality
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
@@ -68,14 +74,14 @@ end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
+		--Can make a 2nd attack
 		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetDescription(3201)
+		e1:SetDescription(aux.Stringid(id,2))
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_EXTRA_ATTACK)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		e1:SetValue(1)
 		tc:RegisterEffect(e1)
-		tc:RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,2))
 	end
 end

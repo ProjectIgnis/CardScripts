769873aa6282f826bcd371554c055ee96bdb978d
Edoardo Lolletti <edoardo762@gmail.com>
2021-09-02 21:44:19 +0200

    Update cards using alias

--- unofficial/c511030056.lua
+++ unofficial/c511030056.lua
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	--Special Summon if not co-linked
 	local e3=e2:Clone()
-	e3:SetCountLimit(1,c:Alias()+1)
+	e3:SetCountLimit(1,{c:Alias(),1})
 	e3:SetCondition(aux.NOT(s.spcon))
 	e3:SetLabel(0)
 	c:RegisterEffect(e3)

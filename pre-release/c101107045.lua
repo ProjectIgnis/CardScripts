9dd8e5beb6f79f5d7739a7e293d3b6a10b9c4996
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-09-11 13:13:48 +0300

    Added new cards' scripts

---
+++ pre-release/c101107045.lua
@@ -0,0 +1,77 @@
+--真血公ヴァンパイア
+--The Undead Vampire
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Xyz.AddProcedure(c,nil,8,2,nil,nil,99)
+	--lv change
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_XYZ_LEVEL)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.lvtg)
+	e1:SetValue(s.lvval)
+	c:RegisterEffect(e1)
+	--cannot be target
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.tgval)
+	c:RegisterEffect(e2)
+	--to grave
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_TOGRAVE+CATEGORY_DECKDES+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(s.gycost)
+	e3:SetTarget(s.gytg)
+	e3:SetOperation(s.gyop)
+	c:RegisterEffect(e3,false,REGISTER_FLAG_DETACH_XMAT)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e3)
+end
+function s.lvtg(e,c)
+	return c:IsLevelAbove(1) and c:GetOwner()~=e:GetHandlerPlayer()
+end
+function s.lvval(e,c,rc)
+	local lv=c:GetLevel()
+	if rc:IsCode(id) then return 8
+	else return lv end
+end
+function s.tgval(e,re,rp)
+	local rc=re:GetHandler()
+	local loc=Duel.GetChainInfo(0,CHAININFO_TRIGGERING_LOCATION)
+	return re:IsActiveType(TYPE_MONSTER) and loc==LOCATION_MZONE and not rc:IsSummonLocation(LOCATION_GRAVE)
+		and rc:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.gycost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,4) and Duel.IsPlayerCanDiscardDeck(1-tp,4) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,0,0,PLAYER_ALL,4)
+end
+function s.spfilter(c,e,tp)
+	return c:IsType(TYPE_MONSTER) and c:IsLocation(LOCATION_GRAVE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local gy1=Duel.DiscardDeck(tp,4,REASON_EFFECT)
+	local gy2=Duel.DiscardDeck(1-tp,4,REASON_EFFECT)
+	if gy1==0 and gy2==0 then return end
+	local og=Duel.GetOperatedGroup()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and og:IsExists(s.spfilter,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=og:FilterSelect(tp,s.spfilter,1,1,nil,e,tp)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

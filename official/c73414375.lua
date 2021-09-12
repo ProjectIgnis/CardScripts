44e93420278f932fae7860efa487811124138879
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-09-13 00:02:47 +0300

    "Dimension Jar" fix
    Does not target, also, the turn player chooses what to banish first.

--- official/c73414375.lua
+++ official/c73414375.lua
@@ -1,38 +1,38 @@
 --ディメンション・ポッド
+--Dimension Jar
 local s,id=GetID()
 function s.initial_effect(c)
-	--flip
+	--Banish
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+	if not GhostBelleTable then GhostBelleTable={} end
+	table.insert(GhostBelleTable,e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
 end
 function s.rmfilter(c,tp)
 	return c:IsAbleToRemove(tp) and aux.SpElimFilter(c)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	if chk==0 then return true end
-	local g=Group.CreateGroup()
-	if Duel.IsExistingTarget(s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil,1-tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-		local rg=Duel.SelectTarget(tp,s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,3,nil,1-tp)
-		g:Merge(rg)
-	end
-	if Duel.IsExistingTarget(s.rmfilter,1-tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil,1-tp) and Duel.SelectYesNo(1-tp,aux.Stringid(id,1)) then
-		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_REMOVE)
-		local rg=Duel.SelectTarget(1-tp,s.rmfilter,1-tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,3,nil,1-tp)
-		g:Merge(rg)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
-end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	if not g then return end
-	local rg=g:Filter(Card.IsRelateToEffect,nil,e)
-	Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+	local g=Group.CreateGroup()
+	local p=Duel.GetTurnPlayer()
+	if Duel.IsExistingTarget(s.rmfilter,p,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil,1-p) and Duel.SelectYesNo(p,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_REMOVE)
+		local rg=Duel.SelectMatchingCard(p,s.rmfilter,p,0,LOCATION_MZONE+LOCATION_GRAVE,1,3,nil,p)
+		Duel.HintSelection(rg,true)
+		g:Merge(rg)
+	end
+	if Duel.IsExistingTarget(s.rmfilter,1-p,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil,1-p) and Duel.SelectYesNo(1-p,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,1-p,HINTMSG_REMOVE)
+		local rg=Duel.SelectMatchingCard(1-p,s.rmfilter,1-p,0,LOCATION_MZONE+LOCATION_GRAVE,1,3,nil,1-p)
+		Duel.HintSelection(rg,true)
+		g:Merge(rg)
+	end
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 end

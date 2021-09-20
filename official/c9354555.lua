dcc52c2ba04df711154d90811b5bac44706e4c4d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-09-20 15:21:12 +0300

    "Photon Veil" update
    Improved the selection procedure for what cards to add to the hand.

--- official/c9354555.lua
+++ official/c9354555.lua
@@ -11,35 +11,34 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.filter1(c)
+function s.tdfilter(c)
 	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToDeck()
 end
-function s.filter2(c)
+function s.thfilter(c)
 	return c:IsLevelBelow(4) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_HAND,0,3,nil)
-		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_HAND,0,3,nil)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,3,tp,LOCATION_HAND)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
+function s.rescon(sg,e,tp,mg)
+	return #sg<2 or sg:GetClassCount(Card.GetCode)==1
+end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_HAND,0,nil)
-	if #g<3 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local dg=g:Select(tp,3,3,nil)
-	Duel.ConfirmCards(1-tp,dg)
-	Duel.SendtoDeck(dg,nil,2,REASON_EFFECT)
-	local sg=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil)
+	local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_HAND,0,3,3,nil)
+	if #g<3 then return end
+	Duel.ConfirmCards(1-tp,g)
+	if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)~=3
+		or not g:IsExists(Card.IsLocation,3,nil,LOCATION_DECK) then return end
+	local sg=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
 	if #sg==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local hg=sg:Select(tp,1,1,nil)
-	sg:Match(Card.IsCode,hg,hg:GetFirst():GetCode())
-	if #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local tg=sg:Select(tp,1,2,nil)
-		hg:Merge(tg)
+	local hg=aux.SelectUnselectGroup(sg,e,tp,1,3,s.rescon,1,tp,HINTMSG_ATOHAND)
+	if #hg>0 then
+		Duel.BreakEffect()
+		Duel.SendtoHand(hg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,hg)
 	end
-	Duel.SendtoHand(hg,nil,REASON_EFFECT)
-	Duel.ConfirmCards(1-tp,hg)
 end

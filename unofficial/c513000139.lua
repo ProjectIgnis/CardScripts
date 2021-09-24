4b807f34420456c7c63cd699cd72c34672696142
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-09-24 19:54:40 +0300

    "The Wicked Eraser (Manga)" fix
    Simplifying the script and fixing a script error.

--- unofficial/c513000139.lua
+++ unofficial/c513000139.lua
@@ -23,53 +23,41 @@ function s.initial_effect(c)
 	e5:SetDescription(aux.Stringid(id,0))
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e5:SetCode(EVENT_TO_GRAVE)
-	e5:SetTarget(s.erastg)
 	e5:SetOperation(s.erasop)
+	e5:SetLabel(0)
 	c:RegisterEffect(e5)
+	--"Divine Evolution (Manga)" check
 	local e6=Effect.CreateEffect(c)
 	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e6:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e6:SetCode(EVENT_ADJUST)
-	e6:SetRange(LOCATION_ALL&~LOCATION_GRAVE)
+	e6:SetCode(EVENT_LEAVE_FIELD_P)
+	e6:SetRange(LOCATION_MZONE)
 	e6:SetLabelObject(e5)
-	e6:SetOperation(s.op)
+	e6:SetOperation(s.divevo)
 	c:RegisterEffect(e6)
 end
 function s.adval(e,c)
 	return Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_ONFIELD)*1000
-end
-function s.erastg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,0,0)
 end
 function s.erasop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,tp,id)
 	Duel.Hint(HINT_CARD,1-tp,id)
 	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	local c=e:GetHandler()
-	local phr=e:GetLabel()
-	local hr=c:GetFlagEffectLabel(513000065)
-	if phr~=hr then
-		if phr then
-			c:ResetFlagEffect(513000065)
-			c:RegisterFlagEffect(513000065,0,0,0,phr)
-			Duel.SendtoGrave(g,REASON_EFFECT)
-			c:ResetFlagEffect(513000065)
-			if hr then
-				c:RegisterFlagEffect(513000065,0,0,0,hr)
-			end
-		else
-			c:ResetFlagEffect(513000065)
-			Duel.SendtoGrave(g,REASON_EFFECT)
-			if hr then
-				c:RegisterFlagEffect(513000065,0,0,0,hr)
-			end
-		end
-	else
+	local divine_evo_label=e:GetLabel()
+	if divine_evo_label==0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
+	elseif divine_evo_label==1 then
+		c:ResetFlagEffect(513000065)
+		c:RegisterFlagEffect(513000065,0,0,0,2)
+		Duel.SendtoGrave(g,REASON_EFFECT)
+		c:ResetFlagEffect(513000065)
+		e:SetLabel(0)
 	end
 end
-function s.op(e,tp,eg,ev,ep,re,r,rp)
-	e:GetLabelObject():SetLabel(e:GetHandler():GetFlagEffectLabel(513000065))
+function s.divevo(e,tp,eg,ev,ep,re,r,rp)
+	local divine_hierarchy=e:GetHandler():GetFlagEffectLabel(513000065)
+	if divine_hierarchy>1 then
+		e:GetLabelObject():SetLabel(1)
+	end
 end

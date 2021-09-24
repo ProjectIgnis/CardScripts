34c7be4ce2b5919039d3c62d70deb44599c6c973
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-09-24 13:12:10 +0200

    added new scripts
    - Necroman III
    - Masked Fiery Noodle Tokunin Nikumashi Gockboot
    - Art of Masked Fiery Noodle Drawing-Out-The-Rich-Tasty-Flavor Reversal

---
+++ rush/c160007038.lua
@@ -0,0 +1,43 @@
+-- 火麺特忍ニクマシゴックブート
+-- Masked Fiery Noodle Tokunin Nikumashi Gockboot
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	-- "Masked Fiery Noodle Jonin Kaedama Gockboot" + "Taste Inspector"
+	Fusion.AddProcMix(c,true,true,160003033,160007008)
+	-- Damage and draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.damtg)
+	e1:SetOperation(s.damop)
+	c:RegisterEffect(e1)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsStatus(STATUS_SPSUMMON_TURN) end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(1000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.setfilter(c)
+	return c:IsCode(160003046,160007051) and c:IsSSetable()
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	local sg=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_GRAVE,0,nil)
+    if Duel.Draw(tp,1,REASON_EFFECT)>0 and ft>0 and #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+    	local tg=aux.SelectUnselectGroup(sg,1,tp,1,ft,function(sg)return #sg==sg:GetClassCount(Card.GetCode)end,1,tp)
+    	if #tg>0 then
+			Duel.BreakEffect()
+			Duel.SSet(tp,tg)
+		end
+    end
+end
---
+++ rush/c160007051.lua
@@ -0,0 +1,39 @@
+-- 火麺ドローっと濃厚返しの術
+-- Art of Masked Fiery Noodle Drawing-Out-The-Rich-Tasty-Flavor Reversal
+-- Scripted bu Hatter
+local s,id=GetID()
+function s.initial_effect(c)	
+	-- Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.drcost)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+end
+function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+end
+function s.pyrofilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsRace(RACE_PYRO)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.pyrofilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	-- Requirement
+	local pg=Duel.GetMatchingGroup(s.pyrofilter,tp,LOCATION_GRAVE,0,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,#pg,e:GetHandler())
+	if #g<1 then return end
+	local dr=Duel.SendtoGrave(g,REASON_COST)
+	-- Effect
+	if dr>0 and Duel.Draw(tp,dr,REASON_EFFECT)>0
+		and Duel.IsExistingMatchingCard(s.pyrofilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end

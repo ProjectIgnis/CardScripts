b9129fe4dd9e877a8b8f22db03eaa5c8ef408c62
Larry126 <28985622+larry126@users.noreply.github.com>
2021-10-02 23:38:42 +0800

    Updated official scripts
    Updated:
    - Sacred Defense Barrier (Anime)
    - Deuce
    Removed:
    - Iron Chain Dragon (Anime)

--- unofficial/c511000129.lua
+++ unofficial/c511000129.lua
@@ -1,4 +1,5 @@
---Sacred Defense Barrier
+--聖地の守護結界 (Anime)
+--Sacred Defense Barrier (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,69 +7,62 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--remove
+	--Counter
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_COUNTER)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_FZONE)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCondition(s.condition)
 	e2:SetTarget(s.target)
-	e2:SetOperation(s.activate)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
+	--Destroy replace
 	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_COUNTER)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	e3:SetTarget(s.target)
-	e3:SetOperation(s.activate)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTarget(s.reptg)
+	e3:SetValue(s.repval)
 	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_COUNTER)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetCondition(s.condition)
-	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e4:SetTarget(s.target)
-	e4:SetOperation(s.activate)
-	c:RegisterEffect(e4)
+end
+function s.filter(c,p)
+	return c:IsFaceup() and c:IsRace(RACE_ROCK) and c:IsControler(p)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(s.filter,nil,tp)
-	if #g~=1 then return end
-	local tc=g:GetFirst()
-	e:SetLabelObject(tc)
-	return tc:IsRace(RACE_ROCK) and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
-end
-function s.filter(c,tp,ep)
-	return c:IsFaceup() and c:IsRace(RACE_ROCK)
+	return eg:IsExists(s.filter,1,nil,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=eg:GetFirst()
-	if chk==0 then return s.filter(tc,tp,ep) end
-	Duel.SetTargetCard(eg)
+	if chk==0 then return true end
+	Duel.SetTargetCard(eg:Filter(s.filter,nil,tp))
 	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x1096)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=eg:GetFirst()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRace(RACE_ROCK) then
-		tc:AddCounter(0x1096,1)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EFFECT_DESTROY_REPLACE)
-		e1:SetTarget(s.reptg)
-		e1:SetOperation(s.repop)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tg=Duel.GetTargetCards(e)
+	for tc in aux.Next(tg) do
+		if tc:IsFaceup() then
+			tc:AddCounter(0x1096,1)
+		end
 	end
 end
+function s.repfilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:GetCounter(0x1096)>0
+		and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetCounter(0x1096)>0 end
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) end
+	Duel.Hint(HINT_CARD,1-tp,id)
+	local g=eg:Filter(s.repfilter,nil,tp)
+	for tc in aux.Next(g) do
+		tc:RemoveCounter(tp,0x1096,1,REASON_EFFECT)
+	end
+	g:KeepAlive()
+	e:SetLabelObject(g)
 	return true
 end
-function s.repop(e,tp,eg,ep,ev,re,r,rp,chk)
-	e:GetHandler():RemoveCounter(tp,0x1096,1,REASON_EFFECT)
+function s.repval(e,c)
+	return e:GetLabelObject():IsContains(c)
 end
+
--- unofficial/c511001004.lua
+++ unofficial/c511001004.lua
@@ -1,11 +1,13 @@
+--デュース
 --Deuce
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--cannot attack
 	local e2=Effect.CreateEffect(c)
@@ -17,7 +19,6 @@ function s.initial_effect(c)
 	e2:SetCondition(s.atkcon)
 	e2:SetTarget(s.atktg)
 	c:RegisterEffect(e2)
-	--check
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -26,81 +27,62 @@ function s.initial_effect(c)
 	e3:SetOperation(s.checkop)
 	e3:SetLabelObject(e2)
 	c:RegisterEffect(e3)
+	--win
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetCode(EFFECT_CHANGE_DAMAGE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_DAMAGE)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetTargetRange(1,0)
-	e4:SetValue(s.damval1)
+	e4:SetOperation(s.winchk)
 	c:RegisterEffect(e4)
 	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_FIELD)
-	e5:SetCode(EFFECT_CHANGE_DAMAGE)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e5:SetCode(EVENT_ADJUST)
 	e5:SetRange(LOCATION_SZONE)
-	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
-	e5:SetTargetRange(0,1)
-	e5:SetValue(s.damval2)
+	e5:SetOperation(s.winop)
 	c:RegisterEffect(e5)
-	--win
+	--cannot lose
 	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e6:SetCode(EVENT_ADJUST)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetCode(EFFECT_CANNOT_LOSE_LP)
 	e6:SetRange(LOCATION_SZONE)
-	e6:SetOperation(s.winop)
+	e6:SetTargetRange(1,1)
+	e6:SetValue(1)
 	c:RegisterEffect(e6)
-	local e7=e6:Clone()
-	e7:SetCode(EVENT_CHAIN_SOLVED)
-	c:RegisterEffect(e7)
-	local e8=e6:Clone()
-	e8:SetCode(EVENT_DAMAGE_STEP_END)
-	c:RegisterEffect(e8)
-	--
-	local e9=Effect.CreateEffect(c)
-	e9:SetType(EFFECT_TYPE_FIELD)
-	e9:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e9:SetCode(EFFECT_CANNOT_LOSE_LP)
-	e9:SetRange(LOCATION_SZONE)
-	e9:SetTargetRange(1,0)
-	e9:SetValue(1)
-	c:RegisterEffect(e9)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetLP(tp)==1000 and Duel.GetLP(1-tp)==1000
 end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ResetFlagEffect(0,id)
+	Duel.ResetFlagEffect(1,id)
+end
 function s.atkcon(e)
-	return e:GetHandler():GetFlagEffect(30606547)~=0
+	return e:GetHandler():GetFlagEffect(id)~=0
 end
 function s.atktg(e,c)
 	return c:GetFieldID()~=e:GetLabel()
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():GetFlagEffect(30606547)~=0 then return end
+	if e:GetHandler():GetFlagEffect(id)~=0 then return end
 	local fid=eg:GetFirst():GetFieldID()
-	e:GetHandler():RegisterFlagEffect(30606547,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 	e:GetLabelObject():SetLabel(fid)
 end
-function s.damval1(e,re,val,r,rp,rc)
-	local tp=e:GetHandler():GetControler()
-	if val~=0 then
-		Duel.RegisterFlagEffect(1-tp,id,RESET_PHASE+PHASE_END,0,1)
-		return 0
-	else return val end
-end
-function s.damval2(e,re,val,r,rp,rc)
-	local tp=e:GetHandler():GetControler()
-	if val~=0 then
-		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-		return 0
-	else return val end
+function s.winchk(e,tp,eg,ep,ev,re,r,rp)
+	if ep~=rp then
+		Duel.RegisterFlagEffect(rp,id,0,0,0)
+		Duel.ResetFlagEffect(ep,id)
+	end
 end
 function s.winop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,id)>1 and Duel.GetFlagEffect(1-tp,id)>1 then
+	if Duel.GetFlagEffect(0,id)>1 and Duel.GetFlagEffect(1,id)>1 then
 		Duel.Win(PLAYER_NONE,WIN_REASON_DEUCE)
-	elseif Duel.GetFlagEffect(tp,id)>1 then
-		Duel.Win(tp,WIN_REASON_DEUCE)
-	elseif Duel.GetFlagEffect(1-tp,id)>1 then
-		Duel.Win(1-tp,WIN_REASON_DEUCE)
+	elseif Duel.GetFlagEffect(0,id)>1 then
+		Duel.Win(0,WIN_REASON_DEUCE)
+	elseif Duel.GetFlagEffect(1,id)>1 then
+		Duel.Win(1,WIN_REASON_DEUCE)
 	end
 end

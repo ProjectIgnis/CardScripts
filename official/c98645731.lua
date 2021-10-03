3459486e51af64c1ba59af6a5b2d0d1167debf5a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-03 22:27:16 +0300

    "Pot of Duality" & "Gateway to Dark World" small update
    Moved the restriction hint applied on the player to the same effect that applies the restriction rather than creating a new one.

--- official/c98645731.lua
+++ official/c98645731.lua
@@ -1,4 +1,5 @@
 --強欲で謙虚な壺
+--Pot of Duality
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -43,20 +44,15 @@ function s.initial_effect(c)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetActivityCount(tp,ACTIVITY_SPSUMMON)==0 and not s[tp] end
-	--oath effects
+	--Cannot Special Summon
 	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
 	Duel.RegisterEffect(e1,tp)
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT+EFFECT_FLAG_OATH)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	e2:SetTargetRange(1,0)
-	Duel.RegisterEffect(e2,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then 

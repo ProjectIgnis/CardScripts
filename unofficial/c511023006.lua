f1925d76af90b67f30fb67eb97767a1dccdd7c4a
Rundas <38040866+Rundas01@users.noreply.github.com>
2021-10-06 15:02:02 +0200

    Updated Anime Neos Fusions to work with Neos Fusion

--- unofficial/c511023006.lua
+++ unofficial/c511023006.lua
@@ -8,30 +8,15 @@ function s.initial_effect(c)
 	Fusion.AddProcMix(c,true,true,CARD_NEOS,89621922,80344569)
 	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
 	--return
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_TODECK)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetCondition(s.retcon1)
-	e3:SetTarget(s.rettg)
-	e3:SetOperation(s.retop)
-	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(0)
-	e4:SetCondition(s.retcon2)
-	c:RegisterEffect(e4)
+	aux.EnableNeosReturn(c,nil,nil,nil)
 	--atkup
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE)
-	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e5:SetCode(EFFECT_UPDATE_ATTACK)
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetValue(s.atkval)
-	c:RegisterEffect(e5)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
 end
 s.listed_names={CARD_NEOS,89621922,80344569,CARD_POLYMERIZATION}
 s.material_setcode=0x8
@@ -47,19 +32,4 @@ function s.contactop(g,tp)
 end
 function s.atkval(e,c)
 	return Duel.GetFieldGroupCount(0,LOCATION_ONFIELD,LOCATION_ONFIELD)*400
-end
-function s.retcon1(e,tp,eg,ep,ev,re,r,rp)
-	return not e:GetHandler():IsHasEffect(42015635)
-end
-function s.retcon2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsHasEffect(42015635)
-end
-function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToExtra() end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
-end
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-	Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
 end

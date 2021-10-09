33711282297c8ac7e91fc35f25ee05b08f8c16a2
Hatter <sanity.sierota@protonmail.com>
2021-10-10 01:29:22 +0800

    Fix "Lightning Storm"
    typo from previous cleanup

--- official/c14532163.lua
+++ official/c14532163.lua
@@ -21,7 +21,7 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g1=Duel.GetMatchingGroup(Card.IsAttackPos,tp,0,LOCATION_MZONE,nil)
 	local g2=Duel.GetMatchingGroup(Card.IsType,tp,0,LOCATION_ONFIELD,nil,TYPE_SPELL+TYPE_TRAP)
 	local b1=#g1>0
-	local b2=#g1>0
+	local b2=#g2>0
 	if chk==0 then return b1 or b2 end
 	local op=aux.SelectEffect(tp,
 		{b1,aux.Stringid(id,0)},

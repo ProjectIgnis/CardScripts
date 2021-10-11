8b30042109f0392628383f6f176f05d2db581eb7
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-12 02:44:20 +0300

    "Blue-Eyes Ultimate Dragon" update
    Treated as listing "Blue-Eyes White Dragon" in its text.

--- official/c23995346.lua
+++ official/c23995346.lua
@@ -1,8 +1,10 @@
 --青眼の究極竜
+--Blue-Eyes Ultimate Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-	--fusion material
+	--Fusion Procedure
 	c:EnableReviveLimit()
 	Fusion.AddProcMixN(c,true,true,CARD_BLUEEYES_W_DRAGON,3)
 end
 s.material_setcode=0xdd
+s.listed_name={CARD_BLUEEYES_W_DRAGON}

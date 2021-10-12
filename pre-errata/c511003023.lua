28bc9e3128913b4e3eb070df339f00b74f40e47a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-12 13:55:14 +0300

    "Ultimate Offering (Pre-Errata)" update
    Use the new Normal Summon/Set function instead of the filter as well.

--- pre-errata/c511003023.lua
+++ pre-errata/c511003023.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--instant
+	--Normal Summon/Set
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(80604091,0))
 	e3:SetCategory(CATEGORY_SUMMON)
@@ -19,20 +19,17 @@ function s.initial_effect(c)
 	e3:SetOperation(s.activate)
 	c:RegisterEffect(e3)
 end
-function s.filter(c)
-	return c:IsSummonable(true,nil) or c:IsMSetable(true,nil)
-end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,500) end
 	if e:GetHandler():GetFlagEffect(id)==0 then
-		e:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1,Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil))
+		e:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1,Duel.GetMatchingGroupCount(Card.CanSummonOrSet,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,true,nil))
 	end
 	Duel.PayLPCost(tp,500)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		if e:GetHandler():GetFlagEffect(id)==0 then
-			return Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)>0
+			return Duel.GetMatchingGroupCount(Card.CanSummonOrSet,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,true,nil)>0
 		else return e:GetHandler():GetFlagEffectLabel(id)>0 end
 	end
 	e:GetHandler():SetFlagEffectLabel(id,e:GetHandler():GetFlagEffectLabel(id)-1)
@@ -41,7 +38,7 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,Card.CanSummonOrSet,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil,true,nil):GetFirst()
 	if tc then
 		Duel.SummonOrSet(tp,tc,true,nil)
 	end

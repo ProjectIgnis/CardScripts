4cfae5a9aee8036019c7537233e1755d499c4d28
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-12 03:06:37 +0300

    "Blue-Eyes Jet Dragon" fix
    Typo in the listed names table.

--- pre-release/c101107004.lua
+++ pre-release/c101107004.lua
@@ -38,7 +38,7 @@ function s.initial_effect(c)
 	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
-s.listed_name={CARD_BLUEEYES_W_DRAGON}
+s.listed_names={CARD_BLUEEYES_W_DRAGON}
 function s.filter(c)
 	return c:IsCode(CARD_BLUEEYES_W_DRAGON) and (c:IsFaceup() and c:IsOnField() or c:IsLocation(LOCATION_GRAVE))
 end

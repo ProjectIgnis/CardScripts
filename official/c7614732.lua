e9497682fd04337b6fe9da35dea337229c01f3ba
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-13 14:14:45 +0300

    "Greater Polymerization" fix
    The granted effects should belong to the Fusion Monster, not to "Greater Polymerization".

--- official/c7614732.lua
+++ official/c7614732.lua
@@ -1,3 +1,4 @@
+--大融合
 --Greater Polymerization
 --Scripted by edo9300
 local s,id=GetID()
@@ -6,15 +7,15 @@ function s.initial_effect(c)
 end
 function s.stage2(e,tc,tp,sg,chk)
 	if chk==0 then
-		--piercing
-		local e1=Effect.CreateEffect(e:GetHandler())
+		--Piercing
+		local e1=Effect.CreateEffect(tc)
 		e1:SetDescription(3208)
 		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_PIERCE)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1,true)
-		--Cannot be destroyed by card effect
+		--Cannot be destroyed by card effects
 		local e2=e1:Clone()
 		e2:SetDescription(3001)
 		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)

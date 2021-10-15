4ae6ca9cabaad91df32d909b0c56c12d87810de8
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-10-15 16:51:28 +0200

    Back beat fix
    fixed the shuffle location

--- rush/c160007053.lua
+++ rush/c160007053.lua
@@ -7,9 +7,13 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,2,nil,tp) end
 end
 function s.tdfilter(c,tp)
 	return c:IsAbleToDeck() and c:IsType(TYPE_MONSTER) and Duel.IsExistingMatchingCard(s.tdfilter2,tp,LOCATION_GRAVE,0,1,c,c:GetRace())
@@ -22,7 +26,7 @@ function s.cfilter(c,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return 
-		 Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
+		 Duel.IsExistingMatchingCard(s.tdfilter,tp,0,LOCATION_GRAVE,1,nil,tp)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,2,tp,LOCATION_GRAVE)
 end
@@ -34,7 +38,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.HintSelection(g)
 	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)>0 then
 		-- effect
-		local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE,0,nil,tp)
+		local g=Duel.GetMatchingGroup(s.tdfilter,tp,0,LOCATION_GRAVE,nil,tp)
 		local sg=aux.SelectUnselectGroup(g,e,tp,2,7,s.rescon,1,tp,HINTMSG_TODECK)
 		if sg and #sg>0 then
 			if Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>3 then

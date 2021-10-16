36b4e2a4c82e30d535cdb674f124136e3706f95c
Larry126 <28985622+larry126@users.noreply.github.com>
2021-10-16 20:43:22 +0800

    Fix anime Timelord related bugs

--- unofficial/c513000047.lua
+++ unofficial/c513000047.lua
@@ -29,6 +29,14 @@ function s.initial_effect(c)
 	e3:SetTargetRange(LOCATION_MZONE,0)
 	e3:SetValue(0)
 	c:RegisterEffect(e3)
+	--
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(id)
+	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x4a))
+	c:RegisterEffect(e4)
 end
 s.listed_names={9409625}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
--- unofficial/c513000048.lua
+++ unofficial/c513000048.lua
@@ -23,5 +23,5 @@ end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	return Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	return Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end

9a824a23cc23344108df6339a72ec0a51fd96cdf
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-16 13:23:33 +0300

    "Inion Cradel the Icejade Depths" fix
    According to rulings the opponent does not need to control a monster to activate its ATK reducing effect.

--- pre-release/c101107052.lua
+++ pre-release/c101107052.lua
@@ -47,8 +47,7 @@ function s.atkfilter(c)
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.atkfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.atkfilter,tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(s.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,s.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end

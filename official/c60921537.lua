98c1bfa3e571d8b3346b3e0f583a3c5cb3b00d44
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-17 19:08:42 +0300

    Update and rename pre-release/c101107054.lua to official/c60921537.lua

--- official/c60921537.lua
+++ official/c60921537.lua
@@ -14,11 +14,11 @@ function s.initial_effect(c)
 	e1:SetCategory(e1:GetCategory()+CATEGORY_TOGRAVE)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
-	-- Negatable by "Ghost Belle and Haunted Mansion"
+	-- Negatable by "Ghost Belle & Haunted Mansion"
 	if not GhostBelleTable then GhostBelleTable={} end
 	table.insert(GhostBelleTable,e1)
 end
-s.listed_names={40352445,101107035}
+s.listed_names={40352445,48654323}
 s.listed_series={0x146}
 function s.matfilter(c)
 	return c:HasLevel() and c:IsType(TYPE_FUSION|TYPE_SYNCHRO) and c:IsAbleToRemove()
@@ -31,7 +31,7 @@ function s.onfield(code)
 	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,code),0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
 end
 function s.stage2(mat,e,tp,eg,ep,ev,re,r,rp,tc)
-	if not s.onfield(40352445) or not s.onfield(101107035) then return end
+	if not (s.onfield(40352445) and s.onfield(48654323)) then return end
 	local g1=Duel.GetFieldGroup(tp,LOCATION_EXTRA,0)
 	local g2=Duel.GetFieldGroup(tp,0,LOCATION_EXTRA)
 	if (#g1<1 and #g2<1) or not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end

a47b2faf71a2a6413b03ee61aa7d6008b1f50fac
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-20 01:21:13 +0300

    "Monster Reborn Reborn" update
    Shouldn't banish if it doesn't Special Summon successfully, also general polishing.

--- official/c50213848.lua
+++ official/c50213848.lua
@@ -1,14 +1,14 @@
 --取捨蘇生
---Selective Reborn
+--Monster Reborn Reborn
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -23,15 +23,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,3,3,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,2,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	if #g==0 or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
-	local tc=g:FilterSelect(1-tp,s.filter,1,1,nil,e,tp):GetFirst()
-	if tc then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-		g:RemoveCard(tc)
-		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	local sg=g:FilterSelect(1-tp,s.filter,1,1,nil,e,tp)
+	if #sg>0 and Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.Remove(g-sg,POS_FACEUP,REASON_EFFECT)
 	end
 end

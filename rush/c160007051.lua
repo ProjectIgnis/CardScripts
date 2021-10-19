8035ad5a0f6128f1bb1b09cb67019fc56396e0c2
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-10-19 19:26:34 +0200

    Art of Masked Fiery Noodle Drawing-Out-The-Rich-Tasty-Flavor Reversal fix

--- rush/c160007051.lua
+++ rush/c160007051.lua
@@ -16,22 +16,25 @@ end
 function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,e:GetHandler()) end
 end
-function s.pyrofilter(c)
+function s.filter(c)
 	return c:IsType(TYPE_NORMAL) and c:IsRace(RACE_PYRO)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.pyrofilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.filter2(c)
+	return c:IsType(TYPE_NORMAL) and c:IsRace(RACE_PYRO)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	-- Requirement
-	local pg=Duel.GetMatchingGroup(s.pyrofilter,tp,LOCATION_GRAVE,0,nil)
+	local pg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,#pg,e:GetHandler())
 	if #g<1 then return end
 	local dr=Duel.SendtoGrave(g,REASON_COST)
 	-- Effect
 	if dr>0 and Duel.Draw(tp,dr,REASON_EFFECT)>0
-		and Duel.IsExistingMatchingCard(s.pyrofilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil)
 		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)

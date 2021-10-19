47aab992553fff8f5f7a686d2a88ecd249a7f6ff
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-10-19 19:54:13 +0200

    Card Devastation fix
    added missing maximum mode handling

--- rush/c160007055.lua
+++ rush/c160007055.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,LOCATION_MZONE,0,1,nil)
 		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_HAND,LOCATION_HAND,1,e:GetHandler())
 	end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,PLAYER_ALL,1)
@@ -22,7 +22,7 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,LOCATION_HAND)
 	if Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)<1 or #g:Match(Card.IsLocation,nil,LOCATION_GRAVE)<1 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
-	local tc=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
 	if tc then
 		Duel.HintSelection(tc,true)
 		Duel.BreakEffect()

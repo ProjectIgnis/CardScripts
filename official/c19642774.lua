34bef7c0bfc51df9c4e662e12776ab08b2440ca4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-10-21 20:15:12 +0300

    "Fleur Synchron" fix
    Shouldn't miss the timing.

--- official/c19642774.lua
+++ official/c19642774.lua
@@ -1,10 +1,12 @@
 --フルール・シンクロン
+--Fleur Synchron
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon
+	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BE_MATERIAL)
 	e1:SetCondition(s.con)

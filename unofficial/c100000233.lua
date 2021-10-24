cc8f42e61fb084226e28480137bcab295097a2b5
Larry126 <28985622+larry126@users.noreply.github.com>
2021-10-24 23:09:13 +0800

    Update "Express Train Trolley Olley (Anime)"
    Use Zoodiac-like handling

--- unofficial/c100000233.lua
+++ unofficial/c100000233.lua
@@ -1,33 +1,11 @@
---豪腕特急トロッコロッコ
+--豪腕特急トロッコロッコ (Anime)
+--Express Train Trolley Olley (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--effect gain
+	--ATK
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_BE_MATERIAL)
-	e1:SetCondition(s.efcon)
-	e1:SetOperation(s.efop)
-	c:RegisterEffect(e1)
-end
-function s.efcon(e,tp,eg,ep,ev,re,r,rp)
-	return r==REASON_XYZ
-end
-function s.efop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local rc=c:GetReasonCard()
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetType(EFFECT_TYPE_XMATERIAL)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(800)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-	rc:RegisterEffect(e1)
-	if not rc:IsType(TYPE_EFFECT) then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_CHANGE_TYPE)
-		e2:SetValue(TYPE_MONSTER+TYPE_EFFECT+TYPE_XYZ)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		rc:RegisterEffect(e2)
-	end
+	c:RegisterEffect(e1)
 end

4ecf0429b9b8b4468a93037a3364a400c51f1e99
Hatter <sanity.sierota@protonmail.com>
2021-10-25 01:53:34 +0800

    Fix "Card Devastation"
    the ATK gain should be optional

--- rush/c160007055.lua
+++ rush/c160007055.lua
@@ -13,14 +13,15 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,LOCATION_MZONE,0,1,nil)
-		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_HAND,LOCATION_HAND,1,e:GetHandler())
-	end
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_HAND,LOCATION_HAND,1,e:GetHandler()) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,PLAYER_ALL,1)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,LOCATION_HAND)
-	if Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)<1 or #g:Match(Card.IsLocation,nil,LOCATION_GRAVE)<1 then return end
+	if Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)<1
+		or #g:Match(Card.IsLocation,nil,LOCATION_GRAVE)<1 
+		or not Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,LOCATION_MZONE,0,1,nil) 
+		or not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
 	local tc=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
 	if tc then

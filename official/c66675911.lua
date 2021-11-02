a03ce6ec7f2411d61deb1903e908787e8a6e90a5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-11-02 12:48:50 +0200

    "Nehshaddoll Genius" fix
    Should not be able to target an opponent's "Shaddoll" monster with its first effect. Also, the second effect should not be applied if the target is face-down when the effect resolves.

--- official/c66675911.lua
+++ official/c66675911.lua
@@ -1,5 +1,5 @@
 --星なる影 ゲニウス
---Nephshaddoll Genius
+--Nehshaddoll Genius
 --Logical Nonsense
 
 --Substitute ID
@@ -35,10 +35,10 @@ function s.immfilter(c)
 end
 	--Activation legality
 function s.immtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.immfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.immfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.immfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.immfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.immfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,s.immfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 	--Make your "Shaddoll" monster becomes unaffected by monster effects
 function s.immop(e,tp,eg,ep,ev,re,r,rp)
@@ -69,15 +69,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:IsType(TYPE_EFFECT) end
 	if chk==0 then return Duel.IsExistingTarget(aux.FilterFaceupFunction(Card.IsType,TYPE_EFFECT),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.IsType,TYPE_EFFECT),tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.IsType,TYPE_EFFECT),tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 	--Targeted monster cannot activate its effects
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsType(TYPE_EFFECT) then
 		--Cannot activate its effects this turn
-		local e1=Effect.CreateEffect(c)
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetDescription(3302)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)

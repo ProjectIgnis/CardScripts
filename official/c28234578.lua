5b6a7420394bfc9ab6611b2d1bf9955dcea0f3c0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-11-21 01:57:50 +0200

    "Spiritual Entanglement" fix
    Small typo caused a script error when interacting with target redirection effects.

--- official/c28234578.lua
+++ official/c28234578.lua
@@ -1,5 +1,5 @@
 --霊子もつれ
---Spiritual Energy Entanglement
+--Spiritual Entanglement
 --scripted by Logical Nonsense
 --Substitute ID
 local s,id=GetID()
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 end
 	--Activation legality
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chck:IsFaceup() and chkc:IsAbleToRemove() end
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:IsAbleToRemove() end
 	if chk==0 then return Duel.IsExistingTarget(aux.FilterFaceupFunction(Card.IsAbleToRemove),tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.IsAbleToRemove),tp,0,LOCATION_MZONE,1,1,nil)

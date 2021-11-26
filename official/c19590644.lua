f4d50a2c17494bb0f4d18747942376051898b3d3
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-11-26 16:50:42 +0200

    "Setuppercut" fix
    Should check for the ATK to be equal to or less, not just less. Also, shouldn't be able to activate it if a monster with "?" ATK is destroyed + now reveals the Special Summoned monster.

--- official/c19590644.lua
+++ official/c19590644.lua
@@ -1,5 +1,5 @@
 --セットアッパー
---Set-Upper
+--Setuppercut
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
@@ -14,11 +14,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.cfilter(c,e,tp)
-	return c:IsReason(REASON_BATTLE) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp)
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp,c:GetAttack())
+	return c:IsAttackAbove(0) and c:IsReason(REASON_BATTLE) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp,c:GetAttack())
 end
-function s.filter(c,e,tp,atk)
-	return c:GetAttack()<atk and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+function s.spfilter(c,e,tp,atk)
+	return c:IsAttackBelow(atk) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -30,8 +30,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ec=eg:Filter(s.cfilter,nil,e,tp):GetFirst()
 	if not ec then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp,ec:GetAttack())
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp,ec:GetAttack())
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)>0 then
+		Duel.ConfirmCards(1-tp,g)
 	end
 end

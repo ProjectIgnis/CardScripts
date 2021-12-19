f9f7208a659d8a84f8f69b2b86073f5e27375001
Hatter <sanity.sierota@protonmail.com>
2021-12-20 01:50:41 +0800

    fix "Handsray Destleo"
    should be able to discard a minimum of 1 card

--- rush/c160402012.lua
+++ rush/c160402012.lua
@@ -16,12 +16,15 @@ function s.initial_effect(c)
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,1-tp,LOCATION_HAND)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,1-tp,LOCATION_HAND)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
-	if #g<2 then return end
-	local sg=g:RandomSelect(tp,2)
-	if #sg<1 then return end
-	Duel.SendtoGrave(sg,REASON_EFFECT)
+	if #g<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local ct=#g==1 and 1 or Duel.AnnounceNumberRange(tp,1,2)
+	local sg=g:RandomSelect(tp,ct)
+	if #sg>0 then
+		Duel.SendtoGrave(sg,REASON_EFFECT)
+	end
 end

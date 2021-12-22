918af5e839e7eb7480cc25ac6e48d9fd77a362e7
Hatter <sanity.sierota@protonmail.com>
2021-12-23 03:26:26 +0800

    Dragunity Oubliette

--- official/c51849216.lua
+++ official/c51849216.lua
@@ -1,5 +1,5 @@
 --ドラグニティ・ヴォイド
---Dragunity Purgatory
+--Dragunity Oubliette
 --scripted by pyrQ
 local s,id=GetID()
 function s.initial_effect(c)
@@ -23,12 +23,13 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsChainNegatable(ev) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return re:GetHandler():IsAbleToRemove() end
+	local rc=re:GetHandler()
+	if chk==0 then return Duel.IsPlayerCanRemove(tp,rc) end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,re:GetHandler():GetLocation())
+	if rc:IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,rc,1,0,rc:GetLocation())
 	else
-		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,re:GetHandler():GetPreviousLocation())
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,0,rc:GetPreviousLocation())
 	end
 end
 function s.atkfilter(c)

3ed338d211b48b1b2243de76bf6bdaca13579187
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-12-22 19:20:51 +0200

    Small correction in the new script

--- pre-release/c101108025.lua
+++ pre-release/c101108025.lua
@@ -73,6 +73,6 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

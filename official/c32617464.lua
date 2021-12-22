a14ce8312dc64131c296257fb6703f6cee7fcdfc
Hatter <sanity.sierota@protonmail.com>
2021-12-23 03:51:10 +0800

    Tri-Gate Wizard

--- official/c32617464.lua
+++ official/c32617464.lua
@@ -1,5 +1,5 @@
 --トライゲート・ウィザード
---Trigate Wizard
+--Tri-Gate Wizard
 local s,id=GetID()
 function s.initial_effect(c)
 	--link summon
@@ -68,12 +68,14 @@ function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev) and c:GetMutualLinkedGroupCount()>=3
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return re:GetHandler():IsAbleToRemove() end
+	local rc=re:GetHandler()
+	if chk==0 then return Duel.IsPlayerCanRemove(tp,rc) and not (rc:IsMonster()
+		and rc:IsLocation(LOCATION_GRAVE) and Duel.IsPlayerAffectedByEffect(rc:GetControler(),CARD_SPIRIT_ELIMINATION)) end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,re:GetHandler():GetLocation())
+	if rc:IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,rc,1,0,rc:GetLocation())
 	else
-		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,re:GetHandler():GetPreviousLocation())
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,0,rc:GetPreviousLocation())
 	end
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)

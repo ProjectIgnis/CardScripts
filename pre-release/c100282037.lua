6cd93e9ec0750bf2bef1c69b12d64aa4ba53c10f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2021-12-28 02:21:10 +0200

    Added new cards' scripts

---
+++ pre-release/c100282037.lua
@@ -0,0 +1,108 @@
+--
+--Zektrike Kou-ou
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x56}
+function s.cfilter(c,e,tp,ft)
+	return c:IsSetCard(0x56) and c:IsAbleToGraveAsCost() and (c:IsFaceup() or c:IsLocation(LOCATION_HAND))
+		and (Duel.IsExistingMatchingCard(s.monfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft,c)
+		or Duel.IsExistingMatchingCard(s.eqspfilter,tp,LOCATION_DECK,0,1,nil,tp,ft,c))
+end
+function s.monfilter(c,e,tp,ft,sc)
+	return c:IsSetCard(0x56) and c:IsMonster() and (s.monspfilter(c,e,tp,sc) or s.moneqfilter(c,tp,ft,sc))
+end
+function s.monspfilter(c,e,tp,sc)
+	return Duel.GetMZoneCount(tp,sc)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.moneqfilter(c,tp,ft,sc)
+	return (ft>0 or (sc and sc:IsLocation(LOCATION_SZONE) and sc:GetSequence()<5)) and not c:IsForbidden()
+		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x56),tp,LOCATION_MZONE,0,1,sc)
+end
+function s.eqspfilter(c,tp,ft,sc)
+	return c:IsSetCard(0x56) and c:IsType(TYPE_EQUIP) and c:IsType(TYPE_SPELL)
+		and (ft>0 or (sc and sc:IsLocation(LOCATION_SZONE) and sc:GetSequence()<5))
+		and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_MZONE,0,1,sc,c)
+end
+function s.eqfilter(c,ec)
+	return c:IsFaceup() and c:IsSetCard(0x56) and ec:CheckEquipTarget(c)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	if c:IsLocation(LOCATION_HAND) then ft=ft-1 end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,c,e,tp,ft) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,c,e,tp,ft)
+	Duel.SendtoGrave(g,REASON_COST)
+	
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	local b1=Duel.IsExistingMatchingCard(s.monfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft)
+	local b2=Duel.IsExistingMatchingCard(s.eqspfilter,tp,LOCATION_DECK,0,1,nil,tp,ft)
+	local op=aux.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	e:SetLabel(op)
+	if op==1 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	else
+		e:SetCategory(CATEGORY_EQUIP)
+		Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	--Special Summon or equip 1 "Inzektor" monster from your Deck
+	if op==1 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+		local sc=Duel.SelectMatchingCard(tp,s.monfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ft):GetFirst()
+		if not sc then return end
+		local sp=s.monspfilter(sc,e,tp)
+		local eq=s.moneqfilter(sc,tp,ft)
+		local op=aux.SelectEffect(tp,
+			{sp,aux.Stringid(id,3)},
+			{eq,aux.Stringid(id,4)})
+		if op==1 then
+			Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+		elseif op==2 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+			local ec=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.IsSetCard,0x56),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+			if not ec then return end
+			Duel.HintSelection(ec,true)
+			if not Duel.Equip(tp,sc,ec,true) then return end
+			--Equip limit
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_EQUIP_LIMIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(function(e,c) return c==e:GetLabelObject() end)
+			e1:SetLabelObject(ec)
+			sc:RegisterEffect(e1)
+		end
+	--Equip 1 "Inzektor" Equip Spell from your Deck
+	elseif op==2 and ft>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+		local sc=Duel.SelectMatchingCard(tp,s.eqspfilter,tp,LOCATION_DECK,0,1,1,nil,tp,ft):GetFirst()
+		if not sc then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+		local ec=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_MZONE,0,1,1,nil,sc):GetFirst()
+		Duel.HintSelection(ec,true)
+		if not ec then return end
+		Duel.Equip(tp,sc,ec)
+	end
+end
---
+++ pre-release/c101108027.lua
@@ -0,0 +1,69 @@
+--神竜 ティタノマキア
+--Divine Dragon Titanomakhia
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	--Cannot be destroyed by battle if Special Summoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL) end)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Destroy all cards your opponent controls
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,0})
+	e2:SetCost(s.descost)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+	--Send cards from the top of your Deck to the GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DECKDES)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.tgtg)
+	e3:SetOperation(s.tgop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={id}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(id) and c:IsAbleToRemoveAsCost()
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:IsContains(e:GetHandler())
+end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+	if chk==0 then return #g>0 and aux.SelectUnselectGroup(g,e,tp,3,3,s.rescon,0) end
+	local rg=aux.SelectUnselectGroup(g,e,tp,3,3,s.rescon,1,tp,HINTMSG_REMOVE)
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	local sg=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Destroy(sg,REASON_EFFECT)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_DRAGON),tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return ct>0 and Duel.IsPlayerCanDiscardDeckAsCost(tp,ct) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,ct)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsRace,RACE_DRAGON),tp,LOCATION_MZONE,0,nil)
+	if ct==0 then return end
+	Duel.DiscardDeck(tp,ct,REASON_EFFECT)
+end

469db6f900a0019c56cc0569c3b5aad82d193333
ClementLouis <clementlouis.gallier@viacesi.fr>
2021-12-30 23:46:12 +0100

    Emitter Stag fix

--- rush/c160008028.lua
+++ rush/c160008028.lua
@@ -23,13 +23,6 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--requirement
 	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
-	--atk change
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(-500)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	c:RegisterEffect(e1)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,0,1,1,nil)
 	if #g>0 then

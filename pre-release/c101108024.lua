58de9fd433130c71db15797d58e3f081eba4b126
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-01-02 20:28:02 +0200

    Added new cards' scripts

---
+++ pre-release/c101108024.lua
@@ -0,0 +1,106 @@
+--戦華の暴－董穎
+--Ancient Warriors – Cruel Dong Zhong
+--Scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--Activate cost
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_ACTIVATE_COST)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(0,1)
+	e3:SetCondition(s.costcon)
+	e3:SetCost(s.costchk)
+	e3:SetOperation(s.costop)
+	c:RegisterEffect(e3)
+	--Accumulate
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(id)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetTargetRange(0,1)
+	c:RegisterEffect(e4)
+	--Banish
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_REMOVE+CATEGORY_DRAW)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,{id,1})
+	e5:SetCondition(s.rmcon)
+	e5:SetTarget(s.rmtg)
+	e5:SetOperation(s.rmop)
+	c:RegisterEffect(e5)
+end
+s.listed_series={0x137}
+function s.thfilter(c)
+	return c:IsSetCard(0x137) and (c:IsLevelAbove(7) or c:IsType(TYPE_CONTINUOUS)) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.cfilter(c)
+	return c:IsSetCard(0x137) and c:IsLevelAbove(7) and c:IsFaceup()
+end
+function s.costcon(e)
+	return Duel.IsExistingMatchingCard(s.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.costchk(e,te_or_c,tp)
+	local ct=#{Duel.GetPlayerEffect(tp,id)}
+	return Duel.CheckLPCost(tp,ct*400)
+end
+function s.costop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.PayLPCost(tp,400)
+end
+function s.rmcfilter(c,tp)
+	return c:IsMonster() and c:IsControler(1-tp)
+end
+function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.rmcfilter,1,nil,tp)
+end
+function s.rmfilter(c)
+	return c:IsAbleToRemove() and aux.SpElimFilter(c)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and s.rmfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,nil)
+		and Duel.IsPlayerCanDraw(tp,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,s.rmfilter,tp,0,LOCATION_MZONE+LOCATION_GRAVE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0
+		and tc:IsLocation(LOCATION_REMOVED) then
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end

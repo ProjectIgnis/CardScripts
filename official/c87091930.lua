ed1049008f514166a5f0a6d04c8c45c10af94233
Edoardo Lolletti <edoardo762@gmail.com>
2022-01-08 22:58:03 +0100

    Fix minor typo in Raider's Unbreakable Mind
    Was passing the wrong parameters to SetHintTiming

--- official/c87091930.lua
+++ official/c87091930.lua
@@ -1,5 +1,5 @@
 --レイダーズ・アンブレイカブル・マインド
---Raiders' Unbreakable Mind
+--Raider's Unbreakable Mind
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_SPSUMMON,TIMING_SPSUMMON)
+	e1:SetHintTiming(TIMING_SPSUMMON,TIMING_SPSUMMON)
 	c:RegisterEffect(e1)
 	--Destroy 1 card on the field
 	local e2=Effect.CreateEffect(c)

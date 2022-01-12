c0ba357cf497a6fc4154b53afae42653ddffeab3
ClementLouis <clementlouis.gallier@viacesi.fr>
2022-01-12 19:07:48 +0100

    Sevens Road Sorcerer Typo fix

--- rush/c160008011.lua
+++ rush/c160008011.lua
@@ -24,7 +24,7 @@ function s.filter(c,e,sp)
 	return c:IsRace(RACE_SPELLCASTER) and c:IsLevelBelow(7) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)

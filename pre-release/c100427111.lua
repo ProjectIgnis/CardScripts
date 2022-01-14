12ee07e76e9cb93e8ca0c780ac4de23b585a8c23
that-hatter <sanity.sierota@outlook.com>
2022-01-14 15:13:19 +0800

    fix "Valiants" Field Spells
    removed incorrect category that allowed them to be negatable by Ash Blossom

--- pre-release/c100427111.lua
+++ pre-release/c100427111.lua
@@ -5,7 +5,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)

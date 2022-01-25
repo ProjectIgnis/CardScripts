3672b656bdf6de242c977f21e4fa455a2e847d43
ClementLouis <clementlouis.gallier@viacesi.fr>
2022-01-25 17:00:32 +0100

    Parallel Birth Gate fix

--- rush/c160008057.lua
+++ rush/c160008057.lua
@@ -26,10 +26,16 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.DiscardDeck(tp,1,REASON_EFFECT)
 	local g=Duel.GetOperatedGroup()
 	local g2=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	
+	local ft=Duel.GetMZoneCount(tp)
+	if ft<=0 then return end
+	if ft>=2 then ft=2 end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	
 	if #g>0 and #g2>0 and Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0)==0  and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g2:Select(tp,1,2,nil)
+		local sg=g2:Select(tp,1,ft,nil)
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

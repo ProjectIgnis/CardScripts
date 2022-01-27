d77bee03f53ece33861219dd3ce8798ef74b288a
Hatter <sanity.sierota@protonmail.com>
2022-01-27 10:48:37 +0800

    update "Accesscode Talker"
    use effect description for the destruction effect

--- official/c86066372.lua
+++ official/c86066372.lua
@@ -25,6 +25,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--Destroy
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)

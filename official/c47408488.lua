ca2559235b64706cd1b11aef2e5af062fa6442d3
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-02-02 01:35:40 +0200

    Fixes
    Official:
    - Crystal Tree: Placing a Crystal Counter shouldn't start a chain + polishing the 2nd effect.
    - Chain Disappearance: Shouldn't be able to activate it if the summoned monster is not on the field when you would activate this card.
    - Destiny HERO - Dasher: The Special Summoning effect shouldn't trigger in the opponent's Draw Phase + general script polish.
    - Mecha Bunny: Fixed a script error when there was no target for its mandatory damaging effect.
    - Ruin, Angel of Oblivion: Should only be able to make up to 2 attacks on monsters, not up to 3 + general script polish.
    - Windwitch Chimes: Shouldn't be able to target a face-down monster + shouldn't use the target's original name.
    Unofficial:
    - Cross Change: Shouldn't have the targeting flag.

--- official/c47408488.lua
+++ official/c47408488.lua
@@ -8,28 +8,20 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--add counters on event custom
+	--Place 1 Crystal Counter on this card
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_COUNTER)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_MOVE)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCode(EVENT_CUSTOM+id)
+	e2:SetCondition(s.ctcon1)
 	e2:SetOperation(s.ctop)
 	c:RegisterEffect(e2)
-	--add counters on equip
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_COUNTER)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_EQUIP)
-	e3:SetCondition(s.eqcon)
-	e3:SetOperation(s.ctop)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_CHAIN_SOLVED)
+	e3:SetCondition(s.ctcon2)
 	c:RegisterEffect(e3)
-	--place from the deck
+	--Place "Crystal Beast" monsters to your Spell/Trap Zone
 	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_COUNTER)
 	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_SZONE)
@@ -40,45 +32,54 @@ function s.initial_effect(c)
 end
 s.listed_series={0x1034}
 s.counter_place_list={0x6}
-function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	return tc:IsFaceup() and tc:IsSetCard(0x1034)
+function s.ctfilter(c)
+	return c:IsLocation(LOCATION_SZONE) and not c:IsPreviousLocation(LOCATION_SZONE) and c:IsSetCard(0x1034) and c:IsFaceup()
+		and c:IsOriginalType(TYPE_MONSTER)
+end
+function s.ctcon1(e,tp,eg,ep,ev,re,r,rp)
+	if not eg:IsExists(s.ctfilter,1,nil) then return end
+	if Duel.GetCurrentChain()>0 then
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_CHAIN,0,1)
+		return false
+	end
+	return true
+end
+function s.ctcon2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)>0
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():AddCounter(0x6,1)
 end
 function s.plcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	e:SetLabel(e:GetHandler():GetCounter(0x6))
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
-end
-function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local ct=e:GetHandler():GetCounter(0x6)
-		return ct>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>=ct-1
-	end
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToGraveAsCost() end
+	e:SetLabel(c:GetCounter(0x6))
+	Duel.SendtoGrave(c,REASON_COST)
 end
 function s.plfilter(c)
-	return c:IsSetCard(0x1034) and not c:IsForbidden()
+	return c:IsSetCard(0x1034) and c:IsMonster() and not c:IsForbidden()
+end
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=e:GetHandler():GetCounter(0x6)
+	if chk==0 then return ct>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>=ct-1
+		and Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_DECK,0,ct,nil) end
 end
 function s.plop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
-	if ft<=0 then return end
-	if ft>e:GetLabel() then ft=e:GetLabel() end
+	local ct=e:GetLabel()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<ct then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local g=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,ft,ft,nil)
-	if #g>0 then
-		local tc=g:GetFirst()
-		for tc in aux.Next(g) do
-			Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetCode(EFFECT_CHANGE_TYPE)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
-			e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
-			tc:RegisterEffect(e1)
-		end
-		Duel.RaiseEvent(g,EVENT_CUSTOM+id,e,0,tp,0,0)
+	local g=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,ct,ct,nil)
+	if #g==0 then return end
+	local c=e:GetHandler()
+	for tc in g:Iter() do
+		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		--Treat it as a Continuous Spell
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_TYPE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+		e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+		tc:RegisterEffect(e1)
 	end
 end
--- official/c50139096.lua
+++ official/c50139096.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--code
+	--Change this card's name
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -12,57 +12,52 @@ function s.initial_effect(c)
 	e0:SetRange(LOCATION_MZONE+LOCATION_HAND)
 	e0:SetValue(46427957)
 	c:RegisterEffect(e0)
-	--multi attack
+	--Multiple attacks
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
 	e1:SetCondition(s.atkcon)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
-	--actlimit
+	--Opponent cannot activate cards/effects when your Ritual attacks
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetTarget(s.aclimtg)
+	e2:SetOperation(s.aclimop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={32828635,46427957}
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return Duel.IsAbleToEnterBP() and c:IsSummonType(SUMMON_TYPE_RITUAL) 
+	return Duel.IsAbleToEnterBP() and e:GetHandler():IsSummonType(SUMMON_TYPE_RITUAL) 
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
+		--Can make up to 2 attacks on monsters
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
-		e1:SetValue(2)
+		e1:SetValue(1)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_RITUAL)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+function s.aclimtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() and chkc:IsType(TYPE_RITUAL) end
+	if chk==0 then return Duel.IsExistingTarget(aux.FilterFaceupFunction(Card.IsType,TYPE_RITUAL),tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,aux.FilterFaceupFunction(Card.IsType,TYPE_RITUAL),tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+function s.aclimop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
+		--Cannot activate cards/effects when your Ritual attacks
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 		e1:SetRange(LOCATION_MZONE)
@@ -83,8 +78,8 @@ function s.actcon(e,tp,eg,ep,ev,re,r,rp)
 	return ac and ac:IsControler(tp) and ac:IsType(TYPE_RITUAL) and tc:GetFlagEffect(id)>0
 end
 function s.actop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
+	--Cannot activate
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)

e6b5f43e7f40ad008dcf0edc66fc4451f2240485
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-03-06 13:55:25 +0200

    Added new Rush cards' scripts

---
+++ rush/c160009038.lua
@@ -0,0 +1,54 @@
+--幻刃棟竜ハンドツーロン
+--Constructor Ridge Wyrm Handtoolon
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--"Constructor Warrior Shovelon" + "Picklon the Constructor Fairy"
+	Fusion.AddProcMix(c,true,true,160004002,160004025)
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.atkcost)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160004002,160004025,160004042,160203030}
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_FIELD) and c:IsType(TYPE_SPELL)
+end
+function s.thfilter(c,e,tp)
+	return c:IsCode(160004042,160203030) and c:IsAbleToHand()
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)==0 then return end
+	--Effect
+	local c=e:GetHandler()
+	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetValue(200)
+	c:RegisterEffect(e1)
+	if not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end

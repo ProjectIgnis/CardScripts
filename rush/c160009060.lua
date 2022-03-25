0bc0e995ca35e1f126b4bc810096eb98b18f6cc8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-03-25 20:57:01 +0200

    Added new Rush cards' scripts

---
+++ rush/c160009060.lua
@@ -0,0 +1,44 @@
+-- 交剣死哀
+-- Cross-Sword Deathmatch
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Inflict damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(s.damcon)
+	e1:SetTarget(s.damtg)
+	e1:SetOperation(s.damop)
+	c:RegisterEffect(e1)
+end
+function s.damconfilter(c,tp)
+	return c:GetPreviousAttributeOnField()&ATTRIBUTE_EARTH>0 and c:IsPreviousControler(tp)
+		and c:GetReasonPlayer()==1-tp
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp) and eg:IsExists(s.damconfilter,1,nil,tp)
+end
+function s.damfilter(c)
+	return c:IsType(TYPE_NORMAL) and c:IsAttribute(ATTRIBUTE_EARTH)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(s.damfilter,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return ct>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*100)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetMatchingGroupCount(s.damfilter,tp,LOCATION_GRAVE,0,nil)
+	if ct>0 and Duel.Damage(1-tp,ct*100,REASON_EFFECT)>0
+		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_NORMAL),tp,0,LOCATION_MZONE,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local dg=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.IsType,TYPE_NORMAL),tp,0,LOCATION_MZONE,1,1,nil)
+		if #dg>0 then
+			Duel.HintSelection(dg,true)
+			Duel.Destroy(dg,REASON_EFFECT)
+		end
+	end
+end
---
+++ rush/c160009061.lua
@@ -0,0 +1,28 @@
+-- 迎え突き！
+-- Greeting Strike!
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Inflict damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(function(_,tp) return Duel.GetAttacker():IsControler(1-tp) end)
+	e1:SetTarget(s.damtg)
+	e1:SetOperation(s.damop)
+	c:RegisterEffect(e1)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.HasLevel),tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local tc=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.HasLevel),tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	if tc then
+		Duel.HintSelection(tc,true)
+		Duel.Damage(1-tp,tc:GetLevel()*100,REASON_EFFECT)
+	end
+end

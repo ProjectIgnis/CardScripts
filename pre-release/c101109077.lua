94b925eeacd760f8ab99229441b60b0c45834509
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-04-02 18:35:14 +0300

    Added new card's script

---
+++ pre-release/c101109077.lua
@@ -0,0 +1,76 @@
+--Ａ・∀・ＴＴ
+--Amaze Attraction Thrill Train
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	--From cards_specific_functions.lua
+	aux.AddAttractionEquipProc(c)
+	--You: Change battle position and Set 1 "Attraction" Trap from your GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCondition(aux.AttractionEquipCon(true))
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	c:RegisterEffect(e1)
+	--Your opponent: Banish the equipped monster until the End Phase
+	local e2=e1:Clone()
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetHintTiming(0)
+	e2:SetCondition(aux.AttractionEquipCon(false))
+	e2:SetTarget(s.rmtg)
+	e2:SetOperation(s.rmop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x15e,0x15f}
+function s.setfilter(c)
+	return c:IsSetCard(0x15f) and c:IsType(TYPE_TRAP) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetHandler():GetEquipTarget()
+	if chk==0 then return tc and tc:IsCanChangePosition()
+		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,tc,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,0)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetEquipTarget()
+	if tc and tc:IsLocation(LOCATION_MZONE) and Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)>0 then
+		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SET)
+		local g=Duel.SelectMatchingCard(1-tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.SSet(tp,g)
+		end
+	end
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetHandler():GetEquipTarget()
+	if chk==0 then return tc and tc:IsAbleToRemove() end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetEquipTarget()
+	if tc and tc:IsLocation(LOCATION_MZONE) and Duel.Remove(tc,0,REASON_EFFECT+REASON_TEMPORARY)>0 then
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		--Return it in the End Phase
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetLabelObject(tc)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.retcon)
+		e1:SetOperation(s.retop)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetLabelObject():GetFlagEffect(id)>0
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ReturnToField(e:GetLabelObject())
+end

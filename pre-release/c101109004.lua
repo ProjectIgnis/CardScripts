e4d145ea06abdd5f689fbf1db20cc23e42eb26b2
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-04-09 02:10:01 +0300

    Added new cards' scripts

---
+++ pre-release/c101109004.lua
@@ -0,0 +1,48 @@
+-- スプライト・ジェット
+-- Splight Jet
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Special Summon self
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	-- Search 1 "Splight" Spell/Trap
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x280}
+function s.spconfilter(c)
+	return c:IsFaceup() and (c:IsLevel(2) or c:IsRank(2))
+end
+function s.spcon(e,c)
+	return not c or Duel.IsExistingMatchingCard(s.spconfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x280) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

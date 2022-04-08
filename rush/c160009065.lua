3d07ae7fd070e0b1fdc093c4271a60a04895fafe
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-04-08 15:20:28 +0300

    "Crisis at the Sacred Tower" update
    Should be able to flip face-down an Effect Monster only.

--- rush/c160009065.lua
+++ rush/c160009065.lua
@@ -17,7 +17,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return ep==1-tp and eg:GetFirst():IsLevelAbove(7)
 end
 function s.filter(c)
-	return c:IsFaceup() and c:IsLevelBelow(8) and c:IsCanTurnSet()
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsLevelBelow(8) and c:IsCanTurnSet()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end

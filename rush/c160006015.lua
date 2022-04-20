837de938418df513d532bb1129654e2858a264e0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-04-21 01:25:39 +0300

    "Thunderbold the Lightningfire Deity" fix
    Shouldn't ask the player if there is no free MMZ.

--- rush/c160006015.lua
+++ rush/c160006015.lua
@@ -2,43 +2,43 @@
 --Thunderbold the Lightningfire Deity
 local s,id=GetID()
 function s.initial_effect(c)
-	-- atk change
+	--Increase ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
+	e1:SetCondition(function(e) return e:GetHandler():IsStatus(STATUS_SUMMON_TURN) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) and e:GetHandler():IsStatus(STATUS_SUMMON_TURN) end
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
 end
 function s.spfilter(c,e,tp)
-	return c:GetLevel()==7 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
-end
-function s.filter(c,tp)
-	return c:GetLevel()==7
+	return c:IsLevel(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--requirement
+	--Requirement
 	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
-	--atk change
+	--Effect
+	local c=e:GetHandler()
+	--Increase ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(300)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	c:RegisterEffect(e1)
-	-- summon
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,tp)
-	if g:GetClassCount(Card.GetCode)>=4 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+	local g=Duel.GetMatchingGroup(Card.IsLevel,tp,LOCATION_GRAVE,0,nil,7)
+	if g:GetClassCount(Card.GetCode)>=4 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 		if #g>0 then
+			Duel.BreakEffect()
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end

2cb53e25363609a5e98dd9f77bbfe406cb84ead2
NaimSantos <naimsantos45@gmail.com>
2022-04-29 18:06:25 -0300

    Update "Underworld Goddess of the Closed World"

--- official/c98127546.lua
+++ official/c98127546.lua
@@ -96,9 +96,12 @@ function s.immval(e,re)
 end
 function s.spdiscon(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or re:GetHandlerPlayer()==tp or
-	   not Duel.IsChainNegatable(ev) then return false end
-	local ex,tg,ct,p,l=Duel.GetOperationInfo(ev,CATEGORY_SPECIAL_SUMMON)
-	return ex and (l&LOCATION_GRAVE)~=0 or (tg and tg:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE))
+		not Duel.IsChainNegatable(ev) then return false end
+	local ex1,tg1,ct1,p1,loc1=Duel.GetOperationInfo(ev,CATEGORY_SPECIAL_SUMMON)
+	local ex2,tg2,ct2,p2,loc2=Duel.GetPossibleOperationInfo(ev,CATEGORY_SPECIAL_SUMMON)
+	local res1 = ((loc1 or 0)&LOCATION_GRAVE~=0) or (tg1 and tg1:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE))
+	local res2 = ((loc2 or 0)&LOCATION_GRAVE~=0) or (tg2 and tg2:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE))
+	return res1 or res2
 end
 function s.spdistg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

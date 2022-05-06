30146e364d2466bdba55ce80a52de757ea1d6ccc
Naim <naimsantos45@gmail.com>
2022-05-06 12:50:38 -0300

    Abyss Improv fix
    Fixed a bug that would happen when no attack target was involved in the damage calculation

--- unofficial/c511004429.lua
+++ unofficial/c511004429.lua
@@ -1,8 +1,9 @@
---Abyss Impromptu Play - Improv
+--魔界即興劇－インプロ
+--Abyss Improv
 --fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
-	--batturo
+	--Change battle damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -11,13 +12,14 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_series={0x10ec}
 function s.filter(c,g)
-	return c:IsFaceup() and c:IsSetCard(0x10ec) and c:GetLevel()<=4
+	return c:IsFaceup() and c:IsSetCard(0x10ec) and c:IsLevelBelow(4)
 end
 function s.target(e,tp,eg,ev,ep,re,r,rp,chk,chkc)
-	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-	local g=Group.FromCards(a,d)
+	local a,d=Duel.GetAttacker(),Duel.GetAttackTarget()
+	local g=Group.FromCards(a)
+	if d then g:AddCard(d) end
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and not g:IsContains(chkc) and s.filter(chkc) end
 	if chk==0 then return d and g:IsExists(Card.IsControler,1,nil,1-tp)
 		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,g) end

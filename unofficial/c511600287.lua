fe7833783581e457b8fb55faab2b64ec49b082f8
Naim <naimsantos45@gmail.com>
2022-05-12 13:29:03 -0300

    Chronomaly Technology (Anime) fix
    It should be able to banish any "Chronomaly" card, not only monsters

--- unofficial/c511600287.lua
+++ unofficial/c511600287.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 end
 s.listed_series={0x70}
 function s.filter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x70) and c:IsAbleToRemove() and aux.SpElimFilter(c,true)
+	return c:IsSetCard(0x70) and c:IsAbleToRemove() and aux.SpElimFilter(c,true)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end

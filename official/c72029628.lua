a38052df92926c9ed0103a80c006f8f395638b27
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-05-31 00:24:07 +0300

    Update c72029628.lua

--- official/c72029628.lua
+++ official/c72029628.lua
@@ -43,5 +43,5 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE)
 end

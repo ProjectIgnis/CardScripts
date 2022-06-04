1e1cfd4082296c4d6976aebdf2774c1b1d977d53
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-06-05 02:27:12 +0300

    Added new cards' scripts

---
+++ pre-release/c100290039.lua
@@ -0,0 +1,63 @@
+--アマゾネスの秘湯
+--Amazoness Hot Spring
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_BATTLE_START+TIMING_END_PHASE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Gain LP equal to the battle damage taken
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.lpcond)
+	e2:SetTarget(s.lptg)
+	e2:SetOperation(s.lpop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x4}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.pendfilter(c,tp)
+	return c:IsType(TYPE_PENDULUM) and not c:IsForbidden() and aux.CheckPendulumZones(tp)
+end
+function s.cfilter(c,tp)
+	return c:IsMonster() and c:IsSetCard(0x4) and (c:IsAbleToHand() or s.pendfilter(c,tp))
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_DECK,0,nil,tp)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+		local tc=g:Select(tp,1,1,nil)
+		aux.ToHandOrElse(tc,tp,function(tc) return s.pendfilter(tc,tp) end,
+						function(tc) Duel.MoveToField(tc:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true) end,
+						aux.Stringid(id,3)
+						)
+	end
+end
+function s.amzfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x4) and c:IsOriginalType(TYPE_MONSTER)
+end
+function s.lpcond(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp and Duel.IsExistingMatchingCard(s.amzfilter,tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.lptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,ev)
+end
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Recover(tp,ev,REASON_EFFECT)
+end

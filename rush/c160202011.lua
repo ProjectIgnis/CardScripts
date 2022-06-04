a257c92fbe1619864f430e8c48749860efad078e
Naim Santos <naimsantos45@gmail.com>
2022-06-04 09:11:22 -0300

    Update Yggdrago the Sky Emperor
    It was skipped in the previous commit

--- rush/c160202011.lua
+++ rush/c160202011.lua
@@ -27,5 +27,5 @@ function s.indcon(e)
 	return e:GetHandler():IsMaximumMode()
 end
 function s.indval(e,re,rp)
-	return re:IsActiveType(TYPE_TRAP)
+	return re:IsActiveType(TYPE_TRAP) and aux.indoval(e,re,rp)
 end

fdb4db0b34202055da9a6afbea99dec0b54c4d91
Naim <naimsantos45@gmail.com>
2022-06-05 06:07:35 -0300

    "Metalsilver Armor"
    Finish the effect implementation

--- official/c33114323.lua
+++ official/c33114323.lua
@@ -1,18 +1,26 @@
 --メタルシルバー・アーマー
---Effect is not fully implemented
+--Metalsilver Armor
+local LOC_MZN_GRV_BAN=LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c)
-	--atk target
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_RANGE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTarget(s.efftg)
-	e2:SetValue(aux.tgoval)
-	c:RegisterEffect(e2)
+	--Limit effect target
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_RANGE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(LOC_MZN_GRV_BAN,LOC_MZN_GRV_BAN)
+	e1:SetCondition(s.cond)
+	e1:SetTarget(s.efftg)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+end
+function s.cond(e)
+	local tc=e:GetHandler():GetEquipTarget()
+	return tc and tc:GetControler()==e:GetHandler():GetControler()
 end
 function s.efftg(e,c)
 	return c~=e:GetHandler():GetEquipTarget() and c:IsType(TYPE_MONSTER)
+		and (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED))--face-down banished cards can still be targeted
 end

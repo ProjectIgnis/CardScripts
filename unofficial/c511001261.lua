e10b263646ad4c5d019461b26d8d3e11f18ca0e6
Naim <naimsantos45@gmail.com>
2022-06-06 17:21:12 -0300

    "Minute Deal" fix
    Previously, it would Special Summon "Royal Cookpal" monster whose total levels were less than or equal to the sum of the levels of the opponent's monsters.

--- unofficial/c511001261.lua
+++ unofficial/c511001261.lua
@@ -1,4 +1,5 @@
---Instant Cooking
+--インスタント・クッキング
+--Minute Meal
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -10,37 +11,33 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,e,tp,sum)
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:GetLevel()<=sum and c:IsSetCard(0x1512)
-end
-function s.lvfilter(c)
-	return c:IsFaceup() and c:GetLevel()>0
+s.listed_series={0x1512}
+function s.spfilter(c,e,tp,lv)
+	return c:IsSetCard(0x1512) and c:IsLevelBelow(lv) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.lvfilter,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.HasLevel),tp,0,LOCATION_MZONE,nil)
 	local sum=g:GetSum(Card.GetLevel)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp,sum) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp,sum) end
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp,sum)
+	local ct=math.min(#sg,Duel.GetLocationCount(tp,LOCATION_MZONE))
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ct,tp,LOCATION_HAND)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<=0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	local g=Duel.GetMatchingGroup(s.lvfilter,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.HasLevel),tp,0,LOCATION_MZONE,nil)
+	if #g==0 then return end
 	local sum=g:GetSum(Card.GetLevel)
-	local spg=Group.CreateGroup()
-	local sp=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil,e,tp,sum)
-	while ft>0 and #sp>0 do
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp,sum)
+	if #sg>0 then
+		local ct=math.min(#sg,ft)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=sp:Select(tp,1,1,nil)
-		spg:Merge(sg)
-		sp:Sub(sg)
-		sum=sum-sg:GetFirst():GetLevel()
-		sp=sp:Filter(s.filter,nil,e,tp,sum)
-		ft=ft-1
-	end
-	if #spg>0 then
-		Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
+        local spg=sg:Select(tp,ct,ct,nil)
+		if #spg>0 then
+			Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 end

aced056ec79ff477513bf47fdc76bef1f486820b
Hatter <sanity.sierota@protonmail.com>
2022-06-10 15:55:56 +0800

    update "Naturia Horneedle"
    - add Camellia support
    - general script improvements

--- official/c84905691.lua
+++ official/c84905691.lua
@@ -1,35 +1,38 @@
 --ナチュル・ホーストニードル
+--Naturia Horneedle
 local s,id=GetID()
 function s.initial_effect(c)
+	--Destroy Special Summoned monster(s)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(aux.CostWithReplace(s.descost,CARD_NATURIA_CAMELLIA))
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x2a}
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,aux.FilterFaceupFunction(Card.IsSetCard,0x2a),1,false,nil,e:GetHandler()) end
-	local g=Duel.SelectReleaseGroupCost(tp,aux.FilterFaceupFunction(Card.IsSetCard,0x2a),1,1,false,nil,e:GetHandler())
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,aux.FilterFaceupFunction(Card.IsSetCard,0x2a),1,false,nil,c) end
+	local g=Duel.SelectReleaseGroupCost(tp,aux.FilterFaceupFunction(Card.IsSetCard,0x2a),1,1,false,nil,c)
 	Duel.Release(g,REASON_COST)
 end
-function s.filter(c,e,tp)
-	return not c:IsSummonPlayer(tp) and (not e or c:IsRelateToEffect(e))
+function s.desfilter(c,e,tp)
+	return c:IsSummonPlayer(1-tp) and (not e or c:IsRelateToEffect(e))
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():IsStatus(STATUS_CHAINING) and not eg:IsContains(e:GetHandler())
-		and eg:IsExists(s.filter,1,nil,nil,tp) end
-	local g=eg:Filter(s.filter,nil,nil,tp)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=eg:Filter(s.desfilter,nil,nil,tp)
+	if chk==0 then return #g>0 and not c:IsStatus(STATUS_CHAINING) and not eg:IsContains(c) end
 	Duel.SetTargetCard(eg)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(s.filter,nil,e,tp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.desfilter,nil,e,tp)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end

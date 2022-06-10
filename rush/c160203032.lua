aa83c510efc0b74a168fb396c3aee75c3d092c02
Naim <naimsantos45@gmail.com>
2022-06-09 21:29:20 -0300

    Update c160203032.lua

--- rush/c160203032.lua
+++ rush/c160203032.lua
@@ -4,7 +4,7 @@
 --Substitute ID
 local s,id=GetID()
 function s.initial_effect(c)
-	--Destroy 1 spell/trap your opponent controls
+	--Destroy 1 Spell/Trap your opponent controls
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -20,7 +20,6 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 	--Activation legality
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsType(TYPE_SPELL+TYPE_TRAP) and chkc~=e:GetHandler() end
 	if chk==0 then return Duel.GetMatchingGroupCount(Card.IsType,tp,0,LOCATION_ONFIELD,e:GetHandler(),TYPE_SPELL+TYPE_TRAP)>0 end
 end
 	--Send 1 card from hand to GY to destroy 1 spell/trap your opponent controls
@@ -30,7 +29,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,e:GetHandler())
 	--Effect
-	if Duel.SendtoGrave(g,REASON_COST)~=0 then
+	if Duel.SendtoGrave(g,REASON_COST)>0 then
 		local dg=Duel.GetMatchingGroup(Card.IsType,tp,0,LOCATION_ONFIELD,e:GetHandler(),TYPE_SPELL+TYPE_TRAP)
 		if #dg>0 then
 			local sg=dg:Select(tp,1,1,nil)

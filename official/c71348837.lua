1909fb58766a86811fefd21d215b7788a263ddc5
Rundas <38040866+Rundas01@users.noreply.github.com>
2022-06-13 12:52:06 +0200

    Small change for "Madolche Salon"
    removed unnecessary double confirm

--- official/c71348837.lua
+++ official/c71348837.lua
@@ -57,7 +57,6 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SSet(tp,sg)
-		Duel.ConfirmCards(1-tp,sg)
 	end
 end
 

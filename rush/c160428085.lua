94cb72f9600711507d82d994b7d09535269bebb6
Naim <naimsantos45@gmail.com>
2022-06-15 15:54:14 -0300

    Update Gradually Approaching Footsteps
    Fixed a bug where the player would be allowed to select more S/Ts to set than the number of Spell/Trap zones available

--- rush/c160428085.lua
+++ rush/c160428085.lua
@@ -18,7 +18,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_ZOMBIE),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end --Duel.IsExistingMatchingCard(aux.NOT(Card.IsPublic),tp,0,LOCATION_HAND,1,nil) 
+	if chk==0 then return true end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
 function s.cfilter(c)
@@ -27,8 +27,8 @@ end
 function s.rescon(ft)
 	return function(sg,e,tp,mg)
 		local fc=sg:FilterCount(Card.IsType,nil,TYPE_FIELD)
-		local c1=#sg
-		return fc<=1 and (c1-fc<=ft or c1>1)
+		local total=#sg
+		return fc<=1 and (total-fc<=ft)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
@@ -37,7 +37,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(tp,hg)
 		local ft=Duel.GetLocationCount(1-tp,LOCATION_SZONE)
 		local g=hg:Filter(s.cfilter,nil)
-		local sg=aux.SelectUnselectGroup(g,e,tp,1,math.min(#g,ft+1,3),s.rescon(ft),1,tp,HINTMSG_SET)
+		local ct=math.min(#g,ft+1,3)
+		local sg=aux.SelectUnselectGroup(g,e,tp,1,ct,s.rescon(ft),1,tp,HINTMSG_SET)
 		if #sg>0 then
 			Duel.SSet(1-tp,sg,1-tp,true)
 			Duel.Damage(1-tp,#sg*300,REASON_EFFECT)

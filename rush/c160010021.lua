df1df93775fc96518d6fc5f3e5e4b60253e7479b
Naim <naimsantos45@gmail.com>
2022-06-19 17:27:32 -0300

    Add script

---
+++ rush/c160010021.lua
@@ -0,0 +1,39 @@
+--パラシューム・コロニー
+--Parashroom Colony
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 1 "Parashroom Colony" from GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={id}
+function s.glxfilter(c)
+	return c:IsLevelAbove(7) and c:IsRace(RACE_GALAXY) and c:IsFaceup()
+end
+function s.condition(e,tp)
+	return Duel.IsExistingMatchingCard(s.glxfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.spfilter(c,e,tp)
+	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #sg>0 then
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

d4bbbd81ee5ad236b4c189e682c2af45d47a3375
Naim <naimsantos45@gmail.com>
2022-06-20 10:57:20 -0300

    Fix "Starry Night, Starry Dragon"
    The second attack should happen even if the opponent's monster was not banished

--- official/c90835938.lua
+++ official/c90835938.lua
@@ -1,13 +1,13 @@
 --聖夜に煌めく竜
---Radiant Seiyaryu
+--Starry Night, Starry Dragon
 --Scripted by edo9300
 
 local s,id=GetID()
 function s.initial_effect(c)
-	--If Normal or Special Summoned from hand, destroy 1
+	--If Normal or Special Summoned from hand, destroy 1 card on the field
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
@@ -32,8 +32,8 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 	--Banish attacked monster until EP and chain attack
 	local e5=Effect.CreateEffect(c)
-	e5:SetCategory(CATEGORY_REMOVE)
 	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_REMOVE)
 	e5:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e5:SetCode(EVENT_BATTLE_START)
 	e5:SetRange(LOCATION_MZONE)
@@ -50,11 +50,11 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
@@ -62,23 +62,23 @@ function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local tc=c:GetBattleTarget()
 	if chk==0 then return c==Duel.GetAttacker() and tc and tc:IsControler(1-tp) and tc:IsAbleToRemove(tp) end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,tp,0)
 end
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
-	local d=Duel.GetAttacker():GetBattleTarget()
-	if not d or not d:IsRelateToBattle() then return end
-	if Duel.Remove(d,0,REASON_EFFECT+REASON_TEMPORARY)==0 then return end
 	local c=e:GetHandler()
-	d:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetLabelObject(d)
-	e1:SetCountLimit(1)
-	e1:SetCondition(s.retcon)
-	e1:SetOperation(s.retop)
-	Duel.RegisterEffect(e1,tp)
+	local d=Duel.GetAttacker():GetBattleTarget()
+	if d and d:IsRelateToBattle() and d:IsControler(1-tp) and Duel.Remove(d,0,REASON_EFFECT+REASON_TEMPORARY)>0 then
+		d:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		e1:SetLabelObject(d)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.retcon)
+		e1:SetOperation(s.retop)
+		Duel.RegisterEffect(e1,tp)
+	end
 	if c:CanChainAttack() then Duel.ChainAttack() end
 end
 function s.retcon(e,tp,eg,ep,ev,re,r,rp)

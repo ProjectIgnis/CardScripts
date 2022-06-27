bb123dcd032f5c5738637dab807d69b9035f2333
Naim <naimsantos45@gmail.com>
2022-06-27 19:17:57 -0300

    Fix "Shitotsu the Talismanic Warrior"
    Fixed a bug where the effect could be activated even if the player didn't have a free monster zone

--- rush/c160009029.lua
+++ rush/c160009029.lua
@@ -27,14 +27,14 @@ function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_WARRIOR) and c:IsAttack(1000) and c:IsDefense(1000) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	--Requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
-	if #dg==0 or Duel.SendtoDeck(dg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)==0 then return end
+	if #dg==0 or Duel.SendtoDeck(dg,nil,SEQ_DECKBOTTOM,REASON_EFFECT)==0 or Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
 	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)

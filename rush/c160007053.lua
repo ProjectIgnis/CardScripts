56168b966adb1ccfef771c9e5e22a51747202cf8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-06-28 13:40:54 +0300

    "Back Beat" fix
    Should shuffle the Deck before drawing if the player chooses to draw + general polish.

--- rush/c160007053.lua
+++ rush/c160007053.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(s.cost)
@@ -12,44 +12,40 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+function s.cfilter(c)
+	return c:IsMonster() and c:IsAbleToDeckOrExtraAsCost() 
+end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,2,nil,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,2,nil) end
 end
 function s.tdfilter(c,tp)
-	return c:IsAbleToDeck() and c:IsType(TYPE_MONSTER) and Duel.IsExistingMatchingCard(s.tdfilter2,tp,0,LOCATION_GRAVE,1,c,c:GetRace())
+	return c:IsAbleToDeck() and c:IsMonster() and Duel.IsExistingMatchingCard(s.tdfilter2,tp,0,LOCATION_GRAVE,1,c,c:GetRace())
 end
 function s.tdfilter2(c,race)
-	return c:IsAbleToDeck() and c:IsRace(race)
-end
-function s.cfilter(c,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeckOrExtraAsCost() 
+	return c:IsAbleToDeck() and c:IsMonster() and c:IsRace(race)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return 
-		 Duel.IsExistingMatchingCard(s.tdfilter,tp,0,LOCATION_GRAVE,1,nil,tp)
-	end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,0,LOCATION_GRAVE,1,nil,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,2,tp,LOCATION_GRAVE)
 end
 function s.tdfilter3(c)
-	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeck() 
+	return c:IsMonster() and c:IsAbleToDeck() 
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--Requirement
+	-- Requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,2,2,nil,tp)
-	Duel.HintSelection(g)
-	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)>0 then
-		-- effect
-		local g=Duel.GetMatchingGroup(s.tdfilter3,tp,0,LOCATION_GRAVE,nil,tp)
-		local sg=aux.SelectUnselectGroup(g,e,tp,2,7,s.rescon,1,tp,HINTMSG_TODECK)
-		if sg and #sg>0 then
-			if Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>3 then
-				if Duel.IsPlayerCanDraw(tp,1) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-					Duel.Draw(tp,1,REASON_EFFECT)
-				end
-			end
-		end
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.HintSelection(g,true)
+	if #g==0 or Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)==0 then return end
+	-- Effect
+	local dg=Duel.GetMatchingGroup(s.tdfilter3,tp,0,LOCATION_GRAVE,nil)
+	local sg=aux.SelectUnselectGroup(dg,e,tp,2,7,s.rescon,1,tp,HINTMSG_TODECK)
+	if #sg==0 then return end
+	Duel.HintSelection(sg,true)
+	if Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>3 and Duel.IsPlayerCanDraw(tp,1)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.ShuffleDeck(tp)
+		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end
 function s.rescon(sg,e,tp,mg)

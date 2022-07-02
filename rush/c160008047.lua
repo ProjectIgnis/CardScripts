8fe7fd8684db257536a1260409420c9c155cade3
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-07-02 13:42:25 +0200

    "Shadow Flower Purple Smoke Spray" fix
    Wrong filter being used. Also, should check for a monster other than the one to be sent for the requirement to change position and shouldn't ask the player to draw if they cannot draw.

--- rush/c160008047.lua
+++ rush/c160008047.lua
@@ -2,11 +2,10 @@
 -- Shadow Flower Purple Smoke Spray
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	-- Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_POSITION+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
@@ -14,27 +13,28 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.costfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_PLANT) and c:IsAbleToGraveAsCost() and c:IsFaceup()
+	return c:IsRace(RACE_PLANT) and c:IsFaceup() and c:IsAbleToGraveAsCost()
+		and Duel.IsExistingMatchingCard(aux.AND(Card.IsDefensePos,Card.IsCanChangePosition),0,LOCATION_MZONE,LOCATION_MZONE,1,c)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDefensePos,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(Card.IsDefensePos,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	-- requirement
+	-- Requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g==0 then return end
 	g=g:AddMaximumCheck()
-	local ct=Duel.SendtoGrave(g,REASON_COST)
-	
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil):GetFirst()
-	if not tc:IsAttackPos() then
-		if Duel.ChangePosition(tc,0,0,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-			Duel.Draw(tp,1,REASON_EFFECT)
-		end
+	if Duel.SendtoGrave(g,REASON_COST)==0 then return end
+	-- Effect
+	local tc=Duel.SelectMatchingCard(tp,aux.AND(Card.IsDefensePos,Card.IsCanChangePosition),tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil):GetFirst()
+	if tc and Duel.ChangePosition(tc,0,0,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)>0 and Duel.IsPlayerCanDraw(tp,1)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end

0db6fb132bf0e6986a96b21db023385d8b3e5e01
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-07-03 23:19:54 +0200

    Added new Rush cards' scripts

---
+++ rush/c160010040.lua
@@ -0,0 +1,44 @@
+--サンダーガゼル
+--Thunder Gazelle
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Give Piercing
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsMonster() and c:IsAbleToDeckOrExtraAsCost() 
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,2,nil) end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP() and Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)==0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.CanAttack),tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	if #g~=2 or Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)~=2 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local sg=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.CanAttack),tp,LOCATION_MZONE,0,1,1,nil)
+	if #sg>0 then
+		Duel.HintSelection(sg)
+		local tc=sg:GetFirst()
+		--Piercing
+		tc:AddPiercing(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,c)
+	end
+end

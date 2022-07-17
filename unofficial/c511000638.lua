4bd63ffecfa90c336b346604251a1591a8e74ca7
Naim <naimsantos45@gmail.com>
2022-07-17 06:59:33 -0300

    Update "Rainbow Snake Eingana"
    Since the effect is mandatory, it should activate even if the opponent doesn't control monsters

--- unofficial/c511000638.lua
+++ unofficial/c511000638.lua
@@ -3,9 +3,9 @@
 --fixed by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	--to grave
+	--Destroy all monsters the opponent controls
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(698785,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_TO_GRAVE)
@@ -14,11 +14,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
-	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return true end
+	local sg=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	local sg=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
 	Duel.Destroy(sg,REASON_EFFECT)
 end

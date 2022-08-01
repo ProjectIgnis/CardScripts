6d2e008c651f7a6f27b1e3ec055d0d0f92638f2c
mercury233 <me@mercury233.me>
2022-08-01 13:52:21 +0800

    fix "Lady of the Labrynth"
    typo

--- official/c81497285.lua
+++ official/c81497285.lua
@@ -64,7 +64,7 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,POS_FACEUP_DEFENSE) end
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)

0cb0f6a9b3a8421b3af6c9865f2928013eea248f
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-08-02 14:16:15 +0200

    Added new Rush card's script

---
+++ rush/c160314022.lua
@@ -0,0 +1,44 @@
+--ネジマジロ
+--Screw Armadillo
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--change position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_MACHINE) and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.posfilter(c)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsLevelBelow(6) and c:IsCanTurnSet() and c:IsCanChangePositionRush()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	if g and Duel.SendtoGrave(g,REASON_COST)>0 then
+		--Effect
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local g2=Duel.SelectMatchingCard(tp,s.posfilter,tp,0,LOCATION_MZONE,1,1,nil)
+		Duel.HintSelection(g2)
+		if #g2>0 then
+			Duel.ChangePosition(g2,POS_FACEDOWN_DEFENSE)
+		end
+	end
+end

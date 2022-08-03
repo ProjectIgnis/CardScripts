91c40aa19e7f5796d8156d7641126871223f7657
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-08-03 14:17:40 +0200

    added Dragon Surpassing the Heavenly Heights

---
+++ rush/c160315029.lua
@@ -0,0 +1,46 @@
+--竜超天山
+--Dragon Surpassing the Heavenly Heights
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.descond)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsMonster() and c:IsRace(RACE_DRAGON)
+end
+function s.descond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_FZONE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_FZONE,LOCATION_FZONE,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+		if Duel.Destroy(g,REASON_EFFECT)>0 and #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sc=Group.Select(sg,tp,1,1,nil)
+			if #sc==0 then return end
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

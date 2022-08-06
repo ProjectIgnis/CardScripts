0e576c3d3a6db6d993de31349c113926869f3dee
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-08-06 17:59:25 +0200

    Added new Rush card's script

---
+++ rush/c160317029.lua
@@ -0,0 +1,57 @@
+--ヴォイド・ヴェール
+--Void Veil
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_GALAXY) and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,2,nil)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsLevelBelow(8) and c:IsCanTurnSet()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.thfilter(c)
+	return c:IsDefensePos() and c:IsAbleToHand() and not c:IsMaximumModeSide()
+end
+function s.cfilter2(c)
+	return c:IsFaceup() and c:IsLevel(8) and c:IsRace(RACE_GALAXY)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g,true)
+	Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+	if not Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_MZONE,0,1,nil) then return end
+	local g2=Duel.GetMatchingGroup(s.thfilter,tp,0,LOCATION_MZONE,nil)
+	if #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g2:Select(tp,1,1,nil)
+		sg=sg:AddMaximumCheck()
+		Duel.HintSelection(sg)
+		if #sg>0 then
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		end
+	end
+end
+

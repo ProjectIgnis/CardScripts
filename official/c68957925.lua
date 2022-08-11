fef9500adbe742e073f26adbab885dcf538eced8
Naim <naimsantos45@gmail.com>
2022-08-11 07:23:11 -0300

    Update c68957925.lua

--- official/c68957925.lua
+++ official/c68957925.lua
@@ -1,6 +1,5 @@
 --リンク・パーティー
 --Link Party
---
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -12,20 +11,22 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	if not AshBlossomTable then AshBlossomTable={} end
-	table.insert(AshBlossomTable,e1)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsLinkMonster()
+function s.spfilter(c,e,tp)
+	return c:IsAttackAbove(2500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.desfilter(c)
+	return c:IsFaceup() and c:IsAttackBelow(3000)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetClassCount(Card.GetOriginalAttribute)
+	local lg=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsLinkMonster),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local ct=lg:GetClassCount(Card.GetOriginalAttribute)
 	if chk==0 then
 		return ct==3 or ct==4
-			or ct==1 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) 
-			or ct==2 and Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil)
-			or ct==5 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) 			
-			or ct==6 and Duel.IsExistingMatchingCard(s.desfilter,tp,0,LOCATION_MZONE,1,nil)
+			or (ct==1 and lg:FilterCount(Card.IsControler,nil,tp)>0)
+			or (ct==2 and lg:FilterCount(Card.IsControler,nil,1-tp)>0)
+			or (ct==5 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp))
+			or (ct==6 and Duel.IsExistingMatchingCard(s.desfilter,tp,0,LOCATION_MZONE,1,nil))
 	end
 	if ct==3 then
 		Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,1500)
@@ -37,19 +38,15 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 	end
-end
-function s.spfilter(c,e,tp)
-	return c:IsAttackAbove(2500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.desfilter(c)
-	return c:IsFaceup() and c:IsAttackBelow(3000)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetClassCount(Card.GetOriginalAttribute)
+	local lg=Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsLinkMonster),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local ct=lg:GetClassCount(Card.GetOriginalAttribute)
 	if ct==1 then
-		local g1=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
+		local g1=lg:Filter(Card.IsControler,nil,tp)>0
 		if #g1==0 then return end
-		for tc1 in aux.Next(g1) do
+		for tc1 in g1:Iter() do
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -58,9 +55,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			tc1:RegisterEffect(e1)
 		end
 	elseif ct==2 then
-		local g2=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+		local g2=lg:Filter(Card.IsControler,nil,1-tp)
 		if #g2==0 then return end
-		for tc2 in aux.Next(g2) do
+		for tc2 in g2:Iter() do
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)

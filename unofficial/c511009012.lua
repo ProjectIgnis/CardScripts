3d3c0196a19f142954310c36138d3e2627faad08
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-08-15 23:01:13 -0700

    Fix "Eva Epsilon"
    Counters could not be placed correctly when this card was sent to the GY.

--- unofficial/c511009012.lua
+++ unofficial/c511009012.lua
@@ -1,31 +1,44 @@
+--イーバ・イプシロン
 --Eva Epsilon
 local s,id=GetID()
 function s.initial_effect(c)
-	--Give Counter
+	--Register Life Star Counters on this card
+	local e0=Effect.CreateEffect(c)
+    	e0:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+    	e0:SetCode(EVENT_LEAVE_FIELD_P)
+    	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    	e0:SetOperation(s.regop)
+    	c:RegisterEffect(e0)
+	--Place Life Star Counters on monsters equal to the number of Life Star Counters this card had
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(39892082,1))
-	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetCategory(CATEGORY_COUNTER)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_LEAVE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCondition(s.damcon)
-	e1:SetOperation(s.damop)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCondition(s.ctcon)
+	e1:SetOperation(s.ctop)
+	e1:SetLabelObject(e0)
 	c:RegisterEffect(e1)
 end
-
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local ct=c:GetCounter(0x1109)
-	e:SetLabel(ct)
-	return ct>0 and c:IsLocation(LOCATION_GRAVE)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+    	local ct=e:GetHandler():GetCounter(0x1109)
+    	e:SetLabel(ct)
 end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.ctfilter(c)
+	return c:IsFaceup() and c:IsCanAddCounter(0x1109,1)
+end
+function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
+    	local ct=e:GetLabelObject():GetLabel()
+    	e:SetLabel(ct)
+    	return ct>0 and Duel.IsExistingMatchingCard(s.ctfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if #g==0 then return end
-	for i=1,ct do
-		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(83604828,1))
-		local tc=g:Select(tp,1,1,nil):GetFirst()
-		tc:AddCounter(0x1109,1)
-	end
+    	local g=Duel.GetMatchingGroup(s.ctfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+    	if #g==0 then return end
+    	for i=1,ct do
+        	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(83604828,1))
+        	local sg=g:Select(tp,1,1,nil)
+        	sg:GetFirst():AddCounter(0x1109,1)
+    	end
 end

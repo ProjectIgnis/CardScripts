4d246b9b96d57d75b2717f73b62d46d14d7597d2
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-08-15 22:17:36 -0700

    Fix "Greed Quasar (Manga)"
    Counters were not being placed by its own effect/general script clean-up.

--- unofficial/c511009013.lua
+++ unofficial/c511009013.lua
@@ -1,39 +1,46 @@
---Greed Quasar (anime)
+--グリード・クエーサー (manga)
+--Greed Quasar (manga)
 local s,id=GetID()
 function s.initial_effect(c)
-	--base attack
+	--Gains 300 ATK/DEF for each Life Star Counter on this card
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SET_BASE_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(s.atkval)
+	e1:SetValue(s.adval)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_SET_BASE_DEFENSE)
 	c:RegisterEffect(e2)
-
-	--Add counter
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e6:SetCode(EVENT_LEAVE_FIELD)
-	e6:SetRange(LOCATION_MZONE)
-	e6:SetOperation(s.addop2)
-	c:RegisterEffect(e6)
+	--Add Life Star Counters to this card from monsters it destroys by battle
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_LEAVE_FIELD_P)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.ctcon)
+	e3:SetOperation(s.ctop)
+	c:RegisterEffect(e3)
 end
-function s.atkval(e,c)
+function s.adval(e,c)
 	return c:GetCounter(0x1109)*300
 end
-function s.addop2(e,tp,eg,ep,ev,re,r,rp)
-	local count=0
-	local c=eg:GetFirst()
-	while c~=nil do
-		if c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_DESTROY) and c:GetReasonCard()==e:GetHandler() then
-			count=count+c:GetCounter(0x1109)
-		end
-		c=eg:GetNext()
-	end
-	if count>0 then
-		e:GetHandler():AddCounter(0x1109,count)
-	end
+function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
+    	local c=e:GetHandler()
+    	local bc=c:GetBattleTarget()
+    	local ct=bc:GetCounter(0x1109)
+    	return c:IsRelateToBattle() and c:IsFaceup() and ct>0 and c:IsCanAddCounter(0x1109,ct)
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+        local c=e:GetHandler()
+        local bc=c:GetBattleTarget()
+        local ct=bc:GetCounter(0x1109)
+        local count=0
+        if bc and bc:IsReason(REASON_BATTLE) and bc:GetReasonCard()==c then
+            count=count+ct
+        end
+        if count>0 then
+            Duel.Hint(HINT_CARD,0,id)
+            c:AddCounter(0x1109,count)
+        end    
 end

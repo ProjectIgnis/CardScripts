61022a3ed0f9f587ed45750fd2fb7838884fe1ed
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-08-17 14:06:52 +0200

    added new Rush cards

---
+++ rush/c160432001.lua
@@ -0,0 +1,47 @@
+--グレイストーム・レヴェリー
+--Greystorm Reverie
+local s,id=GetID()
+function s.initial_effect(c)
+	--Make itself gain 500 ATK until end of this turn
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,1,e:GetHandler()) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+	local ct=Duel.SendtoGrave(g,REASON_COST)
+	if ct>1 then
+		--Effect
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+			e1:SetValue(500)
+			c:RegisterEffect(e1)
+			if Duel.GetFieldGroupCount(tp,0,LOCATION_SZONE)==0 then
+				--Attack up to twice
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+				e2:SetRange(LOCATION_MZONE)
+				e2:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+				e2:SetValue(1)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				c:RegisterEffect(e2)
+			end
+		end
+	end
+end

931f07cc34243573c262ea800135ab5a1bc66a30
Naim <naimsantos45@gmail.com>
2022-08-18 16:01:28 -0300

    Fix "Magmassage"
    Fixed a bug where it would not be possible to activate the effect if only Magmassage (with a reduced level) was face-up on the field

--- rush/c160004036.lua
+++ rush/c160004036.lua
@@ -1,5 +1,5 @@
 --マグマッサーＧ
---Magrrmassa G
+--Magmassage
 local s,id=GetID()
 function s.initial_effect(c)
 	--ATK Change
@@ -20,7 +20,7 @@ function s.filter(c)
 	return c:IsFaceup() and c:IsLevelBelow(4)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -32,7 +32,8 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		if #g>0 then
-			Duel.HintSelection(g)
+			Duel.HintSelection(g,true)
+			--Increase ATK by 500 per card sent to the GY
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)

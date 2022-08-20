df7779b9386d00b4dd10267fce001785e3518d07
Naim <naimsantos45@gmail.com>
2022-08-20 14:15:19 -0300

    Fix "Fusion Substitute"
    The player should not be able to use the second effect is they cannot draw cards due to an effect

--- official/c74335036.lua
+++ official/c74335036.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Fusion.CreateSummonEff(c,nil,Fusion.OnFieldMat)
 	c:RegisterEffect(e1)
-	--to deck
+	--Shuffle monster into the Deck and draw 1 card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
@@ -22,7 +22,7 @@ function s.tdfilter(c)
 end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)

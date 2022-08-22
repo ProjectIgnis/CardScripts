32565acece293bff29dc3f59f5860682347c1b37
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-08-22 22:48:32 +0300

    "Full Armor Gravitation" update
    Small rework, some fixes, and general polish.

--- unofficial/c110000114.lua
+++ unofficial/c110000114.lua
@@ -1,10 +1,10 @@
 --フルアーマー・グラビテーション
 --Full Armor Gravitation
---reworked effect by senpaizuri
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -15,33 +15,34 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetDecktopGroup(tp,10)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummon(tp)
-		and g:FilterCount(Card.IsAbleToRemove,nil)==10 end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_DECK)
+		and g:FilterCount(Card.IsAbleToRemove,nil)==10 and not Duel.IsPlayerAffectedByEffect(tp,CARD_EHERO_BLAZEMAN) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function s.filter(c,e,tp)
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsType(TYPE_ARMOR)
+function s.spfilter(c,e,tp,seq)
+	return c:IsSequence(seq) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsType(TYPE_ARMOR)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetDecktopGroup(tp,10)
+	if #g==0 then return end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	if g:FilterCount(Card.IsAbleToRemove,nil)~=10 then return end
 	Duel.DisableShuffleCheck()
 	Duel.ConfirmDecktop(tp,10)
-	local tg={}
-	for tc in aux.Next(g) do
-		if s.filter(tc,e,tp) then table.insert(tg,tc) end
-	end
-	table.sort(tg,function(a,b) return a:GetSequence()>b:GetSequence() end)
-	local sg=Group.CreateGroup()
-	for _,tc in pairs(tg) do
-		if #sg==ft then break end
-		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-			sg:AddCard(tc)
+	if ft==0 then return Duel.Remove(g,POS_FACEUP,REASON_EFFECT) end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local seq=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)-1
+	local tc=nil
+	for i=1,#g do
+		tc=g:Filter(s.spfilter,nil,e,tp,seq):GetFirst()
+		if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 			g:RemoveCard(tc)
+			ft=ft-1
+			if ft==0 then break end
 		end
+		seq=seq-1
 	end
-	Duel.SpecialSummonComplete()
-	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	if Duel.SpecialSummonComplete()>0 then Duel.BreakEffect() end
+	if #g>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
 end

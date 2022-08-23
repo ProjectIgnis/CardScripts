9b6dce57adfe0bd4eba75c5c3b70633ccfc559af
Larry126 <28985622+larry126@users.noreply.github.com>
2022-08-23 09:47:00 +0800

    Update Necroshade (Anime)
    Effect handling in line with "Blackwing - Simoon the Poison Wind"

--- unofficial/c511023014.lua
+++ unofficial/c511023014.lua
@@ -1,40 +1,54 @@
 --E・HERO ネクロダークマン (Anime)
 --Elemental HERO Necroshade (Anime)
---fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
-	--
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(id)
-	e0:SetValue(SUMMON_TYPE_NORMAL)
-	c:RegisterEffect(e0)
-	--summon
+	--Normal Summon E・HERO without Tributing
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_GRAVE)
-	e1:SetCountLimit(1)
 	e1:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+	e1:SetCountLimit(1)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
-	e1:SetLabelObject(e0)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x3008}
-function s.filter(c,e)
-	return c:IsSetCard(0x3008) and c:IsSummonable(true,e)
+function s.filter(c,ec)
+	if not c:IsSetCard(0x3008) or not c:IsSummonableCard() then return false end
+	local e1=Effect.CreateEffect(ec)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetCondition(s.ntcon)
+	e1:SetReset(RESET_CHAIN)
+	c:RegisterEffect(e1)
+	local res=c:IsSummonable(true,nil)
+	e1:Reset()
+	return res
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e:GetLabelObject()) end
-	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e:GetLabelObject()):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,c):GetFirst()
 	if tc then
-		Duel.Summon(tp,tc,true,e:GetLabelObject())
+		local e1=Effect.CreateEffect(c)
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SUMMON_PROC)
+		e1:SetCondition(s.ntcon)
+		e1:SetReset(RESET_CHAIN)
+		tc:RegisterEffect(e1)
+		Duel.Summon(tp,tc,true,nil)
+		e1:Reset()
 	end
+end
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and c:GetLevel()>4 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end

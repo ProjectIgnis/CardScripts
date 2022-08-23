94d18eea9e5c67627bf948a11e7f8366642923c5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-08-23 13:39:47 +0300

    "Greystorm Reverie" fix
    Should check the whole field instead of only the opponent's Spell/Trap Zone & Field Zone for Spells/Traps.

--- rush/c160432001.lua
+++ rush/c160432001.lua
@@ -2,8 +2,9 @@
 --Greystorm Reverie
 local s,id=GetID()
 function s.initial_effect(c)
-	--Make itself gain 500 ATK until end of this turn
+	--Increase ATK
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
@@ -19,28 +20,26 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	--Requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
-	if Duel.SendtoGrave(g,REASON_COST)>0 then
-		--Effect
-		if c:IsRelateToEffect(e) and c:IsFaceup() then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-			e1:SetValue(500)
-			c:RegisterEffect(e1)
-			if Duel.GetFieldGroupCount(tp,0,LOCATION_SZONE)==0 then
-				--Attack up to twice
-				local e2=Effect.CreateEffect(c)
-				e2:SetType(EFFECT_TYPE_SINGLE)
-				e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-				e2:SetRange(LOCATION_MZONE)
-				e2:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
-				e2:SetValue(1)
-				e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-				c:RegisterEffect(e2)
-			end
-		end
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,1,1,c)
+	if Duel.SendtoGrave(g,REASON_COST)==0 then return end
+	--Effect
+	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+	e1:SetValue(500)
+	c:RegisterEffect(e1)
+	if not Duel.IsExistingMatchingCard(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) then
+		--Can make up to 2 attacks on monsters
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e2)
 	end
 end

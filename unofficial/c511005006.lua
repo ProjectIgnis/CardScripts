db75f75dc70a7d52d1dd97dc8787c5f3835e8938
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-08-31 20:29:39 -0700

    Fix "Line Monster K Horse"
    Was not destroying cards adjacent to it and fixed error if there were no cards to destroy on resolution.

--- unofficial/c511005006.lua
+++ unofficial/c511005006.lua
@@ -1,5 +1,6 @@
+--ラインモンスター Ｋホース
 --Line Monster K Horse
---scripted by Shad3, cleaned up and fixed by MLD
+--scripted by Shad3
 local s,id=GetID()
 function s.initial_effect(c)
 	--Normal/Special Summon effect
@@ -28,15 +29,28 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 s.listed_names={41493640}
+function s.desfilter(c,e)
+	local seq=e:GetHandler():GetSequence()
+	local sseq=c:GetSequence()
+	return sseq<5 and ((seq==0 and sseq==3) or (seq==1 and (sseq==4 or sseq==2)) or (seq==2 and (sseq==3 or sseq==1)) or (seq==3 and (sseq==0 or sseq==2)) 
+	or (seq==4 and sseq==1))
+end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_SZONE):Filter(s.desfilter,nil,e)
 	if chk==0 then return true end
-	local tc=Duel.GetFieldCard(1-tp,LOCATION_SZONE,4-e:GetHandler():GetSequence())
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,Group.FromCards(tc),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFieldCard(1-tp,LOCATION_SZONE,4-e:GetHandler():GetSequence())
-	if not tc then return end
-	Duel.HintSelection(Group.FromCards(tc))
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_SZONE):Filter(s.desfilter,nil,e)
+	if #g==0 then return end
+	local tc=nil
+	if #g==1 then
+		tc=g:GetFirst()
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		tc=g:Select(tp,1,1,nil)
+	end
+	Duel.HintSelection(tc)
 	Duel.Destroy(tc,REASON_EFFECT)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)

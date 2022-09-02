98c3d719659d61615b44992578a2ec6b0eb91d08
Larry126 <28985622+larry126@users.noreply.github.com>
2022-09-02 10:05:58 +0800

    Updated GX Magnet cards
    Overhaul the handling of Plus/Minus attributes

--- unofficial/c170000116.lua
+++ unofficial/c170000116.lua
@@ -3,7 +3,6 @@
 Duel.LoadScript("c419.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	c:AddPlusMinusAttribute()
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)

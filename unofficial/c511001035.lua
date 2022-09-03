a18cd59a42735fdbeac8d580aa4d0805c53f2187
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-09-02 20:29:11 -0700

    Update "Stonehenge Shield"
    Should be able to be activated at any time so long as a monster with 3000 or more original ATK is on the field.

--- unofficial/c511001035.lua
+++ unofficial/c511001035.lua
@@ -1,26 +1,10 @@
 --環状列石の結界
+--Stonehenge Shield
+--Rescripted by The Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCode(EVENT_CHAIN_SOLVED)
-	e2:SetLabelObject(e1)
-	e2:SetCondition(aux.PersistentTgCon)
-	e2:SetOperation(aux.PersistentTgOp(false))
-	c:RegisterEffect(e2)
-	--Destroy
+	aux.AddPersistentProcedure(c,nil,s.filter,CATEGORY_DISABLE+CATEGORY_ATKCHANGE,nil,nil,0x1c0,nil,nil,s.target)	
+	--Destroy this card if target monster leaves the field
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e3:SetRange(LOCATION_SZONE)
@@ -28,7 +12,7 @@ function s.initial_effect(c)
 	e3:SetCondition(s.descon)
 	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
-	--disable
+	--Negate target monster's effects
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetCode(EFFECT_DISABLE)
@@ -36,25 +20,23 @@ function s.initial_effect(c)
 	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e4:SetTarget(aux.PersistentTargetFilter)
 	c:RegisterEffect(e4)
+	--Set target monster's ATK to 0
 	local e5=e4:Clone()
-	e5:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e5:SetCode(EFFECT_SET_ATTACK)
 	e5:SetValue(0)
 	c:RegisterEffect(e5)
+	--Target monster cannot attack
+	local e6=e4:Clone()
+	e6:SetCode(EFFECT_CANNOT_ATTACK)
+	c:RegisterEffect(e6)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetAttacker():IsAttackAbove(3000)
+function s.filter(c)
+	return c:IsFaceup() and c:GetBaseAttack()>2999
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local tg=Duel.GetAttacker()
-	if chkc then return chkc==tg end
-	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
-	Duel.SetTargetCard(tg)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,tg,1,0,0)
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.NegateAttack()
-	end
+function s.target(e,tp,eg,ep,ev,re,r,rp,tc,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,tc,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,tc,1,0,0)
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetHandler():GetFirstCardTarget()

2593252341b5e4c8f8d7cfb15107f6af1aef3d5a
Naim <naimsantos45@gmail.com>
2022-09-07 09:35:38 -0300

    Add Rush card's scripts

---
+++ rush/c160011062.lua
@@ -0,0 +1,44 @@
+--スラッシュ・ヘイロー
+--Slash Halo
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Decrease the ATK of an opponent's monster by 1000
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,500) end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttackTarget()
+	local ac=Duel.GetAttacker()
+	return ac:IsControler(1-tp) and tc and tc:IsControler(tp) and tc:IsFaceup() and tc:IsAttackPos() and tc:IsRace(RACE_FAIRY)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local tc=Duel.GetAttacker()
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,tc,1,0,-1000)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.PayLPCost(tp,500)
+	--Effect
+	local tc=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if tc and tc:IsRelateToBattle() and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-1000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffectRush(e1)
+	end
+end

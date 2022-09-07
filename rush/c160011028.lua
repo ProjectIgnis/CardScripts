e2cc576d76a1338dadcbf86755ab87255e7df3f1
Naim <naimsantos45@gmail.com>
2022-09-07 07:21:59 -0300

    Add Rush card's scripts

---
+++ rush/c160011028.lua
@@ -0,0 +1,45 @@
+--新米寿司天使シャリエル
+--Shariel, the Novice Sushi Fairy
+local s,id=GetID()
+function s.initial_effect(c)
+	--Set 1 card to the field and add another to the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160005065,160004030,160203039}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,3,nil) and e:GetHandler():IsStatus(STATUS_SUMMON_TURN+STATUS_SPSUMMON_TURN)
+end
+function s.setfilter(c)
+	return c:IsCode(160005065) and c:IsSSetable()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thfilter(c)
+	return c:IsCode(160004030,160203039) and c:IsAbleToHand()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g==0 or Duel.SSet(tp,g)==0 then return end
+	if Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local tg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #tg>0 then
+			Duel.HintSelection(tg)
+			Duel.BreakEffect()
+			Duel.SendtoHand(tg,nil,REASON_EFFECT)
+		end
+	end
+end

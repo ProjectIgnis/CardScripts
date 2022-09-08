2bbca04e5e11e1755def67f55721975ac4e885d8
Hatter <sanity.sierota@protonmail.com>
2022-09-09 06:33:39 +0800

    fix "Fire Formation - Yoko"
    should not perform anything on resolution if the player did not choose to destroy a monster

--- official/c36499284.lua
+++ official/c36499284.lua
@@ -7,7 +7,6 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--Increase ATK
 	local e2=Effect.CreateEffect(c)
@@ -30,12 +29,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		e:SetCategory(CATEGORY_DESTROY)
 		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e:SetOperation(s.activate)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 		local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,1,nil)
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	else
 		e:SetCategory(0)
 		e:SetProperty(0)
+		e:SetOperation(nil)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)

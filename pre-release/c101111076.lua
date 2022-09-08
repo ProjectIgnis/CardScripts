bf5d07a840b9d492035c121718fb091edfd30ae1
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-09-08 23:37:32 +0300

    Added new cards' scripts

---
+++ pre-release/c101111076.lua
@@ -0,0 +1,57 @@
+--混沌変幻
+--Chaos Phantasm
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.tgfilter(c,e)
+	if not (c:IsFaceup() and c:HasLevel() and c:IsCanBeEffectTarget(e)) then return false end
+	if c:IsAttribute(ATTRIBUTE_LIGHT) then
+		return c:IsType(TYPE_TUNER)
+	elseif c:IsAttribute(ATTRIBUTE_DARK) then
+		return not c:IsType(TYPE_TUNER) and c:IsLevelBelow(8)
+	end
+	return false
+end
+function s.rescon(sg,e,tp,mg)
+	return #sg==2 and sg:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_LIGHT) and sg:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_DARK)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,sg:GetSum(Card.GetLevel))
+end
+function s.spfilter(c,e,tp,lvl)
+	return c:IsLevel(lvl) and c:IsType(TYPE_SYNCHRO) and c:IsAttribute(ATTRIBUTE_LIGHT|ATTRIBUTE_DARK)
+		and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=Duel.GetMatchingGroup(s.tgfilter,tp,LOCATION_REMOVED,0,nil,e)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and s.tgfilter(chkc,e) end
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE)
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,tg,#tg,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg==2 and Duel.SendtoGrave(tg,REASON_EFFECT+REASON_RETURN)==2 then
+		local og=Duel.GetOperatedGroup()
+		if og:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)~=2 then return end
+		local lvl=tg:GetSum(Card.GetLevel)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,lvl)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

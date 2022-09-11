b146c202de222056a8605194589249d983001978
Naim <naimsantos45@gmail.com>
2022-09-11 16:16:53 -0300

    Remove scripts for Legends cards that ae normal monsters

--- rush/c160001000.lua
+++
@@ -1,6 +0,0 @@
---ブルーアイズ・ホワイト・ドラゴン
---Blue-Eyes White Dragon (Rush)
-local s,id=GetID()
-function s.initial_effect(c)
-	Card.Alias(c,CARD_BLUEEYES_W_DRAGON)
-end
--- rush/c160417002.lua
+++
@@ -1,6 +0,0 @@
--- ブラッド・ヴォルス
--- Vorse Raider
-local s,id=GetID()
-function s.initial_effect(c)
-	Card.Alias(c,14898066)
-end

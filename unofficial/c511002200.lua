61baaf3cbffb9a7ec0296c9b28720f7f88305376
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-09-14 17:40:10 -0700

    Update "Void Ogre Dragon (Manga)"
    Based off the text from the manga panels, Void Ogre's ATK gain should only last until the end of the turn it was used.

--- unofficial/c511002200.lua
+++ unofficial/c511002200.lua
@@ -52,7 +52,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
 		e1:SetValue(500)
 		c:RegisterEffect(e1)
 	end

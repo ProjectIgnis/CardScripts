5a317de3ed23edc162aa4c01353d5181e44e8bc9
Naim <naimsantos45@gmail.com>
2022-09-18 13:52:34 -0300

    Update "Scareclaw Kshatri-La"
    Fixed a bug where it would be unable to negate the effects of monsters that were face-down when they battled

--- pre-release/c101111007.lua
+++ pre-release/c101111007.lua
@@ -9,55 +9,43 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_DEFENSE_ATTACK)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	-- Negate effects on battle
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_BE_BATTLE_TARGET)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.negcon)
-	e2:SetOperation(s.negop)
-	c:RegisterEffect(e2)
 	-- Special Summon this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetCondition(function() return Duel.IsMainPhase() end)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	-- Negate effects of monsters that battle your "Scareclaw" or "Kshatri-la" monsters
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_HAND)
-	e3:SetCountLimit(1,id)
-	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e3:SetCondition(function() return Duel.IsMainPhase() end)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_BATTLE_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.negcon)
+	e3:SetOperation(s.negop)
 	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EVENT_BATTLED)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetOperation(s.operation)
+	c:RegisterEffect(e5)
 end
 s.listed_series={SET_SCARECLAW,SET_KSHATRI_LA}
-function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	local a,b=Duel.GetBattleMonster(tp)
-	if a and b and (a:IsSetCard(SET_SCARECLAW) or a:IsSetCard(SET_KSHATRI_LA)) and b:IsNegatableMonster() then
-		e:SetLabelObject(b)
-		return true
-	end
-	e:SetLabelObject(nil)
-	return false
-end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	if not tc then return end
-	-- Negate its effects
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_DISABLE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	tc:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_DISABLE_EFFECT)
-	e2:SetValue(RESET_TURN_SET)
-	tc:RegisterEffect(e2)
+function s.archfilter(c)
+	return c:IsSetCard(SET_SCARECLAW) or c:IsSetCard(SET_KSHATRI_LA)
 end
 function s.rmfilter(c)
-	return (c:IsSetCard(SET_SCARECLAW) or c:IsSetCard(SET_KSHATRI_LA)) and c:IsAbleToRemove()
-		and (c:IsLocation(LOCATION_HAND) or aux.SpElimFilter(c,true))
+	return s.archfilter(c) and c:IsAbleToRemove() and (c:IsLocation(LOCATION_HAND) or aux.SpElimFilter(c,true))
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -76,4 +64,55 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 	end
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local a,b=Duel.GetBattleMonster(tp)
+	if not a and b then return false end
+	if a:IsControler(tp) and s.archfilter(a) then
+		e:SetLabelObject(b)
+		return true
+	elseif a:IsControler(1-tp) and s.archfilter(b) then
+		e:SetLabelObject(a)
+		return true
+	end
+	e:SetLabelObject(nil)
+	return false
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	if not tc then return end
+	local property=tc:IsPosition(POS_FACEDOWN) and EFFECT_FLAG_SET_AVAILABLE or 0
+	-- Negate its effects
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetProperty(property)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+	tc:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	e2:SetValue(RESET_TURN_SET)
+	tc:RegisterEffect(e2)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local a,b=Duel.GetBattleMonster(tp)
+	local p=e:GetHandler():GetControler()
+	if not b==nil then return end
+	local tc=nil
+	if a:GetControler()==p and s.archfilter(a) and b:IsStatus(STATUS_BATTLE_DESTROYED) then
+		tc=b
+	elseif b:GetControler()==p and s.archfilter(b) and b:IsStatus(STATUS_BATTLE_DESTROYED) then
+		tc=a end
+	if not tc then return end
+	-- Negate its effects
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_EXC_GRAVE)
+	tc:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD_EXC_GRAVE)
+	tc:RegisterEffect(e2)
 end

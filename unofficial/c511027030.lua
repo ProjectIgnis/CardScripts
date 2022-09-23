6c79bd2ae5556d0111a05c68e7abaeba807ae53c
Larry126 <28985622+larry126@users.noreply.github.com>
2022-09-23 19:40:29 +0800

    Update c511027030.lua

--- unofficial/c511027030.lua
+++ unofficial/c511027030.lua
@@ -1,8 +1,8 @@
---サンダー・シーホース
---Thunder Sea Horse (Anime)
+--サンダー・シーホース (Manga)
+--Thunder Sea Horse (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
-	--search
+	--Search
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -14,28 +14,25 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsDiscardable() end
-	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
-function s.filter(c,g)
-	return g:IsExists(Card.IsCode,1,c,c:GetCode())
+function s.rescon(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetCode)==1
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then
-		local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_DECK,0,nil)
-		return g:IsExists(s.filter,1,nil,g)
+		local g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
+		return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_DECK,0,nil)
-	local sg=g:Filter(s.filter,nil,g)
-	if #sg==0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local hg1=sg:Select(tp,1,1,nil)
-	local hg2=sg:Filter(Card.IsCode,hg1:GetFirst(),hg1:GetFirst():GetCode())
-	hg1:AddCard(hg2:GetFirst())
-	Duel.SendtoHand(hg1,nil,REASON_EFFECT)
-	Duel.ConfirmCards(1-tp,hg1)
+	local g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
+	if #g<2 then return end
+	local hg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_ATOHAND)
+	if #hg==2 then
+		Duel.SendtoHand(hg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,hg)
+	end
 end

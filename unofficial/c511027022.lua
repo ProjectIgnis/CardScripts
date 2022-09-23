e8d3ded025d757092fb49a28e196b7b970a1b64b
Larry126 <28985622+larry126@users.noreply.github.com>
2022-09-23 19:10:17 +0800

    Update c511027022.lua

--- unofficial/c511027022.lua
+++ unofficial/c511027022.lua
@@ -1,9 +1,10 @@
---Giant's Might
+--巨大化 (Manga)
+--Megamorph (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
     --activate
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_ATKCHANGE)
+    e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -14,7 +15,7 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
     if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
     Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
@@ -24,11 +25,11 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-        e1:SetValue(tc:GetAttack()*0.2)
+        e1:SetValue(math.floor(tc:GetAttack()*0.2))
         tc:RegisterEffect(e1)
         local e2=e1:Clone()
         e2:SetCode(EFFECT_UPDATE_DEFENSE)
-        e2:SetValue(tc:GetDefense()*0.2)
+		e2:SetValue(math.floor(tc:GetDefense()*0.2))
         tc:RegisterEffect(e2)
     end
 end

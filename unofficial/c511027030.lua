8f3765166ecdd805c93b4e0eba75ef82100e30d1
botist <66886150+botist@users.noreply.github.com>
2022-09-23 07:41:49 -0400

    Add "Thunder Sea Horse (Manga)" script (#712)
    * Add "Thunder Sea Horse (Manga)" script
    
    Co-authored-by: Larry126 <28985622+larry126@users.noreply.github.com>

---
+++ unofficial/c511027030.lua
@@ -0,0 +1,38 @@
+--サンダー・シーホース (Manga)
+--Thunder Sea Horse (Manga)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetCode)==1
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then
+		local g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
+		return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
+	if #g<2 then return end
+	local hg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_ATOHAND)
+	if #hg==2 then
+		Duel.SendtoHand(hg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,hg)
+	end
+end

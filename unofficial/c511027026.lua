e6641823671c9dfb239ec7579e22345ff5d644bf
Larry126 <28985622+larry126@users.noreply.github.com>
2022-09-23 20:08:56 +0800

    Update c511027026.lua

--- unofficial/c511027026.lua
+++ unofficial/c511027026.lua
@@ -1,58 +1,49 @@
+--アルケミー・サイクル (Anime)
 --Alchemy Cycle (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-    e1:SetCondition(s.condition)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetAttacker()
-    if tc:IsControler(1-tp) then tc=Duel.GetAttackTarget() end
-    return tc and tc:IsPosition(POS_FACEUP_ATTACK) and tc:IsOnField() and tc:IsAbleToRemove() 
-        and tc:IsRelateToBattle() and Duel.IsPlayerCanDraw(tp, 1) and tc:GetAttack()>0
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetAttacker()
-    if tc:IsControler(1-tp) then tc=Duel.GetAttackTarget() end
-    if not tc or not tc:IsRelateToBattle() then return end
-    local c=e:GetHandler()
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e1:SetValue(1)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-    tc:RegisterEffect(e1)
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetCode(EFFECT_SET_ATTACK_FINAL)
-    e2:SetValue(0)
-    e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-    tc:RegisterEffect(e2)
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    e3:SetCode(EVENT_DAMAGE_STEP_END)
-    e3:SetTarget(s.target)
-    e3:SetOperation(s.banop)
-    e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-    tc:RegisterEffect(e3)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_REMOVE+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	local tc=Duel.GetBattleMonster(tp)
+	if chk==0 then return tc and tc:IsAttackPos() and tc:HasNonZeroAttack() end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetBattleMonster(tp)
+	if not tc or not tc:IsRelateToBattle() then return end
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e2:SetValue(0)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	tc:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_DAMAGE_STEP_END)
+	e3:SetOperation(s.banop)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+	tc:RegisterEffect(e3)
 end
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetAttacker()
-    if tc:IsControler(1-tp) then tc=Duel.GetAttackTarget() end
-    if not tc or not tc:IsRelateToBattle() then return end
-    Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
-    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-    Duel.Draw(p,d,REASON_EFFECT)
+	local c=e:GetHandler()
+	if c:IsRelateToBattle() and Duel.Remove(c,POS_FACEUP,REASON_EFFECT)>0 and c:IsLocation(LOCATION_REMOVED) then
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
 end

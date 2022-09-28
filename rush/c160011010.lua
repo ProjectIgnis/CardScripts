00858238666854be12f00840143beed199e74d26
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-09-28 14:34:38 +0300

    Exclude Equip Traps

--- rush/c160011010.lua
+++ rush/c160011010.lua
@@ -14,7 +14,9 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.filter(c)
-	return c:GetEquipCount()>0
+	if c:GetEquipCount()==0 then return false end
+	local eqg=c:GetEquipGroup()
+	return eqg:IsExists(Card.IsSpell,1,nil)
 end
 function s.con(e)
 	return Duel.IsExistingMatchingCard(s.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)

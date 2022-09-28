8fd7f66374322bbc02fd73ec21e40cc682af198d
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-09-28 13:36:27 +0200

    Added new Rush card's script

---
+++ rush/c160011010.lua
@@ -0,0 +1,23 @@
+--アストロソルジャ
+--Astrosoldier
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.con)
+	e1:SetValue(700)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	if c:GetEquipCount()==0 then return false end
+	local eqg=c:GetEquipGroup()
+	return eqg:IsExists(Card.IsSpell,1,nil)
+end
+function s.con(e)
+	return Duel.IsExistingMatchingCard(s.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end

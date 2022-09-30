12819201ef352f1b2e9b6c8447b0749a423e7bf4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-09-30 13:04:45 -0700

    Update "Cocoon of Ultra Evolution (Skill Card)"
    Fixed issue with the second Skill not being able to resolve, along with updating to not allow activation if the Skill player had already Normal/Special Summoned, also updated to have it flip itself over during the End Phase per text as opposed to resolution.

--- skill/c300104004.lua
+++ skill/c300104004.lua
@@ -1,4 +1,5 @@
 --Cocoon of Ultra Evolution (Skill Card)
+--Updated by The Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
     aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
@@ -9,7 +10,7 @@ function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
     --condition
     return aux.CanActivateSkill(tp) and (b1 or b2)
 end
---effect 1
+--Tribute 1 Insect monster with an Equip Card to Special Summon 1 Insect monster from your Deck
 function s.cfilter(c)
     return c:IsFaceup() and c:IsRace(RACE_INSECT) and c:IsReleasableByEffect() and c:GetEquipCount()>0
 end
@@ -22,10 +23,13 @@ function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
         if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then loc=0 end
         return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,loc,1,nil)
             and Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+			and Duel.GetActivityCount(ep,ACTIVITY_NORMALSUMMON)==0 
+			and Duel.GetActivityCount(ep,ACTIVITY_SPSUMMON)==0
     end
     Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,0,0)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+--Shuffle 1 Insect monster from your GY into Deck and draw 1 card
 function s.tdfilter(c)
     return c:IsRace(RACE_INSECT) and c:IsAbleToDeck()
 end
@@ -41,16 +45,9 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_CARD,tp,id)
     local b1=(Duel.GetFlagEffect(ep,id)==0 and s.sumtg(e,tp,eg,ep,ev,re,r,rp,0))
     local b2=(Duel.GetFlagEffect(ep,id+1)==0 and s.tdtg(e,tp,eg,ep,ev,re,r,rp,0))
-    local p=0
-    if b1 and b2 then
-        p=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
-    elseif b1 then
-        p=Duel.SelectOption(tp,aux.Stringid(id,0))
-    else
-        p=Duel.SelectOption(tp,aux.Stringid(id,1))
-    end
+    local p=Duel.SelectEffect(tp,{b1,aux.Stringid(id,0)},{b2,aux.Stringid(id,1)})
     if not (b1 or b2) then return false end
-    if (b1 and p==0) or Duel.GetFlagEffect(ep,id+1)>0 then
+    if (b1 and p==1) or Duel.GetFlagEffect(ep,id+1)>0 then
         Duel.RegisterFlagEffect(ep,id,0,0,0)
         local e1=Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_FIELD)
@@ -79,20 +76,31 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
                 sg:GetFirst():RegisterEffect(e1)
                 Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
                 e1:Reset() 
-                Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
             end
         end
-    elseif (b2 and p==1) or Duel.GetFlagEffect(ep,id)>0 then
+    elseif (b2 and p==2) or Duel.GetFlagEffect(ep,id)>0 then
         Duel.RegisterFlagEffect(ep,id+1,0,0,0)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
         local tc=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
         if Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
             if tc:IsLocation(LOCATION_DECK) then Duel.ShuffleDeck(tc:GetControler()) end
             Duel.BreakEffect()
             Duel.Draw(tp,1,REASON_EFFECT)
-            Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
         end
     end
+	--Flip this Skill over during End Phase
+    local e3=Effect.CreateEffect(e:GetHandler())
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_PHASE+PHASE_END)
+    e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e3:SetCountLimit(1)
+    e3:SetOperation(s.EPop)
+    e3:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e3,tp)
 end
 function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
     return se~=e:GetLabelObject()
+end
+function s.EPop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
 end

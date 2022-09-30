5fb36537a29d748188307e229e5e6c98c22cd754
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-10-01 00:49:46 +0300

    "Black Luster Soljersey - Envoy of the Drying" fix
    Undeclared parameter.

--- rush/c160011017.lua
+++ rush/c160011017.lua
@@ -1,5 +1,5 @@
+--カオス・ソルジャージ －乾燥の使者－
 --Black Luster Soljersey - Envoy of the Drying
--- カオス・ソルジャージ －乾燥の使者－
 local s,id=GetID()
 function s.initial_effect(c)	
 	--Shuffle 1 level 8 monster from opponent's field to deck
@@ -26,7 +26,6 @@ function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
 		return aux.SelectUnselectGroup(cg,e,tp,2,2,s.tdcostrescon,0)
 	end
 end
---shuffle up to 2 spellcaster to the deck
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.tdfilter),tp,0,LOCATION_MZONE,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_MZONE)
@@ -42,10 +41,11 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 		--Effect
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 		local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.tdfilter),tp,0,LOCATION_MZONE,1,1,nil)
+		if #g==0 then return end
 		g=g:AddMaximumCheck()
-		Duel.HintSelection(g)
-		if #g>0 then
-			Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+		Duel.HintSelection(g,true)
+		if Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)>0 then
+			local c=e:GetHandler()
 			--Cannot attack directly
 			local e1=Effect.CreateEffect(c)
 			e1:SetDescription(3207)

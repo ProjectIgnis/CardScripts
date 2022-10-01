f4737a05a7d6c4c67a7a308629850cb7eef5412b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-10-01 19:55:05 +0300

    Added new cards' scripts

---
+++ pre-release/c101111004.lua
@@ -0,0 +1,123 @@
+--深淵の獣バルドレイク
+--Bystial Baldrake
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon itself from the hand (Ignition)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(aux.NOT(s.spquickcon))
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Special Summon itself from the hand (Quick if the opponent controls monsters)
+	local e2=e1:Clone()
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e2:SetCondition(s.spquickcon)
+	c:RegisterEffect(e2)
+	--Banish 1 monster summoned by the opponent
+	local e3a=Effect.CreateEffect(c)
+	e3a:SetDescription(aux.Stringid(id,1))
+	e3a:SetCategory(CATEGORY_REMOVE)
+	e3a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3a:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3a:SetCode(EVENT_CUSTOM+id)
+	e3a:SetRange(LOCATION_MZONE)
+	e3a:SetCountLimit(1,{id,1})
+	e3a:SetCost(s.rmcost)
+	e3a:SetTarget(s.rmtg)
+	e3a:SetOperation(s.rmop)
+	c:RegisterEffect(e3a)
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	e3a:SetLabelObject(g)
+	--Register summons
+	local e3b=Effect.CreateEffect(c)
+	e3b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3b:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3b:SetRange(LOCATION_MZONE)
+	e3b:SetLabelObject(e3a)
+	e3b:SetOperation(s.regop)
+	c:RegisterEffect(e3b)
+end
+function s.spquickcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+end
+function s.spfilter(c,tp)
+	return c:IsAttribute(ATTRIBUTE_LIGHT+ATTRIBUTE_DARK) and c:IsAbleToRemove() and aux.SpElimFilter(c,true)
+		and c:IsFaceup() and Duel.GetMZoneCount(tp,c)>0
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and s.spfilter(chkc,tp) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_GRAVE,1,nil,tp)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_REMOVED) and c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsAttribute,1,false,nil,c,ATTRIBUTE_DARK+ATTRIBUTE_LIGHT) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsAttribute,1,1,false,nil,c,ATTRIBUTE_DARK+ATTRIBUTE_LIGHT)
+	Duel.Release(g,REASON_COST)
+end
+function s.tgfilter(c,tp,e)
+	return c:IsSummonPlayer(1-tp) and c:IsLocation(LOCATION_MZONE)
+		and c:IsAbleToRemove() and c:IsType(TYPE_RITUAL|TYPE_EXTRA)
+		and (not e or c:IsCanBeEffectTarget(e))
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=e:GetLabelObject():Filter(s.tgfilter,nil,tp,e)
+	if chkc then return g:IsContains(chkc) and s.tgfilter(chkc,tp,nil) end
+	if chk==0 then return #g>0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	local tc=nil
+	if #g==1 then
+		tc=g:GetFirst()
+		Duel.SetTargetCard(tc)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		tc=g:Select(tp,1,1,nil)
+		Duel.SetTargetCard(tc)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)>0 then return end
+	local c=e:GetHandler()
+	local tg=eg:Filter(s.tgfilter,nil,tp)
+	if #tg>0 then
+		for tc in aux.Next(tg) do
+			tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+		end
+		local g=e:GetLabelObject():GetLabelObject()
+		if Duel.GetCurrentChain()==0 then g:Clear() end
+		g:Merge(tg)
+		g:Remove(function(c) return c:GetFlagEffect(id)==0 end,nil)
+		e:GetLabelObject():SetLabelObject(g)
+		Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+id,e,0,tp,tp,0)
+	end
+end

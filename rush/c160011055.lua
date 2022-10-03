486b8ec1987acd9774b8e22dcf6bf6d50950e883
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-10-03 12:53:04 +0200

    added Music Princess's Prelude

---
+++ rush/c160011055.lua
@@ -0,0 +1,44 @@
+--
+--Music Princess's Prelude
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+end
+function s.filter(c,e,tp)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.thfilter(c)
+	return c:IsCode(CARD_FUSION) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		local g2=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_GRAVE,0,nil)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		if g:GetFirst():IsRace(RACE_WARRIOR) and #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			local sg=g2:Select(tp,1,1,nil)
+			Duel.HintSelection(sg)
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		end
+	end
+end

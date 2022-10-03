54577a137ea7bd4f5474fb07e71805ee5d40ce14
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-10-03 16:22:45 +0300

    Update c160011055.lua

--- rush/c160011055.lua
+++ rush/c160011055.lua
@@ -1,4 +1,4 @@
---
+--楽姫の前奏曲
 --Music Princess's Prelude
 --scripted by YoshiDuels
 local s,id=GetID()
@@ -17,13 +17,14 @@ end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
-function s.filter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function s.thfilter(c)
 	return c:IsCode(CARD_FUSION) and c:IsAbleToHand()
@@ -31,14 +32,15 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if #g>0 then
-		local g2=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_GRAVE,0,nil)
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		if g:GetFirst():IsRace(RACE_WARRIOR) and #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 and g:GetFirst():IsRace(RACE_WARRIOR) then
+		local g2=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,nil)
+		if #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 			local sg=g2:Select(tp,1,1,nil)
-			Duel.HintSelection(sg)
+			Duel.BreakEffect()
 			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
 		end
 	end
 end

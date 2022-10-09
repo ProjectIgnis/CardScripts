757eea435203d9b9767a2f5ae1b44968e66c83be
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-10-09 12:34:58 +0200

    added new Rush cards

---
+++ rush/c160205064.lua
@@ -0,0 +1,39 @@
+--サタニウスの宝珠
+--Cintamani of Saturnius
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.drcon)
+	e1:SetCost(s.drcost)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsLevel,6),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.drcostfilter(c)
+	return c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.drcostfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.drcostfilter,tp,LOCATION_HAND,0,1,1,nil)
+	if Duel.SendtoGrave(g,REASON_COST)>0 then
+		--Effect
+		Duel.Draw(tp,2,REASON_EFFECT)
+	end
+end

1136af21c7f4f610e7afb375e4ba9570bfd7b879
Naim <naimsantos45@gmail.com>
2022-10-10 18:11:06 -0300

    "Zoodiac Boarbow" fix
    It should not be changed to Defense position if no cards were sent to the GY

--- official/c74393852.lua
+++ official/c74393852.lua
@@ -1,10 +1,11 @@
 --十二獣ワイルドボウ
+--Zoodiac Boarbow
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
+	--Xyz Summon Procedure
 	Xyz.AddProcedure(c,nil,4,5,s.ovfilter,aux.Stringid(id,0),99,s.xyzop)
 	c:EnableReviveLimit()
-	--atk
+	--Gain the ATK/DEF of all "Zoodiac" monsters attached to itself
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -16,25 +17,25 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
 	e2:SetValue(s.defval)
 	c:RegisterEffect(e2)
-	--to grave
+	--Send cards on the field to the GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_TOGRAVE)
+	e3:SetCategory(CATEGORY_TOGRAVE+CATEGORY_POSITION)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_BATTLE_DAMAGE)
 	e3:SetCondition(s.condition)
 	e3:SetTarget(s.target)
 	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
-	--direct attack
+	--Can attack directly
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_DIRECT_ATTACK)
 	c:RegisterEffect(e4)
 end
-s.listed_series={0xf1}
+s.listed_series={SET_ZOODIAC}
 function s.ovfilter(c,tp,lc)
-	return c:IsFaceup() and c:IsSetCard(0xf1,lc,SUMMON_TYPE_XYZ,tp) and not c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,id)
+	return c:IsFaceup() and c:IsSetCard(SET_ZOODIAC,lc,SUMMON_TYPE_XYZ,tp) and not c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,id)
 end
 function s.xyzop(e,tp,chk)
 	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
@@ -42,21 +43,21 @@ function s.xyzop(e,tp,chk)
 	return true
 end
 function s.atkfilter(c)
-	return c:IsSetCard(0xf1) and c:GetAttack()>=0
+	return c:IsSetCard(SET_ZOODIAC) and c:GetAttack()>=0
 end
 function s.atkval(e,c)
 	local g=e:GetHandler():GetOverlayGroup():Filter(s.atkfilter,nil)
 	return g:GetSum(Card.GetAttack)
 end
 function s.deffilter(c)
-	return c:IsSetCard(0xf1) and c:GetDefense()>=0
+	return c:IsSetCard(SET_ZOODIAC) and c:GetDefense()>=0
 end
 function s.defval(e,c)
 	local g=e:GetHandler():GetOverlayGroup():Filter(s.deffilter,nil)
 	return g:GetSum(Card.GetDefense)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and e:GetHandler():GetOverlayCount()>=12
+	return ep==1-tp and e:GetHandler():GetOverlayCount()>=12
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND+LOCATION_ONFIELD)
@@ -66,12 +67,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND+LOCATION_ONFIELD)
-	Duel.SendtoGrave(g,REASON_EFFECT)
-	local og=Duel.GetOperatedGroup()
-	if #og>0 then
-		Duel.BreakEffect()
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
+		local og=Duel.GetOperatedGroup()
+		if og:FilterCount(Card.IsLocation,0,LOCATION_GRAVE)==0 then return end
 		local c=e:GetHandler()
 		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			Duel.BreakEffect()
 			Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
 		end
 	end

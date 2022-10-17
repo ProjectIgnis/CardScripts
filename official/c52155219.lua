f0191e9c7c8f7c56b0e727fbea28fe3ec9942dc4
Naim <naimsantos45@gmail.com>
2022-10-17 12:50:26 -0300

    Update "Salamangreat Circle"
    Added Hint when selecting the card + updates to use the Duel.SelectEffect function

--- official/c52155219.lua
+++ official/c52155219.lua
@@ -1,7 +1,6 @@
 --転生炎獣の炎陣
 --Salamangreat Circle
 --Scripted by Eerie Code
-
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -13,21 +12,15 @@ function s.initial_effect(c)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x119}
-
+s.listed_series={SET_SALAMANGREAT}
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.immfilter(chkc) end
 	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
 	local b2=Duel.IsExistingTarget(s.immfilter,tp,LOCATION_MZONE,0,1,nil)
 	if chk==0 then return b1 or b2 end
-	local op=0
-	if b1 and b2 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
-	elseif b1 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,0))
-	else
-		op=Duel.SelectOption(tp,aux.Stringid(id,1))+1
-	end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
 	if op==0 then
 		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 		e:SetProperty(0)
@@ -36,11 +29,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		e:SetCategory(0)
 		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
 		e:SetOperation(s.immop)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
 		Duel.SelectTarget(tp,s.immfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	end
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x119) and c:IsMonster() and c:IsAbleToHand()
+	return c:IsSetCard(SET_SALAMANGREAT) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
@@ -51,18 +45,18 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.immfilter(c)
-	return c:IsLinkMonster() and c:IsSetCard(0x119) and c:IsReincarnationSummoned()
+	return c:IsLinkMonster() and c:IsSetCard(SET_SALAMANGREAT) and c:IsReincarnationSummoned()
 end
 function s.immop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		--Unaffected by monster effects
 		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3101)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
 		e1:SetRange(LOCATION_MZONE)
 		e1:SetCode(EFFECT_IMMUNE_EFFECT)
-		e1:SetDescription(3101)
 		e1:SetValue(s.efilter)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)

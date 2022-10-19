115d9aff461e3704b0c160d611359875a31dd010
TheFlameFlash <omerahmed2001141@gmail.com>
2022-10-19 23:20:33 +0300

    Update Rule of the day
    updated with the following effect:
    Synchro Harmony
    if a player Normal Summons a tuner monster, this turn that player can Normal Summon a non-tuner monster in addition to the Normal Summon/Set and vice versa

--- unofficial/c777777777.lua
+++ unofficial/c777777777.lua
@@ -1,8 +1,7 @@
---Rule of the day "Ritual Re-rite"
+--Rule of the day "Synchro Harmony"
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.GlobalCheck(s,function()
-		--place field
 		local e1=Effect.CreateEffect(c)
 		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -16,74 +15,53 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e1:SetCountLimit(1)
-	e1:SetCondition(s.searchcon)
-	e1:SetTarget(s.searchtg)
-	e1:SetOperation(s.searchop)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.notuncon)
+	e1:SetOperation(s.nsop)
 	Duel.RegisterEffect(e1,0)
 	local e2=e1:Clone()
 	Duel.RegisterEffect(e2,1)
-	local e3=Effect.CreateEffect(e:GetHandler())
-	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
-	e3:SetCountLimit(1)
-	e3:SetCondition(s.thcond)
-	e3:SetTarget(s.thtg)
-	e3:SetOperation(s.tgop)
+	local e3=e1:Clone()
+	e3:SetCondition(s.tuncon)
+	e3:SetOperation(s.nsop2)
 	Duel.RegisterEffect(e3,0)
 	local e4=e3:Clone()
 	Duel.RegisterEffect(e4,1)
 end
-function s.thfilter(c)
-	return c:IsRitualSpell() and c:IsAbleToHand()
+function s.cfilter(c,tp)
+	return c:IsFaceup() and not c:IsType(TYPE_TUNER) and c:IsSummonPlayer(tp)
 end
-function s.searchcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id)==0 and Duel.IsTurnPlayer(tp)
+function s.notuncon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
 end
-function s.searchtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFlagEffect(tp,id)==0
-		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
-		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
-	local apply=0
-	if Duel.GetFlagEffect(tp,id)==0 and Duel.SelectYesNo(tp,aux.Stringid(4388680,1)) then
-		apply=1
-		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-	end
-	e:SetLabel(apply)
-end 
-function s.searchop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==0 then return end
+function s.cfilter2(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_TUNER) and c:IsSummonPlayer(tp)
+end
+function s.tuncon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter2,1,nil,tp)
+end
+function s.nsop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)>0 then return end
 	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(15983048,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_TUNER))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
-function s.thcond(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id+1)==0
-end
-function s.thfilter2(c,id)
-	return (c:GetReason()&REASON_RITUAL)>0 and c:IsMonster() and c:GetTurnID()==id and c:IsAbleToHand()
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter2),tp,LOCATION_GRAVE,0,1,nil,Duel.GetTurnCount()) end
-	local apply=0
-	if Duel.GetFlagEffect(tp,id+1)==0 and Duel.SelectYesNo(tp,aux.Stringid(37021315,1)) then
-		apply=1
-	end
-	e:SetLabel(apply)
-end
-function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==0 then return end
-	Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter2),tp,LOCATION_GRAVE,0,1,1,nil,Duel.GetTurnCount())
-	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+function s.nsop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)>0 then return end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(15983048,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+	e1:SetTarget(aux.NOT(aux.TargetBoolFunction(Card.IsType,TYPE_TUNER)))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end

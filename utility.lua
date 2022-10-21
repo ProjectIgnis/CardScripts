6e5ff1597ec3e811e8bf1a621c483d8b347cdeb8
Edoardo Lolletti <edoardo762@gmail.com>
2022-10-21 11:59:28 +0200

    Add type, race and attribute formatting

--- utility.lua
+++ utility.lua
@@ -691,7 +691,96 @@ local positions={
 	[POS_DEFENSE]='"POS_DEFENSE"'
 }
 
-local function prettyPrintCardRaw(c)
+local types={
+	[TYPE_MONSTER]='"TYPE_MONSTER"',
+	[TYPE_SPELL]='"TYPE_SPELL"',
+	[TYPE_TRAP]='"TYPE_TRAP"',
+	[TYPE_NORMAL]='"TYPE_NORMAL"',
+	[TYPE_EFFECT]='"TYPE_EFFECT"',
+	[TYPE_FUSION]='"TYPE_FUSION"',
+	[TYPE_RITUAL]='"TYPE_RITUAL"',
+	[TYPE_TRAPMONSTER]='"TYPE_TRAPMONSTER"',
+	[TYPE_SPIRIT]='"TYPE_SPIRIT"',
+	[TYPE_UNION]='"TYPE_UNION"',
+	[TYPE_GEMINI]='"TYPE_GEMINI"',
+	[TYPE_TUNER]='"TYPE_TUNER"',
+	[TYPE_SYNCHRO]='"TYPE_SYNCHRO"',
+	[TYPE_TOKEN]='"TYPE_TOKEN"',
+	[TYPE_QUICKPLAY]='"TYPE_QUICKPLAY"',
+	[TYPE_CONTINUOUS]='"TYPE_CONTINUOUS"',
+	[TYPE_EQUIP]='"TYPE_EQUIP"',
+	[TYPE_FIELD]='"TYPE_FIELD"',
+	[TYPE_COUNTER]='"TYPE_COUNTER"',
+	[TYPE_FLIP]='"TYPE_FLIP"',
+	[TYPE_TOON]='"TYPE_TOON"',
+	[TYPE_XYZ]='"TYPE_XYZ"',
+	[TYPE_PENDULUM]='"TYPE_PENDULUM"',
+	[TYPE_SPSUMMON]='"TYPE_SPSUMMON"',
+	[TYPE_LINK]='"TYPE_LINK"',
+	[TYPE_SKILL]='"TYPE_SKILL"',
+	[TYPE_ACTION]='"TYPE_ACTION"',
+	[TYPE_PLUS]='"TYPE_PLUS"',
+	[TYPE_MINUS]='"TYPE_MINUS"',
+	[TYPE_ARMOR]='"TYPE_ARMOR"',
+	[TYPE_MAXIMUM]='"TYPE_MAXIMUM"'
+}
+
+local attributes={
+	[ATTRIBUTE_EARTH]='"ATTRIBUTE_EARTH"',
+	[ATTRIBUTE_WATER]='"ATTRIBUTE_WATER"',
+	[ATTRIBUTE_FIRE]='"ATTRIBUTE_FIRE"',
+	[ATTRIBUTE_WIND]='"ATTRIBUTE_WIND"',
+	[ATTRIBUTE_LIGHT]='"ATTRIBUTE_LIGHT"',
+	[ATTRIBUTE_DARK]='"ATTRIBUTE_DARK"',
+	[ATTRIBUTE_DIVINE]='"ATTRIBUTE_DIVINE"'
+}
+
+local races={
+	[RACE_WARRIOR]='"RACE_WARRIOR"',
+	[RACE_SPELLCASTER]='"RACE_SPELLCASTER"',
+	[RACE_FAIRY]='"RACE_FAIRY"',
+	[RACE_FIEND]='"RACE_FIEND"',
+	[RACE_ZOMBIE]='"RACE_ZOMBIE"',
+	[RACE_MACHINE]='"RACE_MACHINE"',
+	[RACE_AQUA]='"RACE_AQUA"',
+	[RACE_PYRO]='"RACE_PYRO"',
+	[RACE_ROCK]='"RACE_ROCK"',
+	[RACE_WINGEDBEAST]='"RACE_WINGEDBEAST"',
+	[RACE_PLANT]='"RACE_PLANT"',
+	[RACE_INSECT]='"RACE_INSECT"',
+	[RACE_THUNDER]='"RACE_THUNDER"',
+	[RACE_DRAGON]='"RACE_DRAGON"',
+	[RACE_BEAST]='"RACE_BEAST"',
+	[RACE_BEASTWARRIOR]='"RACE_BEASTWARRIOR"',
+	[RACE_DINOSAUR]='"RACE_DINOSAUR"',
+	[RACE_FISH]='"RACE_FISH"',
+	[RACE_SEASERPENT]='"RACE_SEASERPENT"',
+	[RACE_REPTILE]='"RACE_REPTILE"',
+	[RACE_PSYCHIC]='"RACE_PSYCHIC"',
+	[RACE_DIVINE]='"RACE_DIVINE"',
+	[RACE_CREATORGOD]='"RACE_CREATORGOD"',
+	[RACE_WYRM]='"RACE_WYRM"',
+	[RACE_CYBERSE]='"RACE_CYBERSE"'
+}
+
+local function formatProperty(prop,prop_name,table)
+	local str=''
+	if prop~=0 then
+		str=str .. ', "'.. prop_name .. '": [ '
+		local need_comma=false
+		for _,prop in Auxiliary.BitSplit(prop) do
+			if table[prop] then
+				if not need_comma then need_comma=true
+				else str=str .. ', ' end
+				str=str .. table[prop]
+			end
+		end
+		str=str .. ' ]'
+	end
+	return str
+end
+
+local function prettyPrintCardRaw(c,fullInfo)
 	local str='{ "code": ' .. c:GetCode() .. ', "original_code": ' ..c:GetOriginalCode()
 	local location=c:GetLocation()
 	local sequence=c:GetSequence()
@@ -707,11 +796,17 @@ local function prettyPrintCardRaw(c)
 	str=str .. ', "location": ' .. tostring(locations[location])
 	str=str .. ', "position": ' .. tostring(positions[c:GetPosition()])
 	str=str .. ', "sequence": ' .. tostring(sequence)
+	if not fullInfo then
+		return str .. ' }'
+	end
+	str=str .. formatProperty(c:GetType(),'type',types)
+	str=str .. formatProperty(c:GetAttribute(),'attribute',attributes)
+	str=str .. formatProperty(c:GetRace(),'race',races)
 	return str .. ' }'
 end
 
 function Debug.CardToString(c)
-	return 'Card: ' .. prettyPrintCardRaw(c)
+	return 'Card: ' .. prettyPrintCardRaw(c,true)
 end
 
 local function prettyPrintGroupRaw(g)
@@ -723,7 +818,7 @@ local function prettyPrintGroupRaw(g)
 		for tc in g:Iter() do
 			if not need_comma then need_comma=true
 			else str=str .. ', ' end
-			str=str .. prettyPrintCardRaw(tc)
+			str=str .. prettyPrintCardRaw(tc,false)
 		end
 		str=str .. ' ]'
 	end

48d07496e1e86f29e6035f4a56b7dfd963af2302
Edoardo Lolletti <edoardo762@gmail.com>
2022-10-21 19:26:19 +0200

    Update It's My Lucky Day!
    Properly handle the coin tosses/dice rolls, before it was just changing a single value and keeping the other results as they were from the previous call, thus not rolling new values

--- skill/c300102004.lua
+++ skill/c300102004.lua
@@ -1,21 +1,28 @@
 --It's My Lucky Day!
 --credit: edo9300
 local s,id=GetID()
-local register=function(what)
-	return function(...)
-		local params={...}
-		local tp=params[1]
-		if Duel.CheckLPCost(tp,1000) and Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-			Duel.SetFlagEffectLabel(tp,id,1)
-		end
-		return what(...)
+local function check_and_register_flag(tp)
+	if Duel.CheckLPCost(tp,1000) and Duel.GetFlagEffect(tp,id)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.SetFlagEffectLabel(tp,id,1)
 	end
 end
 s.roll_dice=true
-local tossd=Duel.TossDice
-Duel.TossDice=register(tossd)
-local tossc=Duel.TossCoin
-Duel.TossCoin=register(tossc)
+Duel.TossDice=(function()
+	local oldf=Duel.TossDice
+	--technically this also has a count2 for the opponent, leave it
+	--unhandled for now as the core lacks the capabilities
+	return function(tp,count,...)
+		if count>0 then check_and_register_flag(tp) end
+		return oldf(tp,count,...)
+	end
+end)()
+Duel.TossCoin=(function()
+	local oldf=Duel.TossCoin
+	return function(tp,count,...)
+		if count>0 then check_and_register_flag(tp) end
+		return oldf(tp,count,...)
+	end
+end)()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false)
 	local e1=Effect.CreateEffect(c)
@@ -29,41 +36,36 @@ function s.initial_effect(c)
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EFFECT_TOSS_DICE_CHOOSE)
-		ge1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)return Duel.GetFlagEffect(ep,id)>0 and Duel.GetFlagEffectLabel(ep,id)>0 end)
-		ge1:SetOperation(s.repop("dice",Duel.GetDiceResult,Duel.SetDiceResult,function(tp) Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3)) return Duel.AnnounceNumber(tp,1,2,3,4,5,6) end))
+		ge1:SetCondition(function(e,tp,eg,ep)return (Duel.GetFlagEffectLabel(ep,id) or 0)>0 end)
+		ge1:SetOperation(s.repop(true,Duel.SetDiceResult,function(tp)
+			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+			return Duel.AnnounceNumberRange(tp,1,6)
+		end))
 		Duel.RegisterEffect(ge1,0)
 		local ge2=ge1:Clone()
 		ge2:SetCode(EFFECT_TOSS_COIN_CHOOSE)
-		ge2:SetOperation(s.repop("coin",Duel.GetCoinResult,Duel.SetCoinResult,function(tp) Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,4)) return Duel.AnnounceCoin(tp) end))
+		ge2:SetOperation(s.repop(false,Duel.SetCoinResult,function(tp)
+			return Duel.AnnounceCoin(tp,aux.Stringid(id,4))
+		end))
 		Duel.RegisterEffect(ge2,0)
 	end)
 end
-function s.repop(typ,func1,func2,func3)
+function s.repop(isdice,func2,func3)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		Duel.PayLPCost(ep,1000)
 		Duel.ResetFlagEffect(ep,id)
 		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 		Duel.Hint(HINT_CARD,tp,id)
-		local dc={func1()}
-		local ct=(ev&0xff)+(ev>>16)
-		local ac=1
-		if ct>1 then
-			if typ=="dice" then
-				Duel.Hint(HINT_SELECTMSG,ep,aux.Stringid(id,1))
-				local val,idx=Duel.AnnounceNumber(ep,table.unpack(dc,1,ct))
-				ac=idx+1
+		local total=(ev&0xff)+(ev>>16)
+		local res={}
+		res[1]=func3(ep)
+		for i=2,total do
+			if isdice then
+				table.insert(res,Duel.GetRandomNumber(1,6))
 			else
-				local tab={}
-				for i=1,ct do
-					table.insert(tab,60+(1-dc[i]))
-				end
-				Duel.Hint(HINT_SELECTMSG,ep,aux.Stringid(id,2))
-				Duel.SelectOption(ep,table.unpack(tab))
+				table.insert(res,Duel.GetRandomNumber(0,1)==0 and COIN_TAILS or COIN_HEADS)	
 			end
-			dc[ac]=func3(ep)
-		else
-			dc[1]=func3(ep)
 		end
-		func2(table.unpack(dc))
+		func2(table.unpack(res))
 	end
 end

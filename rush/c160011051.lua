0dd6cbcc7774a3d8bbfe4b9d965b6d1d0222f105
Naim <naimsantos45@gmail.com>
2022-10-21 19:40:45 -0300

    Fix "Flame Arms - Fretberge"
    The effect to prevent monsters without equip targets from being selected as battle targets was not working properly and it was missing a condition

--- rush/c160011051.lua
+++ rush/c160011051.lua
@@ -3,20 +3,21 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--equip
+	--Equip only to a Psychic monster
 	aux.AddEquipProcedure(c,0,s.eqfilter,s.eqlimit)
-	--atk up
+	--Increase ATK by 4000
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_EQUIP)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(400)
 	c:RegisterEffect(e1)
-	--cannot be battle target
+	--Opponent cannot target monsters without equip cards as battle target
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetCondition(s.cond)
 	e2:SetValue(s.tg)
 	c:RegisterEffect(e2)
 end
@@ -24,9 +25,12 @@ function s.eqfilter(c)
 	return c:IsFaceup() and c:GetOriginalRace()==RACE_PSYCHIC and not c:IsMaximumModeSide()
 end
 function s.eqlimit(e,c)
-    return c:IsFaceup()
+	return c:IsFaceup()
+end
+function s.cond(e)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec:GetControler()==e:GetHandlerPlayer()
 end
 function s.tg(e,c)
-	local g=c:GetEquipGroup()
-	return c:IsFaceup() and not g:IsExists(Card.IsSpell,1,nil)
+	return #c:GetEquipGroup()==0
 end

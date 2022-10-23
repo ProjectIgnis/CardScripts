40bf4837f2b50e3340143b57f0587e161220dd72
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-10-23 16:06:40 +0200

    added Ultimate Flag Beast Lead Unicorn

---
+++ rush/c160205050.lua
@@ -0,0 +1,48 @@
+--最強旗獣リードユニコーン
+--Ultimate Flag Beast Lead Unicorn
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--change position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,e,sp)
+	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,3,nil)
+end
+function s.posfilter(c)
+	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsCanTurnSet() and c:IsCanChangePositionRush()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_MACHINE) and c:IsLevel(5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.HintSelection(g)
+	if #g>0 then
+		local g2=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+		if Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and #g2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

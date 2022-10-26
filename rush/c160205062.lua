1d9161de2e2fce5893da41237908364609c307fe
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-10-26 12:58:15 +0200

    added Love Angel

---
+++ rush/c160205062.lua
@@ -0,0 +1,17 @@
+--ラヴ・エンジェル
+--Love Angel
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
+end
+function s.val(e,c)
+	return c:IsLevel(7) or c:IsLevel(8)
+end

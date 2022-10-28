f3fbdf8a44326749f5cf0e6c534b30b7fe17acf9
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-10-28 20:51:05 +0300

    "Card of Sacrifice (Anime)" and "Neutrino Dowsing" fixes
    General fixes and polishing all around.

--- unofficial/c511001182.lua
+++ unofficial/c511001182.lua
@@ -4,45 +4,48 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.con)
+	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_series={SET_GALAXY_EYES}
 function s.cfilter(c,tp)
-	return c:IsType(TYPE_XYZ) and c:IsControler(1-tp)
+	return c:IsType(TYPE_XYZ) and c:IsFaceup() and c:IsControler(1-tp)
 end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.xyzfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x107b)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(SET_GALAXY_EYES) and and c:GetOverlayCount()>0
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Group.CreateGroup()
-	Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_MZONE,0,nil):ForEach(function(c)g:Merge(c:GetOverlayGroup()) end)
-	if chk==0 then return #g>0 end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DEATTACHFROM)
+	local xyzc=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	Duel.HintSelection(xyzc,true)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVEXYZ)
-	local sg=g:Select(tp,1,1,nil)
-	Duel.SendtoGrave(sg,REASON_COST)
+	local g=xyzc:GetOverlayGroup():Select(tp,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
 end
-function s.filter(c)
-	return c:GetType()==TYPE_SPELL and c:IsAbleToGrave() and c:CheckActivateEffect(false,true,false)~=nil
+function s.tgfilter(c)
+	return c:IsNormalSpell() and c:IsAbleToGrave()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoGrave(g,REASON_EFFECT)
-		local te,eg,ep,ev,re,r,rp=g:GetFirst():CheckActivateEffect(false,true,false)
+	local tc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) then
+		local te,eg,ep,ev,re,r,rp=tc:CheckActivateEffect(false,true,false)
+		if not (te and Duel.SelectYesNo(tp,aux.Stringid(id,1))) then return end 
 		e:SetLabelObject(te)
 		Duel.ClearTargetCard()
 		local tg=te:GetTarget()
--- unofficial/c511002552.lua
+++ unofficial/c511002552.lua
@@ -4,9 +4,10 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DRAW)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
@@ -14,6 +15,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	aux.GlobalCheck(s,function()
+		--Keep track of manual position changing
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_CHANGE_POS)
@@ -28,12 +30,12 @@ function s.poscheck(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.check(tp)
 	local sg=Duel.GetMatchingGroup(Card.IsPosition,tp,LOCATION_MZONE,0,nil,POS_FACEUP_ATTACK)
-	if #sg<1 then return false end
-	local og=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	if #sg==0 then return false end
+	local og=Duel.GetMatchingGroup(Card.IsPosition,tp,0,LOCATION_MZONE,nil,POS_FACEUP_ATTACK)
 	if #og==0 then return false end
-	local at1=sg:GetSum(Card.GetAttack)
-	local tg,at2=og:GetMinGroup(Card.GetAttack)
-	return at1<at2
+	local atk1=sg:GetSum(Card.GetAttack)
+	local _,atk2=og:GetMinGroup(Card.GetAttack)
+	return atk1<atk2
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return s.check(tp)
@@ -43,9 +45,12 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 		and Duel.GetActivityCount(tp,ACTIVITY_FLIPSUMMON)==0
 		and Duel.GetActivityCount(tp,ACTIVITY_SPSUMMON)==0
 		and Duel.GetFlagEffect(tp,id)==0 end
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	--Cannot Summon monsters this turn
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	e1:SetTargetRange(1,0)
@@ -56,19 +61,14 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local e3=e1:Clone()
 	e3:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
 	Duel.RegisterEffect(e3,tp)
-	local e4=Effect.CreateEffect(e:GetHandler())
+	--Cannot change the battle positions of monsters
+	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_OATH)
 	e4:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
 	e4:SetReset(RESET_PHASE+PHASE_END)
 	e4:SetTargetRange(LOCATION_MZONE,0)
 	Duel.RegisterEffect(e4,tp)
-	local e5=Effect.CreateEffect(e:GetHandler())
-	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e5:SetDescription(aux.Stringid(id,1))
-	e5:SetReset(RESET_PHASE+PHASE_END)
-	e5:SetTargetRange(1,0)
-	Duel.RegisterEffect(e5,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end

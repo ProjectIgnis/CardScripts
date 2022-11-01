cdac5095cbf6c28aed9fbf385376910c5a868977
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-11-01 21:12:20 +0200

    "Aurkus, Lightsworn Druid" fix
    Shouldn't prevent "Lightsworn" monsters that are banished face-down from being targeted + general polish/modernisation.

--- official/c7183277.lua
+++ official/c7183277.lua
@@ -1,38 +1,31 @@
 --ライトロード・ドルイド オルクス
+--Aurkus, Lightsworn Druid
 local s,id=GetID()
 function s.initial_effect(c)
-	--cannot be target
+	--Neither player can target "Lightsworn" monsters (anywhere) with card effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e1:SetProperty(EFFECT_FLAG_IGNORE_RANGE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTarget(s.etarget)
+	e1:SetTarget(function(_,c) return c:IsSetCard(SET_LIGHTSWORN) and c:IsMonster() and c:IsFaceup() end)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	--discard deck
+	--Send the top 2 cards of your Deck to the GY
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_DECKDES)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.discon)
+	e2:SetCondition(function(_,tp) return Duel.IsTurnPlayer(tp) end)
 	e2:SetTarget(s.distg)
-	e2:SetOperation(s.disop)
+	e2:SetOperation(function(_,tp) Duel.DiscardDeck(tp,2,REASON_EFFECT) end)
 	c:RegisterEffect(e2)
 end
-function s.etarget(e,c)
-	return c:IsSetCard(0x38) and c:IsMonster()
-end
-function s.discon(e,tp,eg,ep,ev,re,r,rp)
-	return tp==Duel.GetTurnPlayer()
-end
+s.listed_series={SET_LIGHTSWORN}
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,2)
-end
-function s.disop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.DiscardDeck(tp,2,REASON_EFFECT)
 end

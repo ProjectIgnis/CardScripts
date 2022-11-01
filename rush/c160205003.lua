af12855c01d2357768d42f3eed58df98072d68e3
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-11-01 14:55:06 +0100

    New Rush scripts + Heavenly Rest fix (#822)
    - Heavenly Rest now checks for original Level, according to rulings.
    - Added new rush scripts

---
+++ rush/c160205003.lua
@@ -0,0 +1,39 @@
+--セブンスロード・マジシャン
+--Sevens Road Magician
+--Scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--ATK increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)>0 then
+		--effect
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			local att=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetAttribute)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+			e1:SetValue(att*300)
+			c:RegisterEffect(e1)
+		end
+	end
+end

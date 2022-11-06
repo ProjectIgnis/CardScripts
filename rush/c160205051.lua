6464424ae065452453c3756ff3d28b485843abe0
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-11-06 11:07:25 +0100

    added Double Twin Dragon

---
+++ rush/c160205051.lua
@@ -0,0 +1,49 @@
+--ダブルツイン・ドラゴン
+--Double Twin Dragon
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--double tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return (not Duel.IsPlayerAffectedByEffect(tp,FLAG_NO_TRIBUTE)) and e:GetHandler():CanBeDoubleTribute(FLAG_DOUBLE_TRIB_DRAGON,FLAG_DOUBLE_TRIB_LEVEL7)
+end
+function s.tdfilter(c)
+	return c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE,0,3,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--cost
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+	Duel.HintSelection(g)
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
+	local c=e:GetHandler()
+	c:AddDoubleTribute(id,s.otfilter,s.eftg,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,FLAG_DOUBLE_TRIB_DRAGON+FLAG_DOUBLE_TRIB_LEVEL7)
+	--Prevent non-Dragon or High Dragon from attacking
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(function(_,c) return not (c:IsRace(RACE_DRAGON) or c:IsRace(RACE_HIGHDRAGON)) end)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.otfilter(c,tp)
+	return c:IsDoubleTribute(FLAG_DOUBLE_TRIB_DRAGON+FLAG_DOUBLE_TRIB_LEVEL7) and (c:IsControler(tp) or c:IsFaceup())
+end
+function s.eftg(e,c)
+	return c:IsRace(RACE_DRAGON) and c:IsLevel(7) and c:IsSummonableCard()
+end
+

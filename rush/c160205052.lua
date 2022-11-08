250d87a0f3beef8c1aa317466747cd7a636ee0d0
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-11-08 13:47:15 +0100

    Added new Rush card's script

---
+++ rush/c160205052.lua
@@ -0,0 +1,21 @@
+--魔将ヤメルーラ－武槍
+--Yamiruler the Dark Delayer - Supreme Soldier Spear
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Indestructable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.indtg)
+	e1:SetValue(s.efilter)
+	c:RegisterEffect(e1)
+end
+function s.indtg(e,c)
+	return c:IsLevelAbove(7) and c:IsRace(RACE_WARRIOR+RACE_CELESTIALWARRIOR)
+end
+function s.efilter(e,re,rp)
+	return re:IsActiveType(TYPE_TRAP) and aux.indoval(e,re,rp)
+end

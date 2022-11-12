c07bf3bad33595a20d602f40c0369d647d991afa
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2022-11-12 09:36:54 -0800

    Update "Pompadour Blizzardon"
    Should be a mandatory effect and affect all opponent's Attack Position monsters

--- unofficial/c511001729.lua
+++ unofficial/c511001729.lua
@@ -1,28 +1,29 @@
+--リーゼント・ブリザードン
 --Pompadour Blizzardon
 local s,id=GetID()
 function s.initial_effect(c)
-	--position
+	--Attack Position monsters cannot change their battle positions
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e1:SetTarget(s.postg)
 	e1:SetOperation(s.posop)
 	c:RegisterEffect(e1)
 end
-function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsPosition(POS_FACEUP_ATTACK) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsPosition,tp,0,LOCATION_MZONE,1,nil,POS_FACEUP_ATTACK) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,Card.IsPosition,tp,0,LOCATION_MZONE,1,1,nil,POS_FACEUP_ATTACK)
+function s.postg(e,tp,eg,ep,ev,re,r,rp)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAttackPos,tp,0,LOCATION_MZONE,1,nil) end
 end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
+	local g=Duel.GetMatchingGroup(Card.IsAttackPos,tp,0,LOCATION_MZONE,nil)
+	if #g>0 then
+		for tc in g:Iter() do
+			local e1=Effect.CreateEffect(e:GetHandler())
+        		e1:SetDescription(3313)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+        		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+        		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+        		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        		tc:RegisterEffect(e1)
+		end
 	end
 end

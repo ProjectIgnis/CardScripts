849281a386532798e096f0322c903a1609b748c6
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-11-13 15:38:41 +0100

    add new Rush script (#830)

---
+++ rush/c160205056.lua
@@ -0,0 +1,48 @@
+--憤砕竜アンガーローグ
+--Rage Wrecker Dragon Angerogue
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_DINOSAUR),tp,LOCATION_MZONE,0,1,e:GetHandler())
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsFaceup() and c:GetAttack()>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--requirement
+	Duel.DiscardDeck(tp,1,REASON_COST)
+	--effect
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local pg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+		local og=Duel.GetMatchingGroup(aux.FilterMaximumSideFunctionEx(s.filter),tp,0,LOCATION_MZONE,nil)
+		local _,atk=pg:GetMaxGroup(Card.GetDefense)
+		local sub1,_=og:GetMaxGroup(Card.GetAttack)
+		for tc in sub1:Iter() do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-atk)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffectRush(e1)
+		end
+	end
+end

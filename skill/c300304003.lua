c5d8be19f4807c9d4b8a1dc058721b445fb8b2b9
Naim <naimsantos45@gmail.com>
2022-11-14 17:16:04 -0300

    Update "Land of the Ojamas"
    It was using a deprecated function

--- skill/c300304003.lua
+++ skill/c300304003.lua
@@ -31,7 +31,7 @@ function s.initial_effect(c)
 end
 s.listed_series={SET_OJAMA}
 function s.atcon(e)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,SET_OJAMA),e:GetHandler():GetControler(),LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_OJAMA),e:GetHandler():GetControler(),LOCATION_MZONE,0,1,nil)
 end
 function s.costfilter(c)
 	return c:IsSetCard(SET_OJAMA) and c:IsAbleToGraveAsCost()

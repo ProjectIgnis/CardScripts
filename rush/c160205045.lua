022df31c7dd4c740be20044ff2c3e0cfb517e26b
Edoardo Lolletti <edoardo762@gmail.com>
2022-11-26 18:26:51 +0100

    Update H.D.D. - Hundred Divine Dragon to use new functions

--- rush/c160205045.lua
+++ rush/c160205045.lua
@@ -1,37 +1,17 @@
 --Ｈ・Ｄ・Ｄ
 --H.D.D. - Hundred Divine Dragon
---scripted by YoshiDuels
+--scripted by edo9300
 local s,id=GetID()
 function s.initial_effect(c)
-	--Prompt player if they want to draw
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PREDRAW)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(function(_,tp) return Duel.IsTurnPlayer(tp) and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)<5 end)
-	e1:SetOperation(s.predrop)
-	c:RegisterEffect(e1)
-end
-function s.predrop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--Prevents activating Trap Card after the "initial Normal Draw"
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e1:SetTargetRange(0,1)
-	e1:SetCondition(function(e) return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0)<6 end)
-	e1:SetValue(function(_,re) return re:IsHasType(EFFECT_TYPE_ACTIVATE) end)
-	e1:SetReset(RESET_PHASE+PHASE_DRAW)
-	Duel.RegisterEffect(e1,tp)
-	--Performs the Extra draw
+	--Draw until 6 cards in your hand instead of until 5 cards
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DRAW_COUNT)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetCode(EVENT_PHASE+PHASE_DRAW)
-	e2:SetCountLimit(1)
-	e2:SetOperation(function(_,tp) Duel.Draw(tp,1,REASON_RULE) end)
-	e2:SetReset(RESET_PHASE+PHASE_DRAW)
-	Duel.RegisterEffect(e2,tp)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(1,0)
+	local function get_draw(e) return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0) end
+	e2:SetCondition(function(e)return get_draw(e)<6 end)
+	e2:SetValue(function(e)return 6-get_draw(e) end)
+	c:RegisterEffect(e2)
 end

b79cb05b991efe850c70dcac501500c96c67717c
Naim <naimsantos45@gmail.com>
2022-11-28 16:02:19 -0300

    "Amazement Precious Park" fix
    The first effect should only allow the cards to be activated during your Main Phase

--- official/c33773528.lua
+++ official/c33773528.lua
@@ -8,18 +8,18 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Trap activate in set turn
+	--"Attraction" Trap cards can be activated during the turn they are set (during your Main Phase)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_SZONE,0)
-	e2:SetCondition(s.actcon)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x15f))
+	e2:SetCondition(function(e) return Duel.IsMainPhase() and Duel.IsTurnPlayer(e:GetHandlerPlayer()) end)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_ATTRACTION))
 	e2:SetCountLimit(1,id)
 	c:RegisterEffect(e2)
-	--set
+	--Set 1 "Attraction" Trap that is banished or in the GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_PHASE+PHASE_END)
@@ -31,11 +31,7 @@ function s.initial_effect(c)
 	e3:SetOperation(s.setop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={0x15f}
-function s.actcon(e)
-	local ph=Duel.GetCurrentPhase()
-	return ph==PHASE_MAIN1 or ph==PHASE_MAIN2
-end
+s.listed_series={SET_ATTRACTION}
 function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
 	return true
@@ -44,16 +40,16 @@ function s.eqfilter(c,ec)
 	return c:GetEquipGroup():IsContains(ec)
 end
 function s.setfilter(c,ec)
-	return c:IsTrap() and c:IsSetCard(0x15f) and (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE))
+	return c:IsTrap() and c:IsSetCard(SET_ATTRACTION) and (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE))
 		and not c:IsCode(ec:GetCode()) and c:IsSSetable(true)
 end
 function s.cfilter(c,tp)
-	return c:IsFaceup() and c:IsTrap() and c:IsSetCard(0x15f)
+	return c:IsFaceup() and c:IsTrap() and c:IsSetCard(SET_ATTRACTION)
 		and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,c)
-		and Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,c)
+		and Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil,c)
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.setfilter(chkc,e:GetLabelObject()) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED) and chkc:IsControler(tp) and s.setfilter(chkc,e:GetLabelObject()) end
 	if chk==0 then
 		if e:GetLabel()~=1 then return false end
 		e:SetLabel(0)
@@ -64,12 +60,12 @@ function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	e:SetLabelObject(gc)
 	Duel.SendtoGrave(gc,REASON_COST)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,gc)
+	local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,gc)
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsSSetable() then
+	if tc:IsRelateToEffect(e) and tc:IsSSetable() then
 		Duel.SSet(tp,tc)
 	end
 end

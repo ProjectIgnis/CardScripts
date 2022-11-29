b5a889380562df591788cd930306eecc399b3050
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-11-29 17:16:16 +0100

    fixed typo on Qwerty Keyblade

--- rush/c160011042.lua
+++ rush/c160011042.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--equip
-	aux.AddEquipProcedure(c,0,s.eqfilter,s.eqlimit,,nil,nil,s.gainop)
+	aux.AddEquipProcedure(c,0,s.eqfilter,s.eqlimit,nil,nil,s.gainop)
 	--Increase ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_EQUIP)

f45407f9428154ce7c43da3659c318e9952ad7f5
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2022-12-06 23:14:43 +0300

    Update c51102769.lua

--- unofficial/c51102769.lua
+++ unofficial/c51102769.lua
@@ -171,7 +171,6 @@ function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetLabelObject(g)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
 		e1:SetValue(s.atkupval)
 		c:RegisterEffect(e1)	
 	end

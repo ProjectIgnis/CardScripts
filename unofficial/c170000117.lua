a80ccb2c8a918c53dac1fb5d68af9ffaaac08582
Larry126 <28985622+larry126@users.noreply.github.com>
2022-12-07 17:41:50 +0800

    Updated Time Fusion (Anime)

--- unofficial/c170000117.lua
+++ unofficial/c170000117.lua
@@ -1,32 +1,17 @@
 --時間融合－タイム・フュージョン
 --Time Fusion
---re-scripted by Larry126
+--Re-scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	aux.GlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_DESTROYED)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-	end)
-end
-function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	for tc in aux.Next(eg) do
-		if tc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) then
-			tc:RegisterFlagEffect(id,RESET_EVENT+0x1f20000+RESET_PHASE+PHASE_END,0,1)
-		elseif tc:IsLocation(LOCATION_EXTRA) then
-			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-		end
-	end
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_HAND,0,1,e:GetHandler()) end
@@ -35,45 +20,37 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.spfilter(c,tp)
-	return c:IsType(TYPE_FUSION) and c:GetFlagEffect(id)~=0
-		and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup()) and c:IsPreviousControler(tp)
+	return c:IsType(TYPE_FUSION) and c:IsFaceup()
+		and c:IsReason(REASON_DESTROY) and c:GetTurnID()==Duel.GetTurnCount()
+		and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,0x70,0x70,1,nil,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0x70)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,0x70,0x70,1,1,nil,tp)
-	if #g==0 then return end
-	Duel.HintSelection(g)
-	local tc=g:GetFirst()
-	Duel.Hint(HINT_CARD,1-tp,tc:GetOriginalCode())
-	Duel.Hint(HINT_CARD,tp,tc:GetOriginalCode())
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e1:SetLabelObject(tc)
-	e1:SetCountLimit(1)
-	if Duel.GetCurrentPhase()==PHASE_STANDBY then e1:SetLabel(Duel.GetTurnCount()) end
-	e1:SetCondition(s.spcon)
-	e1:SetOperation(s.spop)
-	e1:SetReset(RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
-	Duel.RegisterEffect(e1,tp)
-	tc:CreateEffectRelation(e1)
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp and Duel.GetTurnCount()~=e:GetLabel()
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	if tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CANNOT_ATTACK)
-		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1,true)
-	end
-	Duel.SpecialSummonComplete()
+	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,0x70,0x70,1,1,nil,tp):GetFirst()
+	if not tc then return end
+	Duel.HintSelection(tc,true)
+	local ct=Duel.GetCurrentPhase()==PHASE_STANDBY and 2 or 1
+	local tid=Duel.GetTurnCount()
+	aux.DelayedOperation(tc,PHASE_STANDBY,id,e,tp,
+		function(ag)
+			local sc=ag:GetFirst()
+			if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SpecialSummonStep(sc,0,tp,tp,true,false,POS_FACEUP) then
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetDescription(3206)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_CANNOT_ATTACK)
+				e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				sc:RegisterEffect(e1,true)
+			end
+			Duel.SpecialSummonComplete()
+		end,
+		function()
+			return Duel.GetTurnPlayer()==tp and (ct==1 or Duel.GetTurnCount()~=tid)
+		end,
+		RESET_PHASE|PHASE_STANDBY|RESET_SELF_TURN,ct)
 end

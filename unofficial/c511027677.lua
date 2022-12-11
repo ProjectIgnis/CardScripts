4d050fe50c27193c0cec2c68e23593cdff8a3b33
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2022-12-11 17:39:26 +0300

    Update c511027677.lua

--- unofficial/c511027677.lua
+++ unofficial/c511027677.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
+	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)

c0e1e80894e54abfd1c08b54697dc2fe4d346f35
YoshiDuels <yoshi.katreuvin@gmail.com>
2022-12-11 10:16:28 +0100

    added Rush script (#862)

---
+++ rush/c160012008.lua
@@ -0,0 +1,39 @@
+--ヴォイドヴェルグ・カタフラクトス
+--Voidvelgr Kataphraktos
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--ATK increase
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_TOHAND)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsStatus(STATUS_SUMMON_TURN) and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_DARK),tp,LOCATION_MZONE,0,1,c)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+end
+function s.thfilter(c)
+	return c:IsRace(RACE_GALAXY) and c:IsLevel(8) and c:IsAbleToHand()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.DiscardDeck(tp,1,REASON_EFFECT)
+	local ct=Duel.GetOperatedGroup():GetFirst()
+	if ct and ct:IsMonster() and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end

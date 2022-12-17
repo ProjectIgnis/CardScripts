44e9f7c195a42507e9113e184ae8693e958f50c0
Naim <naimsantos45@gmail.com>
2022-12-17 13:38:18 -0300

    Update "Constellar" monsters

--- official/c43513897.lua
+++ official/c43513897.lua
@@ -1,26 +1,23 @@
 --セイクリッド・アクベス
+--Constellar Acubens
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Increase the ATK of all "Constellar" monsters you control
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetOperation(s.atkop)
-	c:RegisterEffect(e1)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_TELLAR)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x53}
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x53)
-end
+s.listed_series={SET_CONSTELLAR}
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
-	local tc=g:GetFirst()
-	for tc in aux.Next(g) do
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.SET_CONSTELLAR),tp,LOCATION_MZONE,0,nil)
+	for tc in g:Iter() do
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)

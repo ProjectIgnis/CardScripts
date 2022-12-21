58ae9c555c7aa48f5fd9eec6ce0223d92d6802a3
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2022-12-21 20:42:14 +0300

    Add files via upload

---
+++ unofficial/c511027679.lua
@@ -0,0 +1,30 @@
+--ゼロ・プロテクション
+--Zero Protection
+--Made by Beetron-1 Beetletop
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsOnField()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsChainNegatable(ev) then return false end
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and tg~=nil and tc+tg:FilterCount(s.filter,nil,tp)-#tg>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+end

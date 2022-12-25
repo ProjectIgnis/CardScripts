c0001341ee391172e7e87fac7c7a4f550262cadf
Naim <naimsantos45@gmail.com>
2022-12-25 10:51:20 -0300

    Update "Machine Angel Absolute Ritual"
    Fixed a bug where Necrovalley would negate it in all scenarios

--- official/c11398951.lua
+++ official/c11398951.lua
@@ -5,15 +5,19 @@ function s.initial_effect(c)
 	--Activate
 	Ritual.AddProcEqual{handler=c,filter=s.ritualfil,extrafil=s.extrafil,extraop=s.extraop,extratg=s.extratg}
 end
-s.listed_series={0x2093}
+s.listed_series={SET_CYBER_ANGEL}
 function s.ritualfil(c)
-	return c:IsSetCard(0x2093) and c:IsRitualMonster()
+	return c:IsSetCard(SET_CYBER_ANGEL) and c:IsRitualMonster()
 end
 function s.mfilter(c)
 	return c:HasLevel() and c:IsRace(RACE_WARRIOR+RACE_FAIRY) and c:IsAbleToDeck()
 end
 function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then
+		return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil)
+	else
+		return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil):Filter(aux.nvfilter,nil)
+	end
 end
 function s.extraop(mg,e,tp,eg,ep,ev,re,r,rp)
 	local mat2=mg:Filter(Card.IsLocation,nil,LOCATION_GRAVE):Filter(Card.IsRace,nil,RACE_WARRIOR+RACE_FAIRY)
@@ -23,5 +27,5 @@ function s.extraop(mg,e,tp,eg,ep,ev,re,r,rp)
 end
 function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
 end

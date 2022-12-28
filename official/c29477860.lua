270b72199144a73b7bef21bded6a9bfd36adf122
Naim <naimsantos45@gmail.com>
2022-12-28 19:06:52 -0300

    Fix "Spiral Discharge"
    Fixed a bug where the opponent would be able to target face-down monsters with attacks even when the condition was met

--- official/c29477860.lua
+++ official/c29477860.lua
@@ -5,13 +5,11 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_ATTACK)
-	e1:SetTarget(s.tg)
 	c:RegisterEffect(e1)
-	--Destroy
+	--Destroy 1 card on the field
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetDescription(aux.Stringid(id,1))
@@ -19,12 +17,12 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
 	e2:SetCondition(s.descon)
-	e2:SetCost(s.descos)
 	e2:SetTarget(s.destg)
 	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
-	--cannot be battle target
+	--Prevent attacks on monsters while you control "Gaia the Dragon Champion"
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
@@ -39,36 +37,13 @@ function s.actcon(e)
 	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_GAIA_CHAMPION),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
 function s.atktg(e,c)
-	return c:IsFaceup() and c:GetCode()~=CARD_GAIA_CHAMPION
-end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=Duel.GetAttacker()
-	local dc=Duel.GetAttackTarget()
-	if chk==0 then return true end
-	if s.descos(e,tp,eg,ep,ev,re,r,rp,0)
-		and s.destg(e,tp,eg,ep,ev,re,r,rp,0)
-		and Duel.CheckEvent(EVENT_ATTACK_ANNOUNCE)
-		and ((tc:IsCode(CARD_GAIA_CHAMPION) and tc:IsControler(tp))  or (dc and dc:IsCode(CARD_GAIA_CHAMPION) and dc:IsControler(tp)))
-		and Duel.SelectYesNo(tp,94) then
-		e:SetCategory(CATEGORY_DESTROY)
-		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		e:SetOperation(s.desop)
-		s.destg(e,tp,eg,ep,ev,re,r,rp,1)
-	else
-		e:SetCategory(0)
-		e:SetProperty(0)
-		e:SetOperation(nil)
-	end
+	return c:GetCode()~=CARD_GAIA_CHAMPION
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetAttacker()
 	local dc=Duel.GetAttackTarget()
 	return (tc:IsCode(CARD_GAIA_CHAMPION) and tc:IsControler(tp))
 		or (dc and dc:IsCode(CARD_GAIA_CHAMPION) and dc:IsControler(tp))
-end
-function s.descos(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetFlagEffect(id)==0 end
-	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) end
@@ -79,7 +54,7 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end

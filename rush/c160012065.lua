5417780c0725f07e31a7bc4774d700a177bb4155
Yoshi80 <yoshi.katreuvin@gmail.com>
2022-12-30 15:33:09 +0100

    added Phantom Roar

---
+++ rush/c160012065.lua
@@ -0,0 +1,63 @@
+--ファントム・ロアー
+--Phantom Roar
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Destroy 1 monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,tp)
+	return (c:IsReason(REASON_BATTLE) and Duel.GetAttacker():IsControler(1-tp))
+		and c:GetReasonPlayer()==1-tp and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.costfilter(c)
+	return c:IsLevelAbove(5) and c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,2,nil) end
+end
+function s.desfilter(c)
+	return c:IsFaceup() and c:IsMonster() and c:IsLevelBelow(8) and not c:IsMaximumModeSide()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.HintSelection(g,true)
+	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)>0 then
+		--Effect
+		local dg=Duel.SelectMatchingCard(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil)
+		dg=dg:AddMaximumCheck()
+		if #dg>0 then
+			Duel.HintSelection(dg,true)
+			Duel.Destroy(dg,REASON_EFFECT)
+			if Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+				local sg=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,0,LOCATION_MZONE,1,1,nil)
+				if #sg>0 then
+					Duel.HintSelection(sg,true)
+					local e1=Effect.CreateEffect(e:GetHandler())
+					e1:SetType(EFFECT_TYPE_SINGLE)
+					e1:SetCode(EFFECT_UPDATE_ATTACK)
+					e1:SetValue(-1000)
+					e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+					sg:GetFirst():RegisterEffectRush(e1)
+				end
+			end
+		end
+	end
+end

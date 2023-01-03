30b4b5a0c39c5f7b76490da5c00f543edf755ee7
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-01-03 12:02:25 +0100

    added Cyber Coatl

---
+++ rush/c160012018.lua
@@ -0,0 +1,45 @@
+--サイバー・コアトル
+--Cyber Coatl
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Excavate the top 3 cards and Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=3 end
+end
+function s.filter(c,e,tp)
+	return c:IsMonster() and c:IsRace(RACE_MACHINE) and c:IsLevel(5) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
+	Duel.ConfirmDecktop(tp,3)
+	local g=Duel.GetDecktopGroup(tp,3)
+	if g:IsExists(s.filter,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local tg=g:FilterSelect(tp,s.filter,1,1,nil,e,tp)
+		if #tg>0 then
+			Duel.DisableShuffleCheck()
+			Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP)
+			g:RemoveCard(tg)
+		end
+	end
+	local ct=#g
+	if ct>0 then
+		Duel.MoveToDeckBottom(ct,tp)
+		Duel.SortDeckbottom(tp,tp,ct)
+	end
+end

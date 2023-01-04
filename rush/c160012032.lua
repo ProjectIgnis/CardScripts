f091055c78d4be1eba28faed554673448f5fa10b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-01-04 22:19:31 +0200

    Update c160012032.lua

--- rush/c160012032.lua
+++ rush/c160012032.lua
@@ -1,12 +1,14 @@
+--アルケミカライズ・サラマンドラ
 --Alchemicalize Salamandra
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Fusion Summon Procedure
 	Fusion.AddProcMix(c,true,true,160010020,160316023)
-	--Draw and summon
+	--Destroy all Spells/Traps on the field
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -19,7 +21,6 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsStatus(STATUS_SPSUMMON_TURN)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	local sg=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
@@ -32,9 +33,10 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		local atk=dg:FilterCount(Card.IsSpell,nil)*1000
 		local sg=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
 		if #sg>0 and atk>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			local c=e:GetHandler()
 			for tc in sg:Iter() do
 				--Decrease ATK
-				local e1=Effect.CreateEffect(e:GetHandler())
+				local e1=Effect.CreateEffect(c)
 				e1:SetType(EFFECT_TYPE_SINGLE)
 				e1:SetCode(EFFECT_UPDATE_ATTACK)
 				e1:SetValue(-atk)

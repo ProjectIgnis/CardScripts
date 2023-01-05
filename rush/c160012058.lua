79110875dd6a5fe8120c80e39fa853532b3816d5
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-01-05 13:20:16 +0100

    added new rush cards

---
+++ rush/c160012058.lua
@@ -0,0 +1,38 @@
+--アドバンス・ブロック
+--Tribute Block
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	if #eg~=1 then return false end
+	local tc=eg:GetFirst()
+	return tc:IsFaceup() and tc:IsLevelAbove(5) and tc:IsCanChangePositionRush() and tc:IsSummonPlayer(1-tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.posfilter,tp,LOCATION_MZONE,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,eg:GetFirst(),1,0,0)
+end
+function s.posfilter(c)
+	return c:IsAttackPos() and c:IsRace(RACE_DRAGON+RACE_MACHINE) and c:IsCanChangePositionRush()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g<1 then return end
+	Duel.HintSelection(g,true)
+	g:AddCard(eg)
+	--effect
+	Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
+end

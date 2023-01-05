266f82ff07d53ccf09b62e4977601922656b7734
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-01-05 14:59:19 +0100

    added new rush scripts (#890)

---
+++ rush/c160012056.lua
@@ -0,0 +1,41 @@
+--トランザム・ライトニング
+--Transamu Lightning
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--battle protection
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttackTarget()
+	local ac=Duel.GetAttacker()
+	return tc and tc:IsFaceup() and tc:IsControler(tp) and ac:IsControler(1-tp) 
+		and tc:IsLevelBelow(4) and tc:IsType(TYPE_NORMAL) and tc:IsRace(RACE_GALAXY) and not tc:IsHasEffect(EFFECT_INDESTRUCTABLE_BATTLE)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetAttackTarget()
+	if chk==0 then return tg:IsControler(tp) and tg:IsOnField() end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	local d=Duel.GetAttackTarget()
+	if d and d:IsRelateToBattle() and d:IsFaceup() then
+		--Cannot be destroyed by battle
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3000)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		d:RegisterEffectRush(e1)
+	end
+end
---
+++ rush/c160012057.lua
@@ -0,0 +1,56 @@
+--ジョインテック・グレートウォール
+--Jointech Great Wall
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter1(c,tp)
+	return c:IsSummonPlayer(1-tp) and c:IsLocation(LOCATION_MZONE) and c:IsLevelAbove(7)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsLevelAbove(7)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter1,1,nil,tp) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_EARTH)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	--Effect
+	local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,0,1,3,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g,true)
+	for tc in g:Iter() do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(300)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffectRush(e1)
+		--Cannot be destroyed by battle
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetDescription(3000)
+		e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffectRush(e2)
+	end
+end

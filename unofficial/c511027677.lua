9d7a89ff8ad9a05211b210e75da27d52cb1b9b4b
Larry126 <28985622+larry126@users.noreply.github.com>
2023-01-06 15:05:32 +0800

    Update c511027677.lua

--- unofficial/c511027677.lua
+++ unofficial/c511027677.lua
@@ -5,22 +5,21 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetHintTiming(0,TIMING_DRAW_PHASE+TIMINGS_CHECK_MONSTER_E)
 	e1:SetCondition(s.descon)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c)
-	return c:IsType(TYPE_PENDULUM) and c:IsFaceup()
-end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1 and Duel.GetMatchingGroupCount(s.cfilter,tp,LOCATION_MZONE,0,nil)==1 
-		and Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0)==0
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	return #g==1 and g:GetFirst():IsType(TYPE_PENDULUM) and g:GetFirst():IsFaceup()
+		and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) end

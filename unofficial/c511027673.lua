a42355a23c964e7fd240739630fcd5dfb52d5cd5
Larry126 <28985622+larry126@users.noreply.github.com>
2023-01-06 15:01:13 +0800

    Update c511027673.lua

--- unofficial/c511027673.lua
+++ unofficial/c511027673.lua
@@ -5,10 +5,9 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_REMOVE)
+	e1:SetCategory(CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
@@ -17,24 +16,22 @@ function s.initial_effect(c)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-	local atype=re:GetActiveType()
-	return rp==1-tp and (atype==TYPE_PENDULUM+TYPE_SPELL and (loc&LOCATION_PZONE)~=0 and not re:IsHasType(EFFECT_TYPE_ACTIVATE))
-		and Duel.IsChainNegatable(ev)
+	return re:GetActiveType()==TYPE_PENDULUM|TYPE_SPELL and (loc&LOCATION_PZONE)~=0
+		and not re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
-		Duel.NegateEffect(ev)
+	if chk==0 then return Duel.IsChainDisablable(ev) end
+	Duel.NegateEffect(ev)
+end
+function s.tdfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and c:IsAbleToDeck()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil) end
-	local tg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_EXTRA,0,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	local tg=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_EXTRA,0,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,#tg,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_EXTRA,0,nil)
+	local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_EXTRA,0,nil)
 	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-end
-function s.filter(c)
-	return c:IsType(TYPE_PENDULUM) and c:IsAbleToDeck()
 end

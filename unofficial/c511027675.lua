e6688c9dc20bd111ce215bbb60f4984a7511f908
Larry126 <28985622+larry126@users.noreply.github.com>
2023-01-06 15:36:42 +0800

    Update c511027675.lua

--- unofficial/c511027675.lua
+++ unofficial/c511027675.lua
@@ -3,7 +3,9 @@
 --Made by Beetron-1 Beetletop
 local s,id=GetID()
 function s.initial_effect(c)
+	--Reduce effect damage so that you have 100LP left
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_HAND)
@@ -13,23 +15,24 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.cost(e,tp,eg,ev,ep,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(c,REASON_COST)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function s.condition(e,tp,eg,ev,ep,re,r,rp)
+	local lp=Duel.GetLP(tp)
+	if lp<100 then return false end
 	local e1=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DAMAGE)
 	local e2=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
 	local rd=e1 and not e2
 	local rr=not e1 and e2
 	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
-	if ex and (cp==tp or cp==PLAYER_ALL) and not rd 
-		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv then 
+	if ex and (cp==tp or cp==PLAYER_ALL) and not rd
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and lp<=cv then 
 		return true 
 	end
 	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
-	return ex and (cp==tp or cp==PLAYER_ALL) and rr 
-		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv
+	return ex and (cp==tp or cp==PLAYER_ALL) and rr
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and lp<=cv
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp,val,r,rc)
 	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
@@ -37,18 +40,16 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp,val,r,rc)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
+	e1:SetTargetRange(1,1)
 	e1:SetLabel(cid)
-	e1:SetValue(s.value)
+	e1:SetValue(s.damval)
 	e1:SetReset(RESET_CHAIN)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.value(e,re,val,r,rp,rc)
-	local p=e:GetHandler():GetControler()
-	local lp=Duel.GetLP(p)
+function s.damval(e,re,val,r,rp,rc)
 	local cc=Duel.GetCurrentChain()
-	if cc<100 or (r&REASON_EFFECT)<100 then return end
+	if cc==0 or (r&REASON_EFFECT)==0 then return val end
 	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
-	if cid==e:GetLabel() then e:SetLabel(val) return lp-100
+	if cid==e:GetLabel() then return Duel.GetLP(e:GetHandlerPlayer())-100
 	else return val end
 end

813fa685c4d435ac345fab720fb3cbf53c9121c1
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-01-08 09:30:56 +0300

    Add files via upload

---
+++ unofficial/c511027679.lua
@@ -0,0 +1,41 @@
+--ゼロ・プロテクション
+--Zero Protection
+--Made By Beetron-1 Beetletop
+local s,id=GetID()
+function s.initial_effect(c)
+	--prevent destruction
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,e)
+	return c:IsOnField() and (not e or c:IsRelateToEffect(e)) 
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	if tg==nil then return false end
+	local g=tg:Filter(s.cfilter,nil,nil)
+	g:KeepAlive()
+	e:SetLabelObject(g)
+	return ex and tc+#g-#tg>=1
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=e:GetLabelObject()
+	if chk==0 then return g end
+	Duel.SetTargetCard(g)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetValue(1)
+		e1:SetReset(RESET_CHAIN)
+		tc:RegisterEffect(e1)
+	end
+end

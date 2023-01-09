7ff42acdaac6b967b20e9184609866cc7a7ec69b
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-01-09 09:27:12 +0300

    Add "Mirror Imagine Reflector 1" script (#892)

---
+++ unofficial/c511027670.lua
@@ -0,0 +1,8 @@
+--ミラーイマジン・リフレクター１ワン-1
+--Mirror Imagine Reflector 1
+--Made by Beetron-1 Beetletop
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+end

b75bdae0d7b0297f16dc780d5970e6b8fe173f5b
Larry126 <28985622+larry126@users.noreply.github.com>
2023-01-10 13:17:59 +0800

    Added Zero Protection and D/D Ark (Manga) script

---
+++ unofficial/c511600394.lua
@@ -0,0 +1,34 @@
+--ゼロ・プロテクション
+--Zero Protection
+--Scripted by Larry126
+local s,id=GetID()
+local LOCATION_DESTROY=LOCATION_ONFIELD+LOCATION_HAND+LOCATION_DECK+LOCATION_EXTRA
+function s.initial_effect(c)
+	--Activation
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and (tg~=nil and #tg>0 or tc>0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Cannot be destroyed
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetTargetRange(LOCATION_DESTROY,LOCATION_DESTROY)
+	e1:SetValue(s.efilter)
+	e1:SetLabelObject(re)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.efilter(e,te)
+	return te==e:GetLabelObject()
+end
---
+++ unofficial/c511600395.lua
@@ -0,0 +1,109 @@
+--DDアーク (Manga)
+--D/D Ark (Manga)
+--Scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pendulum Summon
+	Pendulum.AddProcedure(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC_G)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(s.pencon)
+	e1:SetOperation(s.penop)
+	e1:SetValue(SUMMON_TYPE_PENDULUM)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTargetRange(LOCATION_SZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsLocation,LOCATION_PZONE))
+	e2:SetLabelObject(e1)
+	c:RegisterEffect(e2)
+end
+function s.penfilter(c,e,tp,lscale,rscale)
+	return c:IsType(TYPE_PENDULUM) and Pendulum.Filter(c,e,tp,lscale,rscale,true)
+end
+function s.pencon(e,c,og)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+	if rpz==nil or c==rpz or Duel.GetFlagEffect(tp,10000000)>0 then return false end
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local loc=0
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_HAND end
+	if Duel.GetLocationCountFromEx(tp)>0 then loc=loc+LOCATION_EXTRA end
+	if loc==0 then return false end
+	local g=nil
+	if og then
+		g=og:Filter(Card.IsLocation,nil,loc)
+	else
+		g=Duel.GetFieldGroup(tp,loc,0)
+	end
+	return g:IsExists(s.penfilter,1,nil,e,tp,lscale,rscale)
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp,c,sg,inchain)
+	local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local ft1=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ft2=Duel.GetLocationCountFromEx(tp)
+	local ft=Duel.GetUsableMZoneCount(tp)
+	if ft1>0 then ft1=1 end
+	if ft2>0 then ft2=1 end
+	if ft>0 then ft=1 end
+	local loc=0
+	if ft1>0 then loc=loc+LOCATION_HAND end
+	if ft2>0 then loc=loc+LOCATION_EXTRA end
+	local tg=nil
+	if og then
+		tg=og:Filter(Card.IsLocation,nil,loc):Filter(s.penfilter,nil,e,tp,lscale,rscale)
+	else
+		tg=Duel.GetMatchingGroup(s.penfilter,tp,loc,0,nil,e,tp,lscale,rscale)
+	end
+	ft1=math.min(ft1,tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND))
+	ft2=math.min(ft2,tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA))
+	ft2=math.min(ft2,aux.CheckSummonGate(tp) or ft2)
+	while true do
+		local ct1=tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND)
+		local ct2=tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+		local ct=ft
+		if ct1>ft1 then ct=math.min(ct,ft1) end
+		if ct2>ft2 then ct=math.min(ct,ft2) end
+		local loc=0
+		if ft1>0 then loc=loc+LOCATION_HAND end
+		if ft2>0 then loc=loc+LOCATION_EXTRA end
+		local g=tg:Filter(Card.IsLocation,sg,loc)
+		if #g==0 or ft==0 then break end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tc=Group.SelectUnselect(g,sg,tp,#sg>0,Duel.IsSummonCancelable())
+		if not tc then break end
+		if sg:IsContains(tc) then
+				sg:RemoveCard(tc)
+				if tc:IsLocation(LOCATION_HAND) then
+				ft1=ft1+1
+			else
+				ft2=ft2+1
+			end
+			ft=ft+1
+		else
+			sg:AddCard(tc)
+			if tc:IsLocation(LOCATION_HAND) then
+				ft1=ft1-1
+			else
+				ft2=ft2-1
+			end
+			ft=ft-1
+		end
+	end
+	if #sg>0 then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.RegisterFlagEffect(tp,10000000,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
+		Duel.HintSelection(Group.FromCards(c))
+		Duel.HintSelection(Group.FromCards(rpz))
+	end
+end

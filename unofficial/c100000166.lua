7a789e0bf9e4afa40e8ff718e3897527d331d342
Larry126 <28985622+larry126@users.noreply.github.com>
2023-01-14 13:55:29 +0800

    Unofficial script updates
    - See https://github.com/ProjectIgnis/BabelCDB/commit/b77e7e40f2a16b9fffde40615737783210a7f3cc
    - Monster Recovery (Anime) updated
    in accordance with OCG rulings

--- unofficial/c100000166.lua
+++ unofficial/c100000166.lua
@@ -1,4 +1,5 @@
 --酸のラスト・マシン・ウィルス
+--Last Machine Acid Virus
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -27,7 +28,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if #conf>0 then
 		Duel.ConfirmCards(tp,conf)
 		local dg=conf:Filter(Card.IsRace,nil,RACE_MACHINE)
-		Duel.Destroy(dg,REASON_EFFECT)
+		local ct=Duel.Destroy(dg,REASON_EFFECT)
+		Duel.Damage(1-tp,ct*500,REASON_EFFECT)
 		Duel.ShuffleHand(1-tp)
 	end
 	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
@@ -69,7 +71,8 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	if #hg==0 then return end
 	Duel.ConfirmCards(1-ep,hg)
 	local dg=hg:Filter(Card.IsRace,nil,RACE_MACHINE)
-	Duel.Destroy(dg,REASON_EFFECT)
+	local ct=Duel.Destroy(dg,REASON_EFFECT)
+	Duel.Damage(1-tp,ct*500,REASON_EFFECT)
 	Duel.ShuffleHand(ep)
 end
 function s.turncon(e,tp,eg,ep,ev,re,r,rp)
--- unofficial/c511000373.lua
+++ unofficial/c511000373.lua
@@ -1,4 +1,4 @@
---モンスター回収 (Anime
+--モンスター回収 (Anime)
 --Monster Recovery (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
@@ -6,26 +6,29 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp)
-		and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,5)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND+LOCATION_MZONE,0,1,e:GetHandler()) end
 	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(5)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND+LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,5)
 end
+function s.filter(c,p)
+	return c:IsLocation(LOCATION_DECK) and c:IsControler(p)
+end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	local g=Duel.GetFieldGroup(p,LOCATION_HAND+LOCATION_MZONE,0)
-	if #g==0 then return end
-	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-	Duel.ShuffleDeck(p)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_HAND+LOCATION_MZONE,0,nil)
+	if #g==0 or Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==0 then return end
+	local og=Duel.GetOperatedGroup()
+	if not og:IsExists(Card.IsLocation,1,nil,LOCATION_DECK+LOCATION_EXTRA) then return end
+	if og:IsExists(s.filter,1,nil,p) then Duel.ShuffleDeck(p) end
 	Duel.BreakEffect()
-	Duel.Draw(p,d,REASON_EFFECT)
+	Duel.Draw(p,5,REASON_EFFECT)
 end

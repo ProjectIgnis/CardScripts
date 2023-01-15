ce51aa0bee891cc2ce1c5ca62df3f557fea09fd5
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-01-15 13:22:41 +0300

    Update c511027691.lua

--- unofficial/c511027691.lua
+++ unofficial/c511027691.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--draw
+	--Lower the ATK of an enemy monster
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)

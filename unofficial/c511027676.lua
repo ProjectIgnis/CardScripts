0ae239fdfe93af935313facf78404577e304c835
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-01-16 22:19:30 +0300

    Add files via upload

---
+++ unofficial/c511027676.lua
@@ -0,0 +1,49 @@
+ --白の悪夢ホワイト・ナイトメア
+--White Nightmare
+--Scripted by Beetron-1 Beetletop
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--boost atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.costfilter(c)
+	return c:IsWhite() and c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.cfilter(c)
+	return c:IsWhite() and c:IsFaceup()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	local ct=Duel.DiscardHand(tp,s.costfilter,1,60,REASON_COST)
+	e:SetLabel(ct)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(e:GetLabel()*500)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
+end

ac1e7127a5284ef313bd624760efb83434dd6151
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-01-19 22:01:00 +0100

    Added new Rush cards' scripts

---
+++ rush/c160012019.lua
@@ -0,0 +1,73 @@
+--エクスキューティー・フラーメ
+--Excutie Flame
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	--Increase ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsLevelAbove(6)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and	Duel.IsExistingMatchingCard(s.cfilter,c:GetControler(),0,LOCATION_MZONE,1,nil)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsLevelAbove(6)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)  end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 and Duel.SendtoGrave(g,REASON_COST)>0 then
+		--Effect
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			local atk=Duel.GetMatchingGroupCount(aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,LOCATION_MZONE,nil)*600
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+			e1:SetValue(atk)
+			c:RegisterEffectRush(e1)
+			--Other monsters cannot attack
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_FIELD)
+			e2:SetCode(EFFECT_CANNOT_ATTACK)
+			e2:SetTargetRange(LOCATION_MZONE,0)
+			e2:SetTarget(s.ftarget)
+			e2:SetLabel(c:GetFieldID())
+			e2:SetReset(RESET_PHASE+PHASE_END)
+			Duel.RegisterEffect(e2,tp)
+		end
+	end
+end
+function s.ftarget(e,c)
+	return c:GetFieldID()~=e:GetLabel()
+end
---
+++ rush/c160012055.lua
@@ -0,0 +1,61 @@
+--暴風輪
+--Ring of Storms
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsMonster() and c:IsAbleToDeckOrExtraAsCost() 
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,2,nil) end
+end
+function s.tdfilter(c,tp)
+	return c:IsAbleToDeck() and c:IsMonster() and Duel.IsExistingMatchingCard(s.tdfilter2,tp,0,LOCATION_GRAVE,1,c,c:GetCode())
+end
+function s.tdfilter2(c,code)
+	return c:IsAbleToDeck() and c:IsMonster() and not c:IsCode(code)
+end
+function s.tdfilter3(c)
+	return c:IsMonster() and c:IsAbleToDeck() 
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,0,LOCATION_GRAVE,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,2,tp,LOCATION_GRAVE)
+end
+function s.shufflefilter(c)
+	return c:IsFaceup() and c:IsLevelBelow(8) and c:IsAbleToDeck()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	-- Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.HintSelection(g,true)
+	if #g==0 or Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)==0 then return end
+	-- Effect
+	local dg=Duel.GetMatchingGroup(s.tdfilter3,tp,0,LOCATION_GRAVE,nil)
+	local sg=aux.SelectUnselectGroup(dg,e,tp,2,7,s.rescon,1,tp,HINTMSG_TODECK)
+	if #sg==0 then return end
+	Duel.HintSelection(sg,true)
+	if Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>=6 
+		and Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.shufflefilter),tp,0,LOCATION_MZONE,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local g2=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.shufflefilter),tp,0,LOCATION_MZONE,1,1,nil)
+		if #g2>0 then
+			Duel.HintSelection(g2,true)
+			Duel.SendtoDeck(g2,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+		end
+	end
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetCode)==#sg
+end

e20156a75c762c76a33ccf71e28616d61f70b7ca
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-01-22 00:12:41 +0200

    "Formud Skipper" fix + Link procedure small update
    Shouldn't be able to partially apply the effect of "Formud Skipper" when using it as Link Material (e.g. only apply the Attribute part, etc). Updated the Link procedure to be able to handle it properly.

--- official/c50366775.lua
+++ official/c50366775.lua
@@ -2,72 +2,52 @@
 --Formud Skipper
 local s,id=GetID()
 function s.initial_effect(c)
-	--link
+	--Reveal 1 Link Monster in your Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetTarget(s.revtg)
+	e1:SetOperation(s.revop)
 	c:RegisterEffect(e1)
-	--search
+	--Search 1 Level 5 or higher Cyberse monster
 	local e2=Effect.CreateEffect(c)
-	e2:SetCode(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_BE_MATERIAL)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_BE_MATERIAL)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.thcon)
+	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_LINK end)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
-function s.cfilter(c,tc,tp)
-	return c:IsLinkMonster() and not c:IsCode(tc:GetCode(nil,SUMMON_TYPE_LINK,tp))
+function s.revfilter(c,code)
+	return c:IsLinkMonster() and not c:IsCode(code)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e:GetHandler()) end
+function s.revtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local code=e:GetHandler():GetCode(nil,SUMMON_TYPE_LINK,tp)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.revfilter,tp,LOCATION_EXTRA,0,1,nil,code) end
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.revop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local code=c:GetCode(nil,SUMMON_TYPE_LINK,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local cg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_EXTRA,0,1,1,nil,c,tp)
-	if #cg==0 then return end
-	Duel.ConfirmCards(1-tp,cg)
-	local code1,code2=cg:GetFirst():GetOriginalCodeRule()
+	local rc=Duel.SelectMatchingCard(tp,s.revfilter,tp,LOCATION_EXTRA,0,1,1,nil,code):GetFirst()
+	if not rc then return end
+	Duel.ConfirmCards(1-tp,rc)
+	local code1,code2=rc:GetOriginalCodeRule()
+	--Can be treated as the revealed monster's name, Type, and Attribute, when used as Link Material
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_ADD_CODE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(50366775)
+	e1:SetLabel(ASSUME_CODE,code1,ASSUME_RACE,rc:GetRace(),ASSUME_ATTRIBUTE,rc:GetAttribute())
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	e1:SetOperation(s.chngcon)
-	e1:SetValue(code1)
 	c:RegisterEffect(e1)
-	if code2 then
-		local e2=e1:Clone()
-		e2:SetValue(code2)
-		c:RegisterEffect(e2)
-	end
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_ADD_ATTRIBUTE)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	e3:SetOperation(s.chngcon)
-	e3:SetValue(cg:GetFirst():GetOriginalAttribute())
-	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_ADD_RACE)
-	e4:SetValue(cg:GetFirst():GetOriginalRace())
-	c:RegisterEffect(e4)
-end
-function s.chngcon(scard,sumtype,tp)
-	return (sumtype&SUMMON_TYPE_LINK|MATERIAL_LINK)==SUMMON_TYPE_LINK|MATERIAL_LINK
-end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_LINK
 end
 function s.thfilter(c)
 	return c:IsRace(RACE_CYBERSE) and c:IsLevelAbove(5) and c:IsAbleToHand()
--- proc_link.lua
+++ proc_link.lua
@@ -25,7 +25,17 @@ function Link.AddProcedure(c,f,min,max,specialchk,desc)
 	c:RegisterEffect(e1)
 end
 function Link.ConditionFilter(c,f,lc,tp)
-	return c:IsCanBeLinkMaterial(lc,tp) and (not f or f(c,lc,SUMMON_TYPE_LINK|MATERIAL_LINK,tp))
+	local res1=c:IsCanBeLinkMaterial(lc,tp) and (not f or f(c,lc,SUMMON_TYPE_LINK|MATERIAL_LINK,tp))
+	local res2=false
+	local formud_eff=c:IsHasEffect(50366775)
+	if formud_eff then
+		local label={formud_eff:GetLabel()}
+		for i=1,#label-1,2 do
+			c:AssumeProperty(label[i],label[i+1])
+		end
+		res2=c:IsCanBeLinkMaterial(lc,tp) and (not f or f(c,lc,SUMMON_TYPE_LINK|MATERIAL_LINK,tp))
+	end
+	return res1 or res2
 end
 function Link.GetLinkCount(c)
 	if c:IsLinkMonster() and c:GetLink()>1 then
@@ -186,6 +196,20 @@ function Link.Operation(f,minc,maxc,specialchk)
 						ex[3]:GetValue()(1,SUMMON_TYPE_LINK,ex[3],ex[1]&g,c,tp)
 						if ex[3]:CheckCountLimit(tp) then
 							ex[3]:UseCountLimit(tp,1)
+						end
+					end
+				end
+				for tc in g:Iter() do
+					local formud_eff=tc:IsHasEffect(50366775)
+					if formud_eff then
+						local res1=tc:IsCanBeLinkMaterial(c,tp) and (not f or f(tc,c,SUMMON_TYPE_LINK|MATERIAL_LINK,tp))
+						local label={formud_eff:GetLabel()}
+						for i=1,#label-1,2 do
+							tc:AssumeProperty(label[i],label[i+1])
+						end
+						local res2=tc:IsCanBeLinkMaterial(c,tp) and (not f or f(tc,c,SUMMON_TYPE_LINK|MATERIAL_LINK,tp))
+						if not res2 or (res1 and res2 and not Duel.SelectEffectYesNo(tp,tc)) then
+							Duel.AssumeReset()
 						end
 					end
 				end

f2fbf3e1719de40fa700421947da4b5ad4e252a4
Naim <naimsantos45@gmail.com>
2023-01-27 11:59:22 -0300

    "The Origin Stone of Legend" fix
    It wasn't shuffling the Deck

--- rush/c160206013.lua
+++ rush/c160206013.lua
@@ -19,26 +19,23 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=4 end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function s.filter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsMonster() and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_NORMAL) and c:IsLegend() and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
 	Duel.ConfirmDecktop(tp,4)
 	local g=Duel.GetDecktopGroup(tp,4)
-	if g:IsExists(s.filter,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	if g:IsExists(s.spfilter,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local tg=g:FilterSelect(tp,s.filter,1,1,nil,e,tp)
+		local tg=g:FilterSelect(tp,s.spfilter,1,1,nil,e,tp)
 		if #tg>0 then
 			Duel.DisableShuffleCheck()
 			Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP)
 			g:RemoveCard(tg)
 		end
 	end
-	local ct=#g
-	if ct>0 then
-		Duel.MoveToDeckBottom(ct,tp)
-		Duel.SortDeckbottom(tp,tp,ct)
+	if #g>0 then
+		Duel.ShuffleDeck(tp)
 	end
 end
-

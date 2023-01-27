9fb1fb500fce6fdcc8bda3be43dc2f9409b1a4bb
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-01-27 02:40:43 +0200

    Update "aux.DelayedOperation"
    The delayed effect will now ignore any effect immunities.

--- utility.lua
+++ utility.lua
@@ -2104,6 +2104,7 @@ function Auxiliary.DelayedOperation(card_or_group,phase,flag,e,tp,oper,cond,rese
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetCode(EVENT_PHASE|phase)
 	e1:SetReset(reset,reset_count)
 	e1:SetCountLimit(1)

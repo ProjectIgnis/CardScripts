b40ad0d6b65f8f859f2b0e3495fb41abb670e493
Naim <naimsantos45@gmail.com>
2023-01-30 17:33:24 -0300

    "Orphebull the Harmonious Bullfighter Bard" fix
    It was using an incorret function for the summon limination, which would allow the player to Special Summon with effects (other than its own)

--- pre-release/c101111082.lua
+++ pre-release/c101111082.lua
@@ -3,7 +3,7 @@
 --Scripted by fiftyfour
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableUnsummonable()
+	c:EnableReviveLimit()
 	--Must be Special Summoned by own procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)

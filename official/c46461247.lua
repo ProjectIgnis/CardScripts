1fd5a02d7f2a17251ce82944e3bbfc38b98df483
Naim <naimsantos45@gmail.com>
2023-01-31 18:45:04 -0300

    "Trap Master" fix
    Fixed a bug where it would be unable to target a Trap monster

--- official/c46461247.lua
+++ official/c46461247.lua
@@ -1,7 +1,8 @@
 --トラップ・マスター
+--Trap Master
 local s,id=GetID()
 function s.initial_effect(c)
-	--flip
+	--Destroy 1 Trap card on the field
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
@@ -12,13 +13,13 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.filter(c)
-	return c:IsFacedown() or c:IsTrap()
+	return c:IsTrap() or (c:IsFacedown() and c:IsLocation(LOCATION_STZONE))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_SZONE) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and s.filter(chkc) end
 	if chk==0 then return true end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,e:GetHandler())
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,Nnil)
 	if #g>0 and g:GetFirst():IsFaceup() then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	end
@@ -27,6 +28,8 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		if tc:IsFacedown() then Duel.ConfirmCards(tp,tc) end
-		if tc:IsTrap() then Duel.Destroy(tc,REASON_EFFECT) end
+		if tc:IsTrap() then
+			Duel.Destroy(tc,REASON_EFFECT)
+		end
 	end
 end

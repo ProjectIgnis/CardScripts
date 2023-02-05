5cdaaaf3b26fac6c5b29d89be6e59ae6b1cc0c01
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-02-05 23:43:29 +0100

    Added new Rush card's script

--- rush/c160314001.lua
+++ rush/c160314001.lua
@@ -15,6 +15,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
+s.toss_coin=true
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPosition(POS_FACEUP_ATTACK)
 end

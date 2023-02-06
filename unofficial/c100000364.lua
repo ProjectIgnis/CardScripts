91448ba091fa62a8057c8da166c4729ac39e0496
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-02-06 23:06:12 +0200

    "Eternal Reverse" fix
    Shouldn't be able to flip a Pendulum Spell face-down.

--- unofficial/c100000364.lua
+++ unofficial/c100000364.lua
@@ -1,16 +1,18 @@
+--エターナル・リバース
 --Eternal Reverse
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c)
-	--change position
+	--Flip 1 face-up Spell/Trap your opponent controls face-down
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(s.tstg)
-	e1:SetOperation(s.tsop)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
-	--destruction replacement
+	--Send this card to the GY to prevent battle destruction
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_EQUIP)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
@@ -18,19 +20,19 @@ function s.initial_effect(c)
 	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsCanTurnSet()
+function s.setfilter(c)
+	return c:IsFaceup() and c:IsSpellTrap() and c:IsCanTurnSet() and not c:IsType(TYPE_PENDULUM)
 end
-function s.tstg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_SZONE) and s.filter(chkc) and c:IsControler(1-tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_SZONE,1,nil) end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and c:IsControler(1-tp) and s.setfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_SZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.setfilter,tp,0,LOCATION_SZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
-function s.tsop(e,tp,eg,ep,ev,re,r,rp)
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.ChangePosition(tc,POS_FACEDOWN)
 	end
 end
@@ -38,7 +40,7 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local tg=c:GetEquipTarget()
 	if chk==0 then return c and not c:IsStatus(STATUS_DESTROY_CONFIRMED) and tg and tg:IsReason(REASON_BATTLE) end
-	return Duel.SelectYesNo(tp,aux.Stringid(id,0))
+	return Duel.SelectEffectYesNo(tp,c,96)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE)

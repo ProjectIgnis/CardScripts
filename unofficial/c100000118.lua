44d71f8afddf4050b5afadc7ba50f7b9aaf4bea8
ClaireStanfield <51684310+ClaireStanfield@users.noreply.github.com>
2023-02-09 09:34:31 -0500

    Unofficial Fixes (#901)
    ---------
    
    Co-authored-by: Larry126 <28985622+larry126@users.noreply.github.com>

--- unofficial/c100000118.lua
+++ unofficial/c100000118.lua
@@ -2,7 +2,7 @@
 --Ojama Ride
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Special Summon up to 3 Level 4 or lower Machine Union monsters from your Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -13,44 +13,43 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_names={12482652,42941100,79335209}
-function s.costfilter(c,code)
-	return c:IsDiscardable() and c:IsCode(code)
+function s.costfilter(c)
+	return c:IsDiscardable() and c:IsAbleToGraveAsCost() and c:IsCode(12482652,42941100,79335209)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,12482652)
-		and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,42941100)
-		and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,79335209) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	local g1=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil,12482652)
-	local g2=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil,42941100)
-	g1:Merge(g2)
-	local g3=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil,79335209)
-	g1:Merge(g3)
-	Duel.SendtoGrave(g1,REASON_COST+REASON_DISCARD)
+	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_HAND,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,3,3,aux.dncheck,0) end
+	local cg=aux.SelectUnselectGroup(g,e,tp,3,3,aux.dncheck,1,tp,HINTMSG_DISCARD)
+	Duel.SendtoGrave(cg,REASON_COST+REASON_DISCARD)
 end
 function s.filter(c,e,tp)
-	return c:GetLevel()<=4 and c:IsRace(RACE_MACHINE) and c:IsType(TYPE_UNION) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_MACHINE) and c:IsType(TYPE_UNION)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft>3 then ft=3 end
 	if ft<=0 then return end
+	if ft>3 then ft=3 end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,ft,nil,e,tp)
-	for tc in aux.Next(g) do
-		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
+	local c=e:GetHandler()
+	for tc in g:Iter() do
+		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+			--Cannot change its battle position
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(3313)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+			e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+		end
 	end
 	Duel.SpecialSummonComplete()
 end
--- unofficial/c511001151.lua
+++ unofficial/c511001151.lua
@@ -1,8 +1,10 @@
+--悪魔の天秤
 --Shadow Balance
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Destroy monsters your opponent controls until both players control the same amount
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -11,29 +13,31 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	local e3=e1:Clone()
-	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp) 
-		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)>0
-		and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
+	return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)-Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,0,LOCATION_MZONE,ct,nil) end
-	local sg=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_MZONE,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,ct,0,0)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	local ct=#g-Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
+	if chk==0 then return ct>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,ct,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_MZONE,nil)
-	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)-Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	local ct=#g-Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
 	if ct>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local dg=g:Select(tp,ct,ct,nil)
-		Duel.Destroy(dg,REASON_EFFECT)
+		if ct<#g then 
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local dg=g:Select(tp,ct,ct,nil)
+			Duel.Destroy(dg,REASON_EFFECT)
+		else 
+			Duel.Destroy(g,REASON_EFFECT)
+		end
 	end
 end

a7dd8e146954ea5b759c571e6e8f02a0b0c51a00
Larry126 <28985622+larry126@users.noreply.github.com>
2023-02-14 10:14:48 +0800

    Update c511027692.lua

--- unofficial/c511027692.lua
+++ unofficial/c511027692.lua
@@ -1,48 +1,44 @@
 --白しろの鉄壁
 --White Barrier
 --Made by Beetron-1 Beetletop
+Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_BECOME_QUICK)
+	c:RegisterEffect(e0)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_DAMAGE)
 	e1:SetCondition(s.condition)
 	c:RegisterEffect(e1)
+	--You take no effect or battle 
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_BECOME_QUICK)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetCondition(s.damcon)
+	e2:SetValue(s.damval)
 	c:RegisterEffect(e2)
-	--Prevent damage
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_BE_BATTLE_TARGET)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCondition(s.damcon)
-	e3:SetOperation(s.damop)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_NO_EFFECT_DAMAGE)
 	c:RegisterEffect(e3)
 end
+s.listed_series={0x55d}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) and ep==tp
-end
-function s.cfilter(c)
-	return c:IsWhite() and c:IsMonster()
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsWhite),tp,LOCATION_MZONE,0,1,nil) and ep==tp
 end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	local ac=Duel.GetAttackTarget()
-	return ac and ac:IsWhite() and ac:IsControler(tp) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,ac)
+	return ac and ac:IsWhite() and ac:IsFaceup() and ac:IsControler(e:GetHandlerPlayer())
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsWhite),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,ac)
 end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetValue(0)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
-	Duel.RegisterEffect(e1,tp)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
-	Duel.RegisterEffect(e2,tp)
+function s.damval(e,re,val,r,rp,rc)
+	if r&(REASON_BATTLE|REASON_EFFECT)>0 then return 0 end
+	return val
 end

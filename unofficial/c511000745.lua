5a03ef05d7b718c534c7fa8a3cdded1ff98df2a5
Larry126 <28985622+larry126@users.noreply.github.com>
2023-02-15 10:02:26 +0800

    Fix "Phoenix Battle Wings"
    ATK gain calculation should now work correctly

--- unofficial/c511000745.lua
+++ unofficial/c511000745.lua
@@ -34,7 +34,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp,chk)
 	if eq then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_EQUIP)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetCode(EFFECT_SET_ATTACK)
 		e1:SetValue(eq:GetAttack()*2)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		c:RegisterEffect(e1)

fbb8eaa2e36d7dbb2dbd160723abf3adcb655b55
Naim <naimsantos45@gmail.com>
2023-02-16 18:39:23 -0300

    Update c160206008.lua

--- rush/c160206008.lua
+++ rush/c160206008.lua
@@ -2,14 +2,14 @@
 --Vanity's Fiend (Rush)
 local s,id=GetID()
 function s.initial_effect(c)
-	--cannot special summon
+	--Cannot be Special Summoned
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(aux.FALSE)
 	c:RegisterEffect(e1)
-	--disable spsummon
+	--Neither player can Special Summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetRange(LOCATION_MZONE)

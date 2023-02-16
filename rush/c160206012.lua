3a688af502b537d3976f9ef95c693204cbccbb22
Naim <naimsantos45@gmail.com>
2023-02-16 18:41:35 -0300

    Update c160206012.lua

--- rush/c160206012.lua
+++ rush/c160206012.lua
@@ -3,7 +3,7 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Excavate the top 3 cards and Special Summon
+	--Excavate the top 4 cards of your Deck and add monsters to the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)

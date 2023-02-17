6934f85f3cf8db0eb0384058223ddc0454297f3b
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-02-17 14:53:46 +0100

    Added new scripts (#933)

---
+++ rush/c160206009.lua
@@ -0,0 +1,41 @@
+--デーモン・ウォリアー
+--Archfiend Warrior
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send the top 3 cards of your Deck to the GY and draw 1 card
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_FIEND)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.filter(c)
+	return c:IsRace(RACE_FIEND) and c:IsLevel(6) and c:IsLocation(LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.DiscardDeck(tp,3,REASON_EFFECT)
+	local og=Duel.GetOperatedGroup()
+	local ct=og:FilterCount(s.filter,nil)
+	if ct>=1 then
+		Duel.BreakEffect()
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end

515c32e3c760affe69a8483867ef7d9aeaccf798
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-02-24 18:33:19 +0200

    Small updates for "Battle Shogun of the Six Samurai" and "Cosmic Slicer Zer'oll"
    They now use the correct function "Card.PlacesCounter" + general polishing and updates.

--- official/c1487805.lua
+++ official/c1487805.lua
@@ -1,12 +1,12 @@
 --宇宙鋏ゼロオル
---Cosmic Pincer Ze'loor
+--Cosmic Slicer Zer'oll
 --scripted by pyrQ
 local s,id=GetID()
 function s.initial_effect(c)
-	--Link Summon
 	c:EnableReviveLimit()
+	--Link Summon procedure
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_REPTILE),2)
-	--Search 1 A-Counter placing card
+	--Search 1 card that has an effect that places A-Counter(s)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -14,11 +14,11 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.thcon)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-	--Normal Summon 1 Reptile
+	--Normal Summon 1 Reptile monster
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SUMMON)
@@ -35,7 +35,7 @@ function s.initial_effect(c)
 	e3:SetCode(EFFECT_SET_POSITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,LOCATION_MZONE)
-	e3:SetTarget(s.postrigtg)
+	e3:SetTarget(function(_,c) return c:IsFaceup() and c:GetCounter(COUNTER_A)>0 end)
 	e3:SetValue(POS_FACEUP_DEFENSE)
 	c:RegisterEffect(e3)
 	--Opponent's monsters with an A-Counter cannot activate their effects
@@ -44,11 +44,9 @@ function s.initial_effect(c)
 	e4:SetValue(0)
 	c:RegisterEffect(e4)
 end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
-end
+s.counter_list={COUNTER_A}
 function s.thfilter(c)
-	return c:ListsCounter(COUNTER_A) and c:IsAbleToHand()
+	return c:PlacesCounter(COUNTER_A) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -70,16 +68,13 @@ function s.sumfilter(c)
 	return c:IsRace(RACE_REPTILE) and c:IsSummonable(true,nil)
 end
 function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
 end
 function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,nil)
 	if #g>0 then
 		Duel.Summon(tp,g:GetFirst(),true,nil)
 	end
-end
-function s.postrigtg(e,c)
-	return c:IsFaceup() and c:GetCounter(COUNTER_A)>0
 end
--- official/c74752631.lua
+++ official/c74752631.lua
@@ -1,12 +1,13 @@
 --六武衆の軍大将
---Great General of the Six Samurai
+--Battle Shogun of the Six Samurai
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableCounterPermit(0x3)
-	--link summon
-	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),2,2,s.lcheck)
 	c:EnableReviveLimit()
-	--to hand
+	--Bushido Counte permit
+	c:EnableCounterPermit(COUNTER_BUSHIDO)
+	--Link Summon procedure
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),2,2,s.lcheck)
+	--Search 1 card that has an effect that places a Bushido Counter(s)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -14,45 +15,42 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.thcon)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
 	e1:SetCost(s.thcost)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-	--counter
+	--Place 1 Bushido Counter on this card
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.ctcon)
-	e2:SetOperation(s.ctop)
+	e2:SetCondition(aux.zptcon(aux.FaceupFilter(Card.IsSetCard,SET_SIX_SAMURAI)))
+	e2:SetOperation(function(e) e:GetHandler():AddCounter(COUNTER_BUSHIDO,1) end)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
-	--atkup
+	--Gains 100 ATK for each Bushido Counter on your field
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e4:SetCode(EFFECT_UPDATE_ATTACK)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetValue(s.atkval)
+	e4:SetValue(function(_,c) return Duel.GetCounter(c:GetControler(),1,0,COUNTER_BUSHIDO)*100 end)
 	c:RegisterEffect(e4)
 end
-s.listed_series={0x3d}
-s.counter_place_list={0x3}
+s.listed_series={SET_SIX_SAMURAI}
+s.counter_place_list={COUNTER_BUSHIDO}
 function s.lcheck(g,lc,sumtype,tp)
-	return g:IsExists(Card.IsSetCard,1,nil,0x3d,lc,sumtype,tp)
-end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+	return g:IsExists(Card.IsSetCard,1,nil,SET_SIX_SAMURAI,lc,sumtype,tp)
 end
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
 function s.thfilter(c)
-	return c:ListsCounter(0x3) and c:IsAbleToHand()
+	return c:PlacesCounter(COUNTER_BUSHIDO) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -66,21 +64,3 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function s.cfilter(c,ec)
-	if c:IsLocation(LOCATION_MZONE) then
-		return c:IsSetCard(0x3d) and c:IsFaceup() and ec:GetLinkedGroup():IsContains(c)
-	else
-		return c:IsPreviousSetCard(0x3d) and c:IsPreviousPosition(POS_FACEUP)
-			and bit.extract(ec:GetLinkedZone(c:GetPreviousControler()),c:GetPreviousSequence())~=0
-	end
-end
-function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,e:GetHandler())
-end
-function s.ctop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():AddCounter(0x3,1)
-end
-function s.atkval(e,c)
-	return Duel.GetCounter(c:GetControler(),1,0,0x3)*100
-end
-

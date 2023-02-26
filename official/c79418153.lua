bdfb13649ca6bb39277b09ae9b8ee99b5b7213c7
Naim <naimsantos45@gmail.com>
2023-02-26 16:12:18 -0300

    Operator replacement [ci skip]

--- official/c79418153.lua
+++ official/c79418153.lua
@@ -30,7 +30,7 @@ function s.pierceop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_PIERCE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE)
 		a:RegisterEffect(e1)
 	end
 end

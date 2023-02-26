05522e4a059c8d0ac152f6313155fb9a820a0cf7
Larry126 <28985622+larry126@users.noreply.github.com>
2023-02-26 22:07:07 +0800

    Fix "Dark Tinker (Anime)"
    Wrong function name

--- unofficial/c511014001.lua
+++ unofficial/c511014001.lua
@@ -24,7 +24,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
 end

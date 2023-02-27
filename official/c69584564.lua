3a7ee238cfd49d6ef5743bfae7512085aa22c9ed
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-02-27 11:58:17 +0200

    "Fragrance Storm" fix
    If the target isn't a Plant anymore when it resolves then it shouldn't be destroyed.

--- official/c69584564.lua
+++ official/c69584564.lua
@@ -1,40 +1,38 @@
 --フレグランス・ストーム
+--Fragrance Storm
 local s,id=GetID()
 function s.initial_effect(c)
-	--draw
+	--Destroy 1 Plant monster on the field and draw 1 card
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW+CATEGORY_DESTROY)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsRace(RACE_PLANT)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:IsRace(RACE_PLANT) end
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1)
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+		and Duel.IsExistingTarget(aux.FaceupFilter(Card.IsRace,RACE_PLANT),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsRace,RACE_PLANT),tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.Destroy(tc,REASON_EFFECT)~=0 then
-		local ct=Duel.Draw(tp,1,REASON_EFFECT)
-		if ct==0 then return end
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRace(RACE_PLANT) and Duel.Destroy(tc,REASON_EFFECT)>0
+		and Duel.Draw(tp,1,REASON_EFFECT)>0 then
 		local dc=Duel.GetOperatedGroup():GetFirst()
-		if dc:IsRace(RACE_PLANT) and Duel.IsPlayerCanDraw(tp,1)
-			and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		if dc:IsRace(RACE_PLANT) and not dc:IsPublic() and Duel.IsPlayerCanDraw(tp,1)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 			Duel.BreakEffect()
 			Duel.ConfirmCards(1-tp,dc)
-			Duel.Draw(tp,1,REASON_EFFECT)
 			Duel.ShuffleHand(tp)
+			Duel.Draw(tp,1,REASON_EFFECT)
 		end
 	end
 end

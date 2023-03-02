b0940a11dc0c1b48fcf501364d9d2629059e3f8d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-02 21:15:36 +0200

    "Lady Labrynth of the Silver Castle" fix
    Now properly tracks negated activations for her Special Summoning effect.

--- official/c81497285.lua
+++ official/c81497285.lua
@@ -1,18 +1,18 @@
 --迷宮城の白銀姫
---Lady of the Labrynth
+--Lady Labrynth of the Silver Castle
 --scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
-	--Cannot be targeted by the opponent's card effect
+	--Cannot be targeted by the opponent's card effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCondition(s.tgcond)
 	e1:SetValue(aux.tgoval)
 	c:RegisterEffect(e1)
-	--Cannot be destroyed by the opponent's card effect
+	--Cannot be destroyed by the opponent's card effects
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e2:SetValue(aux.indoval)
@@ -26,11 +26,11 @@ function s.initial_effect(c)
 	e3:SetRange(LOCATION_HAND)
 	e3:SetHintTiming(0,TIMING_MAIN_END+TIMINGS_CHECK_MONSTER_E)
 	e3:SetCountLimit(1,id)
-	e3:SetCondition(function() return Duel.GetFlagEffect(0,id)>0 end)
+	e3:SetCondition(function() return Duel.GetCustomActivityCount(id,0,ACTIVITY_CHAIN)>0 or Duel.GetCustomActivityCount(id,1,ACTIVITY_CHAIN)>0 end)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
-	--Set 1 Normal Trap card from the Deck
+	--Set 1 Normal Trap from the Deck
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetType(EFFECT_TYPE_QUICK_O)
@@ -41,25 +41,17 @@ function s.initial_effect(c)
 	e4:SetTarget(s.settg)
 	e4:SetOperation(s.setop)
 	c:RegisterEffect(e4)
-	--Registers card's effects activated 
-	aux.GlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_CHAINING)
-		ge1:SetOperation(s.checkop)
-		Duel.RegisterEffect(ge1,0)
-	end)
+	--Check if a "Labrynth" card or effect or a Normal Trap Card was activated this turn
+	Duel.AddCustomActivityCounter(id,ACTIVITY_CHAIN,s.checkop)
 end
-s.listed_series={0x17f}
+s.listed_series={SET_LABRYNTH}
 s.listed_names={id}
 function s.tgcond(e)
 	return Duel.IsExistingMatchingCard(Card.IsFacedown,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
 end
-function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+function s.checkop(re)
 	local rc=re:GetHandler()
-	if (rc:IsSetCard(0x17f) and not rc:IsCode(id)) or (re:IsHasType(EFFECT_TYPE_ACTIVATE) and rc:GetType()==TYPE_TRAP) then
-		Duel.RegisterFlagEffect(0,id,RESET_PHASE+PHASE_END,0,1)
-	end
+	return not ((rc:IsSetCard(SET_LABRYNTH) and not rc:IsCode(id)) or (re:IsHasType(EFFECT_TYPE_ACTIVATE) and rc:IsNormalTrap()))
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -77,7 +69,7 @@ function s.setcond(e,tp,eg,ep,ev,re,r,rp)
 	return re:GetActiveType()==TYPE_TRAP and re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.setfilter(c,code)
-	return c:GetType()==TYPE_TRAP and c:IsSSetable() and not c:IsCode(code)
+	return c:IsNormalTrap() and c:IsSSetable() and not c:IsCode(code)
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil,re:GetHandler():GetCode()) end

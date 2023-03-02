dd30cb92c34230c8f2ece731310734f4f8067d95
Naim <naimsantos45@gmail.com>
2023-03-02 05:14:55 -0300

    Update "Synchro Chase"
    Fixed a bug where it would prevent even its controller's effect from being activated

--- official/c23442438.lua
+++ official/c23442438.lua
@@ -8,8 +8,9 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Special Summon
+	--Special Summon 1 monster used as material for a Synchro Summon
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -20,7 +21,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
-	--Limit
+	--Prevent activations in response to your monsters' effects
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_CHAINING)
@@ -28,15 +29,15 @@ function s.initial_effect(c)
 	e3:SetOperation(s.chainop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={0x66,0x1017,0xa3}
+s.listed_series={SET_WARRIOR,SET_SYNCHRON,SET_STARDUST}
 function s.cfilter(c,e,tp)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
-		and (c:IsSetCard(0x66) or c:IsSetCard(0x1017) or c:IsSetCard(0xa3))
-		and c:IsControler(tp) and c:GetMaterial():IsExists(s.spfilter,1,nil,e,tp,c)
+		and c:IsSetCard({SET_WARRIOR,SET_SYNCHRON,SET_STARDUST}) and c:IsControler(tp)
+		and c:GetMaterial():IsExists(s.spfilter,1,nil,e,tp,c)
 end
 function s.spfilter(c,e,tp,sync)
 	return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE)
-		and (c:GetReason()&(REASON_SYNCHRO+REASON_MATERIAL))==(REASON_SYNCHRO+REASON_MATERIAL)
+		and (c:GetReason()&(REASON_SYNCHROREASON_MATERIAL))==(REASON_SYNCHRO+REASON_MATERIAL)
 		and c:GetReasonCard()==sync and c:IsCanBeEffectTarget(e)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
@@ -53,15 +54,15 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end
 function s.chainop(e,tp,eg,ep,ev,re,r,rp)
 	local rc=re:GetHandler()
-	if re:IsActiveType(TYPE_MONSTER) and rc:IsType(TYPE_SYNCHRO) and (rc:IsOriginalSetCard(0x66) or rc:IsOriginalSetCard(0x1017) or rc:IsOriginalSetCard(0xa3)) then
+	if ep=tp and re:IsMonsterEffect() and rc:IsType(TYPE_SYNCHRO)
+		and (rc:IsOriginalSetCard(SET_WARRIOR) or rc:IsOriginalSetCard(SET_SYNCHRON) or rc:IsOriginalSetCard(SET_STARDUST)) then
 		Duel.SetChainLimit(s.chainlm)
 	end
 end

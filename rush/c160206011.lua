b5c80b729d9c6f2b422a2fd5085c4d33fcb0d876
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-03-02 13:59:30 +0100

    Added new Rush card's script

---
+++ rush/c160206011.lua
@@ -0,0 +1,44 @@
+--移り気の女神
+--Goddess of Whimsy
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Toss a coin
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COIN+CATEGORY_ATKCHANGE+CATEGORY_TOGRAVE+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.toss_coin=true
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,1)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,1,tp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	local c=e:GetHandler()
+	local res=Duel.TossCoin(tp,1)
+	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
+	if res==COIN_TAILS then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+		Duel.Draw(tp,1,REASON_EFFECT)
+	elseif res==COIN_HEADS then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(1000)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end

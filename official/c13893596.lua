b9073124d7124369aedd1305d589bcc7924f0655
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-04 22:11:53 +0200

    "Exodius the Ultimate Forbidden Lord" fix
    Moved the win condition inside the operation that sends a monster to the GY to eliminate some script errors in certain scenarios. Also includes small fixes and polishing.

--- official/c13893596.lua
+++ official/c13893596.lua
@@ -3,22 +3,22 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--cannot special summon
+	--Special Summoning condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	c:RegisterEffect(e1)
-	--special summon
+	--Special Summon procedure
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
-	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetCondition(s.spcon)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
-	--to grave
+	--Send 1 monster from your hand or Deck to the GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_TOGRAVE)
@@ -27,40 +27,29 @@ function s.initial_effect(c)
 	e3:SetTarget(s.tgtg)
 	e3:SetOperation(s.tgop)
 	c:RegisterEffect(e3)
-	--atkup
+	--Gains 1000 ATK for each Normal Monster in your GY
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetRange(LOCATION_MZONE)
 	e4:SetCode(EFFECT_UPDATE_ATTACK)
-	e4:SetValue(s.atkval)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(function(e,c) return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_NORMAL)*1000 end)
 	c:RegisterEffect(e4)
-	--redirect
+	--Banish itself it it leaves the field
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e5:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-	e5:SetCondition(s.recon)
+	e5:SetCondition(function(e) return e:GetHandler():IsFaceup() end)
 	e5:SetValue(LOCATION_REMOVED)
 	c:RegisterEffect(e5)
-	--win
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e6:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY)
-	e6:SetCode(EVENT_TO_GRAVE)
-	e6:SetRange(LOCATION_MZONE)
-	e6:SetOperation(s.winop)
-	c:RegisterEffect(e6)
 end
-s.listed_series={0x40}
-function s.cfilter(c)
-	return not c:IsAbleToDeckOrExtraAsCost()
-end
+s.listed_series={SET_FORBIDDEN_ONE}
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,0,nil)
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and #g>0
-		and not g:IsExists(s.cfilter,1,nil)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and #g>0 and #g==g:FilterCount(Card.IsAbleToDeckOrExtraAsCost,nil)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,0,nil)
@@ -68,34 +57,22 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
 end
-function s.tgfilter(c)
-	return c:IsMonster() and c:IsAbleToGrave()
-end
-function s.filter(c,rc)
-	return c:IsRelateToCard(rc) and c:IsSetCard(0x40) and c:IsMonster()
+function s.winfilter(c,rc)
+	return c:IsRelateToCard(rc) and c:IsSetCard(SET_FORBIDDEN_ONE) and c:IsMonster()
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local c=e:GetHandler()
-	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
-	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_GRAVE)
-		and c:IsRelateToEffect(e) and c:IsFaceup() then
-		tc:CreateRelation(c,RESET_EVENT+RESETS_STANDARD)
-	end
-end
-function s.atkval(e,c)
-	return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_NORMAL)*1000
-end
-function s.recon(e)
-	return e:GetHandler():IsFaceup()
-end
-function s.winop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,c)
-	if g:GetClassCount(Card.GetCode)==5 then
-		Duel.Win(tp,WIN_REASON_EXODIUS)
+	local tc=Duel.SelectMatchingCard(tp,aux.AND(Card.IsMonster,Card.IsAbleToGrave),tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) and c:IsRelateToEffect(e) then
+		tc:CreateRelation(c,RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
+		--Win condition
+		if not (c:IsFaceup() and c:IsOriginalCode(id) and c:IsControler(tp)) then return end
+		local g=Duel.GetMatchingGroup(s.winfilter,tp,LOCATION_GRAVE,0,nil,c)
+		if s.winfilter(tc,c) and g:GetClassCount(Card.GetCode)==5 then
+			Duel.Win(tp,WIN_REASON_EXODIUS)
+		end
 	end
 end

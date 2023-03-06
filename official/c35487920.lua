f17d7195ff6f2ae2678f992d9aa86ab32705f6c8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-06 10:24:07 +0200

    "Live☆Twin Channel" fix
    Its attack negation effect had the targeting flag but the effect doesn't target.

--- official/c35487920.lua
+++ official/c35487920.lua
@@ -7,23 +7,21 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_END_PHASE)
 	c:RegisterEffect(e1)
-	--Negate attack
+	--Negate an attack
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetRange(LOCATION_FZONE)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_FZONE)
 	e2:SetCountLimit(1,id)
 	e2:SetCost(s.negcost)
-	e2:SetOperation(s.negop)
+	e2:SetOperation(function() Duel.NegateAttack() end)
 	c:RegisterEffect(e2)
-	--Shuffle card into the deck or add it to hand
+	--Return 1 "Ki-sikil" or "Lil-la" monster from your GY to your Deck or hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_TOHAND)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetCode(EVENT_PHASE+PHASE_END)
@@ -33,42 +31,39 @@ function s.initial_effect(c)
 	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={0x153,0x154}
+s.listed_series={SET_KI_SIKIL,SET_LIL_LA}
 function s.costfilter(c,tp)
-	return (c:IsSetCard(0x153) or c:IsSetCard(0x154)) and c:IsMonster()
+	return (c:IsSetCard(SET_KI_SIKIL) or c:IsSetCard(SET_LIL_LA)) and c:IsMonster()
 end
 function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,nil,nil) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsSetCard,1,false,nil,nil,{SET_KI_SIKIL,SET_LIL_LA}) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsSetCard,1,1,false,nil,nil,{SET_KI_SIKIL,SET_LIL_LA})
 	Duel.Release(g,REASON_COST)
 end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	Duel.NegateAttack()
-end
-function s.cfilter(c,check)
-	return (c:IsSetCard(0x153) or c:IsSetCard(0x154)) and c:IsMonster()
-		and (c:IsAbleToDeck() or (check and c:IsAbleToHand()))
+function s.tgfilter(c,mon_check)
+	return c:IsSetCard({SET_KI_SIKIL,SET_LIL_LA}) and c:IsMonster() and (c:IsAbleToDeck() or (mon_check and c:IsAbleToHand()))
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local check=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.cfilter(chkc,check) end
-	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_GRAVE,0,1,nil,check) end
+	local mon_check=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tgfilter(chkc,mon_check) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_GRAVE,0,1,nil,mon_check) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil,check)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-	if g:GetFirst():IsAbleToHand() and check then
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_GRAVE,0,1,1,nil,mon_check)
+	if not mon_check then
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,tp,0)
+	else
+		Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,g,1,tp,0)
+		Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
 	end
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and tc:IsAbleToHand() and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			Duel.SendtoHand(tc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
-		else
-			Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
-		end
+	if not tc:IsRelateToEffect(e) then return end
+	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and tc:IsAbleToHand()
+		and (not tc:IsAbleToDeck() or Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	else
+		Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 	end
 end

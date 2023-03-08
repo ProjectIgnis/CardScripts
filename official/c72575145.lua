84c63ba7a54d8c805ee6b905d8a856483496389f
Naim <naimsantos45@gmail.com>
2023-03-08 16:48:09 -0300

    "Demotion" fix
    Fixed a bug where it would not be possible to equip it to a monster without a Level

--- official/c72575145.lua
+++ official/c72575145.lua
@@ -2,14 +2,11 @@
 --Demotion
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddEquipProcedure(c,nil,s.filter)
-	--lvl
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_UPDATE_LEVEL)
-	e2:SetValue(-2)
-	c:RegisterEffect(e2)
-end
-function s.filter(c)
-	return c:HasLevel()
+	aux.AddEquipProcedure(c)
+	--Decrease the equipped monster's Level by 2
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetValue(-2)
+	c:RegisterEffect(e1)
 end

a16e1cf5b51a83fbf564ad18d8300f21c6a82e11
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-09 13:16:42 +0200

    "Fallen of Albaz" fix
    Shouldn't be able to use other monsters you control as material through the effect of something like "Predaplant Triantis".

--- official/c68468459.lua
+++ official/c68468459.lua
@@ -3,14 +3,14 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	--Fusion summon
+	--Fusion Summon by using this card and opponent's monsters as material
 	local params = {nil,Fusion.CheckWithHandler(aux.FALSE),s.fextra,nil,Fusion.ForcedHandler}
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
 	e1:SetCost(s.cost)
 	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
@@ -20,8 +20,11 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 end
+function s.fcheck(tp,sg,fc)
+	return sg:FilterCount(aux.AND(Card.IsControler,Card.IsOnField),nil,tp)==1
+end
 function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil),s.fcheck
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end

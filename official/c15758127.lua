c7fca8f7dd34f8d769e790205d5e24bcd4b9d726
Naim <naimsantos45@gmail.com>
2023-03-15 05:53:23 -0300

    "Ursarctic Quint Charge" update
    Now it will send to the duel log the option chosen by the player

--- official/c15758127.lua
+++ official/c15758127.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Shuffle
+	--Make the opponent shuffle cards into the Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TODECK)
@@ -20,7 +20,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.tdtg)
 	e2:SetOperation(s.tdop)
 	c:RegisterEffect(e2)
-	--to hand
+	--Add 1 "Ursarctic" monster to the hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_TOHAND)
@@ -32,7 +32,7 @@ function s.initial_effect(c)
 	e3:SetTarget(s.thtg)
 	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
-	--special summon
+	--Special Summon 1 "Ursarctic" monster from the Extra Deck
 	local e4=e3:Clone()
 	e4:SetDescription(aux.Stringid(id,3))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -40,22 +40,22 @@ function s.initial_effect(c)
 	e4:SetOperation(s.spop)
 	c:RegisterEffect(e4)
 end
-s.listed_series={0x165}
+s.listed_series={SET_URSARCTIC}
 function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
 	local bc=tc:GetBattleTarget()
-	return tc:IsPreviousControler(tp) and tc:IsPreviousSetCard(0x165) and tc:GetType()&TYPE_MONSTER+TYPE_SYNCHRO==TYPE_MONSTER+TYPE_SYNCHRO
-		and (tc:GetBattlePosition()&POS_FACEUP)~=0
+	return tc:IsPreviousControler(tp) and tc:IsPreviousSetCard(SET_URSARCTIC)
+		and tc:GetType()&TYPE_MONSTER+TYPE_SYNCHRO==TYPE_MONSTER+TYPE_SYNCHRO
+		and (tc:GetBattlePosition()&POS_FACEUP)>0
 		and bc:IsRelateToBattle() and bc:IsControler(1-tp) and bc==Duel.GetAttacker()
 end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_HAND)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_HAND)
 	if chk==0 then return #g>7 end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g-7,0,0)
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_HAND)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_HAND)
 	if #g>7 then
 		local ct=#g-7
 		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_TODECK)
@@ -68,14 +68,14 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.PayLPCost(tp,700)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x165) and c:IsMonster() and c:IsAbleToHand()
+	return c:IsSetCard(SET_URSARCTIC) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_OPSELECTED,0,aux.Stringid(id,2))
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	local tc=g:GetFirst()
@@ -84,28 +84,28 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,tc)
 	end
 end
+function s.spcfilter(c)
+	return c:IsFaceup() and c:IsSetCard(SET_URSARCTIC) and c:HasLevel() and c:IsReleasableByEffect()
+end
 function s.spfilter(c,e,tp,sg)
 	local tc1=sg:GetFirst()
 	local tc2=sg:GetNext()
 	local lv=math.abs(tc1:GetLevel()-tc2:GetLevel())
-	return c:IsSetCard(0x165) and c:IsLevel(lv) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+	return c:IsSetCard(SET_URSARCTIC) and c:IsLevel(lv) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 		and Duel.GetLocationCountFromEx(tp,tp,sg,c)>0
 end
 function s.spcheck(sg,e,tp,mg)
 	return #sg==2 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,sg)
 end
-function s.spcfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x165) and c:HasLevel() and c:IsReleasableByEffect()
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)	
-	if chk==0 then 
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
 		local g=Duel.GetMatchingGroup(s.spcfilter,tp,LOCATION_MZONE,0,nil)
 		return aux.SelectUnselectGroup(g,e,tp,2,2,s.spcheck,0) 
 	end
+	Duel.Hint(HINT_OPSELECTED,0,aux.Stringid(id,3))
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local g=Duel.GetMatchingGroup(s.spcfilter,tp,LOCATION_MZONE,0,nil)
 	local rg=aux.SelectUnselectGroup(g,e,tp,2,2,s.spcheck,1,tp,HINTMSG_TOGRAVE,s.spcheck)
 	if #rg~=2 then return end

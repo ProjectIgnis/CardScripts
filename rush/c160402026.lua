e1652d9eb650ebbd5cf21ab44ff86ccab546a734
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-03-15 15:02:59 +0100

    Added new Rush card's script

---
+++ rush/c160402026.lua
@@ -0,0 +1,36 @@
+--ディープシー・ハンター
+--Deepsea Hunter
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--change position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp,chk)
+	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>=2
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsLevelBelow(6) and c:IsDefenseBelow(1000) and c:IsCanChangePositionRush()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	-- Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,2,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE,0,POS_FACEDOWN_DEFENSE,0)
+	end
+end

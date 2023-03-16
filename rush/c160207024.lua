7026b7cc74a7f91533c07f6c7c0391646dab5d23
Naim <naimsantos45@gmail.com>
2023-03-16 20:16:05 -0300

    "Super Electromagnetic Maximum" fix
    Fixed a bug where the player would be able to activate it even if the opponent did not control a monster

--- rush/c160207024.lua
+++ rush/c160207024.lua
@@ -3,16 +3,20 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Add up to 2 Machine monsters from the GY to the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
 end
 function s.filter(c)
 	return c:IsMonster() and c:IsRace(RACE_MACHINE) and c:IsLevel(10) and not c:IsPublic()
@@ -29,7 +33,6 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,2,nil)
 	if #g>0 then

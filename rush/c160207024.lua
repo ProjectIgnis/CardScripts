3b4a6b130cd3de57eb6b528b8d86a993f5f9889b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-17 01:25:46 +0200

    "Super Electromagnetic Maximum" fix
    Was missing the revealing requirement.

--- rush/c160207024.lua
+++ rush/c160207024.lua
@@ -6,44 +6,48 @@ function s.initial_effect(c)
 	--Add up to 2 Machine monsters from the GY to the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function(_,tp) return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 end)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0
-end
-function s.filter(c)
-	return c:IsMonster() and c:IsRace(RACE_MACHINE) and c:IsLevel(10) and not c:IsPublic()
+function s.revfilter(c)
+	return c:IsRace(RACE_MACHINE) and c:IsLevel(10) and not c:IsPublic()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.revfilter,tp,LOCATION_HAND,0,1,nil) end
 end
 function s.thfilter(c)
-	return c:IsMonster() and c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
+	return c:IsRace(RACE_MACHINE) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local revg=Duel.SelectMatchingCard(tp,s.revfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,revg)
+	Duel.ShuffleHand(tp)
+	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,2,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
+		local c=e:GetHandler()
+		--Cannot Normal Summon/Set from the hand
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_CANNOT_SUMMON)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetTargetRange(1,0)
-		e1:SetTarget(s.sumlimit)
+		e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_HAND) end)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
 		local e2=e1:Clone()
@@ -51,7 +55,4 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RegisterEffect(e2,tp)
 		Duel.AddNoTributeCheck(c,tp,id,0,1,0)
 	end
-end
-function s.sumlimit(e,c)
-	return c:IsLocation(LOCATION_HAND)
 end

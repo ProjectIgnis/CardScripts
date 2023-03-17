ab89cc05ba742e59f3438b185b034005ee84c67f
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-03-17 16:35:55 +0100

    Added new Rush cards' scripts

---
+++ rush/c160207046.lua
@@ -0,0 +1,41 @@
+--アビス・ガッシュ
+--Abyss Gush
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Increase the ATK of 1 monster you control
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_TO_DECK)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsPreviousLocation,1,nil,LOCATION_GRAVE)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,nil,0,tp,1000)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_SEASERPENT)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local tc=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.HintSelection(tc,true)
+		--Increase ATK
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(1000)
+		tc:RegisterEffect(e1)
+	end
+end
---
+++ rush/c160207047.lua
@@ -0,0 +1,38 @@
+--ラス・オブ・アビス
+--Wrath of Abyss
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate(summon)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_SEASERPENT) and c:IsAttribute(ATTRIBUTE_DARK|ATTRIBUTE_LIGHT) 
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,2,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	if chk==0 then return ep==1-tp and tc:IsFaceup() and tc:IsLocation(LOCATION_MZONE) end
+	tc:CreateEffectRelation(e)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+		local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+		if not Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil) and #g>0 then
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	end
+end
+

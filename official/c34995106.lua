1933be9215c9948b2f1705018bf6b1584eed8a98
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-21 14:10:40 +0200

    "Branded in White" fix
    Should be considered to mention "Fallen of Albaz" in its text.

--- official/c34995106.lua
+++ official/c34995106.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Fusion Summon
-	local e1=Fusion.CreateSummonEff(c,nil,nil,s.fextra,s.extraop,nil,nil,nil,nil,nil,nil,nil,nil,nil,s.extratg)
+	local e1=Fusion.CreateSummonEff({handler=c,extrafil=s.fextra,extraop=s.extraop,extratg=s.extratg})
 	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 	--Set itself during the End Phase
@@ -15,20 +15,19 @@ function s.initial_effect(c)
 	e2:SetOperation(s.regop)
 	c:RegisterEffect(e2)
 end
-function s.filterchk(c)
-	return c:IsCode(CARD_ALBAZ)
-end
+s.listed_names={CARD_ALBAZ}
 function s.fcheck(tp,sg,fc)
 	if not sg:IsExists(Card.IsRace,1,nil,RACE_DRAGON) then
 		return false
 	end
 	if sg:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE) then
-		return sg:IsExists(s.filterchk,1,nil) end
+		return sg:IsExists(Card.IsCode,1,nil,CARD_ALBAZ)
+	end
 	return true
 end
 function s.fextra(e,tp,mg)
 	local eg=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
-	if #eg>0 and (mg+eg):IsExists(s.filterchk,1,nil) then
+	if #eg>0 and (mg+eg):IsExists(Card.IsCode,1,nil,CARD_ALBAZ) then
 		if #eg>0 then
 			return eg,s.fcheck
 		end
@@ -38,33 +37,34 @@ end
 function s.extraop(e,tc,tp,sg)
 	local rg=sg:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
 	if #rg>0 then
-		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT|REASON_MATERIAL|REASON_FUSION)
 		sg:Sub(rg)
 	end
 end
 function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsReason(REASON_COST) and re and re:IsActivated() and re:IsActiveType(TYPE_MONSTER)
-		and re:GetHandler():IsCode(CARD_ALBAZ) then
+	if c:IsReason(REASON_COST) and re and re:IsActivated() and re:GetHandler():IsCode(CARD_ALBAZ) then
+		--Set itself from the GY
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(aux.Stringid(id,1))
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetCountLimit(1,{id,1})
 		e1:SetRange(LOCATION_GRAVE)
+		e1:SetCountLimit(1,{id,1})
 		e1:SetTarget(s.settg)
 		e1:SetOperation(s.setop)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsSSetable() end
-	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsSSetable() end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,tp,0)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()

2f2cafb51e1aaf4c74b57a7faaa71e745a8965e1
YoshiDuels <yoshi.katreuvin@gmail.com>
2023-03-21 15:17:46 +0100

    Added new Rush cards' scripts

---
+++ rush/c160439010.lua
@@ -0,0 +1,23 @@
+--伝説の剣
+--Legendary Sword (Rush)
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--equip
+	aux.AddEquipProcedure(c,nil,s.eqfilter,s.eqlimit)
+	--atk/def up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(300)
+	c:RegisterEffectRush(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffectRush(e2)
+end
+function s.eqfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_WARRIOR) and not c:IsMaximumModeSide()
+end
+function s.eqlimit(e,c)
+	return c:IsFaceup()
+end

db96284952b4c9d3c241a5797bef19603cee5e3c
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-03-23 14:14:25 +0100

    added Heavenly Invitation

---
+++ rush/c160207054.lua
@@ -0,0 +1,35 @@
+--天の招来
+--Heavenly Invitation
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send the top 3 cards of your Deck to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+end
+function s.thfilter(c)
+	return c:IsLevel(10) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.DiscardDeck(tp,3,REASON_EFFECT)
+	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) 
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.BreakEffect()
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+		end
+	end
+end

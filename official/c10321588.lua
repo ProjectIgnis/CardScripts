f25c916576006e79e47c34062ce6756cfab22149
Naim <naimsantos45@gmail.com>
2023-03-23 16:27:30 -0300

    "Sunlight Unicorn" fix
    Fixed a bug where it would not send an excavated Equip Spell to the GY if an effect like Mistake's was applying when it resolves

--- official/c10321588.lua
+++ official/c10321588.lua
@@ -1,30 +1,37 @@
 --サンライト・ユニコーン
+--Sunlight Unicorn
 local s,id=GetID()
 function s.initial_effect(c)
-	--
+	--Excavate the top card of your Deck and add it to the hand if it is an Equip Spell Card
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+	local g=Duel.GetDecktopGroup(tp,1)
+	if chk==0 then return #g>0 and g:GetFirst():IsAbleToHand() end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return end
 	Duel.ConfirmDecktop(tp,1)
 	local g=Duel.GetDecktopGroup(tp,1)
 	local tc=g:GetFirst()
-	if tc:IsType(TYPE_EQUIP) and tc:IsAbleToHand() then
+	if tc:IsEquipSpell() then
 		Duel.DisableShuffleCheck()
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ShuffleHand(tp)
+		if tc:IsAbleToHand() then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ShuffleHand(tp)
+		else
+			Duel.SendtoGrave(tc,REASON_RULE)
+		end
 	else
-		Duel.MoveSequence(tc,1)
+		Duel.MoveSequence(tc,SEQ_DECKBOTTOM)
 	end
 end

dd9c4ee442334b8cffad8f854bbba076b80725ce
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-03-26 15:10:01 +0200

    added new rush cards

---
+++ rush/c160013035.lua
@@ -0,0 +1,4 @@
+--エターナル・ギャラクティカ・オブリビオン
+--Eternal Galactica Oblivion
+--Alt artwork
+Duel.LoadScript("c160441003.lua")
---
+++ rush/c160013040.lua
@@ -0,0 +1,59 @@
+--ヴォイドヴェルグ・ギガントマキア
+--Voidvelgr Gigantomachia
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	-- fusion proc
+	Fusion.AddProcMix(c,true,true,160010025,160013008)
+	--Destroy 1 Spell/Trap your opponent controls
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) 
+		and e:GetHandler():GetEffectCount(EFFECT_EXTRA_ATTACK)==0 and Duel.IsAbleToEnterBP() end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local dg=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if chk==0 then return #dg>0 end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tg=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	if Duel.SendtoGrave(tg,REASON_COST)==0 then return end
+	--Effect
+	local dg=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if #dg>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local sg=dg:Select(tp,1,1,nil)
+		Duel.HintSelection(sg,true)
+		Duel.Destroy(sg,REASON_EFFECT)
+		--Increase ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
+		e1:SetValue(500)
+		c:RegisterEffect(e1)
+		--can attack twice
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(3201)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e2:SetCode(EFFECT_EXTRA_ATTACK)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e2)
+	end
+end

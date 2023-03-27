8a1e626c1b3ce33d8f2e8ace1466d8fd25113c49
Larry126 <28985622+larry126@users.noreply.github.com>
2023-03-27 16:05:38 +0800

    Update Roll of Fate
    Banish instead of sending to grave

--- unofficial/c511001142.lua
+++ unofficial/c511001142.lua
@@ -1,9 +1,11 @@
+--運命の宝札
 --Roll of Fate
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DICE+CATEGORY_DRAW+CATEGORY_DECKDES)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DICE+CATEGORY_DRAW+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
@@ -12,16 +14,17 @@ function s.initial_effect(c)
 end
 s.roll_dice=true
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetDecktopGroup(tp,1)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1,6) and Duel.IsPlayerCanDiscardDeck(tp,1) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanRemove(tp) end
 	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.TossDice(tp,1)
-	Duel.Draw(tp,ct,REASON_EFFECT)
-	Duel.BreakEffect()
-	Duel.DisableShuffleCheck()
-	Duel.DiscardDeck(tp,ct,REASON_EFFECT)
+	local res=Duel.TossDice(tp,1)
+	local ct=Duel.Draw(tp,res,REASON_EFFECT)
+	if ct>0 then
+		local rg=Duel.GetDecktopGroup(tp,ct)
+		Duel.DisableShuffleCheck()
+		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)
+	end
 end

9b63dbe3497d85d5594b0012f56c2cc7e7d51b98
Larry126 <28985622+larry126@users.noreply.github.com>
2023-04-01 04:09:43 +0800

    Fix Yasushi the Skull Knight
    Fusion Material fixed

--- unofficial/c511000990.lua
+++ unofficial/c511000990.lua
@@ -5,6 +5,22 @@ function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,80604091,78010363)
+	aux.GlobalCheck(s,function()
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD)
+		ge2:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
+		ge2:SetTargetRange(LOCATION_ALL,LOCATION_ALL)
+		ge2:SetTarget(s.mttg)
+		ge2:SetValue(s.mtval)
+		Duel.RegisterEffect(ge2,0)
+	end)	
 end
 s.listed_names={80604091,78010363}
 s.illegal=true
+function s.mttg(e,c)
+	return c:IsCode(80604091) and not c:IsMonster()
+end
+function s.mtval(e,c)
+	if not c then return false end
+	return c:IsOriginalCode(id)
+end

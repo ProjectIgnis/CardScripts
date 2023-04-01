892eecc60abd87db9c648a174a3cca6149aefe59
Larry126 <28985622+larry126@users.noreply.github.com>
2023-04-01 17:51:50 +0800

    Fix Cold Sleeper
    It should now Special Summon at the end of the Battle Phase as the text

--- unofficial/c511000080.lua
+++ unofficial/c511000080.lua
@@ -1,7 +1,8 @@
+--コールド・スリーパー
 --Cold Sleeper
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -13,29 +14,39 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.cfilter(c,e,tp)
-	return c:IsLocation(LOCATION_GRAVE) and c:IsPreviousControler(tp) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsLocation(LOCATION_GRAVE) and c:IsPreviousControler(tp)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=eg:GetFirst()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1 and eg:IsExists(s.cfilter,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and eg:IsExists(s.cfilter,1,nil,e,tp) end
 	local dis=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
 	e:SetLabel(dis)
-	local g=eg:Filter(s.cfilter,nil,e,tp)
-	Duel.SetTargetCard(g)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetTargetCard(eg:Filter(s.cfilter,nil,e,tp))
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_DISABLE_FIELD)
-	e1:SetOperation(s.disop)
 	e1:SetLabel(e:GetLabel())
-	e:GetHandler():RegisterEffect(e1)
-	Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
-	Duel.SpecialSummonComplete()
+	e1:SetOperation(function() return e1:GetLabel() end)
+	Duel.RegisterEffect(e1,tp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		aux.DelayedOperation(tc,PHASE_BATTLE,id,e,tp,s.op)
+	end
 end
-function s.disop(e,tp)
-	return e:GetLabel()
+function s.op(ag,e,tp)
+	local tc=ag:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) then
+		-- Cannot change its battle position
+		local e1=Effect.CreateEffect(e:GetOwner())
+		e1:SetDescription(3313)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+	Duel.SpecialSummonComplete()
 end

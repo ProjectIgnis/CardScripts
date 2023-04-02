6751580b1f53bf2ca80ed2ce86aa5bcb28704a40
YoshiDuels <14310248+Yoshi80@users.noreply.github.com>
2023-04-02 15:24:14 +0200

    Added new Rush cards' scripts

---
+++ rush/c160013036.lua
@@ -0,0 +1,54 @@
+--タマーボット・バーストドラゴン
+--Tamabot Burst Dragon
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,160010101,CARD_TAMABOT)
+	-- Gain 500 ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(Card.IsCode,e:GetHandlerPlayer(),LOCATION_GRAVE,LOCATION_GRAVE,nil,CARD_TAMABOT)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,ct) end
+end
+function s.desfilter(c)
+	return c:IsAttackAbove(1500) and c:IsFaceup()
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsAttackBelow(1500) and c:IsLocation(LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	-- Requirement
+	local c=e:GetHandler()
+	local ct=Duel.GetMatchingGroupCount(Card.IsCode,c:GetControler(),LOCATION_GRAVE,LOCATION_GRAVE,nil,CARD_TAMABOT)
+	if Duel.DiscardDeck(tp,ct,REASON_COST)==0 then return end
+	local og=Duel.GetOperatedGroup()
+	--Effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+	e1:SetValue(1000)
+	c:RegisterEffect(e1)
+	local desg=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
+	local ct2=og:FilterCount(s.filter,nil)
+	if #desg>0 and ct2>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local tg=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.desfilter),tp,0,LOCATION_MZONE,1,1,nil)
+		if #tg>0 then 
+			tg=tg:AddMaximumCheck()
+			Duel.HintSelection(tg,true)
+			Duel.Destroy(tg,REASON_EFFECT) 
+		end
+	end
+end

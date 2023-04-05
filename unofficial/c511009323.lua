4a053efd11cb3475b2756dc8101a9bc4a9581849
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-04-04 17:07:44 -0700

    Update "The Phantom Knights of Lost Vambrace (Anime)"
    Missing damage step handling/general script clean-up

--- unofficial/c511009323.lua
+++ unofficial/c511009323.lua
@@ -1,25 +1,31 @@
---The Phantom Knights of Lost Vambrace
+--幻影騎士団ロスト・ヴァンブレイズ (Anime)
+--The Phantom Knights of Lost Vambrace (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--1 monster your opponent controls loses ATK/Special Summon this card
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_series={SET_THE_PHANTOM_KNIGHTS}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
-	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:IsHasType(EFFECT_TYPE_ACTIVATE) 
-			and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x10db,0x11,2,600,0,RACE_WARRIOR,ATTRIBUTE_DARK) 
-			and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:IsHasType(EFFECT_TYPE_ACTIVATE) 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,SET_THE_PHANTOM_KNIGHTS,0x11,2,600,0,RACE_WARRIOR,ATTRIBUTE_DARK) 
+		and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
@@ -29,29 +35,25 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		e1:SetValue(-600)
 		tc:RegisterEffect(e1)
 	end
-	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x10db,0x11,2,600,0,RACE_WARRIOR,ATTRIBUTE_DARK) then
+	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp,id,SET_THE_PHANTOM_KNIGHTS,0x11,2,600,0,RACE_WARRIOR,ATTRIBUTE_DARK) then
 		Duel.BreakEffect()
 		c:AddMonsterAttribute(TYPE_NORMAL)
 		Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP_ATTACK)
 		c:AddMonsterAttributeComplete()
-		
+		--"The Phantom Knights" monsters you control cannot be destroyed by battle this turn
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		e1:SetTarget(s.filter)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetReset(RESET_PHASE|PHASE_END)
+		e1:SetTarget(function(e,c) return c:IsSetCard(SET_THE_PHANTOM_KNIGHTS) end)
 		e1:SetValue(1)
 		Duel.RegisterEffect(e1,tp)
-		
 		Duel.SpecialSummonComplete()
 	end
-end
-function s.filter(e,c)
-	return c:IsSetCard(0x10db)
 end

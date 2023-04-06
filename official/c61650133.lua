ecfc75fba6f0b0335c82c28f04009b23734d0735
Naim <naimsantos45@gmail.com>
2023-04-06 15:56:05 -0300

    Update "Summoning Curse"
    Update to make it call the correct string in the End Phase

--- official/c61650133.lua
+++ official/c61650133.lua
@@ -1,12 +1,13 @@
 --召喚の呪詛
+--Summoning Curse
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--maintain
+	--Pay 500 LP as maintenance cost in the End Phase
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
@@ -16,7 +17,7 @@ function s.initial_effect(c)
 	e2:SetCondition(s.mtcon)
 	e2:SetOperation(s.mtop)
 	c:RegisterEffect(e2)
-	--remove
+	--Banish 1 card from the hand when a monster is Special Summoned
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_REMOVE)
@@ -28,10 +29,10 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.mtcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
+	return Duel.IsTurnPlayer(tp)
 end
 function s.mtop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.CheckLPCost(tp,500) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+	if Duel.CheckLPCost(tp,500) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.PayLPCost(tp,500)
 	else
 		Duel.Destroy(e:GetHandler(),REASON_COST)
@@ -41,11 +42,9 @@ function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsRelateToEffect(e) end
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local rm1=false
 	local rm2=false
-	local tc=eg:GetFirst()
-	for tc in aux.Next(eg) do
+	for tc in eg:Iter() do
 		if tc:IsOnField() then
 			if tc:IsControler(tp) then rm1=true else rm2=true end
 		end

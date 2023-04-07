6edc82b7c684c1e59387749623214ca49928eca4
Naim <naimsantos45@gmail.com>
2023-04-07 05:21:51 -0300

    Fixed "Amplifier"
    fixed a bug where it would be sent to the GY during the initial activation

--- official/c303660.lua
+++ official/c303660.lua
@@ -8,9 +8,10 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-	e1:SetTarget(s.target)
+	e1:SetTarget(s.eqptg)
+	e1:SetOperation(s.eqpop)
 	c:RegisterEffect(e1)
-	--Equip limit
+	--Can only be equipped to "Jinzo"
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_EQUIP_LIMIT)
@@ -27,13 +28,13 @@ function s.initial_effect(c)
 	e3:SetTarget(s.etarget)
 	e3:SetValue(s.efilter)
 	c:RegisterEffect(e3)
-	--Destroy
+	--Destroy the equipped monster when this card leaves the field
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
 	e4:SetCode(EVENT_LEAVE_FIELD)
 	e4:SetOperation(s.desop)
 	c:RegisterEffect(e4)
-	--Cannot disable
+	--Effect cannot be negated
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
 	e5:SetCode(EFFECT_CANNOT_DISABLE)
@@ -46,20 +47,19 @@ end
 function s.filter(c)
 	return c:IsFaceup() and c:IsCode(CARD_JINZO)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.eqptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAIN_SOLVING)
-	e1:SetReset(RESET_CHAIN)
-	e1:SetLabel(Duel.GetCurrentChain())
-	e1:SetLabelObject(e)
-	e1:SetOperation(aux.EquipEquip)
-	Duel.RegisterEffect(e1,tp)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.eqpop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+	end
 end
 function s.etarget(e,c)
 	local ec=e:GetHandler():GetEquipTarget()

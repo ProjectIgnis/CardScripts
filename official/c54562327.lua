58b308eb5071da3e81011b2e20c8128eacc82d6b
Naim <naimsantos45@gmail.com>
2023-04-07 05:06:11 -0300

    Fix "Stake Your Soul!"
    Fixed a bug where it would be possible to activate it even without a free main monster zone

--- official/c54562327.lua
+++ official/c54562327.lua
@@ -27,7 +27,8 @@ function s.spfilter(c,e,tp,rc)
 		and not c:IsCode(rc:GetCode()) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	e:SetLabelObject(g:GetFirst())

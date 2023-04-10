fb3c5727fd3a7ba4258c96fafd8e32d0024005c2
Naim <naimsantos45@gmail.com>
2023-04-10 17:06:36 -0300

    "Damage Diet" update
    -The effects should not stack
    -The player cannot activate another Damage Diet if one has already applied its effect. Also, the player cannot use the GY effect of Damage Diet if either of the effects was already applied that turn 
    -The player can chain one Damage Diet to each, but only 1 will be applied

--- official/c95448692.lua
+++ official/c95448692.lua
@@ -1,12 +1,12 @@
 --ダメージ・ダイエット
 --Damage Diet
-
 local s,id=GetID()
 function s.initial_effect(c)
 	--Halve all damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	--Halve effect damage
@@ -15,8 +15,12 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.target2)
 	e2:SetOperation(s.activate2)
 	c:RegisterEffect(e2)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(tp,id)~=0 then return end
@@ -26,8 +30,12 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetValue(s.val1)
-	e1:SetReset(RESET_PHASE+PHASE_END,1)
+	e1:SetReset(RESET_PHASE|PHASE_END,1)
 	Duel.RegisterEffect(e1,tp)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 and Duel.GetFlagEffect(tp,id+1)==0 end
 end
 function s.activate2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFlagEffect(tp,id+1)~=0 then return end
@@ -37,9 +45,9 @@ function s.activate2(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetValue(s.val2)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,1)
+	Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE|PHASE_END,0,1)
 end
 function s.val1(e,re,dam,r,rp,rc)
 	if r&(REASON_BATTLE+REASON_EFFECT)~=0 then

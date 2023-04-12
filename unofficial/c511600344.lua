bea61466fa31319632b64ba8b9132c1eb9e50068
Larry126 <28985622+larry126@users.noreply.github.com>
2023-04-12 15:51:52 +0800

    Unofficial script fixes
    - Guarded Treasure (Anime): in line with the corrected text
    - Code Hack: fixes
    - Soul Exchange (Anime): Fix the error where you were able to tribute monsters your opponent controls that were controlled by you

--- unofficial/c511600344.lua
+++ unofficial/c511600344.lua
@@ -3,40 +3,39 @@
 --Scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--indes
+	--"Code Talker" monsters you control cannot be destroyed by card effects
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x101))
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_CODE_TALKER))
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--atkup
+	--ATK change
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(30603688,1))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e3:SetHintTiming(TIMING_DAMAGE_STEP)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCountLimit(1)
 	e3:SetCondition(s.atkcon)
+	e3:SetTarget(s.atktg)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
-	--negate
+	--Negate
 	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_NEGATE+CATEGORY_ATKCHANGE)
 	e4:SetType(EFFECT_TYPE_QUICK_O)
 	e4:SetCode(EVENT_CHAINING)
 	e4:SetRange(LOCATION_GRAVE)
-	e4:SetHintTiming(TIMING_DAMAGE_STEP+TIMING_DAMAGE_CAL)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e4:SetCondition(s.negcon)
 	e4:SetCost(aux.bfgcost)
@@ -44,52 +43,53 @@ function s.initial_effect(c)
 	e4:SetOperation(s.negop)
 	c:RegisterEffect(e4)
 end
-s.listed_series={0x101}
+s.listed_series={SET_CODE_TALKER}
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetCurrentPhase()~=PHASE_BATTLE_STEP then return false end
-	local c=Duel.GetAttackTarget()
-	if not c then return false end
-	local tc=Duel.GetAttacker()
-	if c:IsControler(tp) then c,tc=tc,c end
-	e:SetLabelObject(c)
-	return tc:IsSetCard(0x101) and tc:IsRelateToBattle() and not c:IsControler(tp) and c:IsRelateToBattle()
+	local sc,oc=Duel.GetBattleMonster(tp)
+	if not oc or Duel.GetCurrentPhase()~=PHASE_BATTLE_STEP then return false end
+	e:SetLabelObject(oc)
+	return sc and sc:IsFaceup() and sc:IsSetCard(SET_CODE_TALKER)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=e:GetLabelObject()
+	if chk==0 then return tc and tc:IsFaceup() and tc:GetAttack()>0 end
+	Duel.SetTargetCard(tc)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp,chk)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local c=e:GetLabelObject()
-	if c:IsFaceup() and c:IsRelateToBattle() then
-		local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		--Change ATK to 0
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE)
 		e1:SetValue(0)
-		c:RegisterEffect(e1)
+		tc:RegisterEffect(e1)
 	end
-	local e2=Effect.CreateEffect(e:GetHandler())
+	--Cannot be destroyed by that battle
+	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e2:SetValue(1)
-	e2:SetTarget(function(e,c) return c:IsRelateToBattle() end)
-	e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	e2:SetTarget(function(_,_c) return _c:IsRelateToBattle() end)
+	e2:SetReset(RESET_PHASE|PHASE_DAMAGE)
 	Duel.RegisterEffect(e2,tp)
-	local e3=Effect.CreateEffect(e:GetHandler())
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e3:SetOperation(s.damop)
-	e3:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	--Take no battle damage from that battle
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(1,1)
+	e3:SetReset(RESET_PHASE|PHASE_DAMAGE)
 	Duel.RegisterEffect(e3,tp)
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ChangeBattleDamage(tp,0)
-	Duel.ChangeBattleDamage(1-tp,0)
 end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetCurrentPhase()&(PHASE_DAMAGE|PHASE_DAMAGE_CAL)==0 or Duel.IsDamageCalculated() then return false end
-	local tc=Duel.GetAttacker()
-	if not tc:IsControler(tp) then tc=Duel.GetAttackTarget() end
-	return tc and tc:IsControler(tp) and tc:IsSetCard(0x101)
-		and ep==1-tp and Duel.IsChainNegatable(ev) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+	local sc=Duel.GetBattleMonster(tp)
+	return sc and sc:IsFaceup() and sc:IsSetCard(SET_CODE_TALKER) and ep==1-tp
+		and Duel.IsChainNegatable(ev) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -97,13 +97,14 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) then
-		local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,0x101),tp,LOCATION_MZONE,0,nil)
-		for tc in aux.Next(g) do
-			local e1=Effect.CreateEffect(e:GetHandler())
+		local c=e:GetHandler()
+		local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_CODE_TALKER),tp,LOCATION_MZONE,0,nil)
+		for tc in g:Iter() do
+			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(700)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 			tc:RegisterEffect(e1)
 		end
 	end
--- unofficial/c513000054.lua
+++ unofficial/c513000054.lua
@@ -1,4 +1,5 @@
---クロス・ソウル
+--クロス・ソウル (Anime)
+--Soul Exchange (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -13,14 +14,21 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	local tc=g:GetFirst()
-	while tc do
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_EXTRA_RELEASE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		tc=g:GetNext()
+	local c=e:GetHandler()
+	local fid=c:GetFieldID()
+	for p=0,1 do
+		local pfid=fid+p
+		local g=Duel.GetFieldGroup(p,0,LOCATION_MZONE)
+		if #g>0 then
+			g:ForEach(function(c) c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,0,1,pfid) end)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_EXTRA_RELEASE)
+			e1:SetTargetRange(0,LOCATION_MZONE)
+			e1:SetTarget(function(_,c) return c:GetFlagEffectLabel(id)==pfid end)
+			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+			e1:SetReset(RESET_PHASE|PHASE_END)
+			Duel.RegisterEffect(e1,p)
+		end
 	end
 end

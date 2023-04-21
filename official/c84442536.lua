725fcb1488637789d8ea4dbddcbae83fbf2c1057
utso <cccygodev@gmail.com>
2023-04-21 23:18:15 +0200

    Fix Graydle Combat categories

--- official/c84442536.lua
+++ official/c84442536.lua
@@ -4,7 +4,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCondition(s.condition)
@@ -45,6 +44,9 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 			Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 		end
+		e:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	else
+		e:SetCategory(0)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)

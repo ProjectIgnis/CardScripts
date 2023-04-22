d2ea4ed0ff041f7416f4dfbfe84870ef86bb0994
Larry126 <28985622+larry126@users.noreply.github.com>
2023-04-22 14:59:08 +0800

    Fix "Infinity Wall"

--- unofficial/c511000979.lua
+++ unofficial/c511000979.lua
@@ -1,3 +1,4 @@
+--インフィニティ・ウォール
 --Infinity Wall
 local s,id=GetID()
 function s.initial_effect(c)
@@ -9,30 +10,28 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x3013)
-end
+s.listed_series={SET_MEKLORD_EMPEROR}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_MEKLORD_EMPEROR),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e1:SetOperation(s.negop)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetCode(EVENT_CHAIN_SOLVING)
+	e1:SetCondition(s.discon)
+	e1:SetOperation(s.disop)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.cfilter2(c,tp)
+function s.cfilter(c,tp)
 	return c:IsOnField() and c:IsControler(tp)
 end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsChainNegatable(ev) then return end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	if re:IsHasCategory(CATEGORY_NEGATE)
-		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+		and Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
 	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
-	if ex and tg~=nil and tc+tg:FilterCount(s.cfilter2,nil,tp)-#tg>0 then
-		Duel.NegateEffect(ev)
-	end
+	return ex and tg~=nil and tc+tg:FilterCount(s.cfilter,nil,tp)-#tg>0 
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
 end

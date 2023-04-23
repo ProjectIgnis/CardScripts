c51cec1210c2090a90002a98a3519249ca443bfe
Naim <naimsantos45@gmail.com>
2023-04-23 11:58:24 -0300

    Fix "Silent Wooby"
    If the player doesn't draw a card no LP should be gained

--- official/c71015787.lua
+++ official/c71015787.lua
@@ -1,7 +1,8 @@
 --サイレント・ウォビー
+--Silent Wooby
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon
+	--Special Sumon itself to the opponent's field
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -10,18 +11,18 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--draw/recover
+	--Draw 1 card and the opponent gains 2000 LP
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DRAW+CATEGORY_RECOVER)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.effcon)
+	e2:SetCondition(function(e) return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1 end)
 	e2:SetTarget(s.efftg)
 	e2:SetOperation(s.effop)
 	c:RegisterEffect(e2)
-	--limit
+	--Change the hand size limit
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_HAND_LIMIT)
@@ -42,15 +43,13 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
 	end
 end
-function s.effcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
-end
 function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,1-tp,2000)
 end
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Draw(tp,1,REASON_EFFECT)
-	Duel.Recover(1-tp,2000,REASON_EFFECT)
+	if Duel.Draw(tp,1,REASON_EFFECT)>0 then
+		Duel.Recover(1-tp,2000,REASON_EFFECT)
+	end
 end

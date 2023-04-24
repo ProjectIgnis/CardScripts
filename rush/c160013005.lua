d382f8b23a299b941d649b4dfa06e36776371e2c
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-04-24 13:48:40 +0200

    added new rush cards

---
+++ rush/c160013005.lua
@@ -0,0 +1,44 @@
+--ＤＢＦリビルドラ
+--DBF Rebuildra
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Gain LP
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(e) return e:GetHandler():IsStatus(STATUS_SUMMON_TURN+STATUS_SPSUMMON_TURN) end)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_BLUETOOTH_B_DRAGON,CARD_REDBOOT_B_DRAGON,160013048,160013049}
+function s.cfilter(c)
+	return c:IsCode(CARD_BLUETOOTH_B_DRAGON,CARD_REDBOOT_B_DRAGON) and not c:IsPublic()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.setfilter(c)
+	return c:IsCode(160013048,160013049) and c:IsSSetable()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,tc)
+	Duel.ShuffleHand(tp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.setfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g,true)
+	Duel.SSet(tp,g)
+end

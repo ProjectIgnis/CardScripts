3c6384b113ac6c0f2e5882c7ba027c5caa5ae552
YoshiDuels <14310248+Yoshi80@users.noreply.github.com>
2023-04-25 00:10:22 +0200

    Added new Rush cards' scripts

---
+++ rush/c160013038.lua
@@ -0,0 +1,49 @@
+--真紅動の超越撃速竜
+--Red-Boot Enhanced Boost Dragon
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,CARD_REDBOOT_B_DRAGON,160013004)
+	--shuffle to deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DAMAGE+CATEGORY_ATKCHANGE+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_REDBOOT_B_DRAGON,160013004}
+function s.tdfilter(c)
+	return c:IsAbleToDeck() and c:IsMonster()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_GRAVE,LOCATION_GRAVE,1,9,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		local val=#g*200
+		local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+		Duel.Damage(p,val,REASON_EFFECT)
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+		e1:SetValue(val)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
---
+++ rush/c160013049.lua
@@ -0,0 +1,26 @@
+--ドラゴンズ・ブースト・フュージョン
+--Dragon's Boost Fusion
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	Fusion.RegisterSummonEff(c,nil,Fusion.OnFieldMat(aux.FaceupFilter(Card.IsRace,RACE_DRAGON)),nil,nil,nil,s.stage2)
+end
+s.listed_names={CARD_REDBOOT_B_DRAGON}
+function s.cfilter(c)
+	return c:IsLocation(LOCATION_GRAVE) and c:IsCode(CARD_REDBOOT_B_DRAGON)
+end
+function s.stage2(e,tc,tp,mg,chk)
+	if chk==0 then
+		local mg=tc:GetMaterial()
+		local ct=mg:FilterCount(s.cfilter,nil)
+		if ct>0 and Duel.IsPlayerCanDiscardDeck(tp,7) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.DiscardDeck(tp,7,REASON_EFFECT)
+			local g=Duel.GetOperatedGroup()
+			local ct2=g:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)
+			if ct2>0 and Duel.IsPlayerCanDiscardDeck(1-tp,7) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+				Duel.DiscardDeck(1-tp,7,REASON_EFFECT)
+			end
+		end
+	end
+end

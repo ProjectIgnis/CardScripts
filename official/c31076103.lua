0f9bee3a6b3d8aae3c90c6cc82ade0c21fdbe415
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-04-25 20:37:03 +0300

    "The First Sarcophagus" fix
    Rework the script to match OCG functionality. The Special Summoning effect now triggers when one of the pieces is placed on your field and you control all 3.

--- official/c31076103.lua
+++ official/c31076103.lua
@@ -6,88 +6,78 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_END_PHASE)
 	c:RegisterEffect(e1)
-	--
+	--Place "The Second Sarcophagus" or "The Third Sarcophagus" in your Spell/Trap Zone
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.condition)
+	e2:SetCondition(function(e,tp) return Duel.IsTurnPlayer(1-tp) and e:GetHandler():GetFlagEffect(id)<2 end)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
-	--spsummon
+	--Special Summon 1 "Spirit of the Pharaoh" from your hand or Deck
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_MOVE)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
-	e3:SetCountLimit(1)
-	e3:SetCondition(s.condition)
+	e3:SetCondition(s.spcon)
 	e3:SetCost(s.spcost)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
-	--tograve
+	--Send them to the GY if any leave the field
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetRange(LOCATION_SZONE)
 	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetRange(LOCATION_SZONE)
 	e4:SetCondition(s.tgcon)
 	e4:SetOperation(s.tgop)
 	c:RegisterEffect(e4)
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e5:SetCode(EVENT_LEAVE_FIELD)
 	e5:SetOperation(s.tgop)
 	c:RegisterEffect(e5)
 end
-s.listed_names={25343280}
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
-end
-function s.cfilter1(c,code)
-	return c:IsFaceup() and c:IsCode(code)
-end
+s.listed_names={4081094,78697395,id,25343280}
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
-	if not Duel.IsExistingMatchingCard(s.cfilter1,tp,LOCATION_SZONE,0,1,nil,4081094) then
-		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
-		local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,4081094)
-		if #g>0 then
-			Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-		end
-	elseif not Duel.IsExistingMatchingCard(s.cfilter1,tp,LOCATION_SZONE,0,1,nil,78697395) then
-		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
-		local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,78697395)
-		if #g>0 then
-			Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-		end
+	local code=0
+	local c=e:GetHandler()
+	if not c:HasFlagEffect(id) then
+		code=4081094
+	else
+		code=78697395
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_DECK|LOCATION_HAND,0,1,1,nil,code)
+	if #g>0 and Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,0)
 	end
 end
-function s.cfilter2(c,code)
-	return c:IsFaceup() and c:IsCode(code) and c:IsAbleToGraveAsCost()
+function s.confilter(c,tp)
+	return c:IsFaceup() and c:IsCode(4081094,78697395) and c:IsControler(tp) and c:IsOnField()
 end
-function s.cfilter3(c)
-	return c:IsFaceup() and c:IsCode(id,4081094,78697395) and c:IsAbleToGraveAsCost()
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.confilter,1,nil,tp)
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,id),tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,4081094),tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,78697395),tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCode,id,4081094,78697395),tp,LOCATION_ONFIELD,0,nil)
 	if chk==0 then return c:IsAbleToGraveAsCost() and c:IsStatus(STATUS_EFFECT_ENABLED)
-		and Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_SZONE,0,1,nil,4081094)
-		and Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_SZONE,0,1,nil,78697395) end
-	local g=Duel.GetMatchingGroup(s.cfilter3,tp,LOCATION_SZONE,0,nil)
+		and #g==g:FilterCount(Card.IsAbleToGraveAsCost,nil) end
 	Duel.SendtoGrave(g,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
 end
 function s.spfilter(c,e,tp)
 	return c:IsCode(25343280) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
@@ -95,21 +85,15 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)>0 then
 		g:GetFirst():CompleteProcedure()
 	end
 end
-function s.cfilter4(c)
-	return c:IsCode(id,4081094,78697395)
-end
-function s.cfilter5(c)
-	return c:IsFaceup() and c:IsCode(id,4081094,78697395)
-end
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter4,1,nil)
+	return eg:IsExists(aux.FaceupFilter(Card.IsCode,4081094,78697395),1,nil)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.cfilter5,tp,LOCATION_SZONE,0,nil)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCode,4081094,78697395),tp,LOCATION_ONFIELD,0,nil):Merge(e:GetHandler())
 	Duel.SendtoGrave(g,REASON_EFFECT)
 end

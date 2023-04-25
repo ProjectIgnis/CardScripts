de71db85c4b1a63211541e25833784baefd100ac
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-04-24 22:36:49 -0700

    Fix "Contact Soul"
    Should check for "Elemental HERO Neos" on the entire player's field, not just in the MMZ

--- unofficial/c100000551.lua
+++ unofficial/c100000551.lua
@@ -12,13 +12,13 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x1f}
+s.listed_series={SET_NEO_SPACIAN}
 s.listed_names={CARD_NEOS}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_NEOS),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_NEOS),to,LOCATION_ONFIELD,0,1,nil)
 end
 function s.filter(c,e,tp)
-	return c:IsSetCard(0x1f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(SET_NEO_SPACIAN) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end

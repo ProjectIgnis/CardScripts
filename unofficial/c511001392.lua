e8be9b5a1adf4ab2ae50bff1222d7d22aa1f3684
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-04-24 22:32:54 -0700

    Update "Yubel - Terror Incarnate (Anime)"
    Should be an optional summon/general script clean-up

--- unofficial/c511001392.lua
+++ unofficial/c511001392.lua
@@ -1,52 +1,50 @@
---ユベル－Das Abscheulich Ritter
+--ユベル － Das Abscheulich Ritter (Anime)
+--Yubel - Terror Incarnate (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--battle
+	--Special Summon condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e0)
+	--Cannot be destroyed by battle
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
-	e1:SetCondition(s.condition)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
+	--Your opponent takes all battle damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--destroy
+	--Destroy all other monsters, then destroy opponent's monsters
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(4779091,1))
+	e3:SetCategory(CATEGORY_RELEASE+CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.descon)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+	--Special Summon "Yubel - The Ultimate Nightmare" if this card leaves field
 	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_RELEASE+CATEGORY_DESTROY)
-	e4:SetDescription(aux.Stringid(4779091,1))
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1)
-	e4:SetCode(EVENT_PHASE+PHASE_END)
-	e4:SetCondition(s.descon)
-	e4:SetTarget(s.destg)
-	e4:SetOperation(s.desop)
+	e4:SetDescription(aux.Stringid(4779091,2))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
 	c:RegisterEffect(e4)
-	--special summon
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(4779091,2))
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e5:SetCode(EVENT_LEAVE_FIELD)
-	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e5:SetTarget(s.sptg)
-	e5:SetOperation(s.spop)
-	c:RegisterEffect(e5)
-	--cannot special summon
-	local e6=Effect.CreateEffect(c)
-	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e6:SetType(EFFECT_TYPE_SINGLE)
-	e6:SetCode(EFFECT_SPSUMMON_CONDITION)
-	c:RegisterEffect(e6)
 end
-s.listed_names={31764700}
-function s.condition(e)
-	return e:GetHandler():IsAttackPos()
-end
+s.listed_names={78371393,31764700}
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
@@ -69,14 +67,12 @@ function s.filter(c,e,tp)
 	return c:IsCode(31764700) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0x13)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,0x13,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
-	end
+	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if tc then Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP) end
 end

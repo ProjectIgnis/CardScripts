5e6efa89c855ec9196210ea23ccca0ddc337aeb6
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-04-27 04:59:21 -0700

    Fix "Miracle Moment"
    Was not properly checking if the monster leaving was "Panther Warrior" equipped with "Path to Destiny"

--- unofficial/c511000445.lua
+++ unofficial/c511000445.lua
@@ -11,16 +11,23 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--Keep track if a "Panther Warrior" was equipped with "Path to Destiny"
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+		ge1:SetCode(EVENT_LEAVE_FIELD_P)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
 end
 s.listed_names={42035044,511000443,511000444}
 function s.cfilter(c,tp)
-	return c:IsPreviousControler(tp) and c:IsReason(REASON_BATTLE) and c:IsCode(42035044) and c:GetEquipGroup():IsExists(Card.IsCode,1,nil,511000443)
+	return c:IsControler(tp) and c:IsReason(REASON_BATTLE) and c:IsCode(42035044) and c:GetEquipGroup():IsExists(Card.IsCode,1,nil,511000443)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(s.cfilter,nil,tp)
-	return g
+	return Duel.GetFlagEffect(tp,id)>0
 end
-
 function s.spfilter(c,e,tp)
 	return c:IsCode(511000444) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -35,5 +42,10 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if eg:IsExists(s.cfilter,1,nil,tp) then
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_DAMAGE,0,1)
 	end
 end

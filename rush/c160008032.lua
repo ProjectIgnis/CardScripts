823d7c3cb1de6a745a2de79d539306f0c45e4810
Naim <naimsantos45@gmail.com>
2023-04-27 10:40:56 -0300

    Fix "Master of Sevens Road"
    Fixed a bug where the ATK change effect would apply even if it was not face-up

--- rush/c160008032.lua
+++ rush/c160008032.lua
@@ -1,17 +1,19 @@
--- Master of Sevens Road
--- マスター・オブ・セブンスロード
+--マスター・オブ・セブンスロード
+--Master of Sevens Road
 local s,id=GetID()
 function s.initial_effect(c)
-	--fusion material
 	c:EnableReviveLimit()
+	--Fusion Summon Procedure
 	Fusion.AddProcMix(c,true,true,CARD_SEVENS_ROAD_MAGICIAN,160401001)
-	--atk up
+	--Gains ATK per each Spellcaster monster with different name
 	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
-	--pierce
+	--Inflict piercing battle damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_PIERCE)

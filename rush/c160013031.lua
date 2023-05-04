e984a2245b212592b37b49fca3043553a6c1c21c
YoshiDuels <14310248+Yoshi80@users.noreply.github.com>
2023-05-05 01:07:31 +0200

    Added new Rush cards' scripts

---
+++ rush/c160013031.lua
@@ -0,0 +1,53 @@
+--ＥＸプローラー
+--EXTRApoler
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Gain LP
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_FUSION}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,2) end
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsRace(RACE_GALAXY)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	if Duel.DiscardDeck(tp,2,REASON_COST)<0 then return end
+	--atk change
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(300)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+	--gy recover
+	local sg=Duel.GetMatchingGroup(s.sfilter,tp,LOCATION_GRAVE,0,nil)
+	if Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_GRAVE,0,nil)>4 and 
+		#sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.BreakEffect()
+		local tg=aux.SelectUnselectGroup(sg,1,tp,1,ft,s.rescon,1,tp)
+		Duel.SendtoHand(tg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tg)
+	end	
+end
+function s.sfilter(c)
+	return (c:IsCode(CARD_FUSION) or s.sfilter2(c)) and c:IsAbleToHand()
+end
+function s.sfilter2(c)
+	return c:IsMonster() and c:IsRace(RACE_GALAXY) and c:IsType(TYPE_NORMAL)
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:FilterCount(Card.IsCode,nil,CARD_FUSION)<2 and sg:FilterCount(s.sfilter2,nil)<2
+end

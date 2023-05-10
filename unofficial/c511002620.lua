3f163ac88305ffca4102fd5abcdbb049bd3ab70d
Larry126 <28985622+larry126@users.noreply.github.com>
2023-05-11 02:21:49 +0800

    Update c511002620.lua

--- unofficial/c511002620.lua
+++ unofficial/c511002620.lua
@@ -2,8 +2,9 @@
 --Harmonic Synchro Fusion (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--synchro effect
+	--Fusion and Synchro Summon
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)

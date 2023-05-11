329f1461917467e118e56ce98cf61d94b3f7c9d1
YoshiDuels <14310248+Yoshi80@users.noreply.github.com>
2023-05-11 23:08:37 +0200

    Added new Rush cards' scripts

---
+++ rush/c160013046.lua
@@ -0,0 +1,44 @@
+--アドバンス・インパクト
+--Advance Impact
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Use opponent's monster as Tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+end
+function s.filter(c,g)
+	if not c:IsFaceup() or c:IsMaximumModeSide() or c:IsHasEffect(EFFECT_EXTRA_RELEASE) then return false end
+	return g:CheckWithSumEqual(Card.GetLevel,c:GetLevel(),1,99)
+end
+function s.filter2(c)
+	return c:IsMonster() and c:IsAbleToGrave()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_HAND,0,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil,g) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_HAND,0,nil)
+	--Choose a monster
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil,g):GetFirst()
+	Duel.HintSelection(tc,true)
+	local tg=g:SelectWithSumEqual(tp,Card.GetLevel,tc:GetLevel(),1,99)
+	Duel.SendtoGrave(tg,REASON_EFFECT)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EXTRA_RELEASE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	tc:RegisterEffect(e1)
+end

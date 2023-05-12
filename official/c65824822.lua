247ffb7e44eec6906d1ef263512b3bc69611a204
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-05-12 14:55:27 -0700

    Update "Dark Deal"
    Minor script polish to use updated functions.

--- official/c65824822.lua
+++ official/c65824822.lua
@@ -1,14 +1,29 @@
 --闇の取引
+--Dark Deal
 local s,id=GetID()
 function s.initial_effect(c)
-	--change effect
+	--Change effect of Normal Spell to "Your opponent discards 1 random card"
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re:GetHandler()
+	return ep==1-tp and rc:IsNormalSpell() and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+		and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Group.CreateGroup()
+	Duel.ChangeTargetCard(ev,g)
+	Duel.ChangeChainOperation(ev,s.repop)
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():CancelToGrave(false)
@@ -17,18 +32,4 @@ function s.repop(e,tp,eg,ep,ev,re,r,rp)
 		local sg=g:RandomSelect(1-tp,1,nil)
 		Duel.SendtoGrave(sg,REASON_EFFECT+REASON_DISCARD)
 	end
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local rc=re:GetHandler()
-	return ep~=tp and rc:GetType()==TYPE_SPELL and re:IsHasType(EFFECT_TYPE_ACTIVATE)
-		and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)~=0
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,1000) end
-	Duel.PayLPCost(tp,1000)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Group.CreateGroup()
-	Duel.ChangeTargetCard(ev,g)
-	Duel.ChangeChainOperation(ev,s.repop)
 end

0ca82f3e72c287c33abbd04194d73e9de043fab5
Kevdaplog <89976004+Kevdaplog@users.noreply.github.com>
2023-05-18 21:24:43 +0200

    Update c511027294.lua

--- unofficial/c511027294.lua
+++ unofficial/c511027294.lua
@@ -2,9 +2,9 @@
 --Lumenize (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-    --Activate
-    local e1=Effect.CreateEffect(c)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+      -- Activate
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_NEGATE + CATEGORY_ATKCHANGE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_ATTACK_ANNOUNCE)
     e1:SetCondition(s.condition)
@@ -12,33 +12,29 @@ function s.initial_effect(c)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
+
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    return tp~=Duel.GetTurnPlayer()
+    return Duel.GetAttacker():IsControler(1-tp)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    local tg=Duel.GetAttacker()
-    if chkc then return chkc==tg end
-    if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e)
-        and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-    Duel.SetTargetCard(tg)
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk == 0 then return Duel.IsExistingMatchingCard(Card.IsFaceup, tp, LOCATION_MZONE, 0, 1, nil)
+        and Duel.GetAttacker():IsControler(1-tp) end
+    Duel.SetTargetCard(Duel.SelectMatchingCard(tp, Card.IsFaceup, tp, LOCATION_MZONE, 0, 1, 1, nil))
+    Duel.SetOperationInfo(0, CATEGORY_NEGATE, Duel.GetAttacker(), 1, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_ATKCHANGE, Duel.GetAttacker(), 1, 0, 0)
 end
-function s.filter(c)
-    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT)
-end
+
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    Duel.NegateAttack()
-    local tc=Duel.GetFirstTarget()
-    if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-        local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-        local ac=g:GetFirst()
-        if ac then
-            local e1=Effect.CreateEffect(e:GetHandler())
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetCode(EFFECT_UPDATE_ATTACK)
-            e1:SetValue(tc:GetAttack())
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-            ac:RegisterEffect(e1)
-        end
+    local tc = Duel.GetFirstTarget()
+    local atk = Duel.GetAttacker():GetAttack()
+    if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        Duel.NegateAttack()
+        local e1 = Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        e1:SetValue(atk)
+        tc:RegisterEffect(e1)
     end
 end

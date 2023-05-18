41294fab4d18009fe80d46fc034b60658442f145
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-05-18 06:25:39 -0700

    Fix "White Arbitration"
    Missing cost function in initial effect/needs to only discard "White" monsters.

--- unofficial/c511009634.lua
+++ unofficial/c511009634.lua
@@ -1,3 +1,4 @@
+--白の仲裁
 --White Arbitration
 Duel.LoadScript("c420.lua")
 local s,id=GetID()
@@ -8,12 +9,13 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 function s.cfilter(c)
-	return c:IsWhite() and c:IsDiscardable()
+	return c:IsWhite() and c:IsMonster() and c:IsDiscardable()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end

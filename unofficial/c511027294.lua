8fec6a8f8cd2d20ad782e70d0178312b690e6666
Kevdaplog <89976004+Kevdaplog@users.noreply.github.com>
2023-05-19 06:29:44 +0200

    Update c511027294.lua

--- unofficial/c511027294.lua
+++ unofficial/c511027294.lua
@@ -2,7 +2,7 @@
 --Lumenize (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-      -- Activate
+    -- Activate
     local e1 = Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_NEGATE + CATEGORY_ATKCHANGE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -16,9 +16,11 @@ end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
     return Duel.GetAttacker():IsControler(1-tp)
 end
-
+function s.filter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT)
+end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk == 0 then return Duel.IsExistingMatchingCard(Card.IsFaceup, tp, LOCATION_MZONE, 0, 1, nil)
+    if chk == 0 then return Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_MZONE, 0, 1, nil)
         and Duel.GetAttacker():IsControler(1-tp) end
     Duel.SetTargetCard(Duel.SelectMatchingCard(tp, Card.IsFaceup, tp, LOCATION_MZONE, 0, 1, 1, nil))
     Duel.SetOperationInfo(0, CATEGORY_NEGATE, Duel.GetAttacker(), 1, 0, 0)

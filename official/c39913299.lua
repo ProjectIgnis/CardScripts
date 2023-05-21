c5a4cb7f2466f70c4874e2f6e6fa36c02b9bad86
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-05-21 19:38:25 +0300

    "The True Name" fix
    Don't shuffle the Deck if the player doesn't add to the hand or Special Summon 1 DIVINE monster from the Deck. Also now properly handles the case where the excavated card cannot be added to the hand during resolution + general polishing.

--- official/c39913299.lua
+++ official/c39913299.lua
@@ -4,7 +4,8 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DECKDES)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
@@ -12,49 +13,46 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,e,tp,ft)
-	return c:IsAttribute(ATTRIBUTE_DIVINE) and (c:IsAbleToHand()
-		or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1)
 		and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_DECK,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
 	s.announce_filter={TYPE_EXTRA,OPCODE_ISTYPE,OPCODE_NOT}
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
 	local ac=Duel.AnnounceCard(tp,table.unpack(s.announce_filter))
 	Duel.SetTargetParam(ac)
 	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
+function s.thspfilter(c,e,tp,ft)
+	return c:IsAttribute(ATTRIBUTE_DIVINE) and (c:IsAbleToHand() or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ConfirmDecktop(tp,1)
-	local g=Duel.GetDecktopGroup(tp,1)
-	local tc=g:GetFirst()
+	local tc=Duel.GetDecktopGroup(tp,1):GetFirst()
 	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-	if tc:IsCode(ac) and tc:IsAbleToHand() then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil,e,tp,ft)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
-			local sc=g:Select(tp,1,1,nil):GetFirst()
-			local b1=sc:IsAbleToHand()
-			local b2=sc:IsCanBeSpecialSummoned(e,0,tp,false,false) and ft>0
-			local op=Duel.SelectEffect(tp,
-					{b1,aux.Stringid(id,1)},
-					{b2,aux.Stringid(id,2)})
-			if op==1 then
-				Duel.SendtoHand(sc,nil,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,sc)
-			else
-				Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
-			end
-		else
-			Duel.DisableShuffleCheck()
-		end
+	if tc:IsCode(ac) then
+		Duel.DisableShuffleCheck()
+		if not tc:IsAbleToHand() then return Duel.SendtoGrave(tc,REASON_RULE) end
+		if not (Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND)) then return end
 		Duel.ShuffleHand(tp)
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		local g=Duel.GetMatchingGroup(s.thspfilter,tp,LOCATION_DECK,0,nil,e,tp,ft)
+		if #g==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,1)) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+		local sc=g:Select(tp,1,1,nil):GetFirst()
+		if not sc then return end
+		Duel.BreakEffect()
+		aux.ToHandOrElse(sc,tp,
+			function(sc)
+				return ft>0 and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			end,
+			function(sc)
+				return Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+			end,
+			aux.Stringid(id,3)
+		)
+		Duel.ShuffleDeck(tp)
 	else
 		Duel.DisableShuffleCheck()
 		Duel.SendtoGrave(tc,REASON_EFFECT|REASON_EXCAVATE)

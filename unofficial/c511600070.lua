a74e5df7a267f992c9fc847841b80f2d847a49ec
Larry126 <28985622+larry126@users.noreply.github.com>
2023-05-23 23:23:09 +0800

    Overhaul "Number S0: Utopic ZEXAL (Manga)"
    Fix the Xyz Summon prevention counter, and overhaul the ATK/DEF calculation

--- unofficial/c511600070.lua
+++ unofficial/c511600070.lua
@@ -1,97 +1,84 @@
 --ＳＮｏ．０ ホープ・ゼアル (Manga)
 --Number S0: Utopic ZEXAL (Manga)
---scripted by Larry126
-Duel.LoadScript("c420.lua")
+--Scripted by Larry126
 Duel.LoadCardScript("c52653092.lua")
-local s,id,alias=GetID()
-local zexal={}
+local s,id=GetID()
 function s.initial_effect(c)
-	if not c:IsStatus(STATUS_COPYING_EFFECT) then
-		zexal[c]=true
-	end
-	--xyz summon
+	--Xyz Summon
 	c:EnableReviveLimit()
 	Xyz.AddProcedure(c,s.xyzfilter,nil,3)
-	--battle indestructable
+	--This card's Special Summon cannot be prevented or negated
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e1:SetValue(s.indes)
+	e1:SetCode(id)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	c:RegisterEffect(e1)
-	--cannot disable spsummon
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
+	local e2=e1:Clone()
 	e2:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetCondition(s.effcon)
 	c:RegisterEffect(e2)
-	--summon success
+	--When Special Summoned, your opponent's cards and effects cannot be activated
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCondition(s.effcon)
-	e3:SetOperation(s.spsumsuc)
+	e3:SetOperation(function() Duel.SetChainLimitTillChainEnd(function(e,ep,tp) return tp==ep end) end)
 	c:RegisterEffect(e3)
-	--atk & def
+	--ATK/DEF
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_SET_ATTACK)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetValue(s.atkval)
+	e4:SetCondition(function(e) return e:GetHandler():HasFlagEffect(id+1) end)
+	e4:SetValue(function(e,c) return c:GetFlagEffectLabel(id+1)*500 end)
 	c:RegisterEffect(e4)
 	local e5=e4:Clone()
 	e5:SetCode(EFFECT_SET_DEFENSE)
 	c:RegisterEffect(e5)
+	--Cannot be destroyed by battle except with "Number" monsters
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e6:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+	c:RegisterEffect(e6)
 	aux.GlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_ADJUST)
-		ge1:SetCondition(s.con)
-		ge1:SetOperation(s.op)
-		Duel.RegisterEffect(ge1,0)
+		local oldf=Duel.Overlay
+		Duel.Overlay=function(c,g,grave)
+			local mg=g:Clone()
+			if not grave then g:ForEach(function(gc) mg:Merge(gc:GetOverlayGroup()) end) end
+			local rank=mg:GetSum(Card.GetRank)
+			local label=c:GetFlagEffectLabel(id+1)
+			if not label then
+				c:RegisterFlagEffect(id+1,RESET_EVENT|RESETS_STANDARD&~RESET_TOFIELD,0,1,rank)
+			else
+				c:SetFlagEffectLabel(id+1,label+rank)
+			end
+			return oldf(c,g,grave)
+		end
+		local oldcf=Card.RegisterEffect
+		Card.RegisterEffect=function(c,e,...)
+			if e:GetCode()==EFFECT_CANNOT_SPECIAL_SUMMON then
+				local oldTg=e:GetTarget()
+				e:SetTarget(s.splimit(oldTg))
+			end
+			return oldcf(c,e,...)
+		end
+		local oldpf=Duel.RegisterEffect
+		Duel.RegisterEffect=function(e,p)
+			if e:GetCode()==EFFECT_CANNOT_SPECIAL_SUMMON then
+				local oldTg=e:GetTarget()
+				e:SetTarget(s.splimit(oldTg))
+			end
+			return oldpf(e,p)
+		end
 	end)
 end
-s.listed_series={0x48}
 s.xyz_number=0
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	for i=0,1 do
-		return Duel.IsPlayerAffectedByEffect(i,EFFECT_CANNOT_SPECIAL_SUMMON)
-	end
-end
+s.listed_series={SET_NUMBER,SET_NUMBER_S}
 function s.splimit(target)
-	return function (...)
-		return (not target or target(...)) and not zexal[c]
-	end
-end
-local effmap={}
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	for i=0,1 do
-		local effs={Duel.GetPlayerEffect(i,EFFECT_CANNOT_SPECIAL_SUMMON)}
-		for _,eff in ipairs(effs) do
-			local target=eff:GetTarget()
-			if target==nil or not effmap[target] then
-				eff:SetTarget(s.splimit(eff:GetTarget()))
-				effmap[eff:GetTarget()]=true
-			end
-		end
+	return function (e,c,...)
+		return not c:IsHasEffect(id) and (not target or target(e,c,...))
 	end
 end
 function s.xyzfilter(c,xyz,sumtype,tp)
 	return c:IsType(TYPE_XYZ,xyz,sumtype,tp) and c:IsSetCard(SET_NUMBER_S,xyz,sumtype,tp)
-end
-function s.effcon(e)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
-end
-function s.spsumsuc(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(s.chlimit)
-end
-function s.chlimit(e,ep,tp)
-	return tp==ep
-end
-function s.atkval(e,c)
-	return c:GetOverlayGroup():GetSum(Card.GetRank)*500
-end
-function s.indes(e,c)
-	return not c:IsSetCard(0x48)
 end

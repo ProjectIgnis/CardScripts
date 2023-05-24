4279410184a2bb60aef51afdd897ea786acdc799
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-05-24 22:43:48 +0200

    added new rush cards

---
+++ rush/c160208036.lua
@@ -0,0 +1,41 @@
+--三千破界
+--Universal Destruction
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,3000) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetAttacker()
+	if chk==0 then return tg:IsOnField() end
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--requirement
+	Duel.PayLPCost(tp,3000)
+	--Effect
+	local tc=Duel.GetAttacker()
+	if tc and tc:CanAttack() and tc:IsRelateToBattle() and not tc:IsStatus(STATUS_ATTACK_CANCELED) then
+		local tg=Group.CreateGroup()
+		tg:AddCard(tc)
+		tg=tg:AddMaximumCheck()
+		Duel.Destroy(tg,REASON_EFFECT)
+	end
+end

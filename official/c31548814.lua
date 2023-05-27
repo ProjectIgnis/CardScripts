fc2eebc684ff052b2d64792eeaa6266c76b4cb35
Naim <naimsantos45@gmail.com>
2023-05-27 07:58:50 -0300

    Update "Void Feast"
    -added a string to be used when the play selects the monsters to special summon
    -removed an unused function

--- official/c31548814.lua
+++ official/c31548814.lua
@@ -1,8 +1,10 @@
 --煉獄の狂宴
+--Void Feast
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Special Summon up to 3 "Infernoid" monsters from the Deck
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -11,24 +13,20 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0xc5,0xbb}
-function s.costfilter(c,ft)
-	return c:IsSetCard(0xc5) and c:IsSpellTrap() and (c:IsFaceup() or c:IsLocation(LOCATION_HAND)) and c:IsAbleToGraveAsCost() 
-		and (ft>0 or (c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5))
-end
-function s.rescon(sg,e,tp,mg)
-	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:CheckWithSumEqual(Card.GetLevel,8,1,ft)
+s.listed_series={SET_VOID,SET_INFERNOID}
+function s.costfilter(c,tp)
+	return c:IsSetCard(SET_VOID) and c:IsSpellTrap() and c:IsAbleToGraveAsCost() 
+		and (c:IsFaceup() or c:IsLocation(LOCATION_HAND)) and Duel.GetMZoneCount(tp,c)>0
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return ft>-1 and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,e:GetHandler(),ft) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND|LOCATION_ONFIELD,0,1,e:GetHandler(),tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,e:GetHandler(),ft)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND|LOCATION_ONFIELD,0,1,1,e:GetHandler(),tp)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0xbb) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+	return c:IsSetCard(SET_INFERNOID) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
@@ -46,6 +44,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
 	if g:CheckWithSumEqual(Card.GetLevel,8,1,ft) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=g:SelectWithSumEqual(tp,Card.GetLevel,8,1,ft)
 		if #sg>0 then
 			Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)

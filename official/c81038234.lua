fe6a8a2f09b8e7fbd21e4602463cc1f8b03bd1aa
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-05-29 18:56:39 +0300

    Updated some scripts that were misusing functions as conditions
    Functions like "Duel.IsMainPhase" and "Duel.IsBattlePhase" aren't meant to be set as an effect's condition directly.

--- official/c81038234.lua
+++ official/c81038234.lua
@@ -1,86 +1,71 @@
 --夢魔鏡の夢語らい
 --Dream Mirror Recap
 --Logical Nonsense
---Substitute ID
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetHintTiming(0,TIMING_MAIN_END)
 	c:RegisterEffect(e1)
-	--Your "Dream Mirror" monsters are shuffled into the deck instead of going to GY
+	--Your "Dream Mirror" monsters are shuffled into the Deck instead of being sent to the GY
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(s.reptg)
+	e2:SetTarget(function(e,c) return c:IsSetCard(SET_DREAM_MIRROR) and c:IsReason(REASON_COST) and c:GetReasonEffect():GetHandler()==c end)
 	e2:SetValue(LOCATION_DECKSHF)
 	c:RegisterEffect(e2)
-	--Place 1 "Dream Mirror of Joy" or "Dream Mirror of Terror" from banished/GY into field zone
+	--Place 1 "Dream Mirror of Joy" or "Dream Mirror of Terror" face-up in your Field Zone
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetRange(LOCATION_SZONE)
 	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetCountLimit(1,id)
-	e3:SetCondition(Duel.IsMainPhase)
-	e3:SetCost(s.cost)
-	e3:SetTarget(s.target)
-	e3:SetOperation(s.operation)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetHintTiming(0,TIMING_MAIN_END)
+	e3:SetCondition(function() return Duel.IsMainPhase end)
+	e3:SetCost(s.plcost)
+	e3:SetTarget(s.pltg)
+	e3:SetOperation(s.plop)
 	c:RegisterEffect(e3)
 end
-	--Lists "Dream Mirror" archetype
 s.listed_series={SET_DREAM_MIRROR}
-	--Specificially lists "Dream Mirror of Joy" and "Dream Mirror of Terror"
 s.listed_names={CARD_DREAM_MIRROR_JOY,CARD_DREAM_MIRROR_TERROR}
-
-	--Check if a "Dream Mirror" is tributing itself as cost
-function s.reptg(e,c)
-	return c:IsSetCard(SET_DREAM_MIRROR) and c:IsReason(REASON_COST) and c:GetReasonEffect():GetHandler()==c
-end
-	--Send this face-up card to GY as cost
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.plcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsAbleToGraveAsCost() and c:IsStatus(STATUS_EFFECT_ENABLED) end
 	Duel.SendtoGrave(c,REASON_COST)
 end
-	--Check for "Dream Mirror of Joy"/"Dream Mirror of Terror"
-function s.filter(c,tp)
+function s.plfilter(c)
 	return c:IsFieldSpell() and c:IsCode(CARD_DREAM_MIRROR_JOY,CARD_DREAM_MIRROR_TERROR) and c:IsFaceup() and not c:IsForbidden()
 end
-	--Check for 1 monster that specifically lists "Dream Mirror of Joy"/"Dream Mirror of Terror"
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,nil) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
 function s.spfilter(c,e,tp,code)
-	return c:ListsCode(code) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:ListsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-	--Activation legality
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,nil,tp) end
-end
-	--Place 1 "Dream Mirror of Joy" or "Dream Mirror of Terror" from banished/GY into field zone
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local ag=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,1,nil,tp)
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	if #ag>0 then
-		local ac=ag:GetFirst()
-		local fc=Duel.GetFieldCard(tp,LOCATION_FZONE,0)
-		if fc then
-			Duel.SendtoGrave(fc,REASON_RULE)
-			Duel.BreakEffect()
-		end
-		Duel.MoveToField(ac,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
-		--Special summon 1 monster that specifically lists the placed card
-		local code=ac:GetCode()
-		local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp,code)
-		if #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local sg=g:Select(tp,1,1,nil):GetFirst()
-			Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
-		end
+	local plc=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,1,nil):GetFirst()
+	if not plc then return end
+	local fc=Duel.GetFieldCard(1-tp,LOCATION_FZONE,0)
+	if fc then
+		Duel.SendtoGrave(fc,REASON_RULE)
+		Duel.BreakEffect()
+	end
+	if not Duel.MoveToField(plc,tp,tp,LOCATION_FZONE,POS_FACEUP,true) or Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	local code=plc:GetCode()
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp,code)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:Select(tp,1,1,nil):GetFirst()
+		Duel.BreakEffect()
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
--- unofficial/c511024002.lua
+++ unofficial/c511024002.lua
@@ -1,4 +1,4 @@
---EMヒックリカエル (Anime)
+--ＥＭヒックリカエル (Anime)
 --Performapal Turn Toad (Anime)
 --Scripted by IanxWaifu
 local s,id=GetID()
@@ -10,36 +10,37 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_PZONE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_PZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(s.adtg1)
-	e1:SetOperation(s.adop1)
+	e1:SetTarget(s.adtg)
+	e1:SetOperation(s.adop)
 	c:RegisterEffect(e1)
 	--Change position
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_POSITION+CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(Duel.IsBattlePhase)
-	e2:SetTarget(s.adtg2)
-	e2:SetOperation(s.adop2)
+	e2:SetCondition(function() return Duel.IsBattlePhase end)
+	e2:SetTarget(s.postg)
+	e2:SetOperation(s.posop)
 	c:RegisterEffect(e2)
 end
-function s.adtg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.adtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:IsType(TYPE_PENDULUM) end
 	if chk==0 then return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsType,TYPE_PENDULUM),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsType,TYPE_PENDULUM),tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
-function s.adop1(e,tp,eg,ep,ev,re,r,rp)
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local atk=tc:GetAttack()
 		local def=tc:GetDefense()
+		--Switch its current ATK/DEF
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SWAP_ATTACK_FINAL)
@@ -52,14 +53,17 @@ function s.adop1(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e2)
 	end
 end
-function s.adtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsPosition(POS_FACEUP_DEFENSE) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsPosition,tp,LOCATION_MZONE,0,1,nil,POS_FACEUP_DEFENSE) end
+function s.posfilter(c)
+	return c:IsCanChangePosition() and c:IsPosition(POS_FACEUP_DEFENSE)
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.posfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.posfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DEFENSE)
-	local g=Duel.SelectTarget(tp,Card.IsPosition,tp,LOCATION_MZONE,0,1,1,nil,POS_FACEUP_DEFENSE)
+	local g=Duel.SelectTarget(tp,s.posfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
-function s.adop2(e,tp,eg,ep,ev,re,r,rp)
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.ChangePosition(tc,POS_FACEUP_ATTACK)

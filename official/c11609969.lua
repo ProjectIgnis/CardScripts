d23698989edf095203de6fa1269b59c13b97af1b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-05-31 02:30:36 +0300

    D/D Savant Kepler general polish

--- official/c11609969.lua
+++ official/c11609969.lua
@@ -1,99 +1,92 @@
---DD魔導賢者ケプラー
+--ＤＤ魔導賢者ケプラー
 --D/D Savant Kepler
 local s,id=GetID()
 function s.initial_effect(c)
 	--Pendulum Summon
 	Pendulum.AddProcedure(c)
 	--Cannot Pendulum Summon monsters, except "D/D" monsters
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,_c,tp,sumtp,sumpos) return not _c:IsSetCard(SET_DD) and (sumtp&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM end)
+	c:RegisterEffect(e1)
+	--Reduce this card's Pendulum Scale by 2
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e2:SetRange(LOCATION_PZONE)
-	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-	e2:SetTargetRange(1,0)
-	e2:SetTarget(s.splimit)
+	e2:SetCountLimit(1)
+	e2:SetCondition(function(_,tp) return Duel.IsTurnPlayer(tp) end)
+	e2:SetTarget(s.sctg)
+	e2:SetOperation(s.scop)
 	c:RegisterEffect(e2)
-	--Reduce this card's Pendulum Scale
+	--Activate 1 of these effects
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetRange(LOCATION_PZONE)
-	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e3:SetCountLimit(1)
-	e3:SetCondition(s.sccon)
-	e3:SetTarget(s.sctg)
-	e3:SetOperation(s.scop)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetCountLimit(1,id)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
-	--Add or return a card to the hand
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_SUMMON_SUCCESS)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e4:SetCountLimit(1,id)
-	e4:SetTarget(s.thtg)
-	e4:SetOperation(s.thop)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e4)
-	local e5=e4:Clone()
-	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e5)
 end
 s.listed_series={SET_DD,SET_DARK_CONTRACT}
-function s.splimit(e,c,tp,sumtp,sumpos)
-	return not c:IsSetCard(SET_DD) and (sumtp&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
-end
-function s.sccon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
-end
-function s.filter(c,lv)
-	return c:IsFaceup() and not c:IsSetCard(SET_DD) and c:IsLevelAbove(lv)
+function s.desfilter(c,lvl)
+	return c:IsFaceup() and not c:IsSetCard(SET_DD) and c:IsLevelAbove(lvl)
 end
 function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local scl=math.max(1,e:GetHandler():GetLeftScale()-2)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,scl)
-	if e:GetHandler():GetLeftScale()>1 then
+	local scale=e:GetHandler():GetLeftScale()
+	local lvl=math.max(1,scale-2)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,0,nil,lvl)
+	if scale>1 then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 	end
 end
 function s.scop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:GetLeftScale()==1 then return end
-	if c:UpdateScale(-2)~=0 then
-		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,c:GetLeftScale())
-		if #g>0 then
-			Duel.BreakEffect()
-			Duel.Destroy(g,REASON_EFFECT)
-		end
+	if c:GetLeftScale()<=1 then return end
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,0,nil,c:GetLeftScale())
+	if c:UpdateScale(-2)~=0 and #g>0 then
+		Duel.BreakEffect()
+		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
-function s.filter1(c)
+function s.rthfilter(c)
 	return c:IsFaceup() and c:IsSetCard(SET_DD) and c:IsAbleToHand()
 end
-function s.filter2(c)
+function s.athfilter(c)
 	return c:IsSetCard(SET_DARK_CONTRACT) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and s.filter1(chkc) and chkc~=e:GetHandler() end
-	local b1=Duel.IsExistingTarget(s.filter1,tp,LOCATION_ONFIELD,0,1,e:GetHandler())
-	local b2=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsOnField() and s.rthfilter(chkc) and chkc~=c end
+	local b1=Duel.IsExistingTarget(s.rthfilter,tp,LOCATION_ONFIELD,0,1,c)
+	local b2=Duel.IsExistingMatchingCard(s.athfilter,tp,LOCATION_DECK,0,1,nil)
 	if chk==0 then return b1 or b2 end
 	local op=Duel.SelectEffect(tp,
 		{b1,aux.Stringid(id,2)},
 		{b2,aux.Stringid(id,3)})
-
 	e:SetLabel(op)
 	if op==1 then
+		e:SetCategory(CATEGORY_TOHAND)
 		e:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-		local g=Duel.SelectTarget(tp,s.filter1,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+		local g=Duel.SelectTarget(tp,s.rthfilter,tp,LOCATION_ONFIELD,0,1,1,c)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-		e:SetCategory(CATEGORY_TOHAND)
 	else
+		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 		e:SetProperty(EFFECT_FLAG_DELAY)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	end
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
@@ -105,7 +98,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	else
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,s.athfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if #g>0 then
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,g)

a31e3c5f7f43259301a0b746e733dedf5eabd4e7
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-05-31 17:26:15 +0300

    Dark Scorpion - Chick the Yellow polish

--- official/c61587183.lua
+++ official/c61587183.lua
@@ -2,20 +2,17 @@
 --Dark Scorpion - Chick the Yellow
 local s,id=GetID()
 function s.initial_effect(c)
-	--Return to hand or move top card of Deck
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_DAMAGE)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return ep==1-tp end)
+	e1:SetTarget(s.efftg)
+	e1:SetOperation(s.effop)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() end
 	local b1=Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
 	local b2=Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>0
@@ -24,23 +21,25 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		{b1,aux.Stringid(id,1)},
 		{b2,aux.Stringid(id,2)})
 	if op==1 then
+		e:SetCategory(CATEGORY_TOHAND)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 		local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
-		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		e:SetCategory(CATEGORY_TOHAND)
 	else
-		e:SetProperty(0)
 		e:SetCategory(0)
+		e:SetProperty(0)
 	end
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
+		--Return 1 card on the field to the hand
 		local tc=Duel.GetFirstTarget()
 		if tc:IsRelateToEffect(e) then
 			Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		end
 	else
+		--Look at the top card of the opponent's Deck
 		local g=Duel.GetDecktopGroup(1-tp,1)
 		if #g>0 then
 			Duel.ConfirmCards(tp,g)

4a9f476c2334350be60f410abc244eb2fa3892c4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-05-31 17:12:50 +0300

    Yamorimori polish

--- official/c51474037.lua
+++ official/c51474037.lua
@@ -3,70 +3,70 @@
 --scripted by Rundas
 local s,id=GetID()
 function s.initial_effect(c)
-	--Banish from GY + choice effect
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCountLimit(1,id)
 	e1:SetCost(aux.bfgcost)
-	e1:SetTarget(s.btg)
-	e1:SetOperation(s.bop)
+	e1:SetTarget(s.efftg)
+	e1:SetOperation(s.effop)
 	c:RegisterEffect(e1)
 end
-s.listed_names={id}
---Banish from GY + choice effect
-function s.mfilter(c,e,tp)
-	return c:IsCanBeEffectTarget(e) and c:IsMonster() and (c:IsControler(1-tp) or c:IsRace(RACE_REPTILE))
-end
-function s.mfilter2(c,tp)
-	return c:IsControler(tp) and c:IsDestructable()
-end
-function s.mfilter3(c,tp)
-	return c:IsControler(1-tp) and c:IsAttackAbove(1)
+function s.tgfilter(c,e,tp)
+	return c:IsCanBeEffectTarget(e) and c:IsFaceup() and (c:IsControler(1-tp) or c:IsRace(RACE_REPTILE))
 end
 function s.rescon(sg,e,tp,mg)
-	return sg:Filter(Card.IsCanChangePosition,nil):GetClassCount(Card.GetControler)==2
-		or (sg:IsExists(s.mfilter2,1,nil,tp) and sg:IsExists(s.mfilter3,1,nil,tp))
+	if sg:GetClassCount(Card.GetControler)~=2 then return false end
+	local opp_c=sg:Filter(Card.IsControler,nil,1-tp):GetFirst()
+	return sg:FilterCount(Card.IsCanTurnSet,nil)==2 or opp_c:GetAttack()>0
 end
-function s.btg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	local g=Duel.GetMatchingGroup(aux.FaceupFilter(s.mfilter,e,tp),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(s.tgfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e,tp)
 	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
-	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_FACEUP)
-	Duel.SetTargetCard(sg)
-	local check1=sg:IsExists(Card.IsCanChangePosition,2,nil)
-	local check2=sg:IsExists(s.mfilter2,1,nil,tp) and sg:IsExists(s.mfilter3,1,nil,tp)
-	local choice=Duel.SelectEffect(tp,
-		{check1,aux.Stringid(id,1)},
-		{check2,aux.Stringid(id,2)})
-	if choice==1 then
-		Duel.SetOperationInfo(0,CATEGORY_POSITION,sg,2,tp,LOCATION_MZONE)
+	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_FACEUP)
+	Duel.SetTargetCard(tg)
+	local self_g,opp_g=g:Split(Card.IsControler,nil,tp)
+	local b1=sg:IsExists(Card.IsCanChangePosition,2,nil)
+	local b2=opp_g:GetFirst():GetAttack()>0
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	if op==1 then
 		e:SetCategory(CATEGORY_POSITION)
-	elseif choice==2 then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg:Filter(Card.IsControler,nil,tp),1,tp,LOCATION_MZONE)
-		Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,sg:Filter(Card.IsControler,nil,1-tp),1,tp,LOCATION_MZONE)
+		e:SetLabelObject(nil)
+		Duel.SetOperationInfo(0,CATEGORY_POSITION,tg,2,tp,0)
+	elseif op==2 then
 		e:SetCategory(CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
+		e:SetLabelObject(self_g)
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,self_g,1,tp,0)
+		Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,opp_g,1,tp,0)
 	end
-	e:SetLabel(choice)
+	e:SetLabel(op)
 end
-function s.bop(e,tp,eg,ep,ev,re,r,rp)
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
-	if #g~=2 then return end
-	local choice=e:GetLabel()
-	if choice==1 then
+	if #g==0 then return end
+	local op=e:GetLabel()
+	if op==1 then
+		--Change those monsters to face-down Defense Position
 		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
-	elseif choice==2 then
-		local g1,g2=g:Split(Card.IsControler,nil,tp)
-		if Duel.Destroy(g1,REASON_EFFECT)>0 then
+	elseif op==2 then
+		--Destroy that monster you control and change the ATK of the other monster to 0
+		local self_c=e:GetLabelObject():GetFirst()
+		local opp_c=g:RemoveCard(self_c):GetFirst()
+		if self_c:IsRelateToEffect(e) and Duel.Destroy(self_c,REASON_EFFECT)>0
+			and opp_c and opp_c:IsRelateToEffect(e) and opp_c:IsFaceup() then
+			--Change its ATK to 0
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
 			e1:SetValue(0)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			g2:GetFirst():RegisterEffect(e1)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			opp_c:RegisterEffect(e1)
 		end
 	end
 end

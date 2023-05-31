73ea4baa07157ab3c8a99dc77a81aa5c3b8b3dc1
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-05-31 12:52:55 +0300

    Dark Scorpion - Gorg the Strong polish

--- official/c48768179.lua
+++ official/c48768179.lua
@@ -2,21 +2,18 @@
 --Dark Scorpion - Gorg the Strong
 local s,id=GetID()
 function s.initial_effect(c)
-	--Return to top of Deck or send from Deck to GY
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_DAMAGE)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return ep==1-tp end)
+	e1:SetTarget(s.efftg)
+	e1:SetOperation(s.effop)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and chkc:IsAbleToDeck() end
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToDeck() end
 	local b1=Duel.IsExistingTarget(Card.IsAbleToDeck,tp,0,LOCATION_MZONE,1,nil)
 	local b2=Duel.IsPlayerCanDiscardDeck(1-tp,1)
 	if chk==0 then return b1 or b2 end
@@ -25,24 +22,25 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		{b2,aux.Stringid(id,2)})
 	e:SetLabel(op)
 	if op==1 then
+		e:SetCategory(CATEGORY_TODECK)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 		local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,0,LOCATION_MZONE,1,1,nil)
-		Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
-		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		e:SetCategory(CATEGORY_TODECK)
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,tp,0)
 	else
-		Duel.SetOperationInfo(0,CATEGORY_DECKDES,0,0,1-tp,1)
-		e:SetProperty(0)
 		e:SetCategory(CATEGORY_DECKDES)
+		e:SetProperty(0)
+		Duel.SetOperationInfo(0,CATEGORY_DECKDES,0,0,1-tp,1)
 	end
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
+		--Return 1 monster your opponent controls to the top of the Deck
 		local tc=Duel.GetFirstTarget()
-		if tc:IsRelateToEffect(e) then
-			Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)
-		end
+		if not (tc:IsRelateToEffect(e) and tc:IsControler(1-tp)) then return end
+		Duel.SendtoDeck(tc,nil,SEQ_DECKTOP,REASON_EFFECT)
 	else
+		--Send the top card of your opponent's Deck to the GY
 		Duel.DiscardDeck(1-tp,1,REASON_EFFECT)
 	end
 end

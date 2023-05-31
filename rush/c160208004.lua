9b21b266e99597c8269ebcfd563f6c4ae55d94ff
YoshiDuels <14310248+Yoshi80@users.noreply.github.com>
2023-05-31 21:43:46 +0200

    Added new Rush cards' scripts

---
+++ rush/c160208004.lua
@@ -0,0 +1,24 @@
+--ハーピィ・レディ１
+--Harpie Lady 1 (Rush)
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--name change
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_GRAVE+LOCATION_MZONE)
+	e1:SetValue(CARD_HARPIE_LADY)
+	c:RegisterEffect(e1)
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WIND))
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetValue(300)
+	c:RegisterEffect(e2)
+end
+s.listed_names={CARD_HARPIE_LADY}

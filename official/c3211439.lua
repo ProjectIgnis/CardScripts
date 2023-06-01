c4b2d13137dde4d1c581083f2b613271f0481220
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-06-01 09:49:58 +0300

    "Forbidden Apocrypha" fix
    Should also check that there are at least 2 monsters of the declared type on resolution before sending monsters to the GY.

--- official/c3211439.lua
+++ official/c3211439.lua
@@ -12,25 +12,23 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,tpe)
-	return c:IsFaceup() and c:IsType(tpe)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,2,nil,TYPE_FUSION)
-	local b2=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,2,nil,TYPE_SYNCHRO)
-	local b3=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,2,nil,TYPE_XYZ)
+	local b1=Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_FUSION),tp,LOCATION_MZONE,LOCATION_MZONE,2,nil)
+	local b2=Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_SYNCHRO),tp,LOCATION_MZONE,LOCATION_MZONE,2,nil)
+	local b3=Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_XYZ),tp,LOCATION_MZONE,LOCATION_MZONE,2,nil)
 	if chk==0 then return b1 or b2 or b3 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CARDTYPE)
 	local op=Duel.SelectEffect(tp,
-		{b1,aux.Stringid(id,0)},
-		{b2,aux.Stringid(id,1)},
-		{b3,aux.Stringid(id,2)})
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)},
+		{b3,aux.Stringid(id,3)})
 	local opval={TYPE_FUSION,TYPE_SYNCHRO,TYPE_XYZ}
 	e:SetLabel(opval[op])
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g1=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e:GetLabel())
-	local g2=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil,e:GetLabel())
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsType,e:GetLabel()),tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if #g<2 then return end
+	local g1,g2=g:Split(Card.IsControler,nil,tp)
 	if #g1>0 then
 		Duel.SendtoGrave(g1,REASON_RULE,PLAYER_NONE,tp)
 	end

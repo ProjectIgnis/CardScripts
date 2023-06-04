816953f1ede3df3e5555c5ceb7341acaa6fd2dd4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-06-04 08:45:41 -0700

    Update c511003235.lua
    Added proper range for destroy protection

--- unofficial/c511003235.lua
+++ unofficial/c511003235.lua
@@ -1,5 +1,6 @@
 --ビークロイド・コネクション・ゾーン (Anime)
 --Vehicroid Connection Zone (Anime)
+--Scripted by The Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
 	--Fusion Summon 1 "Vehicroid" Fusion monster 
@@ -11,6 +12,8 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(function(e) return e:GetHandler():IsFaceup() end)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
 end

44a635fa619a4c197022f5bd4e293d18664c4bbe
Naim <naimsantos45@gmail.com>
2023-06-10 05:00:13 -0300

    "Piri Reis Map" fix
    Fixed a bug where the player would be able to activate a pendulum monster in the pendulum zone when they should not.

--- official/c33907039.lua
+++ official/c33907039.lua
@@ -3,8 +3,9 @@
 -- Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Add monster to hand
+	-- Search 1 monster with 0 ATK
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -16,16 +17,16 @@ end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()==PHASE_MAIN1 and not Duel.CheckPhaseActivity()
 end
-function s.filter(c)
+function s.thfilter(c)
 	return c:IsMonster() and c:IsAttack(0) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
@@ -39,7 +40,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetCode(EVENT_SUMMON_SUCCESS)
 			e1:SetLabel(tc:GetCode())
 			e1:SetOperation(s.checkop)
-			e1:SetReset(RESET_PHASE+PHASE_END,2)
+			e1:SetReset(RESET_PHASE|PHASE_END,2)
 			Duel.RegisterEffect(e1,tp)
 			-- Cannot activate effects of monsters with the same name
 			local e2=Effect.CreateEffect(c)
@@ -49,7 +50,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e2:SetTargetRange(1,0)
 			e2:SetValue(s.aclimit)
 			e2:SetLabelObject(e1)
-			e2:SetReset(RESET_PHASE+PHASE_END,2)
+			e2:SetReset(RESET_PHASE|PHASE_END,2)
 			Duel.RegisterEffect(e2,tp)
 		end
 	end
@@ -60,5 +61,5 @@ function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 	if sc:IsCode(e:GetLabel()) then e:SetLabel(-1) end
 end
 function s.aclimit(e,re,tp)
-	return not re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsCode(e:GetLabelObject():GetLabel())
+	return re:GetHandler():IsCode(e:GetLabelObject():GetLabel())
 end

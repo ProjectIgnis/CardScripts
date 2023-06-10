f6d603138eabbcbed5e9190d87f3a3adc60798fe
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-06-10 11:13:09 -0700

    Add "Unformed Void (Anime)" (#1029)
    Script

---
+++ unofficial/c511003236.lua
@@ -0,0 +1,64 @@
+--虚構王アンフォームド・ボイド (Anime)
+--Unformed Void (Anime)
+--Scripted by The Razgriz
+Duel.EnableUnofficialProc(PROC_CANNOT_BATTLE_INDES)
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,4,3)
+	c:EnableReviveLimit()
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.atkcon)
+	e1:SetCost(aux.dxmcostgen(1,1))
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--Always Battle Destroy
+   	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BATTLE_INDES)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetTarget(s.tg)
+	e2:SetValue(s.val)
+	c:RegisterEffect(e2)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and Duel.IsMainPhase()
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+		local atk=g:GetSum(Card.GetAttack)
+		if atk>0 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(atk)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+			c:RegisterEffect(e1)
+		end
+	end
+end
+function s.tg(e,c)
+	local bc=e:GetHandler():GetBattleTarget()
+	return bc and bc==c
+end
+function s.val(e,re,c)
+	return re:GetOwnerPlayer()~=e:GetHandlerPlayer()
+end

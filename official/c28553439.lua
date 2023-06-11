40d5d1051619384d85421909e578bd453d0a77f4
Larry126 <28985622+larry126@users.noreply.github.com>
2023-06-11 22:26:44 +0800

    Fix "Magical Dimension"
    Now using proper target functions and general clean-up

--- official/c28553439.lua
+++ official/c28553439.lua
@@ -4,7 +4,8 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -20,33 +21,34 @@ end
 function s.filter(c,e,tp)
 	return c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.rfilter(c,e,tp)
-	return c:IsReleasableByEffect() and c:IsCanBeEffectTarget(e)
-		and Duel.GetMZoneCount(tp,c)>0
+function s.rfilter(c,tp)
+	return c:IsReleasableByEffect() and Duel.GetMZoneCount(tp,c)>0
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.rfilter(chkc,e,tp) end
-	if chk==0 then return Duel.CheckReleaseGroup(tp,s.rfilter,1,nil,e,tp)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.rfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.rfilter,tp,LOCATION_MZONE,0,1,nil,tp)
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	local g=Duel.SelectReleaseGroup(tp,s.rfilter,1,1,nil,e,tp)
-	Duel.SetTargetCard(g)
+	local g=Duel.SelectTarget(tp,s.rfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	local dg=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,LOCATION_MZONE,g)
+	Duel.SetOperationInfo(0,CATEGORY_RELEASE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,dg,1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsControler(tp) and not tc:IsImmuneToEffect(e) then
-		if Duel.Release(tc,REASON_EFFECT)==0 then return end
+	if tc:IsRelateToEffect(e) and tc:IsControler(tp) and Duel.Release(tc,REASON_EFFECT)>0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-		if #sg==0 then return end
-		if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)==0 then return end
-		local dg=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE)
-		if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-			local des=dg:Select(tp,1,1,nil)
-			Duel.HintSelection(des,true)
-			Duel.BreakEffect()
-			Duel.Destroy(des,REASON_EFFECT)
+		if #sg>0 and Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)>0 then
+			local dg=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE)
+			if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+				local des=dg:Select(tp,1,1,nil)
+				Duel.HintSelection(des,true)
+				Duel.BreakEffect()
+				Duel.Destroy(des,REASON_EFFECT)
+			end
 		end
 	end
 end

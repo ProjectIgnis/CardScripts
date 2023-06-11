be9f17f544a44cbe94e487550ab23e5663adbbd1
Larry126 <28985622+larry126@users.noreply.github.com>
2023-06-11 20:36:11 +0800

    Add and update anime scripts

--- unofficial/c511000302.lua
+++ unofficial/c511000302.lua
@@ -1,54 +1,74 @@
---Magnet Force
+--マグネット・フォース (Anime)
+--Magnet Force (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetCondition(s.con)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.operation)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
 	c:RegisterEffect(e1)
+	--Remain field
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_REMAIN_FIELD)
+	c:RegisterEffect(e2)
+	--Redirect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(s.redirectCon)
+	e3:SetTarget(s.redirectTg)
+	e3:SetOperation(s.redirectOp)
+	c:RegisterEffect(e3)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	if g:GetFirst():IsRelateToEffect(e) then
-		Duel.ChangeTargetCard(ev,g)
-	end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) end
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_F)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetCondition(s.con)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_TURN_END)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetOperation(function() Duel.SendtoGrave(c,REASON_RULE) end)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+	c:RegisterEffect(e1)
+	local res,teg,tep,tev,tre,tr,trp=Duel.CheckEvent(EVENT_CHAINING,true)
+	if res and s.redirectCon(e,tp,teg,tep,tev,tre,tr,trp) and s.redirectTg(e,tp,teg,tep,tev,tre,tr,trp,0)
+		and Duel.SelectEffectYesNo(tp,c) then
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		s.redirectTg(e,tp,teg,tep,tev,tre,tr,trp,1)
+		e:SetOperation(s.redirectOp)
+	else
+		e:SetProperty(0)
+		e:SetOperation(nil)
+	end
 end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
+function s.filter(c,p)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE+RACE_ROCK) and c:IsControler(p) and c:IsLocation(LOCATION_MZONE)
+end
+function s.redirectCon(e,tp,eg,ep,ev,re,r,rp)
 	if rp==tp or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	if not g or #g~=1 then return false end
-	local tc=g:GetFirst()
-	e:SetLabelObject(tc)
-	return (tc:IsRace(RACE_ROCK) or tc:IsRace(RACE_MACHINE)) and tc:IsControler(e:GetHandler():GetControler()) and tc:IsLocation(LOCATION_MZONE)
+	return g:IsExists(s.filter,1,nil,tp)
 end
-function s.filter2(c,re,rp,tf,ceg,cep,cev,cre,cr,crp)
-	return tf(re,rp,ceg,cep,cev,cre,cr,crp,0,c)
-end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local tf=re:GetTarget()
-	local res,ceg,cep,cev,cre,cr,crp=Duel.CheckEvent(re:GetCode(),true)
-	if chkc then return chkc~=e:GetLabelObject() and chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and tf(re,rp,ceg,cep,cev,cre,cr,crp,0,chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter2,tp,0,LOCATION_MZONE,1,e:GetLabelObject(),re,rp,tf,ceg,cep,cev,cre,cr,crp) end
+function s.redirectTg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_MZONE,Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS):FilterCount(s.filter,nil,tp),nil) end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	local g=tg:Filter(s.filter,nil,tp)
+	local g2=(tg-g):KeepAlive()
+	e:SetLabelObject(g2)
+	g2:ForEach(function(c) c:CreateEffectRelation(e) end)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.filter2,tp,0,LOCATION_MZONE,1,1,e:GetLabelObject(),re,rp,tf,ceg,cep,cev,cre,cr,crp)
+	Duel.SelectTarget(tp,nil,tp,0,LOCATION_MZONE,#g,#g,nil)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	if g:GetFirst():IsRelateToEffect(e) then
-		Duel.ChangeTargetCard(ev,g)
-	end
+function s.redirectOp(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	local g=e:GetLabelObject():Filter(Card.IsRelateToEffect,nil,e)
+	Duel.ChangeTargetCard(ev+(e:IsHasType(EFFECT_TYPE_ACTIVATE) and 1 or 0),tg+g)
 end
---
+++ unofficial/c511600400.lua
@@ -0,0 +1,448 @@
+--ラーの翼神竜 (Anime)
+--The Winged Dragon of Ra (Anime)
+--Scripted by Larry126
+Duel.EnableUnofficialProc(PROC_RA_DEFUSION)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Summon with 3 Tribute
+	aux.AddNormalSummonProcedure(c,true,false,3,3)
+	aux.AddNormalSetProcedure(c,true,false,3,3)
+	--Destroy Equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_EQUIP)
+	e1:SetOperation(s.eqop)
+	c:RegisterEffect(e1)
+	--Control of this card cannot switch
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	c:RegisterEffect(e2)
+	--Effect immunity
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetValue(s.efilter)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+	e4:SetValue(aux.cannotmatfilter(SUMMON_TYPE_FUSION,SUMMON_TYPE_RITUAL))
+	c:RegisterEffect(e4)
+	--Last for 1 turn & block
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EVENT_TURN_END)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetOperation(s.stgop)
+	c:RegisterEffect(e5)
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e6:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e6:SetOperation(s.spchk)
+	c:RegisterEffect(e6)
+	--Stats when Normal Summoned
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_MATERIAL_CHECK)
+	e7:SetValue(s.valcheck)
+	c:RegisterEffect(e7)
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetCode(EFFECT_SUMMON_COST)
+	e8:SetOperation(function() e7:SetLabel(1) end)
+	c:RegisterEffect(e8)
+	--Unstoppable Attack
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_SINGLE)
+	e9:SetCode(EFFECT_UNSTOPPABLE_ATTACK)
+	e9:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL) end)
+	c:RegisterEffect(e9)
+	--Immortal Phoenix
+	local e12=Effect.CreateEffect(c)
+	e12:SetDescription(aux.Stringid(id,1))
+	e12:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e12:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e12:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+	e12:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e12:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_GRAVE) end)
+	e12:SetOperation(s.egpop)
+	c:RegisterEffect(e12)
+	--Point-to-Point Transfer
+	local e10=Effect.CreateEffect(c)
+	e10:SetDescription(aux.Stringid(id,0))
+	e10:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e10:SetType(EFFECT_TYPE_IGNITION)
+	e10:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+	e10:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e10:SetRange(LOCATION_MZONE)
+	e10:SetCost(s.payatkcost)
+	e10:SetOperation(s.payatkop)
+	c:RegisterEffect(e10)
+	local e11=Effect.CreateEffect(c)
+	e11:SetDescription(aux.Stringid(id,0))
+	e11:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e11:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e11:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e11:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+	e11:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e11:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_GRAVE) end)
+	e11:SetCost(s.payatkcost)
+	e11:SetOperation(s.payatkop)
+	c:RegisterEffect(e11)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Destroy(eg:Filter(function(ec) return ec:GetEquipTarget()==c end,nil),REASON_EFFECT)
+end
+function s.leaveChk(c,category)
+	local ex,tg=Duel.GetOperationInfo(0,category)
+	return ex and tg~=nil and tg:IsContains(c)
+end
+function s.efilter(e,te,c)
+	local c=e:GetOwner()
+	local tc=te:GetOwner()
+	return (te:IsTrapEffect() and te:IsActivated())
+		or (((te:IsSpellEffect() and te:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):IsContains(c))
+		or (te:IsMonsterEffect() and tc~=c and not tc:IsOriginalAttribute(ATTRIBUTE_DIVINE)))
+		and ((c:GetDestination()>0 and c:GetReasonEffect()==te)
+		or (s.leaveChk(c,CATEGORY_TOHAND) or s.leaveChk(c,CATEGORY_DESTROY) or s.leaveChk(c,CATEGORY_REMOVE)
+		or s.leaveChk(c,CATEGORY_TODECK) or s.leaveChk(c,CATEGORY_RELEASE) or s.leaveChk(c,CATEGORY_TOGRAVE))))
+end
+function s.stgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local effs={c:GetCardEffect()}
+	for _,eff in ipairs(effs) do
+		if eff:GetOwner()~=c and not eff:GetOwner():IsCode(0)
+			and not eff:IsHasProperty(EFFECT_FLAG_IGNORE_IMMUNE) and eff:GetCode()~=EFFECT_SPSUMMON_PROC
+			and (eff:GetTarget()==aux.PersistentTargetFilter or not eff:IsHasType(EFFECT_TYPE_GRANT+EFFECT_TYPE_FIELD)) then
+			eff:Reset()
+		end
+	end
+end
+function s.spchk(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if re and (re:IsSpellEffect() or re:IsMonsterEffect()) then
+		local prevCtrl=c:GetPreviousControler()
+		aux.DelayedOperation(c,PHASE_END,id,e,tp,function()
+			if c:IsPreviousLocation(LOCATION_GRAVE) then
+				Duel.SendtoGrave(c,REASON_EFFECT,prevCtrl)
+			elseif c:IsPreviousLocation(LOCATION_DECK) then
+				Duel.SendtoDeck(c,prevCtrl,SEQ_DECKSHUFFLE,REASON_EFFECT)
+			elseif c:IsPreviousLocation(LOCATION_HAND) then
+				Duel.SendtoHand(c,prevCtrl,REASON_EFFECT)
+			elseif c:IsPreviousLocation(LOCATION_REMOVED) then
+				Duel.Remove(c,c:GetPreviousPosition(),REASON_EFFECT,prevCtrl)
+			elseif c:GetPreviousLocation()==0 then
+				Duel.RemoveCards(c)
+			end
+		end,nil,0)
+	end
+	if c:IsAttackPos() then return end
+	local ac=Duel.GetAttacker()
+	if Duel.CheckEvent(EVENT_ATTACK_ANNOUNCE) and ac:CanAttack()
+		and ac:GetAttackableTarget():IsContains(c)
+		and Duel.SelectEffectYesNo(tp,c,aux.Stringid(68823957,0)) then
+		Duel.HintSelection(c,true)
+		Duel.ChangeAttackTarget(c)
+	end
+	local te,tg=Duel.GetChainInfo(ev+1,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TARGET_CARDS)
+	if te and te~=re and te:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and #tg==1
+		and c:IsCanBeEffectTarget(te) and Duel.SelectEffectYesNo(tp,c,aux.Stringid(68823957,1)) then
+		Duel.ChangeTargetCard(ev+1,Group.FromCards(c))
+	end
+end
+-------------------------------------------
+--Stats When Normal Summoned
+function s.valcheck(e,c)
+	local mg=c:GetMaterial()
+	local atk=0
+	local def=0
+	for tc in mg:Iter() do
+		local catk=tc:GetAttack()
+		local cdef=tc:GetDefense()
+		atk=atk+(catk>=0 and catk or 0)
+		def=def+(cdef>=0 and cdef or 0)
+	end
+	if e:GetLabel()==1 then
+		e:SetLabel(0)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_BASE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TOFIELD)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+		e2:SetValue(def)
+		c:RegisterEffect(e2)
+	end
+end
+-------------------------------------------
+--Point-to-Point Transfer
+function s.payatkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local lpCost=Duel.GetLP(tp)-1
+	if chk==0 then return lpCost>0 and Duel.CheckLPCost(tp,lpCost) end
+	e:SetLabel(lpCost)
+	Duel.PayLPCost(tp,lpCost)
+end
+function s.payatkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local lp=e:GetLabel()
+		--Fuse ATK/DEF
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_BASE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCondition(s.dfcon)
+		e1:SetValue(c:GetBaseAttack()+lp)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+		e2:SetValue(c:GetBaseDefense()+lp)
+		c:RegisterEffect(e2)
+		--Treated as Fusion
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_ADD_TYPE)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetCondition(s.dfcon)
+		e3:SetValue(TYPE_FUSION)
+		e3:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e3)
+		--Tribute for stats
+		local e4=Effect.CreateEffect(c)
+		e4:SetDescription(aux.Stringid(id,2))
+		e4:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+		e4:SetType(EFFECT_TYPE_QUICK_O)
+		e4:SetCode(EVENT_FREE_CHAIN)
+		e4:SetRange(LOCATION_MZONE)
+		e4:SetCondition(s.dfcon)
+		e4:SetCost(s.tatkcost)
+		e4:SetOperation(s.tatkop)
+		e4:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e4)
+		--LP is stats
+		local e5=Effect.CreateEffect(c)
+		e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e5:SetCode(EVENT_RECOVER)
+		e5:SetRange(LOCATION_MZONE)
+		e5:SetCondition(s.dfcon)
+		e5:SetOperation(s.atkop1)
+		e5:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e5)
+		local e6=Effect.CreateEffect(c)
+		e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e6:SetCode(EVENT_CHAIN_END)
+		e6:SetRange(LOCATION_MZONE)
+		e6:SetCondition(s.dfcon)
+		e6:SetOperation(s.atkop2)
+		e6:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e6)
+		--Attack all
+		local e7=Effect.CreateEffect(c)
+		e7:SetType(EFFECT_TYPE_FIELD)
+		e7:SetRange(LOCATION_MZONE)
+		e7:SetTargetRange(0,LOCATION_MZONE)
+		e7:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CANNOT_DISABLE)
+		e7:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+		e7:SetCondition(s.dfcon)
+		e7:SetTarget(function(_e,_c) return _c:GetFlagEffect(id+100)>0 end)
+		e7:SetValue(function(_e,_c) return _c==_e:GetHandler() end)
+		e7:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e7)
+		local e8=Effect.CreateEffect(c)
+		e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e8:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e8:SetCode(EVENT_DAMAGE_STEP_END)
+		e8:SetCondition(s.dfcon)
+		e8:SetOperation(s.dirop)
+		e8:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e8)
+		local e9=Effect.CreateEffect(c)
+		e9:SetType(EFFECT_TYPE_SINGLE)
+		e9:SetCode(EFFECT_EXTRA_ATTACK)
+		e9:SetValue(9999)
+		e9:SetCondition(s.dfcon)
+		e9:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e9)
+		local e10=Effect.CreateEffect(c)
+		e10:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e10:SetCode(EVENT_DAMAGE_STEP_END)
+		e10:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e10:SetCondition(s.dfcon)
+		e10:SetOperation(s.unop)
+		e10:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e10)
+	end
+end
+function s.dfcon(e)
+	if e:GetHandler():HasFlagEffect(FLAG_RA_DEFUSION) then
+		e:Reset()
+		return false
+	end
+	return true
+end
+function s.atkop1(e,tp,eg,ep,ev,re,r,rp)
+	if ep==1-tp then return end
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCondition(s.dfcon)
+	e1:SetValue(ev)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
+	Duel.SetLP(tp,1,REASON_EFFECT)
+end
+function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
+	local lp=Duel.GetLP(tp)
+	if lp<=1 then return end
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCondition(s.dfcon)
+	e1:SetValue(lp-1)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
+	Duel.SetLP(tp,1,REASON_EFFECT)
+end
+function s.tatkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,nil,1,false,nil,e:GetHandler()) end
+	local g=Duel.SelectReleaseGroupCost(tp,nil,1,99,false,nil,e:GetHandler())
+	local suma=0
+	local sumd=0
+	for tc in g:Iter() do
+		suma=suma+tc:GetAttack()
+		sumd=sumd+tc:GetDefense()
+	end
+	e:SetLabel(suma,sumd)
+	Duel.Release(g,REASON_COST)
+end
+function s.tatkop(e,tp,eg,ep,ev,re,r,rp)
+	if not s.dfcon(e) then return end
+	local c=e:GetHandler()
+	local atk,def=e:GetLabel()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCondition(s.dfcon)
+	e1:SetValue(atk)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetCondition(s.dfcon)
+	e2:SetValue(def)
+	e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+	c:RegisterEffect(e2)
+end
+function s.unop(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc and bc:IsRelateToBattle() then
+		bc:RegisterFlagEffect(id+100,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE,0,1)
+	end
+end
+function s.dirop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.GetAttackTarget() then
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+		e1:SetCondition(s.dfcon)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE)
+		c:RegisterEffect(e1)
+	end
+end
+-------------------------------------------
+--God Phoenix
+function s.egpop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect() and c:IsFaceup() then
+		--Immunities
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_IMMUNE_EFFECT)
+		e2:SetValue(s.imfilter)
+		c:RegisterEffect(e2)
+		local e3=e1:Clone()
+		e3:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+		e3:SetValue(aux.cannotmatfilter(SUMMON_TYPE_FUSION,SUMMON_TYPE_RITUAL))
+		c:RegisterEffect(e3)
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_SINGLE)
+		e4:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+		e4:SetValue(1)
+		e4:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e4)
+		--Send to Graveyard
+		local e5=Effect.CreateEffect(c)
+		e5:SetDescription(aux.Stringid(id,3))
+		e5:SetCategory(CATEGORY_TOGRAVE)
+		e5:SetType(EFFECT_TYPE_QUICK_O)
+		e5:SetCode(EVENT_FREE_CHAIN)
+		e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e5:SetRange(LOCATION_MZONE)
+		e5:SetCondition(s.tgcon)
+		e5:SetCost(s.tgcost)
+		e5:SetTarget(s.tgtg)
+		e5:SetOperation(s.tgop)
+		e5:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e5)
+	end
+end
+function s.imfilter(e,te)
+	local c=e:GetOwner()
+	return (c:GetDestination()>0 and c:GetReasonEffect()==te)
+		or (s.leaveChk(c,CATEGORY_TOHAND) or s.leaveChk(c,CATEGORY_DESTROY) or s.leaveChk(c,CATEGORY_REMOVE)
+		or s.leaveChk(c,CATEGORY_TODECK) or s.leaveChk(c,CATEGORY_RELEASE) or s.leaveChk(c,CATEGORY_TOGRAVE))
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsHasEffect(EFFECT_UNSTOPPABLE_ATTACK) or (not c:IsHasEffect(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+		and not c:IsHasEffect(EFFECT_FORBIDDEN) and not c:IsHasEffect(EFFECT_CANNOT_ATTACK)
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_CANNOT_ATTACK_ANNOUNCE)
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_CANNOT_ATTACK))
+end
+function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoGrave(tc,REASON_EFFECT)
+	end
+end

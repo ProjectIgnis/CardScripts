5209c9c830c931678d0af230998abe67d51b6ed1
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-06-15 15:06:36 +0200

    added new rush cards scripts

---
+++ rush/c160208058.lua
@@ -0,0 +1,41 @@
+--幻壊奥義－一式攻事
+--Demolition Art - Absolute Destruction
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.atkcon)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp)
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsRace(RACE_WYRM) and c:IsAbleToDeck()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+function s.desfilter(c)
+	return c:IsFaceup() and c:IsAttackPos()
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,6,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g,true)
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	local dg=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
+	if g:Filter(Card.IsLocation,nil,LOCATION_DECK|LOCATION_EXTRA):GetSum(Card.GetLevel)>=16 
+		and #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.BreakEffect()
+		Duel.Destroy(dg,REASON_EFFECT)
+	end
+end

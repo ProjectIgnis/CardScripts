52682b991c303d6723f67caa64be4abb97faef61
Larry126 <28985622+larry126@users.noreply.github.com>
2023-06-16 11:37:47 +0800

    Update "Card Breaker (Anime)"
    Should be full nomi

--- unofficial/c810000050.lua
+++ unofficial/c810000050.lua
@@ -5,6 +5,12 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	--Special Summon condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e0)
 	--Special Summon procedure
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)

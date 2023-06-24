d1f9da83d63b59c9c92b12e60a0d2b04f906cd04
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-06-24 09:24:14 +0200

    added Gold Rush

---
+++ rush/c160208059.lua
@@ -0,0 +1,54 @@
+--ゴールド・ラッシュ
+--Gold Rush
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--When your opponent normal/special summons a monster, draw 3 cards
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+end
+function s.filter1(c,tp)
+	return c:IsSummonPlayer(1-tp) and c:IsLevelAbove(7) and c:IsFaceup()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter1,1,nil,tp) 
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,3) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(3)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
+end
+function s.spfilter(c,e,tp)
+	return c:IsLevelAbove(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	local g=Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_MZONE,0,nil)
+	if Duel.SendtoGrave(g,REASON_COST)<=0 then return end
+	--Effect
+	if Duel.Draw(tp,3,REASON_EFFECT)<2 then return end
+	local dg=Duel.GetOperatedGroup()
+	Duel.ConfirmCards(1-tp,dg)
+	if dg:FilterCount(s.spfilter,nil,e,tp)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local sg=dg:FilterSelect(tp,s.spfilter,1,3,nil,e,tp)
+		if #sg>0 then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+		end
+	end
+end

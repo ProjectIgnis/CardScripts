f7e7082cc1e057502e71aa9937bfe63bb6f481e5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-06-29 20:07:27 +0300

    "Nightmell the Dark Bonder" fix
    Shouldn't be able to Tribute or Special Summon monsters that don't have a Level.

--- official/c66569334.lua
+++ official/c66569334.lua
@@ -1,5 +1,5 @@
 -- 悪醒師ナイトメルト
--- Evil-Awakening Master Nightmelt
+-- Nightmell the Dark Bonder
 -- Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 end
 function s.spfilter(c,e,tp,cc,sumloc,race,att,lv,atk,def,code)
 	if c:IsLocation(LOCATION_EXTRA) and (sumloc~=LOCATION_EXTRA or Duel.GetLocationCountFromEx(tp,tp,cc,c)<1) then return false end
-	return not c:IsOriginalCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:HasLevel() and not c:IsOriginalCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and c:IsOriginalRace(race) and c:IsOriginalAttribute(att)
 		and c:GetOriginalLevel()==lv and c:GetTextAttack()==atk and c:GetTextDefense()==def
 end
@@ -25,8 +25,8 @@ function s.getprops(c)
 	return c:GetSummonLocation(),c:GetOriginalRace(),c:GetOriginalAttribute(),c:GetOriginalLevel(),c:GetTextAttack(),c:GetTextDefense(),c:GetOriginalCode()
 end
 function s.spcostfilter(c,e,tp)
-	local loc=Duel.GetMZoneCount(tp,c)>0 and LOCATION_DECK+LOCATION_EXTRA or LOCATION_EXTRA
-	return Duel.IsExistingMatchingCard(s.spfilter,tp,loc,0,1,nil,e,tp,c,s.getprops(c))
+	local loc=Duel.GetMZoneCount(tp,c)>0 and LOCATION_DECK|LOCATION_EXTRA or LOCATION_EXTRA
+	return c:HasLevel() and Duel.IsExistingMatchingCard(s.spfilter,tp,loc,0,1,nil,e,tp,c,s.getprops(c))
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.spcostfilter,1,false,nil,nil,e,tp) end
@@ -36,13 +36,13 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local rc=e:GetLabelObject()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil,e,tp,nil,s.getprops(rc))
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil,e,tp,nil,s.getprops(rc))
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end

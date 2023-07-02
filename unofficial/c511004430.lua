33daac2b2722b2adaf5a1e6bf24cfce87a59f4bf
Larry126 <28985622+larry126@users.noreply.github.com>
2023-07-02 14:10:10 +0800

    Unofficial script fixes
    - Dragon's Shadow Performance: was not using the correct function name
    - Sage's Stone (Anime): didn't include "Dark Magician" in listed names

--- unofficial/c511004430.lua
+++ unofficial/c511004430.lua
@@ -1,22 +1,30 @@
---Performance Dragon's Shadow
+--エンタメ・ドラゴンズ・シャドー
+--Dragon's Shadow Performance
 --fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x9f}
+s.listed_series={SET_PERFORMAPAL}
 function s.condition(e,tp,eg,ev,ep,re,r,rp)
 	return eg:IsExists(Card.IsControler,1,nil,1-tp) and (Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated())
+end
+function s.filter(c,tp,rc)
+	local g=Group.CreateGroup()
+	if rc then g:AddCard(rc) end
+	g:AddCard(c)
+	return c:IsFaceup() and c:IsSetCard(SET_PERFORMAPAL) and Duel.IsExistingMatchingCard(nil,tp,LOCATION_MZONE,0,1,g)
 end
 function s.cfilter(c,tp)
 	return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,c,tp,c)
@@ -26,16 +34,8 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,tp)
 	Duel.Release(g,REASON_COST)
 end
-function s.filter1(c,tp,rc)
-	local g=Group.CreateGroup()
-	if rc then
-		g:AddCard(rc)
-	end
-	g:AddCard(c)
-	return c:IsFaceup() and c:IsSetCard(0x9f) and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_MZONE,0,1,g)
-end
 function s.target(e,tp,eg,ev,ep,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc,tp) end
 	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
 	if e:IsHasType(EFFECT_TYPE_ACTIVATE) and e:GetHandler():IsLocation(LOCATION_HAND) then
 		ft=ft-1
@@ -53,7 +53,7 @@ function s.activate(e,tp,eg,ev,ep,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetValue(1)
 	e1:SetTargetRange(LOCATION_ONFIELD,0)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
@@ -71,7 +71,7 @@ function s.activate(e,tp,eg,ev,ep,re,r,rp)
 				local e3=Effect.CreateEffect(c)
 				e3:SetType(EFFECT_TYPE_SINGLE)
 				e3:SetCode(EFFECT_EQUIP_LIMIT)
-				e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+				e3:SetReset(RESET_EVENT|RESETS_STANDARD)
 				e3:SetValue(s.eqlimit)
 				e3:SetLabelObject(tc)
 				ec:RegisterEffect(e3)
@@ -79,11 +79,11 @@ function s.activate(e,tp,eg,ev,ep,re,r,rp)
 				e4:SetType(EFFECT_TYPE_EQUIP)
 				e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 				e4:SetCode(EFFECT_UPDATE_ATTACK)
-				e4:SetReset(RESET_EVENT+RESETS_STANDARD)
+				e4:SetReset(RESET_EVENT|RESETS_STANDARD)
 				e4:SetValue(atk)
 				ec:RegisterEffect(e4)
 				local fid=c:GetFieldID()
-				ec:RegisterFlagEffect(51104430,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+				ec:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1,fid)
 				local e5=Effect.CreateEffect(c)
 				e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 				e5:SetCode(EVENT_PHASE+PHASE_END)
@@ -103,7 +103,7 @@ function s.eqlimit(e,c)
 end
 function s.spcon(e,tp,eg,ev,ep,re,r,rp)
 	local tc=e:GetLabelObject()
-	if tc:GetFlagEffectLabel(51104430)~=e:GetLabel() then
+	if tc:GetFlagEffectLabel(id)~=e:GetLabel() then
 		e:Reset()
 		return false
 	else return true end
--- unofficial/c511777009.lua
+++ unofficial/c511777009.lua
@@ -1,9 +1,11 @@
---アポピスの化神
+--賢者の宝石 (Anime)
+--Sage's Stone (Anime)
 --fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
@@ -11,26 +13,23 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={CARD_DARK_MAGICIAN_GIRL}
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsCode(CARD_DARK_MAGICIAN_GIRL)
-end
+s.listed_names={CARD_DARK_MAGICIAN_GIRL,CARD_DARK_MAGICIAN}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_DARK_MAGICIAN_GIRL),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+	if c:IsRelateToEffect(e) and c:IsFaceup() and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
 		and Duel.MoveToField(c,tp,tp,LOCATION_MZONE,POS_FACEUP,true) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_RACE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetValue(RACE_SPELLCASTER)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		c:RegisterEffect(e1,true)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_CHANGE_ATTRIBUTE)
@@ -57,7 +56,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e7:SetValue(TYPE_MONSTER)
 		c:RegisterEffect(e7,true)
 		local fid=c:GetFieldID()
-		c:RegisterFlagEffect(51177709,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,fid)
 		local e8=Effect.CreateEffect(c)
 		e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e8:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
@@ -67,14 +66,14 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e8:SetLabelObject(c)
 		e8:SetCondition(s.tgcon)
 		e8:SetOperation(s.tgop)
-		e8:SetReset(RESET_PHASE+PHASE_END)
+		e8:SetReset(RESET_PHASE|PHASE_END)
 		Duel.RegisterEffect(e8,tp)
 		c:SetStatus(STATUS_FORM_CHANGED,true)
 	end
 end
 function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
-	if tc:GetFlagEffectLabel(51177709)==e:GetLabel() then
+	if tc:GetFlagEffectLabel(id)==e:GetLabel() then
 		return true
 	else
 		e:Reset()

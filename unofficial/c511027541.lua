afb3c9066d92c73a3f652c0c7eaa55d75673d574
When <104731682+idk-what-to-name-here@users.noreply.github.com>
2023-07-05 18:57:12 +0300

    Add "Speedroid Tri-Eyed Dice (Anime)" script (#1037)

---
+++ unofficial/c511027541.lua
@@ -0,0 +1,23 @@
+--スピードロイド 三つ目のダイス (アニメ)
+--Speedroid Tri-Eyed Dice (Anime)
+--Made by When
+local s,id=GetID()
+function s.initial_effect(c)
+	--negate attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.negcon)
+	e1:SetCost(aux.bfgcost)
+	e1:SetOperation(s.negop)
+	c:RegisterEffect(e1)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local at=Duel.GetAttacker()
+	return at:IsControler(1-tp)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateAttack()
+end

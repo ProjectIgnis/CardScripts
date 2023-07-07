5458313a0ff9685b30801e61a1732988bf3c5d2a
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-07-07 15:39:04 +0200

    added Rising Light Angel Esser

---
+++ rush/c160014037.lua
@@ -0,0 +1,56 @@
+--昂光天使エッセル
+--Rising Light Angel Esser
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send the top 2 cards of your Deck to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	aux.GlobalCheck(s,function()
+		s[0]=false
+		s[1]=false
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+		aux.AddValuesReset(function()
+			s[0]=false
+			s[1]=false
+		end)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	s[ep]=true
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not s[tp]
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,2)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,1-tp,0)
+end
+function s.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	if Duel.DiscardDeck(tp,2,REASON_EFFECT)~=2 then return end
+	if Duel.GetTurnCount()==2 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil,e,tp)
+		if #sg>0 then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

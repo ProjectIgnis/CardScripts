b17486a7c030aad02c0b98c9a74b542579b63500
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-07-08 17:12:40 +0300

    Update c511027680.lua
    Fixing my mistakes.

--- unofficial/c511027680.lua
+++ unofficial/c511027680.lua
@@ -1,5 +1,5 @@
---ＧＯ－ＤＤ・Ｗ０ＲＬＤ
 --Go-D/D World
+--ゴー－ディーディー・ワールド
 --Made by Beetron-1 Beetletop
 local s,id=GetID()
 function s.initial_effect(c)
@@ -7,29 +7,37 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--Treated as "Go-D/D"
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_ADD_SETCODE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
-	e2:SetCondition(s.changecon)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_DD))
-	e2:SetValue(0x20af)
-	c:RegisterEffect(e2)
 end
-s.listed_series={SET_DD,0x20af}
+s.listed_series={0xaf,0x20af}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xaf)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,5,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	e1:SetTargetRange(0,1)
-	Duel.RegisterEffect(e1,tp)
-end
-function s.changecon(e)
-	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_DD),e:GetHandlerPlayer(),LOCATION_ONFIELD,0,5,nil)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_ADD_SETCODE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(0x20af)
+		tc:RegisterEffect(e1)
+	end
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetTargetRange(0,1)
+	Duel.RegisterEffect(e2,tp)
 end

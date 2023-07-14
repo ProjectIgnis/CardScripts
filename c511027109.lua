4c304eceef54383b1487b71cf43094f5816097e9
Dakota407 <139493466+Dakota407@users.noreply.github.com>
2023-07-14 09:12:21 -0500

    Add files via upload

---
+++ c511027109.lua
@@ -0,0 +1,75 @@
+--聖地の守護結界 (VG)
+--Sacred Defense Barrier (VG)
+--Made by Dakota
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	--Destroy replace
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTarget(s.reptg)
+	e3:SetValue(s.repval)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e4)
+	local e5=e2:Clone()
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e5)
+end
+function s.filter(c,p)
+	return c:IsFaceup() and c:IsRace(RACE_ROCK) and c:IsControler(p)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetCard(eg:Filter(s.filter,nil,tp))
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x1096)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tg=Duel.GetTargetCards(e)
+	for tc in aux.Next(tg) do
+		if tc:IsFaceup() then
+			Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
+			tc:AddCounter(0x1096,1)
+		end
+	end
+end
+function s.repfilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:GetCounter(0x1096)>0
+		and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) end
+	Duel.Hint(HINT_CARD,1-tp,id)
+	local g=eg:Filter(s.repfilter,nil,tp)
+	for tc in aux.Next(g) do
+		tc:RemoveCounter(tp,0x1096,1,REASON_EFFECT)
+	end
+	g:KeepAlive()
+	e:SetLabelObject(g)
+	return true
+end
+function s.repval(e,c)
+	return e:GetLabelObject():IsContains(c)
+end

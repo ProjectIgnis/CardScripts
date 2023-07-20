df5b05296e55a27cc99b229751a5c2ce70446628
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-07-20 15:31:07 +0200

    added Negatwor Dragon

---
+++ rush/c160014034.lua
@@ -0,0 +1,46 @@
+--ニゲイター・ドラゴン
+--Negatwor Dragon
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send the top 2 cards from the Deck to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_POSITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsStatus(STATUS_SUMMON_TURN)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,2,tp,0)
+end
+function s.atkfilter(c)
+	return c:IsMonster() and c:IsFaceup() and c:GetAttack()>0
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.DiscardDeck(tp,2,REASON_EFFECT)==0 then return end
+	if Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.atkfilter),tp,0,LOCATION_MZONE,1,nil) 
+		and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,14315573)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.atkfilter),tp,0,LOCATION_MZONE,1,3,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			for tc in g:Iter() do
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_UPDATE_ATTACK)
+				e1:SetValue(-2000)
+				e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END|RESET_SELF_TURN,1)
+				tc:RegisterEffectRush(e1)
+			end
+		end
+	end
+end

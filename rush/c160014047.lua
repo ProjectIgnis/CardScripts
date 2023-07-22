817678e1617ec0915d8cd94f8febbec6a387175e
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-07-22 19:25:02 +0200

    added Jointech Dig Up

---
+++ rush/c160014047.lua
@@ -0,0 +1,55 @@
+--ジョインテック・ディガップ
+--Jointech Dig Up
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Add excavated monster to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsRace(RACE_MACHINE) and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=3 end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil):GetFirst()
+	if Duel.SendtoGrave(tc,REASON_COST)<1 then return end
+	--Effect
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
+	Duel.ConfirmDecktop(tp,3)
+	local g=Duel.GetDecktopGroup(tp,3)
+	if g:IsExists(Card.IsAbleToHand,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local tg=g:FilterSelect(tp,Card.IsAbleToHand,1,1,nil)
+		if #tg>0 then
+			Duel.DisableShuffleCheck()
+			Duel.SendtoHand(tg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tg)
+			Duel.ShuffleHand(tp)
+			g:RemoveCard(tg)
+		end
+	end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	else 
+		local ct=#g
+		if ct>0 then
+			Duel.MoveToDeckTop(g,tp)
+			Duel.SortDecktop(tp,tp,ct)
+		end
+	end
+end

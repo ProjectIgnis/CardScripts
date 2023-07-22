0b069eb5bcd4369a4bfcb087726ea9ae02ed500a
When <104731682+idk-what-to-name-here@users.noreply.github.com>
2023-07-22 05:36:43 -0700

    Update c511027651.lua

--- unofficial/c511027651.lua
+++ unofficial/c511027651.lua
@@ -1,12 +1,9 @@
---パラレル・セレクト (アニメ)
---Parallel Select (anime)
---Made by When
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetCode(EVENT_DESTROYED)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCondition(s.condition)
@@ -16,7 +13,6 @@ function s.initial_effect(c)
 end
 function s.cfilter(c,tp)
 	return c:IsType(TYPE_SYNCHRO) and c:IsLocation(LOCATION_GRAVE) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
-		and c:IsMonster()
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
@@ -26,19 +22,14 @@ function s.filter(c)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,nil) and Duel.IsPlayerCanDraw(tp,1) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,1,nil)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	if #g>0 then
-	    if Duel.SendtoHand(g,tp,REASON_EFFECT)~=0 then
-		    Duel.ConfirmCards(1-tp,g)
-	        Duel.Draw(p,d,REASON_EFFECT)
-		end
+	if Duel.SendtoHand(g,tp,REASON_EFFECT)~=0 then
+	    Duel.ConfirmCards(1-tp,g)
+	    Duel.Draw(tp,1,REASON_EFFECT)
 	end
 end

858fcca7fbd1c77cf5c07e7d94487b3b20d67b8d
Larry126 <28985622+larry126@users.noreply.github.com>
2023-07-24 01:27:24 +0800

    Update "Diabound Kernel (Anime)"

--- unofficial/c511000118.lua
+++ unofficial/c511000118.lua
@@ -1,134 +1,71 @@
 --ディアバウンド・カーネル (Anime)
 --Diabound Kernel (Anime)
 --Rescripted by AlphaKretin
-local card, code = GetID()
-function card.initial_effect(c)
-	--equip
-	local e1 = Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(code, 0))
+local s,id=GetID()
+function s.initial_effect(c)
+	--Equip
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTarget(card.eqtg)
-	e1:SetOperation(card.eqop)
+	e1:SetTarget(s.eqtg)
+	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
-	--special summon from equip
-	local e2 = Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(code, 1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	--Return to Monster Zone
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTarget(card.sptg)
-	e2:SetOperation(card.spop)
+	e2:SetTarget(s.rttg)
+	e2:SetOperation(s.rtop)
 	c:RegisterEffect(e2)
-	--special summon from grave
-	local e3 = Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(code, 2))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCountLimit(1, code)
-	e3:SetCondition(card.spcon2)
-	e3:SetTarget(card.sptg2)
-	e3:SetOperation(card.spop2)
-	c:RegisterEffect(e3)
-	--gain effect
-	local e4 = Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
-	e4:SetCode(EVENT_BATTLE_DESTROYING)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetCondition(aux.bdocon)
-	e4:SetTarget(card.efftg)
-	e4:SetOperation(card.effop)
-	c:RegisterEffect(e4)
 end
-
-function card.eqtg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
-		and not c:HasFlagEffect(code)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc~=c end
+	if chk==0 then return not e:GetHandler():HasFlagEffect(id) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,c)
 	end
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_EQUIP)
-	Duel.SelectTarget(tp, Card.IsFaceup, tp, 0, LOCATION_MZONE, 1, 1, nil)
-	c:RegisterFlagEffect(code,RESET_EVENT|(RESETS_STANDARD&~(RESET_TOFIELD|RESET_LEAVE))|RESET_PHASE|PHASE_END,0,1)
+	c:RegisterFlagEffect(id,RESET_EVENT|(RESETS_STANDARD&~(RESET_TOFIELD|RESET_LEAVE))|RESET_PHASE|PHASE_END,0,1)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,c,1,0,0)
 end
-
-function card.eqop(e, tp, eg, ep, ev, re, r, rp)
-	local c = e:GetHandler()
-	if not c:IsRelateToEffect(e) then
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) or tc:IsFacedown() then
+		Duel.SendtoGrave(c,REASON_EFFECT)
 		return
 	end
-	local tc = Duel.GetFirstTarget()
-	if
-		not tc or Duel.GetLocationCount(tp, LOCATION_SZONE) <= 0 or tc:IsControler(tp) or tc:IsFacedown() or
-			not tc:IsRelateToEffect(e)
-	 then
-		Duel.SendtoGrave(c, REASON_EFFECT)
-		return
-	end
-	Duel.Equip(tp, c, tc, true)
+	local pos=c:GetPosition()
+	if not Duel.Equip(tp,c,tc) then return end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetValue(function(_,_c) return _c==tc end)
 	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 	c:RegisterEffect(e1)
-	--equip effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e2:SetValue(function(_e) return -(_e:GetHandler():GetBaseAttack()) end)
 	e2:SetReset(RESET_EVENT|RESETS_STANDARD)
 	c:RegisterEffect(e2)
+	c:RegisterFlagEffect(id+1,RESET_EVENT|RESETS_STANDARD,0,1,pos)
 end
-
-function card.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.rttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk == 0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:HasFlagEffect(code)
-	end
-	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
-	c:RegisterFlagEffect(code,RESET_EVENT|(RESETS_STANDARD&~(RESET_TOFIELD|RESET_LEAVE))|RESET_PHASE|PHASE_END,0,1)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and not c:HasFlagEffect(id) and c:GetFlagEffectLabel(id+1) end
+	c:RegisterFlagEffect(id,RESET_EVENT|(RESETS_STANDARD&~(RESET_TOFIELD|RESET_LEAVE))|RESET_PHASE|PHASE_END,0,1)
 end
-
-function card.spop(e, tp, eg, ep, ev, re, r, rp)
-	local c = e:GetHandler()
-	if not c:IsRelateToEffect(e) then
-		return
-	end
-	Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
-end
-
-function card.spcon2(e, tp, eg, ep, ev, re, r, rp)
-	local c = e:GetHandler()
-	local ec = c:GetPreviousEquipTarget()
-	return c:IsReason(REASON_LOST_TARGET) and ec and ec:IsReason(REASON_DESTROY + REASON_BATTLE)
-end
-function card.sptg2(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then
-		return true
-	end
-	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, tp, 0)
-end
-function card.spop2(e, tp, eg, ep, ev, re, r, rp)
-	Duel.SpecialSummon(e:GetHandler(), 0, tp, tp, false, false, POS_FACEUP)
-end
-
-function card.efftg(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then
-		return true
-	end
-	local bc = e:GetHandler():GetBattleTarget()
-	e:SetLabelObject(bc)
-end
-
-function card.effop(e, tp, eg, ep, ev, re, r, rp)
-	local tc = e:GetLabelObject()
-	if tc and e:GetHandler():IsRelateToEffect(e) then
-		e:GetHandler():CopyEffect(tc:GetOriginalCode(), RESET_EVENT + RESETS_STANDARD, 1)
-	end
+function s.rtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.MoveToField(c,tp,tp,LOCATION_MZONE,c:GetFlagEffectLabel(id+1),true)
 end

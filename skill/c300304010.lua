eca9955dd41f647ae2e3a90a3c04189ee1494b5a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-07-30 09:55:32 +0300

    "Looking into the Future" fix
    Wasn't properly preventing the player from activating Spells after using it.

--- skill/c300304010.lua
+++ skill/c300304010.lua
@@ -27,7 +27,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(function(e,re,tp) return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsSpellEffect() end)
+	e1:SetValue(function(e,re,tp) return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsSpellEffect() end)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	--Flip this card during the End Phase

8d95dc015229d720e96028de7d6919d68ab02402
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-08 14:09:52 +0300

    Added new cards' scripts

---
+++ pre-release/c100314025.lua
@@ -0,0 +1,59 @@
+--炎王神天焼
+--Fire King Sacred Immolation
+--Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	--Destroy an equal number of "Fire King" monsters and opponent cards
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMING_STANDBY_PHASE+TIMINGS_CHECK_MONSTER_E)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	--Replace destruction
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(s.reptg)
+	e2:SetCountLimit(1,id)
+	e2:SetValue(function(e,c) return s.repfilter(c,e:GetHandlerPlayer()) end)
+	e2:SetOperation(function(e) Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT) end)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_FIRE_KING}
+function s.desfilter(c,e,tp)
+	return (c:IsControler(1-tp) or (c:IsFaceup() and c:IsSetCard(SET_FIRE_KING))) and c:IsCanBeEffectTarget(e)
+end
+function s.desrescon(sg,e,tp,mg)
+	return sg:FilterCount(Card.IsControler,nil,tp)==sg:FilterCount(Card.IsControler,nil,1-tp)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsSetCard,SET_FIRE_KING),tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_ONFIELD,nil,e,tp)
+	local tg=aux.SelectUnselectGroup(g,e,tp,2,#g,s.desrescon,1,tp,HINTMSG_DESTROY,s.desrescon)
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tg,#tg,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsOnField() and c:IsSetCard(SET_FIRE_KING)
+		and c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_REPLACE) 
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end

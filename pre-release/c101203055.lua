31898ad52f595aa0215767ea19b03be73cab1918
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-10 16:39:39 +0300

    Added new cards' scripts

---
+++ pre-release/c101203055.lua
@@ -0,0 +1,154 @@
+--マチュア・クロニクル
+--Mature Chronicle
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableCounterPermit(0x25) --Chronicle Counter
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--Place 1 Chronicle Counter on this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCondition(s.countercon)
+	e1:SetOperation(s.counterop)
+	c:RegisterEffect(e1)
+	--Remove any number of counters to activate the appropriate effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.efftg)
+	e2:SetOperation(s.effop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_YUBEL}
+s.listed_names={CARD_YUBEL,48130397} --Super Polymerization
+s.counter_place_list={0x25}
+function s.yubelfilter(c)
+	return c:IsFaceup() and (c:IsCode(CARD_YUBEL) or c:ListsCode(CARD_YUBEL))
+end
+function s.countercon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.yubelfilter,1,nil)
+end
+function s.counterop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not Duel.IsChainSolving() then
+		c:AddCounter(0x25,1)
+	else
+		--Place 1 Chronicle Counter on this card at the end of the Chain Link
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_CHAIN_SOLVED)
+		e1:SetRange(LOCATION_SZONE)
+		e1:SetOperation(function(_e) _e:GetHandler():AddCounter(0x25,1) end)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_CHAIN)
+		c:RegisterEffect(e1)
+		--Reset "e1" at the end of the Chain Link
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_CHAIN_SOLVED)
+		e2:SetOperation(function() e1:Reset() end)
+		e2:SetReset(RESET_CHAIN)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsCode(CARD_YUBEL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.superpolyfilter(c)
+	return c:IsCode(48130397) and c:IsAbleToHand()
+end
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	--Special Summon 1 "Yubel" from your GY
+	local b1=c:IsCanRemoveCounter(tp,0x25,1,REASON_COST)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	--Add 1 of your banished cards to your hand
+	local b2=c:IsCanRemoveCounter(tp,0x25,2,REASON_COST)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_REMOVED,0,1,nil)
+	--Banish 1 card from your Deck
+	local b3=c:IsCanRemoveCounter(tp,0x25,3,REASON_COST)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_DECK,0,1,nil)
+	--Destroy 1 card on the field
+	local b4=c:IsCanRemoveCounter(tp,0x25,4,REASON_COST)
+		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	--Search 1 "Super Polymerization"
+	local b5=c:IsCanRemoveCounter(tp,0x25,5,REASON_COST)
+		and Duel.IsExistingMatchingCard(s.superpolyfilter,tp,LOCATION_DECK,0,1,nil)
+	if chk==0 then return (b1 or b2 or b3 or b4 or b5) end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)},
+		{b3,aux.Stringid(id,3)},
+		{b4,aux.Stringid(id,4)},
+		{b5,aux.Stringid(id,5)})
+	e:SetLabel(op)
+	c:RemoveCounter(tp,0x25,op,REASON_COST)
+	if op==1 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	elseif op==2 then
+		e:SetCategory(CATEGORY_TOHAND)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
+	elseif op==3 then
+		e:SetCategory(CATEGORY_REMOVE)
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+	elseif op==4 then
+		e:SetCategory(CATEGORY_DESTROY)
+		local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
+	elseif op==5 then
+		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	end
+end
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		--Special Summon 1 "Yubel" from your GY
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	elseif op==2 then
+		--Add 1 of your banished cards to your hand
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_REMOVED,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	elseif op==3 then
+		--Banish 1 card from your Deck
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+		end
+	elseif op==4 then
+		--Destroy 1 card on the field
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	elseif op==5 then
+		--Search 1 "Super Polymerization"
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.superpolyfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
---
+++ pre-release/c101203073.lua
@@ -0,0 +1,92 @@
+--エターナル・フェイバリット
+--Eternal Favorite
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Activate 1 of these effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetHintTiming(0,TIMING_MAIN_END+TIMINGS_CHECK_MONSTER_E)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+s.listed_names={CARD_YUBEL}
+s.listed_series={SET_YUBEL}
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_YUBEL) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.fcheck(tp,sg,fc)
+	return sg:IsExists(Card.IsSetCard,1,nil,SET_YUBEL)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil)--,s.fcheck
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local fustg=Fusion.SummonEffTG(nil,Fusion.OnFieldMat,s.fextra)
+	--Special Summon 1 of your "Yubel" monsters that is banished or in your GY
+	local b1=not Duel.HasFlagEffect(tp,id) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil,e,tp)
+	--Fusion Summon using monsters from either field as material, including a "Yubel" monster
+	local b2=not Duel.HasFlagEffect(tp,id+100) and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_YUBEL),tp,LOCATION_ONFIELD,0,1,nil)
+		and c:IsAbleToGraveAsCost() and c:IsStatus(STATUS_EFFECT_ENABLED)
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+		and fustg(e,tp,eg,ep,ev,re,r,rp,0)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	e:SetLabel(op)
+	if op==1 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE|LOCATION_REMOVED)
+	elseif op==2 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST|REASON_DISCARD)
+		Duel.SendtoGrave(c,REASON_COST)
+		fustg(e,tp,eg,ep,ev,re,r,rp,1)
+		Duel.RegisterFlagEffect(tp,id+100,RESET_PHASE|PHASE_END,0,1)
+	end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		--Special Summon 1 of your "Yubel" monsters that is banished or in your GY
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e,tp):GetFirst()
+		if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+			sc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+			--Neither player can activate cards or effects when that monster is Special Summoned
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_CHAIN_END)
+			e1:SetLabelObject(sc)
+			e1:SetOperation(s.limop)
+			Duel.RegisterEffect(e1,tp)
+		end
+		Duel.SpecialSummonComplete()
+	elseif op==2 then
+		--Fusion Summon using monsters from either field as material, including a "Yubel" monster
+		Fusion.SummonEffOP(nil,Fusion.OnFieldMat,s.fextra)(e,tp,eg,ep,ev,re,r,rp)
+	end
+end
+function s.limop(e,tp,eg,ep,ev,re,r,rp)
+	local sc=e:GetLabelObject()
+	local ex,sg=Duel.CheckEvent(EVENT_SPSUMMON_SUCCESS,true)
+	if ex and #sg==1 and sg:GetFirst()==sc and sc:HasFlagEffect(id) then
+		Duel.SetChainLimitTillChainEnd(aux.FALSE)
+	end
+	e:Reset()
+end

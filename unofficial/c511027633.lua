dc6c1e9428864e6e478762272f555d1947fcfc90
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-08-12 03:35:05 +0300

    Add files via upload

---
+++ unofficial/c511027633.lua
@@ -0,0 +1,27 @@
+--フレイム・ウォール
+--Flame Wall (Anime)
+--Made by Beetron-1 Beetletop
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--No effect damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(s.damval)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	c:RegisterEffect(e2)
+end
+function s.damval(e,re,val,r,rp,rc)
+	if rp~=e:GetHandlerPlayer() and (r&REASON_EFFECT)~=0 then return 0
+	else return val end
+end

a593527c9053ef799586f8fe1a8bdea2047385bf
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-14 21:30:08 +0300

    Enemy Controller polish

--- official/c98045062.lua
+++ official/c98045062.lua
@@ -2,7 +2,7 @@
 --Enemy Controller
 local s,id=GetID()
 function s.initial_effect(c)
-	--Change battle position or Tribute 1 monster to take control
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -18,64 +18,58 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(9)
 	return true
 end
-function s.filter1(c)
-	return c:IsFaceup() and c:IsCanChangePosition()
-end
-function s.filter2(c)
-	return c:IsFaceup() and c:IsControlerCanBeChanged(true)
-end
 function s.cfilter(c,tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_CONTROL)
-	if c:IsControler(tp) and c:GetSequence()<5 then ft=ft+1 end
-	return ft>0 and Duel.IsExistingTarget(s.filter2,tp,0,LOCATION_MZONE,1,c)
+	return Duel.GetMZoneCount(tp,c,tp,LOCATION_REASON_CONTROL)>0 and Duel.IsExistingTarget(aux.FaceupFilter(Card.IsControlerCanBeChanged,true),tp,0,LOCATION_MZONE,1,c)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then
+		if not (chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:IsControler(1-tp)) then return false end
 		if e:GetLabel()==1 then
-			return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter1(chkc)
+			return chkc:IsCanChangePosition()
 		else
-			return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter2(chkc)
+			return chkc:IsControlerCanBeChanged()
 		end
 	end
-	local b1=Duel.IsExistingTarget(s.filter1,tp,0,LOCATION_MZONE,1,nil)
+	local b1=Duel.IsExistingTarget(aux.FaceupFilter(Card.IsCanChangePosition),tp,0,LOCATION_MZONE,1,nil)
 	local b2=nil
 	if e:GetLabel()==9 then
 		b2=Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,tp)
 	else
 		b2=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_CONTROL)>0
-			and Duel.IsExistingTarget(s.filter2,tp,0,LOCATION_MZONE,1,nil)
+			and Duel.IsExistingTarget(aux.FaceupFilter(Card.IsControlerCanBeChanged),tp,0,LOCATION_MZONE,1,nil)
 	end
 	if chk==0 then
 		e:SetLabel(0)
 		return b1 or b2
 	end
-	local sel=Duel.SelectEffect(tp,
+	local op=Duel.SelectEffect(tp,
 		{b1,aux.Stringid(id,1)},
 		{b2,aux.Stringid(id,2)})
-	if sel==1 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-		local g=Duel.SelectTarget(tp,s.filter1,tp,0,LOCATION_MZONE,1,1,nil)
-		Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+	if op==1 then
 		e:SetCategory(CATEGORY_POSITION)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+		local g=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsCanChangePosition),tp,0,LOCATION_MZONE,1,1,nil)
+		Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 	else
+		e:SetCategory(CATEGORY_CONTROL)
 		if e:GetLabel()==9 then
 			local rg=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,tp)
 			Duel.Release(rg,REASON_COST)
 		end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
-		local g=Duel.SelectTarget(tp,s.filter2,tp,0,LOCATION_MZONE,1,1,nil)
+		local g=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsControlerCanBeChanged),tp,0,LOCATION_MZONE,1,1,nil)
 		Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
-		e:SetCategory(CATEGORY_CONTROL)
 	end
-	e:SetLabel(sel)
+	e:SetLabel(op)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		if e:GetLabel()==1 then
-			Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,0,POS_FACEUP_ATTACK,0)
-		else
-			Duel.GetControl(tc,tp,PHASE_END,1)
-		end
+	if not (tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsControler(1-tp)) then return end
+	if e:GetLabel()==1 then
+		--Change that target's battle position
+		Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,0,POS_FACEUP_ATTACK,0)
+	else
+		--Take control of that target until the End Phase
+		Duel.GetControl(tc,tp,PHASE_END,1)
 	end
 end

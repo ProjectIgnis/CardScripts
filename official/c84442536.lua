b5d1226c5c6595cb4bfa5b3c4cfcd3bcede6fc01
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-14 20:58:33 +0300

    Graydle Combat polish

--- official/c84442536.lua
+++ official/c84442536.lua
@@ -2,7 +2,7 @@
 --Graydle Combat
 local s,id=GetID()
 function s.initial_effect(c)
-	--Change effect or negate activation
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -13,54 +13,52 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_GRAYDLE}
-function s.cfilter(c,tp)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsSetCard(SET_GRAYDLE)
-end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	if not (re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)
-		and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE))) then return false end
+	if not (re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and (re:IsMonsterEffect() or re:IsHasType(EFFECT_TYPE_ACTIVATE))) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
 	if not g or #g~=1 then return false end
 	local tc=g:GetFirst()
 	e:SetLabelObject(tc)
-	return s.cfilter(tc,tp)
+	return tc:IsControler(tp) and tc:IsLocation(LOCATION_MZONE) and tc:IsFaceup() and tc:IsSetCard(SET_GRAYDLE)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=e:GetLabelObject()
 	local b1=tc:IsLocation(LOCATION_MZONE)
 	local b2=Duel.IsChainNegatable(ev)
 	if chk==0 then return tc and (b1 or b2) end
-	local sel=Duel.SelectEffect(tp,
+	local op=Duel.SelectEffect(tp,
 		{b1,aux.Stringid(id,1)},
 		{b2,aux.Stringid(id,2)})
-	e:SetLabel(sel)
-	if sel==1 then
+	e:SetLabel(op)
+	if op==1 then
 		e:SetCategory(0)
 	else
-		Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-		if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		e:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+		local rc=re:GetHandler()
+		if rc:IsDestructable() and rc:IsRelateToEffect(re) then
 			Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 		end
-		e:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+		Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local sel=e:GetLabel()
 	if sel==1 then
+		--The activated effect becomes "Destroy that monster"
 		Duel.ChangeChainOperation(ev,s.repop)
 	else
+		--Negate the activation, and if you do, destroy that card
 		if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 			Duel.Destroy(eg,REASON_EFFECT)
 		end
 	end
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
-	if e:IsActiveType(TYPE_SPELL+TYPE_TRAP) and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		c:CancelToGrave(false)
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		e:GetHandler():CancelToGrave(false)
 	end
 end

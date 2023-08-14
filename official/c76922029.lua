897253840c2f08fa91a0d1f40d1fa4bc1b8f8ee6
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-14 20:42:28 +0300

    Don Zaloog polish

--- official/c76922029.lua
+++ official/c76922029.lua
@@ -2,20 +2,17 @@
 --Don Zaloog
 local s,id=GetID()
 function s.initial_effect(c)
-	--Discard or send from Deck to GY
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_DAMAGE)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return ep==1-tp end)
+	e1:SetTarget(s.efftg)
+	e1:SetOperation(s.effop)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0
 	local b2=Duel.IsPlayerCanDiscardDeck(1-tp,2)
 	if chk==0 then return b1 or b2 end
@@ -24,19 +21,22 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		{b2,aux.Stringid(id,2)})
 	e:SetLabel(op)
 	if op==1 then
-		Duel.SetOperationInfo(0,CATEGORY_HANDES,0,0,1-tp,1)
 		e:SetCategory(CATEGORY_HANDES)
+		Duel.SetOperationInfo(0,CATEGORY_HANDES,0,0,1-tp,1)
 	else
-		Duel.SetOperationInfo(0,CATEGORY_DECKDES,0,0,1-tp,2)
 		e:SetCategory(CATEGORY_DECKDES)
+		Duel.SetOperationInfo(0,CATEGORY_DECKDES,0,0,1-tp,2)
 	end
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
-		local g=Duel.GetFieldGroup(ep,LOCATION_HAND,0,nil)
-		local sg=g:RandomSelect(ep,1)
-		Duel.SendtoGrave(sg,REASON_DISCARD+REASON_EFFECT)
+		--Discard 1 random card from your opponent's hand
+		local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND,nil)
+		if #g==0 then return end
+		local sg=g:RandomSelect(tp,1)
+		Duel.SendtoGrave(sg,REASON_DISCARD|REASON_EFFECT)
 	else
+		--Send the top 2 cards of your opponent's Deck to the GY
 		Duel.DiscardDeck(1-tp,2,REASON_EFFECT)
 	end
 end

a001252368b6757fff3ba6e00d96543e011f6b8d
Larry126 <28985622+larry126@users.noreply.github.com>
2023-08-16 00:34:37 +0800

    Unofficial scripts updates

--- unofficial/c511003242.lua
+++ unofficial/c511003242.lua
@@ -5,6 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetCondition(function(_,tp) return Duel.GetAttacker():IsControler(1-tp) end)

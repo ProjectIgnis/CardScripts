f41d62c0d41b42f9253bc9a18ffeecf1307ce00d
Larry126 <28985622+larry126@users.noreply.github.com>
2023-08-16 00:19:52 +0800

    Update c511027633.lua

--- unofficial/c511027633.lua
+++ unofficial/c511027633.lua
@@ -1,24 +1,24 @@
---フレイム・ウォール
+--フレイム・ウォール (Anime)
 --Flame Wall (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e0)
-	--No effect damage
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetTargetRange(1,0)
-	e1:SetValue(s.damval)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	--No effect damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetValue(s.damval)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	c:RegisterEffect(e3)
 end
 function s.damval(e,re,val,r,rp,rc)
 	if rp~=e:GetHandlerPlayer() and (r&REASON_EFFECT)~=0 then return 0

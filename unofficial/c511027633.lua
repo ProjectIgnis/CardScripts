3e226d93a057192ef444c1cd028904388ace1607
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-08-15 19:33:32 +0300

    Add "Flame Wall (Anime)" (#1074)
    Co-authored-by: Larry126 <28985622+larry126@users.noreply.github.com>

---
+++ unofficial/c511027633.lua
@@ -0,0 +1,26 @@
+--フレイム・ウォール (Anime)
+--Flame Wall (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--No effect damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetValue(s.damval)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	c:RegisterEffect(e3)
+end
+function s.damval(e,re,val,r,rp,rc)
+	if rp~=e:GetHandlerPlayer() and (r&REASON_EFFECT)~=0 then return 0
+	else return val end
+end

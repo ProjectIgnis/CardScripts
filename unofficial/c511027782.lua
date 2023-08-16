307a495bc85f3a53bac82a32f493ba5a23d56c9a
Larry126 <28985622+larry126@users.noreply.github.com>
2023-08-16 16:15:14 +0800

    Update c511027782.lua

--- unofficial/c511027782.lua
+++ unofficial/c511027782.lua
@@ -7,9 +7,8 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
-    --damage
+    --Inflict 400 damage
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_DAMAGE)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
     e2:SetRange(LOCATION_SZONE)
@@ -26,6 +25,6 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
     return eg:IsExists(s.cfilter,1,nil,1-tp)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    local ct=eg:FilterCount(s.cfilter,nil,1-tp)
-    Duel.Damage(1-tp,ct*400,REASON_EFFECT)
+	Duel.Hint(HINT_CARD,1-tp,id)
+    Duel.Damage(1-tp,400,REASON_EFFECT)
 end

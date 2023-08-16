867d188c07eccedba706348db73db8eb0a4a9b99
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-08-16 12:52:48 +0200

    added Sanctum of Legend

---
+++ rush/c160319028.lua
@@ -0,0 +1,35 @@
+--サンクタム・オブ・レジェンド
+--Sanctum of Legend
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Increase ATK of Aqua Normal monsters by 200
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.filter)
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	--Indestructable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(s.indtg)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
+function s.filter(e,c)
+	return c:IsRace(RACE_DRAGON) and c:IsLevelAbove(8)
+end
+function s.indtg(e,c)
+	return c:IsRace(RACE_DRAGON) and c:IsLegend()
+end

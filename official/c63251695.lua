17d8382a94af9a1509dd09ac3e2bbeaf755ab5d4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-20 14:06:34 +0300

    A couple more multiple choice effects fixes + updates

--- official/c63251695.lua
+++ official/c63251695.lua
@@ -4,106 +4,108 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Pendulum Summon
 	Pendulum.AddProcedure(c)
-	--Negate an activated effect that targets 1 "Dinomist" card
+	--Negate an activated effect that targets another "Dinomist" card(s) you control
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAIN_SOLVING)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(s.negcon)
+	e1:SetOperation(s.negop)
+	c:RegisterEffect(e1)
+	--Activate 1 of these effects
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_CHAIN_SOLVING)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetCondition(s.negcon)
-	e2:SetOperation(s.negop)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetCondition(s.effcon)
+	e2:SetCost(s.effcost)
+	e2:SetTarget(s.efftg)
+	e2:SetOperation(s.effop)
 	c:RegisterEffect(e2)
-	--Tribute 1 "Dinomist" and apply 1 effect
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_TODECK)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_DAMAGE_STEP_END)
-	e3:SetCondition(s.effcon)
-	e3:SetCost(s.effcost)
-	e3:SetTarget(s.efftg)
-	e3:SetOperation(s.effop)
-	c:RegisterEffect(e3)
 end
 s.listed_series={SET_DINOMIST}
-function s.tfilter(c,tp)
-	return c:IsFaceup() and c:IsSetCard(SET_DINOMIST) and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+function s.negconfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(SET_DINOMIST) and c:IsControler(tp) and c:IsOnField()
 end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not (re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and not c:HasFlagEffect(id)) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return e:GetHandler():GetFlagEffect(id)==0 and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)
-		and g and g:IsExists(s.tfilter,1,e:GetHandler(),tp) and Duel.IsChainDisablable(ev)
+	return g and g:IsExists(s.negconfilter,1,c,tp) and Duel.IsChainDisablable(ev)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
-		if Duel.NegateEffect(ev) then
-			Duel.BreakEffect()
-			Duel.Destroy(e:GetHandler(),REASON_EFFECT)
-		end
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,1)) then return end
+	local c=e:GetHandler()
+	c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+	if Duel.NegateEffect(ev) then
+		Duel.BreakEffect()
+		Duel.Destroy(c,REASON_EFFECT)
 	end
 end
 function s.effcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler()==Duel.GetAttacker() and e:GetHandler():IsRelateToBattle()
+	local c=e:GetHandler()
+	return Duel.GetAttacker()==c and c:IsRelateToBattle()
 end
 function s.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsSetCard,1,false,nil,e:GetHandler(),SET_DINOMIST) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsSetCard,1,false,nil,c,SET_DINOMIST) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g=Duel.SelectReleaseGroupCost(tp,Card.IsSetCard,1,1,false,nil,e:GetHandler(),SET_DINOMIST)
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsSetCard,1,1,false,nil,c,SET_DINOMIST)
 	Duel.Release(g,REASON_COST)
 end
 function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=e:GetHandler():CanChainAttack(0,true)
 	local b2=Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD|LOCATION_HAND,1,nil)
 	if chk==0 then return b1 or b2 end
-	local opt=Duel.SelectEffect(tp,
-		{b1,aux.Stringid(id,1)},
-		{b2,aux.Stringid(id,2)})
-	e:SetLabel(opt)
-	if opt==2 then
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,2)},
+		{b2,aux.Stringid(id,3)})
+	e:SetLabel(op)
+	if op==1 then
+		e:SetCategory(0)
+	else
+		e:SetCategory(CATEGORY_TODECK)
 		Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,1-tp,LOCATION_ONFIELD|LOCATION_HAND)
 	end
 end
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if e:GetLabel()==1 and c:IsRelateToBattle() then
+	if e:GetLabel()==1 then
+		if not (c:IsFaceup() and c:IsRelateToEffect(e) and c:IsRelateToBattle()) then return end
 		Duel.ChainAttack()
+		--The consecutive attack must be on a monster
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE|PHASE_DAMAGE_CAL)
 		c:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
+		--Inflicts piercing battle damage
+		local e2=e1:Clone()
 		e2:SetCode(EFFECT_PIERCE)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE|PHASE_DAMAGE_CAL)
 		c:RegisterEffect(e2)
 	else
-		local g1=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_HAND,nil)
-		local g2=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)
-		local b1=#g1>0
-		local b2=#g2>0
+		local hg=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_HAND,nil)
+		local fg=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)
+		local b1=#hg>0
+		local b2=#fg>0
 		if not (b1 or b2) then return end
-		local opt=Duel.SelectEffect(tp,
-			{b1,aux.Stringid(id,3)},
-			{b2,aux.Stringid(id,4)})
+		local op=Duel.SelectEffect(tp,
+			{b1,aux.Stringid(id,4)},
+			{b2,aux.Stringid(id,5)})
 		local sg=nil
-		if opt==1 then
-			sg=g1:RandomSelect(tp,1)
+		if op==1 then
+			sg=hg:RandomSelect(tp,1)
 		else
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-			sg=g2:Select(tp,1,1,nil)
+			sg=fg:Select(tp,1,1,nil)
+			Duel.HintSelection(sg,true)
 		end
-		Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
-		if sg:GetFirst():IsLocation(LOCATION_DECK) and c:IsRelateToEffect(e) and c:IsFaceup() then
+		if Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and sg:GetFirst():IsLocation(LOCATION_DECK|LOCATION_EXTRA)
+			and c:IsRelateToEffect(e) and c:IsFaceup() then
 			Duel.BreakEffect()
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(100)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
-			c:RegisterEffect(e1)
+			--Gains 100 ATK
+			c:UpdateAttack(100)
 		end
 	end
 end
--- official/c86686671.lua
+++ official/c86686671.lua
@@ -2,20 +2,18 @@
 --Cyber Repair Plant
 local s,id=GetID()
 function s.initial_effect(c)
-	--Search 1 LIGHT <achine monster and/or Shuffle 1 LIGHT Machine monster into the Deck
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function(e,tp) return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,CARD_CYBER_DRAGON) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_names={CARD_CYBER_DRAGON}
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,CARD_CYBER_DRAGON)
-end
 function s.thfilter(c)
 	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToHand()
 end
@@ -23,27 +21,24 @@ function s.tdfilter(c)
 	return c:IsRace(RACE_MACHINE) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToDeck()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then
-		if e:GetLabel()~=0 then
-			return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdfilter(chkc)
-		else return false end
-	end
+	if chkc then return e:GetLabel()~=0 and chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdfilter(chkc) end
 	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
 	local b2=Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil)
 	local b3=b1 and b2 and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,3,nil,CARD_CYBER_DRAGON)
 	if chk==0 then return b1 or b2 end
 	local op=Duel.SelectEffect(tp,
-		{b1,aux.Stringid(id,0)},
-		{b2,aux.Stringid(id,1)},
-		{b3,aux.Stringid(id,2)})
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)},
+		{b3,aux.Stringid(id,3)})
 	e:SetLabel(op)
 	local cat=0
-	if op&1>0 then
-		cat=cat|CATEGORY_TOHAND+CATEGORY_SEARCH
+	if op==1 or op==3 then
+		cat=CATEGORY_TOHAND+CATEGORY_SEARCH
 		e:SetCategory(cat)
+		e:SetProperty(0)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 	end
-	if op&2>0 then
+	if op==2 or op==3 then
 		cat=cat|CATEGORY_TODECK
 		e:SetCategory(cat)
 		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -55,16 +50,18 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local op=e:GetLabel()
 	local breakeff=false
-	if op&1>0 then
+	if op==1 or op==3 then
+		--Add 1 LIGHT Machine monster from your Deck to your hand
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if #g>0 then
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,g)
+			breakeff=true
 		end
-		breakeff=true
 	end
-	if op&2>0 then
+	if op==2 or op==3 then
+		--Shuffle 1 LIGHT Machine monster from your GY into your Deck
 		local tc=Duel.GetFirstTarget()
 		if tc:IsRelateToEffect(e) then
 			if breakeff then Duel.BreakEffect() end

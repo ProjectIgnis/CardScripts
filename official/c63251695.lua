ef436a73380c04061cda53a58fa6f1bccde8d2a4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-20 13:58:20 +0300

    conflict 2

--- official/c63251695.lua
+++ official/c63251695.lua
@@ -4,108 +4,106 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Pendulum Summon
 	Pendulum.AddProcedure(c)
-	--Negate an activated effect that targets another "Dinomist" card(s) you control
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAIN_SOLVING)
-	e1:SetRange(LOCATION_PZONE)
-	e1:SetCondition(s.negcon)
-	e1:SetOperation(s.negop)
-	c:RegisterEffect(e1)
-	--Activate 1 of these effects
+	--Negate an activated effect that targets 1 "Dinomist" card
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_DAMAGE_STEP_END)
-	e2:SetCondition(s.effcon)
-	e2:SetCost(s.effcost)
-	e2:SetTarget(s.efftg)
-	e2:SetOperation(s.effop)
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAIN_SOLVING)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCondition(s.negcon)
+	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
+	--Tribute 1 "Dinomist" and apply 1 effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TODECK)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DAMAGE_STEP_END)
+	e3:SetCondition(s.effcon)
+	e3:SetCost(s.effcost)
+	e3:SetTarget(s.efftg)
+	e3:SetOperation(s.effop)
+	c:RegisterEffect(e3)
 end
 s.listed_series={SET_DINOMIST}
-function s.negconfilter(c,tp)
-	return c:IsFaceup() and c:IsSetCard(SET_DINOMIST) and c:IsControler(tp) and c:IsOnField()
+function s.tfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(SET_DINOMIST) and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
 end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not (re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and not c:HasFlagEffect(id)) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	return g and g:IsExists(s.negconfilter,1,c,tp) and Duel.IsChainDisablable(ev)
+	return e:GetHandler():GetFlagEffect(id)==0 and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)
+		and g and g:IsExists(s.tfilter,1,e:GetHandler(),tp) and Duel.IsChainDisablable(ev)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.SelectYesNo(tp,aux.Stringid(id,1)) then return end
-	local c=e:GetHandler()
-	c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
-	if Duel.NegateEffect(ev) then
-		Duel.BreakEffect()
-		Duel.Destroy(c,REASON_EFFECT)
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+		if Duel.NegateEffect(ev) then
+			Duel.BreakEffect()
+			Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+		end
 	end
 end
 function s.effcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return Duel.GetAttacker()==c and c:IsRelateToBattle()
+	return e:GetHandler()==Duel.GetAttacker() and e:GetHandler():IsRelateToBattle()
 end
 function s.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsSetCard,1,false,nil,c,SET_DINOMIST) end
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsSetCard,1,false,nil,e:GetHandler(),SET_DINOMIST) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g=Duel.SelectReleaseGroupCost(tp,Card.IsSetCard,1,1,false,nil,c,SET_DINOMIST)
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsSetCard,1,1,false,nil,e:GetHandler(),SET_DINOMIST)
 	Duel.Release(g,REASON_COST)
 end
 function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=e:GetHandler():CanChainAttack(0,true)
 	local b2=Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD|LOCATION_HAND,1,nil)
 	if chk==0 then return b1 or b2 end
-	local op=Duel.SelectEffect(tp,
-		{b1,aux.Stringid(id,2)},
-		{b2,aux.Stringid(id,3)})
-	e:SetLabel(op)
-	if op==1 then
-		e:SetCategory(0)
-	else
-		e:SetCategory(CATEGORY_TODECK)
+	local opt=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	e:SetLabel(opt)
+	if opt==2 then
 		Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,1-tp,LOCATION_ONFIELD|LOCATION_HAND)
 	end
 end
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if e:GetLabel()==1 then
-		if not (c:IsFaceup() and c:IsRelateToEffect(e) and c:IsRelateToBattle()) then return end
+	if e:GetLabel()==1 and c:IsRelateToBattle() then
 		Duel.ChainAttack()
-		--The consecutive attack must be on a monster
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE|PHASE_DAMAGE_CAL)
 		c:RegisterEffect(e1)
-		--Inflicts piercing battle damage
-		local e2=e1:Clone()
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_PIERCE)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE|PHASE_DAMAGE_CAL)
 		c:RegisterEffect(e2)
 	else
-		local hg=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_HAND,nil)
-		local fg=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)
-		local b1=#hg>0
-		local b2=#fg>0
+		local g1=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_HAND,nil)
+		local g2=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,nil)
+		local b1=#g1>0
+		local b2=#g2>0
 		if not (b1 or b2) then return end
-		local op=Duel.SelectEffect(tp,
-			{b1,aux.Stringid(id,4)},
-			{b2,aux.Stringid(id,5)})
+		local opt=Duel.SelectEffect(tp,
+			{b1,aux.Stringid(id,3)},
+			{b2,aux.Stringid(id,4)})
 		local sg=nil
-		if op==1 then
-			sg=hg:RandomSelect(tp,1)
+		if opt==1 then
+			sg=g1:RandomSelect(tp,1)
 		else
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-			sg=fg:Select(tp,1,1,nil)
-			Duel.HintSelection(sg,true)
+			sg=g2:Select(tp,1,1,nil)
 		end
-		if Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and sg:GetFirst():IsLocation(LOCATION_DECK|LOCATION_EXTRA)
-			and c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+		if sg:GetFirst():IsLocation(LOCATION_DECK) and c:IsRelateToEffect(e) and c:IsFaceup() then
 			Duel.BreakEffect()
-			--Gains 100 ATK
-			c:UpdateAttack(100)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(100)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+			c:RegisterEffect(e1)
 		end
 	end
 end

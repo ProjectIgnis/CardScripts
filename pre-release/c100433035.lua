07295042a15000285686e7ddaa2640f5d062e6c9
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-21 10:08:54 +0300

    "Angelic-Demonic Selector - "Valmonica"" fix
    Wrong category in the control switching effect.

--- pre-release/c100433035.lua
+++ pre-release/c100433035.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	--Take control of 1 opponent's monster
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_CONTROL)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_CUSTOM+id)

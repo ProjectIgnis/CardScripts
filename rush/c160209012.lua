285df13d015a7014c2a0698ddb271bbd2be19fd5
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-08-23 13:23:23 +0200

    added Accel Wonder Light

---
+++ rush/c160209012.lua
@@ -0,0 +1,43 @@
+--アクセルワンダー・ライト
+--Accel Wonder Light
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Excavate the top 3 cards and send 1 to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5 end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.filter(c)
+	return not c:IsTrap()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<4 then return end
+	Duel.ConfirmDecktop(tp,4)
+	local g=Duel.GetDecktopGroup(tp,4)
+	if g:IsExists(s.filter,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local sg=g:FilterSelect(tp,s.filter,1,2,nil)
+		Duel.SendtoGrave(sg,REASON_EFFECT)
+		Duel.BreakEffect()
+	end
+	Duel.ShuffleDeck(tp)
+end
+

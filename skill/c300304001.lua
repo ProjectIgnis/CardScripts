170fb20e6314a9d2b85c29301bcaedd882b851da
Naim <naimsantos45@gmail.com>
2023-08-23 10:45:21 -0300

    Fix "Here Goes Something!"
    It should only apply its effects to "Elemental Hero" monsters
    Fxed a bug where the player would be continuously prompted to activate it

--- skill/c300304001.lua
+++ skill/c300304001.lua
@@ -12,27 +12,28 @@ function s.initial_effect(c)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 end
-s.listed_series={SET_HERO}
+s.listed_series={SET_ELEMENTAL_HERO}
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	--Battle Start
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetCountLimit(1)
 	e1:SetCondition(s.flipcon)
 	e1:SetOperation(s.flipop)
 	Duel.RegisterEffect(e1,tp)
 end
 function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_HERO) and c:IsMonster()
+	return c:IsFaceup() and c:IsSetCard(SET_ELEMENTAL_HERO) and c:IsMonster()
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():GetFlagEffect(id)>0 then return false end
-	return Duel.GetTurnPlayer()==tp and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1
+	return Duel.IsTurnPlayer(tp) and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1
 		and Duel.GetFlagEffect(ep,id)==0
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-        if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	--OPD register
@@ -45,7 +46,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_BATTLE_CONFIRM)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 	e1:SetCondition(s.condition)
 	e1:SetOperation(s.operation)
 	tc:RegisterEffect(e1)
@@ -54,16 +55,16 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetCode(EFFECT_ATTACK_ALL)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e2:SetValue(1)
-	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 	tc:RegisterEffect(e2)
 	--Cannot Special Summon monsters for the rest of this turn
 	local e3=Effect.CreateEffect(e:GetHandler())
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetTargetRange(1,0)
-	e3:SetReset(RESET_PHASE+PHASE_END)
+	e3:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e3,tp)
 end
 function s.condition(e)
@@ -72,5 +73,10 @@ function s.condition(e)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetHandler()
-	tc:UpdateAttack(800,RESET_PHASE+PHASE_DAMAGE_CAL)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE_CAL)
+	e1:SetValue(800)
+	tc:RegisterEffect(e1)
 end

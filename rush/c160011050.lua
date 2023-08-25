2093eb16cdcb5e48bde645bed73e2637cbaca22e
Naim <naimsantos45@gmail.com>
2023-08-25 06:11:57 -0300

    Road Arms - Sevens Lance

--- rush/c160011050.lua
+++ rush/c160011050.lua
@@ -18,9 +18,9 @@ function s.initial_effect(c)
 	e2:SetValue(s.efilter)
 	c:RegisterEffect(e2)
 end
-s.listed_names={160301001,160011050}
+s.listed_names={id,CARD_SEVENS_ROAD_MAGICIAN}
 function s.eqfilter(c)
-	return c:IsFaceup() and c:IsCode(160301001) and not c:IsMaximumModeSide()
+	return c:IsFaceup() and c:IsCode(CARD_SEVENS_ROAD_MAGICIAN) and not c:IsMaximumModeSide()
 end
 function s.eqlimit(e,c)
 	return c:IsFaceup()
@@ -29,7 +29,7 @@ function s.value(e,c)
 	return Duel.GetMatchingGroup(Card.IsMonster,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil):GetClassCount(Card.GetAttribute)*400
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,160011050),tp,LOCATION_ONFIELD,0,1,nil)
+	return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,id),tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.efilter(e,te)
 	return te:IsTrapEffect() and te:GetOwnerPlayer()~=e:GetHandlerPlayer()

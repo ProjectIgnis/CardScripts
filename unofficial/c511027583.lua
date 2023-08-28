c92e9c03d965249a0a8e59988605f5227da038fb
When <104731682+idk-what-to-name-here@users.noreply.github.com>
2023-08-28 03:52:48 +0300

    Update c511027583.lua

--- unofficial/c511027583.lua
+++ unofficial/c511027583.lua
@@ -6,6 +6,7 @@ function s.initial_effect(c)
 	--Double Level
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_LVCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -17,18 +18,17 @@ function s.cfilter(c)
 	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+        return Duel.IsExistingMatchingCard(aux.FaceupFilter(s.cfilter),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
+	    local e1=Effect.CreateEffect(c)
 	    e1:SetType(EFFECT_TYPE_SINGLE)
-	    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-        e1:SetRange(LOCATION_MZONE)
+            e1:SetRange(LOCATION_MZONE)
 	    e1:SetCode(EFFECT_CHANGE_LEVEL)
 	    e1:SetValue(c:GetLevel()*2)
-	    e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	    e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
 	    c:RegisterEffect(e1)
 	end
 end

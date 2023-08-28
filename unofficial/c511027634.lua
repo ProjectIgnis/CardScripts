f514974d6170c1c199960068bd3d5345bafc37cf
Beetron-1 Beetletop <113409293+Beetletop@users.noreply.github.com>
2023-08-28 13:20:07 +0300

    Add files via upload

---
+++ unofficial/c511027634.lua
@@ -0,0 +1,31 @@
+--マリスボラス・ナイフ
+--Malicevorous Knife
+local s,id=GetID()
+function s.initial_effect(c)
+	--summon success
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.sumtg)
+	e1:SetOperation(s.sumop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x8b}
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x8b) and c:IsCanBeSpecialSummoned(e,152,tp,false,false)
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,152,tp,tp,false,false,POS_FACEUP)
+	end
+end

b5bb242252bea7cf98317c1cc22f786d9c61a7ad
ClaireStanfield <51684310+ClaireStanfield@users.noreply.github.com>
2023-08-30 20:22:17 -0400

    Add Fortune Lady Earth (Anime) script (#1076)
    * Add Fortune Lady Earth (Anime)

---
+++ unofficial/c511027035.lua
@@ -0,0 +1,74 @@
+--フォーチュンレディ・アーシー (Anime)
+--Fortune Lady Earth (Anime)
+Duel.EnableUnofficialProc(PROC_STATS_CHANGED)
+local s,id=GetID()
+function s.initial_effect(c)
+	--This card's ATK/DEF become its Level x 400
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(s.value)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_DEFENSE)
+	c:RegisterEffect(e2)
+	--Increase this card's Level by 1 (max. 12)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetCondition(s.lvcon)
+	e3:SetOperation(s.lvop)
+	c:RegisterEffect(e3)
+	--Inflict 400 damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_DAMAGE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetCode(511002524)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.damcon)
+	e4:SetTarget(s.damtg)
+	e4:SetOperation(s.damop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={SET_FORTUNE_LADY}
+function s.value(e,c)
+	return c:GetLevel()*400
+end
+function s.lvcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(tp) and e:GetHandler():IsLevelBelow(11)
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) or c:IsLevelAbove(12) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,tp)
+	local val=0
+	if c:GetFlagEffect(584)>0 then val=c:GetFlagEffectLabel(584) end
+	return c:GetLevel()>val and c:IsFaceup() and c:IsSetCard(SET_FORTUNE_LADY) and c:IsControler(tp)
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(400)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,400)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end

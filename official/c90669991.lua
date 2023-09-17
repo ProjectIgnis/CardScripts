60841442e96ed2e7b8ed84d5b925c8c656d2f5ae
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-09-17 20:48:51 +0300

    "Pineapple Blast" update
    Updated its script to accurately reflect its more recent EN text.

--- official/c90669991.lua
+++ official/c90669991.lua
@@ -1,8 +1,10 @@
 --パイナップル爆弾
+--Pineapple Blast
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -16,16 +18,16 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
 	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)-Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,ct,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
-	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)-Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
-	if ct>0 then
-		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_DESTROY)
-		local dg=g:Select(1-tp,ct,ct,nil)
-		Duel.Destroy(dg,REASON_EFFECT)
-	end
+	local ct=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
+	if Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)-ct<=0 then return end
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(id,1))
+	local dg=g:Select(1-tp,ct,ct,nil)
+	Duel.HintSelection(dg,true)
+	Duel.Destroy(g-dg,REASON_EFFECT)
 end

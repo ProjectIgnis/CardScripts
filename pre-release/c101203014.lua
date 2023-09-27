06e418c67488e556de787e85e34776e690babaec
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-09-27 16:07:21 +0300

    Added new card's script

---
+++ pre-release/c101203014.lua
@@ -0,0 +1,81 @@
+--幻魔の召喚神
+--Phantasmal Summoning Beast
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	--Search 1 "Uria, Lord of Searing Flames", "Hamon, Lord of Striking Thunder", or "Raviel, Lord of Phantasms"
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(aux.selfreleasecost)
+	e1:SetTarget(s.mzthtg)
+	e1:SetOperation(s.mzthop)
+	c:RegisterEffect(e1)
+	--Search 1 "Dimension Fusion Destruction"
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCost(aux.selfbanishcost)
+	e2:SetTarget(s.gythtg)
+	e2:SetOperation(s.gythop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={6007213,32491822,69890967,89190953}
+function s.mzthfilter(c)
+	return c:IsCode(6007213,32491822,69890967) and c:IsAbleToHand()
+end
+function s.mzthtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.mzthfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spfilter(c,e,tp)
+	return c:IsLevel(10) and c:IsRace(RACE_PYRO|RACE_THUNDER|RACE_FIEND) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+		and c:IsDefense(c:GetAttack()) and c:IsAttackAbove(0) and c:IsDefenseAbove(0)
+end
+function s.mzthop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.mzthfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
+	if #g==0 or Duel.SendtoHand(g,nil,REASON_EFFECT)==0 then return end
+	Duel.ConfirmCards(1-tp,g)
+	Duel.ShuffleHand(tp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
+		if not sc then return end
+		Duel.BreakEffect()
+		if Duel.SpecialSummonStep(sc,0,tp,tp,true,false,POS_FACEUP) then
+			--Cannot attack directly this turn
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetDescription(3207)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+			e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			sc:RegisterEffect(e1)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+function s.gythfilter(c)
+	return c:IsCode(89190953) and c:IsAbleToHand()
+end
+function s.gythtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.gythfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.gythop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.gythfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

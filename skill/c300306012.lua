e966eecb67148ccdc9d4e48c25f9754bb17d09c5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-09-29 19:32:11 +0300

    "Order of the Queen" fix
    Prevent a script error in some cases + general simplifications and updates to the whole script.

--- skill/c300306012.lua
+++ skill/c300306012.lua
@@ -5,53 +5,43 @@ function s.initial_effect(c)
 	--Activate
 	aux.AddSkillProcedure(c,2,false,nil,nil)
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_STARTUP)
 	e1:SetCountLimit(1)
 	e1:SetRange(0x5f)
-	e1:SetLabelObject(e)
-	e1:SetLabel(0)
-	e1:SetOperation(s.op)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={SET_AMAZONESS}
 s.listed_names={15951532}
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+s.listed_series={SET_AMAZONESS}
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_BATTLED)
-	e1:SetRange(0x5f)
-	e1:SetCondition(s.condition)
-	e1:SetOperation(s.operation)
+	e1:SetCondition(s.negcon)
+	e1:SetOperation(s.negop)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.qfilter(c)
-	return c:IsCode(15951532) and c:IsFaceup()
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local ac=Duel.GetAttacker()
+	local dc=Duel.GetAttackTarget()
+	return ac and dc and ac:IsControler(tp) and ac:IsSetCard(SET_AMAZONESS) and dc:IsStatus(STATUS_BATTLE_DESTROYED) and dc:IsType(TYPE_EFFECT)
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,15951532),tp,LOCATION_ONFIELD,0,1,nil)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.qfilter,tp,LOCATION_MZONE,0,1,nil)
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-	local p=e:GetHandler():GetControler()
-	local tc=nil
-	if a:GetControler()==p and a:IsSetCard(SET_AMAZONESS) and d:IsStatus(STATUS_BATTLE_DESTROYED) then
-		tc=d
-	elseif d:GetControler()==p or not tc or d==nil then
-		return false
-	end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local dc=Duel.GetAttackTarget()
+	--Negate its effects
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EFFECT_DISABLE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_LEAVE-RESET_TOGRAVE)
-	tc:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD&~(RESET_LEAVE|RESET_TOGRAVE))
+	dc:RegisterEffect(e1)
+	local e2=e1:Clone()
 	e2:SetCode(EFFECT_DISABLE_EFFECT)
-	e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_LEAVE-RESET_TOGRAVE)
-	tc:RegisterEffect(e2)
+	e2:SetValue(RESET_TURN_SET)
+	dc:RegisterEffect(e2)
 end

a05ff87abfe86cff81e8e7804028d270af34c360
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-04 12:49:54 +0300

    "T.G. Tank Grub" fix
    Shouldn't miss timing.

--- official/c74627016.lua
+++ official/c74627016.lua
@@ -3,12 +3,12 @@
 --Scripted by Logical Nonsense
 local s,id=GetID()
 function s.initial_effect(c)
-	--Can be used a non-Tuner for the Synchro Summon of a "T.G." monster
+	--Can be treated as a non-Tuner for the Synchro Summon of a "T.G." monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_NONTUNER)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(function(e,sc) return sc:IsSetCard(SET_TG) end)
 	c:RegisterEffect(e1)
 	--Special Summon 1 "T.G. Token"
@@ -16,31 +16,29 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_BE_MATERIAL)
 	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.tcon)
-	e2:SetTarget(s.ttg)
-	e2:SetOperation(s.top)
+	e2:SetCondition(s.tkcon)
+	e2:SetTarget(s.tktg)
+	e2:SetOperation(s.tkop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={74627017} --"T.G. Token"
 s.listed_series={SET_TG}
-	--If monster was sent to GY for synchro summon of "T.G." monster
-function s.tcon(e,tp,eg,ep,ev,re,r,rp)
+function s.tkcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO and c:GetReasonCard():IsSetCard(SET_TG)
 end
-	--Activation legality
-function s.ttg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.tktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_TG,TYPES_TOKEN,0,0,1,RACE_MACHINE,ATTRIBUTE_EARTH) end
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_TG,TYPES_TOKEN,0,0,1,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_ATTACK) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
 end
-	--Performing the effect of special summoning token
-function s.top(e,tp,eg,ep,ev,re,r,rp)
+function s.tkop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_TG,TYPES_TOKEN,0,0,1,RACE_MACHINE,ATTRIBUTE_EARTH) then
+	if Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_TG,TYPES_TOKEN,0,0,1,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_ATTACK) then
 		local token=Duel.CreateToken(tp,id+1)
 		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP_ATTACK)
 	end

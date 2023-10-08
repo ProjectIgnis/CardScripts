533904e3f77618bee8e1269b57626514a140097d
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-10-08 13:31:09 +0200

    added Gias x Gias

---
+++ rush/c160209032.lua
@@ -0,0 +1,66 @@
+--ギアス×ギアス
+--Gias x Gias
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Make 1 of opponent's monsters lose 300 ATK per Spells in their GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:GetAttack()>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,-1500)
+end
+function s.filter2(c,tc)
+	return c:IsLevel(tc:GetLevel()) and c:IsRace(tc:GetRace()) and c:GetEffectCount(EFFECT_EXTRA_ATTACK)==0
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+	if Duel.SendtoGrave(g,REASON_COST)<1 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	if #g==0 then return end
+	local tc=g:GetFirst()
+	Duel.HintSelection(g,true)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(-1500)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+	tc:RegisterEffectRush(e1)
+	if Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil,tc) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local sc=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_MZONE,0,1,1,nil,tc):GetFirst()
+		if sc then
+			Duel.HintSelection(sc,true)
+			--Piercing
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(3201)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_CLIENT_HINT)
+			e1:SetCode(EFFECT_EXTRA_ATTACK)
+			e1:SetValue(1)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			sc:RegisterEffectRush(e1)
+		end
+	end
+end

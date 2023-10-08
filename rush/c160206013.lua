1840c5e4dda2d70db3f4a5391e870b03d21e8b05
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-09 01:37:32 +0300

    "The Origin Stone of Legend" fix
    Shouldn't be able to select to Special Summon a monster if the player has no empty Monster Zone.

--- rush/c160206013.lua
+++ rush/c160206013.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(function(e)return e:GetHandler():IsStatus(STATUS_SUMMON_TURN)end)
+	e1:SetCondition(function(e) return e:GetHandler():IsStatus(STATUS_SUMMON_TURN) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
@@ -20,13 +20,13 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.spfilter(c,e,tp)
-	return c:IsMonster() and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_NORMAL) and c:IsLegend() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return cc:IsRace(RACE_DRAGON) and c:IsType(TYPE_NORMAL) and c:IsLegend() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<3 then return end
 	Duel.ConfirmDecktop(tp,4)
 	local g=Duel.GetDecktopGroup(tp,4)
-	if g:IsExists(s.spfilter,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and g:IsExists(s.spfilter,1,nil,e,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local tg=g:FilterSelect(tp,s.spfilter,1,1,nil,e,tp)
 		if #tg>0 then

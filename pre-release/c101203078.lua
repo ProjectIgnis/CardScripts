d4aea3232345c209dccaeab64e8aa1ef3c78ef1d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-09 10:50:26 +0300

    Added new card's script

---
+++ pre-release/c101203078.lua
@@ -0,0 +1,61 @@
+--嗤う黒山羊
+--Rumble Teufel
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Neither player can Special Summon monsters with the declared name, except from the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMINGS_CHECK_MONSTER)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.decltg)
+	e1:SetOperation(s.sumlimop)
+	c:RegisterEffect(e1)
+	--Neither player can activate the effects on the field of monsters with the declared name
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMINGS_CHECK_MONSTER)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.decltg)
+	e2:SetOperation(s.efflimop)
+	c:RegisterEffect(e2)
+end
+function s.decltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
+	local code=Duel.AnnounceCard(tp,TYPE_MONSTER,OPCODE_ISTYPE)
+	Duel.SetTargetParam(code)
+	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
+end
+function s.sumlimop(e,tp,eg,ep,ev,re,r,rp)
+	local code=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	--Prevent Special Summons
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,1)
+	e1:SetTarget(function(_,c) return c:IsOriginalCodeRule(code) and not c:IsLocation(LOCATION_GRAVE) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.efflimop(e,tp,eg,ep,ev,re,r,rp)
+	local code=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	--Prevent effect activations
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,3))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(function(_e,_re,_tp) return _re:GetHandler():IsOriginalCodeRule(code) and _re:IsMonsterEffect() and _re:GetActivateLocation()==LOCATION_MZONE end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end

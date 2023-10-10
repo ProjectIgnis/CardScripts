f198d456147d32cc5767189fde9ba872fd79d4c5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-10 10:20:54 +0300

    "Goblin Rider" cards fix
    Fixed an issue where they would be able to proceed with their effects even if there were no materials to detach on resolution. ("Duel.RemoveOverlayCard" returns an int, not a boolean.)

--- pre-release/c101203075.lua
+++ pre-release/c101203075.lua
@@ -71,7 +71,7 @@ function s.dttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
 function s.dtop(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.RemoveOverlayCard(tp,1,1,1,1,REASON_EFFECT) then return end
+	if Duel.RemoveOverlayCard(tp,1,1,1,1,REASON_EFFECT)==0 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		tc:NegateEffects(e:GetHandler(),RESET_PHASE|PHASE_END)

91d2d417c349dd176879a4fa8900306464a0a437
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-11 16:25:44 +0300

    Added new cards' scripts

---
+++ pre-release/c101203015.lua
@@ -0,0 +1,86 @@
+--ゴーティスの朧キーフ
+--Kiif, Haze of the Ghoti
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon itself from the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function() return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_FISH),0,LOCATION_MZONE,LOCATION_MZONE,1,nil) end)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Banish this card and 1 monster your opponent controls and Special Summon 1 Level 6 or lower banished Fish monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.rmvtg)
+	e2:SetOperation(s.rmvop)
+	c:RegisterEffect(e2)
+	--Special Summon itself after being banished
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(function(e) return Duel.GetTurnCount()==e:GetHandler():GetTurnID()+1 end)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.rmvfilter(c,eg)
+	return c:IsAbleToRemove() and eg:IsContains(c)
+end
+function s.fishspfilter(c,e,tp)
+	return c:IsFaceup() and c:IsRace(RACE_FISH) and c:IsLevelBelow(6)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.rmvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemove() and Duel.GetMZoneCount(tp,c)>0
+		and Duel.IsExistingTarget(s.rmvfilter,tp,0,LOCATION_MZONE,1,nil,eg)
+		and Duel.IsExistingTarget(s.fishspfilter,tp,LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rg=Duel.SelectTarget(tp,s.rmvfilter,tp,0,LOCATION_MZONE,1,1,nil,eg)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=Duel.SelectTarget(tp,s.fishspfilter,tp,LOCATION_REMOVED,0,1,1,nil,e,tp)
+	rg:AddCard(c)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,rg,2,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,1,tp,0)
+end
+function s.rmvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetTargetCards(e)
+	if #g==0 then return end
+	local rg,sg=g:Split(Card.IsControler,nil,1-tp)
+	if #rg==0 then return end
+	rg:AddCard(c)
+	if Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)==2 and #sg>0 then
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

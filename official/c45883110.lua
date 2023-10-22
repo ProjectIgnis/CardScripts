4459c1d49615c38e938217f53e0c54df144e0ed4
Naim <naimsantos45@gmail.com>
2023-10-22 18:29:24 -0300

    Update "Guiding Quem, the Virtuous"
    It was calling an incorrect string in its first effect

--- official/c45883110.lua
+++ official/c45883110.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Send 1 "Fallen of Albaz" or 1 card that mentions it from your Deck to GY
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)

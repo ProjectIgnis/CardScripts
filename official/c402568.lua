1c080eacb4cc5e149632880d0200138ef4feacce
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-23 13:02:14 +0300

    "Wattgiraffe" script update
    The restriction effect will now display a hint on the opponent.

--- official/c402568.lua
+++ official/c402568.lua
@@ -2,30 +2,29 @@
 --Wattgiraffe
 local s,id=GetID()
 function s.initial_effect(c)
-	--direct attack
+	--Can attack directly
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_DIRECT_ATTACK)
 	c:RegisterEffect(e1)
-	--act limit
+	--Make your opponent unable to activate cards or effects that turn
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_BATTLE_DAMAGE)
-	e2:SetCondition(s.condition)
+	e2:SetCondition(function(e,tp,eg,ep) return ep==1-tp and Duel.GetAttackTarget()==nil end)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and Duel.GetAttackTarget()==nil
-end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Cannot activate cards or effects until the end of this turn
 	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetTargetRange(0,1)
 	e1:SetValue(1)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end

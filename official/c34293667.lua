834a87a261e768db8335473ccda111a9d8a2f466
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-30 12:47:49 +0200

    "Ice Barrier" fixes
    - Now checks that the monster is face-up and that at least 1 of the 3 effects can be applied to it to activate the card.
    - Now checks that the opponent is still the controller of card and that it's still the same face-up card on resolution before applying the effects.
    - Added hint timings to its GY effect.
    - General script polishing and modernisation.

--- official/c34293667.lua
+++ official/c34293667.lua
@@ -5,58 +5,54 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--Send to GY
+	--Send 1 Level 5 or higher WATER monster from your Deck to the GY
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
+	e2:SetHintTiming(0,TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
 	e2:SetCountLimit(1,id)
-	e2:SetCost(aux.bfgcost)
+	e2:SetCost(aux.selfbanishcost)
 	e2:SetTarget(s.tgtg)
 	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local at=Duel.GetAttackTarget()
-	return Duel.GetAttacker():IsControler(1-tp) or (at and at:IsControler(1-tp))
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local _,bc=Duel.GetBattleMonster(tp)
+	if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttackAbove(1) or bc:IsNegatableMonster()
+		or not bc:IsHasEffect(EFFECT_CANNOT_CHANGE_POSITION)) end
+	Duel.SetTargetCard(bc)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,bc,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local bc=Duel.GetFirstTarget()
+	if not (bc:IsRelateToEffect(e) and bc:IsFaceup() and bc:IsRelateToBattle() and bc:IsControler(1-tp)) then return end
 	local c=e:GetHandler()
-	local tc=Duel.GetAttacker()
-	if tc:IsControler(tp) then tc=Duel.GetAttackTarget() end
-	if tc and tc:IsFaceup() then
-		--Change attack to 0
-		local e1=Effect.CreateEffect(c)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetValue(0)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		--Negate effect
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e2)
-		local e3=Effect.CreateEffect(c)
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_DISABLE_EFFECT)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e3)
-		--Cannot change position
-		local e4=Effect.CreateEffect(c)
-		e4:SetType(EFFECT_TYPE_SINGLE)
-		e4:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-		e4:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e4)
-	end
+	--Negate its effects
+	bc:NegateEffects(c)
+	--Change its ATK to 0
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(3313)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e1:SetValue(0)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	bc:RegisterEffect(e1)
+	--Cannot change its battle position
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	bc:RegisterEffect(e2)
 end
 function s.tgfilter(c)
 	return c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToGrave()
@@ -70,35 +66,28 @@ function s.thfilter(c)
 	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToHand()
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-	--Waterlock
+	local ct=Duel.IsTurnPlayer(tp) and 2 or 1
+	--Cannot Special Summon monsters, except WATER monsters
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(s.splimit)
-	if Duel.GetTurnPlayer()==tp then
-		e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
-	else
-		e1:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN)
-	end
+	e1:SetTarget(function(_e,_c) return not _c:IsAttribute(ATTRIBUTE_WATER) end)
+	e1:SetReset(RESET_PHASE|PHASE_END|RESET_SELF_TURN,ct)
 	Duel.RegisterEffect(e1,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_GRAVE) then
-		local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,nil)
-		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+	local tgc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if not (tgc and Duel.SendtoGrave(tgc,REASON_EFFECT)>0 and tgc:IsLocation(LOCATION_GRAVE)) then return end
+	local thg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,nil)
+	if #thg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=thg:Select(tp,1,1,nil)
+		if #sg>0 then
 			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-			local sg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
-			if #sg>0 then
-				Duel.SendtoHand(sg,tp,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,sg)
-			end
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
 		end
 	end
-end
-function s.splimit(e,c)
-	return not c:IsAttribute(ATTRIBUTE_WATER)
 end

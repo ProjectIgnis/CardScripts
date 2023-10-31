4a86fdb986c8ad104fda57b255f1bbeaa990e693
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-31 20:40:24 +0200

    "Dingirsu, the Orcust of the Evening Star" update
    General polishing and minor fixes.

--- official/c93854893.lua
+++ official/c93854893.lua
@@ -4,85 +4,81 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Can only be Special Summoned once per turn
+	--You can only Special Summon "Dingirsu, the Orcust of the Evening Star(s)" once per turn
 	c:SetSPSummonOnce(id)
 	--Xyz Summon Procedure
-	Xyz.AddProcedure(c,nil,8,2,s.ovfilter,aux.Stringid(id,0),2)
-	--Send a card to the GY or Attach 1 banished card to this monster
+	Xyz.AddProcedure(c,nil,8,2,s.ovfilter,aux.Stringid(id,0))
+	--Detach 1 material from this card instead of a card(s) you control being destroyed by battle or card effect
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EFFECT_DESTROY_REPLACE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.reptg)
+	e1:SetValue(function(e,_c) return s.repfilter(_c,e:GetHandlerPlayer()) end)
 	c:RegisterEffect(e1)
-	--Destruction replacement
+	--Activate 1 of these effects
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EFFECT_DESTROY_REPLACE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetTarget(s.reptg)
-	e2:SetValue(s.repval)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.efftg)
+	e2:SetOperation(s.effop)
 	c:RegisterEffect(e2)
 end
+s.listed_names={id}
 s.listed_series={SET_ORCUST}
 function s.ovfilter(c,tp,xyzc)
 	return c:IsFaceup() and c:IsType(TYPE_LINK,xyzc,SUMMON_TYPE_XYZ,tp) and c:IsSetCard(SET_ORCUST,xyzc,SUMMON_TYPE_XYZ,tp)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=s.tgtg(e,tp,eg,ep,ev,re,r,rp,0)
-	local b2=s.mattg(e,tp,eg,ep,ev,re,r,rp,0)
-	if chk==0 then return b1 or b2 end
-	local op=Duel.SelectEffect(tp,
-		{b1,aux.Stringid(id,1)},
-		{b2,aux.Stringid(id,2)})
-	e:SetLabel(op)
-	if op==0 then
-		s.tgtg(e,tp,eg,ep,ev,re,r,rp,1)
-	else
-		s.mattg(e,tp,eg,ep,ev,re,r,rp,1)
-	end
-end
-function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,nil) end
-	e:SetCategory(CATEGORY_TOGRAVE)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
-end
-function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsType(TYPE_XYZ)
-		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_MACHINE),tp,LOCATION_REMOVED,0,1,nil) end
-	e:SetCategory(0)
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local op=e:GetLabel()
-	if op==1 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
-		if #g>0 then
-			Duel.SendtoGrave(g,REASON_EFFECT)
-		end
-	elseif op==2 then
-		local c=e:GetHandler()
-		if not c:IsRelateToEffect(e) then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-		local g=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsRace,RACE_MACHINE),tp,LOCATION_REMOVED,0,1,1,nil)
-		Duel.Overlay(c,g)
-	end
-end
 function s.repfilter(c,tp)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
-		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+	return c:IsControler(tp) and c:IsOnField() and c:IsReason(REASON_BATTLE|REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
 end
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local g=c:GetLinkedGroup()
 	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
 	if Duel.SelectEffectYesNo(tp,c,96) then
 		c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
 		return true
 	else return false end
 end
-function s.repval(e,c)
-	return s.repfilter(c,e:GetHandlerPlayer())
+function s.matfilter(c,tp)
+	return c:IsRace(RACE_MACHINE) and c:IsFaceup() and c:IsCanBeXyzMaterial(nil,tp,REASON_EFFECT)
+end
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,nil)
+	local b2=e:GetHandler():IsType(TYPE_XYZ) and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_REMOVED,0,1,nil,tp)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,2)},
+		{b2,aux.Stringid(id,3)})
+	e:SetLabel(op)
+	if op==1 then
+		e:SetCategory(CATEGORY_TOGRAVE)
+		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,1-tp,LOCATION_ONFIELD)
+	elseif op==2 then
+		e:SetCategory(0)
+	end
+end
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		--Send 1 card your opponent controls to the GY
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.SendtoGrave(g,REASON_EFFECT)
+		end
+	elseif op==2 then
+		--Attach 1 of your banished Machine monsters to this card as material
+		local c=e:GetHandler()
+		if not (c:IsRelateToEffect(e) and c:IsType(TYPE_XYZ)) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local g=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_REMOVED,0,1,1,nil,tp)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.Overlay(c,g)
+		end
+	end
 end

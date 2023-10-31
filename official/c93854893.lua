603d04d089674e7a87402f67b8c4e344210e2854
Naim <naimsantos45@gmail.com>
2023-10-31 14:32:17 -0300

    Update "Dingirsu, the Orcust of the Evening Star"
    - Added a description to be shown when the player has to select a card to send to the graveyard
    - Changed the on summon effect to use the Duel.SelectEffect function

--- official/c93854893.lua
+++ official/c93854893.lua
@@ -1,13 +1,14 @@
 --宵星の機神ディンギルス
---Sheorcust Dingirsu
+--Dingirsu, the Orcust of the Evening Star
 --Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
-	c:SetSPSummonOnce(id)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,8,2,s.ovfilter,aux.Stringid(id,0),2)
 	c:EnableReviveLimit()
-	--on summon
+	--Can only be Special Summoned once per turn
+	c:SetSPSummonOnce(id)
+	--Xyz Summon Procedure
+	Xyz.AddProcedure(c,nil,8,2,s.ovfilter,aux.Stringid(id,0),2)
+	--Send a card to the GY or Attach 1 banished card to this monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -15,7 +16,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	--destroy replace
+	--Destruction replacement
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
@@ -24,33 +25,22 @@ function s.initial_effect(c)
 	e2:SetValue(s.repval)
 	c:RegisterEffect(e2)
 end
+s.listed_series={SET_ORCUST}
 function s.ovfilter(c,tp,xyzc)
-	return c:IsFaceup() and c:IsType(TYPE_LINK,xyzc,SUMMON_TYPE_XYZ,tp) and c:IsSetCard(0x11b,xyzc,SUMMON_TYPE_XYZ,tp)
+	return c:IsFaceup() and c:IsType(TYPE_LINK,xyzc,SUMMON_TYPE_XYZ,tp) and c:IsSetCard(SET_ORCUST,xyzc,SUMMON_TYPE_XYZ,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return s.tgtg(e,tp,eg,ep,ev,re,r,rp,0) or s.mattg(e,tp,eg,ep,ev,re,r,rp,0) end
-	local sel
-	if s.tgtg(e,tp,eg,ep,ev,re,r,rp,0) and s.mattg(e,tp,eg,ep,ev,re,r,rp,0) then
-		sel=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-	elseif s.tgtg(e,tp,eg,ep,ev,re,r,rp,0) then
-		sel=0
-		Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
-	else 
-		sel=1
-		Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,2))
-	end
-	e:SetLabel(sel)
-	if sel==0 then
+	local b1=s.tgtg(e,tp,eg,ep,ev,re,r,rp,0)
+	local b2=s.mattg(e,tp,eg,ep,ev,re,r,rp,0)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	e:SetLabel(op)
+	if op==0 then
 		s.tgtg(e,tp,eg,ep,ev,re,r,rp,1)
 	else
 		s.mattg(e,tp,eg,ep,ev,re,r,rp,1)
-	end
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==0 then
-		s.tgop(e,tp,eg,ep,ev,re,r,rp)
-	else
-		s.matop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -58,24 +48,27 @@ function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetCategory(CATEGORY_TOGRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
 end
-function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
-	if #g>0 then
-		Duel.SendtoGrave(g,REASON_EFFECT)
-	end
-end
 function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsType(TYPE_XYZ) 
+	if chk==0 then return c:IsType(TYPE_XYZ)
 		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_MACHINE),tp,LOCATION_REMOVED,0,1,nil) end
 	e:SetCategory(0)
 end
-function s.matop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local g=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsRace,RACE_MACHINE),tp,LOCATION_REMOVED,0,1,1,nil)
-	Duel.Overlay(c,g)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.SendtoGrave(g,REASON_EFFECT)
+		end
+	elseif op==2 then
+		local c=e:GetHandler()
+		if not c:IsRelateToEffect(e) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local g=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsRace,RACE_MACHINE),tp,LOCATION_REMOVED,0,1,1,nil)
+		Duel.Overlay(c,g)
+	end
 end
 function s.repfilter(c,tp)
 	return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
@@ -93,4 +86,3 @@ end
 function s.repval(e,c)
 	return s.repfilter(c,e:GetHandlerPlayer())
 end
-

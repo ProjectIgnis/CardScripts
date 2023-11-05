c0ec54b7b1d7b5343f13588e1d52b6b1feeb910d
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-11-05 11:49:39 +0100

    added Autum Leaves of Blisstopia

---
+++ rush/c160015053.lua
@@ -0,0 +1,49 @@
+--透幻郷の錦綉
+--Autum Leaves of Blisstopia
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--ATK increase
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.target)
+	e2:SetValue(300)
+	c:RegisterEffect(e2)
+	--pierce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_PIERCE)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_GRANT)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e4:SetTarget(s.target)
+	e4:SetLabelObject(e3)
+	c:RegisterEffect(e4)
+end
+function s.cfilter(c)
+	return c:IsRace(RACE_WYRM) and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g,true)
+	Duel.SendtoDeck(g,nil,SEQ_DECKTOP,REASON_COST)
+end
+function s.target(e,c)
+	return c:IsFaceup() and c:IsRace(RACE_WYRM)
+end

78c34ce6c69f0099ebec950ae4e1f9e7f8f0dec9
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-11-06 15:18:37 +0200

    "Fusion Cancel" fix
    Wasn't checking that the Fusion Monster is face-up + wrong parameter in a function call + small simplifications.

--- rush/c160015066.lua
+++ rush/c160015066.lua
@@ -3,30 +3,23 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special summon
+	--Return 1 face-up Fusion Monster on the field to the owner's Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOEXTRA+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_DESTROYED)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP|EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,tp)
-	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_BATTLE)
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.filter,1,nil,tp)
-end
 function s.tdfilter(c)
-	return c:IsAbleToDeck() and c:IsType(TYPE_FUSION) and not c:IsMaximumModeSide()
+	return c:IsAbleToDeck() and c:IsType(TYPE_FUSION) and c:IsFaceup() and not c:IsMaximumModeSide()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_MZONE)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.sumfilter(c,e,tp,fc)
 	return c:IsCode(table.unpack(fc.material)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -45,9 +38,10 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if tc.named_material then named_material_count=math.min(named_material_count,#tc.named_material) end
 	if Duel.GetLocationCount(sp,LOCATION_MZONE)<named_material_count then return end
 	local sg=Duel.GetMatchingGroup(s.sumfilter,sp,LOCATION_GRAVE,0,nil,e,sp,tc)
-	if aux.SelectUnselectGroup(sg,1,sp,named_material_count,named_material_count,s.rescon(tc),0) and Duel.SelectYesNo(sp,aux.Stringid(id,1)) then
-		local spg=aux.SelectUnselectGroup(sg,1,sp,named_material_count,named_material_count,s.rescon(tc),1,sp)
+	if aux.SelectUnselectGroup(sg,e,sp,named_material_count,named_material_count,s.rescon(tc),0) and Duel.SelectYesNo(sp,aux.Stringid(id,1)) then
+		local spg=aux.SelectUnselectGroup(sg,e,sp,named_material_count,named_material_count,s.rescon(tc),1,sp,HINTMSG_SPSUMMON)
 		if #spg>0 then
+			Duel.BreakEffect()
 			Duel.SpecialSummon(spg,0,sp,sp,false,false,POS_FACEUP)
 		end
 	end

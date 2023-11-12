95a25467e7f5de79ac4809c22cf6b12be0fd0802
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-11-12 18:22:26 +0200

    "Subterror Nemesis Warrior" update
    Small script update to match the update of "Duel.GetReleaseGroup".

--- official/c16719140.lua
+++ official/c16719140.lua
@@ -2,46 +2,47 @@
 --Subterror Nemesis Warrior
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Tribute this card and at least 1 other monster and Special Summon 1 "Subterror" monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetCost(s.spcost)
-	e1:SetTarget(s.sptg1)
-	e1:SetOperation(s.spop1)
+	e1:SetHintTiming(0,TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	e1:SetCost(s.subspcost)
+	e1:SetTarget(s.subsptg)
+	e1:SetOperation(s.subspop)
 	c:RegisterEffect(e1)
-	--special summon
+	--Special Summon this card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
 	e2:SetCode(EVENT_FLIP)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg2)
-	e2:SetOperation(s.spop2)
+	e2:SetCondition(s.selfspcon)
+	e2:SetTarget(s.selfsptg)
+	e2:SetOperation(s.selfspop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0xed,0x10ed}
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+s.listed_series={SET_SUBTERROR,SET_SUBTERROR_BEHEMOTH}
+function s.subspcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(100)
 	if chk==0 then return true end
 end
 function s.costfilter(c,e,tp,mg,rlv)
-	if not (c:HasLevel() and c:IsSetCard(0xed) and c:IsMonster() and c:IsAbleToGraveAsCost()
+	if not (c:HasLevel() and c:IsSetCard(SET_SUBTERROR) and c:IsMonster() and c:IsAbleToGraveAsCost()
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_DEFENSE)) then return false end
 	local lv=c:GetLevel()-rlv
 	return #mg>0 and (lv<=0 or mg:CheckWithSumGreater(Card.GetOriginalLevel,lv))
 end
-function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.subsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local mg=Duel.GetReleaseGroup(tp):Filter(Card.IsLevelAbove,nil,1)
+	local mg=Duel.GetReleaseGroup(tp,false,false,REASON_EFFECT):Filter(Card.IsLevelAbove,nil,1)
 	if chk==0 then
 		if e:GetLabel()~=100 then return false end
 		e:SetLabel(0)
@@ -58,12 +59,12 @@ function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetTargetCard(g)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
-function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+function s.subspop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<-1 then return end
 	local tc=Duel.GetFirstTarget()
 	if not tc or not tc:IsRelateToEffect(e) then return end
-	local mg=Duel.GetReleaseGroup(tp):Filter(Card.IsLevelAbove,nil,1)
+	local mg=Duel.GetReleaseGroup(tp,false,false,REASON_EFFECT):Filter(Card.IsLevelAbove,nil,1)
 	if not mg:IsContains(c) then return end
 	mg:RemoveCard(c)
 	if #mg==0 then return end
@@ -83,18 +84,19 @@ function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function s.cfilter(c,tp)
-	return c:IsSetCard(0x10ed) and c:IsControler(tp)
+function s.selfspconfilter(c,tp)
+	return c:IsSetCard(SET_SUBTERROR_BEHEMOTH) and c:IsControler(tp)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function s.selfspcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.selfspconfilter,1,nil,tp)
 end
-function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)

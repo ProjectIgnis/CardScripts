1baa910bda9c4f4ac43cf39d2d68dabe493fd171
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-11-28 14:08:20 +0100

    added Blaze Fiend Deity Luculess

---
+++ rush/c160210036.lua
@@ -0,0 +1,51 @@
+--焔魔神ルシュレス
+--Blaze Fiend Deity Luculess
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Summon with 1 tribute
+	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),s.otfilter)
+	-- Add to hand or Special Summon 1 FIRE Maximum monster from the GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.otfilter(c)
+	return c:IsType(TYPE_MAXIMUM) and c:IsFaceup()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsRace,tp,LOCATION_GRAVE,0,3,nil,RACE_FIEND)
+end
+function s.filter(c,e,tp)
+	return c:IsType(TYPE_MAXIMUM) and c:IsAttribute(ATTRIBUTE_FIRE) 
+		and (c:IsAbleToHand() or (Duel.GetMZoneCount(tp)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if tc then
+		aux.ToHandOrElse(tc,tp,
+			function()
+				return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			end,
+			function()
+				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+			end,
+			aux.Stringid(id,2)
+		)
+	end
+end

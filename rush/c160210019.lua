f1b5b0cdfa39396e1de5d425f3706bcd58312b8a
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-12-02 12:31:02 +0100

    added new rush cards

---
+++ rush/c160210019.lua
@@ -0,0 +1,53 @@
+--ヴォイドヴェルグ・カオスマキア
+--Voidvelgr Chaosmachia
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,160010025,1,s.ffilter,1)
+	--Shuffle cards into deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.named_material={160010025}
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsAttribute(ATTRIBUTE_DARK,scard,sumtype,tp) and c:IsLevelAbove(8)
+end
+function s.cfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetRace)==#sg
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsAbleToDeck()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.filter),tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_ONFIELD)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
+	local cg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TODECK)
+	Duel.HintSelection(cg,true)
+	if Duel.SendtoDeck(cg,nil,SEQ_DECKSHUFFLE,REASON_COST)<1 then return end
+	--Effect
+	local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.filter),tp,0,LOCATION_ONFIELD,1,2,nil)
+	if #g==0 then return end
+	g=g:AddMaximumCheck()
+	Duel.HintSelection(g,true)
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+end
---
+++ rush/c160210085.lua
@@ -0,0 +1,23 @@
+--ヴォイドヴェルグ・モルゲンスタイン
+--Voidvelgr Morgenstein
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--equip
+	aux.AddEquipProcedure(c,0,s.eqfilter,s.eqlimit,nil,nil,nil,s.condition)
+	--Multiple attacks
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_EXTRA_ATTACK)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+end
+function s.eqfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_GALAXY) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevelAbove(7) and not c:IsMaximumModeSide()
+end
+function s.eqlimit(e,c)
+	return c:IsFaceup()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,0,LOCATION_MZONE,1,nil)
+end

de5e1b0a172bad8e1be4b7346f4ad4c92f9e189c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-12-07 12:29:48 +0200

    "Engraver of the Mark" fix
    Fixed an issue where separate activations of its second effect weren't differentiated when checking for a card to destroy.

--- official/c50078320.lua
+++ official/c50078320.lua
@@ -2,37 +2,40 @@
 --Engraver of the Mark
 local s,id=GetID()
 function s.initial_effect(c)
-	--Announce
+	--Declare 1 other card name
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.condition)
-	e1:SetCost(s.cost)
-	e1:SetOperation(s.operation)
+	e1:SetCondition(s.declcon)
+	e1:SetCost(s.declcost)
+	e1:SetOperation(s.declop)
 	c:RegisterEffect(e1)
-	--destroy
+	--Destroy 1 face-up card on the field during the End Phase of the next turn
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e2:SetCountLimit(1)
-	e2:SetTarget(s.regtg)
-	e2:SetOperation(s.regop)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.declcon(e,tp,eg,ep,ev,re,r,rp)
 	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_ANNOUNCE)
-	return rp~=tp and ex and (cv&ANNOUNCE_CARD+ANNOUNCE_CARD_FILTER)~=0
+	return rp==1-tp and ex and (cv&ANNOUNCE_CARD+ANNOUNCE_CARD_FILTER)~=0
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+function s.declcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(c,REASON_COST)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.declop(e,tp,eg,ep,ev,re,r,rp)
 	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_ANNOUNCE)
 	local ac=0
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
@@ -43,35 +46,25 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 	Duel.ChangeTargetParam(ev,ac)
 end
-function s.regtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
-		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetDescription(aux.Stringid(id,1))
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetCountLimit(1)
-		e1:SetLabel(Duel.GetTurnCount())
-		e1:SetLabelObject(tc)
-		e1:SetCondition(s.descon)
-		e1:SetOperation(s.desop)
-		e1:SetReset(RESET_PHASE+PHASE_END,2)
-		Duel.RegisterEffect(e1,tp)
-	end
-end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	return Duel.GetTurnCount()~=e:GetLabel() and tc:GetFlagEffect(id)~=0
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	Duel.Hint(HINT_CARD,0,id)
-	Duel.Destroy(tc,REASON_EFFECT)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		local turn_count=Duel.GetTurnCount()
+		--Destroy it during the End Phase of the next turn
+		aux.DelayedOperation(tc,PHASE_END,id,e,tp,
+			function(ag)
+				Duel.Hint(HINT_CARD,0,id)
+				Duel.Destroy(ag,REASON_EFFECT)
+			end,
+			function() return Duel.GetTurnCount()==turn_count+1 end,
+			nil,2,aux.Stringid(id,2)
+		)
+	end
 end

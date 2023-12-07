c5fcacf9ef164288ac44b2b8311a78a3e9d44bdd
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-12-07 11:55:12 +0200

    "Maestroke the Symphony Djinn" fix
    Fixed an issue introduced in a previous commit that caused it to not detach materials from the monsters protected by its second effect.

--- official/c25341652.lua
+++ official/c25341652.lua
@@ -5,12 +5,12 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Xyz Summon Procedure
 	Xyz.AddProcedure(c,nil,4,2)
-	--Change a monster to face-down Defense position
+	--Change a monster to face-down Defense Position
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_POSITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
 	e1:SetCost(aux.dxmcostgen(1,1,nil))
@@ -23,8 +23,8 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTarget(s.reptg)
-	e2:SetValue(s.repval)
 	e2:SetOperation(s.repop)
+	e2:SetValue(function(e,c) return s.repfilter(c,e:GetHandlerPlayer()) end)
 	c:RegisterEffect(e2)
 end
 s.listed_series={SET_DJINN}
@@ -52,15 +52,12 @@ function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) end
 	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
 		local g=eg:Filter(s.repfilter,nil,tp)
-		Duel.SetTargetCard(g)
+		e:SetLabelObject(g)
 		return true
 	else return false end
 end
-function s.repval(e,c)
-	return s.repfilter(c,e:GetHandlerPlayer())
-end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetTargetCards(e)
+	local g=e:GetLabelObject()
 	for tc in g:Iter() do
 		tc:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
 	end

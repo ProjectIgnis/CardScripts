18a844ce958bee69ecfbd41fe3190d694487e786
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-12-13 13:49:41 +0100

    added new rush cards

---
+++ rush/c160210072.lua
@@ -0,0 +1,60 @@
+--花牙アーマー・クレマチス
+--Flower Fan Clematis
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Give Battle Protection
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetLP(tp)>=Duel.GetLP(1-tp) and e:GetHandler():IsStatus(STATUS_SUMMON_TURN)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAttackPos() and c:IsCanChangePositionRush() end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_PLANT) and c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	local c=e:GetHandler()
+	if Duel.ChangePosition(c,POS_FACEUP_DEFENSE,0,0,0)<1 then return end
+	--Effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(3000)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCondition(s.pccon)
+	e1:SetValue(1)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END|RESET_OPPO_TURN,1)
+	c:RegisterEffect(e1)
+	if Duel.GetMatchingGroupCount(Card.IsMonster,tp,LOCATION_GRAVE,0,nil)>=3
+		and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+function s.pccon(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetLP(tp)>=Duel.GetLP(1-tp)
+end

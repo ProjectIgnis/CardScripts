20b7ad29e90b64015c4f4fa6a8d809344ffa1541
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-12-15 11:13:08 +0100

    added new rush cards

---
+++ rush/c160210075.lua
@@ -0,0 +1,41 @@
+--ツイン・クールスター
+--Twin Twinkle Star
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Increase Level by 1
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():HasLevel() end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	local ct=Duel.GetOperatedGroup():GetFirst()
+	--Effect
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		c:UpdateLevel(2,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,c)
+		if ct:IsAttribute(ATTRIBUTE_LIGHT) and ct:IsRace(RACE_GALAXY) and (not Duel.IsPlayerAffectedByEffect(tp,FLAG_NO_TRIBUTE)) and e:GetHandler():CanBeDoubleTribute(FLAG_DOUBLE_TRIB_LIGHT,FLAG_DOUBLE_TRIB_GALAXY) then
+			c:AddDoubleTribute(id,s.otfilter,s.eftg,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,FLAG_DOUBLE_TRIB_LIGHT+FLAG_DOUBLE_TRIB_GALAXY)
+		end
+	end
+end
+function s.otfilter(c,tp)
+	return c:IsDoubleTribute(FLAG_DOUBLE_TRIB_LIGHT+FLAG_DOUBLE_TRIB_GALAXY) and (c:IsControler(tp) or c:IsFaceup())
+end
+function s.eftg(e,c)
+	return c:IsRace(RACE_GALAXY) and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsLevelAbove(7) and c:IsSummonableCard()
+end

dc3ddcbc440cc21edc06b0a9eaf16e402028d621
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-12-17 08:10:28 +0100

    added Transamu Praime Armor Nova

--- proc_maximum.lua
+++ proc_maximum.lua
@@ -823,6 +823,7 @@ function aux.ThreeTributeOperation()
 	return function (e,tp,eg,ep,ev,re,r,rp,c)
 		local g=e:GetLabelObject()
 		if not g then return end
+		c:SetMaterial(g)
 		Duel.Release(g,REASON_COST)
 		g:DeleteGroup()
 	end

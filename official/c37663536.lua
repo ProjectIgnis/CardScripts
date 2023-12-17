cc52f228a9a0977f6710c4a9fe26c308a9d720fd
Naim <naimsantos45@gmail.com>
2023-12-17 07:21:14 -0300

    "Ancient Gear Megaton Golem" update
    It was not listing "Ancient Gear Golem"

--- official/c37663536.lua
+++ official/c37663536.lua
@@ -1,11 +1,11 @@
 --古代の機械超巨人
---Megaton Ancient Gear Golem
+--Ancient Gear Megaton Golem
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x7),3)
-	--multi attack
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_ANCIENT_GEAR),3)
+	--Can make attacks up to the number of  "Ancient Gear Golem" and "Ancient Gear Golem - Ultimate Pound" used for its summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -19,17 +19,17 @@ function s.initial_effect(c)
 	e2:SetValue(s.valcheck)
 	e2:SetLabelObject(e1)
 	c:RegisterEffect(e2)
-	--actlimit
+	--Your opponent cannot activate Spell/Trap cards until the End of the Damage Step
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,1)
-	e3:SetValue(s.aclimit)
-	e3:SetCondition(s.actcon)
+	e3:SetValue(function(e,re,tp) return re:IsHasType(EFFECT_TYPE_ACTIVATE) end)
+	e3:SetCondition(function(e) return Duel.GetAttacker()==e:GetHandler() end)
 	c:RegisterEffect(e3)
-	--special summon
+	--Special Summon 1 "Ultimate Ancient Gear Golem" from your Extra Deck
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -41,8 +41,8 @@ function s.initial_effect(c)
 	e4:SetOperation(s.spop)
 	c:RegisterEffect(e4)
 end
-s.listed_series={0x7}
-s.listed_names={12652643,95735217}
+s.listed_series={SET_ANCIENT_GEAR}
+s.listed_names={12652643,95735217,CARD_ANCIENT_GEAR_GOLEM} --Ultimate Ancient Gear Golem, Ancient Gear Golem - Ultimate Pound
 function s.valcheck(e,c)
 	e:GetLabelObject():SetLabel(c:GetMaterial():FilterCount(Card.IsCode,nil,83104731,95735217))
 end
@@ -59,19 +59,13 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_EXTRA_ATTACK)
 		e1:SetValue(ct-1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		c:RegisterEffect(e1)
 	end
 end
-function s.aclimit(e,re,tp)
-	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
-end
-function s.actcon(e)
-	return Duel.GetAttacker()==e:GetHandler()
-end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_FUSION) and c:GetReasonPlayer()~=tp and c:IsPreviousControler(tp)
+	return c:IsSummonType(SUMMON_TYPE_FUSION) and c:GetReasonPlayer()==1-tp and c:IsPreviousControler(tp)
 		and c:IsReason(REASON_EFFECT) and c:IsPreviousPosition(POS_FACEUP)
 end
 function s.spfilter(c,e,tp)
@@ -88,4 +82,3 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)
 	end
 end
-

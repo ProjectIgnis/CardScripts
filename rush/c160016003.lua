61ae8ed8bbca2dd987d081513e59b6232db60fa9
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-12-24 14:26:09 +0100

    added new rush cards

---
+++ rush/c160016003.lua
@@ -0,0 +1,54 @@
+--プライム・ドワーフ
+--Intersprime
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send the top 3 cards of your Deck to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_TRANSAMU_RAINAC,160016008}
+function s.condfilter(c)
+	return c:IsMonster() and not c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil)
+		and not Duel.IsExistingMatchingCard(s.condfilter,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.cfilter(c)
+	return c:IsMonster() and c:IsType(TYPE_EFFECT) and c:IsLevel(7,8) and not c:IsPublic()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+end
+function s.thfilter(c)
+	return c:IsCode(CARD_TRANSAMU_RAINAC,160016008) and c:IsAbleToHand()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,tc)
+	--Effect
+	if Duel.DiscardDeck(tp,3,REASON_EFFECT)~=3 then return end
+	if Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+		end
+	end
+end

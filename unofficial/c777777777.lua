d9f4eea4894b3ece1f10353bae4beac9db19212c
Naim <naimsantos45@gmail.com>
2023-12-30 08:30:58 -0300

    Update rule of the day
    - Use cards from the 2024.01 Worlds banlist
    - Remove non-generic cards that cannot be used in most of the duels.

--- unofficial/c777777777.lua
+++ unofficial/c777777777.lua
@@ -42,106 +42,84 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 ban_spell={
-7394770, --Brilliant Fusion
-04031928, --Change of Heart
-11110587, --That Grass Looks Greener
-17375316, --Confiscation
-19613556, --Heavy Storm
-23557835, --Dimension Fusion
-34906152, --Mass Driver
-35059553, --Kaiser Colloseum
-41482598, --Mirage of Nightmare
-42703248, --Giant Trunade
-42829885, --The Forceful Sentry
-44763025, --Deliquent Duo
-45986603, --Snatch Steal
-31423101, --Divine Sword - Phoenix Blade
-46411259, --Metamorphosis
-55144522, --Pot of greed
-57953380, --Card of Safe Return
-60682203, --Cold Wave
-67616300, --Chicken Game
 69243953, --Butterfly Dagger - Elma
-70828912, --Premature Burial
-74191942, --Painful Choice
+57953380, --Card of Safe Return
+67616300, --Chicken Game
+60682203, --Cold Wave
+17375316, --Confiscation
+44763025, --Deliquent Duo
+23557835, --Dimension Fusion
+31423101, --Divine Sword - Phoenix Blade
+42703248, --Giant Trunade
 79571449, --Graceful Charity
+19613556, --Heavy Storm
+35059553, --Kaiser Colloseum
 85602018, --Last Will
-54447022, --Soul Charge
-63789924, --Smoke Grenade of the Thief
+34906152, --Mass Driver
+41482598, --Mirage of Nightmare
 76375976, --Mystic Mine
-46060017, --Zoodiac Barrage
-59750328, --Card of Demise
+74191942, --Painful Choice
+55144522, --Pot of greed
+70828912, --Premature Burial
+63789924, --Smoke Grenade of the Thief
+45986603, --Snatch Steal
+54447022, --Soul Charge
+11110587, --That Grass Looks Greener
+42829885, --The Forceful Sentry
 24224830, --Called by the Grave
-18144506, --Harpie's Feather Duster
-93946239, --Into the Void
-24940422, --Sekka’s Light
-02295440, --One for One
-14733538, --Draco Face-off
-15854426, --Divine Wind of the Mist Valley
-23701465, --Primal Seed
-27970830, --Gateway of the Six
-32807846, --Reinforcement of the Army
-33782437, --One Day of Peace
-52340444, --Sky Striker Mecha - Hornet Drones
-24010609, --Sky Striker Mecha Modules - Multirole
-58577036, --Reasoning
-66957584, --Infernity Launcher
-13035077, --Dragonic Diagram
-71344451, --Slash Draw
-75500286, --Gold Sarcophagus
-70368879, --Upstart Goblin
 72892473, --Card Destruction
-73468603, --Set Rotation
-73628505, --Terraforming
+59750328, --Card of Demise
+91623717, --Chain Strike
+4031928, --Change of Heart
+99266988, --Chaos Space
+65681983, --Crossout Designator
+95308449, --Final Countdown
 81439173, --Foolish Burial
+75500286, --Gold Sarcophagus
+18144506, --Harpie's Feather Duster
+1845204, --Instant Fusion
+93946239, --Into the Void
+71650854, --Magical Mid-Breaker Field
+43040603, --Monster Gate
 83764718, --Monster Reborn
 83764719, --Monster Reborn
-91623717, --Chain Strike
-93600443, --Mask Change 2
-95308449, --Final Countdown
-71650854, --Magical Mid-Breaker Field
-1845204, --Instant Fusion
-37520316, --Mind Control
-01984618, --Nadir Servant
-63166095, --Sky Striker Mobilize - Engage!
-65681983, --Crossout Designator
-43040603, --Monster Gate
-52947044, --Fusion Destiny
-36637374, --Branded Opening
-77103950, --Primeval Planet Perlereino
-15443125, --Spright Starter
-28126717, --Floowandereeze and the Magnificent Map
-46448938, --Spellbook of Judgment
-04031928, --Change of Heart
-71832012, --Prime Planet Paraisos
-44362883, --Branded Fusion
-48130397, --Super Polymerization
-31434645, --Cursed Eldland
-67723438, --Emergency Teleport
+33782437, --One Day of Peace
+2295440, --One for One
 84211599, --Pot of Prosperity
-57103969, --Fire Formation - Tenki
-35261759, --Pot of Desires
-14532163, --Lightning Storm
-03285551, --Rite of Aramesir
+55584558, --Purrely Delicious Memory
+21347668, --Purrely Sleepy Memory
+58577036, --Reasoning
+32807846, --Reinforcement of the Army
+24940422, --Sekka's Light
+71344451, --Slash Draw
+73628505, --Terraforming
+67723438, --Emergency Teleport
 35726888, --Foolish Burial Goods
+14532163, --Lightning Storm
+35261759, --Pot of Desires
 12580477, --Raigeki
-35371948, --Trickstar Light Stage
+3285551, --Rite of Aramesir
+48130397, --Super Polymerization
 }
 ban_trap={
-03280747, --Sixth Sense
-05851097, --Vanity's Emptiness
-17178486, --Life Equalizer
-27174286, --Return from the Different Dimension
+43262273, --Appointer of the Red Lotus
+53334471, --Gozen Match
+61740673, --Imperial Order
 28566710, --Last Turn
+17178486, --Life Equalizer
 32723153, --Magical Explosion
+23002292, --Red Reboot
+27174286, --Return from the Different Dimension
+90846359, --Rivalry of Warlords
+93016201, --Royal Oppression
 57585212, --Self-Destruct Button
+03280747, --Sixth Sense
+82732705, --Skill Drain
+24207889, --There Can Be Only One
+35316708, --Time Seal
 64697231, --Trap Dustshot
+21076084, --Trickstar Reincarnation
 80604091, --Ultimate Offering
 80604092, --Ultimate Offering
-93016201, --Royal Oppression
-61740673, --Imperial Order
-23002292, --Red Reboot
-21076084, --Trickstar Reincarnation
-35316708, --Time Seal
-43262273, --Appointer of the Red Lotus
+05851097, --Vanity's Emptiness
 }

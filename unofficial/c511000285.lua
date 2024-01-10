c3e0f82b4ce3069e0b6aed90416e019fc60524aa
ClaireStanfield <hirakimitsu1123@gmail.com>
2024-01-09 21:13:51 -0500

    Fix Revival Jam,Jam Defender,God Slime,Flame Wingman,Fire Trap

--- unofficial/c511000285.lua
+++ unofficial/c511000285.lua
@@ -1,52 +1,35 @@
---Egyptian God Slime 
---ジェムナイト·ジルコニア
+--神・スライム (Anime)
+--Egyptian God Slime (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--fusion material
+	--Fusion Materials
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,26905245,31709826)
-	--Revival
+	--This card's ATK/DEF become the DEF of "Metal Reflect Slime"
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetCondition(s.regcon)
-	e1:SetOperation(s.regop)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(s.atkdefval)
 	c:RegisterEffect(e1)
-	--change name
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetCode(EFFECT_CHANGE_CODE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetValue(31709826)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_DEFENSE)
 	c:RegisterEffect(e2)
-	--atk
+	--Special Summon this card in Defense Position
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetOperation(s.atkop)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
-	--spsummon
-	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetDescription(aux.Stringid(45894482,0))
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_EVENT_PLAYER+EFFECT_FLAG_DAMAGE_STEP)
-	e4:SetCode(id)
-	e4:SetTarget(s.sptg)
-	e4:SetOperation(s.spop)
-	c:RegisterEffect(e4)
 end
-s.listed_names={26905245}
+s.listed_names={26905245,31709826}
 s.material_setcode=0x54b
-function s.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RaiseSingleEvent(e:GetHandler(),id,e,r,rp,e:GetHandler():GetPreviousControler(),0)
-end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkdefval(e,c)
 	local c=e:GetHandler()
 	local g=c:GetMaterial()
 	local sum=0
@@ -59,24 +42,19 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		end
 		tc=g:GetNext()
 	end
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SET_ATTACK)
-	e1:SetValue(sum)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_SET_DEFENSE)
-	c:RegisterEffect(e2)
+	return sum
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end

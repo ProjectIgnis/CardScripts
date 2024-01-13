7340be6881ecbb7de2dde691a3caed75aa7b225c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-01-13 22:08:59 +0200

    Added new cards' scripts

---
+++ pre-release/c101204026.lua
@@ -0,0 +1,77 @@
+--鋼鉄の大魔人ゴルゴイル
+--Golgoil the Iron Giant Djinn
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	--Neither monster can be destroyed by battle
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.indestg)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Special Summon 1 of your monsters that is banished or in your GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_BATTLED)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.rmgyspcon)
+	e2:SetTarget(s.rmgysptg)
+	e2:SetOperation(s.rmgyspop)
+	c:RegisterEffect(e2)
+	--Special Summon this card
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) end)
+	e3:SetTarget(s.selfsptg)
+	e3:SetOperation(s.selfspop)
+	c:RegisterEffect(e3)
+end
+function s.indestg(e,c)
+	local handler=e:GetHandler()
+	return c==handler or c==handler:GetBattleTarget()
+end
+function s.rmgyspcon(e,tp,eg,ep,ev,re,r,rp)
+	local bc=Duel.GetBattleMonster(tp)
+	return bc and bc:IsRace(RACE_MACHINE|RACE_ILLUSION)
+end
+function s.rmgyspfilter(c,e,tp)
+	return c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.rmgysptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED|LOCATION_GRAVE) and chkc:IsControler(tp) and s.rmgyspfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.rmgyspfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.rmgyspfilter,tp,LOCATION_REMOVED|LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,0)
+end
+function s.rmgyspop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

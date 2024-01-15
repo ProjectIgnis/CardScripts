91e5af6a80157bbf462f345bf7134d94b1d1f8d9
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-01-14 18:44:59 -0800

    Fix "Into the Darkness Below"
    Should only check if player can draw 1 card/script updates to check activation legality.

--- skill/c300205002.lua
+++ skill/c300205002.lua
@@ -5,23 +5,26 @@ function s.initial_effect(c)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	--condition
-	return aux.CanActivateSkill(tp)
-	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil)
+	local b1=Duel.IsPlayerCanDraw(tp,1)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
+	local ct=g:GetClassCount(Card.GetCode)
+	local b2=(Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and ct>3)
+	return aux.CanActivateSkill(tp)	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) and (b1 or b2)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 	Duel.DiscardHand(tp,s.filter,1,1,REASON_COST+REASON_DISCARD)
-	local g1=Duel.IsPlayerCanDraw(tp,2)
+	local b1=Duel.IsPlayerCanDraw(tp,1)
 	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
 	local ct=g:GetClassCount(Card.GetCode)
-	local g2=(Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and ct>3)
+	local b2=(Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and ct>3)
 	local opt=0
-	if g1 and g2 then
+	if b1 and b2 then
 		opt=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-	elseif g1 then
+	elseif b1 then
 		opt=Duel.SelectOption(tp,aux.Stringid(id,1))
-	elseif g2 then
+	elseif b2 then
 		opt=Duel.SelectOption(tp,aux.Stringid(id,2))+1
 	else return end
 	if opt==0 then

64252fc457a68aaa023a4d703d34a065b0ab2592
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-01-18 11:03:22 +0100

    added new rush cards

---
+++ rush/c160016044.lua
@@ -0,0 +1,64 @@
+--セレブローズ・インフルエンサーズ
+--Celeb Rose Influncers
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,160210050,160015008)
+	--Place 1 monsters from opponent's GY at the bottom of the Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK|CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsAbleToDeck()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_GRAVE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,0,LOCATION_GRAVE,1,1,nil)
+	Duel.HintSelection(g,true)
+	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)>0 then
+		local atk=Duel.GetOperatedGroup():GetFirst():GetLevel()*200
+		-- Gain ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
+		e1:SetValue(atk)
+		c:RegisterEffectRush(e1)
+		if c:GetEquipCount()>0 then
+			--Cannot be destroyed by card effects
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+			e2:SetDescription(3001)
+			e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE|EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_CLIENT_HINT)
+			e2:SetRange(LOCATION_MZONE)
+			e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			e2:SetValue(1)
+			c:RegisterEffect(e2)
+			--Can make up to 2 attacks on monsters
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+			e3:SetRange(LOCATION_MZONE)
+			e3:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+			e3:SetValue(1)
+			e3:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
+			c:RegisterEffect(e3)
+		end
+	end
+end

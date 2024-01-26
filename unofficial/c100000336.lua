039b2237a9b73737b32a8bde056561fc6ebad4b5
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-01-25 18:37:24 -0800

    Update c100000336.lua
    Missing parenthesis

--- unofficial/c100000336.lua
+++ unofficial/c100000336.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Synchro Summon
-	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
 	--Destroy all other Attack Position monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))

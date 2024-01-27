8a6a65db19debc5e5261671005e9cbd0f68ad989
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-01-27 10:53:49 -0800

    Fix "Drone Force Fusion"
    Should only use monsters on field/script modernization update.

--- unofficial/c511600249.lua
+++ unofficial/c511600249.lua
@@ -4,31 +4,34 @@
 Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--Fusion
-	local e1=Fusion.CreateSummonEff({handler=c,desc=aux.Stringid(id,0),extrafil=function()return nil,s.matcheck end,extraop=s.extraop})
+	--Fusion Summon 1 Fusion monster, using monsters from your field, including a "Drone" monster
+	local e1=Fusion.CreateSummonEff(c,nil,Fusion.OnFieldMat,s.extrafil,s.extraop)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x581}
+function s.extrafil()
+	return nil,s.matcheck
+end
 function s.matcheck(tp,sg,fc)
 	return sg:IsExists(Card.IsDrone,1,nil,fc,SUMMON_TYPE_FUSION,tp)
 end
 function s.extraop(e,tc,tp,mat1)
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local e1=Effect.CreateEffect(tc)
 	e1:SetDescription(aux.Stringid(68473226,0))
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e1:SetValue(1)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD&-RESET_TOFIELD)
 	tc:RegisterEffect(e1,true)
 	if not tc:IsType(TYPE_EFFECT) then
 		local e2=Effect.CreateEffect(e:GetHandler())
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_ADD_TYPE)
 		e2:SetValue(TYPE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD&-RESET_TOFIELD)
 		tc:RegisterEffect(e2,true)
 	end
 end

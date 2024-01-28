ca5900f2b389d182ec8557b31b4813e63a0ed975
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-01-28 13:50:24 +0200

    "Crystal Pair (Anime)" fix
    The destroyed monster doesn't need to currently be a "Crystal Beast" card + it doesn't treat the placed card as a Continuous Spell.

--- unofficial/c511001129.lua
+++ unofficial/c511001129.lua
@@ -2,55 +2,45 @@
 --Crystal Pair (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--Place 1 "Crystal Beast" from your Deck face-up in your Spell/Trap Zone
+	--Place 1 "Crystal Beast" monster from your Deck in your Spell & Trap Card Zone, face-up
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(47121070,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetCondition(s.plcon)
+	e1:SetTarget(s.pltg)
+	e1:SetOperation(s.plop)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_CRYSTAL_BEAST}
-function s.cfilter(c,tp)
-	return c:IsSetCard(SET_CRYSTAL_BEAST) and c:IsPreviousControler(tp) and c:IsPreviousSetCard(SET_CRYSTAL_BEAST)
+function s.confilter(c,tp)
+	return c:IsPreviousSetCard(SET_CRYSTAL_BEAST) and c:IsPreviousControler(tp)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function s.plcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.confilter,1,nil,tp)
 end
 function s.plfilter(c)
 	return c:IsSetCard(SET_CRYSTAL_BEAST) and c:IsMonster() and not c:IsForbidden()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 		and Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_DECK,0,1,nil) end
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
+	--You take no battle damage for the rest of this turn 
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetValue(1)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local g=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		local tc=g:GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
 		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
-		--Treat it as a Continuous Spell
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_CHANGE_TYPE)
-		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
-		tc:RegisterEffect(e2)
-		--Event raise for "Crystal Tree"
-		Duel.RaiseEvent(tc,47408488,e,0,tp,0,0)
 	end
 end

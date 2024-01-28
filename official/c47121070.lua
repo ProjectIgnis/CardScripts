f8c183cb398606acf75aea7b317ed89546fa8a96
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-01-28 13:34:57 +0200

    "Crystal Pair" fix
    Shouldn't apply the no battle effect if you don't successfully place a "Crystal Beast" monster in the Spell & Trap Zone.

--- official/c47121070.lua
+++ official/c47121070.lua
@@ -2,54 +2,55 @@
 --Crystal Pair
 local s,id=GetID()
 function s.initial_effect(c)
-	--Place 1 "Crystal Beast" from your Deck face-up in your Spell/Trap Zone
+	--Place 1 "Crystal Beast" monster from your Deck face-up in your Spell & Trap Zone as a Continuous Spell
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_BATTLE_DESTROYED)
-	e1:SetCondition(s.tfcon)
-	e1:SetTarget(s.tftg)
-	e1:SetOperation(s.tfop)
+	e1:SetCondition(s.plcon)
+	e1:SetTarget(s.pltg)
+	e1:SetOperation(s.plop)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_CRYSTAL_BEAST}
-function s.cfilter(c,tp)
+function s.confilter(c,tp)
 	return c:IsSetCard(SET_CRYSTAL_BEAST) and c:IsLocation(LOCATION_GRAVE) and c:IsPreviousControler(tp) and c:IsPreviousSetCard(SET_CRYSTAL_BEAST)
+		and c:IsMonster()
 end
-function s.tfcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function s.plcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.confilter,1,nil,tp)
 end
 function s.plfilter(c)
 	return c:IsSetCard(SET_CRYSTAL_BEAST) and c:IsMonster() and not c:IsForbidden()
 end
-function s.tftg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 		and Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_DECK,0,1,nil) end
 end
-function s.tfop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--You take no battle damage
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetTargetRange(1,0)
-	e1:SetValue(1)
-	e1:SetReset(RESET_PHASE|PHASE_END)
-	Duel.RegisterEffect(e1,tp)
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
 	local tc=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
 	if tc and Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
+		local c=e:GetHandler()
 		--Treat it as a Continuous Spell
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_CHANGE_TYPE)
-		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
-		tc:RegisterEffect(e2)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_TYPE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
+		tc:RegisterEffect(e1)
 		Duel.RaiseEvent(tc,EVENT_CUSTOM+47408488,e,0,tp,0,0)
+		--You take no battle damage for the rest of this turn
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,1))
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+		e2:SetTargetRange(1,0)
+		e2:SetValue(1)
+		e2:SetReset(RESET_PHASE|PHASE_END)
+		Duel.RegisterEffect(e2,tp)
 	end
 end

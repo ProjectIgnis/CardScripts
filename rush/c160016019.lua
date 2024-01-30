9fdf379beaebc2aba12d37cda7bf31c0e8f1f0e0
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-01-30 12:59:26 +0100

    added Electro-Lion Bande

---
+++ rush/c160016019.lua
@@ -0,0 +1,33 @@
+--電獅子バンデ
+--Electro-Lion Bande
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Mill
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCountRush(tp,LOCATION_MZONE,0)<=2
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_PYRO|RACE_AQUA|RACE_THUNDER)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,0,nil)*2
+	if chk==0 then return ct>0 and Duel.IsPlayerCanDiscardDeck(tp,ct) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,ct)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ct=Duel.GetMatchingGroupCount(aux.FilterMaximumSideFunctionEx(s.filter),tp,LOCATION_MZONE,0,nil)*2
+	Duel.DiscardDeck(tp,ct,REASON_EFFECT)
+end

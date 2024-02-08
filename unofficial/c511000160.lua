d69db873dd6d65490dc12ad3669a62f9afe2d5a3
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-02-08 10:23:17 -0800

    Update "Hyper Refresh"
    Updated script and added checks to ensure proper execution

--- unofficial/c511000160.lua
+++ unofficial/c511000160.lua
@@ -1,30 +1,26 @@
+--ハイパーフレッシュ
 --Hyper Refresh
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Double your LP
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,e)
-	return c:IsFaceup()
-end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local atk=0
-	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
-	local bc=g:GetFirst()
-	while bc do
-		atk=atk+bc:GetAttack()
-		bc=g:GetNext()
-	end
-	local p=e:GetHandler():GetControler()
-	return Duel.GetLP(p)<atk and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+	local og=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local cg=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local atk=og:GetSum(Card.GetAttack)
+	return Duel.GetLP(tp)<atk and #cg==0
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local p=e:GetHandler():GetControler()
-	Duel.SetLP(tp,(Duel.GetLP(p)*2),REASON_EFFECT)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+        local og=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local cg=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	local atk=og:GetSum(Card.GetAttack)
+        if Duel.GetLP(tp)>=atk or #cg>0 then return false end
+	Duel.SetLP(tp,(Duel.GetLP(tp)*2),REASON_EFFECT)
 end

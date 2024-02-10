2ec2e92d33a0e38d6346f76947a1f073877b7ba6
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-02-09 21:27:25 -0800

    Update "Hologram Projection"
    Minor script polishing

--- unofficial/c511001775.lua
+++ unofficial/c511001775.lua
@@ -2,19 +2,19 @@
 --Hologram Projection
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Special Summon this card as a monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 function s.filter(c,e,tp)
 	return c:IsFaceup() and c:GetLevel()>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x1,c:GetAttack(),c:GetDefense(),c:GetLevel(),c:GetRace(),c:GetAttribute())
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,TYPE_MONSTER,c:GetAttack(),c:GetDefense(),c:GetLevel(),c:GetRace(),c:GetAttribute())
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc,e,tp) end
@@ -24,19 +24,20 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
-	if not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,0x1,tc:GetAttack(),tc:GetDefense(),tc:GetLevel(),tc:GetRace(),tc:GetAttribute()) then return end
-	c:AddMonsterAttribute(TYPE_MONSTER+TYPE_SPELL,tc:GetAttribute(),tc:GetRace(),tc:GetLevel(),tc:GetAttack(),tc:GetDefense())
+	if not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,TYPE_MONSTER,tc:GetAttack(),tc:GetDefense(),tc:GetLevel(),tc:GetRace(),tc:GetAttribute()) then return end
+	c:AddMonsterAttribute(TYPE_MONSTER|TYPE_SPELL,tc:GetAttribute(),tc:GetRace(),tc:GetLevel(),tc:GetAttack(),tc:GetDefense())
 	if Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP) then
 		c:AddMonsterAttributeComplete()
+		--Add monster attributes
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		e1:SetCode(EFFECT_CHANGE_CODE)
 		e1:SetValue(tc:GetCode())
 		c:RegisterEffect(e1)
@@ -64,11 +65,13 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e7:SetCode(EFFECT_CHANGE_LEVEL)
 		e7:SetValue(tc:GetLevel())
 		c:RegisterEffect(e7)
+		--Cannot attack
 		local e8=Effect.CreateEffect(c)
 		e8:SetType(EFFECT_TYPE_SINGLE)
 		e8:SetCode(EFFECT_CANNOT_ATTACK)
 		e8:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e8)
+		--Destroy this card if it is targeted for an attack or card effect
 		local e9=Effect.CreateEffect(c)
 		e9:SetDescription(aux.Stringid(5361647,0))
 		e9:SetCategory(CATEGORY_DESTROY)
@@ -76,17 +79,11 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e9:SetCode(EVENT_BE_BATTLE_TARGET)
 		e9:SetTarget(s.destg)
 		e9:SetOperation(s.desop)
-		e9:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e9:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e9)
-		local e10=Effect.CreateEffect(c)
-		e10:SetDescription(aux.Stringid(5361647,0))
-		e10:SetCategory(CATEGORY_DESTROY)
-		e10:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+		local e10=e9:Clone()
 		e10:SetRange(LOCATION_MZONE)
 		e10:SetCode(EVENT_BECOME_TARGET)
-		e10:SetTarget(s.destg)
-		e10:SetOperation(s.desop)
-		e10:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e10)
 	end
 	Duel.SpecialSummonComplete()

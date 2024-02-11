5f81ae2af39dc79e6871038e2dddc15e19617b92
Naim <naimsantos45@gmail.com>
2024-02-11 11:12:48 -0300

    updates

--- unofficial/c511000383.lua
+++ unofficial/c511000383.lua
@@ -1,31 +1,31 @@
+--封印されし者の憤怒
 --Rage of the Forbidden One
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Negate the effects of all your opponent cards
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,code)
-	return c:IsCode(code) and c:IsFaceup()
-end
+s.listed_names={13893596} --Exodius the Ultimate Forbidden Lord
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil,13893596)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,13893596),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_DISABLE)
-	e1:SetTargetRange(0,LOCATION_ONFIELD+LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_EXTRA)
+	e1:SetTargetRange(0,LOCATION_ALL)
 	e1:SetTarget(s.distarget)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 end
 function s.distarget(e,c)
-	return c~=e:GetHandler() and c:IsType(TYPE_TRAP+TYPE_SPELL+TYPE_MONSTER)
+	return c~=e:GetHandler()
 end

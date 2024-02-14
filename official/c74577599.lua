f299ad4566107403ae7c8cfdd18e73f6061be787
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-14 19:48:59 +0200

    "Traptrix Cularia" + "Traptrix Holeutea" fixes
    - Traptrix Cularia: The re-Setting effect should be applied after the Chain Link resolves, not at the beginning of resolution. Also, it should be HOPT, not SOPT, and it should check that the Trap is still in the Spell & Trap Zone (cannot re-Set a "Traptrix Holeutea" that was successfully Special Summoned).
    - Traptrix Holeutea: Should be sent to the GY if there are no available MMZs when it resolves.

--- official/c74577599.lua
+++ official/c74577599.lua
@@ -1,9 +1,9 @@
 --ホールティアの蟲惑魔
---Traptrix Holetaea
+--Traptrix Holeutea
 --Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon this card
+	--Special Summon this card as a Normal Monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -11,23 +11,23 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e1:SetCountLimit(1,id)
-	e1:SetCost(s.spcost)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetCost(s.selfspcost)
+	e1:SetTarget(s.selfsptg)
+	e1:SetOperation(s.selfspop)
 	c:RegisterEffect(e1)
-	--Can be activated the same turn it was set
+	--Can be activated the turn it was Set by discarding 1 Normal Trap
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
 	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
 	e2:SetValue(function(e,c) e:SetLabel(1) end)
-	e2:SetCondition(function(e) return Duel.IsExistingMatchingCard(s.spcostfilter,e:GetHandlerPlayer(),LOCATION_HAND,0,1,nil) end)
+	e2:SetCondition(function(e) return Duel.IsExistingMatchingCard(s.selfspcostfilter,e:GetHandlerPlayer(),LOCATION_HAND,0,1,nil) end)
 	c:RegisterEffect(e2)
 	e1:SetLabelObject(e2)
-	--Special Summon 1 "Traptrix" monster
+	--Special Summon 1 "Traptrix" monster from your GY
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -35,29 +35,29 @@ function s.initial_effect(c)
 	e3:SetRange(LOCATION_GRAVE)
 	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e3:SetCountLimit(1,id)
-	e3:SetCost(aux.bfgcost)
-	e3:SetTarget(s.gsptg)
-	e3:SetOperation(s.gspop)
+	e3:SetCost(aux.selfbanishcost)
+	e3:SetTarget(s.gysptg)
+	e3:SetOperation(s.gyspop)
 	c:RegisterEffect(e3)
 end
 s.listed_series={SET_TRAPTRIX}
-function s.spcostfilter(c)
+function s.selfspcostfilter(c)
 	return c:IsNormalTrap() and c:IsDiscardable()
 end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then e:GetLabelObject():SetLabel(0) return true end
-	if e:GetLabelObject():GetLabel()>0 then
-		e:GetLabelObject():SetLabel(0)
-		Duel.DiscardHand(tp,s.spcostfilter,1,1,REASON_COST+REASON_DISCARD)
+function s.selfspcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local label_obj=e:GetLabelObject()
+	if chk==0 then label_obj:SetLabel(0) return true end
+	if label_obj:GetLabel()>0 then
+		label_obj:SetLabel(0)
+		Duel.DiscardHand(tp,s.selfspcostfilter,1,1,REASON_COST|REASON_DISCARD)
 	end
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:IsHasType(EFFECT_TYPE_ACTIVATE)
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,SET_TRAPTRIX,TYPE_MONSTER|TYPE_NORMAL,400,2400,4,RACE_PLANT,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,0)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e)
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,SET_TRAPTRIX,TYPE_MONSTER|TYPE_NORMAL,400,2400,4,RACE_PLANT,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE) then
@@ -67,19 +67,18 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummonComplete()
 	end
 end
-function s.spfilter(c,e,tp)
+function s.gyspfilter(c,e,tp)
 	return c:IsSetCard(SET_TRAPTRIX) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.gsptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+function s.gysptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.gyspfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(s.gyspfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	local g=Duel.SelectTarget(tp,s.gyspfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,0)
 end
-function s.gspop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+function s.gyspop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)

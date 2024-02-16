53416c99124690a184b25e4d14827462dd8e0817
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-02-16 07:24:52 -0800

    Update c511001788.lua
    Wrong function declaration.

--- unofficial/c511001788.lua
+++ unofficial/c511001788.lua
@@ -50,7 +50,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end

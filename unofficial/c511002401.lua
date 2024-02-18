131f0c870ccd7e469dc8a65fdf38bddacbffbfaf
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-18 15:32:10 +0200

    "Neospace Wave" fix
    Should only count the monsters that end up in the GY + added "Blue-Eyes Spirit Dragon" handling.

--- unofficial/c511002401.lua
+++ unofficial/c511002401.lua
@@ -2,8 +2,9 @@
 --Neospace Wave
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -12,36 +13,36 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x1f}
-function s.filter(c)
-	return c:IsSetCard(0x1f) and c:IsMonster()
+s.listed_series={SET_NEO_SPACIAN}
+function s.nsfilter(c)
+	return c:IsSetCard(SET_NEO_SPACIAN) and c:IsMonster()
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
-	local neoct=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_DECK,0,nil)
-	return neoct>ct
+	return Duel.GetMatchingGroupCount(s.nsfilter,tp,LOCATION_DECK,0,nil)>Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x1f) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(SET_NEO_SPACIAN) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local mg=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
 	local ct=#mg
-	local ft=Duel.GetMZoneCount(tp,mg)
 	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
-	if chk==0 then return ct>0 and ft>=ct and mg:FilterCount(Card.IsAbleToGrave,nil)==ct and #sg>=ct end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,mg,ct,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,ct,0,0)
+	if chk==0 then return ct>0 and Duel.GetMZoneCount(tp,mg)>=ct and mg:FilterCount(Card.IsAbleToGrave,nil)==ct and #sg>=ct
+		and (ct==1 or not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,mg,ct,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,ct,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
-	if Duel.SendtoGrave(mg,REASON_EFFECT)>0 then
-		local ct=#Duel.GetOperatedGroup()
-		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
-		if ct>0 and ft>=ct and #sg>=ct then
-			local spg=sg:RandomSelect(tp,ct)
-			Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
-		end
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	if #g==0 or Duel.SendtoGrave(g,REASON_EFFECT)==0 then return end
+	local og=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	local ct=#og
+	if ct==0 or (ct>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)) then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<ct then return end
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	if #sg>=ct then
+		local spg=sg:RandomSelect(tp,ct)
+		Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

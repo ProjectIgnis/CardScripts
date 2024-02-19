026608b3d9511e742096be321fc029a0a11c7818
Naim <naimsantos45@gmail.com>
2024-02-19 14:43:10 -0300

    "Reign-Beaux, Overlord of Dark World" fix
    Fixed a bug where it would not be able to destroy Spells/Traps that were in the monster zone

--- official/c99458769.lua
+++ official/c99458769.lua
@@ -1,7 +1,8 @@
 --暗黒界の魔神 レイン
+--Reign-Beaux, Overlord of Dark World
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon
+	--Special Summon this card if it is discarded to the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -11,7 +12,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--destroy
+	--Destroy all monsters OR all Spells/Traps your opponent controls
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
@@ -23,16 +24,18 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_HAND) and (r&0x4040)==0x4040 and rp~=tp
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND) and rp==1-tp
+		and (r&REASON_DISCARD|REASON_EFFECT)==(REASON_DISCARD|REASON_EFFECT)
 		and e:GetHandler():IsPreviousControler(tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SpecialSummon(e:GetHandler(),1,tp,tp,false,false,POS_FACEUP)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
@@ -40,27 +43,27 @@ function s.descon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local c1=Duel.GetMatchingGroupCount(aux.TRUE,tp,0,LOCATION_MZONE,nil)
-	local c2=Duel.GetMatchingGroupCount(aux.TRUE,tp,0,LOCATION_SZONE,nil)
-	if (c1>c2 and c2~=0) or c1==0 then c1=c2 end
-	if c1~=0 then
-		local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,c1,0,0)
+	local c1=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
+	local c2=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,0,LOCATION_ONFIELD,nil)
+	if (#c1+#c2)>0 then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,c1+c2,1,tp,0)
+	else
+		Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
 	end
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g1=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
-	local g2=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_SZONE,nil)
-	if #g1>0 or #g2>0 then
-		if #g1==0 then
-			Duel.Destroy(g2,REASON_EFFECT)
-		elseif #g2==0 then
-			Duel.Destroy(g1,REASON_EFFECT)
-		else
-			Duel.Hint(HINT_SELECTMSG,tp,0)
-			local ac=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
-			if ac==0 then Duel.Destroy(g1,REASON_EFFECT)
-			else Duel.Destroy(g2,REASON_EFFECT) end
-		end
+	local g1=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
+	local g2=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,0,LOCATION_ONFIELD,nil)
+	local b1=#g1>0
+	local b2=#g2>0
+	if not (b1 or b2) then return end
+	local op=nil
+	if (b1 and b2) then
+		op=Duel.SelectEffect(tp,
+			{b1,aux.Stringid(id,2)},
+			{b2,aux.Stringid(id,3)})
+	else
+		op=(b1 and 1 or 2)
 	end
+	Duel.Destroy(op==1 and g1 or g2,REASON_EFFECT)
 end

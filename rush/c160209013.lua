ba9d7ee639f7a7bc120b304ff5da6f457404a55e
Naim <naimsantos45@gmail.com>
2024-02-19 08:02:05 -0300

    "Ultimate Flag Beast Bolt Tricorn" fix
    Fixed a bug in which it would be possible to select a face-down monster to apply the effect to

--- rush/c160209013.lua
+++ rush/c160209013.lua
@@ -3,7 +3,7 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special summon 1 0 atk fairy monster from GY
+	--Prevent the activation of a face-down Spell/Trap your opponent controls
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
@@ -21,15 +21,18 @@ end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
 end
+function s.cfilter(c)
+	return c:IsFacedown() and c:IsSpellTrap()
+end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFacedown,tp,0,LOCATION_SZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,0,LOCATION_ONFIELD,1,nil) end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	--Requirement
 	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
 	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
-	local tc=Duel.SelectMatchingCard(tp,Card.IsFacedown,tp,0,LOCATION_ONFIELD,1,1,nil):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,0,LOCATION_ONFIELD,1,1,nil):GetFirst()
 	if tc then
 		Duel.HintSelection(tc,true)
 		local e1=Effect.CreateEffect(e:GetHandler())

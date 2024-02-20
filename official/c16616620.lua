56f303735cc98b3da39d37e6ca1b0b0a6f385d17
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-20 14:03:58 +0200

    "Contact" fix
    Should only send face-up "Chrysalis" monsters to the GY + should only Special Summon "Neo-Spacian" monsters.

--- official/c16616620.lua
+++ official/c16616620.lua
@@ -1,38 +1,36 @@
 --コンタクト
+--Contact
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x1e}
-function s.spfilter(c,g,e,tp)
-	return g:IsExists(Card.ListsCode,1,nil,c:GetCode()) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.cfilter(c)
-	return c:GetSequence()<5
+s.listed_series={SET_CHRYSALIS,SET_NEO_SPACIAN}
+function s.spfilter(c,e,tp,g)
+	return c:IsSetCard(SET_NEO_SPACIAN) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and g:IsExists(Card.ListsCode,1,nil,c:GetCode())
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x1e)
-	if chk==0 then return #g>0 and g:FilterCount(Card.IsAbleToGrave,nil)==#g 
-		and g:FilterCount(s.cfilter,nil)+Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,g,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_CHRYSALIS),tp,LOCATION_MZONE,0,nil)
+	if chk==0 then return #g>0 and g:FilterCount(Card.IsAbleToGrave,nil)==#g and Duel.GetMZoneCount(tp,g)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp,g) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0x1e)
-	if Duel.SendtoGrave(g,REASON_EFFECT)>0 then
-		local sg=g:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
-		if #sg==0 then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local spg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,sg,e,tp)
-		if #spg>0 then
-			Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
-		end
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_CHRYSALIS),tp,LOCATION_MZONE,0,nil)
+	if Duel.SendtoGrave(g,REASON_EFFECT)==0 or Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	local og=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	if #og==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp,og)
+	if #sg>0 then
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

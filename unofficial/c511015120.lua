28bba60cbe6d2a548b07431a3040220de29b9328
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-02-19 21:32:44 -0800

    Fix "Junk Anchor (Manga)
    Effect shouldn't be OPT/minor script update.

--- unofficial/c511015120.lua
+++ unofficial/c511015120.lua
@@ -1,40 +1,41 @@
+--ジャンク・アンカー (Manga)
 --Junk Anchor (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
-	--synchro
+	--Synchro Summon using this card and 1 "Junk" monster in your GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetCost(s.sccost)
-	e1:SetTarget(s.sctg)
-	e1:SetOperation(s.scop)
+	e1:SetCost(s.syncost)
+	e1:SetTarget(s.syntg)
+	e1:SetOperation(s.synop)
 	c:RegisterEffect(e1)
 end
-function s.sccost(e,tp,eg,ep,ev,re,r,rp,chk)
+s.listed_series={SET_JUNK}
+function s.syncost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST)
 end
-function s.mfilter(c,e,tp,mc)
+function s.matfilter(c,e,tp,mc)
 	local mg=Group.FromCards(c,mc)
-	return c:IsSetCard(0x43) and not c:IsType(TYPE_TUNER) 
-		and Duel.IsExistingMatchingCard(s.scfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
+	return c:IsSetCard(SET_JUNK) and not c:IsType(TYPE_TUNER) 
+		and Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
 end
-function s.scfilter(c,mg)
+function s.synfilter(c,mg)
 	return c:IsSynchroSummonable(nil,mg)
 end
-function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.mfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,e:GetHandler()) end
+function s.syntg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,e:GetHandler()) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.scop(e,tp,eg,ep,ev,re,r,rp)
+function s.synop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-	local tc=Duel.SelectMatchingCard(tp,s.mfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,e:GetHandler()):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,e:GetHandler()):GetFirst()
 	local mg=Group.FromCards(tc,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sc=Duel.SelectMatchingCard(tp,s.scfilter,tp,LOCATION_EXTRA,0,1,1,nil,mg):GetFirst()
+	local sc=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_EXTRA,0,1,1,nil,mg):GetFirst()
 	if sc then Duel.SynchroSummon(tp,sc,nil,mg) end
 end

27bc768f6b1528e85f5ee3935e571184a5c1ec70
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-21 14:01:14 +0200

    Added new cards' scripts

---
+++ pre-release/c100436001.lua
@@ -0,0 +1,66 @@
+--デーモンの光来
+--Archfiend's Arrival
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Can be Normal Summoned without Tributing if you control "Gold Sarcophagus of Light"
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.notribcon)
+	c:RegisterEffect(e1)
+	--Take control of 1 of your opponent's monsters until the End Phase
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_CONTROL)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.ctrltg)
+	e2:SetOperation(s.ctrlop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--Other monsters you control gain 500 ATK during your turn
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_UPDATE_ATTACK)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_MZONE,0)
+	e4:SetCondition(function(e) return Duel.IsTurnPlayer(e:GetHandlerPlayer()) end)
+	e4:SetTarget(function(e,c) return c~=e:GetHandler() end)
+	e4:SetValue(500)
+	c:RegisterEffect(e4)
+end
+s.listed_names={CARD_GOLD_SARC_OF_LIGHT}
+function s.notribcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_GOLD_SARC_OF_LIGHT),tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.ctrltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsControlerCanBeChanged() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectTarget(tp,Card.IsControlerCanBeChanged,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,tp,0)
+	e:SetLabel(Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_GOLD_SARC_OF_LIGHT),tp,LOCATION_ONFIELD,0,1,nil) and 1 or 0)
+end
+function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.GetControl(tc,tp,PHASE_END,1) and e:GetLabel()==0 then
+		--Cannot attack while you control it
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3206)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_CONTROL)
+		tc:RegisterEffect(e1)
+	end
+end
---
+++ pre-release/c100436002.lua
@@ -0,0 +1,73 @@
+--ファントム・オブ・ユベル
+--Phantom of Yubel
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Special Summon Procedure
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_YUBEL),s.fiendfilter)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,true)
+	--Cannot be used as Fusion Material
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Cannot be destroyed by battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--You take no battle damage from battles involving this card
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--Change a monster effect activated by the opponent
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp and re:IsMonsterEffect() end)
+	e4:SetCost(aux.selfreleasecost)
+	e4:SetTarget(s.chngtg)
+	e4:SetOperation(s.chngop)
+	c:RegisterEffect(e4)
+end
+s.material_setcode=SET_YUBEL
+s.listed_series={SET_YUBEL}
+function s.fiendfilter(c)
+	return c:IsRace(RACE_FIEND) and c:IsAttack(0) and c:IsDefense(0)
+end
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToDeckOrExtraAsCost,tp,LOCATION_MZONE|LOCATION_HAND|LOCATION_GRAVE,0,nil)
+end
+function s.contactop(g,tp)
+	local fu,fd=g:Split(Card.IsFaceup,nil)
+	if #fu>0 then Duel.HintSelection(fu,true) end
+	if #fd>0 then Duel.ConfirmCards(1-tp,fd) end
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST|REASON_MATERIAL)
+end
+function s.yubelfilter(c)
+	return c:IsSetCard(SET_YUBEL) and c:IsMonster() and (c:IsFaceup() or not c:IsOnField())
+end
+function s.chngtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.yubelfilter,tp,LOCATION_HAND|LOCATION_MZONE|LOCATION_DECK,0,1,e:GetHandler()) end
+end
+function s.chngop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Group.CreateGroup()
+	Duel.ChangeTargetCard(ev,g)
+	Duel.ChangeChainOperation(ev,s.repop)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(1-tp,s.yubelfilter,1-tp,LOCATION_HAND|LOCATION_MZONE|LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT,LOCATION_GRAVE,1-tp)
+	end
+end

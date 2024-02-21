a5bd76238af02203451707abcfb10595fd0781a3
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-21 10:21:08 +0200

    "Lunalight Panther Dancer (Anime)" fix
    The opponent's monsters should not be destroyed by battle only when this card is attacking them. They should be destroyed normally if they attack this card.

--- unofficial/c511002009.lua
+++ unofficial/c511002009.lua
@@ -2,35 +2,36 @@
 --Lunalight Panther Dancer (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--fustion material
-	Fusion.AddProcMix(c,true,true,51777272,aux.FilterBoolFunction(Card.IsSetCard,0xdf))
 	c:EnableReviveLimit()
-	--indestructable
+	--Fustion Materials
+	Fusion.AddProcMix(c,true,true,51777272,aux.FilterBoolFunction(Card.IsSetCard,SET_LUNALIGHT))
+	--Cannot be destroyed by your opponent's card effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e1:SetValue(s.indval)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(aux.indoval)
 	c:RegisterEffect(e1)
-	--attack
+	--This card can attack all monsters your opponent controls, twice each
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EFFECT_ATTACK_ALL)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetValue(2)
 	c:RegisterEffect(e2)
+	--The opponent's monsters cannot be destroyed by the first attack of this card
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 	e3:SetTargetRange(0,LOCATION_MZONE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTarget(s.indtg)
 	e3:SetValue(s.indct)
 	c:RegisterEffect(e3)
-	--atk up
+	--This card gains 200 ATK until the end of the Battle Phase
 	local e4=Effect.CreateEffect(c)
 	e4:SetCategory(CATEGORY_ATKCHANGE)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
@@ -39,16 +40,15 @@ function s.initial_effect(c)
 	e4:SetOperation(s.atkop)
 	c:RegisterEffect(e4)
 end
-s.listed_series={0xdf}
-s.material_setcode=0xdf
-function s.indval(e,re,tp)
-	return tp~=e:GetHandlerPlayer()
-end
+s.listed_names={51777272}
+s.listed_series={SET_LUNALIGHT}
+s.material_setcode=SET_LUNALIGHT
 function s.indtg(e,c)
-	return c:GetReasonCard()==e:GetHandler()
+	local hc=e:GetHandler()
+	return c:GetReasonCard()==hc and Duel.GetAttacker()==hc
 end
 function s.indct(e,re,r,rp,c)
-	if (r&REASON_BATTLE)~=0 then
+	if (r&REASON_BATTLE)>0 then
 		return 1
 	else return 0 end
 end
@@ -59,12 +59,13 @@ function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToBattle() and c:IsFaceup() then
+	if c:IsRelateToBattle() and c:IsFaceup() and not c:IsStatus(STATUS_BATTLE_DESTROYED) then
+		--Gains 200 ATK
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(200)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_BATTLE)
 		c:RegisterEffect(e1)
 	end
 end

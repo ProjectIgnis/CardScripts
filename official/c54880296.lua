ac323b9faebd9e8be1cfe308a849ebd6b22a160a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-22 13:16:55 +0200

    "Yosenju Wind Worship" fix
    Should look for all "Yosenju" Monster Cards in your Spell & Trap Zone, not just Pendulums + returning the cards to the hand and drawing shouldn't happen at the same time.

--- official/c54880296.lua
+++ official/c54880296.lua
@@ -1,47 +1,43 @@
 --妖仙獣の風祀り
---Yosenju Wind Ritual
+--Yosenju Wind Worship
 --scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
+	--Return as many "Yosenju" Monster Cards you control to the hand as possible
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW+CATEGORY_TOHAND)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0xb3}
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0xb3) and (c:IsMonster() or c:IsType(TYPE_PENDULUM))
+s.listed_series={SET_YOSENJU}
+function s.yosenjufilter(c)
+	return c:IsSetCard(SET_YOSENJU) and c:IsFaceup() and c:IsOriginalType(TYPE_MONSTER)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,0,nil)
+	local g=Duel.GetMatchingGroup(s.yosenjufilter,tp,LOCATION_ONFIELD,0,nil)
 	return g:GetClassCount(Card.GetCode)>=3
 end
-function s.thfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xb3) and (c:IsMonster() or c:IsType(TYPE_PENDULUM)) and c:IsAbleToHand()
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,0,1,nil) end
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_ONFIELD,0,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
+	local g=Duel.GetMatchingGroup(s.yosenjufilter,tp,LOCATION_ONFIELD,0,nil):Match(Card.IsAbleToHand,nil)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,tp,0)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,1,tp,1)
 end
-function s.rtfilter(c,p)
-	return c:IsLocation(LOCATION_HAND) and c:IsControler(p)
-end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,0,nil)
-	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 then
-		local ct=Duel.GetOperatedGroup():FilterCount(s.rtfilter,nil,tp)
-		if ct==#g then
-			local ct=5-Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
-			if ct>0 and Duel.IsPlayerCanDraw(tp,ct) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-				Duel.Draw(tp,ct,REASON_EFFECT) 
-			end
+	local g=Duel.GetMatchingGroup(s.yosenjufilter,tp,LOCATION_ONFIELD,0,nil):Match(Card.IsAbleToHand,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and g:IsExists(Card.IsLocation,1,nil,LOCATION_HAND) then
+		Duel.ShuffleHand(tp)
+		local ct=5-Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
+		if ct>0 and Duel.IsPlayerCanDraw(tp,ct) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.Draw(tp,ct,REASON_EFFECT) 
 		end
 	end
 end

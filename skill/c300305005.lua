655563f6d6a0567e44caef2f244ef63f39d2a40e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-24 15:54:03 +0200

    "Under Pressure" fix
    Shouldn't be able to destroy a face-down monster + shouldn't be able to use it with less than 4 cards in your Deck + separate the timing between actions accordingly.

--- skill/c300305005.lua
+++ skill/c300305005.lua
@@ -5,8 +5,8 @@ function s.initial_effect(c)
 	--Activate
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_STARTUP)
 	e1:SetCountLimit(1)
 	e1:SetRange(0x5f)
@@ -20,58 +20,65 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
 end
-function s.hyofilter(c)
-	return c:IsFaceup() and c:IsCode(62397231)
-end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.hyofilter,tp,LOCATION_MZONE,0,1,nil)
+	return aux.CanActivateSkill(tp) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=4
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,62397231),tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,id)
 	Duel.ConfirmDecktop(tp,4)
 	local c=e:GetHandler()
 	local g=Duel.GetDecktopGroup(tp,4)
-	--Carbonnedon effect check (Hyozanryu gains 1000 ATK)
+	local break_chk=false
+	--"Carbonnedon": 1 "Hyozanryu" you control gains 1000 ATK until the end of this turn
 	if g:IsExists(Card.IsCode,1,nil,15981690) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
-		local tc=Duel.SelectMatchingCard(tp,s.hyofilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(id,0))
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
-		e1:SetValue(1000)
-		tc:RegisterEffect(e1)
-	end
-	--Hydrogeddon effect check (Hyozanryu unaffected by opponent's monster effects)
-	if g:IsExists(Card.IsCode,1,nil,22587018) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
-		local sc=Duel.SelectMatchingCard(tp,s.hyofilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
-		local e2=Effect.CreateEffect(c)
-		e2:SetDescription(aux.Stringid(id,1))
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_IMMUNE_EFFECT)
-		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
-		e2:SetRange(LOCATION_MZONE)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
-		e2:SetValue(s.efilter)
-		sc:RegisterEffect(e2)
-	end
-	--Oxygeddon effect check (Destroy 1 face-up monster you opponent controls and inflict 800 damage)
-	if g:IsExists(Card.IsCode,1,nil,58071123) then
-		if not Duel.IsExistingMatchingCard(Card.IsType,tp,0,LOCATION_MZONE,1,nil,TYPE_MONSTER) then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local dg=Duel.SelectMatchingCard(tp,Card.IsType,tp,0,LOCATION_MZONE,1,1,nil,TYPE_MONSTER)
-		Duel.HintSelection(dg)
-		if Duel.Destroy(dg,REASON_EFFECT)>0 then
-			Duel.Damage(1-tp,800,REASON_EFFECT)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+		local tc=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsCode,62397231),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+		if tc then
+			Duel.HintSelection(tc,true)
+			break_chk=true
+			--Gains 1000 ATK
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(1000)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			tc:RegisterEffect(e1)
 		end
 	end
+	--"Hydrogeddon": 1 "Hyozanryu" you control becomes unaffected by your opponent's monster effects until the end of this turn
+	if g:IsExists(Card.IsCode,1,nil,22587018) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+		local sc=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsCode,62397231),tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+		if sc then
+			Duel.HintSelection(sc,true)
+			if break_chk then Duel.BreakEffect() end
+			break_chk=true
+			--Unaffected by your opponent's monster effects
+			local e2=Effect.CreateEffect(c)
+			e2:SetDescription(3111)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e2:SetCode(EFFECT_IMMUNE_EFFECT)
+			e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			e2:SetValue(function(e,te) return te:IsMonsterEffect() and te:GetOwnerPlayer()==1-e:GetHandlerPlayer() end)
+			sc:RegisterEffect(e2)
+		end
+	end
+	--"Oxygeddon": Destroy 1 face-up monster you opponent controls, then inflict 800 damage to your opponent
+	if g:IsExists(Card.IsCode,1,nil,58071123) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local dg=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+		if #dg>0 then
+			Duel.HintSelection(dg,true)
+			if break_chk then Duel.BreakEffect() end
+			if Duel.Destroy(dg,REASON_EFFECT)>0 then
+				Duel.BreakEffect()
+				Duel.Damage(1-tp,800,REASON_EFFECT)
+			end
+		end
+	end
+	Duel.BreakEffect()
 	Duel.MoveToDeckBottom(g,tp)
 	Duel.SortDeckbottom(tp,tp,4)
-end
-function s.efilter(e,te)
-	return te:IsActiveType(TYPE_MONSTER) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end

23da4ffe925fb0b253b0f02cf39684796790becc
Naim <naimsantos45@gmail.com>
2024-02-26 20:20:05 -0300

    Update "Radiance of the Forbidden Spell"
    Rescripted

--- unofficial/c511009166.lua
+++ unofficial/c511009166.lua
@@ -1,4 +1,6 @@
---Radiance of the FOrbidden Spell
+--封魔閃光
+--Radiance of the Forbidden Spell
+--scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,222 +8,147 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--disable your field
+	--Make 1 Monster Zone unusable
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
 	e2:SetRange(LOCATION_SZONE)
+	e2:SetProperty(EFFECT_FLAG_BOTH_SIDE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.dscon1)
-	e2:SetOperation(s.dsop1)
+	e2:SetCondition(s.disfieldcond)
+	e2:SetOperation(s.disfieldop)
 	c:RegisterEffect(e2)
-	-- disable opponent field
+	--Make all your Monster Zones usuable an increase the ATK of your battling monster by 800 per zone
 	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1)
-	e3:SetCondition(s.dscon2)
-	e3:SetOperation(s.dsop2)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetHintTiming(TIMING_DAMAGE_STEP)
+	e3:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
+	e3:SetCondition(s.atkcond)
+	e3:SetCost(s.atkcost)
+	e3:SetTarget(s.atktg)
+	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
-	--atk & def
+	--Inflict damage equal to the number of zones unusable due to this card's effects
 	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_ATKCHANGE)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_DAMAGE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_PHASE+PHASE_BATTLE)
 	e4:SetRange(LOCATION_SZONE)
-	e4:SetCondition(s.atkcon)
-	e4:SetOperation(s.atkop)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.damtg)
+	e4:SetOperation(s.damop)
 	c:RegisterEffect(e4)
-	--damage
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(46128076,0))
-	e5:SetCategory(CATEGORY_DAMAGE)
-	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e5:SetCode(EVENT_PHASE+PHASE_BATTLE)
-	e5:SetRange(LOCATION_SZONE)
-	e5:SetCountLimit(1)
-	e5:SetTarget(s.damtg)
-	e5:SetOperation(s.damop)
-	c:RegisterEffect(e5)
 end
---disable your zone
-function s.dscon1(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
+function s.disfieldcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(tp)
 end
-function s.dsop1(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	--disable field
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetCode(EFFECT_DISABLE_FIELD)
-	e1:SetOperation(s.disop1)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-	e:GetHandler():RegisterEffect(e1)
-	local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-	if ct==nil then
-		e:GetHandler():RegisterFlagEffect(95100886,RESET_EVENT+RESETS_STANDARD,0,0,1)
-	else
-		ct=ct+1
-		e:GetHandler():SetFlagEffectLabel(95100886,ct)
+function s.disfieldop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE,PLAYER_NONE,LOCATION_REASON_COUNT)<=0 then return end
+	--Make one of the turn player's Monster Zones unusable
+	local zone=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
+	local c=e:GetHandler()
+	if Duel.IsTurnPlayer(1-c:GetControler()) then
+		zone=zone<<16
 	end
-end
-function s.disop1(e,tp)
-	local dis1=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
-	return dis1
-end
---disable opponent zone
-function s.dscon2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
-end
-function s.dsop2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)<=0 then return end
-	--disable field
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_SZONE)
 	e1:SetCode(EFFECT_DISABLE_FIELD)
-	e1:SetOperation(s.disop2)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-	e:GetHandler():RegisterEffect(e1)
-	local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-	if ct==nil then
-		e:GetHandler():RegisterFlagEffect(95100886,RESET_EVENT+RESETS_STANDARD,0,0,1)
-	else
-		ct=ct+1
-		e:GetHandler():SetFlagEffectLabel(95100886,ct)
-	end
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetOperation(function(e,tp) return zone end)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	--e1:SetLabel(zone)
+	c:RegisterEffect(e1)
+	c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
 end
-function s.disop2(e,tp)
-	local dis1=Duel.SelectDisableField(1-tp,1,LOCATION_MZONE,0,0)
-	local seq=0
-	if dis1==1 then seq=0 end
-	if dis1==2 then seq=1 end
-	if dis1==4 then seq=2 end
-	if dis1==8 then seq=3 end
-	if dis1==16 then seq=4 end
-	return (0x1<<16+seq)
+function s.atkcond(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPhase(PHASE_DAMAGE) or Duel.IsDamageCalculated() then return false end
+	return Duel.GetBattleMonster(tp)
 end
---atk up
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetAttacker()
-	local bc=Duel.GetAttackTarget()
-	if not bc then return false end
-	if bc:IsControler(1-tp) then bc=tc end
-	e:SetLabelObject(bc)
-	return bc:IsFaceup() and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
+end
+function s.posfilter(c)
+	return c:IsAttackPos() and c:IsCanChangePosition()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local at=Duel.GetBattleMonster(tp)
+	if chk==0 then return at and not at:HasFlagEffect(id)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_COUNT) end
+	Duel.SetTargetCard(at)
+	at:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE,0,1)
+	local g=Duel.GetMatchingGroup(s.posfilter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,tp,POS_DEFENSE)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,at,1,tp,800)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local c=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if c==0 then return end
-	local tc=e:GetLabelObject()
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local zonect=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_COUNT)
+	if zonect==0 then return end
+	local c=e:GetHandler()
+	--Make all your Monster Zones usuable your monster
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCode(EFFECT_DISABLE_FIELD)
-	e1:SetOperation(s.disop3)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetOperation(s.disallop)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 	e:GetHandler():RegisterEffect(e1)
-	if tc:IsRelateToBattle() and tc:IsFaceup() and tc:IsControler(tp) then
-		local e2=Effect.CreateEffect(e:GetHandler())
+	--Increase the ATK of your monster but destroy it during at the End of the Damage Step
+	local at=Duel.GetBattleMonster(tp)
+	if at and at:IsFaceup() and at:IsRelateToBattle() then
+		--Gain 800 ATK per zone
+		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_UPDATE_ATTACK)
-		e2:SetValue(c*800)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e2)
-		local e3=Effect.CreateEffect(e:GetHandler())
+		e2:SetValue(zonect*800)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+		at:RegisterEffect(e2)
+		--Destroy it during the End of the Damage Step and change the opponent's monsters to Defense position
+		local e3=Effect.CreateEffect(c)
+		e3:SetDescription(aux.Stringid(id,2))
 		e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 		e3:SetRange(LOCATION_MZONE)
 		e3:SetCode(EVENT_DAMAGE_STEP_END)
-		e3:SetOperation(s.desop)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
 		e3:SetCountLimit(1)
-		tc:RegisterEffect(e3,true)
+		e3:SetOperation(s.desop)
+		e3:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE)
+		at:RegisterEffect(e3,true)
 	end
 end
-function s.disop3(e,tp)
-	local c=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if c==0 then return end
-	local dis1=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
-	local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-	if ct==nil then
-		e:GetHandler():RegisterFlagEffect(95100886,RESET_EVENT+RESETS_STANDARD,0,0,1)
-	else
-		ct=ct+1
-		e:GetHandler():SetFlagEffectLabel(95100886,ct)
+function s.disallop(e,tp)
+	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_COUNT)
+	local zones=Duel.SelectDisableField(tp,ct,LOCATION_MZONE,0,0)
+	for i=1,ct do
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
 	end
-	if c>1 then
-		local dis2=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,dis1)	
-		local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-		if ct==nil then
-			e:GetHandler():RegisterFlagEffect(95100886,RESET_EVENT+RESETS_STANDARD,0,0,1)
-		else
-			ct=ct+1
-			e:GetHandler():SetFlagEffectLabel(95100886,ct)
-		end
-		dis1=(dis1|dis2)
-		if c>2 then
-			local dis3=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,dis1)
-			local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-			if ct==nil then
-				e:GetHandler():RegisterFlagEffect(95100886,RESET_EVENT+RESETS_STANDARD,0,0,1)
-			else
-				ct=ct+1
-				e:GetHandler():SetFlagEffectLabel(95100886,ct)
-			end
-			dis1=(dis1|dis3)
-			if c>3 then
-				local dis4=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,dis1)
-				local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-				if ct==nil then
-					e:GetHandler():RegisterFlagEffect(95100886,RESET_EVENT+RESETS_STANDARD,0,0,1)
-				else
-					ct=ct+1
-					e:GetHandler():SetFlagEffectLabel(95100886,ct)
-				end
-				dis1=(dis1|dis4)
-				if c>4 then
-					local dis5=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,dis1)
-					local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-					if ct==nil then
-						e:GetHandler():RegisterFlagEffect(95100886,RESET_EVENT+RESETS_STANDARD,0,0,1)
-					else
-						ct=ct+1
-						e:GetHandler():SetFlagEffectLabel(95100886,ct)
-					end
-					dis1=(dis1|dis5)
-				end
-			end
-		end
-	end
-	return dis1
+	return zones
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.Destroy(e:GetHandler(),REASON_EFFECT) then
 		local g=Duel.GetMatchingGroup(Card.IsPosition,tp,0,LOCATION_MZONE,nil,POS_FACEUP_ATTACK)
+		if #g==0 then return end
 		Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
 	end
 end
---damage
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-	-- Duel.SetTargetPlayer(1-tp)
-		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,ct*500)
-	-- Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*500)
+	local ct=e:GetHandler():GetFlagEffect(id)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,ct*500)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	-- local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local ct=e:GetHandler():GetFlagEffectLabel(95100886)
-	-- Duel.Damage(p,ct*300,REASON_EFFECT)
+	local ct=e:GetHandler():GetFlagEffect(id)
 	Duel.Damage(tp,ct*500,REASON_EFFECT)
 	Duel.Damage(1-tp,ct*500,REASON_EFFECT)
 end
-
-

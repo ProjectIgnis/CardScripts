5d900c9bc0488ee6d9a69e4ec79bf39ee7d33946
Naim <naimsantos45@gmail.com>
2024-02-29 18:21:48 -0300

    "Libromancer Firestarter" update
    Use the proper event to detect and register the ATK/DEF change

--- official/c45001322.lua
+++ official/c45001322.lua
@@ -1,16 +1,16 @@
--- 
--- Libromancer Firestarter
--- Scripted by Hatter
+--リブロマンサー・ファイアスターター
+--Libromancer Firestarter
+--Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	-- Check materials on Ritual Summon
+	--Check materials use for its Ritual Summon
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetCode(EFFECT_MATERIAL_CHECK)
 	e0:SetValue(s.matcheck)
 	c:RegisterEffect(e0)
-	-- Cannot be destroyed by card effects
+	--Cannot be destroyed by card effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
@@ -19,7 +19,7 @@ function s.initial_effect(c)
 	e1:SetCondition(s.matcon)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	-- Cannot be banished by card effects
+	--Cannot be banished by card effects
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -29,39 +29,51 @@ function s.initial_effect(c)
 	e2:SetTarget(function(e,c,tp,r) return c==e:GetHandler() and r==REASON_EFFECT end)
 	e2:SetCondition(s.matcon)
 	c:RegisterEffect(e2)
-	-- Gain ATK/DEF
+	--Register a flag when an effect resolves
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_CHAINING)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(s.atkcon)
-	e3:SetOperation(s.atkop)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetOperation(s.chainop)
 	c:RegisterEffect(e3)
+	--Gains 200 ATK/DEF each time an effect activated by the opponent resolves
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_CHAIN_SOLVED)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.atkcon)
+	e4:SetOperation(s.atkop)
+	c:RegisterEffect(e4)
 end
-s.listed_series={0x17d}
+s.listed_series={SET_LIBROMANCER}
 function s.matcheck(e,c)
 	if c:GetMaterial():IsExists(Card.IsLocation,1,nil,LOCATION_MZONE) then
-		local reset=RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD
+		local reset=RESET_EVENT|RESETS_STANDARD&~RESET_TOFIELD
 		c:RegisterFlagEffect(id,reset,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,0))
 	end
 end
 function s.matcon(e)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_RITUAL) and c:GetFlagEffect(id)>0
+	return c:IsSummonType(SUMMON_TYPE_RITUAL) and c:HasFlagEffect(id)
+end
+function s.chainop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id+1,RESET_EVENT|RESETS_STANDARD|RESET_CHAIN|RESET_TURN_SET,0,1)
 end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep==1-tp and e:GetHandler():GetAttack()<3000
+	local c=e:GetHandler()
+	return ep==1-tp and c:GetAttack()<3000 and c:HasFlagEffect(id+1)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,id)
 	local c=e:GetHandler()
-	-- Gain ATK/DEF
+	--Gain 200 ATK/DEF
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
 	e1:SetValue(200)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)

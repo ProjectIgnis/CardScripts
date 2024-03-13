a233072ef80caac28d6bc9738d6c69bbcd055c35
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-03-13 19:06:52 +0200

    "Silent Wolf Calupo" fix
    Shouldn't be able to use "Tailor of the Fickle" with it.

--- official/c15947754.lua
+++ official/c15947754.lua
@@ -1,9 +1,9 @@
 --沈黙狼－カルーポ
---Omerta - Calupoh
+--Silent Wolf Calupo
 --scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
-	--Equip the top card
+	--Equip the top card of your Deck to this card
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	--Opponent must guess the card at the End Phase
+	--Your opponent calls the original type of the Equip Card
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TOGRAVE+CATEGORY_HANDES+CATEGORY_TOHAND)
@@ -23,9 +23,9 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_PHASE+PHASE_END)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,id)
-	e3:SetCondition(s.condition)
-	e3:SetTarget(s.target)
-	e3:SetOperation(s.operation)
+	e3:SetCondition(function(e) return e:GetHandler():GetEquipGroup():IsExists(Card.HasFlagEffect,1,nil,id) end)
+	e3:SetTarget(s.calltg)
+	e3:SetOperation(s.callop)
 	c:RegisterEffect(e3)
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -36,68 +36,53 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
 	local ec=Duel.GetDecktopGroup(tp,1):GetFirst()
-	if ec then
-		Duel.DisableShuffleCheck()
-		s.equipop(c,e,tp,ec)
-	end
-end
-function s.equipop(c,e,tp,tc)
-	if not c:EquipByEffectAndLimitRegister(e,tp,tc,id,false) then return end
-	--ATK increase
-	local e1=Effect.CreateEffect(tc)
+	if not ec then return end
+	Duel.DisableShuffleCheck()
+	if not c:EquipByEffectAndLimitRegister(e,tp,ec,id,false) then return end
+	--The equipped monster gains 500 ATK
+	local e1=Effect.CreateEffect(ec)
 	e1:SetType(EFFECT_TYPE_EQUIP)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(500)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	tc:RegisterEffect(e1)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	ec:RegisterEffect(e1)
 	--Equip limit
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_EQUIP_LIMIT)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetValue(s.eqlimit)
-	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-	tc:RegisterEffect(e2)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetValue(function(e,_c) return _c==c end)
+	e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+	ec:RegisterEffect(e2)
 end
-function s.eqlimit(e,c)
-	return c:GetControler()==e:GetHandlerPlayer()
-end
-function s.eqfilter(c)
-	return c:GetFlagEffect(id)>0
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=c:GetEquipGroup():Filter(s.eqfilter,nil)
-	return #g>0
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.calltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local c=e:GetHandler()
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,c,1,tp,0)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_HANDES,nil,0,1-tp,1)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,c,1,tp,0)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.callop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local eg=c:GetEquipGroup():Filter(s.eqfilter,nil)
-	if not eg or #eg==0 then return end
-	local op=Duel.SelectOption(1-tp,70,71,72)
-	Duel.ConfirmCards(1-tp,eg:GetFirst())
-	local res=s.testtype(op,eg:GetFirst())
-	if res then --correctly guessed
+	if not c:IsRelateToEffect(e) then return end
+	local ec=c:GetEquipGroup():Filter(Card.HasFlagEffect,nil,id):GetFirst()
+	if not ec then return end
+	local op=Duel.SelectOption(1-tp,DECLTYPE_MONSTER,DECLTYPE_SPELL,DECLTYPE_TRAP)
+	Duel.ConfirmCards(1-tp,ec)
+	local res=(op==0 and ec:IsMonsterCard())
+		or (op==1 and ec:IsSpellCard())
+		or (op==2 and ec:IsTrapCard())
+	if res then
+		--Send this card to the GY
 		Duel.SendtoGrave(c,REASON_EFFECT)
-	else --incorrectly guessed
+	else
+		--Discard 1 random card from your opponent's hand, and if you do, return this card to the hand
 		local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND,nil)
 		if #g==0 then return end
 		local sg=g:RandomSelect(1-tp,1)
-		if Duel.SendtoGrave(sg,REASON_DISCARD+REASON_EFFECT)>0 then
+		if Duel.SendtoGrave(sg,REASON_DISCARD|REASON_EFFECT)>0 then
 			Duel.SendtoHand(c,nil,REASON_EFFECT)
 		end
 	end
-end
-function s.testtype(op,c)
-	return (op==0 and c:IsOriginalType(TYPE_MONSTER))
-		or (op==1 and c:IsOriginalType(TYPE_SPELL))
-		or (op==2 and c:IsOriginalType(TYPE_TRAP))
 end

be7265d531aa1af2c53435dfc3a02354a7e91829
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-03-13 21:41:11 +0200

    Added new cards' scripts

---
+++ pre-release/c101205041.lua
@@ -0,0 +1,79 @@
+--ＤＰＡジャンダムーア
+--DPA Jeanne d’Moore
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Synchro Summon procedure
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	--Special Summon 1 Level 4 Cyberse monster from your GY in Defense Position
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) end)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Inflict an equal amount of damage to your opponent
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.damcon)
+	e2:SetCost(aux.selfbanishcost)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_CYBERSE) and c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+		if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+			--Negate its effects
+			sc:NegateEffects(c)
+		end
+		Duel.SpecialSummonComplete()
+	end
+	--Cannot Special Summon for the rest of this turn, except Cyberse monsters
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,c) return not c:IsRace(RACE_CYBERSE) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)  
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	if ep==tp then return false end
+	local ac,bc=Duel.GetBattleMonster(tp)
+	return ac and ac:IsRace(RACE_CYBERSE) and bc
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(ev)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ev)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
---
+++ pre-release/c101205051.lua
@@ -0,0 +1,100 @@
+--星界樹イルミスティル
+--Astral World Tree Illumistil
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Summon procedure
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3)
+	--You can only control 1 "Astral World Tree Illumistil"
+	c:SetUniqueOnField(1,0,id)
+	--Gain LP equal to the ATK of a monster(s) Special Summoned by your opponent
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(function(e,tp,eg) return Duel.IsMainPhase() and eg:IsExists(aux.FaceupFilter(Card.IsSummonPlayer,1-tp),1,nil) end)
+	e1:SetOperation(s.lpgainop)
+	c:RegisterEffect(e1)
+	--Make this card gain ATK based on how much LP you pay
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP,TIMING_DAMAGE_STEP|TIMINGS_CHECK_MONSTER_E)
+	e2:SetCountLimit(1)
+	e2:SetCondition(function() return not (Duel.IsPhase(PHASE_DAMAGE) and Duel.IsDamageCalculated()) end)
+	e2:SetCost(s.atkcost)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={id}
+function s.lpgainop(e,tp,eg,ep,ev,re,r,rp)
+	local val=eg:Filter(aux.FaceupFilter(Card.IsSummonPlayer,1-tp),nil):GetSum(Card.GetAttack)
+	if not Duel.IsChainSolving() then
+		if val>0 then
+			Duel.Hint(HINT_CARD,1-tp,id)
+			Duel.Recover(tp,val,REASON_EFFECT)
+		end
+	else
+		local eff=e:GetLabelObject()
+		if eff and not eff:IsDeleted() then
+			eff:SetLabel(eff:GetLabel()+val)
+		else
+			local c=e:GetHandler()
+			--Gain LP equal to the ATK of a monster(s) Special Summoned by your opponent at the end of the Chain Link
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_CHAIN_SOLVED)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetOperation(s.chainsolvedop)
+			e1:SetLabel(val)
+			e1:SetLabelObject(e)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_CHAIN)
+			c:RegisterEffect(e1)
+			e:SetLabelObject(e1)
+			--Reset the label object of "e" in the case that "e1" isn't executed
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e2:SetCode(EVENT_CHAIN_SOLVED)
+			e2:SetOperation(function() e:SetLabelObject(nil) end)
+			e2:SetReset(RESET_CHAIN)
+			Duel.RegisterEffect(e2,tp)
+		end
+	end
+end
+function s.chainsolvedop(e,tp,eg,ep,ev,re,r,rp)
+	local val=e:GetLabel()
+	if val>0 then
+		Duel.Hint(HINT_CARD,1-tp,id)
+		Duel.Recover(tp,val,REASON_EFFECT)
+	end
+	e:Reset()
+	e:GetLabelObject():SetLabelObject(nil)
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	local cost_options={}
+	for i=1,math.floor(math.min(Duel.GetLP(tp),3000)/1000) do
+		cost_options[i]=i*1000
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local lp_cost=Duel.AnnounceNumber(tp,cost_options)
+	e:SetLabel(lp_cost)
+	Duel.PayLPCost(tp,lp_cost)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		--Gains ATK equal to the amount of LP you paid
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(e:GetLabel())
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end

91a663f2df1c22ac3fb0d46dfef73e12e613d72e
Naim <naimsantos45@gmail.com>
2024-03-15 20:52:53 -0300

    update "Fabled Dianaira"
    Added a card hint to be shown when the effect is applied

--- official/c53199020.lua
+++ official/c53199020.lua
@@ -2,42 +2,43 @@
 --Fabled Dianaira
 local s,id=GetID()
 function s.initial_effect(c)
-	--summon with 1 tribute
+	--Can be Tribute Summoned by Tributing 1 "Fabled" monster
 	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),s.otfilter)
-	--change effect
+	--The effect of a Normal Spell activated by the opponent becomes "Your opponent discards 1 card"
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.chcon1)
+	e1:SetOperation(s.chop1)
+	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_CHAINING)
+	e2:SetCode(EVENT_CHAIN_ACTIVATING)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.chcon1)
-	e2:SetOperation(s.chop1)
+	e2:SetCondition(s.chcon2)
+	e2:SetOperation(s.chop2)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_CHAIN_ACTIVATING)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetCondition(s.chcon2)
-	e3:SetOperation(s.chop2)
-	c:RegisterEffect(e3)
 end
-s.listed_series={0x35}
+s.listed_series={SET_FABLED}
 function s.otfilter(c,tp)
-	return c:IsSetCard(0x35) and (c:IsControler(tp) or c:IsFaceup())
+	return c:IsSetCard(SET_FABLED) and (c:IsControler(tp) or c:IsFaceup())
 end
 function s.chcon1(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and re:GetHandler():GetType()==TYPE_SPELL and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+	return ep==1-tp and re:GetHandler():IsNormalSpell() and re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.chop1(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	return e:GetHandler():RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
 end
 function s.chcon2(e,tp,eg,ep,ev,re,r,rp)
-	return ev==1 and e:GetHandler():GetFlagEffect(id)>0
+	return ev==1 and e:GetHandler():HasFlagEffect(id)
 end
 function s.chop2(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.ChangeChainOperation(1,s.rep_op)
 end
 function s.rep_op(e,tp,eg,ep,ev,re,r,rp)
-	Duel.DiscardHand(1-tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
+	Duel.Hint(HINT_CARD,1-tp,id)
+	Duel.DiscardHand(1-tp,nil,1,1,REASON_EFFECT|REASON_DISCARD)
 end

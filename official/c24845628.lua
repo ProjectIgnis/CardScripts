e9b501bcc8b4e4651e97d911ac99da5c2cf34e58
Naim <naimsantos45@gmail.com>
2024-03-15 05:23:00 -0300

    "Magicalized Duston Mop" fix
    Fixed a bug where the search effect could be used even when the card was not destroyed by effects

--- official/c24845628.lua
+++ official/c24845628.lua
@@ -1,52 +1,50 @@
 --ダストンのモップ
+--Magicalized Duston Mop
 local s,id=GetID()
 function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
 	aux.AddEquipProcedure(c)
-	--
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_EQUIP)
-	e3:SetCode(EFFECT_UNRELEASABLE_SUM)
-	e3:SetValue(1)
+	--Equipped monster cannot be Tributed
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e2)
+	--Equipped monster cannot be used as material for a Fusion, Synchro, or Xyz Summon
+	local e3=e1:Clone()
+	e3:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+	e3:SetValue(aux.cannotmatfilter(SUMMON_TYPE_FUSION,SUMMON_TYPE_SYNCHRO,SUMMON_TYPE_XYZ))
 	c:RegisterEffect(e3)
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	--Search 1 "Duston" monster
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetCondition(s.thcon)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
 	c:RegisterEffect(e4)
-	local e5=e3:Clone()
-	e5:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
-	c:RegisterEffect(e5)
-	local e6=e3:Clone()
-	e6:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
-	c:RegisterEffect(e6)
-	local e7=e3:Clone()
-	e7:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
-	c:RegisterEffect(e7)
-	--search
-	local e7=Effect.CreateEffect(c)
-	e7:SetDescription(aux.Stringid(id,0))
-	e7:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e7:SetCode(EVENT_TO_GRAVE)
-	e7:SetCondition(s.thcon)
-	e7:SetTarget(s.thtg)
-	e7:SetOperation(s.thop)
-	c:RegisterEffect(e7)
 end
-s.listed_series={0x80}
+s.listed_series={SET_DUSTON}
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return re and rp~=tp and c:IsReason(REASON_DESTROY) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_ONFIELD)
+	return re and rp==1-tp and c:IsReason(REASON_DESTROY) and c:IsReason(REASON_EFFECT)
+		and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
-function s.filter(c)
-	return c:IsSetCard(0x80) and c:IsMonster() and c:IsAbleToHand()
+function s.thfilter(c)
+	return c:IsSetCard(SET_DUSTON) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)

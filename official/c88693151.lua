f77196ca74c15b02db8f94a7f74d3afe7596366c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-03-16 12:29:37 +0200

    "Trickstar Fusion" small script polishing

--- official/c88693151.lua
+++ official/c88693151.lua
@@ -2,14 +2,14 @@
 --Trickstar Fusion
 local s, id = GetID()
 function s.initial_effect(c)
-	--Fusion Summon 1 "Trickstar" onster
+	--Fusion Summon 1 "Trickstar" Fusion Monster using monsters from your hand or field as Fusion Material
 	c:RegisterEffect(Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,SET_TRICKSTAR)))
-	--Add 1 "Trickstar" monster from the GY to the hand
+	--Add 1 "Trickstar" monster from your GY to your hand
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_LEAVE_GRAVE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCost(aux.selfbanishcost)
 	e1:SetTarget(s.thtg)
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 end
 s.listed_series={SET_TRICKSTAR}
 function s.thfilter(c)
-	return c:IsMonster() and c:IsSetCard(SET_TRICKSTAR) and c:IsAbleToHand()
+	return c:IsSetCard(SET_TRICKSTAR) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
@@ -26,28 +26,27 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
-	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,tp,0)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) then
+		local code=tc:GetCode()
+		tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
+		--Cannot Normal Summon/Set or Special Summon that monster, or monsters with the same name for the rest of this turn
 		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id,2))
 		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_CANNOT_SUMMON)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetTargetRange(1,0)
-		e1:SetTarget(s.sumlimit)
-		e1:SetLabel(tc:GetCode())
+		e1:SetTarget(function(e,c) return c:HasFlagEffect(id) or c:IsCode(code) end)
 		e1:SetReset(RESET_PHASE|PHASE_END)
 		Duel.RegisterEffect(e1,tp)
 		local e2=e1:Clone()
-		e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e2:SetCode(EFFECT_CANNOT_MSET)
 		Duel.RegisterEffect(e2,tp)
 		local e3=e1:Clone()
-		e3:SetCode(EFFECT_CANNOT_MSET)
+		e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 		Duel.RegisterEffect(e3,tp)
 	end
-end
-function s.sumlimit(e,c)
-	return c:IsCode(e:GetLabel())
 end

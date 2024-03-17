ba8d3610916ce5be22ecdb68a74923e937fe09a6
Naim <naimsantos45@gmail.com>
2024-03-17 07:00:06 -0300

    "Nobleman of Extermination" fix
    Fixed a bug where it would be possible to activate it when it was the one Spell/Trap set on the field

--- official/c17449108.lua
+++ official/c17449108.lua
@@ -2,8 +2,9 @@
 --Nobleman of Extermination
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Destroy 1 face-down Spell/Trap card, banish it if it is a Trap and all cards with the same name
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -16,26 +17,28 @@ function s.filter(c)
 	return c:IsFacedown() and c:IsAbleToRemove()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:GetLocation()==LOCATION_SZONE and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil) end
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_SZONE) and s.filter(chkc) and chkc~=c end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,c) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,1,PLAYER_ALL,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsFacedown() and tc:IsRelateToEffect(e) then
-		if Duel.Destroy(tc,REASON_EFFECT,LOCATION_REMOVED)~=0 and tc:IsTrap() then
-			local code=tc:GetCode()
-			local g=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_DECK,LOCATION_DECK,nil,code)
-			Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
-			g=Duel.GetFieldGroup(tp,0,LOCATION_DECK)
-			Duel.ConfirmCards(tp,g)
-			g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
-			Duel.ConfirmCards(1-tp,g)
-			Duel.ShuffleDeck(tp)
-			Duel.ShuffleDeck(1-tp)
+	if tc:IsFacedown() and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT,LOCATION_REMOVED)>0 and tc:IsTrap() then
+		local code=tc:GetCode()
+		local g=Duel.GetFieldGroup(tp,0,LOCATION_DECK)
+		Duel.ConfirmCards(tp,g)
+		g=Duel.GetFieldGroup(tp,LOCATION_DECK,0)
+		Duel.ConfirmCards(1-tp,g)
+		Duel.ShuffleDeck(tp)
+		Duel.ShuffleDeck(1-tp)
+		local rmvg=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_DECK,LOCATION_DECK,nil,code)
+		if #rmvg>0 then
+			Duel.Remove(rmvg,POS_FACEUP,REASON_EFFECT)
 		end
 	end
 end

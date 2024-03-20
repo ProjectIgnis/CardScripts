7835ed303f4e69af5163bb9e9eb3b1699686dcc4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-03-19 17:42:18 -0700

    Fix "Natural Selection"
    Should not be able to target monsters without Levels/script clean-up.

--- unofficial/c511000370.lua
+++ unofficial/c511000370.lua
@@ -1,3 +1,4 @@
+--ナチュラル・セレクション
 --Natural Selection
 local s,id=GetID()
 function s.initial_effect(c)
@@ -7,37 +8,38 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function(e) return Duel.GetTurnPlayer()==1-e:GetHandlerPlayer()
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
-end
-function s.filter(c)
-	return c:IsFaceup() and c:IsDestructable()
+function s.filter(c,e,tp)
+	return c:IsFaceup() and c:GetLevel()>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,c:GetAttack(),c:GetDefense(),c:GetLevel(),c:GetRace(),c:GetAttribute())
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and 
-		Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,nil,nil,nil,nil,nil,nil) end
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil,e,tp) 
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
+	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
+	if Duel.Destroy(tc,REASON_EFFECT)>0 then
 		Duel.BreakEffect()
 		local token=Duel.CreateToken(tp,id+1)
+		--Change monster attributes
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK)
 		e1:SetValue(tc:GetAttack())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TOFIELD)
 		token:RegisterEffect(e1,true)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_SET_DEFENSE)
@@ -56,13 +58,14 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e5:SetValue(tc:GetAttribute())
 		token:RegisterEffect(e5,true)
 		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+		--"Selection Token" is destroyed during the End Phase
 		local e6=Effect.CreateEffect(e:GetHandler())
 		e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e6:SetCode(EVENT_PHASE+PHASE_END)
 		e6:SetRange(LOCATION_MZONE)
 		e6:SetCountLimit(1)
 		e6:SetOperation(s.desop)
-		e6:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e6:SetReset(RESET_EVENT|RESETS_STANDARD)
 		token:RegisterEffect(e6,true)
 	end
 end

428c279e16496191bf4bd080455cc05403aa25e8
Naim <naimsantos45@gmail.com>
2024-03-20 07:53:09 -0300

    "Trickstar Bella Madonna" fix
    - Fixed a bug where a non-Link monster that copied its effects would be able to use the effect to inflict damage
    - updated the conditions used by the first and second effect to better reflect the text.

--- official/c41302052.lua
+++ official/c41302052.lua
@@ -1,12 +1,11 @@
 --トリックスター・ベラマドンナ
 --Trickstar Bella Madonna
---
 local s,id=GetID()
 function s.initial_effect(c)
-	--link summon
-	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xfb),2)
 	c:EnableReviveLimit()
-	--immune
+	--Link Summon Procedure
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_TRICKSTAR),2)
+	--Unaffected by other card effects while it doesn't point to a monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
@@ -15,7 +14,7 @@ function s.initial_effect(c)
 	--e1:SetCondition(s.imcon) --handled in value for mid-resolution updating
 	e1:SetValue(s.immval)
 	c:RegisterEffect(e1)
-	--damage
+	--Inflict 200 damage to your opponent for each "Trickstar" monster in your GY with a different name
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_DAMAGE)
@@ -27,19 +26,19 @@ function s.initial_effect(c)
 	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0xfb}
+s.listed_series={SET_TRICKSTAR}
 function s.imcon(e)
 	local c=e:GetHandler()
-	return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetLinkedGroupCount()==0
+	return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetLinkedGroup():FilterCount(Card.IsMonster,nil)==0
 end
 function s.immval(e,te)
 	return te:GetOwner()~=e:GetHandler() and te:IsActivated() and s.imcon(e) --condition handling for mid-resolution updating
 end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetLinkedGroupCount()==0
+	return e:GetHandler():IsLinkMonster() and c:GetLinkedGroup():FilterCount(Card.IsMonster,nil)==0
 end
 function s.damfilter(c)
-	return c:IsSetCard(0xfb) and c:IsMonster()
+	return c:IsSetCard(SET_TRICKSTAR) and c:IsMonster()
 end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.damfilter,tp,LOCATION_GRAVE,0,1,nil) end

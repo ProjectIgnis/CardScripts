2df10b2afa45529399d71b1ec01b0783025a36ca
Naim <naimsantos45@gmail.com>
2024-03-24 06:09:28 -0300

    update c160016050.lua

--- rush/c160016050.lua
+++ rush/c160016050.lua
@@ -42,7 +42,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local d=Duel.TossDice(tp,1)
 	if Duel.Draw(tp,d,REASON_EFFECT)>0 and d<=3 and Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,nil)
 		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,2,nil)
 		if #g>0 then
 			Duel.SendtoHand(g,nil,REASON_EFFECT)

8dbc278fe43c7e70cbbe117487cfed1e882600e4
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-03-25 13:27:45 +0100

    added Absurd Void Hole

---
+++ rush/c160211021.lua
@@ -0,0 +1,53 @@
+--不条理の虚孔
+--Absurd Void Hole
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Negate opponent monster's attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttackTarget()
+	return tc and tc:IsFaceup() and tc:IsControler(tp) and tc:IsRace(RACE_CYBERSE) and tc:IsAttackPos()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local at=Duel.GetAttacker()
+	if chk==0 then return at:IsControler(1-tp) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	e1:SetTargetRange(0,1)
+	Duel.RegisterEffect(e1,tp)
+	local g2=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	for tc in g2:Iter() do
+		--Cannot be destroyed by battle
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(3000)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e2:SetValue(1)
+		e2:SetReset(RESETS_STANDARD_PHASE_END)
+		tc:RegisterEffectRush(e2)
+	end
+	local ac=Duel.GetAttacker()
+	local dc=Duel.GetAttackTarget()
+	local atkdiff=math.abs(ac:GetAttack()-dc:GetAttack())
+	if atkdiff>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.BreakEffect()
+		Duel.Recover(tp,atkdiff*2,REASON_EFFECT)
+	end
+end

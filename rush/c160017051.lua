88a041bf39ab73fdd1e8ba306776327423477cc5
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-03-26 13:07:09 +0100

    added Tribemirror Dragon

---
+++ rush/c160017051.lua
@@ -0,0 +1,52 @@
+--トライブミラー・ドラゴン
+--Tribemirror Dragon
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,s.ffilter,1,aux.FilterBoolFunction(Card.IsRace,RACE_DRAGON),2)
+	--1 monster gains effect protection and piercing
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsLevel(7) and c:IsRace(RACE_DRAGON,scard,sumtype,tp) and c:IsAttribute(ATTRIBUTE_EARTH,scard,sumtype,tp)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+	local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g,true)
+		local tc=g:GetFirst()
+		local c=e:GetHandler()
+		--Protection
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetDescription(3001)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		e1:SetValue(1)
+		tc:RegisterEffectRush(e1)
+		tc:AddPiercing(RESETS_STANDARD_PHASE_END,c)
+	end
+end

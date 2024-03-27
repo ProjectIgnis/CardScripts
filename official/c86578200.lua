e296506c891a85decb26d9d3693a9de728aa76e5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-03-27 13:51:56 +0200

    "Abyss Prop - Wild Wagon" fixes
    - Shouldn't be able to target a monster that has already had this effect applied to it.
    - Fixed an issue where the targeting protection effect wouldn't wear off correctly if control of the monster changed.
    - The last effect should check that the player has an "Abyss Actor" monster face-up in their Extra Deck when the effect would be activated, not when this card is destroyed.

--- official/c86578200.lua
+++ official/c86578200.lua
@@ -1,68 +1,66 @@
---魔界大道具 「ニゲ馬車」
+--魔界大道具「ニゲ馬車」
+--Abyss Prop - Wild Wagon
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--The first time each "Abyss Actor" monster you control would be destroyed by battle each turn, it is not destroyed
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_ABYSS_ACTOR))
+	e1:SetValue(function(e,re,r,rp) return r&REASON_BATTLE>0 and 1 or 0 end)
 	c:RegisterEffect(e1)
-	--indes
+	--Apply a "your opponent cannot target it with card effects until the end of their turn" effect on 1 "Abyss Actor" monster you control
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x10ec))
-	e2:SetValue(s.indct)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.cannottgtg)
+	e2:SetOperation(s.cannottgop)
 	c:RegisterEffect(e2)
-	--cannot be target
+	--Return all cards your opponent controls to the hand
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetCountLimit(1)
-	e3:SetTarget(s.tgtg)
-	e3:SetOperation(s.tgop)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
-	--return
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_TOHAND)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_DESTROYED)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetCondition(s.thcon)
-	e4:SetTarget(s.thtg)
-	e4:SetOperation(s.thop)
-	c:RegisterEffect(e4)
 end
-s.listed_series={0x10ec}
-function s.indct(e,re,r,rp)
-	if (r&REASON_BATTLE)~=0 then
-		return 1
-	else return 0 end
+s.listed_series={SET_ABYSS_ACTOR}
+function s.cannottgfilter(c)
+	return c:IsSetCard(SET_ABYSS_ACTOR) and c:IsFaceup() and not c:HasFlagEffect(id)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0x10ec)
-end
-function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+function s.cannottgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.cannottgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.cannottgfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,s.cannottgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.tgop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+function s.cannottgop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
+	if tc:IsRelateToEffect(e) then
+		local ct=Duel.IsTurnPlayer(tp) and 2 or 1
+		tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,ct)
+		--Your opponent cannot target it with card effects until the end of their turn
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3061)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-		e1:SetRange(LOCATION_MZONE)
 		e1:SetValue(aux.tgoval)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		e1:SetReset(RESETS_STANDARD_PHASE_END,ct)
 		tc:RegisterEffect(e1)
 	end
 end
@@ -70,12 +68,12 @@ function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return rp==1-tp and c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp)
 		and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEDOWN)
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_ABYSS_ACTOR),tp,LOCATION_EXTRA,0,1,nil)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,tp,0)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,nil)

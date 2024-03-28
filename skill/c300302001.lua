c4520ac2c8b8d673faf92a58a121c1ca3cd43548
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-03-28 13:44:25 +0200

    "Fusion Party!" update
    Updated and simplified its script to use the Fusion Summoning procedure.

--- skill/c300302001.lua
+++ skill/c300302001.lua
@@ -1,73 +1,38 @@
 --Fusion Party!
 local s,id=GetID()
-local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return aux.CanActivateSkill(tp) and s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0) and Duel.GetFlagEffect(tp,id)==0
+	return aux.CanActivateSkill(tp) and s.cost(e,tp,eg,ep,ev,re,r,rp,0) and not Duel.HasFlagEffect(tp,id)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--fusion
-	local g2=s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0)
+	--You can only apply this effect once per Duel
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
-	s.fusTarget(e,tp,eg,ep,ev,re,r,rp,1)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
-	local sg1=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-	tc:CompleteProcedure()
-	end
+	--Discard 1 card
+	s.cost(e,tp,eg,ep,ev,re,r,rp,1)
+	--Fusion Summon 1 Fusion Monster using monsters from your hand or field as material
+	Fusion.SummonEffTG()(e,tp,eg,ep,ev,re,r,rp,1)
+	Fusion.SummonEffOP()(e,tp,eg,ep,ev,re,r,rp)
 end
-function s.cfilter(c,e,tp)
+function s.costfilter(c,e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsDiscardable() then return false end
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp)
-	local res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg1-c,nil,chkf)
-	if not res then
-		local ce=Duel.GetChainMaterial(tp)
-		if ce~=nil then
-			local fgroup=ce:GetTarget()
-			local mg2=fgroup(ce,e,tp)
-			local mf=ce:GetValue()
-			res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg2-c,mf,chkf)
-		end
+	local params={extrafil=s.fextra(c)}
+	return Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp,eg,ep,ev,re,r,rp) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST|REASON_DISCARD,nil,e,tp,eg,ep,ev,re,r,rp)
+end
+function s.fextra(exc)
+	return function(e,tp,mg)
+		return nil,s.fcheck(exc)
 	end
-	return res
 end
-function s.fusfilter(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and (not f or f(c)) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function s.fusTarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST+REASON_DISCARD,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+function s.fcheck(exc)
+	return function(tp,sg,fc)
+		return not (exc and sg:IsContains(exc))
+	end
 end

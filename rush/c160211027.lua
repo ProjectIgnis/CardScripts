c4e66b96201f8c2a855bd43dcca34c0eaea0e26f
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-03-28 11:40:35 +0100

    added Magnet Citadel Jar

---
+++ rush/c160211027.lua
@@ -0,0 +1,42 @@
+--マグネ・シタデルポッド
+--Magnet Citadel Jar
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	c:RegisterFlagEffect(FLAG_TRIPLE_TRIBUTE,0,0,1)
+	--Summon with 3 tribute
+	local e1=aux.AddNormalSummonProcedure(c,true,true,3,3,SUMMON_TYPE_TRIBUTE+1,aux.Stringid(id,0),aux.FilterBoolFunction(Card.IsRace,RACE_ROCK))
+	--Destroy 1 card your opponent controls
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsStatus(STATUS_SUMMON_TURN) and c:IsSummonType(SUMMON_TYPE_TRIBUTE+1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsNotMaximumModeSide,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_ONFIELD)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,e:GetHandler(),1,tp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=Duel.SelectMatchingCard(tp,Card.IsNotMaximumModeSide,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #dg>0 then
+		dg=dg:AddMaximumCheck()
+		Duel.HintSelection(dg,true)
+		if Duel.Destroy(dg,REASON_EFFECT)>0 and Duel.IsPlayerCanDraw(tp,1) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.BreakEffect()
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end

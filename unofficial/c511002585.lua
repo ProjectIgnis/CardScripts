1585313831385ede73ef7f52ca4ccdd35707fcfb
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-03-29 09:11:25 -0700

    Update"Revival Ticket"
    - Should be able to activate it anytime a monster you control is destroyed by battle, not just at the end of your opponent's Battle Phase.
    - Should only Special Summon from the Graveyards.
    - Was missing the Blue-Eyes Spirit check to determine the number of available zones.

--- unofficial/c511002585.lua
+++ unofficial/c511002585.lua
@@ -1,15 +1,17 @@
+--リバイバル・チケット
 --Revival Ticket
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Special Summon monsters you control destroyed by battle from the GYs
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(id)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+	--Battle Damage register
 	aux.GlobalCheck(s,function()
 		s[0]=0
 		s[1]=0
@@ -18,12 +20,6 @@ function s.initial_effect(c)
 		ge1:SetCode(EVENT_BATTLE_DAMAGE)
 		ge1:SetOperation(s.checkop)
 		Duel.RegisterEffect(ge1,0)
-		local ge3=Effect.CreateEffect(c)
-		ge3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge3:SetCode(EVENT_PHASE+PHASE_BATTLE)
-		ge3:SetOperation(s.trigop)
-		ge3:SetCountLimit(1)
-		Duel.RegisterEffect(ge3,0)
 		aux.AddValuesReset(function()
 			s[0]=0
 			s[1]=0
@@ -38,14 +34,11 @@ function s.checkop(e,tp,eg,ep,ev,re,r,rp)
 		s[1-tp]=s[1-tp]+ev
 	end
 end
-function s.trigop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RaiseEvent(e:GetHandler(),id,e,REASON_RULE,Duel.GetTurnPlayer(),1-Duel.GetTurnPlayer(),ev)
+function s.cfilter(c,tp,tid)
+	return c:IsPreviousControler(tp) and c:IsMonster() and c:GetTurnID()==tid and c:GetReason()&REASON_BATTLE~=0
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
-end
-function s.cfilter(c,tp,tid)
-	return c:IsPreviousControler(tp) and c:GetTurnID()==tid and c:GetReason()&REASON_BATTLE~=0
+    	return eg:IsExists(s.cfilter,1,nil,tp,Duel.GetTurnCount())
 end
 function s.ftchk(ft,tp,ct,e)
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,0,e:GetHandler())
@@ -54,12 +47,13 @@ function s.ftchk(ft,tp,ct,e)
 	end
 end
 function s.chkfilter(c,e,tp)
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_GRAVE)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.cfilter,tp,0x7f,0x7f,nil,tp,Duel.GetTurnCount())
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil,tp,Duel.GetTurnCount())
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local ct=#g
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	s.ftchk(ft,tp,ct,e)
 	if chk==0 then return ct>0 and ft>=ct and g:FilterCount(s.chkfilter,nil,e,tp)==#g
 		and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,0,ct,e:GetHandler()) end
@@ -68,13 +62,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,ct,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,s[tp])
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.cfilter,tp,0x7f,0x7f,nil,tp,Duel.GetTurnCount())
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil,tp,Duel.GetTurnCount())
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local ct=#g
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
 	s.ftchk(ft,tp,ct,e)
 	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,0,e:GetHandler())
-	if ft<ct or g:FilterCount(aux.NecroValleyFilter(s.chkfilter),nil,e,tp)~=#g then return end
+	if ft<ct or g:FilterCount(s.chkfilter,nil,e,tp)~=#g then return end
 	ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local dg=Group.CreateGroup()
 	for i=1,ct do

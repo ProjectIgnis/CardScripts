3394ef4485d086c69945e6cd49c570c1cb5d54e0
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-04-03 12:03:42 +0200

    added Magnet Thunder Charge

---
+++ rush/c160211035.lua
@@ -0,0 +1,46 @@
+--マグネット・サンダーチャージ
+--Magnet Thunder Charge
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsMonster() and c:IsRace(RACE_ROCK) and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_ROCK) and c:GetBaseAttack()==3500
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+	if Duel.SendtoGrave(g,REASON_COST)<1 then return end
+	--Effect
+	local sg=Duel.GetMatchingGroup(aux.FilterMaximumSideFunctionEx(Card.IsMonster),tp,0,LOCATION_MZONE,nil)
+	if Duel.Draw(tp,2,REASON_EFFECT)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) 
+		and #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local tg=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(Card.IsMonster),tp,0,LOCATION_MZONE,1,1,nil)
+		if #tg>0 then 
+			tg=tg:AddMaximumCheck()
+			Duel.HintSelection(tg)
+			Duel.Destroy(tg,REASON_EFFECT) 
+		end
+	end
+end

c984cd62b5e0dff8e4a40db7b97d013836bcb2ee
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-04-03 13:33:40 +0300

    "Pain and Gain" update
    Remove duplicate function and general update.

--- unofficial/c152000001.lua
+++ unofficial/c152000001.lua
@@ -3,40 +3,33 @@
 --Scripted by The Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
-	--skill
+	--Activate
 	aux.AddVrainsSkillProcedure(c,s.flipcon,s.flipop)
 end
-s.listed_series={0xfc}
-function s.gkfilter(c,e,tp,tid)
-	return c:IsReason(REASON_BATTLE) and c:GetTurnID()==tid and c:IsPreviousLocation(LOCATION_MZONE)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp) and c:IsSetCard(0xfc)
-end
+s.listed_series={SET_GOUKI}
 function s.gkfilter(c,tid)
-	return c:IsReason(REASON_BATTLE) and c:GetTurnID()==tid and c:IsPreviousLocation(LOCATION_MZONE) and c:IsSetCard(0xfc)
+	return c:IsSetCard(SET_GOUKI) and c:IsReason(REASON_BATTLE) and c:GetTurnID()==tid and c:IsPreviousSetCard(SET_GOUKI)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
+	if Duel.HasFlagEffect(tp,id) then return false end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if ft<=0 then return false end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	local g=Duel.GetMatchingGroup(s.gkfilter,tp,LOCATION_ALL-LOCATION_MZONE,0,nil,Duel.GetTurnCount())
-	return #g>0 and ft>=#g and g:FilterCount(Card.IsCanBeSpecialSummoned,nil,e,0,tp,false,false,POS_FACEUP,tp)==#g
+	local g=Duel.GetMatchingGroup(s.gkfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED|LOCATION_EXTRA,0,nil,Duel.GetTurnCount())
+	return #g>0 and ft>=#g and g:FilterCount(Card.IsCanBeSpecialSummoned,nil,e,0,tp,false,false)==#g
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	--opd check and ask if you want to activate the skill or not
-	if Duel.GetFlagEffect(tp,id)>0 or not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
-	--opd register
-	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	--Ask the player if they want to activate the Skill
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--check if skill is negated
+	--This Skill can only be used once per Duel
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	--Check if the Skill is negated ("Anti Skill")
 	if aux.CheckSkillNegation(e,tp) then return end
-	--Special Summon
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 then return end
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	local g=Duel.GetMatchingGroup(s.gkfilter,tp,LOCATION_ALL-LOCATION_MZONE,0,nil,Duel.GetTurnCount())
-	if not (#g>0 and ft>=#g and g:FilterCount(Card.IsCanBeSpecialSummoned,nil,e,0,tp,false,false,POS_FACEUP,tp)==#g) then return end
-	Duel.HintSelection(g)
-	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	--Special Summon all your "Gouki" monsters that were destroyed by battle this turn
+	local g=Duel.GetMatchingGroup(s.gkfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED|LOCATION_EXTRA,0,nil,Duel.GetTurnCount()):Filter(Card.IsCanBeSpecialSummoned,nil,e,0,tp,false,false)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end

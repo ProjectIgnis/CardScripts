7df9be80731dcc3428cbf6e9f7bdfdf3d50d3882
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-04-04 20:21:25 -0700

    Fix "Raidraptor - Noir Lanius"
    Level change should not reset at the End Phase.

--- official/c99707692.lua
+++ official/c99707692.lua
@@ -90,7 +90,7 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_LEVEL)
 		e1:SetValue(value)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
 end

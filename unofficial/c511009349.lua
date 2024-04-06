ae67e9f4e10b66aa2b7af6167fb552956da916aa
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-04-06 20:21:19 +0300

    "Raidraptor - Blade Burner Falcon (Manga)" fix
    Prevent a script error + its destruction effect should be Continuous.

--- unofficial/c511009349.lua
+++ unofficial/c511009349.lua
@@ -1,5 +1,5 @@
---ＲＲ－ブレード・バーナー・ファルコン (Anime)
---Raidraptor - Blade Burner Falcon (Anime)
+--ＲＲ－ブレード・バーナー・ファルコン (Manga)
+--Raidraptor - Blade Burner Falcon (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
@@ -11,25 +11,19 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.atkcond)
+	e1:SetCondition(function(e,tp) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and Duel.GetLP(1-tp)-Duel.GetLP(tp)>=3000 end)
 	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 	--Destroy monsters on the field up to the number of materials this card has
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_BATTLE_DESTROYING)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCondition(aux.bdcon)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetCondition(s.descon)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 end
-function s.atkcond(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and Duel.GetLP(1-tp)-Duel.GetLP(tp)>=3000
-end
-function s.atktg(e,tp,eg,ep,ev,re,r,rp)
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,3000)
 end
@@ -45,16 +39,15 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1)
 	end
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local numxyz=e:GetHandler():GetOverlayCount()
-	if chk==0 then return numxyz>0 and Duel.IsExistingMatchingCard(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,math.min(#g,numxyz),tp,0)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsRelateToBattle() and c:GetOverlayCount()>0 and Duel.IsExistingMatchingCard(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.SelectYesNo(tp,aux.Stringid(id,1)) then return end
 	local numxyz=e:GetHandler():GetOverlayCount()
 	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	if #g>1 and numxyz>0 then
+	if numxyz>0 and #g>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 		local sg=g:Select(tp,1,numxyz,nil)
 		Duel.HintSelection(sg,true)

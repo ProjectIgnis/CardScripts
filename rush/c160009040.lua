20a1384e5fdcb24ec2e34675ac973b6031b7f355
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-04-08 12:38:11 +0300

    "Hero of the Sweep" fix
    Undeclared variable was causing a script error.

--- rush/c160009040.lua
+++ rush/c160009040.lua
@@ -4,13 +4,13 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	-- “Hero of the Yeast” + “Year-Old Broom”
+	--Fusion Materials
 	Fusion.AddProcMix(c,true,true,160305015,160009007)
-	-- Gain ATK/DEF
+	--Make this card gain 200 ATK for each EARTH monster you control
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
 	e1:SetCost(s.atkcost)
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 end
-s.listed_materials={160305015,160009007}
+s.listed_materials={160305015,160009007} --"Hero of the Yeast", "One Year Broom"
 function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
 end
@@ -34,13 +34,14 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	--Effect
 	local ct=Duel.GetMatchingGroupCount(aux.FilterMaximumSideFunctionEx(s.atkfilter),tp,LOCATION_MZONE,0,nil)
 	if ct>0 then
+		local c=e:GetHandler()
 		--Increase ATK/DEF
-		local e1=Effect.CreateEffect(e:GetHandler())
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(ct*200)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
 		c:RegisterEffect(e1)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_UPDATE_DEFENSE)

eb28c3ed7f427f0fcc62a0eb567d1a803bdf4088
Edoardo Lolletti <edoardo762@gmail.com>
2024-04-08 13:13:14 +0200

    Update Destiny HERO - Departed
    Fix interaction with Destiny Hero - Dunker and other effects discarding it as cost

--- official/c39829561.lua
+++ official/c39829561.lua
@@ -1,7 +1,8 @@
 --D－HERO ディパーテッドガイ
+--Destiny HERO - Departed
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon
+	--Special Summon itself from the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
@@ -13,32 +14,32 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	--redirect
+	--Banish when destroyed by battle or sent to the GY from the Deck or Hand by an effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_TO_GRAVE_REDIRECT)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetCondition(s.recon)
-	e2:SetValue(LOCATION_REMOVED)
+	e2:SetValue(s.redirectval)
 	c:RegisterEffect(e2)
-	if departchk==nil then departchk=0 departchktable={} else departchk=departchk+1 end
-	departchktable[departchk]={} departchktable[departchk][-1]={} 
-	departchktable[departchk][0]=0 departchktable[departchk][-1][0]=0
+	--Infinite loop prevention when only forced triggers are into play (Slifer the Sky Dragon, King Tiger Wanghu, etc.)
+	local departchktable={}
+	departchktable[-1]={}
+	departchktable[0]=0
+	departchktable[-1][0]=0
 	local ge1=Effect.CreateEffect(c)
 	ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	ge1:SetCode(EVENT_CHAINING)
-	ge1:SetLabel(departchk)
-	ge1:SetLabelObject(e1)
+	ge1:SetLabelObject({e1,departchktable})
 	ge1:SetOperation(s.checkop)
 	Duel.RegisterEffect(ge1,0)
-	e1:SetLabel(departchk)
+	e1:SetLabelObject(departchktable)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	if tp~=Duel.GetTurnPlayer() then return false end
-	local num=e:GetLabel()
-	if departchktable[num][0]~=departchktable[num][-1][0] or departchktable[num][1]==nil then return true end
-	for i=1,departchktable[num][0] do
-		if departchktable[num][i]~=departchktable[num][-1][i] then return true end
+	if not Duel.IsTurnPlayer(tp) then return false end
+	local departchktable=e:GetLabelObject()
+	if departchktable[0]~=departchktable[-1][0] or departchktable[1]==nil then return true end
+	for i=1,departchktable[0] do
+		if departchktable[i]~=departchktable[-1][i] then return true end
 	end
 	return false
 end
@@ -51,36 +52,39 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(e:GetHandler(),0,tp,1-tp,false,false,POS_FACEUP_ATTACK)
 	end
 end
-function s.recon(e)
+function s.redirectval(e,c,reason)
 	local c=e:GetHandler()
-	return (c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_BATTLE))
-		or (c:IsLocation(LOCATION_DECK+LOCATION_HAND) and c:IsReason(REASON_EFFECT))
+	if (c:IsLocation(LOCATION_MZONE) and reason==REASON_BATTLE) or
+		(c:IsLocation(LOCATION_DECK|LOCATION_HAND) and reason==REASON_EFFECT) then
+		return LOCATION_REMOVED
+	end
+	return 0
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local num=e:GetLabel()
-	if re==e:GetLabelObject() then
-		if departchktable[num][1]==nil then
-			departchktable[num][1]=re
-			departchktable[num][0]=1
+	local labelEff,departchktable=table.unpack(e:GetLabelObject())
+	if re==labelEff then
+		if departchktable[1]==nil then
+			departchktable[1]=re
+			departchktable[0]=1
 		else
-			for i=1,departchktable[num][0] do
-				departchktable[num][-1][i]=departchktable[num][i]
+			for i=1,departchktable[0] do
+				departchktable[-1][i]=departchktable[i]
 			end
-			departchktable[num][1]=re
-			departchktable[num][-1][0]=departchktable[num][0]
-			departchktable[num][0]=1
+			departchktable[1]=re
+			departchktable[-1][0]=departchktable[0]
+			departchktable[0]=1
 		end
 	else
 		if not re:IsHasType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_QUICK_F) then
-			for i=1,departchktable[num][0] do
-				departchktable[num][-1][i]=nil
-				departchktable[num][i]=nil
+			for i=1,departchktable[0] do
+				departchktable[-1][i]=nil
+				departchktable[i]=nil
 			end
-			departchktable[num][-1][0]=0
-			departchktable[num][0]=0
-		elseif departchktable[num][0]>0 then
-			departchktable[num][0]=departchktable[num][0]+1
-			departchktable[num][departchktable[num][0]]=re
+			departchktable[-1][0]=0
+			departchktable[0]=0
+		elseif departchktable[0]>0 then
+			departchktable[0]=departchktable[0]+1
+			departchktable[departchktable[0]]=re
 		end
 	end
 end

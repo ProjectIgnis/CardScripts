404de9932dd01ed667d10aff76d0d3509e761dde
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-04-08 23:23:44 +0300

    Added new cards' scripts

---
+++ pre-release/c101205019.lua
@@ -0,0 +1,91 @@
+--幻禄の天盃龍
+--Tenpai Dragon of Genroku
+--Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon this card as a Tuner
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_LVCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function(e) return not e:GetHandler():IsReason(REASON_DRAW) end)
+	e1:SetTarget(s.selfsptg)
+	e1:SetOperation(s.selfspop)
+	c:RegisterEffect(e1)
+	--Special Summon 1 "Tenpai Dragon" monster from your Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetHintTiming(0,TIMING_MAIN_END|TIMING_BATTLE_START|TIMING_BATTLE_END|TIMINGS_CHECK_MONSTER_E)
+	e2:SetCost(aux.selfreleasecost)
+	e2:SetTarget(s.dsptg)
+	e2:SetOperation(s.dspop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_TENPAI_DRAGON}
+s.listed_names={id}
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	c:AssumeProperty(ASSUME_TYPE,c:GetType()|TYPE_TUNER)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	c:AssumeProperty(ASSUME_TYPE,c:GetType()|TYPE_TUNER)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		--Treat this card as a Tuner
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_ADD_TYPE)
+		e1:SetValue(TYPE_TUNER)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e1)
+		if not Duel.SelectYesNo(tp,aux.Stringid(id,2)) then return end
+		Duel.BreakEffect()
+		--Increase its Level by 1
+		local e2=e1:Clone()
+		e2:SetProperty(0)
+		e2:SetCode(EFFECT_UPDATE_LEVEL)
+		e2:SetValue(1)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e2)
+	end
+end
+function s.dspfilter(c,e,tp)
+	return c:IsSetCard(SET_TENPAI_DRAGON) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.dsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetMZoneCount(tp,e:GetHandler())>0
+		and Duel.IsExistingMatchingCard(s.dspfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.dspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then 
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.dspfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+	--Cannot Special Summon monsters, except Dragon monsters
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,3))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(_,c) return not c:IsRace(RACE_DRAGON) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
---
+++ pre-release/c101205073.lua
@@ -0,0 +1,72 @@
+--燦幻開花
+--Sangen Kaihou
+--Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	--End the Main Phase
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.phcon)
+	e1:SetOperation(s.phop)
+	c:RegisterEffect(e1)
+	--Special Summon any number of "Tenpai Dragon" monsters from your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetHintTiming(0,TIMING_MAIN_END|TIMING_BATTLE_END|TIMINGS_CHECK_MONSTER_E)
+	e2:SetCondition(function() return Duel.GetFlagEffect(0,id)+Duel.GetFlagEffect(1,id)>=3 end)
+	e2:SetCost(aux.selfbanishcost)
+	e2:SetTarget(s.drtg)
+	e2:SetOperation(s.drop)
+	c:RegisterEffect(e2)
+	--Count attacks declared this turn
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ATTACK_ANNOUNCE)
+		ge1:SetOperation(function(_,_,_,ep) Duel.RegisterFlagEffect(ep,id,RESET_PHASE|PHASE_END,0,1) end)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+s.listed_series={SET_TENPAI_DRAGON}
+function s.phconfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_DRAGON)
+end
+function s.phcon(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsMainPhase() then return false end
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	return #g>0 and g:FilterCount(s.phconfilter,nil)==#g and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>#g
+end
+function s.phop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SkipPhase(1-tp,Duel.GetCurrentPhase(),RESET_PHASE|PHASE_END,1)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_TENPAI_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.Draw(tp,1,REASON_EFFECT)==0 then return end
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
+	local ct=math.min(#g,Duel.GetLocationCount(tp,LOCATION_MZONE))
+	if ct<=0 or not Duel.SelectYesNo(tp,aux.Stringid(id,2)) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
+	Duel.ShuffleHand(tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:Select(tp,1,ct,nil)
+	if #sg>0 then
+		Duel.BreakEffect()
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

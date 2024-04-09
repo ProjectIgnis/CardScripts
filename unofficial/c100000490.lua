bb2aa578521ff2ea084d0103d0684a5cb0e1ca53
Edoardo Lolletti <edoardo762@gmail.com>
2024-04-09 11:57:50 +0200

    Modernize and cleanup some cards changed by previous commit
    Halfway to Forever
    Gazer Shark
    Galaxy Tyranno
    Geargia Change
    Artifacts Unleashed
    Advanced Heraldry Art
    Shining Hope Road
    Destiny overlay
    Dimension Xyz
    The Phantom Knights' Twin Medals
    D/D/D Xyz
    Salamangreat Zebroid X (Anime)

--- unofficial/c100000490.lua
+++ unofficial/c100000490.lua
@@ -15,70 +15,56 @@ end
 function s.filter(c,e)
 	return c:IsFaceup() and c:IsCanBeEffectTarget(e) and not c:IsType(TYPE_TOKEN)
 end
-function s.xyzfilter(c,mg,sc,set)
-	local reset={}
-	if not set then
-		for tc in aux.Next(mg) do
-			local e1=Effect.CreateEffect(sc)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-			e1:SetCode(EFFECT_XYZ_MATERIAL)
-			tc:RegisterEffect(e1)
-			table.insert(reset,e1)
-		end
-	end
-	local res=c:IsXyzSummonable(nil,mg,#mg,#mg)
-	for _,te in ipairs(reset) do
-		te:Reset()
-	end
-	return res
+function s.registerxyzmateffect(e,tp)
+	local matEff=Effect.CreateEffect(e:GetHandler())
+	matEff:SetType(EFFECT_TYPE_FIELD)
+	matEff:SetCode(EFFECT_XYZ_MATERIAL)
+	matEff:SetTargetRange(0,LOCATION_MZONE)
+	Duel.RegisterEffect(matEff,tp)
+	return matEff
 end
-function s.rescon(set)
-	return function(sg,e,tp,mg)
-				return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,sg,e:GetHandler(),set)
-			end
+function s.xyzfilter(c,mg,fg,minc,maxg)
+	return c:IsXyzSummonable(mg,fg,minc,maxg)
+end
+function s.rescon(exg)
+	return function(sg)
+		return exg:IsExists(Card.IsXyzSummonable,1,nil,nil,sg,#sg,#sg)
+	end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
-	if chk==0 then return aux.SelectUnselectGroup(mg,e,tp,nil,nil,s.rescon(false),0) end
-	local reset={}
-	for tc in aux.Next(mg) do
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetCode(EFFECT_XYZ_MATERIAL)
-		tc:RegisterEffect(e1)
-		table.insert(reset,e1)
+	if chk==0 then
+		local matEff=s.registerxyzmateffect(e,tp)
+		local res=Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,mg)
+		matEff:Reset()
+		return res
 	end
-	local tg=aux.SelectUnselectGroup(mg,e,tp,nil,nil,s.rescon(true),1,tp,HINTMSG_XMATERIAL,s.rescon(true))
+	local matEff=s.registerxyzmateffect(e,tp)
+	local exg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,mg)
+	local tg=aux.SelectUnselectGroup(mg,e,tp,nil,nil,s.rescon(exg),1,tp,HINTMSG_XMATERIAL,s.rescon(exg))
+	matEff:Reset()
 	Duel.SetTargetCard(tg)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-	for _,te in ipairs(reset) do
-		te:Reset()
-	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetTargetCards(e)
-	local reset={}
-	for tc in aux.Next(g) do
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetCode(EFFECT_XYZ_MATERIAL)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		table.insert(reset,e1)
-	end
-	local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,g,c,true)
+	local matEff=s.registerxyzmateffect(e,tp)
+	local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,nil,g,#g,#g)
 	if #xyzg>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
 		Duel.XyzSummon(tp,xyz,g,nil,#g,#g)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SPSUMMON_COST)
+		e1:SetOperation(function()
+			matEff:Reset()
+		end)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		xyz:RegisterEffect(e1,true)
 	else
-		for _,te in ipairs(reset) do
-			te:Reset()
-		end
+		matEff:Reset()
 	end
 end

bf1b31c0a8ba0e3b5ac96d61154c405a05837ab1
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-04-16 10:17:37 +0200

    added Mage Power

---
+++ rush/c160402039.lua
@@ -0,0 +1,26 @@
+--魔導師の力
+--Mage Power
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--equip
+	aux.AddEquipProcedure(c,0,s.eqfilter,s.eqlimit)
+	--Atk Change
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.value)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
+end
+function s.eqfilter(c)
+	return c:IsFaceup() and not c:IsMaximumModeSide()
+end
+function s.eqlimit(e,c)
+	return c:IsFaceup()
+end
+function s.value(e,c)
+	return Duel.GetMatchingGroupCount(Card.IsSpellTrap,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,nil)*500
+end

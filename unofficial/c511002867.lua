37fb56d17a86a430c1e1fe8d3f12c82a36ba6bc3
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-04-20 07:32:37 -0700

    Fix "D-Faceoff"
    Incorrect operation category/general script polish

--- unofficial/c511002867.lua
+++ unofficial/c511002867.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--damage
+	--Inflict damage to the player with the lowest total monster Levels
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_DAMAGE)
@@ -19,7 +19,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.damtg)
 	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
-	--atk
+	--Increase the ATK of 1 "Destiny HERO" monster by the difference in Levels x 100
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
@@ -33,6 +33,7 @@ function s.initial_effect(c)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
 end
+s.listed_series={SET_DESTINY_HERO}
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g1=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
 	local lv1=g1:GetSum(Card.GetLevel)
@@ -40,10 +41,10 @@ function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local lv2=g2:GetSum(Card.GetLevel)
 	if chk==0 then return true end
 	if lv1>lv2 then
-		Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,1-tp,(lv1-lv2)*100)
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,(lv1-lv2)*100)
 	end
 	if lv2<lv1 then
-		Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,(lv2-lv1)*100)
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,(lv2-lv1)*100)
 	end
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
@@ -66,14 +67,11 @@ function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local lv2=g2:GetSum(Card.GetLevel)
 	return lv1>lv2
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(0xc008)
-end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	if chkc then return chkc:IsFaceup() and chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsSetCard(SET_DESTINY_HERO) end
+	if chk==0 then return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsSetCard,SET_DESTINY_HERO),tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsSetCard,SET_DESTINY_HERO),tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g1=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
@@ -88,7 +86,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		e1:SetValue(atk)
 		tc:RegisterEffect(e1)
 	end

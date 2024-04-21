8a58e3bc5785110839e6c6f86129b65a6309ecb5
XGlitchy30 <antonio.buglione2014@gmail.com>
2024-04-21 19:15:24 +0200

    Fix Chain Destruction

--- official/c1248895.lua
+++ official/c1248895.lua
@@ -17,25 +17,64 @@ function s.initial_effect(c)
 	local e3=e1:Clone()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
+	--Hidden Token to check eligible names when Tokens/Extra Deck Monsters are Summoned
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_CONTINUOUS|EFFECT_TYPE_FIELD)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+		ge1:SetOperation(s.createtoken)
+		Duel.RegisterEffect(ge1,0)
+	end)
 end
-function s.filter(c,e)
-	return c:IsFaceup() and c:IsAttackBelow(2000) and c:IsCanBeEffectTarget(e)
+function s.filter(c,e,codechk)
+	return c:IsFaceup() and c:IsAttackBelow(2000) and (not e or c:IsCanBeEffectTarget(e)) and (not codechk or s.mdnamecheck(c))
+end
+function s.mdnamecheck(c)
+	if c:IsType(TYPE_TOKEN|TYPE_EXTRA) or c:IsHasEffect(EFFECT_CHANGE_CODE) then
+		local codes={c:GetCode()}
+		for _,code in ipairs(codes) do
+			s.CheckCodeToken:Recreate(code)
+			if not s.CheckCodeToken:IsType(TYPE_TOKEN|TYPE_EXTRA) then
+				return true
+			end
+		end
+		return false
+	end
+	return true
+end
+function s.publicfilter(c,p,...)
+	return c:IsCode(...) and (c:IsPublic() or (c:IsLocation(LOCATION_DECK) and c:GetSequence()==Duel.GetFieldGroupCount(p,LOCATION_DECK,0)-1 and Duel.IsPlayerAffectedByEffect(p,EFFECT_REVERSE_DECK)))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return eg:IsContains(chkc) end
-	if chk==0 then return eg:IsExists(s.filter,1,nil,e) end
+	if chkc then return eg:IsContains(chkc) and s.filter(chkc,nil,true) end
+	if chk==0 then return eg:IsExists(s.filter,1,nil,e,true) end
+	local tc
 	if #eg==1 then
 		Duel.SetTargetCard(eg)
+		tc=eg:GetFirst()
 	else
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-		local g=eg:FilterSelect(tp,s.filter,1,1,nil,e)
+		local g=eg:FilterSelect(tp,s.filter,1,1,nil,e,true)
 		Duel.SetTargetCard(g)
+		tc=g:GetFirst()
+	end
+	local p=tc:GetControler()
+	local public=Duel.GetMatchingGroup(s.publicfilter,p,LOCATION_DECK|LOCATION_HAND,0,nil,p,tc:GetCode())
+	if #public>0 then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,public,#public,p,LOCATION_DECK|LOCATION_HAND)
+	else
+		Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,p,LOCATION_DECK|LOCATION_HAND)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	local tpe=tc:GetType()
-	if (tpe&TYPE_TOKEN)~=0 then return end
-	local dg=Duel.GetMatchingGroup(Card.IsCode,tc:GetControler(),LOCATION_DECK+LOCATION_HAND,0,nil,tc:GetCode())
+	if not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
+	local dg=Duel.GetMatchingGroup(Card.IsCode,tc:GetControler(),LOCATION_DECK|LOCATION_HAND,0,nil,tc:GetCode())
 	Duel.Destroy(dg,REASON_EFFECT)
+end
+
+function s.createtoken(e)
+	s.CheckCodeToken=Duel.CreateToken(0,0)
+	e:Reset()
 end

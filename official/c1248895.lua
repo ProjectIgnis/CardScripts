c2c8de60950c34927d1f1f5d7a73b5f3d414ce44
XGlitchy30 <antonio.buglione2014@gmail.com>
2024-04-22 19:09:26 +0200

    Update c1248895.lua

--- official/c1248895.lua
+++ official/c1248895.lua
@@ -19,7 +19,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.filter(c,e,tp)
-	if not (c:IsFaceup() and c:IsAttackBelow(12000) and (not e or c:IsCanBeEffectTarget(e))) then return false end
+	if not (c:IsFaceup() and c:IsAttackBelow(2000) and (not e or c:IsCanBeEffectTarget(e))) then return false end
 	local p=c:GetControler()
 	if p==tp then
 		return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK|LOCATION_HAND,0,1,nil,c:GetCode())

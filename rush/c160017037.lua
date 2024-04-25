9daa33a4b9714c2447888f6104e291c9b6117d59
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-04-25 13:07:28 +0200

    added Fadronir the Pure

---
+++ rush/c160017037.lua
@@ -0,0 +1,41 @@
+--純真のファドロニール
+--Fadronir the Pure
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_DRAW)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsMainPhase() then
+		Duel.RegisterFlagEffect(rp,id,RESET_PHASE|PHASE_END,0,1)
+	end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFlagEffect(tp,id)==0 and e:GetHandler():IsStatus(STATUS_SUMMON_TURN)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(tp,1,REASON_EFFECT)
+	Duel.Draw(1-tp,1,REASON_EFFECT)
+end

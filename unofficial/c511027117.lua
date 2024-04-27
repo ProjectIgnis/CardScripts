05f8846dcd2415f67ca04acd039b8a68407c7a56
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-04-27 09:22:19 -0700

    Fix "Gorgonic Guardian (Anime)"
    Was not setting the ATK of the negated monster to 0/script modernization.

--- unofficial/c511027117.lua
+++ unofficial/c511027117.lua
@@ -1,11 +1,11 @@
---ゴルゴニック・ガーディアン
---Gorgonic Guardian (anime)
+--ゴルゴニック・ガーディアン (Anime)
+--Gorgonic Guardian (Anime)
 --Scripted by the Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,3,2)
 	c:EnableReviveLimit()
+	--Xyz Summon procedure
+	Xyz.AddProcedure(c,nil,3,2)
 	--Destroy monsters with 0 ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -13,62 +13,48 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
-	--Negate Effect/0 ATK
+	--Negate the effects of 1 monster on the field and change its ATK to 0
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(s.cost)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetCost(aux.dxmcostgen(1,1,nil))
+	e2:SetTarget(s.diszatg)
+	e2:SetOperation(s.diszaop)
 	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
 end
-function s.filter1(c,e)
+function s.desfilter(c,e)
 	return c:GetAttack()==0 and c:IsPosition(POS_FACEUP) and c:IsDestructable(e) and not c:IsImmuneToEffect(e)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
 	if #g>0 then
 		Duel.Hint(HINT_CARD,1-tp,id)
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	c:RemoveOverlayCard(tp,1,1,REASON_COST)
+function s.diszafilter(c)
+	return c:IsFaceup() and c:HasNonZeroAttack() and c:IsNegatableMonster()
 end
-function s.filter2(c,e,tp)
-	return c:IsFaceup() and (c:GetAttack()>0 or not c:IsDisabled())
+function s.diszatg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.diszafilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,0,LOCATION_MZONE)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.diszaop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.diszafilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	Duel.HintSelection(g)
 	local tc=g:GetFirst()
 	if tc then
+		tc:NegateEffects(c)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		local r1=tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		local r2=tc:RegisterEffect(e2)
-		if not r1 and not r2 then
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
-			e3:SetValue(0)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc:RegisterEffect(e3)
-		end
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
 	end
 end

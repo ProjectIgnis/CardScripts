b850e67032a6faa355c9a2aab18778960bc20e73
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-04-29 14:00:28 +0300

    "Number C39: Utopia Ray" fix
    Its effect should resolve doing nothing if the player has more than 1000 LP on resolution.

--- official/c56840427.lua
+++ official/c56840427.lua
@@ -1,49 +1,45 @@
---CNo.39 希望皇ホープレイ
---Number C39:Utopia Ray
+--ＣＮｏ．３９ 希望皇ホープレイ
+--Number C39: Utopia Ray
 local s,id=GetID()
 function s.initial_effect(c)
-	--Xyz Summon
 	c:EnableReviveLimit()
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),4,3,s.ovfilter,aux.Stringid(id,1))
+	--Xyz Summon procedure
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),4,3,s.ovfilter,aux.Stringid(id,0))
 	--Increase ATK by 1000 for each Xyz Material detached
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function(e,tp) return Duel.GetLP(tp)<=1000 end)
 	e1:SetCost(aux.dxmcostgen(1,1,nil))
-	e1:SetOperation(s.operation)
+	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 end
 s.xyz_number=39
-s.listed_names={84013237}
+s.listed_names={84013237} --"Number 39: Utopia"
 function s.ovfilter(c,tp,lc)
 	return c:IsFaceup() and c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,84013237)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetLP(tp)<=1000
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLP(tp)>1000 then return end
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(500)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
-		c:RegisterEffect(e1)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-		if #g>0 then
-			Duel.HintSelection(g)
-			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_UPDATE_ATTACK)
-			e2:SetValue(-1000)
-			e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
-			g:GetFirst():RegisterEffect(e2)
-		end
+	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
+	--This card gains 500 ATK until the end of this turn
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(500)
+	e1:SetReset(RESETS_STANDARD_PHASE_END)
+	c:RegisterEffect(e1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local sc=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	if sc then
+		Duel.HintSelection(sc)
+		--Loses 1000 ATK until the end of this turn
+		local e2=e1:Clone()
+		e2:SetValue(-1000)
+		sc:RegisterEffect(e2)
 	end
 end

9311ab584a86038a3115640eb8212575c03eefe0
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-05-08 13:59:05 +0200

    added Deity of Seven Treasures - Heitai

---
+++ rush/c160017042.lua
@@ -0,0 +1,60 @@
+--七宝神－米大
+--Deity of Seven Treasures - Heitai
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCondition(s.spcon)
+	c:RegisterEffect(e0)
+	--Send the top 2 cards from the Deck to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160007052}
+function s.cfilter(c)
+	return c:IsCode(160007052) or (c:IsMonster() and c:IsDefense(2700))
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.cfilter,c:GetControler(),LOCATION_GRAVE,0,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,2,tp,0)
+end
+function s.atkfilter(c)
+	return c:IsMonster() and c:IsFaceup() and c:GetAttack()>0
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.DiscardDeck(tp,2,REASON_EFFECT)==0 then return end
+	if Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(s.atkfilter),tp,0,LOCATION_MZONE,1,nil) 
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+		local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.atkfilter),tp,0,LOCATION_MZONE,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-700)
+			e1:SetReset(RESETS_STANDARD_PHASE_END,2)
+			g:GetFirst():RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			g:GetFirst():RegisterEffect(e2)
+		end
+	end
+end

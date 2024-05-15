1f266a6c8328f12611a0060075948999b8301bb4
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-05-15 11:23:29 +0200

    added Copycat

---
+++ rush/c160402040.lua
@@ -0,0 +1,63 @@
+--ものマネ幻想師
+--Copycat
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsStatus(STATUS_SUMMON_TURN|STATUS_SPSUMMON_TURN)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:GetTextAttack()>=100 and c:IsNotMaximumModeSide()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,nil,1,tp,100)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		local tc=g:GetFirst()
+		local atk=tc:GetTextAttack()
+		if tc:IsMaximumMode() then
+			atk=tc:GetMaximumAttack()
+		end
+		local def=tc:GetTextDefense()
+		--Gains ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e1)
+		if def>=100 then
+			--Gains DEF
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			e2:SetValue(def)
+			e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+			c:RegisterEffect(e2)
+		end
+	end
+end

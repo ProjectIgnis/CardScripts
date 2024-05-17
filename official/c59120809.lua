9047451e28379e8b2f160237770d03892a18498f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-05-17 11:59:20 +0300

    "Scareclaw Tri-Heart" fix
    Fixed an issue with its unaffected effect.

--- official/c59120809.lua
+++ official/c59120809.lua
@@ -4,52 +4,47 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	--Link Summon procedure: 3 Effect Monsters
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3,3)
-	--Must be Link Summoned
+	c:AddMustBeLinkSummoned()
+	--All face-up monsters on the field are changed to Defense Position
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.lnklimit)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SET_POSITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(function(e,c) return c:IsFaceup() end)
+	e1:SetValue(POS_FACEUP_DEFENSE)
 	c:RegisterEffect(e1)
-	--Change monsters to Defense Position
+	--Unaffected by the activated effects of Defense Position monsters
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SET_POSITION)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(function(_,c) return c:IsFaceup() end)
-	e2:SetValue(POS_FACEUP_DEFENSE)
+	e2:SetValue(s.immval)
 	c:RegisterEffect(e2)
-	--Unaffected by activated effects of Defense Position monsters
+	--Special Summon 1 Level 3 "Scareclaw" monster from your GY and add 1 "Scareclaw" monster from your Deck to your hand
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_IMMUNE_EFFECT)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetValue(s.immval)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_EMZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
-	--Special Summon 1 "Scareclaw" and add another to the hand
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e4:SetType(EFFECT_TYPE_IGNITION)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetRange(LOCATION_EMZONE)
-	e4:SetCountLimit(1)
-	e4:SetTarget(s.sptg)
-	e4:SetOperation(s.spop)
-	c:RegisterEffect(e4)
 end
 s.listed_series={SET_SCARECLAW}
 function s.immval(e,te)
-	if not (te:IsMonsterEffect() and te:IsActivated() and te:GetActivateLocation()==LOCATION_MZONE) then return false end
+	local trig_loc,trig_pos=Duel.GetChainInfo(0,CHAININFO_TRIGGERING_LOCATION,CHAININFO_TRIGGERING_POSITION)
+	if not (te:IsMonsterEffect() and te:IsActivated() and trig_loc==LOCATION_MZONE) then return false end
 	local tc=te:GetHandler()
-	if not Duel.IsChainSolving() then return tc:IsDefensePos() end
-	if tc:IsRelateToEffect(te) then
+	if not Duel.IsChainSolving() or (tc:IsRelateToEffect(te) and tc:IsFaceup() and tc:IsLocation(trig_loc)) then
 		return tc:IsDefensePos()
 	else
-		return Duel.GetChainInfo(0,CHAININFO_TRIGGERING_POSITION)==POS_DEFENSE
+		return trig_pos&POS_DEFENSE>0
 	end
 end
 function s.spfilter(c,e,tp)
@@ -65,13 +60,12 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and s.spfilter(tc,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if #g>0 then
@@ -79,17 +73,14 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 			Duel.ConfirmCards(1-tp,g)
 		end
 	end
-	--Cannot Special Summon, except "Scareclaw" monsters
+	--Cannot Special Summon monsters for the rest of this turn, except "Scareclaw" monsters
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(s.splimit)
+	e1:SetTarget(function(e,c) return not c:IsSetCard(SET_SCARECLAW) end)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return not c:IsSetCard(SET_SCARECLAW)
 end

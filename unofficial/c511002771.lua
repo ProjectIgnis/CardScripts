e8719e0b553a72738f98841282e25794f7dc29f0
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-05-22 09:11:35 -0700

    Fix "Silent Wobby (Anime)"
    Added a check to prevent monsters that couldn't have their positions changed/added category info/slight modernization update

--- unofficial/c511002771.lua
+++ unofficial/c511002771.lua
@@ -1,27 +1,29 @@
---サイレント・ウォビー
+--サイレント・ウォビー (Anime)
+--Silent Wobby (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon
+	--Change 1 monster your opponent controls to Defense Position and take control of it
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION+CATEGORY_CONTROL)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(s.sumtg)
-	e1:SetOperation(s.sumop)
+	e1:SetTarget(s.posctrltg)
+	e1:SetOperation(s.posctrlop)
 	c:RegisterEffect(e1)
 end
-function s.filter(c)
-	return not c:IsPosition(POS_FACEUP_DEFENSE) and c:IsControlerCanBeChanged()
+function s.posctrlfilter(c)
+	return not c:IsPosition(POS_FACEUP_DEFENSE) and c:IsCanChangePosition() and c:IsControlerCanBeChanged()
 end
-function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+function s.posctrltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.posctrlfilter,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.posctrlfilter,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
 end
-function s.sumop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+function s.posctrlop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.posctrlfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.HintSelection(g)

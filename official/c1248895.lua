0cd37567a4f945b906c9359a7f25b16e15a0ead5
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-05-23 13:01:27 +0300

    Some simplifications

--- official/c1248895.lua
+++ official/c1248895.lua
@@ -4,6 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -18,60 +19,52 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 end
-
-function s.filter(c,e,tp)
-	if not (c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsAttackBelow(2000) and (not e or c:IsCanBeEffectTarget(e))) then return false end
-	local p=c:GetControler()
-	if p==tp then
-		return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK|LOCATION_HAND,0,1,nil,c:GetCode())
+function s.tgfilter(c,e,tp)
+	if not (c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsAttackBelow(2000) and c:IsCanBeEffectTarget(e)) then return false end
+	if c:IsControler(tp) then
+		return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,c:GetCode())
 	else
-		return Duel.GetFieldGroupCount(p,LOCATION_HAND|LOCATION_DECK,0)>0 and s.mdnamecheck(c)
+		return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_HAND|LOCATION_DECK,0)>0 and s.mdnamecheck(c)
 	end
 end
 function s.mdnamecheck(c)
-	if c:IsType(TYPE_TOKEN|TYPE_EXTRA) or c:IsHasEffect(EFFECT_CHANGE_CODE) then
-		local codes={c:GetCode()}
-		for _,code in ipairs(codes) do
-			local typ=Duel.GetCardTypeFromCode(code)
-			if typ&(TYPE_TOKEN|TYPE_EXTRA)==0 then
-				return true
-			end
+	local codes={c:GetCode()}
+	for _,code in ipairs(codes) do
+		local typ=Duel.GetCardTypeFromCode(code)
+		if typ&(TYPE_TOKEN|TYPE_EXTRA)==0 then
+			return true
 		end
-		return false
 	end
-	return true
+	return false
 end
-function s.publicfilter(c,p,...)
-	return c:IsCode(...) and (c:IsPublic() or (c:IsLocation(LOCATION_DECK) and c:GetSequence()==Duel.GetFieldGroupCount(p,LOCATION_DECK,0)-1 and Duel.IsPlayerAffectedByEffect(p,EFFECT_REVERSE_DECK)))
+function s.publicfilter(c,convulsion,top_card,...)
+	return c:IsCode(...) and (c:IsPublic() or (convulsion and c==top_card))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return eg:IsContains(chkc) and s.filter(chkc,nil,tp) end
-	if chk==0 then return eg:IsExists(s.filter,1,nil,e,tp) end
-	local tc
-	if #eg==1 then
-		Duel.SetTargetCard(eg)
-		tc=eg:GetFirst()
+	local tg=eg:Filter(s.tgfilter,nil,e,tp)
+	if chkc then return tg:IsContains(chkc) end
+	if chk==0 then return #tg>0 end
+	local tc=nil
+	if #tg==1 then
+		tc=tg:GetFirst()
 	else
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-		local g=eg:FilterSelect(tp,s.filter,1,1,nil,e,tp)
-		Duel.SetTargetCard(g)
-		tc=g:GetFirst()
+		tc=tg:Select(tp,1,1,nil):GetFirst()
 	end
-	local p=tc:GetControler()
-	if p==tp then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,p,LOCATION_DECK|LOCATION_HAND)
-	else
-		local public=Duel.GetMatchingGroup(s.publicfilter,p,LOCATION_DECK|LOCATION_HAND,0,nil,p,tc:GetCode())
-		if #public>0 then
-			Duel.SetOperationInfo(0,CATEGORY_DESTROY,public,#public,p,LOCATION_DECK|LOCATION_HAND)
-		else
-			Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,p,LOCATION_DECK|LOCATION_HAND)
-		end
+	Duel.SetTargetCard(tc)
+	local ctrl_player=tc:GetControler()
+	local top_card=Duel.GetDecktopGroup(ctrl_player,1):GetFirst()
+	local convulsion=Duel.IsPlayerAffectedByEffect(ctrl_player,EFFECT_REVERSE_DECK)
+	local dg=Duel.GetMatchingGroup(s.publicfilter,ctrl_player,LOCATION_HAND|LOCATION_DECK,0,nil,convulsion,top_card,tc:GetCode())
+	if #dg>0 then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,tp,0)
 	end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,ctrl_player,LOCATION_HAND|LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
-	local dg=Duel.GetMatchingGroup(Card.IsCode,tc:GetControler(),LOCATION_DECK|LOCATION_HAND,0,nil,tc:GetCode())
-	Duel.Destroy(dg,REASON_EFFECT)
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local g=Duel.GetMatchingGroup(Card.IsCode,tc:GetControler(),LOCATION_HAND|LOCATION_DECK,0,nil,tc:GetCode())
+		Duel.Destroy(g,REASON_EFFECT)
+	end
 end

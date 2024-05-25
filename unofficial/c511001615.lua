557a1ecf3a771722860388889ee30a65e12dad09
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-05-25 14:20:03 -0700

    Fix "Xyz Spiral"
    Should only draw 1 card at the end of the Battle Phase/script modernization update and clean-up.

--- unofficial/c511001615.lua
+++ unofficial/c511001615.lua
@@ -1,3 +1,4 @@
+--エクシーズ・スパイラル
 --Xyz Spiral
 local s,id=GetID()
 function s.initial_effect(c)
@@ -5,46 +6,32 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Inflict damage to your opponent equal to the Rank of attacking Xyz monster x 200
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_PIERCE)
-	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_XYZ))
-	e1:SetValue(1)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetCondition(s.damcon)
+	e1:SetOperation(function(_,tp) Duel.Damage(1-tp,Duel.GetAttacker():GetRank()*200,REASON_EFFECT) end
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	--
+	--Draw 1 card at the end of the Battle Phase
 	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e2:SetCondition(s.damcon)
-	e2:SetOperation(s.damop)
-	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e2:SetCountLimit(1)
+	e2:SetOperation(function(_,tp) Duel.Draw(tp,1,REASON_EFFECT) end)
+	e2:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e2,tp)
 end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	local a=Duel.GetAttacker()
 	local d=Duel.GetAttackTarget()
 	if not d then return false end
-	return Duel.GetTurnPlayer()==tp and Duel.GetBattleDamage(1-tp)>0 and a:IsType(TYPE_XYZ) 
-		and a:IsHasEffect(EFFECT_PIERCE) and d:IsDefensePos()
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	Duel.ChangeBattleDamage(1-tp,a:GetRank()*200)
-	--draw
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
-	e1:SetCountLimit(1)
-	e1:SetOperation(s.drop)
-	e1:SetReset(RESET_PHASE+PHASE_BATTLE)
-	Duel.RegisterEffect(e1,tp)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Draw(tp,1,REASON_EFFECT)
+	return a:IsType(TYPE_XYZ) and d:IsDefensePos() and a:GetAttack()>d:GetDefense()
 end

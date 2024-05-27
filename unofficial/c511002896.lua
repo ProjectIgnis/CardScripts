f7bd296ee916c99c9a593bd92c8624519056d44c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-05-27 10:54:23 +0300

    "Starliege Paladynamo (Anime)" fix
    Fixed an incorrect check in the condition of the ATK changing effect + added a hint timing for the Damage Step.

--- unofficial/c511002896.lua
+++ unofficial/c511002896.lua
@@ -3,55 +3,49 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Xyz Summon Procedure
+	--Xyz Summon procedure: 2 Level 4 LIGHT monsters
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),4,2)
-	--Change a monster's ATK to 0 and negate the effects of all other cards
+	--Change a monster's ATK to 0 and negate the effects of all other face-up cards on the field
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.condition)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) and Duel.IsBattlePhase() and not (Duel.IsPhase(PHASE_DAMAGE) and Duel.IsDamageCalculated()) end)
 	e1:SetCost(aux.dxmcostgen(1,1,nil))
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
-	--Draw 1 card when it is destroyed by the opponent
+	--Draw 1 card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCode(EVENT_DESTROYED)
 	e2:SetCondition(s.drcon)
 	e2:SetTarget(s.drtg)
 	e2:SetOperation(s.drop)
 	c:RegisterEffect(e2)
-	--Double Snare
-	aux.DoubleSnareValidity(c,LOCATION_MZONE)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local ph=Duel.GetCurrentPhase()
-	return Duel.IsTurnPlayer(tp) and Duel.IsBattlePhase()
-		and not (Duel.IsPhase(PHASE_DAMAGE) or Duel.IsDamageCalculated())
+function s.atkfilter(c)
+	return c:IsType(TYPE_XYZ) and not c:IsAttack(0) and c:IsFaceup()
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:GetAttack()>0
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
-	local tc=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
-	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,tc,1,0,-tc:GetAttack())
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.atkfilter(chkc) end
 	local g=Duel.GetMatchingGroup(Card.IsNegatable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+	if chk==0 then return #g>0 and Duel.IsExistingTarget(s.atkfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local tc=Duel.SelectTarget(tp,s.atkfilter,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,tc,1,tp,-tc:GetAttack())
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,tp,0)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and tc:GetAttack()>0 then
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsAttack(0) then
 		local c=e:GetHandler()
 		--Target's ATK becomes 0
 		local e1=Effect.CreateEffect(c)
@@ -60,8 +54,11 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(0)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE)
 		tc:RegisterEffect(e1)
+		Duel.AdjustInstantly(tc)
+		if not tc:IsAttack(0) then return end
 		--Negate the effects of all other cards until the End of the Battle Phase
-		local g=Duel.GetMatchingGroup(Card.IsNegatable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,c)
+		local exc=c:IsRelateToEffect(e) and c or nil
+		local g=Duel.GetMatchingGroup(Card.IsNegatable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,exc)
 		if #g==0 then return end
 		g:ForEach(function(nc) nc:NegateEffects(c,RESET_PHASE|PHASE_BATTLE,true) end)
 	end
@@ -69,7 +66,7 @@ end
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_BATTLE|REASON_EFFECT)
-		and c:IsPreviousControler(tp) and c:GetReasonPlayer()==1-tp
+		and c:IsPreviousControler(tp) and c:IsReasonPlayer(1-tp)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

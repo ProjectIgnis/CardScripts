6f07f2670321fe1754aa2815eb1cf9606bd5d0f0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-05-30 12:15:00 +0300

    "Acceleration" fix
    Should be able to chain it to an LP recovering effect only if the player is affected by an effect that turns LP gain into damage. Also, shouldn't be able to use it if only the opponent would receive damage.

--- unofficial/c150000001.lua
+++ unofficial/c150000001.lua
@@ -2,8 +2,9 @@
 --Acceleration
 local s,id=GetID()
 function s.initial_effect(c)
-	--reflect
+	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCondition(s.condition)
@@ -12,24 +13,24 @@ function s.initial_effect(c)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
-	if ex then return true end
+	if ex and (cp==tp or cp==PLAYER_ALL) then return true end
 	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
-	return ex 
+	return ex and (cp==tp or cp==PLAYER_ALL) and Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+	--Make that effect damage to you 0
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetTargetRange(1,0)
 	e1:SetLabel(cid)
-	e1:SetValue(s.refcon)
+	e1:SetValue(s.value)
 	e1:SetReset(RESET_CHAIN)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.refcon(e,re,val,r,rp,rc)
-	local cc=Duel.GetCurrentChain()
+function s.value(e,re,val,r,rp,rc)
 	local cc=Duel.GetCurrentChain()
 	if cc==0 or (r&REASON_EFFECT)==0 then return end
 	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)

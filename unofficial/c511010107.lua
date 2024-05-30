8a88b38b0fa33a18e65b06b79fd1228006ae34c7
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-05-30 15:30:02 +0300

    "Number 107: Galaxy-Eyes Tachyon Dragon (Anime)" fixes
    - Should count effects that resolved even before this card was face-up on the field + heavily simplified the logic used there.
    - Fixed an issue where the ATK gain wouldn't be applied if the 2nd player used the effect.
    - Keep track of which card was the last one to declare an attack and only grant the additional attack to this card if the last attacker was this card.
    - Grant the additional attack only if this card has declared exactly 1 attack during this Battle Phase.
    - Don't allow using this effect at the start of the Battle Phase.
    - Don't allow this effect to be used if another copy of this card has already used this effect this Battle Phase.
    - After resolving this effect during your turn, if this card cannot perform an additional attack, or if it stops being face-up on the field, or if it attacks and the end of the Damage Step is reached, or if it attacks and it ever stops battling, then end the Battle Phase immediately.

--- unofficial/c511010107.lua
+++ unofficial/c511010107.lua
@@ -1,161 +1,178 @@
---No.107 銀河眼の時空竜 (Anime)
+--Ｎｏ．１０７ 銀河眼の時空竜 (Anime)
 --Number 107: Galaxy-Eyes Tachyon Dragon (Anime)
---fixed and cleaned up by MLD
---fixed by Larry126
+--scripted by MLD, Larry126, and pyrQ
 Duel.LoadCardScript("c88177324.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,8,2)
 	c:EnableReviveLimit()
-	--battle indestructable
+	--Xyz Summon procedure: 2 Level 8 monsters
+	Xyz.AddProcedure(c,nil,8,2)
+	--Cannot be destroyed by battle, except with a "Number" monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
 	c:RegisterEffect(e1)
-	--negate opponent's turn
+	--Negate the effects of all other face-up monsters currently on the field, also their ATK and DEF become their original ATK and DEF
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_PHASE|PHASE_BATTLE)
+	e2:SetCode(EVENT_PHASE+PHASE_BATTLE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(function(_,tp) return Duel.IsTurnPlayer(1-tp) end)
-	e2:SetCost(s.negcost)
-	e2:SetTarget(s.negtg)
-	e2:SetOperation(s.negop)
+	e2:SetCondition(function(e,tp) return Duel.IsTurnPlayer(1-tp) end)
+	e2:SetCost(aux.dxmcostgen(1,1,nil))
+	e2:SetTarget(s.distg)
+	e2:SetOperation(s.disop)
 	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
-	--negate
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_DISABLE+CATEGORY_ATKCHANGE)
+	--Workaround to make the extra attack part work on your turn
+	local e3=e2:Clone()
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
-	e3:SetCondition(s.negcon)
-	e3:SetCost(s.negcost)
-	e3:SetTarget(s.negtg)
-	e3:SetOperation(s.negop)
+	e3:SetCondition(s.discon)
+	e3:SetCost(s.discost)
 	c:RegisterEffect(e3,false,REGISTER_FLAG_DETACH_XMAT)
+	--Global effects to keep track of total effect resolutions and the last attacker
 	aux.GlobalCheck(s,function()
-		--Cards that resolved effects check
-		BPResolvedEffects={}
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_CHAIN_SOLVED)
-		e1:SetCondition(function() return Duel.IsBattlePhase() end)
-		e1:SetOperation(s.regop)
-		Duel.RegisterEffect(e1,0)
-		aux.AddValuesReset(function()
-			BPResolvedEffects={}
-		end)
+		--Keep track of how many effects have resolved during each Battle Phase
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_CHAIN_SOLVED)
+		ge1:SetCondition(function() return Duel.IsBattlePhase() end)
+		ge1:SetOperation(function() Duel.RegisterFlagEffect(0,id,RESET_PHASE|PHASE_BATTLE,0,1) end)
+		Duel.RegisterEffect(ge1,0)
+		--Keep track of which card was the last one that declared an attack
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_ATTACK_ANNOUNCE)
+		ge2:SetOperation(s.atkregop)
+		Duel.RegisterEffect(ge2,0)
+		s.last_attacker=nil
 	end)
 end
 s.xyz_number=107
 s.listed_series={SET_NUMBER}
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	for c in g:Iter() do
-		local cid=c:GetFieldID()
-		if not BPResolvedEffects[cid] then BPResolvedEffects[cid]={} end
-		local fid=re:GetHandler():GetFieldID()
-		for _,f in ipairs(BPResolvedEffects[cid]) do
-			if f==fid then return end
-		end
-		table.insert(BPResolvedEffects[cid],fid)
-	end
+function s.atkregop(e,tp,eg,ep,ev,re,r,rp)
+	local ac=eg:GetFirst()
+	s.last_attacker=ac
+	ac:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE,0,1)
 end
-function s.cfilter(c)
-	return c:GetFlagEffect(id+1)>0
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsBattlePhase() and Duel.IsTurnPlayer(tp) and not Duel.GetAttacker() and Duel.GetCurrentChain()==0
+		and not Duel.HasFlagEffect(tp,id+1) and not Duel.IsPhase(PHASE_BATTLE_START)
 end
-function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE and Duel.GetTurnPlayer()==tp
-		and not Duel.GetAttacker() and Duel.GetCurrentChain()==0
-		and not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
-end
-function s.ftarget(e,c)
-	return c:GetFlagEffectLabel(id+1)~=e:GetLabel()
-end
-function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
+	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	c:RemoveOverlayCard(tp,1,1,REASON_COST)
-	if Duel.GetTurnPlayer()==tp then
+	if Duel.IsTurnPlayer(tp) then
+		Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE|PHASE_BATTLE,0,1)
+		c:RegisterFlagEffect(id+1,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE|PHASE_BATTLE,0,1)
+		--Cannot attack with any other card after activating this effect
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 		e1:SetCode(EFFECT_CANNOT_ATTACK)
-		e1:SetProperty(EFFECT_FLAG_OATH+EFFECT_FLAG_IGNORE_IMMUNE)
 		e1:SetTargetRange(LOCATION_MZONE,0)
-		e1:SetTarget(s.ftarget)
-		e1:SetLabel(c:GetFieldID())
+		e1:SetTarget(function(e,c) return c~=s.last_attacker or not c:HasFlagEffect(id+1) end)
 		e1:SetReset(RESET_PHASE|PHASE_BATTLE)
 		Duel.RegisterEffect(e1,tp)
 	end
 end
 function s.disfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and not c:IsDisabled()
+	return c:IsNegatableMonster() or not (c:IsAttack(c:GetBaseAttack()) and c:IsDefense(c:GetBaseDefense()))
 end
-function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.disfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,c)
-		or BPResolvedEffects[c:GetFieldID()] end
-end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local res=false
 	local g=Duel.GetMatchingGroup(s.disfilter,tp,LOCATION_MZONE,LOCATION_MZONE,c)
-	for tc in aux.Next(g) do
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,PLAYER_ALL,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local exc=c:IsRelateToEffect(e) and c or nil
+	local res=false
+	local g=Duel.GetMatchingGroup(s.disfilter,tp,LOCATION_MZONE,LOCATION_MZONE,exc)
+	for tc in g:Iter() do
+		--Negate its effects
+		tc:NegateEffects(c)
+		--Its ATK and DEF become its original ATK and DEF
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(tc:GetBaseAttack())
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-		local r1=tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
-		local r2=tc:RegisterEffect(e2)
-		if not r1 and not r2 then
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
-			e3:SetValue(tc:GetBaseAttack())
-			e3:SetReset(RESET_EVENT|RESETS_STANDARD)
-			tc:RegisterEffect(e3)
-			local e4=Effect.CreateEffect(c)
-			e4:SetType(EFFECT_TYPE_SINGLE)
-			e4:SetCode(EFFECT_SET_DEFENSE_FINAL)
-			e4:SetValue(tc:GetBaseDefense())
-			e4:SetReset(RESET_EVENT|RESETS_STANDARD)
-			tc:RegisterEffect(e4)
-		end
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		e2:SetValue(tc:GetBaseDefense())
+		tc:RegisterEffect(e2)
 		res=true
 	end
 	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	local fid=c:GetFieldID()
-	local bpre=BPResolvedEffects[fid]
-	if bpre then
+	local resolved_eff_ct=Duel.GetFlagEffect(0,id)
+	if resolved_eff_ct>0 then
 		Duel.BreakEffect()
-		local e5=Effect.CreateEffect(c)
-		e5:SetType(EFFECT_TYPE_SINGLE)
-		e5:SetCode(EFFECT_UPDATE_ATTACK)
-		e5:SetValue(#bpre*1000)
-		e5:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
-		c:RegisterEffect(e5)
+		--This card gains 1000 ATK for each card whose effect resolved during the Battle Phase this turn
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_UPDATE_ATTACK)
+		e3:SetValue(resolved_eff_ct*1000)
+		e3:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e3)
 		res=true
 	end
-	local atkct=c:GetAttackAnnouncedCount()
-	if atkct>0 and Duel.GetTurnPlayer()==tp and res then
+	--Stop here if it's the opponent's turn
+	if Duel.IsTurnPlayer(1-tp) then return end
+	local atk_ct=c:GetAttackAnnouncedCount()
+	if res and atk_ct==1 and c:CanAttack() and s.last_attacker==c and c:HasFlagEffect(id) then
+		--This card can make a second attack in a row
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_SINGLE)
+		e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e4:SetCode(EFFECT_EXTRA_ATTACK)
+		e4:SetValue(atk_ct)
+		e4:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE)
+		c:RegisterEffect(e4)
+		--Register effects to end the Battle Phase manually
+		local e5=Effect.CreateEffect(c)
+		e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+		e5:SetOperation(s.endbpop)
+		e5:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE)
+		c:RegisterEffect(e5)
+		--End the Battle Phase manually if this card ever stops being face-up on the field
 		local e6=Effect.CreateEffect(c)
-		e6:SetType(EFFECT_TYPE_SINGLE)
-		e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e6:SetCode(EFFECT_EXTRA_ATTACK)
-		e6:SetValue(atkct)
-		e6:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE)
-		c:RegisterEffect(e6)
-		c:RegisterFlagEffect(id+1,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE|PHASE_BATTLE,0,1,fid)
+		e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e6:SetCode(EVENT_ADJUST)
+		e6:SetCondition(function(e) return not c:HasFlagEffect(id) end)
+		e6:SetOperation(function(e) e:Reset() Duel.SkipPhase(tp,PHASE_BATTLE,RESET_PHASE|PHASE_BATTLE_STEP,1) end)
+		e6:SetReset(RESET_PHASE|PHASE_END|PHASE_BATTLE)
+		Duel.RegisterEffect(e6,tp)
+	else
+		--End the Battle Phase manually if this card cannot perform another attack in a row
+		Duel.SkipPhase(tp,PHASE_BATTLE,RESET_PHASE|PHASE_BATTLE_STEP,1)
 	end
+end
+function s.endbpop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--End the Battle Phase manually if this card ever stops battling
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_ADJUST)
+	e1:SetCondition(function(e) return not c:IsRelateToBattle() end)
+	e1:SetOperation(function(e) e:Reset() Duel.SkipPhase(tp,PHASE_BATTLE,RESET_PHASE|PHASE_BATTLE_STEP,1) end)
+	e1:SetReset(RESET_PHASE|PHASE_END|PHASE_BATTLE)
+	Duel.RegisterEffect(e1,tp)
+	--End the Battle Phase manually at the end of the Damage Step
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetOperation(function(e) e:Reset() Duel.SkipPhase(tp,PHASE_BATTLE,RESET_PHASE|PHASE_BATTLE_STEP,1) end)
+	e2:SetReset(RESET_PHASE|PHASE_END|PHASE_BATTLE)
+	Duel.RegisterEffect(e2,tp)
 end

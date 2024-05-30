3025e904ca550ee11ce27b3c32775e97d8b20b44
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-05-30 11:31:40 +0300

    Added new card's script

---
+++ pre-release/c100305001.lua
@@ -0,0 +1,83 @@
+--嘆きの石版
+--Grief Tablet
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	local e0=aux.AddEquipProcedure(c)
+	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	--The equipped monster cannot attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	c:RegisterEffect(e1)
+	--The equipped monster cannot be Tributed
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e3)
+	--The equipped monster's effects are negated
+	local e4=e1:Clone()
+	e4:SetCode(EFFECT_DISABLE)
+	c:RegisterEffect(e4)
+	--Add 1 "Wedju" card from your Deck to your hand, except "Grief Tablet"
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1)
+	e5:SetCondition(s.thcon)
+	e5:SetTarget(s.thtg)
+	e5:SetOperation(s.thop)
+	c:RegisterEffect(e5)
+	--Inflict 500 damage to your opponent
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,1))
+	e6:SetCategory(CATEGORY_DAMAGE)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
+	e6:SetCode(EVENT_TO_GRAVE)
+	e6:SetCondition(s.damcon)
+	e6:SetTarget(s.damtg)
+	e6:SetOperation(s.damop)
+	c:RegisterEffect(e6)
+end
+s.listed_series={SET_WEDJU}
+s.listed_names={id}
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec and ec:IsControler(tp)
+end
+function s.thfilter(c)
+	return c:IsSetCard(SET_WEDJU) and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ec=c:GetPreviousEquipTarget()
+	return c:IsReason(REASON_LOST_TARGET) and ec and ec:IsReason(REASON_DESTROY)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end

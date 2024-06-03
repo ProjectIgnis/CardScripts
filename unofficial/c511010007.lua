efe9051d2244ab9c4e0147eaf083c404ee96ac67
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-06-03 14:14:29 +0300

    "Number 7: Lucky Straight (Anime)" fix
    - Should be able to use its effect only when it battles an opponent's monster.
    - It should make its ATK become equal to its original ATK multipled by the result, not 700 multipled by the result.
    - Should be able to use the effect at any point during the occuring battle as long as damage calculation hasn't already been performed.
    - If its effects are negated then the ATK change should also disappear.

--- unofficial/c511010007.lua
+++ unofficial/c511010007.lua
@@ -1,44 +1,53 @@
---Number 7: Lucky Straight (anime)
+--Ｎｏ．７ ラッキー・ストライプ (Anime)
+--Number 7: Lucky Straight (Anime)
 Duel.LoadCardScript("c82308875.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,7,3)
 	c:EnableReviveLimit()
-	--atkup
+	--Xyz Summon procedure: 3 Level 7 monsters
+	Xyz.AddProcedure(c,nil,7,3)
+	--Cannot be destroyed by battle, except with a "Number" monster
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(96864105,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(s.atkcost)
-	e1:SetOperation(s.atkop)
-	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
-	--battle indestructable
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+	c:RegisterEffect(e1)
+	--Make this card's ATK become equal to its original ATK multiplied by a die roll result
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e2:SetValue(s.indes)
-	c:RegisterEffect(e2)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP)
+	e2:SetCondition(s.atkcon)
+	e2:SetCost(aux.dxmcostgen(1,1,nil))
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
 end
-s.listed_series={0x48}
-s.roll_dice=true
 s.xyz_number=7
-function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	c:RemoveOverlayCard(tp,1,1,REASON_COST)
+s.roll_dice=true
+s.listed_series={SET_NUMBER}
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	return bc and bc:IsControler(1-tp) and not (Duel.IsPhase(PHASE_DAMAGE) and Duel.IsDamageCalculated())
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local d1=Duel.TossDice(tp,1)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
-	e1:SetValue(d1*700)
-	e:GetHandler():RegisterEffect(e1)
-end
-function s.indes(e,c)
-	return not c:IsSetCard(0x48)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local die_result=Duel.TossDice(tp,1)
+		--This card's ATK becomes equal to its original ATK multiplied by the result, until the end of the Battle Phase
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(c:GetBaseAttack()*die_result)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE|RESET_PHASE|PHASE_BATTLE)
+		c:RegisterEffect(e1)
+	end
 end

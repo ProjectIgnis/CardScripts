dd25a15129354985a07829da46be5b24eaa4d16e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-06-03 12:52:35 +0300

    "Rising HERO Argent" fix
    Shouldn't shuffle the Deck if the player chooses to add a card(s) to their hand.

--- rush/c160320011.lua
+++ rush/c160320011.lua
@@ -1,5 +1,5 @@
---
---Rising HERO Argento
+--Ｒ・ＨＥＲＯ アルジェント
+--Rising HERO Argent
 --Scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
@@ -15,32 +15,34 @@ function s.initial_effect(c)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c)
-	return c:IsRace(RACE_WARRIOR) and c:IsType(TYPE_FUSION) and c:IsLevel(6,7) and c.material and not c:IsPublic()
+function s.revealfilter(c)
+	return c:IsRace(RACE_WARRIOR) and c:IsType(TYPE_FUSION) and c:IsLevel(6,7) and not c:IsPublic()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.revealfilter,tp,LOCATION_EXTRA,0,1,nil) end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=4 end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.filter(c,fc)
-	return c:IsCode(table.unpack(fc.material)) and c:IsType(TYPE_NORMAL) and c:IsAbleToHand()
+function s.thfilter(c,fc)
+	return fc.material and c:IsCode(table.unpack(fc.material)) and c:IsType(TYPE_NORMAL) and c:IsAbleToHand()
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	--Requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local tc=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_EXTRA,0,1,1,nil):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.revealfilter,tp,LOCATION_EXTRA,0,1,1,nil):GetFirst()
 	Duel.ConfirmCards(1-tp,tc)
+	Duel.ShuffleExtra(tp)
 	--Effect
 	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<5 then return end
 	Duel.ConfirmDecktop(tp,4)
 	local g=Duel.GetDecktopGroup(tp,4)
-	if g:IsExists(s.filter,1,nil,tc) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	if g:IsExists(s.thfilter,1,nil,tc) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local tg=g:FilterSelect(tp,s.filter,1,2,nil,tc)
+		local tg=g:FilterSelect(tp,s.thfilter,1,2,nil,tc)
 		if #tg>0 then
+			Duel.DisableShuffleCheck()
 			Duel.SendtoHand(tg,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,tg)
 			Duel.ShuffleHand(tp)

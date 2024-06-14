4a4e2fa1540bcdb1025531c839fbeabc61b15b57
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-06-14 12:03:08 +0200

    added new rush cards

---
+++ rush/c160320014.lua
@@ -0,0 +1,47 @@
+--放浪の勇者 フリード
+--Freed the Brave Wanderer
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Destroy 1 face-up monster on the field
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,2,nil) end
+end
+function s.filter(c,atk)
+	return c:IsMonster() and c:IsFaceup() and c:GetAttack()>atk and not c:IsMaximumModeSide()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e:GetHandler():GetAttack())
+	if chk==0 then return #dg>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.HintSelection(g)
+	Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+	Duel.SortDeckbottom(tp,tp,#g)
+	--Effect
+	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e:GetHandler():GetAttack())
+	if #dg>0 then
+		local sg=dg:Select(tp,1,1,nil)
+		sg=sg:AddMaximumCheck()
+		Duel.HintSelection(sg)
+		Duel.Destroy(sg,REASON_EFFECT)
+	end
+end
---
+++ rush/c160320031.lua
@@ -0,0 +1,34 @@
+--ミラクル・フュージョン
+--Miracle Fusion
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,s.filter,s.mfilter,s.fextra,s.extraop)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	local e=c:IsHasEffect(EFFECT_SPSUMMON_CONDITION)
+	return c:IsRace(RACE_WARRIOR) and e and e:GetValue()==aux.fuslimit
+end
+function s.mfilter(c)
+	return c:IsLocation(LOCATION_GRAVE|LOCATION_MZONE) and c:IsAbleToDeck()
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE|LOCATION_MZONE,0,nil)
+end
+function s.extraop(e,tc,tp,sg)
+	local gg=sg:Filter(Card.IsLocation,nil,LOCATION_HAND+LOCATION_GRAVE)
+	if #gg>0 then Duel.HintSelection(gg,true) end
+	local rg=sg:Filter(Card.IsFacedown,nil)
+	if #rg>0 then Duel.ConfirmCards(1-tp,rg) end
+	Duel.SendtoDeck(sg,nil,SEQ_DECKBOTTOM,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+	local dg=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_DECK)
+	local ct=dg:FilterCount(Card.IsControler,nil,tp)
+	if ct>0 then
+		Duel.SortDeckbottom(tp,tp,ct)
+	end
+	if #dg>ct then
+		Duel.SortDeckbottom(tp,1-tp,#dg-ct)
+	end
+	sg:Clear()
+end

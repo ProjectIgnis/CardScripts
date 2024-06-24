424d3dc068ad8c0b32ba883bc3818fcdf24cf3a6
Naim <naimsantos45@gmail.com>
2024-06-24 05:21:59 -0300

    Update "Spring of Rebirth"
    According to the database, the effect to gain LP is an activated effect.

--- official/c94425169.lua
+++ official/c94425169.lua
@@ -1,4 +1,5 @@
 --エレメントの泉
+--Spring of Rebirth
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,13 +7,15 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--LP up
+	--Gain 500 LP everytime a monster on the field returns to the hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_TO_HAND)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
@@ -21,6 +24,10 @@ function s.filter(c)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.filter,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Recover(tp,500,REASON_EFFECT)

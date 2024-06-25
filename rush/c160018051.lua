11c5e92af6ff1d28439e011c26e9b1c5a5ea5c93
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-06-25 13:04:01 +0200

    added Twin Ocean Tides

---
+++ rush/c160018051.lua
@@ -0,0 +1,37 @@
+--双海潮流
+--Twin Ocean Tides
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.costfilter(c)
+	return c:IsMonster() and c:IsRace(RACE_FISH|RACE_SEASERPENT) and c:IsLevelAbove(7) and c:IsAbleToDeckOrExtraAsCost() and not c:IsPublic()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)<1 then return end
+	Duel.ShuffleDeck(tp)
+	--Effect
+	Duel.Draw(tp,2,REASON_EFFECT)
+	Duel.ShuffleHand(tp)
+end

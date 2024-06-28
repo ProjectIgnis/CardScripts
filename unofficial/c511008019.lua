bfa6b6fd38b5e9f863dea45ae79d942df56f924a
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-06-27 21:32:35 -0700

    Re-script "Fighting Fund"
    Missing effect code/script clean-up to bring in line with similar OCG cards.

--- unofficial/c511008019.lua
+++ unofficial/c511008019.lua
@@ -3,21 +3,33 @@
 --Scripted by Snrk
 local s,id=GetID()
 function s.initial_effect(c)
-	--Pevent monsters from battle destruction
+	--Prevent monsters from being destroyed by this battle
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetReset(RESET_PHASE|PHASE_DAMAGE)
-	e1:SetValue(1)
-	Duel.RegisterEffect(e1,tp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if a then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE)
+		a:RegisterEffect(e1)
+	end
+	if d then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE)
+		d:RegisterEffect(e1)
+	end
 end

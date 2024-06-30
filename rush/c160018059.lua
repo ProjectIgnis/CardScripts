3deb06472388ddcc7f83fc9a0d660eca7a580285
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-06-30 15:34:31 +0200

    added Fish Missile Launcher

---
+++ rush/c160018059.lua
@@ -0,0 +1,40 @@
+--フィッシャーランチャー
+--Fish Missile Launcher
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Destroy 1 spell/trap your opponent controls
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_FISH) and c:IsAbleToGraveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSpellTrap,tp,0,LOCATION_ONFIELD,1,nil) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if #g==0 or Duel.SendtoGrave(g,REASON_COST)==0 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,0,LOCATION_ONFIELD,nil)
+	if #dg>0 then
+		local sg=dg:Select(tp,1,1,nil)
+		Duel.HintSelection(sg)
+		if Duel.Destroy(sg,REASON_EFFECT)>0 then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end

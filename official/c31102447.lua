70ef0839522d75460dd2a0325a7db3153430a7d9
Naim <naimsantos45@gmail.com>
2024-07-03 04:17:37 -0300

    Fix "Amazoness Spy"
    Fixed a bug in which it would be possible to activate its effect when it was destroyed by battle and banished

--- official/c31102447.lua
+++ official/c31102447.lua
@@ -1,7 +1,8 @@
 --アマゾネスの斥候
+--Amazoness Spy
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Special Summon this card from the hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -12,21 +13,22 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--to hand
+	--Return 1 "Amazoness" monster to the hand or Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_BATTLE_DESTROYED)
-	e2:SetTarget(s.thtg2)
-	e2:SetOperation(s.thop2)
+	e2:SetCondition(s.thcond)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x4}
+s.listed_series={SET_AMAZONESS}
 s.listed_names={id}
 function s.cfilter(c)
-	return c:IsSetCard(0x4) and c:IsMonster() and not c:IsPublic()
+	return c:IsSetCard(SET_AMAZONESS) and c:IsMonster() and not c:IsPublic()
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
@@ -36,9 +38,10 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.ShuffleHand(tp)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -46,22 +49,24 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function s.thfilter(c)
-	return c:IsMonster() and c:IsSetCard(0x4) and not c:IsCode(id) and (c:IsAbleToHand() or c:IsAbleToDeck())
+function s.thcond(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE)
 end
-function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.thfilter(c)
+	return c:IsMonster() and c:IsSetCard(SET_AMAZONESS) and not c:IsCode(id) and (c:IsAbleToHand() or c:IsAbleToDeck())
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.thfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
 	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,g,1,tp,0)
 end
-function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsAbleToHand() and (not tc:IsAbleToDeck() or Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
-	else
-		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
-	end
+	if not tc:IsRelateToEffect(e) then return end
+	aux.ToHandOrElse(tc,tp, function(c) return tc:IsAbleToDeck() end,
+							function(c) Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT) end,
+					aux.Stringid(id,3))
 end

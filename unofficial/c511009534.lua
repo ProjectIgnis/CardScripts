65e72114643e34eff20fbd26a1c0e5dc32b0e4fd
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-07-04 08:09:44 -0700

    Revise scripts of "En Birds/Moon/Winds"
    Fix issues with effect negation relating to monster types.

--- unofficial/c511009534.lua
+++ unofficial/c511009534.lua
@@ -1,3 +1,4 @@
+--エン・ムーン
 --En Moon
 --fixed by MLD
 local s,id=GetID()
@@ -8,28 +9,21 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
 	c:RegisterEffect(e1)
-	--disable
+	--Fusion monsters that banished or on the field/in the GYS become Normal monsters and their effects are negated
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_FUSION))
 	e2:SetCode(EFFECT_DISABLE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE|LOCATION_GRAVE|LOCATION_REMOVED,LOCATION_MZONE|LOCATION_GRAVE|LOCATION_REMOVED)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsOriginalType,TYPE_FUSION))
 	c:RegisterEffect(e2)
-	--type
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED)
+	local e3=e2:Clone()
 	e3:SetCode(EFFECT_CHANGE_TYPE)
-	e3:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_FUSION))
 	e3:SetValue(TYPE_NORMAL)
 	c:RegisterEffect(e3)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsCode(id+1) and c:GetSequence()<5
-end
+s.listed_names={511009535}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_SZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,511009535),tp,LOCATION_STZONE,0,1,nil)
 		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,3,nil,TYPE_FUSION)
 end
--- unofficial/c511009535.lua
+++ unofficial/c511009535.lua
@@ -1,3 +1,4 @@
+--エン・ウィンズ
 --En Winds
 --fixed by MLD
 local s,id=GetID()
@@ -8,28 +9,21 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
 	c:RegisterEffect(e1)
-	--disable
+	--Synchro monsters that are banished, on the field or in the GYs become Normal monsters with their effects negated
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DISABLE)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SYNCHRO))
-	e2:SetCode(EFFECT_DISABLE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsOriginalType,TYPE_SYNCHRO))
 	c:RegisterEffect(e2)
-	--type
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED)
+	local e3=e2:Clone()
 	e3:SetCode(EFFECT_CHANGE_TYPE)
-	e3:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SYNCHRO))
 	e3:SetValue(TYPE_NORMAL)
 	c:RegisterEffect(e3)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsCode(id+1) and c:GetSequence()<5
-end
+s.listed_names={511009536}
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_SZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,511009536),tp,LOCATION_STZONE,0,1,nil)
 		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE,0,3,nil,TYPE_SYNCHRO)
 end

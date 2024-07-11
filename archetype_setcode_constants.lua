c69be9b7ef1a6c7d14d3d866c8f163bbbe1511c3
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-07-11 14:49:05 +0300

    Added new archetype constants
    SET_MIMIGHOUL = 0x1b5
    SET_RAIZEO = 0x1b6

--- archetype_setcode_constants.lua
+++ archetype_setcode_constants.lua
@@ -572,3 +572,5 @@ SET_SIX_STRIKE                    = 0x1b1
 SET_METALMORPH                    = 0x1b2
 SET_MORGANITE                     = 0x1b3
 SET_AZAMINA                       = 0x1b4
+SET_MIMIGHOUL                     = 0x1b5
+SET_RAIZEOL                       = 0x1b6

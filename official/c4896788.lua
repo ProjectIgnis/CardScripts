a310691cab81898e7458f7326baea40809fbb79e
Naim <naimsantos45@gmail.com>
2024-07-22 14:22:31 -0300

    "Spirit of the Pot of Greed" fix
    - The effect's activation should be mandatory
    - The player should be prompted to draw during the resolution

--- official/c4896788.lua
+++ official/c4896788.lua
@@ -2,10 +2,10 @@
 --Spirit of the Pot of Greed
 local s,id=GetID()
 function s.initial_effect(c)
-	--draw
+	--Draw 1 card when "Pot of Greed" is activated
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_DRAW)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_MZONE)
@@ -16,17 +16,19 @@ function s.initial_effect(c)
 end
 s.listed_names={55144522}
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsAttackPos()
-		and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsCode(55144522)
+	return e:GetHandler():IsAttackPos() and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsCode(55144522)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(rp,1) end
+	if chk==0 then return true end
 	Duel.SetTargetPlayer(rp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,rp,1)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsAttackPos() or not e:GetHandler():IsRelateToEffect(e) then return end
+	local c=e:GetHandler()
+	if not (c:IsRelateToEffect(e) and c:IsAttackPos()) then return end
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
+	if Duel.SelectYesNo(p,aux.Stringid(id,1)) then
+		Duel.Draw(p,d,REASON_EFFECT)
+	end
 end

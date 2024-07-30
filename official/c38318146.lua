f3f5aff7afd60748a90f857433c902842cf2cf4e
Naim <naimsantos45@gmail.com>
2024-07-30 16:27:19 -0300

    "Tyrant's Tantrum" update
    It was not handling the scenarios where multiple copies were applying their effects

--- official/c38318146.lua
+++ official/c38318146.lua
@@ -1,4 +1,5 @@
 --暴君の暴力
+--Tyrant's Tantrum
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -7,41 +8,44 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(s.cost)
 	c:RegisterEffect(e1)
-	--activate cost
+	--Players must send 1 Spell card from the Deck to activate Spell cards
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_ACTIVATE_COST)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetTargetRange(0,1)
-	e2:SetCondition(s.accon)
-	e2:SetTarget(s.actarget)
-	e2:SetCost(s.accost)
-	e2:SetOperation(s.acop)
+	e2:SetCost(s.actcost)
+	e2:SetTarget(s.actcosttarget)
+	e2:SetOperation(s.actcostop)
 	c:RegisterEffect(e2)
+	--Accumulation
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(id)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(0,1)
+	c:RegisterEffect(e3)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroup(tp,nil,2,nil) end
 	local rg=Duel.SelectReleaseGroup(tp,nil,2,2,nil)
 	Duel.Release(rg,REASON_COST)
 end
-function s.accon(e)
-	s[0]=false
-	return true
-end
-function s.acfilter(c)
+function s.actvcostfilter(c)
 	return c:IsSpell() and c:IsAbleToGraveAsCost()
 end
-function s.actarget(e,te,tp)
-	return te:IsActiveType(TYPE_SPELL) and te:IsHasType(EFFECT_TYPE_ACTIVATE)
+function s.actcost(e,te,tp)
+	local ct=#{Duel.GetPlayerEffect(tp,id)}
+	return Duel.IsExistingMatchingCard(s.actvcostfilter,tp,LOCATION_DECK,0,ct,nil)
 end
-function s.accost(e,te,tp)
-	return Duel.IsExistingMatchingCard(s.acfilter,tp,LOCATION_DECK,0,1,nil)
+function s.actcosttarget(e,te,tp)
+	return te:IsSpellEffect() and te:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
-function s.acop(e,tp,eg,ep,ev,re,r,rp)
-	if s[0] then return end
+function s.actcostop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=#{Duel.GetPlayerEffect(tp,id)}
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.acfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.actvcostfilter,tp,LOCATION_DECK,0,ct,ct,nil)
 	Duel.SendtoGrave(g,REASON_COST)
-	s[0]=true
 end

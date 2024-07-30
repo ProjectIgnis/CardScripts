0a6b999370f3258b01d0fe20989ed0be7d4083d6
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-07-30 22:59:44 +0200

    fixed Terrorphone Number
    The attack restriction should be working correctly

--- rush/c160428087.lua
+++ rush/c160428087.lua
@@ -40,6 +40,6 @@ end
 function s.atklimit(e,c)
 	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.HasLevel),e:GetHandlerPlayer(),LOCATION_MZONE,0,nil)
 	if #g==0 then return end
-	local maxg,lv=g:GetMaxGroup(Card.GetLevel)
-	return c:IsFaceup() and (not c:HasLevel() or (#g~=#maxg and c:IsLevelAbove(lv)))
+	local maxg,lv=g:GetMinGroup(Card.GetLevel)
+	return c:IsFaceup() and (not c:HasLevel() or c:GetLevel()~=lv)
 end

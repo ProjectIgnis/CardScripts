ee7ae629bd317da0bccb0320be7cc73068fe4814
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-08-02 13:10:09 +0200

    added Goodgoo

---
+++ rush/c160018032.lua
@@ -0,0 +1,50 @@
+--グッドグー
+--Goodgoo
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Decrease ATK and gain LP
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCountRush(tp,0,LOCATION_MZONE)>1
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsLevelAbove(7) and not c:IsMaximumModeSide()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	local c=e:GetHandler()
+	if Duel.SendtoGrave(c,REASON_COST)<1 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local sg=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	if #sg==0 then return end
+	Duel.HintSelection(sg)
+	--Decrease ATK
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESETS_STANDARD_PHASE_END)
+	e1:SetValue(-1000)
+	sg:GetFirst():RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	sg:GetFirst():RegisterEffect(e2)
+end

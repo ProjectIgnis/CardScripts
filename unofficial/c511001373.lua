55d7cf55a284f87567bf244e7a65d752390a1a80
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-08-02 07:17:30 -0700

    Fix "Overlay Satellite"
    Incorrect event code was causing this card to attach itself even when it left the field without the equipped monster being used as material/script updates.

--- unofficial/c511001373.lua
+++ unofficial/c511001373.lua
@@ -13,36 +13,44 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_BE_MATERIAL)
-	e2:SetRange(LOCATION_GRAVE|LOCATION_REMOVED)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetCondition(s.atcon)
 	e2:SetTarget(s.attg)
 	e2:SetOperation(s.atop)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e3)
 end
 function s.atcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local rc=c:GetPreviousEquipTarget()
-	return c:IsReason(REASON_LOST_TARGET) and c:IsPreviousLocation(LOCATION_SZONE) and rc and eg:IsContains(rc) and rc:IsReason(REASON_XYZ)
+	local tc=c:GetPreviousEquipTarget()
+	if c:IsReason(REASON_LOST_TARGET) and tc and tc:IsReason(REASON_XYZ) then
+		e:SetLabelObject(tc:GetReasonCard())
+		return true
+	else
+		return false
+	end
 end
 function s.attg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
 	local c=e:GetHandler()
-	Duel.SetTargetCard(c:GetPreviousEquipTarget():GetReasonCard())
+	local tc=e:GetLabelObject()
+	if chk==0 then return tc and c:IsCanBeXyzMaterial(tc,tp,REASON_EFFECT) end
+	tc:CreateEffectRelation(e)
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,0,0)
 end
 function s.atop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	local rc=c:GetPreviousEquipTarget():GetReasonCard()
-	if not rc:IsRelateToEffect(e) then return end
-	Duel.Overlay(rc,c)
-	--Cannot attack this turn
+	local tc=e:GetLabelObject()
+	if not (c:IsRelateToEffect(e) and tc:IsRelateToEffect(e)) then return end
+	Duel.Overlay(tc,c)
+	--This card cannot attack this turn
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(3206)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)
 	e1:SetReset(RESETS_STANDARD_PHASE_END)
-	rc:RegisterEffect(e1)
+	tc:RegisterEffect(e1)
 end

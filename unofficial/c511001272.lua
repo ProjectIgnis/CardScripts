cf6227cb812e228e38aa4cc3485f3fc3b2df966d
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-08-03 10:34:46 -0700

    Update "Damage Capture"
    Should apply to all damage/general script updates.

--- unofficial/c511001272.lua
+++ unofficial/c511001272.lua
@@ -1,33 +1,30 @@
+--ダメージ・キャプチャー
 --Damage Capture
 local s,id=GetID()
 function s.initial_effect(c)
-	--
+	--1 monster you control gains ATK equal to the damage you took
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_DAMAGE)
-	e1:SetCondition(s.con)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+	e1:SetTarget(s.atkgaintg)
+	e1:SetOperation(s.atkgainop)
 	c:RegisterEffect(e1)
 end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and tp~=rp and (r&REASON_BATTLE)==0
-end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.atkgaintg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return ep==tp and Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.atkgainop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsImmuneToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		e1:SetValue(ev)
 		tc:RegisterEffect(e1)
 	end

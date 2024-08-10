f3a43c30cfc3a3feb1c0c3cbd7636c2660f34a08
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-08-10 20:43:34 +0300

    "Cyber Blade Fusion" fix
    Shouldn't let you discard a "Etoile Cyber" or "Blade Skater" if you don't have another one to use for the Fusion Summon in your hand, Monster Zone, or Deck.

--- skill/c300304005.lua
+++ skill/c300304005.lua
@@ -4,58 +4,59 @@ local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
 end
-s.listed_names={10248389,11460577,97023549}
---Skill condition
+s.listed_names={10248389,11460577,97023549} --"Cyber Blader", "Etoile Cyber", "Blade Skater"
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	local params={fusfilter=s.fusmonfilter,
+	return aux.CanActivateSkill(tp) and not Duel.HasFlagEffect(tp,id,2) and s.cost(e,tp,eg,ep,ev,re,r,rp,0)
+end
+function s.costfilter(c,e,tp,eg,ep,ev,re,r,rp)
+	if not c:IsDiscardable() then return false end
+	local params={fusfilter=aux.FilterBoolFunction(Card.IsCode,10248389),
 		matfilter=Fusion.OnFieldMat,
-		extrafil=s.extrafusmat,
-		extratg=s.extratarget,
+		extrafil=s.fextra(c),
 		stage2=s.stage2}
-	return aux.CanActivateSkill(tp) and Duel.GetFlagEffect(tp,id)<2 and Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
+	return Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
 end
---Fusion Summon functions
-function s.fusmonfilter(c)
-	return c:IsCode(10248389)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp,eg,ep,ev,re,r,rp) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST|REASON_DISCARD,nil,e,tp,eg,ep,ev,re,r,rp)
 end
-function s.extrafusmat(e,tp,mg)
-	return Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_HAND|LOCATION_MZONE|LOCATION_DECK,0,nil),s.specialcheck
+function s.fextra(exc)
+	return function(e,tp,mg)
+		return Duel.GetMatchingGroup(Card.IsAbleToGrave,tp,LOCATION_HAND|LOCATION_MZONE|LOCATION_DECK,0,nil),s.fcheck(exc)
+	end
 end
-function s.specialcheck(tp,sg,fc)
-	return sg:IsExists(Card.IsLocation,1,nil,LOCATION_MZONE)
+function s.fcheckmatfilter(c)
+	return c:IsLocation(LOCATION_MZONE) and c:IsCode(11460577,97023549)
 end
-function s.extratarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST|REASON_DISCARD,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_HAND|LOCATION_MZONE|LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+function s.fcheck(exc)
+	return function(tp,sg,fc)
+		return not (exc and sg:IsContains(exc)) and sg:IsExists(s.fcheckmatfilter,1,nil)
+	end
 end
 function s.stage2(e,tc,tp,sg,chk)
 	if chk==0 then
+		--It gains 400 ATK
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 		e1:SetValue(400)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
 end
---Fusion Summon operation
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	local params={fusfilter=s.fusmonfilter,
+	--You can only apply this effect twice per Duel
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	--Discard 1 card
+	s.cost(e,tp,eg,ep,ev,re,r,rp,1)
+	--Fusion Summon 1 "Cyber Blader"
+	local params={fusfilter=aux.FilterBoolFunction(Card.IsCode,10248389),
 		matfilter=Fusion.OnFieldMat,
-		extrafil=s.extrafusmat,
-		extratg=s.extratarget,
+		extrafil=s.fextra(c),
 		stage2=s.stage2}
-	if Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0) then
-		--TPD Register
-		Duel.RegisterFlagEffect(tp,id,0,0,0)
-		--Fusion Summon "Cyber Blader" using either "Etoile Cyber" or "Blade Skater" from field and the other from hand, Deck or GY
-		Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,1)
-        	Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp,1)
-	end
+	Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,1)
+	Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp)
 end
-

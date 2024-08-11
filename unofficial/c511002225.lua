a76bbe8e3227a0453a8a8fee4d63a052cde4ed96
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-08-11 16:02:03 -0700

    Update unofficial scripts
    Update scripts to reflect OCG releases

--- unofficial/c511002225.lua
+++ unofficial/c511002225.lua
@@ -1,7 +1,8 @@
---Bombing Beast Fire Bomber 
+--爆撃獣ファイヤ・ボンバ (Manga)
+--Blazing Bombardment Beast (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
-	--fusion material
 	c:EnableReviveLimit()
+	--Fusion Material
 	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_MACHINE),aux.FilterBoolFunctionEx(Card.IsRace,RACE_PYRO))
 end
--- unofficial/c511010519.lua
+++ unofficial/c511010519.lua
@@ -1,11 +1,11 @@
---Blaze Fenix, the Burning Bombardment Bird
---重爆撃禽 ボム・フェネクス
+--重爆撃禽 ボム・フェネクス (Manga)
+--Blaze Fenix, the Burning Bombardment Bird (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
-	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,511002225,511002226)
-	--damage
+	--Fusion material: "Blazing Bombardment Beast" + "Firebird, the Burning Skywing"
+	Fusion.AddProcMix(c,true,true,70117791,511002226)
+	--Inflict 300 damage to your opponent for each card on the field
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(6602300,0))
 	e2:SetCategory(CATEGORY_DAMAGE)
@@ -19,12 +19,12 @@ function s.initial_effect(c)
 end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local ct=Duel.GetFieldGroupCount(tp,0xc,0xc)
+	local ct=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*300)
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local ct=Duel.GetFieldGroupCount(tp,0xc,0xc)
+	local ct=Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
 	Duel.Damage(p,ct*300,REASON_EFFECT)
 end

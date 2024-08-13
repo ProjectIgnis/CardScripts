05c6a923e7843ba4613974b4b2352dec813ad08f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-08-13 13:59:43 +0300

    Added new helper function
    "Card.AddMustBeSpecialSummonedByDarkFusion". Registers the Special Summoning condition used by the "Evil HERO" Fusion Monsters and marks them as 'Must be Special Summoned by "Dark Fusion".'

--- cards_specific_functions.lua
+++ cards_specific_functions.lua
@@ -330,6 +330,27 @@ function Auxiliary.EvilHeroLimit(e,se,sp,st)
 	return se:GetHandler():IsCode(CARD_DARK_FUSION) or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),SKILL_DARK_UNITY) and se:GetHandler():IsCode(CARD_SUPER_POLYMERIZATION))
 		or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),EFFECT_SUPREME_CASTLE) and st&SUMMON_TYPE_FUSION==SUMMON_TYPE_FUSION)
 end
+
+function Card.AddMustBeSpecialSummonedByDarkFusion(c)
+	local metatable=Duel.GetMetatable(c:GetOriginalCode())
+	metatable.dark_calling=true
+	--Must be Special Summoned with "Dark Fusion"
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.EvilHeroLimit)
+	c:RegisterEffect(e0)
+	--"Clock Lizard" check
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(CARD_CLOCK_LIZARD)
+	e1:SetCondition(function(e) return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),EFFECT_SUPREME_CASTLE) end)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	return e0
+end
 --Special Summon limit for "Fossil" Fusion monsters
 function Auxiliary.FossilLimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or se:GetHandler():IsCode(CARD_FOSSIL_FUSION)

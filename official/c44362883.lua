1ec3e61962699fc7f437d2b200248b89bb07c7c4
Naim <naimsantos45@gmail.com>
2024-08-15 17:31:21 -0300

    "Branded Fusion" fix
    Fixed a bug in which it would be possible to use a fusion substitute effect to replace the "Fallen of Albaz", which should not be allowed.

--- official/c44362883.lua
+++ official/c44362883.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.ListsCodeAsMaterial,CARD_ALBAZ),nil,s.fextra,nil,nil,nil,2,nil,nil,nil,nil,nil,nil,s.extratg)
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.ListsCodeAsMaterial,CARD_ALBAZ),extrafil=s.fextra,exactcount=2,extratg=s.extratg})
 	e1:SetCost(s.cost)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
@@ -12,7 +12,10 @@ function s.initial_effect(c)
 end
 s.listed_names={CARD_ALBAZ}
 function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil),s.fcheck
+end
+function s.fcheck(tp,sg,fc)
+	return sg:IsExists(Card.IsSummonCode,1,nil,fc,SUMMON_TYPE_FUSION,tp,CARD_ALBAZ)
 end
 function s.counterfilter(c)
 	return not c:IsSummonLocation(LOCATION_EXTRA) or c:IsType(TYPE_FUSION)

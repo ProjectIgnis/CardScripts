6f78cb317566407836b3867d457a0016661eece5
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-08-15 20:18:52 -0700

    Fix "Ascension Sky Dragon (Manga)"
    Re-scripted target function to allow for proper determination of material availability/general script modernization

--- unofficial/c511001780.lua
+++ unofficial/c511001780.lua
@@ -1,48 +1,54 @@
---天穹覇龍ドラゴアセンション
+--天穹覇龍ドラゴアセンション (Manga)
+--Ascension Sky Dragon (Manga)
 local s,id=GetID()
 function s.initial_effect(c)
-	--synchro summon
-	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
-	--atk
+	--Synchro Summon procedure: 1 Tuner + 1 or more non-Tuner monsters
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	--ATK becomes 1000 x number of cards in your hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_SET_ATTACK)
-	e1:SetValue(s.atkval)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(function(e,c) return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_HAND,0)*1000 end)
 	c:RegisterEffect(e1)
-	--Destroy replace
+	--Return this card to the Extra Deck and Special Summon its materials
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTarget(s.desreptg)
 	e2:SetOperation(s.desrepop)
 	c:RegisterEffect(e2)
 end
-function s.atkval(e,c)
-	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_HAND,0)*1000
-end
-function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsOnField() and c:IsFaceup() 
-		and c:IsAbleToExtra() end
-	return true
-end
 function s.mgfilter(c,e,tp,sync)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_HAND+LOCATION_GRAVE+LOCATION_REMOVED)
-		and c:GetReason()&0x80008==0x80008 and c:GetReasonCard()==sync
+	return c:IsControler(tp) and c:IsLocation(LOCATION_HAND|LOCATION_GRAVE|LOCATION_REMOVED)
+		and c:GetReason()&(REASON_MATERIAL|REASON_SYNCHRO)==(REASON_MATERIAL|REASON_SYNCHRO) and c:GetReasonCard()==sync
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local sumtype=c:GetSummonType()
 	local mg=c:GetMaterial():Filter(aux.NecroValleyFilter(s.mgfilter),nil,e,tp,c)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	if Duel.SendtoDeck(c,nil,0,REASON_EFFECT+REASON_REPLACE)>0 
+	if c:GetSequence()<5 then ft=ft+1
+	if chk==0 then return not (c:IsReason(REASON_REPLACE) or Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)) and c:IsOnField() and c:IsFaceup() and c:IsAbleToExtra() 
+		and #mg>0 and ft>-#mg and sumtype==SUMMON_TYPE_SYNCHRO end
+	if #mg>0 and ft>-#mg then
+		e:SetLabelObject(mg)
+		return true
+	else return false
+	end
+end
+
+function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local sumtype=c:GetSummonType()
+	local mg=e:GetLabelObject()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if Duel.SendtoDeck(c,nil,0,REASON_EFFECT|REASON_REPLACE)>0 
 		and sumtype==SUMMON_TYPE_SYNCHRO and #mg>0 
 		and #mg<=ft then
 		Duel.SpecialSummon(mg,0,tp,tp,false,false,POS_FACEUP)

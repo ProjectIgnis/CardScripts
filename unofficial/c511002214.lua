ddab83be3ec2eac2d2144aeceffb39613eb7c1d8
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-08-21 20:32:05 -0700

    Fix "Fallen Angel of Roses (Manga)"
    Should not gain ATK when itself is Summoned/slight script update.

--- unofficial/c511002214.lua
+++ unofficial/c511002214.lua
@@ -1,9 +1,9 @@
---魔天使ローズ・ソーサラー (Anime)
---Fallen Angel of Roses (Anime)
+--魔天使ローズ・ソーサラー (Manga)
+--Fallen Angel of Roses (Manga)
 Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Special Summon this card from your hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--atk up
+	--Gains the ATK of "Angel" monsters Normal or Special Summoned
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(55099248,0))
 	e2:SetCategory(CATEGORY_ATKCHANGE)
@@ -27,10 +27,11 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 end
-s.listed_names={c}
-s.listed_series={0x218}
+s.listed_names={96470883} --Queen Angel of Roses
+s.listed_series={0x218} -- "Angel" archetype
 function s.spfilter(c,ft)
-	return c:IsFaceup() and c:IsCode(96470883) and c:IsAbleToHandAsCost()
+	return c:IsFaceup() and c:IsCode(96470883) and c:IsAbleToHandAsCost() and
+		and (ft>0 or c:GetSequence()<5)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -40,7 +41,8 @@ function s.spcon(e,c)
 	return ft>-1 and #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,nil,0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,Duel.GetLocationCount(tp,LOCATION_MZONE))
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,ft)
 	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_RTOHAND,nil,nil,true)
 	if #g>0 then
 		g:KeepAlive()
@@ -55,17 +57,17 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.SendtoHand(g,nil,REASON_COST)
 	g:DeleteGroup()
 end
-function s.filter(c,tp)
+function s.atkfilter(c,tp)
 	return c:IsFaceup() and c:IsAngel() and c:IsLocation(LOCATION_MZONE) and c:IsSummonPlayer(tp)
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg:IsExists(s.filter,1,nil,tp) end
+	if chk==0 then return eg:IsExists(s.atkfilter,1,nil,tp) and not eg:IsContains(e:GetHandler()) end
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=eg:FilterSelect(tp,s.filter,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+	local g=eg:FilterSelect(tp,s.atkfilter,1,1,nil,tp)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.HintSelection(g)
@@ -73,7 +75,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(tc:GetAttack())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e1)
 	end
 end

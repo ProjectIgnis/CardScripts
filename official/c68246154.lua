8ccb486c2c3f5840b1d770d4ec2bfd1a5060e8a0
Edoardo Lolletti <edoardo762@gmail.com>
2024-08-31 14:32:37 +0200

    Update Magical Musketeer Doc/Caspar
    Rework their effect handilng to account for more scenarios

--- official/c68246154.lua
+++ official/c68246154.lua
@@ -25,39 +25,51 @@ function s.initial_effect(c)
 	e3:SetLabelObject(g)
 	e3:SetOperation(s.regop)
 	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
+	local e4=e3:Clone()
 	e4:SetCode(EVENT_CHAINING)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,id)
-	e4:SetCondition(s.thcon)
-	e4:SetLabelObject(g)
-	e4:SetTarget(s.thtg)
-	e4:SetOperation(s.thop)
+	e4:SetCondition(s.regcon)
+	e4:SetOperation(s.regop2)
 	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_CHAIN_END)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,id)
+	e5:SetCondition(s.thcon)
+	e5:SetLabelObject(g)
+	e5:SetTarget(s.thtg)
+	e5:SetOperation(s.thop)
+	c:RegisterEffect(e5)
 end
 s.listed_series={SET_MAGICAL_MUSKET}
-function s.regop(e)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local flageff={c:GetFlagEffectLabel(1)}
-	if flageff[1]==nil or c:GetFlagEffect(2)>0 then return end
-	c:RegisterFlagEffect(2,RESET_EVENT|(RESETS_STANDARD&~RESET_TURN_SET)|RESET_CHAIN,0,1)
+	local cardid=re:GetHandler():GetCardID()
+	if flageff[1]==nil then return end
 	local g=e:GetLabelObject()
-	g:Clear()
 	for _,i in ipairs(flageff) do
-		g:AddCard(Duel.GetCardFromCardID(i))
+		if cardid==i then
+			g:AddCard(re:GetHandler())
+			return
+		end
 	end
 end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local p,loc,seq=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_CONTROLER,CHAININFO_TRIGGERING_LOCATION,CHAININFO_TRIGGERING_SEQUENCE)
 	if not (re:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsColumn(seq,p,LOCATION_SZONE)) then return false end
 	local flageff=c:GetFlagEffectLabel(1)
-	c:RegisterFlagEffect(1,RESET_EVENT|(RESETS_STANDARD&~RESET_TURN_SET)|RESET_CHAIN,0,1,re:GetHandler():GetCardID())
 	if flageff==nil then e:GetLabelObject():Clear() end
-	return flageff==nil
+	return true
+end
+function s.regop2(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(1,RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET|RESET_CHAIN,0,1,re:GetHandler():GetCardID())
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return #e:GetLabelObject()>0
 end
 function s.thfilter(c,...)
 	return c:IsSetCard(SET_MAGICAL_MUSKET) and not c:IsCode(...) and c:IsAbleToHand()

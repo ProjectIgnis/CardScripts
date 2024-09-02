6e8370d6f78e9bc87a1afad50dcbeaf9d460011d
ClementLouis <clementlouis.gallier@viacesi.fr>
2024-09-02 16:08:23 +0200

    Beast Machine King Barbaros Ür (manga) fix
    reworked the script as it had problem when direct attacking (the attack was canceled)

--- unofficial/c513000021.lua
+++ unofficial/c513000021.lua
@@ -1,36 +1,25 @@
-local s,id=GetID()function s.initial_effect(c)
+-- Beast Machine King Barbaros Ür (manga)local s,id=GetID()function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Fusion.AddProcCode2(c,78651105,96938777,true,true)
-	local e1=Effect.CreateEffect(c)
+	-- attack limite	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e1:SetValue(s.vala)
 	c:RegisterEffect(e1)
-	--pos Change
+	--target level register
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e2:SetOperation(s.regop)
 	c:RegisterEffect(e2)
-	--attack
+	--extra attack
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
 	e3:SetValue(9999)
 	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
-	e4:SetCondition(s.dircon)
-	c:RegisterEffect(e4)
-	local e5=e4:Clone()
-	e5:SetCode(EFFECT_CANNOT_ATTACK)
-	e5:SetCondition(s.atkcon2)
-	c:RegisterEffect(e5)
 end
 function s.vala(e,c)
 	local ca=e:GetHandler()
@@ -47,10 +36,4 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 			i=i+1
 		end
 	end
-end
-function s.dircon(e)
-	return e:GetHandler():GetAttackAnnouncedCount()>0
-end
-function s.atkcon2(e)
-	return e:GetHandler():IsDirectAttacked()
 end

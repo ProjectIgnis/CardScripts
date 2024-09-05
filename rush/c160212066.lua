8ea3de31489f8584494c177d64d9d4715e7ead14
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-09-05 13:31:46 +0200

    added Guardian of Order

---
+++ rush/c160212066.lua
@@ -0,0 +1,22 @@
+--ガーディアン・オブ・オーダー
+--Guardian of Order
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+end
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and not c:IsCode(71799173)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and	Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,2,nil)
+end

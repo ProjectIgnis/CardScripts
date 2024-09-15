00beff7a78a608ded20ab344a5743f2835287e98
yoshiduels <yoshi.katreuvin@gmail.com>
2024-09-15 09:32:42 +0200

    added Card Trooper

---
+++ rush/c160402044.lua
@@ -0,0 +1,60 @@
+--カードガンナー
+--Card Trooper
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--ATK increase+double attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=10
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,500)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	local ct={}
+	for i=3,1,-1 do
+		if Duel.IsPlayerCanDiscardDeckAsCost(tp,i) then
+			table.insert(ct,i)
+		end
+	end
+	local ct2=0
+	if #ct==1 then 
+		ct2=Duel.DiscardDeck(tp,ct[1],REASON_COST)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+		local ac=Duel.AnnounceNumber(tp,table.unpack(ct))
+		ct2=Duel.DiscardDeck(tp,ac,REASON_COST)
+	end
+	if ct2<1 then return end
+	--Effect
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(ct2*500)
+	e1:SetReset(RESETS_STANDARD_PHASE_END)
+	c:RegisterEffect(e1)
+	if c:IsAbleToGrave() and Duel.IsPlayerCanDraw(tp,1) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		if Duel.SendtoGrave(c,REASON_EFFECT)>0 then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end

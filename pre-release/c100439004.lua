43595ace5a2f8b8ec31d35f186eb6486e7369035
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-09-23 13:16:10 +0300

    Added new card scripts

---
+++ pre-release/c100439004.lua
@@ -0,0 +1,113 @@
+--魔轟神界の復活
+--Fablem Realm Resurrection
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Set 1 "Fabled" Spell/Trap from your Deck
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e0:SetOperation(s.setop)
+	c:RegisterEffect(e0)
+	--Your opponent cannot activate cards or effects when your "Fabled" monster is Synchro Summoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCondition(s.limcon)
+	e1:SetOperation(s.limop)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_CHAIN_END)
+	e2:SetOperation(s.limop2)
+	c:RegisterEffect(e2)
+	--Activate 1 of these effects
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1)
+	e3:SetCost(s.effcost)
+	e3:SetTarget(s.efftg)
+	e3:SetOperation(s.effop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={id}
+s.listed_series={SET_FABLED}
+function s.setfilter(c)
+	return c:IsSetCard(SET_FABLED) and c:IsSpellTrap() and not c:IsCode(id) and c:IsSSetable()
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK,0,nil)
+	if #g==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,2)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local sg=g:Select(tp,1,1,nil)
+	if #sg>0 then
+		Duel.SSet(tp,sg)
+	end
+end
+function s.sumfilter(c,tp)
+	return c:IsSetCard(SET_FABLED) and c:IsType(TYPE_SYNCHRO) and c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:IsSummonPlayer(tp)
+end
+function s.limcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.sumfilter,1,nil,tp)
+end
+function s.limop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetCurrentChain()==0 then
+		Duel.SetChainLimitTillChainEnd(function(e,rp,tp) return tp==rp end)
+	elseif Duel.GetCurrentChain()==1 then
+		e:GetHandler():RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
+	end
+end
+function s.limop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:HasFlagEffect(id) then
+		Duel.SetChainLimitTillChainEnd(function(e,rp,tp) return tp==rp end)
+	end
+	c:ResetFlagEffect(id)
+end
+function s.costfilter(c)
+	return c:IsDiscardable() and c:IsSetCard(SET_FABLED)
+end
+function s.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST|REASON_DISCARD)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_FABLED) and c:IsFaceup() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsPlayerCanDraw(tp,1)
+	local b2=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil,e,tp)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,3)},
+		{b2,aux.Stringid(id,4)})
+	e:SetLabel(op)
+	if op==1 then
+		e:SetCategory(CATEGORY_DRAW)
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	elseif op==2 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE|LOCATION_REMOVED)
+	end
+end
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		--Draw 1 card
+		Duel.Draw(tp,1,REASON_EFFECT)
+	elseif op==2 then
+		--Special Summon 1 "Fabled" monster from your GY or banishment
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

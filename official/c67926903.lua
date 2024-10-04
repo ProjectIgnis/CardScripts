c7732792dd69eee07de79dc4639d1776122fc8f7
Naim <naimsantos45@gmail.com>
2024-10-04 13:02:42 -0300

    "CXyz Barian Hope" fix
    - updated the reset approach used by it to allow the player to reset its effects first in case other effects were also applied

--- official/c67926903.lua
+++ official/c67926903.lua
@@ -2,10 +2,10 @@
 --CXyz Barian Hope
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,7,3,s.ovfilter,aux.Stringid(id,0),99)
 	c:EnableReviveLimit()
-	--atk
+	--Xyz Summon procedure: 3+ Level 7 monsters OR 1 "Number C101" through "Number C107"
+	Xyz.AddProcedure(c,nil,7,3,s.ovfilter,aux.Stringid(id,0),99)
+	--Gains 1000 ATK for each Xyz material it has
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
-	--copy
+	--Change this card's name and effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_IGNITION)
@@ -24,18 +24,18 @@ function s.initial_effect(c)
 	e2:SetOperation(s.copyop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x48}
+s.listed_series={SET_NUMBER}
 function s.ovfilter(c,tp,lc)
 	local class=c:GetMetatable(true)
 	if class==nil then return false end
 	local no=class.xyz_number
-	return c:IsFaceup() and no and no>=101 and no<=107 and c:IsSetCard(0x1048,lc,SUMMON_TYPE_XYZ,tp)
+	return c:IsFaceup() and no and no>=101 and no<=107 and c:IsSetCard(SET_NUMBER_C,lc,SUMMON_TYPE_XYZ,tp)
 end
 function s.atkval(e,c)
 	return c:GetOverlayCount()*1000
 end
 function s.filter(c)
-	return c:IsSetCard(0x48) and c:IsType(TYPE_EFFECT)
+	return c:IsSetCard(SET_NUMBER) and c:IsType(TYPE_EFFECT)
 end
 function s.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc) end
@@ -48,14 +48,44 @@ function s.copyop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and c:IsFaceup() and tc:IsRelateToEffect(e) then
 		local code=tc:GetOriginalCode()
+		--This card's name becomes the target's name
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_CODE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetValue(code)
 		e1:SetLabel(tp)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		e1:SetReset(RESETS_STANDARD_PHASE_END|RESET_OPPO_TURN)
 		c:RegisterEffect(e1)
-		c:CopyEffect(code,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		--Replace this card's effect with that monster's original effect
+		local cid=c:CopyEffect(code,RESETS_STANDARD_PHASE_END|RESET_OPPO_TURN)
+		--Reset the effects manually at the End Phase of the opponent's next turn
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,2))
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_PHASE+PHASE_END)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetCountLimit(1)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetCondition(s.resetcon)
+		e2:SetOperation(s.resettop)
+		e2:SetLabel(cid)
+		e2:SetLabelObject(e1)
+		e2:SetReset(RESETS_STANDARD_PHASE_END|RESET_OPPO_TURN)
+		c:RegisterEffect(e2)
 	end
+end
+function s.resetcon(e,tp,eg,ep,ev,re,r,rp)
+	local p=e:GetLabelObject():GetLabel()
+	return Duel.IsTurnPlayer(1-p)
+end
+function s.resettop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local cid=e:GetLabel()
+	c:ResetEffect(cid,RESET_COPY)
+	c:ResetEffect(RESET_DISABLE,RESET_EVENT)
+	local e1=e:GetLabelObject()
+	e1:Reset()
+	Duel.HintSelection(Group.FromCards(c))
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 end

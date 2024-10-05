b71db8c9bb02b27e446afbd9b00726630df2d34e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-10-04 20:49:59 -0700

    Update "Gearspring Catapult"
    Minor script update

--- unofficial/c511002903.lua
+++ unofficial/c511002903.lua
@@ -1,22 +1,24 @@
+--ネジマキのカタパルト
 --Gearspring Catapult
+local GEARSPRING_COUNTER=0x107
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableCounterPermit(0x107)
+	c:EnableCounterPermit(GEARSPRING_COUNTER)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--add counter
+	--During each of your Standby Phases, place 1 Gearspring Counter on this card
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1)
 	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetCondition(s.ctcon)
+	e2:SetCondition(function(e,tp) return Duel.GetTurnPlayer()==tp end)
 	e2:SetOperation(s.ctop)
 	c:RegisterEffect(e2)
-	--gain atk
+	--Make 1 monster you control gain 500 ATK 
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
@@ -28,17 +30,14 @@ function s.initial_effect(c)
 	e3:SetOperation(s.atkop)
 	c:RegisterEffect(e3)
 end
-function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
-end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	c:AddCounter(0x107,1)
-	Duel.RaiseEvent(c,511002905,e,REASON_EFFECT,tp,tp,0x107)
+	c:AddCounter(GEARSPRING_COUNTER,1)
+	Duel.RaiseEvent(c,511002905,e,REASON_EFFECT,tp,tp,GEARSPRING_COUNTER)
 end
 function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x107,1,REASON_COST) end
-	e:GetHandler():RemoveCounter(tp,0x107,1,REASON_COST)
+	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,GEARSPRING_COUNTER,1,REASON_COST) end
+	e:GetHandler():RemoveCounter(tp,GEARSPRING_COUNTER,1,REASON_COST)
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:IsControler(tp) end
@@ -52,7 +51,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		e1:SetValue(500)
 		tc:RegisterEffect(e1)
 	end

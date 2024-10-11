2ad9c56c18e5dca349d76e64db20004b415c1f0e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-10-11 17:43:04 +0300

    Added new card scripts

---
+++ pre-release/c101207035.lua
@@ -0,0 +1,80 @@
+--蛇眼の原罪龍
+--Snake-Eyes Damned Dragon
+--Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	c:AddMustBeFusionSummoned()
+	--Fusion Materials: 1 "Snake-Eye" monster + 1 Illusion monster
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_SNAKE_EYE),aux.FilterBoolFunctionEx(Card.IsRace,RACE_ILLUSION))
+	--Alternative Summon Procedure
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCondition(s.hspcon)
+	e0:SetTarget(s.hsptg)
+	e0:SetOperation(s.hspop)
+	c:RegisterEffect(e0)
+	--Place 1 monster from the field in the Spell & Trap Zone as a Continuous Spell
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.pltg)
+	e1:SetOperation(s.plop)
+	c:RegisterEffect(e1)
+end
+function s.hspfilter(c)
+	return c:IsMonsterCard() and c:IsFaceup() and c:IsAbleToGraveAsCost()
+end
+function s.hspcon(e,c)
+	if not c then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+		and Duel.IsExistingMatchingCard(s.hspfilter,tp,LOCATION_STZONE,0,2,nil)
+end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.hspfilter,tp,LOCATION_STZONE,0,2,2,true,nil)
+	if not g then return false end
+	g:KeepAlive()
+	e:SetLabelObject(g)
+	return true
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_COST|REASON_MATERIAL)
+	g:DeleteGroup()
+end
+function s.plfilter(c)
+	local p=c:GetOwner()
+	return c:IsFaceup() and Duel.GetLocationCount(p,LOCATION_SZONE)>0 and not c:IsForbidden() and c:CheckUniqueOnField(p)
+end
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.plfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.plfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	Duel.SelectTarget(tp,s.plfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) or tc:IsImmuneToEffect(e) then return end
+	local p=tc:GetOwner()
+	if Duel.GetLocationCount(p,LOCATION_SZONE)==0 then
+		Duel.SendtoGrave(tc,REASON_RULE,nil,PLAYER_NONE)
+	elseif tc:CheckUniqueOnField(p) and Duel.MoveToField(tc,tp,p,LOCATION_SZONE,POS_FACEUP,true) then
+		--Treated as a Continuous Spell
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CHANGE_TYPE)
+		e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
+		e1:SetReset((RESET_EVENT|RESETS_STANDARD)&~RESET_TURN_SET)
+		tc:RegisterEffect(e1)
+	end
+end
---
+++ pre-release/c101207036.lua
@@ -0,0 +1,72 @@
+--聖アザミナ
+--St. Azamina
+--Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Materials: 1 Level 6 or higher Fusion Monster + 1 Level 6 or higher Synchro Monster
+	Fusion.AddProcMix(c,true,true,s.matfilter(TYPE_FUSION),s.matfilter(TYPE_SYNCHRO))
+	--Your opponent cannot target this card, cards they control, and cards in their GY and banishment with card effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_ONFIELD|LOCATION_GRAVE|LOCATION_REMOVED)
+	e1:SetTarget(function(e,c) return c==e:GetHandler() or c:IsControler(1-e:GetHandlerPlayer()) end)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--Your opponent takes any battle damage you would have taken instead
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--Special Summon 1 Level 9 or lower "Azamina" monster from your Deck or Extra Deck
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	e3:SetCondition(function(e) return e:GetHandler():HasFlagEffect(id) end)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+	--Register Fusion Summon
+	local e3a=Effect.CreateEffect(c)
+	e3a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3a:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3a:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) end)
+	e3a:SetOperation(function(e) e:GetHandler():RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1) end)
+	c:RegisterEffect(e3a)
+end
+s.miracle_synchro_fusion=true
+s.listed_series={SET_AZAMINA}
+function s.matfilter(ctype)
+	return function(c,...) return c:IsLevelAbove(6) and c:IsType(ctype,...) end
+end
+function s.spfilter(c,e,tp)
+	if not (c:IsSetCard(SET_AZAMINA) and c:IsLevelBelow(9) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) then return false end
+	if c:IsLocation(LOCATION_DECK) then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	else
+		return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+	end
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

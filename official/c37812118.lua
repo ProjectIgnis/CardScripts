892036ce896d4ad8f0b84cdaa7e193e70d762ff3
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-10-18 13:12:32 +0300

    "Cup of Ace" fix
    Should be able to activate it if either player can draw 2 cards, not only if both players can draw 2 cards.

--- official/c37812118.lua
+++ official/c37812118.lua
@@ -4,7 +4,8 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW+CATEGORY_COIN)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COIN+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
@@ -13,12 +14,12 @@ function s.initial_effect(c)
 end
 s.toss_coin=true
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) and Duel.IsPlayerCanDraw(1-tp,2) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) or Duel.IsPlayerCanDraw(1-tp,2) end
 	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_EITHER,2)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local res=Duel.TossCoin(tp,1)
-	if res==COIN_HEADS then Duel.Draw(tp,2,REASON_EFFECT)
-	elseif res==COIN_TAILS then Duel.Draw(1-tp,2,REASON_EFFECT)
-	end
+	local coin=Duel.TossCoin(tp,1)
+	local player=coin==COIN_HEADS and tp or 1-tp
+	Duel.Draw(player,2,REASON_EFFECT)
 end

2ab92c1b80f1fa970ef8190a1285ff2db6ed2fb5
Naim <naimsantos45@gmail.com>
2024-10-22 04:10:47 -0300

    Fix "Sales Ban"
    Fixed a bug where it would not possible to activate a second copy if the first one had its activation negated

--- official/c64964750.lua
+++ official/c64964750.lua
@@ -1,5 +1,5 @@
 --発禁令
---Proscription
+--Sales Ban
 --Scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -23,7 +23,7 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-	--Opponent cannot activate this turn
+	--Opponent cannot activate cards or effects of cards with the same original name this turn
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -32,7 +32,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTargetRange(0,1)
 	e1:SetLabel(ac)
 	e1:SetValue(s.aclimit)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	--You cannot activate this Duel
 	local e2=e1:Clone()

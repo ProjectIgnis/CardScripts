ecfb4e26e89a69bf555345988b67686ed397aba4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-10-22 12:27:27 -0700

    Fix "Gate Guardian Ritual"
    Should be able to be searched by "Gate Guardian of Thunder and Wind"/script update

--- unofficial/c511001815.lua
+++ unofficial/c511001815.lua
@@ -1,48 +1,50 @@
+--ゲート・ガーディアンのぎ式
 --Gate Guardian Ritual
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Ritual Summon "Gate Guardian" from your hand or Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-s.fit_monster={25833572}
-function s.filter(c,e,tp,m)
+s.fit_monster={25833572} --"Gate Guardian"
+s.listed_names=CARDS_SANGA_KAZEJIN_SUIJIN
+function s.ritfilter(c,e,tp,m)
 	local cd=c:GetCode()
 	if cd~=25833572 or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,true,false) then return false end
 	if m:IsContains(c) then
 		m:RemoveCard(c)
-		result=m:IsExists(Card.IsCode,1,nil,25955164) and m:IsExists(Card.IsCode,1,nil,62340868) 
-			and m:IsExists(Card.IsCode,1,nil,98434877)
+		result=m:IsExists(Card.IsCode,1,nil,CARD_SUIJIN) and m:IsExists(Card.IsCode,1,nil,CARD_KAZEJIN) 
+			and m:IsExists(Card.IsCode,1,nil,CARD_SANGA_OF_THE_THUNDER)
 		m:AddCard(c)
 	else
-		result=m:IsExists(Card.IsCode,1,nil,25955164) and m:IsExists(Card.IsCode,1,nil,62340868) 
-			and m:IsExists(Card.IsCode,1,nil,98434877)
+		result=m:IsExists(Card.IsCode,1,nil,CARD_SUIJIN) and m:IsExists(Card.IsCode,1,nil,CARD_KAZEJIN) 
+			and m:IsExists(Card.IsCode,1,nil,CARD_SANGA_OF_THE_THUNDER)
 	end
 	return result
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local mg=Duel.GetRitualMaterial(tp)
-		return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp,mg)
+		return Duel.IsExistingMatchingCard(s.ritfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp,mg)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetRitualMaterial(tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp,mg)
+	local tg=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp,mg)
 	if #tg>0 then
 		local tc=tg:GetFirst()
 		mg:RemoveCard(tc)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-		local mat1=mg:FilterSelect(tp,Card.IsCode,1,1,nil,25955164)
-		local mat2=mg:FilterSelect(tp,Card.IsCode,1,1,nil,62340868)
-		local mat3=mg:FilterSelect(tp,Card.IsCode,1,1,nil,98434877)
+		local mat1=mg:FilterSelect(tp,Card.IsCode,1,1,nil,CARD_SUIJIN)
+		local mat2=mg:FilterSelect(tp,Card.IsCode,1,1,nil,CARD_KAZEJIN)
+		local mat3=mg:FilterSelect(tp,Card.IsCode,1,1,nil,CARD_SANGA_OF_THE_THUNDER)
 		mat1:Merge(mat2)
 		mat1:Merge(mat3)
 		tc:SetMaterial(mat1)

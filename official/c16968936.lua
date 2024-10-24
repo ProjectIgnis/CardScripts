a7a2284ac9685253db6760287785b811eecb965d
Hatter <47074795+that-hatter@users.noreply.github.com>
2024-10-24 08:06:56 +0800

    fix "Tactical Trainer of the Six Samurai"
    should be able to add "Six Strike" card from the GY to the hand

--- official/c16968936.lua
+++ official/c16968936.lua
@@ -1,5 +1,5 @@
 --六武衆の指南番
---Instructor of the Six Samurai
+--Tactical Trainer of the Six Samurai
 --scripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
@@ -50,12 +50,12 @@ function s.thfilter(c)
 	return c:IsSetCard(SET_SIX_STRIKE) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)

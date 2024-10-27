4bd5a7e759a3553ebf06567ce673341c90d9edc5
Naim <naimsantos45@gmail.com>
2024-10-27 07:24:33 -0300

    Rescript "Raging Cloudian"
    - It was not checking if the monster destroyed was a "Cloudian" monster or if it could be Special Summoned in Attack Position
    - Fixed a bug where it could summon a non-"Cloudian" monster
    - Added target redirection handling
    - Added support to proper target selection when multiple monsters were sent to the GY at the same time

--- official/c23639291.lua
+++ official/c23639291.lua
@@ -2,28 +2,66 @@
 --Raging Cloudian
 local s,id=GetID()
 function s.initial_effect(c)
+	--Special Summon 1 "Cloudian" monster that was destroyed by its own effect
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_TO_GRAVE)
-	e1:SetCondition(s.condition)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_CUSTOM+id)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	e1:SetLabelObject(g)
+	--Register when "Cloudian" monsters are sent to the GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SET_AVAILABLE)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetLabelObject(e1)
+	e2:SetOperation(s.regop)
+	Duel.RegisterEffect(e2,0)
 end
-s.counter_place_list={0x1019}
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	return #eg==1 and tc:IsControler(tp) and tc:IsPreviousControler(tp) and tc:IsReason(REASON_DESTROY)
-		and tc:GetReasonEffect() and tc:GetReasonEffect():GetOwner()==tc
+s.counter_place_list={COUNTER_FOG}
+s.listed_series={SET_CLOUDIAN}
+function s.cfilter(c,e,tp)
+	return c:IsSetCard(SET_CLOUDIAN) and c:IsControler(tp) and c:IsPreviousControler(tp)
+		and c:IsReason(REASON_DESTROY) and c:GetReasonEffect() and c:GetReasonEffect():GetOwner()==c
+		and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=eg:Filter(s.cfilter,nil,e,tp)
+	if #tg>0 then
+		for tc in tg:Iter() do
+			tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+		end
+		local g=e:GetLabelObject():GetLabelObject()
+		if Duel.GetCurrentChain()==0 then g:Clear() end
+		g:Merge(tg)
+		g:Remove(function(c) return c:GetFlagEffect(id)==0 end,nil)
+		e:GetLabelObject():SetLabelObject(g)
+		if Duel.GetFlagEffect(tp,id)==0 then
+			Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
+			Duel.RaiseEvent(eg,EVENT_CUSTOM+id,e,0,tp,tp,0)
+		end
+	end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc==eg:GetFirst() end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and eg:GetFirst():IsCanBeSpecialSummoned(e,0,tp,false,false) and eg:GetFirst():IsCanBeEffectTarget(e) end
-	Duel.SetTargetCard(eg:GetFirst())
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,eg:GetFirst(),1,0,0)
+	local g=e:GetLabelObject():Filter(s.cfilter,nil,e,tp)
+	if chkc then return g:IsContains(chkc) and s.cfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and #g>0 end
+	local tg=nil
+	if #g>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		tg=g:Select(tp,1,1,nil)
+	else
+		tg=g
+	end
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tg,1,tp,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
@@ -33,9 +71,9 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e1:SetCode(EFFECT_CANNOT_CHANGE_POS_E)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
 	Duel.SpecialSummonComplete()
-	tc:AddCounter(0x1019,1)
+	tc:AddCounter(COUNTER_FOG,1)
 end

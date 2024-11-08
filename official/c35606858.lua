44b90866cc4d88fbaeced4330617006f5bc9887d
Naim <naimsantos45@gmail.com>
2024-11-08 09:44:35 -0300

    Update "Masterking Archfiend"
    - Fixed a bug that allowed the effects to be activated more than once per chain.
    - Fixed a bug that prevented the second effect from applying to different cards/groups if they were sent to the GY at different times in the previous chain.

--- official/c35606858.lua
+++ official/c35606858.lua
@@ -23,8 +23,8 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_PAY_LPCOST)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
 	e2:SetCondition(function(_,tp,_,ep) return ep==tp end)
-	e2:SetLabel(0)
 	e2:SetTarget(s.tgtg)
 	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
@@ -32,14 +32,26 @@ function s.initial_effect(c)
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DICE+CATEGORY_TOHAND+CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCode(EVENT_CUSTOM+id)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetLabel(1)
+	e3:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
 	e3:SetTarget(s.dctg)
 	e3:SetOperation(s.dcop)
 	c:RegisterEffect(e3)
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	e3:SetLabelObject(g)
+	--Register Fiend monsters sent to your GY
+	local e3a=Effect.CreateEffect(c)
+	e3a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3a:SetCode(EVENT_TO_GRAVE)
+	e3a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3a:SetRange(LOCATION_MZONE)
+	e3a:SetLabelObject(e3)
+	e3a:SetOperation(s.regop)
+	c:RegisterEffect(e3a)
 end
 s.roll_dice=true
 function s.mtop(e,tp,eg,ep,ev,re,r,rp)
@@ -48,12 +60,6 @@ function s.mtop(e,tp,eg,ep,ev,re,r,rp)
 	else
 		Duel.Destroy(e:GetHandler(),REASON_COST)
 	end
-end
-function s.once_per_chain_chk(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	local label=e:GetLabel()
-	if chk==0 then return c:GetFlagEffect(id+label)==0 and not c:IsStatus(STATUS_CHAINING) end
-	c:RegisterFlagEffect(id+label,RESET_CHAIN,0,1)
 end
 function s.tgfilter(c,val)
 	return c:IsRace(RACE_FIEND) and c:IsMonster() and (c:IsAttack(val) or c:IsDefense(val)) and c:IsAbleToGrave()
@@ -71,12 +77,30 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.cfilter(c,e,tp,ft)
 	return c:IsRace(RACE_FIEND) and c:IsControler(tp) and (c:IsAbleToHand() or c:IsAbleToDeck()
-		or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
+		or (ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false))) and c:HasFlagEffect(id+1)
+end
+function s.regfilter(c,tp)
+	return c:IsRace(RACE_FIEND) and c:IsControler(tp)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=eg:Filter(s.regfilter,nil,tp)
+	if #tg>0 then
+		for tc in tg:Iter() do
+			tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+			tc:RegisterFlagEffect(id+1,RESET_EVENT|RESET_REMOVE|RESET_TEMP_REMOVE|RESET_TOHAND|RESET_TODECK|RESET_LEAVE|RESET_TOFIELD,0,1)
+		end
+		local g=e:GetLabelObject():GetLabelObject()
+		if Duel.GetCurrentChain()==0 then g:Clear() end
+		g:Merge(tg)
+		g:Remove(function(c) return c:GetFlagEffect(id)==0 end,nil)
+		e:GetLabelObject():SetLabelObject(g)
+		Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+id,e,0,tp,tp,0)
+	end
 end
 function s.dctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return eg:IsExists(s.cfilter,1,nil,e,tp,ft) end
-	local g=eg:Filter(s.cfilter,nil,e,tp,ft)
+	local g=e:GetLabelObject():Filter(s.cfilter,nil,e,tp,ft)
+	if chk==0 then return #g>0 end
 	Duel.SetTargetCard(g)
 	Duel.SetOperationInfo(0,CATEGORY_DICE,nil,0,tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,tp,0)
@@ -85,7 +109,8 @@ function s.dctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.dcop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetTargetCards(e)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local g=e:GetLabelObject():Filter(s.cfilter,nil,e,tp,ft)
 	if #g==0 then return end
 	local d=Duel.TossDice(tp,1)
 	local tc=nil

0266734a55883daefb917efc7679654824ac878e
yoshiduels <yoshi.katreuvin@gmail.com>
2024-11-13 12:44:45 +0100

    added new rush cards

---
+++ rush/c160019003.lua
@@ -0,0 +1,38 @@
+--ＨＰアシスタント・ビリー
+--Hyper Assistant Biri
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Mill and add to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160316023} --Super Assistant Achi
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	local c=e:GetHandler()
+	if Duel.SendtoGrave(c,REASON_COST)<1 then return end
+	--Effect
+	if Duel.DiscardDeck(tp,3,REASON_EFFECT)<1 then return end
+	local dg=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,0,LOCATION_ONFIELD,nil)
+	if Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,160316023) and #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local sg=dg:Select(tp,1,1,nil)
+		Duel.HintSelection(sg)
+		Duel.Destroy(sg,REASON_EFFECT)
+	end
+end

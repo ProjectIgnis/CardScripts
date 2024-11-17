076d1d317fcbb3da746bde11ff10cb4a95a1b1e8
Naim <naimsantos45@gmail.com>
2024-11-17 06:57:17 -0300

    "Geargia Change" update
    - Now it allows cancelation of the targets
    - General script cleanup and modernization

--- official/c29087919.lua
+++ official/c29087919.lua
@@ -2,8 +2,9 @@
 --Geargia Change
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Special Summon 2 or more "Geargiano" monsters with different names from your GY and Xyz Summon 1 Xyz Monster using the Summoned monsters
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -13,64 +14,44 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x1072}
-function s.filter(c,e,tp)
-	return c:IsSetCard(0x1072) and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+s.listed_series={SET_GEARGIANO}
+function s.gyspfilter(c,e,tp)
+	return c:IsSetCard(SET_GEARGIANO) and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.xyzfilter(c,mg,ct)
-	return c:IsXyzSummonable(nil,mg,2,ct)
+function s.extraspfilter(c,mg)
+	return c:IsXyzSummonable(nil,mg,2,#mg)
 end
-function s.mfilter1(c,mg,exg,ct)
-	return mg:IsExists(s.mfilter2,1,nil,Group.FromCards(c),mg,exg,ct)
-end
-function s.mfilter2(c,g,mg,exg,ct)
-	local tc=g:GetFirst()
-	for tc in aux.Next(g) do
-		if c:IsCode(tc:GetCode()) then return false end
-	end
-	g:AddCard(c)
-	local result=exg:IsExists(Card.IsXyzSummonable,1,nil,nil,g,#g,#g)
-		or (#g<ct and mg:IsExists(s.mfilter2,1,nil,g,mg,exg,ct))
-	g:RemoveCard(c)
-	return result
+function s.rescon(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetCode)==#sg and Duel.IsExistingMatchingCard(s.extraspfilter,tp,LOCATION_EXTRA,0,1,nil,sg)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,e,tp)
-	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	local exg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,mg,ct)
+	local mg=Duel.GetMatchingGroup(s.gyspfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	local maxct=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),mg:GetClassCount(Card.GetCode))
 	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
 		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
-		and ct>1 and mg:IsExists(s.mfilter1,1,nil,mg,exg,ct) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg1=mg:FilterSelect(tp,s.mfilter1,1,1,nil,mg,exg,ct)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg2=mg:FilterSelect(tp,s.mfilter2,1,1,nil,sg1,mg,exg,ct)
-	sg1:Merge(sg2)
-	while #sg1<ct and mg:IsExists(s.mfilter2,1,nil,sg1,mg,exg,ct)
-		and (not exg:IsExists(Card.IsXyzSummonable,1,nil,nil,sg1,#sg1,#sg1) or Duel.SelectYesNo(tp,aux.Stringid(id,0))) do
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg3=mg:FilterSelect(tp,s.mfilter2,1,1,nil,sg1,mg,exg,ct)
-		sg1:Merge(sg3)
-	end
-	Duel.SetTargetCard(sg1)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg1,#sg1,0,0)
+		and maxct>=2 and aux.SelectUnselectGroup(mg,e,tp,2,maxct,s.rescon,0) end
+	local sg=aux.SelectUnselectGroup(mg,e,tp,2,maxct,s.rescon,1,tp,HINTMSG_SPSUMMON)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,#sg,tp,0)
 end
-function s.filter2(c,e,tp)
-	return c:IsRelateToEffect(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.spfilter(c,mg,ct)
+function s.xyzfilter(c,mg,ct)
 	return c:IsXyzSummonable(nil,mg,ct,ct)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(s.filter2,nil,e,tp)
-	local ct=Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	local xyzg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,g,ct)
+	local sg=Duel.GetTargetCards(e):Filter(Card.IsCanBeSpecialSummoned,nil,e,0,tp,false,false)
+	if #sg>=2 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if #sg>ft then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		sg=sg:Select(tp,ft,ft,nil)
+	end
+	local ct=Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,sg,ct)
 	if ct>=2 and #xyzg>0 then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
-		Duel.XyzSummon(tp,xyz,nil,g)
+		local xyzc=xyzg:Select(tp,1,1,nil):GetFirst()
+		Duel.XyzSummon(tp,xyzc,nil,sg)
 	end
 end

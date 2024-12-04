9d3925a72d622dbf6d0f9c3eb2b9e86cb2129a1b
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-12-03 20:49:01 -0800

    Update "The Dragon Hunting Swordsman"
    Cleaned up Fusion Summon code to be more inline with modern script

--- skill/c300307009.lua
+++ skill/c300307009.lua
@@ -17,13 +17,13 @@ s.listed_names={CARD_BUSTER_BLADER}
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--Fusion Summon 1 Fusion Monster that mentions "Buster Blader"
+	--Fusion Summon 1 Fusion Monster that lists "Buster Blader" as material
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(0x5f)
-	e1:SetCondition(function(_,tp) return aux.CanActivateSkill(tp) and Duel.GetFlagEffect(tp,id)==0 end)
-	e1:SetOperation(s.flipop)
+	e1:SetCondition(s.fuscon)
+	e1:SetOperation(s.fusop)
 	Duel.RegisterEffect(e1,tp)
 	--Opponent's GY becomes Dragon monsters
 	local e2=Effect.CreateEffect(e:GetHandler())
@@ -36,76 +36,46 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetValue(RACE_DRAGON)
 	Duel.RegisterEffect(e2,tp)
 end
-function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	local g2=s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0)
-	--OPD Register
-	Duel.RegisterFlagEffect(tp,id,0,0,0)
-	--Fusion Summon 1 Fusion Monster that lists "Buster Blader" as material
-	s.fusTarget(e,tp,eg,ep,ev,re,r,rp,1)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
-	local sg1=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-	tc:CompleteProcedure()
-	end
-end
---Fusion Summon Functions
-function s.cfilter(c,e,tp)
+function s.costfilter(c,e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsDiscardable() then return false end
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp)
-	local res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg1-c,nil,chkf)
-	if not res then
-		local ce=Duel.GetChainMaterial(tp)
-		if ce~=nil then
-			local fgroup=ce:GetTarget()
-			local mg2=fgroup(ce,e,tp)
-			local mf=ce:GetValue()
-			res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg2-c,mf,chkf)
-		end
+	local params={fusfilter=aux.FilterBoolFunction(Card.ListsCodeAsMaterial,CARD_BUSTER_BLADER),extrafil=s.fextra(c)}
+	return Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp,eg,ep,ev,re,r,rp) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST|REASON_DISCARD,nil,e,tp,eg,ep,ev,re,r,rp)
+end
+function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
+	return aux.CanActivateSkill(tp) and s.cost(e,tp,eg,ep,ev,re,r,rp,0) and not Duel.HasFlagEffect(tp,id)
+end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	local params={fusfilter=aux.FilterBoolFunction(Card.ListsCodeAsMaterial,CARD_BUSTER_BLADER),extrafil=s.fextra(c)}
+	--You can only apply this effect once per Duel
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	--Discard 1 card
+	s.cost(e,tp,eg,ep,ev,re,r,rp,1)
+	--Fusion Summon 1 Fusion Monster that lists "Buster Blader" as material, using monsters from your hand or field as material
+	Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,1)
+	Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp)
+end
+function s.fextra(exc)
+	return function(e,tp,mg)
+		return nil,s.fcheck(exc)
 	end
-	return res
 end
-function s.fusfilter(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and (not f or f(c)) and c:ListsCodeAsMaterial(CARD_BUSTER_BLADER)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function s.fusTarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST+REASON_DISCARD,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+function s.fcheck(exc)
+	return function(tp,sg,fc)
+		return not (exc and sg:IsContains(exc))
+	end
 end
 --Race Change Functions
-function s.bfilter(c)
+function s.busterbladerfilter(c)
 	return c:IsFaceup() and (c:IsCode(CARD_BUSTER_BLADER) or (c:IsType(TYPE_FUSION) and c:ListsCodeAsMaterial(CARD_BUSTER_BLADER)))
 end
 function s.racecon(e)
 	local tp=e:GetHandlerPlayer()
 	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
-	return #g==1 and Duel.IsExistingMatchingCard(s.bfilter,tp,LOCATION_MZONE,0,1,nil)
+	return #g==1 and Duel.IsExistingMatchingCard(s.busterbladerfilter,tp,LOCATION_MZONE,0,1,nil)
 end

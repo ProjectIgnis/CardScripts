0abc1c9ba297e0d84d0969a1dce2a893a7a6906e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-12-06 14:34:00 -0800

    Update "Beasts of Phantom"
    Cleaned up Fusion Summon effect code/general script update

--- skill/c300303014.lua
+++ skill/c300303014.lua
@@ -3,15 +3,15 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
-		--Checks if Chimera was Fusion Summoned
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_STARTUP)
-		e1:SetRange(0x5f)
-		e1:SetCountLimit(1)
-		e1:SetOperation(s.op) 
-		c:RegisterEffect(e1)
-		aux.GlobalCheck(s,function()
+	--Checks if Chimera was Fusion Summoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        e1:SetCode(EVENT_STARTUP)
+        e1:SetRange(0x5f)
+        e1:SetCountLimit(1)
+        e1:SetOperation(s.op) 
+        c:RegisterEffect(e1)
+	aux.GlobalCheck(s,function()
 		s[0]=0
 		s[1]=0
 		local ge1=Effect.CreateEffect(c)
@@ -20,163 +20,93 @@ function s.initial_effect(c)
 		ge1:SetOperation(s.checkop)
 		Duel.RegisterEffect(ge1,0)
 		aux.AddValuesReset(function()
-			s[0]=0
-			s[1]=0
+        		s[0]=0
+        		s[1]=0
 		end)
 	end)
 end
+s.listed_names={4796100} --"Chimera the Flying Mythical Beast"
+s.listed_series={SET_PHANTOM_BEAST}
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetCountLimit(1)
-		e1:SetCondition(s.flipcon2)
-		e1:SetOperation(s.flipop2)
-		Duel.RegisterEffect(e1,tp)
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        e1:SetCode(EVENT_PHASE+PHASE_END)
+        e1:SetCountLimit(1)
+        e1:SetCondition(s.flipcon2)
+        e1:SetOperation(s.flipop2)
+        Duel.RegisterEffect(e1,tp)
 end
-s.listed_names={4796100}
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	for tc in aux.Next(eg) do
-		if tc:IsCode(4796100) and tc:IsSummonType(SUMMON_TYPE_FUSION) then
-			local p=tc:GetSummonPlayer()
-			s[p]=s[p]+1
+	for tc in eg:Iter() do
+	if tc:IsCode(4796100) and tc:IsSummonType(SUMMON_TYPE_FUSION) then
+		local p=tc:GetSummonPlayer()
+		s[p]=s[p]+1
 		end
 	end
 end
+function s.costfilter(c,e,tp,eg,ep,ev,re,r,rp)
+	if not c:IsDiscardable() then return false end
+	local params={fusfilter=aux.FilterBoolFunction(Card.IsCode,4796100),extrafil=s.fextra(c)}
+	return Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp,eg,ep,ev,re,r,rp) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST|REASON_DISCARD,nil,e,tp,eg,ep,ev,re,r,rp)
+end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return aux.CanActivateSkill(tp) and s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0) and Duel.GetFlagEffect(tp,id)==0
+	return aux.CanActivateSkill(tp) and s.cost(e,tp,eg,ep,ev,re,r,rp,0) and not Duel.HasFlagEffect(tp,id)
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--fusion
-	local g2=s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0)
-	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-	s.fusTarget(e,tp,eg,ep,ev,re,r,rp,1)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
-	local sg1=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-	tc:CompleteProcedure()
+	local params={fusfilter=aux.FilterBoolFunction(Card.IsCode,4796100),extrafil=s.fextra(c)}
+	--You can only apply this effect once per turn
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	--Discard 1 card
+	s.cost(e,tp,eg,ep,ev,re,r,rp,1)
+	--Fusion Summon "Chimera the King of Mythical Beasts", using monsters from your hand or field as material
+	Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,1)
+	Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp)
+end
+function s.fextra(exc)
+	return function(e,tp,mg)
+		return nil,s.fcheck(exc)
 	end
 end
---fusion Summon Chimera
-function s.cfilter(c,e,tp)
-	if not c:IsDiscardable() then return false end
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp)
-	local res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg1-c,nil,chkf)
-	if not res then
-		local ce=Duel.GetChainMaterial(tp)
-		if ce~=nil then
-			local fgroup=ce:GetTarget()
-			local mg2=fgroup(ce,e,tp)
-			local mf=ce:GetValue()
-			res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg2-c,mf,chkf)
-		end
+function s.fcheck(exc)
+	return function(tp,sg,fc)
+		return not (exc and sg:IsContains(exc))
 	end
-	return res
-end
-function s.fusfilter(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and (not f or f(c)) and c:IsCode(4796100)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function s.fusTarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST+REASON_DISCARD,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 --Add "Phantom Beast" card to hand
 function s.thfilter(c)
-	return c:IsSetCard(0x1b) and c:IsAbleToHand()
-end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return (s[tp]>0 or s[1-tp]>0)  and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-		if tc then
-			Duel.SendtoHand(tc,tp,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
-		end
-	end
+	return c:IsSetCard(SET_PHANTOM_BEAST) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
-	return (s[tp]>0 or s[1-tp]>0) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.GetFlagEffect(tp,id+100)==0
+	return (s[tp]>0 or s[1-tp]>0) and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.GetTurnPlayer()==tp and not Duel.HasFlagEffect(tp,id+100)
 end
 function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
+	--Add 1 "Phantom Beast" monster from your GY to  your hand
 	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.RegisterFlagEffect(tp,id+100,RESET_PHASE+PHASE_END,0,1)
+		Duel.RegisterFlagEffect(tp,id+100,RESET_PHASE|PHASE_END,0,1)
 		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 		Duel.Hint(HINT_CARD,tp,id)
-		local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-		if tc then
-			Duel.SendtoHand(tc,tp,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.SendtoHand(g,tp,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
 		end
 	end
-	--Fusion Summon Chimera
-	if not (Duel.IsMainPhase() and Duel.GetTurnPlayer()==tp) then return end
-	local g2=s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0)
-	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-	s.fusTarget(e,tp,eg,ep,ev,re,r,rp,1)
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
-	local sg1=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-	local mg2=nil
-	local sg2=nil
-	local ce=Duel.GetChainMaterial(tp)
-	if ce~=nil then
-		local fgroup=ce:GetTarget()
-		mg2=fgroup(ce,e,tp)
-		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-	end
-	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-		local sg=sg1:Clone()
-		if sg2 then sg:Merge(sg2) end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:Select(tp,1,1,nil)
-		local tc=tg:GetFirst()
-		if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-			local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-			tc:SetMaterial(mat1)
-			Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-		else
-			local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-			local fop=ce:GetOperation()
-			fop(ce,e,tp,tc,mat2)
-		end
-	tc:CompleteProcedure()
-	end
+	--Fusion Summon effect
+	if not (Duel.IsMainPhase() and aux.CanActivateSkill(tp) and Duel.GetTurnPlayer()==tp and s.cost(e,tp,eg,ep,ev,re,r,rp,0)) then return end
+	local params={fusfilter=aux.FilterBoolFunction(Card.IsCode,4796100),extrafil=s.fextra(c)}
+	--You can only apply this effect once per turn
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	--Discard 1 card
+	s.cost(e,tp,eg,ep,ev,re,r,rp,1)
+	--Fusion Summon "Chimera the King of Mythical Beasts", using monsters from your hand or field as material
+	Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,1)
+	Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp)
 end

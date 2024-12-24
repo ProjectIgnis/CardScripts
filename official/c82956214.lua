ee6c218cbd96ac3d778981411b0418484d3806b9
Naim <naimsantos45@gmail.com>
2024-12-24 04:20:21 -0300

    "Dogmatika Punishment" fix
    Fixed a bug where a card applying its effect would also apply the Special Summon restriction even if that card was not being activated (e.g. the effect from the GY of "Transaction Rollback")

--- official/c82956214.lua
+++ official/c82956214.lua
@@ -1,13 +1,11 @@
 --ドラグマ・パニッシュメント
 --Dogmatika Punishment
---Logical Nonsense
-
---Substitute ID
+--scripted by Logical Nonsense
 local s,id=GetID()
 function s.initial_effect(c)
 	--Send 1 monster from extra deck to GY to destroy opponent's monster
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -32,33 +30,28 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,0,LOCATION_MZONE,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,s.desfilter,tp,0,LOCATION_MZONE,1,1,nil,tp)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
 end
 	--Send 1 monster from extra deck to GY to destroy opponent's monster
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 		local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil,tc)
 		if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_GRAVE) then
 			Duel.Destroy(tc,REASON_EFFECT)
 		end
 	end
+	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+	--Until the end of your next turn after this card resolves, you cannot Special Summon monsters from the Extra Deck
+	local ct=Duel.IsTurnPlayer(tp) and 2 or 1
 	local e0=Effect.CreateEffect(e:GetHandler())
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e0:SetDescription(aux.Stringid(id,1))
+	e0:SetType(EFFECT_TYPE_FIELD)
 	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	if Duel.GetTurnPlayer()==tp then
-		e0:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
-	else
-		e0:SetReset(RESET_PHASE+PHASE_END+RESET_SELF_TURN)
-	end
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e0:SetReset(RESET_PHASE|PHASE_END|RESET_SELF_TURN,ct)
 	e0:SetTargetRange(1,0)
-	e0:SetTarget(s.splimit)
+	e0:SetTarget(function(e,c,sump,sumtype,sumpos,targetp) return c:IsLocation(LOCATION_EXTRA) end)
 	Duel.RegisterEffect(e0,tp)
-end
-	--Cannot special summon from extra deck
-function s.splimit(e,c,sump,sumtype,sumpos,targetp)
-	return c:IsLocation(LOCATION_EXTRA)
 end

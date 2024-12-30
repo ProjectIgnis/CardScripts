b88c791185386c5f1405eb894ea0b9396c19836f
yoshiduels <yoshi.katreuvin@gmail.com>
2024-12-30 11:29:22 +0100

    added Dead Line the Mad Fiend of Despair

---
+++ rush/c160020035.lua
@@ -0,0 +1,43 @@
+--絶望狂魔デッド・ライン
+--Dead Line the Mad Fiend of Despair
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send the top 2 cards of your Deck to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(e)return e:GetHandler():IsStatus(STATUS_SUMMON_TURN)end)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160015033} --Dead End the Mad Fiend of Despair
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,2)
+end
+function s.spfilter(c,e,tp)
+	return c:IsCode(160015033) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.SendtoGrave(e:GetHandler(),REASON_COST)<1 then return end
+	--Effect
+	if Duel.DiscardDeck(tp,2,REASON_EFFECT)~=2 then return end
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if #sg>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local ssg=sg:Select(tp,1,1,nil)
+		if #ssg>0 then
+			Duel.SpecialSummon(ssg,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

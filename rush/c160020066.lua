ce5ba660c6c2eeb4def6729736de9ec75414499b
yoshiduels <yoshi.katreuvin@gmail.com>
2025-01-02 14:13:40 +0100

    added Miracle Contact

---
+++ rush/c160020066.lua
@@ -0,0 +1,17 @@
+--ミラクル・コンタクト
+--Miracle Contact
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Fusion.CreateSummonEff(c,s.filter,s.mfilter,s.fextra,Fusion.ShuffleMaterial)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:ListsCodeAsMaterial(CARD_NEOS)
+end
+function s.mfilter(c)
+	return c:IsLocation(LOCATION_GRAVE|LOCATION_MZONE) and c:IsAbleToDeck()
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE|LOCATION_MZONE,0,nil)
+end

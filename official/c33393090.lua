ec87a0cc1f768b2c2b0736aaf8de123662da4452
Naim <naimsantos45@gmail.com>
2025-01-17 06:38:40 -0300

    "Yowie" fix
    Fixed a bug where the next Draw Phase would not be properly skipped if the effect of "Yowie" was activated during the opponent's Draw Phase

--- official/c33393090.lua
+++ official/c33393090.lua
@@ -1,15 +1,15 @@
--- ヨーウィー
--- Yowie
--- scripted by Hatter
+--ヨーウィー
+--Yowie
+--Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Skip opponent's next draw phase
+	--Skip opponent's next draw phase
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
@@ -21,7 +21,7 @@ function s.initial_effect(c)
 	local e3=e1:Clone()
 	e3:SetCode(EVENT_SUMMON_SUCCESS)
 	c:RegisterEffect(e3)
-	-- Count special summons
+	--Count special summons
 	aux.GlobalCheck(s,function()
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -31,20 +31,20 @@ function s.initial_effect(c)
 	end)
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(ep,id,RESET_PHASE+PHASE_END,0,1)
+	Duel.RegisterFlagEffect(ep,id,RESET_PHASE|PHASE_END,0,1)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return #eg==1 and eg:IsContains(e:GetHandler())
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFlagEffect(e:GetOwnerPlayer(),id)<=1 end
-	-- Cannot special summon more than once
+	--Cannot special summon more than once
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	e1:SetOwnerPlayer(tp)
 	e1:SetCondition(s.spcon)
 	e1:SetTargetRange(1,0)
@@ -54,11 +54,12 @@ function s.spcon(e)
 	return Duel.GetFlagEffect(e:GetOwnerPlayer(),id)>0
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local is_op_drawphase=Duel.IsTurnPlayer(1-tp) and Duel.IsPhase(PHASE_DRAW)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(0,1)
 	e1:SetCode(EFFECT_SKIP_DP)
-	e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_OPPO_TURN)
+	e1:SetReset(RESET_PHASE|PHASE_DRAW|RESET_OPPO_TURN,is_op_drawphase and 2 or 1)
 	Duel.RegisterEffect(e1,tp)
 end

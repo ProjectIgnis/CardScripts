05a07e6b9a4133d80451079ebd8461dcc8b4e400
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-01-18 17:45:59 -0800

    Fix "Magikey Mechmortar - Garesglasser"
    Wrong effect identifier for count limit/minor script modernization

--- official/c45877457.lua
+++ official/c45877457.lua
@@ -1,16 +1,16 @@
 --魔鍵砲－ガレスヴェート
---Magikey Cannon - Garesvet
---scripted by Rundas
+--Magikey Mechmortar - Garesglasser
+--Scripted by Rundas
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--ATK Up
+	--Gains 300 ATK for each different Attribute in your GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
-	--Negate
+	--Negate the activation of an opponent's monster effect and destroy it
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
@@ -23,33 +23,33 @@ function s.initial_effect(c)
 	e2:SetTarget(s.negtg)
 	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
-	--Search
+	--Add 1 "Magikey" monster from your Deck to your hand
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,{id,1})
+	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.thcon)
 	e3:SetTarget(s.thtg)
 	e3:SetOperation(s.thop)
 	c:RegisterEffect(e3)
 end
-s.listed_names={99426088}
-s.listed_series={0x167}
---ATK Up
+s.listed_series={SET_MAGIKEY}
+--ATK value (300 for each different Attribute in your GY)
 function s.atkval(e,c)
 	local g=Duel.GetMatchingGroup(Card.IsMonster,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil)
 	return g:GetClassCount(Card.GetAttribute)*300
 end
---Negate
+--Negate the activation of an opponent's monster effect
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,0,nil)
 	local c,rc,att=e:GetHandler(),re:GetHandler(),0
 	local mat=c:GetMaterial()
-	if re:IsActiveType(TYPE_MONSTER) and rp==1-tp and not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
-		and c:IsSummonType(SUMMON_TYPE_RITUAL) and mat:GetClassCount(Card.GetAttribute)>1 then
-		for tc in Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,0,nil):Iter() do
+	if re:IsMonsterEffect() and rp==1-tp and not c:IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
+		and c:IsRitualSummoned() and mat:GetClassCount(Card.GetAttribute)>1 then
+		for tc in g:Iter() do
 			att=att|tc:GetAttribute()
 		end
 		return rc:GetAttribute()&att>0
@@ -68,13 +68,13 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
 end
---Search
+--Add 1 "Magikey" monster from your Deck to your hand
 function s.thfilter(c)
-	return c:IsSetCard(0x167) and c:IsMonster() and c:IsAbleToHand()
+	return c:IsSetCard(SET_MAGIKEY) and c:IsMonster() and c:IsAbleToHand()
 end
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_RITUAL)
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsRitualSummoned()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -82,8 +82,8 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #tc>0 then
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tc)
 	end

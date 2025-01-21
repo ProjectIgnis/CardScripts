bd190086fcea443f8eb96170aeac399d33dc649f
yoshiduels <yoshi.katreuvin@gmail.com>
2025-01-21 11:51:25 +0100

    added Awakened Stegoceratops

---
+++ rush/c160020018.lua
@@ -0,0 +1,52 @@
+--覚醒のステゴケラトプス
+--Awakened Stegoceratops
+--Scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Mill and add to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160213051} --Ultra Evolution Miracle
+function s.filter(c)
+	return c:IsMonster() and not c:IsRace(RACE_DINOSAUR)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil)
+		and not Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,500) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thfilter(c)
+	return (c:IsCode(160213051) or (c:IsMonster() and c:IsRace(RACE_DINOSAUR) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsAttack(1100))) and c:IsAbleToHand()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.PayLPCost(tp,500)
+	--Effect
+	if Duel.DiscardDeck(tp,3,REASON_EFFECT)<1 then return end
+	if Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end

0d934fa50184074a02d964f80841eedd3d4a75ca
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-01-22 19:17:48 -0800

    Update "Royal Tribute"
    Add "Necrovalley" identifier/minor script update

--- official/c72405967.lua
+++ official/c72405967.lua
@@ -1,4 +1,5 @@
 --王家の生け贄
+--Royal Tribute
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,22 +7,20 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_HANDES+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(s.condition)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetCondition(function(e,tp) return Duel.IsEnvironment(CARD_NECROVALLEY,tp) end)
+	e1:SetTarget(s.handestg)
+	e1:SetOperation(s.handesop)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsEnvironment(CARD_NECROVALLEY,tp)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+s.listed_names={CARD_NECROVALLEY}
+function s.handestg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_HAND,LOCATION_HAND,1,e:GetHandler()) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,PLAYER_ALL,0)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,PLAYER_ALL,LOCATION_HAND)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.handesop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_HAND,LOCATION_HAND,nil)
 	if #g>0 then
-		Duel.SendtoGrave(g,REASON_EFFECT+REASON_DISCARD)
+		Duel.SendtoGrave(g,REASON_EFFECT|REASON_DISCARD)
 	end
 end

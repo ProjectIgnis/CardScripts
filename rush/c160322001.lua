72096c78b6981cb1783db2422ea3eca74f38de9c
yoshiduels <yoshi.katreuvin@gmail.com>
2025-01-27 23:08:04 +0100

    fixed Harpie Lady Sisters Triangle Beauty

--- rush/c160322001.lua
+++ rush/c160322001.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CHANGE_CODE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_GRAVE+LOCATION_MZONE)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(160208002)
 	c:RegisterEffect(e1)
 	--Cannot be destroyed

2b2e7c9838d49d1637bdf32259210f6c1ffd271a
yoshiduels <yoshi.katreuvin@gmail.com>
2025-01-27 18:07:11 +0100

    fixed Galactic Chaos Enforcer

--- rush/c160020005.lua
+++ rush/c160020005.lua
@@ -33,7 +33,10 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local td=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TODECK)
 	Duel.HintSelection(td)
 	if Duel.SendtoDeck(td,nil,SEQ_DECKBOTTOM,REASON_COST)<=0 then return end
-	Duel.SortDeckbottom(tp,tp,#td)
+	local g2=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_DECK)
+	if #g2>1 then
+		Duel.SortDeckbottom(tp,tp,#g2)
+	end
 	--Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)

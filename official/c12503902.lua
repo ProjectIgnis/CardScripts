1a05b2c418ab6598b399127008c184c864214272
Naim <naimsantos45@gmail.com>
2025-01-27 09:46:25 -0300

    "Rare Metalmorph" fix
    - fixed a bug where it would be able to negate the effect of card that was already targetting the monster before this card's activation such as Equip Spell

--- official/c12503902.lua
+++ official/c12503902.lua
@@ -14,20 +14,13 @@ function s.initial_effect(c)
 	e1:SetValue(500)
 	c:RegisterEffect(e1)
 	--Destroy this card when the monster leaves the field
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetCondition(s.descon2)
-	e3:SetOperation(s.desop2)
-	c:RegisterEffect(e3)
-end
-function s.descon2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetHandler():GetFirstCardTarget()
-	return tc and eg:IsContains(tc)
-end
-function s.desop2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetCondition(s.selfdescond)
+	e2:SetOperation(s.selfdesop)
+	c:RegisterEffect(e2)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
@@ -35,51 +28,34 @@ end
 function s.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_MACHINE)
 end
-function s.tfilter1(c,tc)
-	return c:IsSpell() and c:IsHasCardTarget(tc)
-end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and s.filter(tc) and tc:IsRelateToEffect(e) then
-		local g=Duel.GetMatchingGroup(s.tfilter1,tp,LOCATION_SZONE,LOCATION_SZONE,nil,tc)
-		if #g>0 then
-			local sg,fid=g:GetMaxGroup(Card.GetFieldID)
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD)
-			e1:SetRange(LOCATION_SZONE)
-			e1:SetTargetRange(LOCATION_SZONE,LOCATION_SZONE)
-			e1:SetCode(EFFECT_DISABLE)
-			e1:SetCondition(s.discon)
-			e1:SetTarget(s.distg)
-			e1:SetLabel(fid)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			c:RegisterEffect(e1,true)
-		else
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			e1:SetRange(LOCATION_SZONE)
-			e1:SetCode(EVENT_CHAIN_SOLVING)
-			e1:SetCondition(s.discon2)
-			e1:SetOperation(s.disop2)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			c:RegisterEffect(e1,true)
-		end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetRange(LOCATION_SZONE)
+		e1:SetCode(EVENT_CHAIN_SOLVING)
+		e1:SetCondition(s.discon)
+		e1:SetOperation(s.disop)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e1,true)
 	end
 end
-function s.discon(e)
-	return e:GetHandler():GetCardTargetCount()>0
-end
-function s.distg(e,c)
-	return c:GetFieldID()<=e:GetLabel() and e:GetHandler():GetFirstCardTarget() and c:IsHasCardTarget(e:GetHandler():GetFirstCardTarget()) and c:IsSpell()
-end
-function s.discon2(e,tp,eg,ep,ev,re,r,rp)
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetHandler():GetFirstCardTarget()
 	if not tc or not re:IsActiveType(TYPE_SPELL) or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
 	return g:IsContains(tc)
 end
-function s.disop2(e,tp,eg,ep,ev,re,r,rp)
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateEffect(ev)
 	e:Reset()
+end
+function s.selfdescond(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	return tc and eg:IsContains(tc)
+end
+function s.selfdesop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 end

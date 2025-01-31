4d6e1bd3a1e3a4f7681b1048c14b989255f76416
yoshiduels <yoshi.katreuvin@gmail.com>
2025-01-31 13:15:00 +0100

    added new rush cards

---
+++ rush/c160020040.lua
@@ -0,0 +1,42 @@
+--魂を喰らう者 バズー
+--Bazoo the Soul-Eater
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Increase self ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,1,3,nil)
+	Duel.HintSelection(g)
+	Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+	local g2=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_DECK)
+	if #g2>1 then
+		Duel.SortDeckbottom(tp,tp,#g2)
+	end
+	--Effect
+	--Gain ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESETS_STANDARD_PHASE_END,2)
+	e1:SetValue(#g*300)
+	c:RegisterEffect(e1)
+end
---
+++ rush/c160020050.lua
@@ -0,0 +1,44 @@
+--綺羅流聖のプリアージュ
+--Pliage the Stylish Sacred Shooting Star
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Summon procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,160213069,aux.FilterBoolFunctionEx(Card.IsType,TYPE_NORMAL))
+	-- Decrease ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.named_material={160213069}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,0,LOCATION_MZONE,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		-- Decrease ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-1700)
+		e1:SetReset(RESETS_STANDARD_PHASE_END,2)
+		g:GetFirst():RegisterEffect(e1)
+	end
+end

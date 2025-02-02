9611902e77f115356f733dd13713513d43704f56
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-02-02 13:28:14 +0200

    "Beastborg Fusioner" fix
    Should be SOPT, not HOPT.

--- unofficial/c511001548.lua
+++ unofficial/c511001548.lua
@@ -1,34 +1,34 @@
+--獣闘機融合装置
 --Beastborg Fusioner
---rescripted by Naim (to match the Fusion Summon Procedure)
+--rescripted by Naim
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--Fusion Summon 1 "Beastborg" Fusion Monster from your Extra Deck, using monsters from your hand or your side of the field as Fusion Materials
+	local params={aux.FilterBoolFunction(Card.IsSetCard,0x50b)}
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
 	c:RegisterEffect(e1)
-	--perform a Fusion Summon
-	local params = {aux.FilterBoolFunction(Card.IsSetCard,0x50b)}
+	--You cannot Normal Summon/Set
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_SUMMON)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
-	e2:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	e2:SetTargetRange(1,0)
 	c:RegisterEffect(e2)
-	--cannot normal summon
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(1,0)
-	e3:SetCode(EFFECT_CANNOT_SUMMON)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_CANNOT_MSET)
 	c:RegisterEffect(e3)
-	--cannot set
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_CANNOT_MSET)
-	c:RegisterEffect(e4)
 end
 s.listed_series={0x50b}

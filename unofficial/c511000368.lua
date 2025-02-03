c0a284977f651a1a47e3833cc7f048bdc421ede4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-02-03 12:50:47 +0200

    "Number 92: Heart-eartH Dragon (Anime)" fixes
    - The position change effect should trigger even if the opponent has no Attack Position monsters since it's mandatory.
    - The position change effect targets.
    - The damage changing, inflicting, and LP gaining, is all one effect.
    - The banishing effect should trigger even if there aren't any cards to banish since it's mandatory.
    - Should banish any cards placed on the opponent's field even if the opponent doesn't control them anymore during the next turn's Standby Phase.
    - The banishing effect should only trigger once during the Standby Phase of the next turn after this card is Special Summoned, not during every Standby Phase.
    - Start keeping track only after this card is Special Summoned.
    - Have the card forget what was already tracked if it's flipped face-down.
    - The Special Summoning effect is a "When" effect.
    - Its ATK should become the set value, it shouldn't gain ATK.
    - The ATK change is part of the Special Summoning effect.
    - Allow the Special Summoning effect to be used again if this card stops being in the Monster Zone or GY (e.g. if it's banished, returned to the Extra Deck, etc).
    - Register a hint on the card to indicate that the Special Summoning effect has already been used.
    - Fixed indentation throughout the whole script.

--- unofficial/c511000368.lua
+++ unofficial/c511000368.lua
@@ -12,164 +12,162 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
-	--Change all monsters your opponent controls to Defense Position
+	--Change all Attack Position monsters your opponent controls to Defense Position
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_POSITION)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetTarget(s.postg)
 	e2:SetOperation(s.posop)
 	c:RegisterEffect(e2)
-	--Banish all cards placed on your opponent's field 
+	--Negate any battle damage you would have taken from a battle involving this card, and if you do, inflict damage to your opponent equal to that amount, and if you do that, you gain LP equal to the amount of damage inflicted
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_REMOVE)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CHANGE_DAMAGE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetTarget(s.rmtg)
-	e3:SetOperation(s.rmop)
+	e3:SetTargetRange(1,0)
+	e3:SetValue(s.damval)
 	c:RegisterEffect(e3)
-	--Register cards placed on your opponent's the field
-    	aux.GlobalCheck(s,function()
-        	local ge1=Effect.CreateEffect(c)
-        	ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-        	ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CANNOT_DISABLE)
-        	ge1:SetCode(EVENT_MOVE)
-        	ge1:SetRange(LOCATION_MZONE)
-        	ge1:SetCondition(s.chkcon)
-        	ge1:SetOperation(s.chkop)
-        	Duel.RegisterEffect(ge1,0)
-    	end)
-	--Battle damage from battles involving this card becomes 0
-    	local e4=Effect.CreateEffect(c)
-    	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-    	e4:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-    	e4:SetRange(LOCATION_MZONE)
-    	e4:SetOperation(s.battledamageop)
-    	c:RegisterEffect(e4)
-    	--Inflict damage to your opponent equal to the battle damage you would have taken, then gain LP equal to that amount
-    	local e5=Effect.CreateEffect(c)
-    	e5:SetDescription(aux.Stringid(id,2))
-    	e5:SetCategory(CATEGORY_DAMAGE+CATEGORY_RECOVER)
-    	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    	e5:SetCode(EVENT_BATTLED)
-    	e5:SetRange(LOCATION_MZONE)
-    	e5:SetLabelObject(e4)
-    	e5:SetTarget(s.damrectg)
-    	e5:SetOperation(s.damrecop)
-    	c:RegisterEffect(e5)
-	--Special Summon this card from the GY
+	--Register the tracking effect upon successful Special Summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetOperation(s.regop)
+	c:RegisterEffect(e4)
+	--Banish all cards placed on your opponent's field during the Standby Phase of the next turn after this card was Special Summoned
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_REMOVE)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1)
+	e5:SetCondition(function(e) return e:GetHandler():HasFlagEffect(id) and e:GetHandler():GetTurnID()~=Duel.GetTurnCount() end)
+	e5:SetTarget(s.rmtg)
+	e5:SetOperation(s.rmop)
+	c:RegisterEffect(e5)
+	--Special Summon this card from your GY
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(id,3))
-	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetDescription(aux.Stringid(id,2))
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e6:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e6:SetCode(EVENT_TO_GRAVE)
-	e6:SetCost(s.spcost)
 	e6:SetCondition(s.spcon)
 	e6:SetTarget(s.sptg)
 	e6:SetOperation(s.spop)
 	c:RegisterEffect(e6)
-	--Gains 1000 ATK for each banished card if it's Special Summoned by its own effect
-	local e7=Effect.CreateEffect(c)
-	e7:SetDescription(aux.Stringid(id,4))
-	e7:SetCategory(CATEGORY_ATKCHANGE)
-	e7:SetType(EFFECT_TYPE_SINGLE)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetCode(EFFECT_UPDATE_ATTACK)
-	e7:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+1) end)
-	e7:SetValue(function(e,c) return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_REMOVED,LOCATION_REMOVED)*1000 end)
-	c:RegisterEffect(e7)
 end
 s.xyz_number=92
+function s.posfilter(c,e)
+	return c:IsAttackPos() and c:IsCanBeEffectTarget(e)
+end
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
-    	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAttackPos,tp,0,LOCATION_MZONE,1,nil) end
-    	local g=Duel.GetMatchingGroup(Card.IsAttackPos,tp,0,LOCATION_MZONE,nil)
-    	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,0,0)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.posfilter,tp,0,LOCATION_MZONE,nil,e)
+	if #g>0 then
+		Duel.SetTargetCard(g)
+		Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,tp,0)
+	end
 end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
-    	local g=Duel.GetMatchingGroup(Card.IsAttackPos,tp,0,LOCATION_MZONE,nil)
-    	if Duel.ChangePosition(g,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,0,0,true)>0 then
-		local og=Duel.GetOperatedGroup()
-		for tc in og:Iter() do
+	local tg=Duel.GetTargetCards(e)
+	if tg and #tg>0 then
+		local posg=tg:Filter(Card.IsAttackPos,nil)
+		if #posg>0 then
+			Duel.ChangePosition(posg,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE)
+		end
+		local c=e:GetHandler()
+		for tc in tg:Iter() do
 			--Cannot change their battle positions
-			local e1=Effect.CreateEffect(e:GetHandler())
-            		e1:SetDescription(3313)
-            		e1:SetType(EFFECT_TYPE_SINGLE)
-            		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-            		e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-            		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-            		tc:RegisterEffect(e1)
-        	end
-    	end
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(3313)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+		end
+	end
 end
-function s.chkfilter(c,tp)
-    	return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+function s.damval(e,re,val,r,rp,rc)
+	local c=e:GetHandler()
+	if not (c:IsRelateToBattle() and r&REASON_BATTLE==REASON_BATTLE) then return val end
+	local tp=e:GetHandlerPlayer()
+	--Inflict damage to your opponent equal to the damage you would have taken, and if you do, you gain LP equal to the damage inflicted
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_BATTLED)
+	e1:SetOperation(function()
+						local dam=Duel.Damage(1-tp,val,REASON_EFFECT)
+						if dam>0 then
+							Duel.Recover(tp,dam,REASON_EFFECT)
+						end
+					end)
+	e1:SetReset(RESET_PHASE|PHASE_DAMAGE)
+	Duel.RegisterEffect(e1,tp)
+	return 0
 end
-function s.chkcon(e,tp,eg,ep,ev,re,r,rp)
-    	return e:GetHandler():IsSpecialSummoned() and eg:IsExists(s.chkfilter,1,nil,1-tp)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local reset_ct=Duel.GetCurrentPhase()<=PHASE_STANDBY and 2 or 1
+	c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY,0,reset_ct)
+	--Keep track of cards placed on your opponent's fiel after this card's Special Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_MOVE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetOperation(s.trackop)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	c:RegisterEffect(e1)
 end
-function s.chkop(e,tp,eg,ep,ev,re,r,rp)
-    	local g=eg:Filter(s.chkfilter,nil,1-tp)
-    	for tc in g:Iter() do
-        	tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD&~RESET_TOFIELD,0,1)
-    	end
+function s.trackop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(Card.IsControler,nil,1-tp)
+	local reset_ct=Duel.GetCurrentPhase()<=PHASE_STANDBY and 2 or 1
+	for tc in g:Iter() do
+		tc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY,0,reset_ct)
+	end
 end
 function s.rmfilter(c,turn)
-    	return c:IsAbleToRemove() and c:HasFlagEffect(id,1)
+	return c:IsAbleToRemove() and c:HasFlagEffect(id)
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,0,LOCATION_ONFIELD,1,nil) end
-    	local g=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_ONFIELD,nil)
-    	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,tp,0)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-    	local g=Duel.GetMatchingGroup(s.rmfilter,tp,0,LOCATION_ONFIELD,nil)
-    	if #g>0 then
-        	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
-    	end
-end
-function s.battledamageop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ChangeBattleDamage(ep,0)
-	e:SetLabel(ev)
-end
-function s.damrectg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local dam=e:GetLabelObject():GetLabel()
-	if chk==0 then return dam>0 end
-	Duel.SetTargetPlayer(1-tp)
-	Duel.SetTargetParam(dam)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,dam)
-end
-function s.damrecop(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	if Duel.Damage(p,d,REASON_EFFECT) then
-		Duel.Recover(tp,d,REASON_EFFECT)
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	if #g>0 then
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 	end
-	e:GetLabelObject():SetLabel(0)
-end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    	local c=e:GetHandler()
-    	if chk==0 then return c:GetFlagEffect(id)==0 end
-	c:RegisterFlagEffect(id,0,0,1)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    	local c=e:GetHandler()
-    	return c:IsReason(REASON_DESTROY) and e:GetHandler():GetOverlayCount()==0
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:GetOverlayCount()==0
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    	local c=e:GetHandler()
-    	if chk==0 then return c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        	and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return not c:HasFlagEffect(id+1)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	c:RegisterFlagEffect(id+1,(RESET_EVENT|RESETS_STANDARD|RESET_MSCHANGE)&~(RESET_TOFIELD|RESET_TOGRAVE|RESET_LEAVE|RESET_TURN_SET),EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,3))
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    	local c=e:GetHandler()
-    	if c:IsRelateToEffect(e) then
-        	Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)
-    	end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local atk=Duel.GetFieldGroupCount(0,LOCATION_REMOVED,LOCATION_REMOVED)*1000
+		--This card's ATK becomes the number of banished cards x 1000
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
 end

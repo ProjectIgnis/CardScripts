425db6fbca7e8df3ccdac47aa40d61b5cef81de4
yoshiduels <yoshi.katreuvin@gmail.com>
2025-02-05 12:24:33 +0100

    added Giftarist - Speedy Mix

---
+++ rush/c160020017.lua
@@ -0,0 +1,46 @@
+--ギフトタリスト ～ＳＰ ＭＩＸ～
+--Giftarist - Speedy Mix
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Name becomes "Giftarist" in the Graveyard
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_CHANGE_CODE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_GRAVE)
+	e0:SetValue(160306003)
+	c:RegisterEffect(e0)
+	--Send the top 2 cards of your Deck to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DECKDES+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={160015059}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,2)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	if Duel.DiscardDeck(tp,2,REASON_EFFECT)~=2 then return end
+	local g=Duel.GetMatchingGroup(aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,LOCATION_MZONE,0,nil)
+	local ct=Duel.GetMatchingGroupCount(Card.IsCode,tp,LOCATION_GRAVE,0,nil,160015059)
+	if ct>0 and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		for tc in g:Iter() do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(ct*500)
+			e1:SetReset(RESETS_STANDARD_PHASE_END)
+			tc:RegisterEffect(e1)
+		end
+	end
+end

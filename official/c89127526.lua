f7cb8b8fbec85bccb16a12d5677ffa87455a2c2e
Naim <naimsantos45@gmail.com>
2025-02-05 15:41:53 -0300

    "Barrier Resonator" fix
    Fixed a bug where the player was able to activate its effect even when they could not conduct a Battle Phase

--- official/c89127526.lua
+++ official/c89127526.lua
@@ -1,24 +1,20 @@
 --バリア・リゾネーター
 --Barrier Resonator
-
 local s,id=GetID()
 function s.initial_effect(c)
-	--Targeted tuner monster cannot be destroyed by battle, also take no battle damage involving it
+	--1 Tuner monster you control cannot be destroyed by battle, also take no battle damage involving it
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCost(s.cost)
+	e1:SetHintTiming(0,TIMING_BATTLE_START)
+	e1:SetCondition(function() return Duel.IsAbleToEnterBP() end)
+	e1:SetCost(aux.SelfToGraveCost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
-	e1:SetHintTiming(0,TIMING_BATTLE_START)
 	c:RegisterEffect(e1)
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function s.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_TUNER)
@@ -26,8 +22,8 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,tp,1,nil)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -40,16 +36,12 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
 		e1:SetValue(1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		tc:RegisterEffect(e1)
 		--Cannot be destroyed by battle
-		local e2=Effect.CreateEffect(c)
+		local e2=e1:Clone()
 		e2:SetDescription(3000)
-		e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-		e2:SetValue(1)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e2)
 	end
 end

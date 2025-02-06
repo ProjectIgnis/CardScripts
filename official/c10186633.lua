3d98ae048d087a78a4f78619f13d130650a06b20
Naim <naimsantos45@gmail.com>
2025-02-06 20:21:58 -0300

    "EN Shuffle" fix
    - fixed an incorrect hint message when the player had to select monsters to return to the Deck

--- official/c10186633.lua
+++ official/c10186633.lua
@@ -3,7 +3,7 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon 1 "Elemental HERO" or "Neo-Spacian" monster
+	--Special Summon 1 "Elemental HERO" or "Neo-Spacian" monster from your Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
@@ -20,20 +20,20 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCost(aux.bfgcost)
+	e2:SetCost(aux.SelfBanishCost)
 	e2:SetTarget(s.drtg)
 	e2:SetOperation(s.drop)
 	c:RegisterEffect(e2)
 end
 s.listed_names={CARD_NEOS}
-s.listed_series={0x3008,0x1f}
+s.listed_series={SET_ELEMENTAL_HERO,SET_NEO_SPACIAN}
 function s.tdfilter(c,e,tp)
-	return (c:IsSetCard(0x3008) or c:IsSetCard(0x1f)) and c:IsFaceup()
+	return c:IsSetCard({SET_ELEMENTAL_HERO,SET_NEO_SPACIAN}) and c:IsFaceup()
 		and c:IsAbleToDeck() and Duel.GetMZoneCount(tp,c)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
 end
 function s.spfilter(c,e,tp,code)
-	return (c:IsSetCard(0x3008) or c:IsSetCard(0x1f)) and not c:IsCode(code)
+	return c:IsSetCard({SET_ELEMENTAL_HERO,SET_NEO_SPACIAN}) and not c:IsCode(code)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -47,17 +47,17 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not tc then return end
 	Duel.HintSelection(tc,true)
 	local code=tc:GetCode()
-	if Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
+	if Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_DECK|LOCATION_EXTRA) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,code)
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 function s.tdfilter2(c)
-	return (c:IsSetCard(0x3008) or c:IsSetCard(0x1f)) and c:IsMonster() and c:IsAbleToDeck()
+	return (c:IsSetCard(SET_ELEMENTAL_HERO) or c:IsSetCard(SET_NEO_SPACIAN)) and c:IsMonster() and c:IsAbleToDeck()
 end
 function s.resfilter(c,sg)
-	return c:IsSetCard(0x3008) and sg:IsExists(Card.IsSetCard,1,c,0x1f)
+	return c:IsSetCard(SET_ELEMENTAL_HERO) and sg:IsExists(Card.IsSetCard,1,c,SET_NEO_SPACIAN)
 end
 function s.rescon(sg,e,tp)
 	return (#sg==1 and sg:IsExists(Card.IsCode,1,nil,CARD_NEOS)) or sg:IsExists(s.resfilter,1,nil,sg)
@@ -70,7 +70,7 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.tdfilter2,tp,LOCATION_GRAVE,0,nil)
-	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,1,tp,HINTMSG_DISCARD,s.rescon)
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,1,tp,HINTMSG_TODECK,s.rescon)
 	if #sg==0 then return end
 	Duel.HintSelection(sg,true)
 	if Duel.SendtoDeck(sg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)==#sg then

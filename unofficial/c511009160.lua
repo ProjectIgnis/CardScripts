2d5fdfb5d3e37e245d2107807cbe6e1addac6b28
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-02-10 19:54:39 +0200

    "Sacred Sword Thanatos" fix
    Fixed an issue where Tokens wouldn't be attempted to be destroyed if they had a protection effect that is only applied the first time they would be destroyed.

--- unofficial/c511009160.lua
+++ unofficial/c511009160.lua
@@ -1,34 +1,45 @@
+--霊剣タナトス
 --Sacred Sword Thanatos
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c)
-	--atkup
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_EQUIP)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
-	e3:SetValue(300)
-	c:RegisterEffect(e3)
-	--Token destroy and atk up
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e4:SetCode(EVENT_ADJUST)
-	e4:SetRange(LOCATION_SZONE)	
-	e4:SetOperation(s.desop)
-	c:RegisterEffect(e4)
+	--The equipped monster gains 300 ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(300)
+	c:RegisterEffect(e1)
+	--Destroy all Tokens on the field, and if you do, the equipped monster gains ATK equal to their ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetRange(LOCATION_SZONE)	
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
 end
-function s.filter(c,e)
-	return c:IsType(TYPE_TOKEN) and c:IsDestructable(e)
+function s.desfilter(c,e,tp)
+	if not c:IsType(TYPE_TOKEN) then return false end
+	if c:IsDestructable(e) then return true end
+	local indes_eff=c:IsHasEffect(EFFECT_INDESTRUCTABLE_EFFECT)
+	return c:IsHasEffect(EFFECT_INDESTRUCTABLE_COUNT) and not (indes_eff
+		and (type(indes_eff:GetValue())~='function' or indes_eff:GetValue()(indes_eff,e,tp,c)))
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e)
-	local atk=g:GetSum(Card.GetAttack)
-	Duel.Destroy(g,REASON_EFFECT)
-	if e:GetHandler():GetEquipTarget() and atk>0 then
-		local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	local ec=c:GetEquipTarget()
+	if not ec then return end
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e,tp)
+	if #g>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
+		local atk=Duel.GetOperatedGroup():GetSum(Card.GetPreviousAttackOnField)
+		if atk==0 then return end
+		--The equipped monster gains ATK equal to the total ATK of the destroyed Tokens
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetValue(atk)
-		e:GetHandler():GetEquipTarget():RegisterEffect(e1)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		ec:RegisterEffect(e1)
 	end
 end

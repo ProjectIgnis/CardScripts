b2fff5248c1a2de66d1373adf1657c6156be781c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-02-11 14:32:08 +0200

    Add files via upload

--- unofficial/c511002903.lua
+++ unofficial/c511002903.lua
@@ -1,59 +1,63 @@
+--ネジマキのカタパルト
 --Gearspring Catapult
 local s,id=GetID()
+local COUNTER_GEARSPRING=0x107
 function s.initial_effect(c)
-	c:EnableCounterPermit(0x107)
+	c:EnableCounterPermit(COUNTER_GEARSPRING)
 	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--During each of your Standby Phases, place 1 Gearspring Counter on this card
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) and e:GetHandler():IsCanAddCounter(COUNTER_GEARSPRING,1) end)
+	e1:SetOperation(s.standbycounterop)
 	c:RegisterEffect(e1)
-	--add counter
+	--Increase the ATK of 1 monster you control by 500 until the end of the turn
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetCondition(s.ctcon)
-	e2:SetOperation(s.ctop)
+	e2:SetCost(s.atkcost)
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
-	--gain atk
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCost(s.atkcost)
-	e3:SetTarget(s.atktg)
-	e3:SetOperation(s.atkop)
-	c:RegisterEffect(e3)
 end
-function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
-end
-function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+s.counter_place_list={COUNTER_GEARSPRING}
+function s.standbycounterop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	c:AddCounter(0x107,1)
-	Duel.RaiseEvent(c,511002905,e,REASON_EFFECT,tp,tp,0x107)
+	if c:AddCounter(COUNTER_GEARSPRING,1) then
+		Duel.RaiseEvent(c,EVENT_CUSTOM+511002905,e,REASON_EFFECT,tp,tp,1)
+	end
 end
 function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,0x107,1,REASON_COST) end
-	e:GetHandler():RemoveCounter(tp,0x107,1,REASON_COST)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanRemoveCounter(tp,COUNTER_GEARSPRING,1,REASON_COST) end
+	c:RemoveCounter(tp,COUNTER_GEARSPRING,1,REASON_COST)
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and chkc:IsControler(tp) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		--It gains 500 ATK until the end of this turn
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		e1:SetValue(500)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
--- unofficial/c511002904.lua
+++ unofficial/c511002904.lua
@@ -1,77 +1,78 @@
+--自動ネジマキ機
 --Automatic Gearspring Machine
 local s,id=GetID()
+local COUNTER_GEARSPRING=0x107
 function s.initial_effect(c)
-	c:EnableCounterPermit(0x107)
-	--Activate
+	c:EnableCounterPermit(COUNTER_GEARSPRING)
+	--When you activate this card: Place 2 Gearspring Counters on it
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_COUNTER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--add counter
+	--During each of your Standby Phases, place 1 Gearspring Counter on this card
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetCondition(s.ctcon)
-	e2:SetOperation(s.ctop)
+	e2:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) and e:GetHandler():IsCanAddCounter(COUNTER_GEARSPRING,1) end)
+	e2:SetOperation(s.standbycounterop)
 	c:RegisterEffect(e2)
-	--place
-	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_COUNTER)
-	e4:SetDescription(aux.Stringid(47408488,1))
-	e4:SetType(EFFECT_TYPE_IGNITION)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetCost(s.plcost)
-	e4:SetTarget(s.pltg)
-	e4:SetOperation(s.plop)
-	c:RegisterEffect(e4)
+	--Place Gearspring Counters on another card you can place a Gearspring Counter on, equal to the number of Gearspring Counters that were on this card
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_COUNTER)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCost(s.maincountercost)
+	e3:SetTarget(s.maincountertg)
+	e3:SetOperation(s.maincounterop)
+	c:RegisterEffect(e3)
 end
+s.counter_place_list={COUNTER_GEARSPRING}
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,2,0,0x107)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanAddCounter(COUNTER_GEARSPRING,2,false,LOCATION_SZONE) end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,c,2,tp,COUNTER_GEARSPRING)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		c:AddCounter(0x107,2)
-		Duel.RaiseEvent(c,id+1,e,REASON_EFFECT,tp,tp,0x107)
+	if c:IsRelateToEffect(e) and c:AddCounter(COUNTER_GEARSPRING,2) then
+		Duel.RaiseEvent(c,EVENT_CUSTOM+511002905,e,REASON_EFFECT,tp,tp,2)
 	end
 end
-function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
-end
-function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+function s.standbycounterop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	c:AddCounter(0x107,1)
-	Duel.RaiseSingleEvent(c,id+1,e,0,0,tp,0)
+	if c:AddCounter(COUNTER_GEARSPRING,1) then
+		Duel.RaiseEvent(c,EVENT_CUSTOM+511002905,e,REASON_EFFECT,tp,tp,1)
+	end
 end
-function s.plcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	e:SetLabel(e:GetHandler():GetCounter(0x107))
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+function s.maincountercost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToGraveAsCost() end
+	e:SetLabel(c:GetCounter(COUNTER_GEARSPRING))
+	Duel.SendtoGrave(c,REASON_COST)
 end
-function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.maincountertg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local c=e:GetHandler()
-		local ct=c:GetCounter(0x107)
-		return ct>0 and Duel.IsExistingMatchingCard(Card.IsCanAddCounter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c,0x107,ct)
+		local ct=c:GetCounter(COUNTER_GEARSPRING)
+		return ct>0 and Duel.IsExistingMatchingCard(Card.IsCanAddCounter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c,COUNTER_GEARSPRING,ct)
 	end
-	Duel.SetTargetParam(e:GetLabel())
-	e:SetLabel(0)
-	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,ct,0,0x107)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,e:GetLabel(),tp,COUNTER_GEARSPRING)
 end
-function s.plop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(73853830,1))
-	local g=Duel.SelectMatchingCard(tp,Card.IsCanAddCounter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,0x107,ct)
-	local tc=g:GetFirst()
-	if tc then
-		Duel.HintSelection(g)
-		tc:AddCounter(0x107,ct)
-		Duel.RaiseEvent(tc,id+1,e,REASON_EFFECT,tp,tp,0x107)
+function s.maincounterop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_COUNTER)
+	local sc=Duel.SelectMatchingCard(tp,Card.IsCanAddCounter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,COUNTER_GEARSPRING,ct):GetFirst()
+	if sc then
+		Duel.HintSelection(sc)
+		if sc:AddCounter(COUNTER_GEARSPRING,ct) then
+			Duel.RaiseEvent(sc,EVENT_CUSTOM+511002905,e,REASON_EFFECT,tp,tp,ct)
+		end
 	end
 end

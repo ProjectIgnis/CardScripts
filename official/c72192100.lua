21ad08009a42f5b4c75907420c60c6bcd831ff82
Naim <naimsantos45@gmail.com>
2025-02-12 10:04:47 -0300

    "Desrook Archfiend" fix
    - fixed a bug where the effect to Special Summoned could be activated even when "Terrorking Archfiend" was not destroyed
    - fixed a bug where the effect to negate an effect that targets "Desrook Archfiend" would apply even the effect targeted it outside the field (e.g. in the GY) and "Desrook Archfiend" was moved to the field during the resolution

--- official/c72192100.lua
+++ official/c72192100.lua
@@ -2,41 +2,38 @@
 --Desrook Archfiend
 local s,id=GetID()
 function s.initial_effect(c)
-	--maintain
+	--Once per turn, during your Standby Phase, you must pay 500 LP (this is not optional), or this card is destroyed
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(s.mtcon)
+	e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
 	e1:SetOperation(s.mtop)
 	c:RegisterEffect(e1)
-	--disable and destroy
+	--Roll a six-sided die when resolving an opponent's card effect that targets this card
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_CHAIN_SOLVING)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetOperation(s.disop)
 	c:RegisterEffect(e2)
-	--special summon
+	--Special Summon 1 "Terrorking Archfiend" from your GY
 	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetRange(LOCATION_HAND)
-	e3:SetCost(s.spcost)
+	e3:SetCost(aux.SelfToGraveCost)
 	e3:SetTarget(s.sptg)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
 end
-s.listed_names={35975813}
+s.listed_names={35975813} --"Terrorking Archfiend"
 s.roll_dice=true
-function s.mtcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
-end
 function s.mtop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.CheckLPCost(tp,500) then
 		Duel.PayLPCost(tp,500)
@@ -45,38 +42,36 @@ function s.mtop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
-	if ep==tp then return end
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local c=e:GetHandler()
+	if not (c:IsRelateToEffect(re) and ep==1-tp) then return end
 	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	if not tg or not tg:IsContains(e:GetHandler()) or not Duel.IsChainDisablable(ev) then return false end
+	if not tg or not tg:IsContains(c) or not Duel.IsChainDisablable(ev) then return false end
 	local rc=re:GetHandler()
+	Duel.Hint(HINT_CARD,1-tp,id)
 	local dc=Duel.TossDice(tp,1)
 	if dc~=3 then return end
 	if Duel.NegateEffect(ev) and rc:IsRelateToEffect(re) then
 		Duel.Destroy(rc,REASON_EFFECT)
 	end
 end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
-end
-function s.filter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsCode(35975813) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_ONFIELD)
-		and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and c:IsReason(REASON_DESTROY) and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return eg:IsContains(chkc) and s.filter(chkc,e,tp) end
+	if chkc then return eg:IsContains(chkc) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and eg:IsExists(s.filter,1,nil,e,tp) end
+		and eg:IsExists(s.spfilter,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=eg:FilterSelect(tp,s.filter,1,1,nil,e,tp)
+	local g=eg:FilterSelect(tp,s.spfilter,1,1,nil,e,tp)
 	Duel.SetTargetCard(g)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

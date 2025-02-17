f1a928d8a43f641b716668689674570ed81ec88f
yoshiduels <yoshi.katreuvin@gmail.com>
2025-02-17 11:45:34 +0100

    added Dark Paladin

---
+++ rush/c160214002.lua
@@ -0,0 +1,52 @@
+--超魔導剣士－ブラック・パラディン
+--Dark Paladin
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,CARD_DARK_MAGICIAN,78193831)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
+	--Cannot be destroyed
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.condition)
+	e2:SetValue(s.indval)
+	c:RegisterEffect(e2)
+	--Opponent cannot activate Legend Spells
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetTargetRange(0,1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.condition)
+	e3:SetValue(s.aclimit)
+	c:RegisterEffect(e3)
+end
+function s.filter(c)
+	return c:IsRace(RACE_DRAGON) and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+end
+function s.val(e,c)
+	return Duel.GetMatchingGroupCount(s.filter,0,LOCATION_MZONE|LOCATION_GRAVE,LOCATION_MZONE|LOCATION_GRAVE,nil)*500
+end
+function s.condition(e)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_HAND,0)>=1
+end
+function s.indval(e,re,rp)
+	return re:IsSpellEffect() and aux.indoval(e,re,rp)
+end
+function s.aclimit(e,re,tp)
+	local tc=re:GetHandler()
+	return tc:IsSpell() and tc:IsLegend() and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end

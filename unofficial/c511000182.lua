2898826e4f8e9c85be63d947077aca5b6842132c
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-02-18 20:40:21 -0800

    Update "Arduous Decision"
    - Should draw instead of excavate/updated validity checks to reflect
    - Opponent should get to choose which card
    - Added operation infos in target
    - Updated categories to reflect both mandatory and possible actions

--- unofficial/c511000182.lua
+++ unofficial/c511000182.lua
@@ -5,30 +5,43 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanSpecialSummon(tp) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>1 end
+	if chk==0 then return Duel.IsPlayerCanSpecialSummon(tp) and Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<2 then return end
-	Duel.ConfirmDecktop(tp,2)
-	local g=Duel.GetDecktopGroup(tp,2)
-	if #g>0 then
-		Duel.DisableShuffleCheck()
-		local tg=g:RandomSelect(1-tp,1)
-		local tc=tg:GetFirst()
-		if tc:IsMonster() and tc:IsCanBeSpecialSummoned(e,0,tp,false,false) then
-			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-			local g2=g:Filter(aux.TRUE,tg)
-			Duel.SendtoHand(g2,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g2)
-			Duel.ShuffleHand(tp)
-		else Duel.SendtoGrave(g,REASON_EFFECT|REASON_EXCAVATE) end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.Draw(tp,2,REASON_EFFECT)>0 then
+		local g=Duel.GetOperatedGroup()
+		local rcard=g:GetFirst()
+		local lcard=g:GetNext()
+		local op=Duel.SelectOption(1-tp,aux.Stringid(id,1),aux.Stringid(id,2))
+		if op==0 then
+			Duel.ConfirmCards(1-tp,lcard)
+			if lcard:IsMonster() and lcard:IsCanBeSpecialSummoned(e,0,tp,true,false) then
+				Duel.BreakEffect()
+				Duel.SpecialSummon(lcard,0,tp,tp,true,false,POS_FACEUP)
+				Duel.SendtoGrave(rcard,REASON_EFFECT)
+			else
+				Duel.SendtoGrave(g,REASON_EFFECT)
+			end
+		elseif op==1 then
+			Duel.ConfirmCards(1-tp,rcard)
+			if rcard:IsMonster() and rcard:IsCanBeSpecialSummoned(e,0,tp,true,false) then
+				Duel.BreakEffect()
+				Duel.SpecialSummon(rcard,0,tp,tp,true,false,POS_FACEUP)
+				Duel.SendtoGrave(lcard,REASON_EFFECT)
+			else
+				Duel.SendtoGrave(g,REASON_EFFECT)
+			end
+		end
 	end
 end

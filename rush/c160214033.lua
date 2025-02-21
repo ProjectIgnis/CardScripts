1e13bb803d74e69b1b9352ef142e08438128bf6c
yoshiduels <yoshi.katreuvin@gmail.com>
2025-02-21 12:59:58 +0100

    added Excutie Prouty Frontiere

---
+++ rush/c160214033.lua
@@ -0,0 +1,42 @@
+--エクスキューティー・プラウティ・Ｆ
+--Excutie Prouty Frontiere
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,160205037,160205032)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,nil,nil,SUMMON_TYPE_FUSION,nil,false)
+	--Lose ATK/DEF
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(aux.NOT(aux.TargetBoolFunction(Card.IsLevel,6)))
+	e1:SetValue(-600)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
+	--gain LP
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
+end
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsAbleToDeckOrExtraAsCost),tp,LOCATION_ONFIELD,0,nil)
+end
+function s.contactop(g,tp)
+	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST+REASON_MATERIAL)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc and bc:IsLocation(LOCATION_GRAVE) then
+		Duel.Recover(tp,600,REASON_EFFECT)
+	end
+end

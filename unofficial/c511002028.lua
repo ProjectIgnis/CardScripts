506456fb7aefc2b552cbac45486eb2a017c03562
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-02-22 18:48:09 -0800

    Fix "Final Question"
    -Destruction effects should not apply to monsters immune to Trap effects (i.e. Litmus Doom Swordsman)
    -Changed the announced number range to account for modes with EMZ enabled

--- unofficial/c511002028.lua
+++ unofficial/c511002028.lua
@@ -8,17 +8,14 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function(e) return Duel.GetTurnPlayer()==1-e:GetHandlerPlayer() and Duel.IsAbleToEnterBP() end)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp and Duel.IsAbleToEnterBP()
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_MESSAGE,1-tp,aux.Stringid(id,0))
 	Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(id,0))
-	local quest=Duel.AnnounceNumber(1-tp,0,1,2,3,4,5,6,7,8,9,10)
+	local quest=Duel.AnnounceNumber(1-tp,0,1,2,3,4,5,6,7,8,9,10,11,12)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PHASE_START+PHASE_BATTLE)
@@ -38,6 +35,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e2,tp)
 	e1:SetLabelObject(e2)
 end
+function s.cfilter(c,e)
+	return not c:IsImmuneToEffect(e)
+end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	e:GetLabelObject():SetLabel(1)
 	Duel.Hint(HINT_CARD,0,id)
@@ -45,17 +45,17 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	local c=e:GetHandler()
 	if ct==quest then
-		local sg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,nil)
+		local sg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil,e)
 		Duel.Destroy(sg,REASON_EFFECT)
 	else
-		local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+		local sg=Duel.GetMatchingGroup(s.cfilter,tp,0,LOCATION_MZONE,nil,e)
 		Duel.Destroy(sg,REASON_EFFECT)
 	end
 end
 function s.endop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==0 then
 		Duel.Hint(HINT_CARD,0,id)
-		local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
+		local sg=Duel.GetMatchingGroup(s.cfilter,tp,0,LOCATION_MZONE,nil,e)
 		Duel.Destroy(sg,REASON_EFFECT)
 	end
 end

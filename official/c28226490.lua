ac4864941595d594daafe1fbb69bc7bebe191801
Naim <naimsantos45@gmail.com>
2025-02-24 16:38:46 -0300

    "Tearlaments Kaleido-Heart" fix
    Fixed a bug where its effect could not be actvated if a monster was sent to the GY in the Damage Step

--- official/c28226490.lua
+++ official/c28226490.lua
@@ -1,5 +1,5 @@
 --ティアラメンツ・カレイドハート
---Tearalaments Kaleidoheart
+--Tearlaments Kaleido-Heart
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	e0:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
 	e0:SetValue(1)
 	c:RegisterEffect(e0)
-	--Shuffle into the Deck
+	--Shuffle 1 card your opponent controls into the Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TODECK)
@@ -25,12 +25,12 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCondition(s.tdcon)
 	c:RegisterEffect(e2)
-	--Special Summon itself
+	--Special Summon this card from the GY, and if you do, send 1 "Tearlaments" card to the GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
@@ -43,8 +43,8 @@ function s.initial_effect(c)
 	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
 end
-s.listed_names={73956664}
-s.listed_series={0x182}
+s.listed_names={73956664} --Tearlaments Reinoheart
+s.listed_series={SET_TEARLAMENTS}
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) and chkc:IsAbleToDeck() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,nil) end
@@ -65,14 +65,14 @@ function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.tdcfilter,1,nil,tp)
 end
 function s.gyfilter(c)
-	return c:IsSetCard(0x182) and c:IsAbleToGrave()
+	return c:IsSetCard(SET_TEARLAMENTS) and c:IsAbleToGrave()
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)

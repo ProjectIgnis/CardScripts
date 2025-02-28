7d518efbfc22c859656f35ca8e756e4faa3f5672
Naim <naimsantos45@gmail.com>
2025-02-28 17:08:34 -0300

    Update c160002043.lua

--- rush/c160002043.lua
+++ rush/c160002043.lua
@@ -24,21 +24,22 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tg=Duel.SelectMatchingCard(tp,s.rvfilter,tp,LOCATION_HAND,0,1,1,nil,tp):GetFirst()
-	if #tg==0 then return end
-	Duel.ConfirmCards(1-tp,tg)
-	Duel.ShuffleHand(tp)
-	if Duel.GetMatchingGroupCount(aux.FilterMaximumSideFunctionEx(s.attfilter,tg:GetAttribute()),tp,0,LOCATION_MZONE,nil,tg:GetAttribute())>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
-		local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.attfilter,tg:GetAttribute()),tp,0,LOCATION_MZONE,1,3,nil,tg:GetAttribute())
-		if #g==0 then return end
-		Duel.HintSelection(g)
-		for tc in g:Iter() do
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-			e1:SetValue(tg:GetAttribute())
-			e1:SetReset(RESETS_STANDARD_PHASE_END)
-			tc:RegisterEffect(e1)
+	if tg then
+		Duel.ConfirmCards(1-tp,tg)
+		Duel.ShuffleHand(tp)
+		if Duel.GetMatchingGroupCount(aux.FilterMaximumSideFunctionEx(s.attfilter,tg:GetAttribute()),tp,0,LOCATION_MZONE,nil,tg:GetAttribute())>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+			local g=Duel.SelectMatchingCard(tp,aux.FilterMaximumSideFunctionEx(s.attfilter,tg:GetAttribute()),tp,0,LOCATION_MZONE,1,3,nil,tg:GetAttribute())
+			if #g==0 then return end
+			Duel.HintSelection(g)
+			for tc in g:Iter() do
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+				e1:SetValue(tg:GetAttribute())
+				e1:SetReset(RESETS_STANDARD_PHASE_END)
+				tc:RegisterEffect(e1)
+			end
 		end
 	end
 end

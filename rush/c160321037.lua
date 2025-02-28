155d86750a53538f7e68bf84da081475a1168b65
yoshiduels <yoshi.katreuvin@gmail.com>
2025-02-28 13:13:29 +0100

    added Cybernetic Overflow

---
+++ rush/c160321037.lua
@@ -0,0 +1,53 @@
+--サイバネティック・オーバーフロー
+--Cybernetic Overflow
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_CYBER_DRAGON}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsSummonPlayer,1,nil,1-tp)
+end
+function s.cfilter(c)
+	return c:IsCode(CARD_CYBER_DRAGON) and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local cg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(cg,e,tp,2,2,s.rescon,0) end
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetLevel)==#sg
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(aux.FilterMaximumSideFunctionEx(aux.TRUE),tp,0,LOCATION_ONFIELD,nil)
+	if chk==0 then return #g>1 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,LOCATION_ONFIELD)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	local cg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE,0,nil)
+	local tdg=aux.SelectUnselectGroup(cg,e,tp,2,2,s.rescon,1,tp,HINTMSG_TODECK)
+	Duel.HintSelection(tdg)
+	if Duel.SendtoDeck(tdg,nil,SEQ_DECKBOTTOM,REASON_COST)==0 then return end
+	local og=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_DECK)
+	if #og>0 then Duel.SortDeckbottom(tp,tp,#og) end
+	--Effect
+	local g=Duel.GetMatchingGroup(aux.FilterMaximumSideFunctionEx(aux.TRUE),tp,0,LOCATION_ONFIELD,nil)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local sg=g:Select(tp,2,2,nil)
+		Duel.HintSelection(sg)
+		Duel.Destroy(sg,REASON_EFFECT)
+	end
+end

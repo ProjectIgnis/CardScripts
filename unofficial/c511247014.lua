0da10d60478b7b3a15e97ce26d07fce9a9c47533
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-03-02 16:37:12 -0800

    Update "Blackwing - Hillen the Tengu-wind (Anime)"
    - Effects should be negated on Special Summon
    - Mild script modernization update

--- unofficial/c511247014.lua
+++ unofficial/c511247014.lua
@@ -3,7 +3,7 @@
 --fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Special Summon this card and 1 Level 3 or lower "Blackwing" monster from your GY when you take 2000 or more battle damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -11,39 +11,40 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_BATTLE_DAMAGE)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
-	e1:SetCondition(s.spcon1)
+	e1:SetCondition(function(_,tp,_,ep,ev) return ep==tp and ev>=2000 end)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+	--Special Summon this card and 1 Level 3 or lower "Blackwing" monster from your GY when your opponent declares a direct attack
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2:SetCondition(s.spcon2)
+	e2:SetCondition(function(e,tp) return Duel.GetAttacker():IsControler(1-tp) and Duel.GetAttackTarget()==nil end)
 	c:RegisterEffect(e2)
 end
-function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and ev>=2000
-end
-function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetAttacker():IsControler(1-tp) and Duel.GetAttackTarget()==nil
-end
-function s.filter(c,e,tp)
-	return c:IsLevelBelow(3) and c:IsSetCard(0x33) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+s.listed_series={SET_BLACKWING}
+function s.spfilter(c,e,tp)
+	return c:IsLevelBelow(3) and c:IsSetCard(SET_BLACKWING) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1 
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,c,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,2,tp,LOCATION_GRAVE)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,c,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_GRAVE)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 
-		or not c:IsRelateToEffect(e) or not c:IsCanBeSpecialSummoned(e,0,tp,false,false) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 
+		or not c:IsRelateToEffect(e) or not Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,c,e,tp)
+		or not c:IsCanBeSpecialSummoned(e,0,tp,false,false) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,c,e,tp)
-	if #g>0 then
-		Duel.HintSelection(g)
-		g:AddCard(c)
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,c,e,tp)
+	g:AddCard(c)
+	Duel.HintSelection(g,true)
+	if #g==2 then
+		for tc in g:Iter() do
+			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+			tc:NegateEffects(c)
+		end
+		Duel.SpecialSummonComplete()
 	end
 end

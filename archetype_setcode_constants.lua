723f06d681317ba6c08f279025aae7af11cb5240
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-03-10 10:48:08 +0200

    Added new archetype constant
    SET_K9 = 0x1c4

--- archetype_setcode_constants.lua
+++ archetype_setcode_constants.lua
@@ -590,3 +590,4 @@ SET_DRAGONTAIL                    = 0x1c0
 SET_YUMMY                         = 0x1c1
 SET_APOPHIS                       = 0x1c2
 SET_SERKET                        = 0x1c3
+SET_K9                            = 0x1c4

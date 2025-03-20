1d9e46d68d2faaa5e3f2ce451ea286fb3b568d45
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-03-19 18:31:07 -0700

    Fix "Barrier Resonator"
    Parameter error caused effect to not apply to targeted monster

--- official/c89127526.lua
+++ official/c89127526.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetHintTiming(0,TIMING_BATTLE_START)
 	e1:SetCondition(function() return Duel.IsAbleToEnterBP() or (Duel.IsBattlePhase() and not Duel.IsPhase(PHASE_BATTLE)) end)
-	e1:SetCost(aux.SelfToGraveCost)
+	e1:SetCost(Cost.SelfToGrave)
 	e1:SetTarget(s.efftg)
 	e1:SetOperation(s.effop)
 	c:RegisterEffect(e1)
@@ -23,11 +23,11 @@ function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.tgfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
-	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,tp,1,nil)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.effop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if tc and tc:IsRelateToEffect(e) then
 		tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
 		--It cannot be destroyed by battle this turn
 		local e1=Effect.CreateEffect(e:GetHandler())

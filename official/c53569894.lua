fd7ac4c09a38938d627f33405242ba02d75fde20
Naim <naimsantos45@gmail.com>
2025-03-24 06:32:50 -0300

    "Pyramid of Light" fix
    Fixed a bug where it would still destroy the cards even when it left the field while it was face-down

--- official/c53569894.lua
+++ official/c53569894.lua
@@ -7,33 +7,38 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--destroy
+	--Register when this card leaves the field
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e2:SetCode(EVENT_LEAVE_FIELD_P)
 	e2:SetOperation(s.checkop)
 	c:RegisterEffect(e2)
+	--Destroy and banish "Andro Sphinx" and "Sphinx Teleia you control
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetOperation(s.desop)
 	e3:SetLabelObject(e2)
-	e3:SetOperation(s.leave)
 	c:RegisterEffect(e3)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsCode(15013468,51402177)
-end
+s.listed_names={15013468,51402177} --"Andro Sphinx","Sphinx Teleia
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsDisabled() then
-		e:SetLabel(1)
-	else e:SetLabel(0) end
-end
-function s.leave(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if e:GetLabelObject():GetLabel()==0 and c:GetPreviousControler()==tp and c:GetPreviousPosition(POS_FACEUP) then
-		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,0,nil)
-		Duel.Destroy(g,REASON_EFFECT,LOCATION_REMOVED)
+	if c:IsFaceup() and not c:IsDisabled() then
+		e:SetLabel(1)
+	else
+		e:SetLabel(0)
+	end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if e:GetLabelObject():GetLabel()==1 and c:IsPreviousControler(tp) then
+		local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCode,15013468,51402177),tp,LOCATION_ONFIELD,0,nil)
+		if #g>0 then
+			Duel.Hint(HINT_CARD,1-tp,id)
+			Duel.Destroy(g,REASON_EFFECT,LOCATION_REMOVED)
+		end
 	end
 end

0ae57300ca7e2e7055dcc38f20b391a50a6c0ac0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-03-29 13:08:38 +0200

    Duel.GetCurrentPhase(PHASE_BATTLE_START)

--- official/c97120394.lua
+++ official/c97120394.lua
@@ -4,34 +4,29 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_BATTLE_START)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function() return Duel.IsPhase(PHASE_BATTLE_START) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase(PHASE_BATTLE_START)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		Duel.SetChainLimit(aux.FALSE)
-	end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) end
+	Duel.SetChainLimit(aux.FALSE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then return end
+	--For the rest of this turn after this card resolves, neither player can activate Spell/Trap Cards, or their effects
 	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetTargetRange(1,1)
-	e1:SetValue(s.aclimit)
+	e1:SetValue(function(e,re,tp) return re:IsSpellTrapEffect() end)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-end
-function s.aclimit(e,re,tp)
-	return re:IsSpellTrapEffect()
 end

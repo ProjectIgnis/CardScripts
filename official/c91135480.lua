3a57453028938e30e8d13ad1529c0d9c446aa165
Naim <naimsantos45@gmail.com>
2025-03-30 09:17:11 -0300

    Update "Time Thief Double Barrel"

--- official/c91135480.lua
+++ official/c91135480.lua
@@ -21,20 +21,18 @@ function s.initial_effect(c)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 end
-	--Maximum of 1 for each card type
 function s.rescon(sg,e,tp,mg)
 	return sg:FilterCount(Card.IsMonster,nil)<=1
 		and sg:FilterCount(Card.IsSpell,nil)<=1
 		and sg:FilterCount(Card.IsTrap,nil)<=1
 end
-	--Activation legality
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local g=c:GetOverlayGroup()
 	local ty=0
-	if c:IsFaceup() then ty=ty | TYPE_MONSTER end
-	if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsControlerCanBeChanged),tp,0,LOCATION_MZONE,1,nil) then ty=ty | TYPE_SPELL end
-	if Duel.IsExistingMatchingCard(Card.IsNegatableMonster,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) then ty=ty | TYPE_TRAP end
+	if c:IsFaceup() then ty=ty|TYPE_MONSTER end
+	if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsControlerCanBeChanged),tp,0,LOCATION_MZONE,1,nil) then ty=ty|TYPE_SPELL end
+	if Duel.IsExistingMatchingCard(Card.IsNegatableMonster,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) then ty=ty|TYPE_TRAP end
 	if chk==0 then return ty>0 and g:IsExists(Card.IsType,1,nil,ty) end
 end
 	--Detach and apply multiple effects
@@ -43,20 +41,19 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToEffect(e) then return end
 	local g=c:GetOverlayGroup()
 	local ty=0
-	if c:IsFaceup() then ty=ty | TYPE_MONSTER end
-	if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsControlerCanBeChanged),tp,0,LOCATION_MZONE,1,nil) then ty=ty | TYPE_SPELL end
-	if Duel.IsExistingMatchingCard(Card.IsNegatableMonster,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) then ty=ty | TYPE_TRAP end
+	if c:IsFaceup() then ty=ty|TYPE_MONSTER end
+	if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsControlerCanBeChanged),tp,0,LOCATION_MZONE,1,nil) then ty=ty|TYPE_SPELL end
+	if Duel.IsExistingMatchingCard(Card.IsNegatableMonster,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) then ty=ty|TYPE_TRAP end
 	if ty==0 then return end
 	local sg=aux.SelectUnselectGroup(g:Filter(Card.IsType,nil,ty),e,tp,1,3,s.rescon,1,tp,HINTMSG_XMATERIAL)
-	local lb=0
-	for tc in aux.Next(sg) do
-		lb=lb | tc:GetType()
+	local card_type=0
+	for tc in sg:Iter() do
+		card_type=card_type|tc:GetMainCardType()
 	end
-	lb=lb & 0x7
 	Duel.SendtoGrave(sg,REASON_EFFECT)
 	Duel.RaiseSingleEvent(c,EVENT_DETACH_MATERIAL,e,0,0,0,0)
 	Duel.BreakEffect()
-	if lb & TYPE_MONSTER ~=0 then
+	if (card_type&TYPE_MONSTER)>0 then
 		if c:IsFaceup() and c:IsRelateToEffect(e) then
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
@@ -67,7 +64,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 			c:RegisterEffect(e1)
 		end
 	end
-	if lb & TYPE_SPELL ~=0 then
+	if (card_type&TYPE_SPELL)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
 		local tc=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsControlerCanBeChanged),tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
 		if tc and Duel.GetControl(tc,tp,PHASE_END,1)~=0 then
@@ -87,7 +84,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 			tc:RegisterEffect(e2)
 		end
 	end
-	if lb & TYPE_TRAP ~=0 then
+	if (card_type&TYPE_TRAP)>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
 		local g=Duel.SelectMatchingCard(tp,Card.IsNegatableMonster,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 		if #g>0 then

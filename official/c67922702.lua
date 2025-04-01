d9fff778c0cdced5989be0a02f769401a6b9535e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-01 13:50:39 +0300

    Update c67922702.lua

--- official/c67922702.lua
+++ official/c67922702.lua
@@ -2,43 +2,43 @@
 --Mecha Phantom Beast Tetherwolf
 local s,id=GetID()
 function s.initial_effect(c)
-	--Gains the levels of all "Mecha Phantom Beast Token"
+	--Special Summon 1 "Mecha Phantom Beast Token" (Machine/WIND/Level 3/ATK 0/DEF 0)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_UPDATE_LEVEL)
-	e1:SetValue(s.lvval)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--Cannot be destroyed by battle or effects while you control a Token
+	--This card's Level is increased by the total Levels of all "Mecha Phantom Beast Tokens" you control
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_LEVEL)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e2:SetCondition(s.indcon)
-	e2:SetValue(1)
+	e2:SetValue(function(e,c) return Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCode,TOKEN_MECHA_PHANTOM_BEAST),c:GetControler(),LOCATION_MZONE,0,nil):GetSum(Card.GetLevel) end)
 	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	--While you control a Token, this card cannot be destroyed by battle or card effects
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.indcon)
+	e3:SetValue(1)
 	c:RegisterEffect(e3)
-	--Special Summon 1 "Mecha Phantom Beast Token"
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e4:SetCode(EVENT_SUMMON_SUCCESS)
-	e4:SetTarget(s.sptg)
-	e4:SetOperation(s.spop)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	c:RegisterEffect(e4)
-	--Increase its ATK by 800
+	--Make this card gain 800 ATK, until the end of that turn
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,1))
 	e5:SetCategory(CATEGORY_ATKCHANGE)
 	e5:SetType(EFFECT_TYPE_QUICK_O)
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetCode(EVENT_FREE_CHAIN)
 	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_MZONE)
 	e5:SetHintTiming(TIMING_DAMAGE_STEP)
 	e5:SetCondition(s.atkcon)
 	e5:SetCost(s.atkcost)
@@ -46,15 +46,6 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 s.listed_names={TOKEN_MECHA_PHANTOM_BEAST}
-function s.lvval(e,c)
-	return Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsCode,TOKEN_MECHA_PHANTOM_BEAST),c:GetControler(),LOCATION_MZONE,0,nil):GetSum(Card.GetLevel)
-end
-function s.tknfilter(c)
-	return c:IsType(TYPE_TOKEN) or c:IsOriginalType(TYPE_TOKEN)
-end
-function s.indcon(e)
-	return Duel.IsExistingMatchingCard(s.tknfilter,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
-end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
@@ -67,16 +58,26 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
+function s.tknfilter(c)
+	return c:IsType(TYPE_TOKEN) or c:IsOriginalType(TYPE_TOKEN)
+end
+function s.indcon(e)
+	return Duel.IsExistingMatchingCard(s.tknfilter,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
+end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetBattleTarget()~=nil and aux.StatChangeDamageStepCondition()
+	local bc=e:GetHandler():GetBattleTarget()
+	return Duel.IsPhase(PHASE_DAMAGE) and not Duel.IsDamageCalculated() and bc and bc:IsControler(1-tp)
 end
 function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not e:GetHandler():HasFlagEffect(id)
-		and Duel.CheckReleaseGroupCost(tp,Card.IsType,1,false,nil,nil,TYPE_TOKEN) end
+	local c=e:GetHandler()
+	if chk==0 then return not c:HasFlagEffect(id) and Duel.CheckReleaseGroupCost(tp,Card.IsType,1,false,nil,nil,TYPE_TOKEN) end
 	local g=Duel.SelectReleaseGroupCost(tp,Card.IsType,1,1,false,nil,nil,TYPE_TOKEN)
 	Duel.Release(g,REASON_COST)
-	e:GetHandler():RegisterFlagEffect(id,RESET_PHASE|PHASE_DAMAGE_CAL,0,1)
+	c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE_CAL,0,1)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():UpdateAttack(800,RESETS_STANDARD_DISABLE_PHASE_END)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		c:UpdateAttack(800,RESETS_STANDARD_DISABLE_PHASE_END)
+	end
 end

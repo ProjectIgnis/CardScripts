e114696871916fe953038fec84792b5934c130bc
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-02 15:31:16 +0300

    Update c14677495.lua

--- official/c14677495.lua
+++ official/c14677495.lua
@@ -9,41 +9,43 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BATTLE_DESTROYED)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.selfspcond)
+	e1:SetCondition(s.selfspcon)
 	e1:SetTarget(s.selfsptg)
 	e1:SetOperation(s.selfspop)
 	c:RegisterEffect(e1)
-	--Special Summon 1 "Nordic Beast" monster from your Deck
+	--Special Summon 1 "Nordic Beast" monster from your Deck in Defense Position, except "Tanngnjostr of the Nordic Beasts"
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetCode(EVENT_CHANGE_POS)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.deckspcond)
+	e2:SetCondition(s.deckspcon)
 	e2:SetTarget(s.decksptg)
 	e2:SetOperation(s.deckspop)
 	c:RegisterEffect(e2)
 end
 s.listed_series={SET_NORDIC_BEAST}
-function s.cfilter(c,tp)
-	return c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_BATTLE) and c:IsPreviousControler(tp)
+s.listed_names={id}
+function s.selfspconfilter(c,tp)
+	return c:IsLocation(LOCATION_GRAVE) and c:IsPreviousControler(tp)
 end
-function s.selfspcond(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function s.selfspcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.selfspconfilter,1,nil,tp)
 end
 function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,0)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
 function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
-function s.deckspcond(e,tp,eg,ep,ev,re,r,rp)
+function s.deckspcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsPreviousPosition(POS_DEFENSE)
 end

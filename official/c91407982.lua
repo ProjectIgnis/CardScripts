2c093b03348261c2ddea850e5901300abd907735
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-03 11:52:00 +0300

    one more hex value

--- official/c91407982.lua
+++ official/c91407982.lua
@@ -3,15 +3,16 @@
 --Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--When this card is activated: You can add 1 "Fortune Lady" card from your Deck to your hand
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--immdes
+	--Apply a "this turn, monsters you control cannot be destroyed by card effects" effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -19,14 +20,14 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_REMOVE)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.imdcon)
-	e2:SetOperation(s.imdop)
+	e2:SetCondition(s.cannotdescon)
+	e2:SetOperation(s.cannotdesop)
 	c:RegisterEffect(e2)
-	--zerodamage
+	--Apply a "make the next battle damage you take this turn become 0" effect
 	local e3=e2:Clone()
 	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCondition(s.damcon)
-	e3:SetOperation(s.damop)
+	e3:SetCondition(s.nodamcon)
+	e3:SetOperation(s.nodamop)
 	c:RegisterEffect(e3)
 end
 s.listed_series={SET_FORTUNE_LADY}
@@ -34,44 +35,46 @@ function s.thfilter(c)
 	return c:IsSetCard(SET_FORTUNE_LADY) and c:IsAbleToHand()
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+	if Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local sg=g:Select(tp,1,1,nil)
-		Duel.SendtoHand(sg,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,sg)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
 	end
 end
-function s.imdfilter(c,tp)
-	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousControler(tp) and (c:GetReason()&REASON_EFFECT)==REASON_EFFECT
+function s.confilter(c,player)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousControler(player) and c:IsReason(REASON_EFFECT)
 end
-function s.imdcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg and eg:IsExists(s.imdfilter,1,nil,tp)
+function s.cannotdescon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.confilter,1,nil,tp) and not Duel.HasFlagEffect(tp,id)
 end
-function s.imdop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local e1=Effect.CreateEffect(e:GetHandler())
+function s.cannotdesop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.HasFlagEffect(tp,id) then return end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	local c=e:GetHandler()
+	--This turn, monsters you control cannot be destroyed by card effects
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetValue(1)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
+	aux.RegisterClientHint(c,0,tp,1,0,aux.Stringid(id,4))
 end
-function s.damfilter(c,tp)
-	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousControler(1-tp) and (c:GetReason()&0x40)==0x40
+function s.nodamcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.confilter,1,nil,1-tp)
 end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg and eg:IsExists(s.damfilter,1,nil,tp)
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
+function s.nodamop(e,tp,eg,ep,ev,re,r,rp)
+	--Make the next battle damage you take this turn become 0
 	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,5))
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(1,0)
 	e1:SetReset(RESET_PHASE|PHASE_DAMAGE_CAL|PHASE_END)
 	Duel.RegisterEffect(e1,tp)

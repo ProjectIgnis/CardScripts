6fb9d41b154e979d6045b887fa4f318cfae66d1a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-05 20:38:10 +0300

    use Cost.Detach, Duel.SelectEffect, and Card.IsNormalSpellTrap

--- official/c69840739.lua
+++ official/c69840739.lua
@@ -2,86 +2,86 @@
 --Artifact Durendal
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,5,2)
 	c:EnableReviveLimit()
-	--change effect
+	--Xyz Summon procedure: 2 Level 5 monsters
+	Xyz.AddProcedure(c,nil,5,2)
+	--Activate 1 of these effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_CHAINING)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
-	e1:SetCondition(s.chcon)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.chtg)
-	e1:SetOperation(s.chop)
+	e1:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	e1:SetCountLimit(1)
+	e1:SetCost(Cost.Detach(1))
+	e1:SetTarget(s.efftg)
+	e1:SetOperation(s.effop)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
-	--reload
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
-	e2:SetCost(s.cost)
-	e2:SetTarget(s.drtg)
-	e2:SetOperation(s.drop)
-	c:RegisterEffect(e2)
 end
-function s.chcon(e,tp,eg,ep,ev,re,r,rp)
-	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-	return (re:IsMonsterEffect() and loc==LOCATION_MZONE)
-		or ((re:GetActiveType()==TYPE_SPELL or re:GetActiveType()==TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE))
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local event_chaining,_,_,event_value,reason_effect,_,reason_player=Duel.CheckEvent(EVENT_CHAINING,true)
+	--The activated effect becomes "Destroy 1 Spell/Trap Card your opponent controls"
+	local b1=event_chaining and Duel.IsExistingMatchingCard(Card.IsSpellTrap,reason_player,0,LOCATION_ONFIELD,1,nil)
+		and ((reason_effect:IsMonsterEffect() and Duel.GetChainInfo(event_value,CHAININFO_TRIGGERING_LOCATION)==LOCATION_MZONE)
+		or (reason_effect:GetHandler():IsNormalSpellTrap() and reason_effect:IsHasType(EFFECT_TYPE_ACTIVATE)))
+	--Each player with a hand shuffles their entire hand into the Deck, then draws the same number of cards they shuffled into the Deck
+	local b2=(Duel.IsPlayerCanDraw(tp) or Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0)
+		and (Duel.IsPlayerCanDraw(1-tp) or Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)==0)
+		and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND,LOCATION_HAND,1,nil)
+	if chk==0 then return b1 or b2 end
+	local op=not b1 and 2
+		or Duel.SelectEffect(tp,
+			{b1,aux.Stringid(id,1)},
+			{b2,aux.Stringid(id,2)})
+	e:SetLabel(op)
+	if op==1 then
+		e:SetCategory(0)
+		Duel.SetTargetParam(event_value)
+	elseif op==2 then
+		e:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+		if not b1 then Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,2)) end
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,PLAYER_ALL,LOCATION_HAND)
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
+	end
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-end
-function s.chtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,rp,0,LOCATION_ONFIELD,1,nil) end
-end
-function s.chop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Group.CreateGroup()
-	Duel.ChangeTargetCard(ev,g)
-	Duel.ChangeChainOperation(ev,s.repop)
-end
-function s.filter(c)
-	return c:IsSpellTrap()
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		--The activated effect becomes "Destroy 1 Spell/Trap Card your opponent controls"
+		local g=Group.CreateGroup()
+		local event_value=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+		Duel.ChangeTargetCard(event_value,g)
+		Duel.ChangeChainOperation(event_value,s.repop)
+	elseif op==2 then
+		--Each player with a hand shuffles their entire hand into the Deck, then draws the same number of cards they shuffled into the Deck
+		local g=Duel.GetFieldGroup(tp,LOCATION_HAND,LOCATION_HAND)
+		if Duel.SendtoDeck(g,nil,SEQ_DECKTOP,REASON_EFFECT)>0 then
+			local og=Duel.GetOperatedGroup():Match(Card.IsLocation,nil,LOCATION_DECK)
+			local turn_player=Duel.GetTurnPlayer()
+			local non_turn_player=1-turn_player
+			if og:IsExists(Card.IsControler,1,nil,turn_player) then Duel.ShuffleDeck(turn_player) end
+			if og:IsExists(Card.IsControler,1,nil,non_turn_player) then Duel.ShuffleDeck(non_turn_player) end
+			local ct1=og:FilterCount(Card.IsPreviousControler,nil,turn_player)
+			local ct2=og:FilterCount(Card.IsPreviousControler,nil,non_turn_player)
+			Duel.BreakEffect()
+			if ct1>0 then
+				Duel.Draw(turn_player,ct1,REASON_EFFECT)
+			end
+			if ct2>0 then
+				Duel.Draw(non_turn_player,ct2,REASON_EFFECT)
+			end
+		end
+	end
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsNormalSpell() or c:IsNormalTrap() then
+	if c:IsNormalSpellTrap() then
 		c:CancelToGrave(false)
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,Card.IsSpellTrap,tp,0,LOCATION_ONFIELD,1,1,nil)
 	if #g>0 then
 		Duel.HintSelection(g)
 		Duel.Destroy(g,REASON_EFFECT)
-	end
-end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local h1=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
-	local h2=Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)
-	if chk==0 then return (Duel.IsPlayerCanDraw(tp) or h1==0)
-		and (Duel.IsPlayerCanDraw(1-tp) or h2==0)
-		and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND,LOCATION_HAND,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,PLAYER_ALL,LOCATION_HAND)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,LOCATION_HAND)
-	if Duel.SendtoDeck(g,nil,SEQ_DECKTOP,REASON_EFFECT)~=0 then
-		local og=g:Filter(Card.IsLocation,nil,LOCATION_DECK)
-		if og:IsExists(Card.IsControler,1,nil,tp) then Duel.ShuffleDeck(tp) end
-		if og:IsExists(Card.IsControler,1,nil,1-tp) then Duel.ShuffleDeck(1-tp) end
-		Duel.BreakEffect()
-		local ct1=og:FilterCount(aux.FilterEqualFunction(Card.GetPreviousControler,tp),nil)
-		local ct2=og:FilterCount(aux.FilterEqualFunction(Card.GetPreviousControler,1-tp),nil)
-		Duel.Draw(tp,ct1,REASON_EFFECT)
-		Duel.Draw(1-tp,ct2,REASON_EFFECT)
 	end
 end

9d56df9180b9222a7f4fcbe95804f309b3305c03
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-05 14:39:07 +0300

    function names + can draw 1 check

--- official/c65984457.lua
+++ official/c65984457.lua
@@ -2,29 +2,30 @@
 --Gladiator Beast Torax
 local s,id=GetID()
 function s.initial_effect(c)
-	--draw
+	--Draw 1 card
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_PHASE|PHASE_BATTLE)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.spcon)
+	e1:SetCondition(s.drcon)
 	e1:SetCost(Cost.SelfToDeck)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
 	c:RegisterEffect(e1)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(id)>0 and e:GetHandler():GetBattledGroupCount()>0
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:HasFlagEffect(id) and c:GetBattledGroupCount()>0
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
 end

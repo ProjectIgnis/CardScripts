0e2cb08db16f926f8a7c3ef382e9742d8eb4b52b
yoshiduels <yoshi.katreuvin@gmail.com>
2025-04-07 13:23:38 +0200

    added Torna the Magician of Whirlwinds

---
+++ rush/c160402051.lua
@@ -0,0 +1,53 @@
+--旋風の魔導師トルネ
+--Torna the Magician of Whirlwinds
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,160301002,1,s.ffilter,1)
+	--Destroy 1 Defense Position monster the opponent controls
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.named_material={160301002}
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_SPELLCASTER,scard,sumtype,tp) and c:IsAttribute(ATTRIBUTE_DARK,scard,sumtype,tp)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsStatus(STATUS_SPSUMMON_TURN) and c:IsSummonType(SUMMON_TYPE_FUSION)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsDefensePos),tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spfilter(c,e,tp)
+	return c:IsLevel(9) and c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsDefensePos,8),tp,0,LOCATION_MZONE,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local dg=g:Select(tp,1,1,nil)
+	local dg2=dg:AddMaximumCheck()
+	Duel.HintSelection(dg2)
+	local ct=Duel.Destroy(dg,REASON_EFFECT) 
+	local g2=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,nil,e,tp)
+	if ct>0 and #g2>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		if #sg>0 then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

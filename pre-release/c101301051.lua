6e5e609cc32804619042c7a9ed32659ac9d05274
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-08 13:50:10 +0300

    Added new card scripts

---
+++ pre-release/c101301051.lua
@@ -0,0 +1,93 @@
+--うかのみつねのおなり
+--Ukanomitsune-no-Onari
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Summon procedure: 2 LIGHT monsters, including a Beast monster
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),2,2,s.matcheck)
+	--Your opponent cannot target cards in the Field Zones with card effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_FZONE,LOCATION_FZONE)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--Set 1 Quick-Play Spell from your GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_LEAVE_GRAVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(function(e) return e:GetHandler():IsLinkSummoned() end)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+	--Apply effects up to the number of cards in the Field Zones
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.efftg)
+	e3:SetOperation(s.effop)
+	c:RegisterEffect(e3)
+end
+function s.matcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsRace,1,nil,RACE_BEAST,lc,sumtype,tp)
+end
+function s.setfilter(c)
+	return c:IsQuickPlaySpell() and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.setfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,tp,0)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsSSetable() then
+		Duel.SSet(tp,tc)
+	end
+end
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(0,LOCATION_FZONE,LOCATION_FZONE)>0 end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,1-tp,LOCATION_ONFIELD)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,1,1-tp,1500)
+end
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetFieldGroupCount(0,LOCATION_FZONE,LOCATION_FZONE)
+	if ct==0 then return end
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+	local b1=#g>0
+	local b2=true
+	local b3=b1 and ct==2
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,2)},
+		{b2,aux.Stringid(id,3)},
+		{b3,aux.Stringid(id,4)})
+	local breakeffect=false
+	if op&1>0 then
+		--Destroy 1 card your opponent controls
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local sg=g:Select(tp,1,1,nil)
+		if #sg>0 then
+			Duel.HintSelection(sg)
+			Duel.Destroy(sg,REASON_EFFECT)
+			breakeffect=true
+		end
+	end
+	if op&2>0 then
+		--Inflict 1500 damage to your opponent
+		if breakeffect then Duel.BreakEffect() end
+		Duel.Damage(1-tp,1500,REASON_EFFECT)
+	end
+end

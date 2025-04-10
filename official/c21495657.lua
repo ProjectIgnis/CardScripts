50e2b6fbd00488ee8e3c02c993205603555bca9b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-10 12:10:08 +0300

    use table in Card.IsSetCard + typo in archetype constant

--- official/c21495657.lua
+++ official/c21495657.lua
@@ -2,66 +2,66 @@
 --Zefraxi, Treasure of the Yang Zing
 local s,id=GetID()
 function s.initial_effect(c)
-	--Enable pendulum summon
+	--Pendulum Summon procedure
 	Pendulum.AddProcedure(c)
-	--Cannot pendulum summon monsters, except "Zefra" and "Yang Zing" monsters
+	--You cannot Pendulum Summon monsters, except "Yang Zing" and "Zefra" monsters
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,c,sump,sumtype,sumpos,targetp) return sumtype&SUMMON_TYPE_PENDULUM==SUMMON_TYPE_PENDULUM and not c:IsSetCard({SET_YANG_ZING,SET_ZEFRA}) end)
+	c:RegisterEffect(e1)
+	--Treat 1 "Yang Zing" or "Zefra" monster you control, except "Zefraxi, Treasure of the Yang Zing", as a Tuner this turn
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
-	e2:SetTargetRange(1,0)
-	e2:SetTarget(s.splimit)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.tunercon)
+	e2:SetTarget(s.tunertg)
+	e2:SetOperation(s.tunerop)
 	c:RegisterEffect(e2)
-	--Treat 1 "Zefra" or "Yang Zing" monster as a tuner
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCountLimit(1,id)
-	e3:SetCondition(s.condition)
-	e3:SetTarget(s.target)
-	e3:SetOperation(s.operation)
-	c:RegisterEffect(e3)
 end
 s.listed_series={SET_YANG_ZING,SET_ZEFRA}
 s.listed_names={id}
-function s.splimit(e,c,sump,sumtype,sumpos,targetp)
-	if c:IsSetCard(SET_YANG_ZING) or c:IsSetCard(SET_ZEFRA) then return false end
-	return (sumtype&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
+function s.tunercon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPendulumSummoned() or c:IsSummonLocation(LOCATION_DECK)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPendulumSummoned() or e:GetHandler():IsPreviousLocation(LOCATION_DECK)
+function s.tunerfilter(c)
+	return c:IsSetCard({SET_YANG_ZING,SET_ZEFRA}) and not c:IsType(TYPE_TUNER) and c:IsFaceup() and not c:IsCode(id)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard({SET_YANG_ZIN,SET_ZEFRA}) and not c:IsType(TYPE_TUNER) and not c:IsCode(id)
+function s.tunertg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.tunerfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tunerfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.tunerfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.tunerop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		--That face-up monster is treated as a Tuner this turn
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_ADD_TYPE)
-		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		e1:SetValue(TYPE_TUNER)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 	if c:IsRelateToEffect(e) then
-		--Return it to deck if it leaves the field
+		--Place this card on the bottom of the Deck when it leaves the field
 		local e2=Effect.CreateEffect(c)
 		e2:SetDescription(3301)
 		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
-		e2:SetReset(RESET_EVENT|RESETS_REDIRECT)
+		e2:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 		e2:SetValue(LOCATION_DECKBOT)
+		e2:SetReset(RESET_EVENT|RESETS_REDIRECT)
 		c:RegisterEffect(e2)
 	end
 end

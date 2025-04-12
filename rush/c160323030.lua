01cf2014c5af0712448dc34b504359880872885e
yoshiduels <yoshi.katreuvin@gmail.com>
2025-04-12 18:26:07 +0200

    removed location from rush Ritual Spells

--- rush/c160323030.lua
+++ rush/c160323030.lua
@@ -3,7 +3,7 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil,location=LOCATION_EXTRA})
+	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil})
 	c:RegisterEffect(e1)
 end
 s.listed_names={30208479}
--- rush/c160323031.lua
+++ rush/c160323031.lua
@@ -3,7 +3,7 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil,matfilter=s.forcedgroup,location=LOCATION_EXTRA})
+	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil,matfilter=s.forcedgroup})
 	c:RegisterEffect(e1)
 end
 s.listed_names={160323002,160323003}

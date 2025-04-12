11d2199f2bd469c7e1efd4a6937d6bb832d35c9a
Naim <naimsantos45@gmail.com>
2025-04-12 12:11:01 -0300

    Update Etraynze the Sprinting Shadow Flower Ninja

--- rush/c160021012.lua
+++ rush/c160021012.lua
@@ -11,17 +11,17 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetValue(160005029)
 	c:RegisterEffect(e1)
-	--Name change
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_ATKCHANGE)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
-	c:RegisterEffect(e1)
+	--This card's name becomes "Etraynze the Shadow Flower Ninja", then you can add up to 2 EARTH Attribute Plant Type Normal Monsters from your Graveyard to your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
 end
 s.listed_names={160005029} --Etraynze the Shadow Flower Ninja
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -30,6 +30,7 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return not c:IsCode(160005029) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
 end
 function s.thfilter(c)
 	return c:IsRace(RACE_PLANT) and c:IsAttribute(ATTRIBUTE_EARTH) and c:IsType(TYPE_NORMAL) and c:IsAbleToHand()

80584e1bf6a4c19aead8cdba733d95362a736cc2
yoshiduels <yoshi.katreuvin@gmail.com>
2025-04-12 16:35:15 +0200

    added new rush ritual scripts

---
+++ rush/c160323003.lua
@@ -0,0 +1,50 @@
+--セレモニアル・ナイト
+--Ceremonial Knight
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Increase self ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,1000)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsLevel(8) and c:IsRace(RACE_SPELLCASTER) and c:IsRitualMonster()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	local c=e:GetHandler()
+	--Gain ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+	e1:SetValue(1000)
+	c:RegisterEffect(e1)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	if #g>0 and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) then
+		for tc in g:Iter() do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(500)
+			e1:SetReset(RESETS_STANDARD_PHASE_END)
+			tc:RegisterEffect(e1)
+		end
+	end
+end

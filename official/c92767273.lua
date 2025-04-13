874e285f00cafcc62963119634d5ef2223361841
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-13 21:17:36 +0300

    Update c92767273.lua
    - Can target a monster on either field.
    - Fixed hint message.
    - Added cannot disable flags to the ATK changing effects.

--- official/c92767273.lua
+++ official/c92767273.lua
@@ -1,10 +1,10 @@
---EMバラクーダ
+--ＥＭバラクーダ
 --Performapal Barracuda
 local s,id=GetID()
 function s.initial_effect(c)
-	--pendulum summon
+	--Pendulum Summon procedure
 	Pendulum.AddProcedure(c)
-	--atk up
+	--Make an opponent's monster that battles your "Performapal" monster lose ATK equal to the difference between its original ATK and current ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
@@ -12,60 +12,60 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_BATTLE_CONFIRM)
 	e1:SetRange(LOCATION_PZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(s.atkcon1)
-	e1:SetOperation(s.atkop1)
+	e1:SetCondition(s.oppatkcon)
+	e1:SetOperation(s.oppatkop)
 	c:RegisterEffect(e1)
-	--atk down
+	--Make 1 "Performapal" monster whose current ATK is different from its original ATK gain ATK equal to the difference until the end of this turn
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_ATKCHANGE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP)
 	e2:SetCondition(aux.StatChangeDamageStepCondition)
-	e2:SetTarget(s.atktg2)
-	e2:SetOperation(s.atkop2)
+	e2:SetTarget(s.ppalatktg)
+	e2:SetOperation(s.ppalatkop)
 	c:RegisterEffect(e2)
 end
 s.listed_series={SET_PERFORMAPAL}
-function s.atkcon1(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetAttacker()
-	local bc=Duel.GetAttackTarget()
-	if not bc then return false end
-	if tc:IsControler(1-tp) then bc,tc=tc,bc end
-	e:SetLabelObject(bc)
-	return bc:IsFaceup() and tc:IsFaceup() and tc:IsSetCard(SET_PERFORMAPAL) and bc:GetBaseAttack()~=bc:GetAttack()
+function s.oppatkcon(e,tp,eg,ep,ev,re,r,rp)
+	local bc1,bc2=Duel.GetBattleMonster(tp)
+	return bc1 and bc1:IsSetCard(SET_PERFORMAPAL) and bc2 and not bc2:IsAttack(bc2:GetBaseAttack())
 end
-function s.atkop1(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local bc=e:GetLabelObject()
+function s.oppatkop(e,tp,eg,ep,ev,re,r,rp)
+	local _,bc=Duel.GetBattleMonster(tp)
 	if bc:IsRelateToBattle() and bc:IsFaceup() and bc:IsControler(1-tp) then
 		local diff=math.abs(bc:GetBaseAttack()-bc:GetAttack())
+		--It loses ATK equal to the difference between its original ATK and current ATK
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(-diff)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		bc:RegisterEffect(e1)
 	end
 end
-function s.atkfilter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_PERFORMAPAL) and c:GetBaseAttack()~=c:GetAttack()
+function s.ppalatkfilter(c)
+	return c:IsSetCard(SET_PERFORMAPAL) and c:IsFaceup() and not c:IsAttack(c:GetBaseAttack())
 end
-function s.atktg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.atkfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
+function s.ppalatktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.ppalatkfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.ppalatkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	Duel.SelectTarget(tp,s.ppalatkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
-function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
+function s.ppalatkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local diff=math.abs(tc:GetBaseAttack()-tc:GetAttack())
+		--It gains ATK equal to the difference until the end of this turn
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(diff)
 		e1:SetReset(RESETS_STANDARD_PHASE_END)

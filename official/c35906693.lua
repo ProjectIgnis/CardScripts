1daba273cfcbbbe8ace388bfda8eb10be3b22fe9
Naim <naimsantos45@gmail.com>
2025-04-13 08:14:17 -0300

    "Shining Draw" update
    -  it was missing a hint message for the target part
    - it was making the player select the effect before targetting
    - use only one function for the resolution

--- official/c35906693.lua
+++ official/c35906693.lua
@@ -3,20 +3,22 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Reveal it during the Draw Phase
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_DRAW)
 	e1:SetCondition(s.regcon)
 	e1:SetOperation(s.regop)
 	c:RegisterEffect(e1)
+	--Activate 1 of these effects
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_ACTIVATE)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCondition(s.condition)
+	e2:SetCondition(function() return Duel.IsPhase(PHASE_MAIN1) end)
 	e2:SetCost(s.cost)
 	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
 end
 s.listed_series={SET_UTOPIA,SET_ZW}
@@ -35,17 +37,14 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_MAIN1,EFFECT_FLAG_CLIENT_HINT,1,0,66)
 	end
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsPhase(PHASE_MAIN1)
-end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():GetFlagEffect(id)~=0 end
 end
-function s.filter(c)
+function s.utopiafilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(SET_UTOPIA)
 end
-function s.filter2(c,e,tp)
-	return s.filter(c) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
+function s.utopiafilter2(c,e,tp)
+	return s.utopiafilter(c) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
 end
 function s.spfilter(c,e,tp,mc)
 	if c.rum_limit and not c.rum_limit(mc,e) then return false end
@@ -58,66 +57,68 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then
 		local b=chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE)
-		if e:GetLabel()==0 then return b and s.filter(chkc)
-		else return b and s.filter2(chkc,e,tp) end
+		if e:GetLabel()==0 then
+			return b and s.utopiafilter(chkc)
+		else
+			return b and s.filter2(chkc,e,tp)
+		end
 	end
 	local ct=Duel.GetLocationCount(tp,LOCATION_SZONE)
-	local b1=Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil,tp) and (ct>1 or (ct>0 and not e:GetHandler():IsLocation(LOCATION_HAND)))
-	local b2=Duel.IsExistingTarget(s.filter2,tp,LOCATION_MZONE,0,1,nil,e,tp)
+	if e:GetHandler():IsLocation(LOCATION_HAND) then ct=ct-1 end
+	local b1=Duel.IsExistingTarget(s.utopiafilter,tp,LOCATION_MZONE,0,1,nil)
+		and ct>=1 and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil,tp)
+	local b2=Duel.IsExistingTarget(s.utopiafilter2,tp,LOCATION_MZONE,0,1,nil,e,tp)
 	if chk==0 then return b1 or b2 end
-	local op=0
-	if b1 and b2 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-	elseif b1 then
-		op=Duel.SelectOption(tp,aux.Stringid(id,1))
-	else
-		op=Duel.SelectOption(tp,aux.Stringid(id,2))+1
-	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.utopiafilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
 	e:SetLabel(op)
-	if op==0 then
+	if op==1 then
 		e:SetCategory(CATEGORY_EQUIP)
-		e:SetOperation(s.eqop)
-		Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-	else
-		e:SetCategory(0)
-		e:SetOperation(s.spop)
-		Duel.SelectTarget(tp,s.filter2,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+		Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
+	elseif op==2 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 	end
 end
-function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
 	local tc=Duel.GetFirstTarget()
-	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
-	local g=Duel.GetMatchingGroup(s.eqfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,nil,tp)
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and #g>0 and ft>0 then
-		local sg=aux.SelectUnselectGroup(g,e,tp,1,ft,aux.dncheck,1,tp,HINTMSG_EQUIP)
-		local sc=sg:GetFirst()
-		for sc in aux.Next(sg) do
-			Duel.Equip(tp,sc,tc,true)
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_EQUIP_LIMIT)
-			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-			e1:SetValue(s.eqlimit)
-			e1:SetLabelObject(tc)
-			sc:RegisterEffect(e1)
+	if tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
+	if op==1 then
+		--Equip any number of "ZW -" monsters with different names from your Deck/Extra Deck to it
+		local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+		local g=Duel.GetMatchingGroup(s.eqfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,nil,tp)
+		if #g>0 and ft>0 then
+			local sg=aux.SelectUnselectGroup(g,e,tp,1,ft,aux.dncheck,1,tp,HINTMSG_EQUIP)
+			for sc in sg:Iter() do
+				Duel.Equip(tp,sc,tc,true)
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_EQUIP_LIMIT)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+				e1:SetValue(s.eqlimit)
+				e1:SetLabelObject(tc)
+				sc:RegisterEffect(e1)
+			end
+			Duel.EquipComplete()
 		end
-		Duel.EquipComplete()
+	elseif op==2 then
+		--Special Summon from your Extra Deck, 1 "Utopia" Xyz Monster with a different name from that target, by using that target as material
+		if tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc):GetFirst()
+		if sc then
+			sc:SetMaterial(tc)
+			Duel.Overlay(sc,tc)
+			Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+			sc:CompleteProcedure()
+		end
 	end
 end
 function s.eqlimit(e,c)
 	return c==e:GetLabelObject()
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if not tc or tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc)
-	local sc=g:GetFirst()
-	if sc then
-		sc:SetMaterial(tc)
-		Duel.Overlay(sc,tc)
-		Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
-		sc:CompleteProcedure()
-	end
 end

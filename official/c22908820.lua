08cf483234ed6b7b21a81c80193a9e2244876938
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-13 18:45:34 +0300

    Update c22908820.lua
    it's "Must be", not "Must first be"

--- official/c22908820.lua
+++ official/c22908820.lua
@@ -1,38 +1,38 @@
---E・HERO サンライザー
+--Ｅ・ＨＥＲＯ サンライザー
 --Elemental HERO Sunrise
 --Scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Fusion materials: 2 "Elemental HERO" monsters with different attributes
-	Fusion.AddProcMixN(c,true,true,s.ffilter,2)
-	c:AddMustFirstBeFusionSummoned()
-	--Add 1 "Miracle Fusion" from your Deck to your hand.
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
-	c:RegisterEffect(e1)
+	--Fusion Materials: 2 "HERO" monsters with different Attributes
+	Fusion.AddProcMixN(c,true,true,s.matfilter,2)
+	c:AddMustBeFusionSummoned()
 	--Monsters you control gain 200 ATK for each different Attribute you control
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetValue(function(e,c) return 200*Duel.GetMatchingGroup(Card.IsFaceup,e:GetHandlerPlayer(),LOCATION_MZONE,0,nil):GetBinClassCount(Card.GetAttribute) end)
+	c:RegisterEffect(e1)
+	--Add 1 "Miracle Fusion" from your Deck to your hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetValue(s.val)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 	--Destroy 1 card on the field
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DESTROY)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,{id,1})
 	e3:SetCondition(s.descon)
@@ -43,10 +43,10 @@ end
 s.material_setcode={SET_HERO}
 s.listed_series={SET_HERO}
 s.listed_names={45906428} --"Miracle Fusion"
-function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
-	return c:IsSetCard(SET_HERO,fc,sumtype,tp) and c:GetAttribute(fc,sumtype,tp)~=0 and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetAttribute(fc,sumtype,tp),fc,sumtype,tp))
+function s.matfilter(c,fc,sumtype,tp,sub,mg,sg)
+	return c:IsSetCard(SET_HERO,fc,sumtype,tp) and c:GetAttribute(fc,sumtype,tp)~=0 and (not sg or not sg:IsExists(s.diffattrfilter,1,c,c:GetAttribute(fc,sumtype,tp),fc,sumtype,tp))
 end
-function s.fusfilter(c,attr,fc,sumtype,tp)
+function s.diffattrfilter(c,attr,fc,sumtype,tp)
 	return c:IsAttribute(attr,fc,sumtype,tp) and not c:IsHasEffect(511002961)
 end
 function s.thfilter(c)
@@ -64,23 +64,16 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function s.val(e,c)
-	local tp=e:GetHandlerPlayer()
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
-	return 200*g:GetBinClassCount(Card.GetAttribute)
-end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	local at=Duel.GetAttackTarget()
-	return a~=e:GetHandler() and a:IsControler(tp) and a:IsSetCard(SET_HERO)
-		or at and at~=e:GetHandler() and at:IsControler(tp) and at:IsFaceup() and at:IsSetCard(SET_HERO)
+	local bc=Duel.GetBattleMonster(tp)
+	return bc and bc~=e:GetHandler() and bc:IsSetCard(SET_HERO) and bc:IsFaceup()
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() end
 	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()

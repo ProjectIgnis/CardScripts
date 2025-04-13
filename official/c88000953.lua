a11fd82e7aab7dd60a0f93b24c1b4be02e75f8b4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-13 20:19:38 +0300

    Update c88000953.lua
    - Use RESETS_STANDARD_DISABLE_PHASE_END for the ATK boost.
    - Check that there are enough cards on the field to destroy to not allow Tributing more causing the number of cards on the field to be fewer than the number Tributed.
    - Don't destroy anything if there are fewer cards on the field than the number Tributed.
    - Cannot apply the effect without paying the cost.

--- official/c88000953.lua
+++ official/c88000953.lua
@@ -11,13 +11,13 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
-	--Destroy a number of cards on the field equal to the number of cards tributed
+	--Destroy a number of cards on the field equal to the number of cards Tributed
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DESTROY)
@@ -29,51 +29,56 @@ function s.initial_effect(c)
 	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 end
-function s.atkfilter(c)
-	return c:IsLinkMonster() and c:GetAttack()>0
-end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.atkfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.atkfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsLinkMonster() and chkc:HasNonZeroAttack() end
+	if chk==0 then return Duel.IsExistingTarget(aux.AND(Card.IsLinkMonster,Card.HasNonZeroAttack),tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.atkfilter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
+	Duel.SelectTarget(tp,aux.AND(Card.IsLinkMonster,Card.HasNonZeroAttack),tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsFaceup() and c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
+		--This card gains ATK equal to that target's ATK until the end of this turn
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(tc:GetAttack())
-		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		e1:SetReset(RESETS_STANDARD_DISABLE_PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end
-function s.cfilter(c,e)
-	local g=e:GetHandler():GetLinkedGroup()
-	return g:IsContains(c) 
+function s.descostfilter(c,lg)
+	return lg:IsContains(c)
+end
+function s.rescon(sg,tp,exg)
+	return Duel.IsExistingMatchingCard(nil,0,LOCATION_ONFIELD,LOCATION_ONFIELD,#sg,sg)
 end
 function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,false,nil,e) end 
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local cg=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,#g,false,false,nil,e)
-	e:SetLabel(#cg)
-	Duel.Release(cg,REASON_COST)
+	e:SetLabel(-100)
+	local lg=e:GetHandler():GetLinkedGroup()
+	if chk==0 then return #lg>0 and Duel.CheckReleaseGroupCost(tp,s.descostfilter,1,false,s.rescon,nil,lg) end 
+	local g=Duel.SelectReleaseGroupCost(tp,s.descostfilter,1,#lg,false,s.rescon,nil,lg)
+	e:SetLabel(#g)
+	Duel.Release(g,REASON_COST)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	local label=e:GetLabel()
+	if chk==0 then
+		local res=label==-100
+		e:SetLabel(0)
+		return res
+	end
 	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,e:GetLabel(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,label,tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	local n=e:GetLabel()
-	if n>#g then n=#g end
+	local ct=e:GetLabel()
+	if Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)<ct then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local tg=g:Select(tp,n,n,nil)
-	if #tg>0 then 
-		Duel.Destroy(tg,REASON_EFFECT)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,ct,ct,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT)
 	end
 end

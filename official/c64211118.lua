400a153613b0f2a6b3b5e50d911e91912b22a892
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-15 23:58:13 +0300

    Update c64211118.lua

--- official/c64211118.lua
+++ official/c64211118.lua
@@ -4,17 +4,17 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Link Summon procedure
+	--Link Summon procedure: 3+ Cyberse monsters
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_CYBERSE),3)
-	--Negate opponent's monster effects activated in the Battle Phase
+	--Negate any monster effects activated by your opponent during the Battle Phase
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_CHAIN_SOLVING)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.negcon)
-	e1:SetOperation(s.negop)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return Duel.IsBattlePhase() and rp==1-tp and re:IsMonsterEffect() and Duel.IsChainDisablable(ev) end)
+	e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp) Duel.Hint(HINT_CARD,0,id) Duel.NegateEffect(ev) end)
 	c:RegisterEffect(e1)
-	--Gain Attribute and ATK
+	--Send 1 Cyberse monster from your Deck or Extra Deck to the GY, and if you do, this card gains the Attribute of that monster sent to the GY, also it gains 2500 ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE)
@@ -25,60 +25,42 @@ function s.initial_effect(c)
 	e2:SetHintTiming(TIMING_DAMAGE_STEP)
 	e2:SetCountLimit(1,id)
 	e2:SetCondition(aux.StatChangeDamageStepCondition)
-	e2:SetTarget(s.gytg)
-	e2:SetOperation(s.gyop)
+	e2:SetTarget(s.tgtg)
+	e2:SetOperation(s.tgop)
 	c:RegisterEffect(e2)
-	--Multiple attacks
+	--This card can attack monsters a number of times each Battle Phase, up to the number of different Attributes it has.
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
-	e3:SetValue(s.raval)
+	e3:SetValue(function(e,c) return Group.FromCards(e:GetHandler()):GetBinClassCount(Card.GetAttribute)-1 end)
 	c:RegisterEffect(e3)
 end
-function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return rp==1-tp and re:IsMonsterEffect() 
-		and Duel.IsChainDisablable(ev) and Duel.IsBattlePhase()
-end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_CARD,0,id)
-	Duel.NegateEffect(ev)
-end
-function s.gyfilter(c)
+function s.tgfilter(c)
 	return c:IsRace(RACE_CYBERSE) and c:IsAbleToGrave()
 end
-function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil) end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,2500)
 end
-function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local tc=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil):GetFirst()
-	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE)
+	local sc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil):GetFirst()
+	if sc and Duel.SendtoGrave(sc,REASON_EFFECT)>0 and sc:IsLocation(LOCATION_GRAVE)
 		and c:IsRelateToEffect(e) and c:IsFaceup() then
-		--Gain Attribute
+		--This card gains the Attribute of that monster sent to the GY, also it gains 2500 ATK
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-		e1:SetRange(LOCATION_MZONE)
 		e1:SetCode(EFFECT_ADD_ATTRIBUTE)
-		e1:SetValue(tc:GetAttribute())
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(sc:GetAttribute())
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e1)
-		--Increase ATK
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
+		local e2=e1:Clone()
 		e2:SetCode(EFFECT_UPDATE_ATTACK)
-		e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-		e2:SetRange(LOCATION_MZONE)
 		e2:SetValue(2500)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e2)
 	end
-end
-function s.raval(e,c)
-	local g=Group.CreateGroup()
-	g:AddCard(e:GetHandler())
-	return g:GetBinClassCount(Card.GetAttribute)-1
 end

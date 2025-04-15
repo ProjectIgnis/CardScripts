3a76ed4a99c6b1e1d04f511ba00f8c9017c6c6a4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-15 23:43:43 +0300

    Update c4022819.lua

--- official/c4022819.lua
+++ official/c4022819.lua
@@ -2,11 +2,11 @@
 --Hieratic Dragon of Eset
 local s,id=GetID()
 function s.initial_effect(c)
-	--You can Normal Summon this card without Tributing
+	--You can Normal Summon this card without Tributing, but its original ATK becomes 1000
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SUMMON_PROC)
 	e1:SetCondition(s.ntcon)
 	e1:SetOperation(s.ntop)
@@ -16,16 +16,16 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_LVCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
 	e2:SetTarget(s.lvtg)
 	e2:SetOperation(s.lvop)
 	c:RegisterEffect(e2)
-	--Special Summon 1 Dragon Normal Monster from your hand, Deck, or GY
+	--Special Summon 1 Dragon Normal Monster from your hand, Deck, or GY, and make its ATK/DEF 0
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_RELEASE)
 	e3:SetTarget(s.sptg)
@@ -35,31 +35,31 @@ end
 s.listed_series={SET_HIERATIC}
 function s.ntcon(e,c,minc)
 	if c==nil then return true end
-	return minc==0 and c:GetLevel()>4 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+	return minc==0 and c:IsLevelAbove(5) and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
 function s.ntop(e,tp,eg,ep,ev,re,r,rp,c)
 	--Its original ATK becomes 1000
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE-RESET_TOFIELD)
 	e1:SetCode(EFFECT_SET_BASE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(1000)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE&~RESET_TOFIELD)
 	c:RegisterEffect(e1)
 end
-function s.lvfilter(c,tp)
-	return c:IsFaceup() and c:IsType(TYPE_NORMAL) and c:IsRace(RACE_DRAGON)
-		and Duel.IsExistingMatchingCard(s.hieraticfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,c:GetLevel())
+function s.lvfilter(c)
+	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_NORMAL) and c:IsFaceup()
+		and Duel.IsExistingMatchingCard(s.hieraticfilter,0,LOCATION_MZONE,LOCATION_MZONE,1,nil,c:GetLevel())
 end
-function s.hieraticfilter(c,lvl)
-	return c:IsSetCard(SET_HIERATIC) and c:HasLevel() and c:IsFaceup() and not c:IsLevel(lvl)
+function s.hieraticfilter(c,lv)
+	return c:IsSetCard(SET_HIERATIC) and c:HasLevel() and c:IsFaceup() and not c:IsLevel(lv)
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.lvfilter(chkc,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.lvfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local tc=Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,tp):GetFirst()
+	local tc=Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil):GetFirst()
 	local g=Duel.GetMatchingGroup(s.hieraticfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tc:GetLevel())
 	Duel.SetOperationInfo(0,CATEGORY_LVCHANGE,g,#g,tp,tc:GetLevel())
 end
@@ -68,37 +68,42 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	if not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
 	local g=Duel.GetMatchingGroup(s.hieraticfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tc:GetLevel())
 	if #g==0 then return end
+	local c=e:GetHandler()
 	local lv=tc:GetLevel()
 	for lc in g:Iter() do
-		local e1=Effect.CreateEffect(e:GetHandler())
+		--Their Levels become the Level of that monster
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
 		e1:SetValue(lv)
 		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		lc:RegisterEffect(e1)
 	end
 end
-function s.spfilter(c,e,tp)
-	return c:IsType(TYPE_NORMAL) and c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE)
 end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_NORMAL) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
-	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+	local sc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+		--Its ATK/DEF become 0
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_SET_ATTACK)
 		e1:SetValue(0)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-		tc:RegisterEffect(e1)
+		sc:RegisterEffect(e1)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_SET_DEFENSE)
-		tc:RegisterEffect(e2)
+		sc:RegisterEffect(e2)
 	end
 	Duel.SpecialSummonComplete()
 end

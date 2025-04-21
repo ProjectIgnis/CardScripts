653c8af33a1b2cf8a88d7151a93b75787fd756ac
yoshiduels <yoshi.katreuvin@gmail.com>
2025-04-21 22:12:58 +0200

    Explorer of the Temple update

--- rush/c160323010.lua
+++ rush/c160323010.lua
@@ -3,7 +3,7 @@
 --scripted by YoshiDuels
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)

517b75c21f32d54c2eac85fcbb27916365c345f3
Naim <naimsantos45@gmail.com>
2025-04-22 19:20:16 -0300

    "Meklord Astro Dragon Triskelion" fix
    - prevent script errors with an undefined group when effect to Equip a monster was activated
    -  general script modernization

--- official/c4837861.lua
+++ official/c4837861.lua
@@ -4,80 +4,47 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Equip
+	--Must first be Special Summoned (from your hand) by banishing 3 "Meklord" monsters with different names from your GY
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetCountLimit(1)
-	e1:SetTarget(s.eqtg)
-	e1:SetOperation(s.eqop)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--Attack up to thrice
+	--Look at your opponent's Extra Deck and equip 1 monster from it to this card
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.atkcon)
-	e2:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
-	e2:SetValue(2)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.eqtg)
+	e2:SetOperation(s.eqop)
 	c:RegisterEffect(e2)
-	--Special summon
+	--Gains ATK equal to the combined ATK of the equipped monsters
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_SPSUMMON_PROC)
-	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e3:SetRange(LOCATION_HAND)
-	e3:SetCondition(s.spcon)
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.atkcon)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
-	--increase ATK
+	--While equipped with a Synchro Monster, this card can make up to 3 attacks on monsters during each Battle Phase
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EFFECT_UPDATE_ATTACK)
-	e4:SetCondition(s.adcon)
-	e4:SetValue(s.atkval)
+	e4:SetCondition(s.extraatkcon)
+	e4:SetValue(2)
 	c:RegisterEffect(e4)
 end
 s.listed_series={SET_MEKLORD}
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetEquipGroup():IsExists(Card.IsOriginalType,1,nil,TYPE_SYNCHRO)
-end
-function s.eqfilter(c,tp)
-	return c:CheckUniqueOnField(tp) and c:IsMonster() and not c:IsForbidden()
-end
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.eqfilter,tp,0,LOCATION_EXTRA,1,nil,tp)
-		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
-end
-function s.eqop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		local g=Duel.GetMatchingGroup(s.eqfilter,tp,0,LOCATION_EXTRA,nil,tp)
-		if #g==0 then return end
-		Duel.ConfirmCards(tp,Duel.GetFieldGroup(tp,0,LOCATION_EXTRA))
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-		local sg=g:Select(tp,1,1,nil):GetFirst()
-		if Duel.Equip(tp,sg,c,true) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_EQUIP_LIMIT)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-			e1:SetValue(s.eqlimit)
-			e1:SetLabelObject(c)
-			sg:RegisterEffect(e1)
-			sg:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
-			Duel.ShuffleExtra(1-tp)
-		end
-	end
-end
-function s.eqlimit(e,c)
-	return c==e:GetLabelObject()
-end
-function s.spfilter(c)
+function s.spcostfilter(c)
 	return c:IsSetCard(SET_MEKLORD) and c:IsMonster() and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
 end
 function s.rescon(sg,e,tp,mg)
@@ -86,11 +53,11 @@ end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,nil)
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-3 and #rg>2 and aux.SelectUnselectGroup(rg,e,tp,3,3,s.rescon,0)
+	local rg=Duel.GetMatchingGroup(s.spcostfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,nil)
+	return #rg>=2 and aux.SelectUnselectGroup(rg,e,tp,3,3,s.rescon,0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,nil)
+	local rg=Duel.GetMatchingGroup(s.spcostfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,nil)
 	local g=aux.SelectUnselectGroup(rg,e,tp,3,3,s.rescon,1,tp,HINTMSG_REMOVE,nil,nil,true)
 	if #g>0 then
 		g:KeepAlive()
@@ -105,12 +72,44 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 	g:DeleteGroup()
 end
-function s.efilter(c)
-	return c:GetFlagEffect(id)~=0
+function s.eqfilter(c,tp)
+	return c:CheckUniqueOnField(tp) and c:IsMonster() and not c:IsForbidden()
 end
-function s.adcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetEquipGroup():IsExists(s.efilter,1,nil)
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 
+		and Duel.IsExistingMatchingCard(s.eqfilter,tp,0,LOCATION_EXTRA,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_EXTRA)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local g=Duel.GetMatchingGroup(s.eqfilter,tp,0,LOCATION_EXTRA,nil,tp)
+		if #g==0 then return end
+		Duel.ConfirmCards(tp,Duel.GetFieldGroup(tp,0,LOCATION_EXTRA))
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+		local ec=g:Select(tp,1,1,nil):GetFirst()
+		if ec and Duel.Equip(tp,ec,c,true) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_EQUIP_LIMIT)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+			e1:SetValue(s.eqlimit)
+			e1:SetLabelObject(c)
+			ec:RegisterEffect(e1)
+			ec:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+			Duel.ShuffleExtra(1-tp)
+		end
+	end
+end
+function s.eqlimit(e,c)
+	return c==e:GetLabelObject()
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetEquipGroup():IsExists(Card.HasFlagEffect,1,nil,id)
 end
 function s.atkval(e,c)
-	return e:GetHandler():GetEquipGroup():Filter(s.efilter,nil):GetSum(Card.GetAttack)
+	return e:GetHandler():GetEquipGroup():Filter(Card.HasFlagEffect,nil,id):GetSum(Card.GetAttack)
+end
+function s.extraatkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetEquipGroup():IsExists(Card.IsOriginalType,1,nil,TYPE_SYNCHRO)
 end

433e96a13e1f729c8f8d140134deca19a92f8ce8
Naim <naimsantos45@gmail.com>
2025-04-23 11:46:24 -0300

    "Deskbot 009" fix
    Fixed a bug where the destruction replacement effect could be applied from the Spell/Trap Zone

--- official/c25494711.lua
+++ official/c25494711.lua
@@ -2,17 +2,18 @@
 --Deskbot 009
 local s,id=GetID()
 function s.initial_effect(c)
-	--atkup
+	--Make this card gain ATK equal to the combined ATK of all "Deskbot" monsters you currently control
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(s.atkcon)
+	e1:SetCondition(function() return Duel.IsPhase(PHASE_MAIN1) end)
 	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
-	--actlimit
+	--Your opponent's cards and effects cannot be activated until the end of the Damage Step
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -22,24 +23,24 @@ function s.initial_effect(c)
 	e2:SetValue(1)
 	e2:SetCondition(s.actcon)
 	c:RegisterEffect(e2)
-	--destroy replace
+	--If this card would be destroyed by battle or card effect, you can destroy 1 "Deskbot" card you control instead.
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
 	e3:SetTarget(s.reptg)
 	e3:SetOperation(s.repop)
 	c:RegisterEffect(e3)
 end
 s.listed_series={SET_DESKBOT}
-s.listed_names={}
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsPhase(PHASE_MAIN1)
-end
+s.listed_names={id}
 function s.atkfilter(c)
 	return c:IsFaceup() and c:IsSetCard(SET_DESKBOT) and not c:IsCode(id)
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
+	--Only this card can attack the turn this effect is activated
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)

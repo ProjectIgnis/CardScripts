585483f9c9241122222b9eed6d265d12be72eeab
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-04-26 09:37:59 -0700

    Update "Tachyon Refresh"
    Prevent script error when targeting a monster for exclusion.

--- unofficial/c511001183.lua
+++ unofficial/c511001183.lua
@@ -1,3 +1,4 @@
+--時空浄化
 --Tachyon Refresh
 local s,id=GetID()
 function s.initial_effect(c)
@@ -15,12 +16,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsAbleToRemove() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsControler(tp) and Duel.Remove(tc,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+	if tc:IsRelateToEffect(e) and tc:IsControler(tp) and Duel.Remove(tc,0,REASON_EFFECT|REASON_TEMPORARY)~=0 then
 		Duel.BreakEffect()
 		Duel.ReturnToField(tc)
 	end

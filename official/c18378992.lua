bd5ec0be2a622af147f3633c3d5996d536f3909b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-26 13:52:15 +0300

    "Malefic Selector" update
    Move the cost to the cost function and simplify the selection checks  a little bit.

--- official/c18378992.lua
+++ official/c18378992.lua
@@ -3,7 +3,7 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	--Add 2 "Malefic" cards from the Deck to the hand
+	--Add 2 "Malefic" cards from your Deck to your hand, except "Malefic Selector", with different names from each other and from the banished cards
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -17,40 +17,40 @@ function s.initial_effect(c)
 end
 s.listed_series={SET_MALEFIC}
 s.listed_names={id}
-function s.cfilter(c)
+function s.costfilter(c)
 	return c:IsSetCard(SET_MALEFIC) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
 end
-function s.thfilter(c,g)
-	return c:IsSetCard(SET_MALEFIC) and not c:IsCode(id) and c:IsAbleToHand() and not g:IsExists(Card.IsCode,1,nil,c:GetCode())
-end
 function s.rescon(sg,e,tp,mg)
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil,sg)
+	if #sg~=2 then return false end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil,sg:GetFirst():GetCode(),sg:GetNext():GetCode())
 	return g:GetClassCount(Card.GetCode)>1
 end
+function s.thfilter(c,code1,code2)
+	return c:IsSetCard(SET_MALEFIC) and not c:IsCode(id,code1,code2) and c:IsAbleToHand()
+end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	e:SetLabel(100)
-	if chk==0 then return true end
+	e:SetLabel(-100)
+	local g=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_GRAVE|LOCATION_MZONE,0,nil)
+	if chk==0 then return #g>=2 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	local rg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_REMOVE)
+	Duel.Remove(rg,POS_FACEUP,REASON_COST)
+	e:SetLabel(rg:GetFirst():GetCode(),rg:GetNext():GetCode())
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		if e:GetLabel()~=100 then e:SetLabel(0) return false end
-		local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE|LOCATION_MZONE,0,nil,tp)
-		return #g>=2 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
+		local res=e:GetLabel()==-100
+		e:SetLabel(0)
+		return res
 	end
-	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_GRAVE|LOCATION_MZONE,0,nil,tp)
-	local rg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_REMOVE)
-	Duel.Remove(rg,POS_FACEUP,REASON_COST)
-	rg:KeepAlive()
-	e:SetLabelObject(rg)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local rg=e:GetLabelObject()
-	local hg=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil,rg)
-	local g=aux.SelectUnselectGroup(hg,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_ATOHAND)
-	if #g==2 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+	local code1,code2=e:GetLabel()
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil,code1,code2)
+	if #g<2 then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_ATOHAND)
+	if #sg==2 then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
 	end
-	rg:DeleteGroup()
 end

326369824e96e1ac78bebde11e2ccd7bff607904
Naim <naimsantos45@gmail.com>
2025-04-27 07:52:56 -0300

    batch 1 + fix for "Sprind the Irondash Dragon"
    It was making the player select the zones during the activation, not during the resolution

--- official/c11082056.lua
+++ official/c11082056.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOGRAVE|CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
--- official/c1906812.lua
+++ official/c1906812.lua
@@ -1,16 +1,16 @@
 --灰燼竜バスタード
 --Sprind the Irondash Dragon
---Logical Nonsense
---Substitute ID
+--scripted by Logical Nonsense
 local s,id=GetID()
 function s.initial_effect(c)
 	--Must be properly summoned before reviving
 	c:EnableReviveLimit()
-	--Fusion summon procedure
+	--Fusion Materials: "Fallen of Albaz" + 1 Effect Monster Special Summoned this turn
 	Fusion.AddProcMix(c,true,true,CARD_ALBAZ,aux.FilterBoolFunctionEx(s.filter))
-	--Move itself to 1 of your unused MMZ
+	--Move this card you control to another of your Main Monster Zones, then you can destroy all other face-up cards in this card's column
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -25,38 +25,32 @@ function s.initial_effect(c)
 	e2:SetOperation(s.regop)
 	c:RegisterEffect(e2)
 end
-	--Lists "Sprigguns" archetype
 s.listed_series={SET_SPRINGANS}
-	--Specifically lists "Fallen of Albaz"
 s.listed_names={CARD_ALBAZ}
-	--Check for an effect that was special summoned this turn
 function s.filter(c)
 	return c:IsLocation(LOCATION_MZONE) and c:IsType(TYPE_EFFECT) and c:IsStatus(STATUS_SPSUMMON_TURN)
 end
-	--Activation legality
 function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-	local seq=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
-	Duel.Hint(HINT_ZONE,tp,seq)
-	e:SetLabel(math.log(seq,2))
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
 end
-	--Move itself to 1 of your unused MMZ, then destroy all face-up cards in its new column
 function s.seqop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local seq=e:GetLabel()
-	if not c:IsRelateToEffect(e) or c:IsControler(1-tp) or not Duel.CheckLocation(tp,LOCATION_MZONE,seq) then return end
+	if not c:IsRelateToEffect(e) or c:IsControler(1-tp) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-	Duel.MoveSequence(c,seq)
-	local dg=c:GetColumnGroup():Filter(Card.IsFaceup,c,nil) --Check for face-up cards in its current column, except itself
-	if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-		Duel.BreakEffect()
-		Duel.Destroy(dg,REASON_EFFECT)
+	local seq=Duel.SelectDisableField(tp,1,LOCATION_MZONE,0,0)
+	if seq then
+		Duel.MoveSequence(c,math.log(seq,2))
+		local dg=c:GetColumnGroup():Filter(Card.IsFaceup,c,nil)
+		if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.Destroy(dg,REASON_EFFECT)
+		end
 	end
 end
-	--Add or special summon 1 "Sprigguns" monster or "Fallen of Albaz" from deck
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	--Add to your hand, or Special Summon, 1 "Springans" monster or 1 "Fallen of Albaz" from your Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -69,22 +63,19 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESETS_STANDARD_PHASE_END)
 	c:RegisterEffect(e1)
 end
-	--Check for a "Sprigguns" monster or "Fallen of Albaz"
 function s.thfilter(c,e,tp,ft)
 	return c:IsMonster() and (c:IsSetCard(SET_SPRINGANS) or c:IsCode(CARD_ALBAZ))
 		and (c:IsAbleToHand() or (c:IsCanBeSpecialSummoned(e,0,tp,false,false) and ft>0))
 end
-	--Activation legality
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-	--Add or special summon 1 "Sprigguns" monster or "Fallen of Albaz" from deck
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ft):GetFirst()
 	if tc then
 		aux.ToHandOrElse(tc,tp,function(c)

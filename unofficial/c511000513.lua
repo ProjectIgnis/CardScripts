bcc501e06c05bdde271a6a6d2782409a2c1daccf
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-04-27 12:09:35 -0700

    Update "Number 77: The Seven Sins (Manga)"
    Script polish/modernization update

--- unofficial/c511000513.lua
+++ unofficial/c511000513.lua
@@ -1,47 +1,44 @@
---Number 77: The Seven Sins
+--Ｎｏ．７７ ザ・セブン・シンズ (Manga)
+--Number 77: The Seven Sins (Manga)
 Duel.LoadCardScript("c62541668.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,12,2)
 	c:EnableReviveLimit()
-	--destroy
+	--Xyz Summon procedure: 2 Level 12 monsters
+	Xyz.AddProcedure(c,nil,12,2)
+	--Cannot be destroyed by battle, except by "Number" monsters
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
-	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
-	--banish replace
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+	c:RegisterEffect(e1)
+	--Destroy monsters your opponent controls, then attach them to this card as Xyz Material
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_SEND_REPLACE)
-	e2:SetTarget(s.reptg)
-	c:RegisterEffect(e2)
-	--battle indestructable
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE)
-	e5:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e5:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
-	c:RegisterEffect(e5)
+	e2:SetCountLimit(1)
+	e2:SetCost(Cost.Detach(1))
+	e2:SetTarget(s.desatchtg)
+	e2:SetOperation(s.desatchop)
+	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
+	--If this card would be banished, you can detach 1 material from this card instead
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_SEND_REPLACE)
+	e3:SetTarget(s.reptg)
+	c:RegisterEffect(e3)	
 end
-s.listed_series={0x48}
+s.listed_series={SET_NUMBER}
 s.xyz_number=77
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.desatchtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.desatchop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
 	if Duel.Destroy(g,REASON_EFFECT)>0 then

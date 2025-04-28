c5b7e5c04aa922b83abae81bad09eb58127f14b9
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-04-28 13:59:19 +0300

    Update c11082056.lua

--- official/c11082056.lua
+++ official/c11082056.lua
@@ -2,7 +2,14 @@
 --The Fang of Critias
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--This card is also always treated as "Legendary Dragon Critias"
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_ADD_CODE)
+	e0:SetValue(10000060)
+	c:RegisterEffect(e0)
+	--Send 1 Trap from your hand or field to the GY, that is listed on a Fusion Monster that can only be Special Summoned with "The Fang of Critias" (if that card is Set, reveal it), then Special Summon that Fusion Monster from your Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
@@ -12,44 +19,37 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--This card is also always treated as "Legendary Dragon Critias"
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_ADD_CODE)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetValue(10000060)
-	c:RegisterEffect(e2)
 end
-function s.tgfilter(c,e,tp,rp)
-	return c:IsTrap() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetCode(),rp)
+s.listed_names={10000060,id} --"Legendary Dragon Critias"
+function s.tgfilter(c,e,tp)
+	return c:IsTrap() and c:IsAbleToGrave() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetCode())
 end
-function s.spfilter(c,e,tp,code,rp)
-	return c:IsType(TYPE_FUSION) and c.material_trap and code==c.material_trap
-		and Duel.GetLocationCountFromEx(tp,rp,nil,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+function s.spfilter(c,e,tp,code)
+	return c:IsType(TYPE_FUSION) and c.material_trap and code==c.material_trap and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_HAND|LOCATION_SZONE,0,1,nil,e,tp,rp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_HAND|LOCATION_ONFIELD,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND|LOCATION_ONFIELD)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
-	local tc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_HAND|LOCATION_SZONE,0,1,1,nil,e,tp,rp):GetFirst()
-	if tc and not tc:IsImmuneToEffect(e) then
-		if tc:IsOnField() and tc:IsFacedown() then
-			Duel.ConfirmCards(1-tp,tc)
+	local mc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_HAND|LOCATION_ONFIELD,0,1,1,nil,e,tp):GetFirst()
+	if mc and not mc:IsImmuneToEffect(e) then
+		if mc:IsOnField() and mc:IsFacedown() then
+			Duel.ConfirmCards(1-tp,mc)
 		end
-		Duel.SendtoGrave(tc,REASON_EFFECT)
-		if not tc:IsLocation(LOCATION_GRAVE) then return end
-		local code=tc:GetCode()
-		if tc:IsPreviousLocation(LOCATION_SZONE) and tc:IsPreviousPosition(POS_FACEUP) then 
-			code=tc:GetPreviousCodeOnField()
+		if Duel.SendtoGrave(mc,REASON_EFFECT)==0 or not mc:IsLocation(LOCATION_GRAVE) then return end
+		local code=mc:GetCode()
+		if mc:IsPreviousLocation(LOCATION_ONFIELD) and mc:IsPreviousPosition(POS_FACEUP) then 
+			code=mc:GetPreviousCodeOnField()
 		end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,code):GetFirst()
 		if sc then
 			Duel.BreakEffect()
-			Duel.SpecialSummon(sc,0,tp,tp,true,false,POS_FACEUP)
+			if Duel.SpecialSummon(sc,0,tp,tp,true,false,POS_FACEUP)==0 then return end
 			sc:CompleteProcedure()
 		end
 	end

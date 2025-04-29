daef584eec9e69f3710fe001a4f8a997ae413064
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-04-28 19:58:46 -0700

    Update "No More Mrs. Nice Mai!"
    -Rescripted to match text errata
    -Added handling for damage dealt by your monsters
    -Reworked hint prompts for both normal and BP Skill activations
    -Updated code to match modernized script architecture

--- skill/c300303011.lua
+++ skill/c300303011.lua
@@ -1,44 +1,70 @@
 --No More Mrs. Nice Mai!
+--Re-scripted by The Razgriz
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false)
+	--Activate
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCode(EVENT_STARTUP)
 	e1:SetCountLimit(1)
 	e1:SetRange(0x5f)
+	e1:SetLabelObject(e)
 	e1:SetLabel(0)
-	e1:SetOperation(s.flipop2)
+	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
 end
-function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
-	--immune
-	local e1=Effect.CreateEffect(e:GetHandler())
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Effect handling for battle damage from your monsters
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+		ge1:SetCode(EVENT_BATTLE_DAMAGE)
+		ge1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return ep==1-tp end)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+	--Destroy 1 Spell/Trap card on the field
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_BATTLE_DESTROYING)
+	e1:SetCode(EVENT_PHASE|PHASE_BATTLE)
+	e1:SetRange(0x5f)
+	e1:SetCountLimit(1)
 	e1:SetCondition(s.descon)
 	e1:SetOperation(s.desop)
 	Duel.RegisterEffect(e1,tp)
 end
-function s.afilter(c,tp)
-	return c:IsControler(tp)
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if eg:IsExists(Card.IsControler,1,nil,tp) then
+		--OPT damage flag register
+		Duel.RegisterFlagEffect(tp,id+100,RESET_PHASE|PHASE_END,0,1)
+	end
 end
-function s.descon(e,tp,eg,ep,ev,re,r,rp,chk)
-	return eg:IsExists(s.afilter,1,nil,tp)
-	and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	return aux.CanActivateSkill(tp) and Duel.GetFlagEffect(tp,id)==0
 end
-function s.filter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--opt register
-	Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE+PHASE_END,0,0)
-	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local sg=dg:Select(tp,1,1,nil)
-	Duel.HintSelection(sg)
-	Duel.Destroy(sg,REASON_EFFECT)
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	Duel.ResetFlagEffect(tp,id+100,0,0,0)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(tp) and Duel.IsExistingMatchingCard(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) and Duel.GetFlagEffect(tp,id+100)>0
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+		Duel.Hint(HINT_CARD,tp,id)
+		--Destroy 1 Spell/Trap Card on the field
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	end
 end

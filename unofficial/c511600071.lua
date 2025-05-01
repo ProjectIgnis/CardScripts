222969c820f637f6c1fefb9120f421fb6c0ab81d
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-04-30 19:57:43 -0700

    Update "Shining Hope Road (Manga)"
    Duel.XyzSummon function call was breaking script/removed undefined constant.

--- unofficial/c511600071.lua
+++ unofficial/c511600071.lua
@@ -13,8 +13,8 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={SET_NUMBER,SET_NUMBER_S}
-function s.filter(c,e)
+s.listed_series={SET_NUMBER,0x2048} --"Number S"
+function s.matfilter(c,e)
 	return c:IsSetCard(SET_NUMBER) and c:IsCanBeEffectTarget(e)
 end
 function s.rescon(sg,e,tp,g)
@@ -23,13 +23,13 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	local c=e:GetHandler()
-	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,e)
-	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,SET_NUMBER_S)
+	local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_GRAVE,0,nil,e)
+	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,0x2048)
 	for _c in notSg:Iter() do
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ADD_SETCODE)
-		e1:SetValue(SET_NUMBER_S)
+		e1:SetValue(0x2048)
 		_c:RegisterEffect(e1,true)
 		_c:AssumeProperty(ASSUME_RANK,_c:GetRank()+1)
 	end
@@ -48,20 +48,22 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetTargetCards(e)
 	local c=e:GetHandler()
-	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,SET_NUMBER_S)
-	for _c in aux.Next(notSg) do
+	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,0x2048)
+	for _c in notSg:Iter() do
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ADD_SETCODE)
-		e1:SetValue(SET_NUMBER_S)
+		e1:SetValue(0x2048)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		_c:RegisterEffect(e1,true)
 		_c:AssumeProperty(ASSUME_RANK,_c:GetRank()+1)
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local xyz=Duel.SelectMatchingCard(tp,Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,1,1,nil,nil,mg,#mg,#mg):GetFirst()
-	if xyz then
-		Duel.XyzSummon(tp,xyz,mg,nil,#mg,#mg)
+	if xyz and #mg>0 and mg:Includes(notSg) then
+		xyz:SetMaterial(mg)
+		Duel.Overlay(xyz,mg)
+		Duel.SpecialSummon(xyz,SUMMON_TYPE_XYZ,tp,tp,false,true,POS_FACEUP)
 		notSg:KeepAlive()
 		notSg:ForEach(function(_c) _c:Level((_c:Level()+1)) end)
 		local e1=Effect.CreateEffect(c)
@@ -74,6 +76,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e:Reset()
 		end)
 		xyz:RegisterEffect(e1,true)
+		xyz:CompleteProcedure()
 	else
 		notSg:ForEach(function(_c) _c:ResetEffect(id,RESET_CARD) end)
 	end

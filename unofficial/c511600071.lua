429c8c93a57785914837aea8ba2f020b91330637
Edoardo Lolletti <edoardo762@gmail.com>
2025-05-01 10:16:14 +0200

    Properly fix Shining Hope Road (Manga)
    Use the proper procedure

--- unofficial/c511600071.lua
+++ unofficial/c511600071.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={SET_NUMBER,0x2048} --"Number S"
+s.listed_series={SET_NUMBER,SET_NUMBER_S}
 function s.matfilter(c,e)
 	return c:IsSetCard(SET_NUMBER) and c:IsCanBeEffectTarget(e)
 end
@@ -24,12 +24,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	local c=e:GetHandler()
 	local mg=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_GRAVE,0,nil,e)
-	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,0x2048)
+	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,SET_NUMBER_S)
 	for _c in notSg:Iter() do
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ADD_SETCODE)
-		e1:SetValue(0x2048)
+		e1:SetValue(SET_NUMBER_S)
 		_c:RegisterEffect(e1,true)
 		_c:AssumeProperty(ASSUME_RANK,_c:GetRank()+1)
 	end
@@ -48,22 +48,20 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local mg=Duel.GetTargetCards(e)
 	local c=e:GetHandler()
-	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,0x2048)
+	local notSg=mg:Filter(aux.NOT(Card.IsSetCard),nil,SET_NUMBER_S)
 	for _c in notSg:Iter() do
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ADD_SETCODE)
-		e1:SetValue(0x2048)
+		e1:SetValue(SET_NUMBER_S)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		_c:RegisterEffect(e1,true)
 		_c:AssumeProperty(ASSUME_RANK,_c:GetRank()+1)
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local xyz=Duel.SelectMatchingCard(tp,Card.IsXyzSummonable,tp,LOCATION_EXTRA,0,1,1,nil,nil,mg,#mg,#mg):GetFirst()
-	if xyz and #mg>0 and mg:Includes(notSg) then
-		xyz:SetMaterial(mg)
-		Duel.Overlay(xyz,mg)
-		Duel.SpecialSummon(xyz,SUMMON_TYPE_XYZ,tp,tp,false,true,POS_FACEUP)
+	if xyz then
+		Duel.XyzSummon(tp,xyz,mg,mg,#mg,#mg)
 		notSg:KeepAlive()
 		notSg:ForEach(function(_c) _c:Level((_c:Level()+1)) end)
 		local e1=Effect.CreateEffect(c)
@@ -76,7 +74,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e:Reset()
 		end)
 		xyz:RegisterEffect(e1,true)
-		xyz:CompleteProcedure()
 	else
 		notSg:ForEach(function(_c) _c:ResetEffect(id,RESET_CARD) end)
 	end

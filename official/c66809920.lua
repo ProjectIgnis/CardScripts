8cd62b10494441caab07bba48621f9b529ba7552
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-05-03 21:51:01 +0300

    Update c66809920.lua
    - Missing target flag
    - Check that re is a Spell effect rather than the card being a Spell
    - Should also check face-up for other "Valkyrie" monsters
    - ATK change should reset on negate and be negatable

--- official/c66809920.lua
+++ official/c66809920.lua
@@ -3,19 +3,19 @@
 --scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
-	--Add 1 "Mischief of the Time Goddess" from your Gy to your hand
+	--Add 1 "Mischief of the Time Goddess" from your GY to your hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.thcon)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return re and re:IsSpellEffect() and e:GetHandler():IsSummonLocation(LOCATION_HAND) end)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-	--Banish 1 monster from your opponent's GY, then this card's ATK becomes equal to the banished monster's original ATK
+	--Banish 1 monster from your opponent's GY, then this card's ATK becomes equal to the banished monster's original ATK until the end of this turn
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
@@ -23,16 +23,14 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
+	e2:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
 	e2:SetCondition(s.rmcon)
 	e2:SetTarget(s.rmtg)
 	e2:SetOperation(s.rmop)
 	c:RegisterEffect(e2)
 end
 s.listed_series={SET_VALKYRIE}
-s.listed_names={92182447} --"Mischief of the Time Goddess" 
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return re and re:GetHandler():IsSpell() and e:GetHandler():IsPreviousLocation(LOCATION_HAND)
-end
+s.listed_names={92182447,id} --"Mischief of the Time Goddess" 
 function s.thfilter(c)
 	return c:IsCode(92182447) and c:IsAbleToHand()
 end
@@ -49,33 +47,34 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
-function s.cfilter(c)
-	return c:IsSetCard(SET_VALKYRIE) and not c:IsCode(id)
+function s.rmconfilter(c)
+	return c:IsSetCard(SET_VALKYRIE) and c:IsFaceup() and not c:IsCode(id)
 end
 function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(s.rmconfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.rmfilter(c)
 	return c:IsAbleToRemove() and c:IsMonster()
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,0,LOCATION_GRAVE,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_GRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,e:GetHandler(),1,tp,0)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local rc=Duel.SelectMatchingCard(tp,s.rmfilter,tp,0,LOCATION_GRAVE,1,1,nil):GetFirst()
-	if rc and Duel.Remove(rc,POS_FACEUP,REASON_EFFECT)>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
+	local sc=Duel.SelectMatchingCard(tp,s.rmfilter,tp,0,LOCATION_GRAVE,1,1,nil):GetFirst()
+	if not sc then return end
+	Duel.HintSelection(sc)
+	local c=e:GetHandler()
+	if Duel.Remove(sc,POS_FACEUP,REASON_EFFECT)>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
 		Duel.BreakEffect()
-		--Tis card's ATK becomes equal to the banished monster's original ATK
+		--This card's ATK becomes equal to the banished monster's original ATK until the end of this turn
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		e1:SetCode(EFFECT_SET_ATTACK)
-		e1:SetValue(rc:GetBaseAttack())
+		e1:SetValue(sc:GetBaseAttack())
+		e1:SetReset(RESETS_STANDARD_DISABLE_PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end

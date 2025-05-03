ff6307b7da3759827f390bb2d0a53deffecbfdb1
Naim <naimsantos45@gmail.com>
2025-05-03 16:16:28 -0300

    "Chimeratech Crush Dragon" fix
    - Fixed a bug where it could increase the ATK of non-Machine monsters
    - Fixed a bug where it would not reset the ATK increase effect even after leaving the field.

--- rush/c160214031.lua
+++ rush/c160214031.lua
@@ -4,7 +4,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--fusion material
+	--Fusion Materials: "Cyber Dragon" + 2 or more Machine Type monsters
 	Fusion.AddProcMixRep(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_MACHINE),2,99,CARD_CYBER_DRAGON)
 	--Monsters you control cannot be destroyed by Spell effects
 	local e1=Effect.CreateEffect(c)
@@ -29,12 +29,14 @@ function s.efilter(e,te)
 end
 function s.sucop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	--Gain ATK
+	--Face-up Machine Type monsters on your field gain ATK equal to [the number of monsters used as material for this card's Fusion Summon] x 100.
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE))
 	e1:SetValue(c:GetMaterialCount()*100)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 	c:RegisterEffect(e1)
 end

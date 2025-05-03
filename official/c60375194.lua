202c4256331381af843921306b303d4527b499ce
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-05-04 02:23:23 +0300

    Vendread Daybreak
    Shouldn't destroy itself if it's still related

--- official/c60375194.lua
+++ official/c60375194.lua
@@ -3,45 +3,45 @@
 --Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
-	--Destroy all cards on the field, except the targeted "Vendread" ritual monster
+	--Choose 1 Ritual Summoned "Vendread" monster you control, and destroy all cards on the field, except the chosen monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(s.condition)
+	e1:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	e1:SetCondition(function(e,tp) return Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)>Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER|TIMING_END_PHASE)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_VENDREAD}
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)>Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,0)
-end
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_VENDREAD) and c:IsRitualSummoned()
+function s.vendreadfilter(c)
+	return c:IsRitualSummoned() and c:IsSetCard(SET_VENDREAD) and c:IsFaceup()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.vendreadfilter,tp,LOCATION_MZONE,0,1,nil) end
 	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,0,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g-2,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-	if #g==0 then return end
-	Duel.HintSelection(g)
-	local dg=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,g)
-	if #dg~=0 then
-		Duel.Destroy(dg,REASON_EFFECT)
+	local sc=Duel.SelectMatchingCard(tp,s.vendreadfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if not sc then return end
+	Duel.HintSelection(sc)
+	local c=e:GetHandler()
+	local exc=c:IsRelateToEffect(e) and Group.FromCards(sc,c) or sc
+	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,exc)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
 	end
-	--It cannot attack directly
-	local e1=Effect.CreateEffect(e:GetHandler())
+	--It cannot attack directly while it remains face-up on the field
+	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(3207)
 	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-	g:GetFirst():RegisterEffect(e1)
+	sc:RegisterEffect(e1)
 end

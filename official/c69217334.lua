477d2ab5c8cd5292f00234ef629b0de0ad0b3466
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-05-05 21:18:56 +0300

    Breaking of the World
    - Missing category
    - Strings
    - Show a card on resolution, not on activation
    - Check that the target is still face-up on resolution
    - Modern "activate 1 of these effects" implementation

--- official/c69217334.lua
+++ official/c69217334.lua
@@ -4,102 +4,107 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--Make the Level of 1 Ritual Monster in your hand become the Level of 1 Ritual Monster you control
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_LVCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.lvtg)
+	e1:SetOperation(s.lvop)
 	c:RegisterEffect(e1)
-	--Change the level of Ritual monster
+	--Activate 1 of these effects
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetCountLimit(1)
-	e2:SetTarget(s.lvtg)
-	e2:SetOperation(s.lvop)
+	e2:SetCondition(s.effcon)
+	e2:SetTarget(s.efftg)
+	e2:SetOperation(s.effop)
 	c:RegisterEffect(e2)
-	--Draw 1 card or destroy 1 card on the field
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetCountLimit(1)
-	e3:SetCondition(s.condition)
-	e3:SetTarget(s.target)
-	c:RegisterEffect(e3)
 end
 s.listed_names={46427957,72426662} --"Ruin, Queen of Oblivion", "Demise, King of Armageddon"
-function s.lvfilter(c,tp)
-	return c:IsFaceup() and c:IsType(TYPE_RITUAL)
-		and Duel.IsExistingMatchingCard(s.lvcfilter,tp,LOCATION_HAND,0,1,nil,c)
+function s.tgfilter(c,tp)
+	return c:IsRitualMonster() and c:IsFaceup() and Duel.IsExistingMatchingCard(s.revealfilter,tp,LOCATION_HAND,0,1,nil,c:GetLevel())
 end
-function s.lvcfilter(c,mc)
-	return c:IsRitualMonster() and not c:IsPublic() and not c:IsLevel(mc:GetLevel())
+function s.revealfilter(c,lv)
+	return c:IsRitualMonster() and not c:IsPublic() and not c:IsLevel(lv)
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.lvfilter(chkc,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.tgfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local cg=Duel.SelectMatchingCard(tp,s.lvcfilter,tp,LOCATION_HAND,0,1,1,nil,g:GetFirst())
-	Duel.ConfirmCards(1-tp,cg)
-	Duel.ShuffleHand(tp)
-	e:SetLabelObject(cg:GetFirst())
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
 end
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	local pc=e:GetLabelObject()
-	if tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local lv=tc:GetLevel()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+		local sc=Duel.SelectMatchingCard(tp,s.revealfilter,tp,LOCATION_HAND,0,1,1,nil,lv):GetFirst()
+		if not sc then return end
+		--Show it until the end of this turn
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_LEVEL)
-		e1:SetValue(tc:GetLevel())
+		e1:SetCode(EFFECT_PUBLIC)
 		e1:SetReset(RESETS_STANDARD_PHASE_END)
-		pc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_PUBLIC)
-		e2:SetReset(RESETS_STANDARD_PHASE_END)
-		pc:RegisterEffect(e2)
+		sc:RegisterEffect(e1)
+		--The Level of the shown Ritual Monster becomes the targeted monster's Level, until the end of this turn
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_LEVEL)
+		e2:SetValue(lv)
+		sc:RegisterEffect(e2)
 	end
 end
-function s.cfilter(c,tp)
-	return c:IsCode(46427957,72426662) and c:IsControler(tp) and c:IsRitualSummoned()
+function s.effconfilter(c,tp)
+	return c:IsCode(46427957,72426662) and c:IsRitualSummoned() and c:IsControler(tp) and c:IsFaceup()
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function s.effcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.effconfilter,1,nil,tp)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local b1=Duel.IsPlayerCanDraw(tp,1)
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	local g=Duel.GetFieldGroup(tp,LOCATION_ONFIELD,LOCATION_ONFIELD)
 	local b2=#g>0
 	if chk==0 then return b1 or b2 end
-	local sel=Duel.SelectEffect(tp,
-		{b1,aux.Stringid(id,0)},
-		{b2,aux.Stringid(id,1)})
-	if sel==1 then
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,2)},
+		{b2,aux.Stringid(id,3)})
+	e:SetLabel(op)
+	if op==1 then
 		e:SetCategory(CATEGORY_DRAW)
 		e:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
-		e:SetOperation(s.drop)
 		Duel.SetTargetPlayer(tp)
 		Duel.SetTargetParam(1)
 		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-	else
+	elseif op==2 then
 		e:SetCategory(CATEGORY_DESTROY)
-		e:SetOperation(s.desop)
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+		e:SetProperty(EFFECT_FLAG_DELAY)
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
 	end
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-	if #g>0 then
-		Duel.Destroy(g,REASON_EFFECT)
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		--Draw 1 card
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Draw(p,d,REASON_EFFECT)
+	elseif op==2 then
+		--Destroy 1 card on the field
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
 	end
 end

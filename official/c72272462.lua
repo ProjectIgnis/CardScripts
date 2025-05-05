92d6c4666f25dcbfc95fe28327ada6b70ea5aa13
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-05-06 02:18:19 +0300

    Despian Quaeritis
    The ATK change itself should be unnegatable

--- official/c72272462.lua
+++ official/c72272462.lua
@@ -3,17 +3,18 @@
 --scripted by Rundas
 local s,id=GetID()
 function s.initial_effect(c)
-	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_DESPIA),s.matfilter)
 	c:EnableReviveLimit()
-	--Change the ATK of all monsters currently on the field to 0, except Level 8 or higher Fusion Monsters
+	--Fusion Materials: 1 "Despia" monster + 1 LIGHT or DARK monster
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_DESPIA),aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT|ATTRIBUTE_DARK))
+	--Change the ATK of all monsters currently on the field to 0 until the end of this turn, except Level 8 or higher Fusion Monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetHintTiming(0,TIMING_MAIN_END)
 	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMING_MAIN_END|TIMINGS_CHECK_MONSTER)
 	e1:SetCondition(function() return Duel.IsMainPhase() end)
 	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
@@ -26,61 +27,57 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_LEAVE_FIELD)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.thcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
+	e2:SetCondition(s.thspcon)
+	e2:SetTarget(s.thsptg)
+	e2:SetOperation(s.thspop)
 	c:RegisterEffect(e2)
 end
 s.listed_series={SET_DESPIA}
 s.listed_names={CARD_ALBAZ}
-function s.matfilter(c,fc,st,tp)
-	return c:IsAttribute(ATTRIBUTE_LIGHT,fc,st,tp) or c:IsAttribute(ATTRIBUTE_DARK,fc,st,tp)
-end
 function s.atkfilter(c)
-	return c:IsFaceup() and c:GetAttack()>0 and not (c:IsType(TYPE_FUSION) and c:IsLevelAbove(8))
+	return c:HasNonZeroAttack() and not (c:IsLevelAbove(8) and c:IsType(TYPE_FUSION))
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	if chk==0 then return #g>0 end
-	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,#g,PLAYER_ALL,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,#g,tp,0)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	if #g==0 then return end
 	local c=e:GetHandler()
 	for tc in g:Iter() do
-		--Change the ATK to 0 until the end of this turn
+		--Their ATK becomes 0 until the end of this turn
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
 		e1:SetValue(0)
 		e1:SetReset(RESETS_STANDARD_PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+function s.thspcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return rp==1-tp and c:IsReason(REASON_EFFECT) and c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp)
 end
-function s.thfilter(c,e,tp,ft)
-	return ((c:IsSetCard(SET_DESPIA) and c:IsMonster()) or c:IsCode(CARD_ALBAZ))
-		and (c:IsAbleToHand() or (c:IsCanBeSpecialSummoned(e,0,tp,false,false) and ft>0))
+function s.thspfilter(c,e,tp,mzone_chk)
+	return (c:IsSetCard(SET_DESPIA) or c:IsCode(CARD_ALBAZ)) and c:IsMonster() and (c:IsAbleToHand()
+		or (mzone_chk and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft) end
+function s.thsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thspfilter,tp,LOCATION_DECK,0,1,nil,e,tp,Duel.GetLocationCount(tp,LOCATION_MZONE)>0) end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+function s.thspop(e,tp,eg,ep,ev,re,r,rp)
+	local mzone_chk=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
-	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ft):GetFirst()
-	if tc then
-		aux.ToHandOrElse(tc,tp,function(c)
-			return tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and ft>0 end,
-		function(c)
-			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) end,
-		aux.Stringid(id,3))
-	end
+	local sc=Duel.SelectMatchingCard(tp,s.thspfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,mzone_chk):GetFirst()
+	if not sc then return end
+	aux.ToHandOrElse(sc,tp,
+		function() return mzone_chk and sc:IsCanBeSpecialSummoned(e,0,tp,false,false) end,
+		function() Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) end,
+		aux.Stringid(id,3)
+	)
 end

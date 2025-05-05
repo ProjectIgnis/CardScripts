35709ce6d9951365f4dfa30b25e326754c073cd4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-05-05 22:45:46 +0300

    Appliancer Test
    General modernisation + hint on the summoned monsters

--- official/c66380357.lua
+++ official/c66380357.lua
@@ -3,12 +3,12 @@
 --Anime version scripted by pyrQ, updated by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Special Summon as many Link-1 "Appliancer" monsters from your GY as possible to your zones 1 "Appliancer" Link Monster you control points to, but banish them during the End Phase
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
@@ -16,67 +16,31 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_APPLIANCER}
-function s.filter(c,e,tp)
-	return c:IsSetCard(SET_APPLIANCER) and c:IsLinkMonster() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,c)
+function s.tgfilter(c,e,tp)
+	return c:IsSetCard(SET_APPLIANCER) and c:IsLinkMonster() and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,c:GetFreeLinkedZone())
 end
-function s.spfilter(c,e,tp,fc)
-	local zone=fc:GetFreeLinkedZone()&ZONES_MMZ
-	return c:IsSetCard(SET_APPLIANCER) and c:IsLinkMonster() and c:IsLink(1)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
+function s.spfilter(c,e,tp,zone)
+	return c:IsLink(1) and c:IsSetCard(SET_APPLIANCER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
-	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp,g:GetFirst())
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,1,0,LOCATION_GRAVE)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local fid=e:GetHandler():GetFieldID()
 	local tc=Duel.GetFirstTarget()
-	local zone=tc:GetFreeLinkedZone()&ZONES_MMZ
-	local count=s.zone_count(zone)
-	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE,0,nil,e,tp,tc)
-	if #sg<count then count=#sg end
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then count=1 end
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and zone~=0 then
-		for i=0,count,1 do
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,tc):GetFirst()
-			if sc then
-				local zone=tc:GetFreeLinkedZone()&ZONES_MMZ
-				if zone>0 and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP,zone) then
-					sc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1,fid)
-				end
-			end
-		end
-		Duel.SpecialSummonComplete()
-		--banish them during this End Phase
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetCode(EVENT_PHASE+PHASE_END)
-		e1:SetLabel(fid)
-		e1:SetOperation(s.rmop)
-		e1:SetReset(RESET_PHASE|PHASE_END)
-		e1:SetCountLimit(1)
-		Duel.RegisterEffect(e1,tp)
+	if not (tc:IsRelateToEffect(e) and tc:IsFaceup()) then return end
+	local zone=tc:GetFreeLinkedZone()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)
+	if not (zone>0 and ft>0) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,ft,ft,nil,e,tp,zone)
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP,zone)>0 then
+		--Banish them during the End Phase
+		aux.DelayedOperation(g,PHASE_END,id,e,tp,function(ag) Duel.Remove(ag,POS_FACEUP,REASON_EFFECT) end,nil,nil,nil,aux.Stringid(id,1))
 	end
-end
-function s.rmfilter(c,e)
-	return c:GetFlagEffectLabel(id)==e:GetLabel()
-end
-function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_MZONE,0,nil,e)
-	if #g>0 then Duel.Remove(g,POS_FACEUP,REASON_EFFECT) end
-end
-function s.zone_count(z)
-	local c=0
-	while z>0 do
-		c=c+1
-		z=z&(z-1)
-	end
-	return c
 end

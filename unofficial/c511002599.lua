891553ea33a40ffd0309cf713ef7d6532c1996c7
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-05-14 21:41:03 -0700

    Update "Number 39: Utopia (Anime)

--- unofficial/c511002599.lua
+++ unofficial/c511002599.lua
@@ -3,35 +3,26 @@
 Duel.LoadCardScript("c84013237.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,4,2)
 	c:EnableReviveLimit()
-	--disable attack
+	--Xyz Summon procedure: 2 Level 4 monsters
+	Xyz.AddProcedure(c,nil,4,2)
+	--Cannot be destroyed by battle, except with "Number" monsters
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.condition)
-	e1:SetCost(s.cost)
-	e1:SetOperation(s.operation)
-	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
-	--battle indestructable
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+	c:RegisterEffect(e1)
+	--Negate the attack of 1 monster on the field
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e2:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
-	c:RegisterEffect(e2)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(function(e) return Duel.GetAttacker() and not e:GetHandler():IsStatus(STATUS_CHAINING) end)
+	e2:SetCost(Cost.Detach(1))
+	e2:SetOperation(function(e,tp,eg,ep,ev,re,r,rp) Duel.NegateAttack() end)
+	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
+	
 end
 s.listed_series={SET_NUMBER}
 s.xyz_number=39
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetAttacker() and not e:GetHandler():IsStatus(STATUS_CHAINING)
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateAttack()
-end

158cf37845a6bce84c872b6ffcec6cd3f2e431ff
Hatter <47074795+that-hatter@users.noreply.github.com>
2025-05-22 18:36:50 +0800

    fix "Night Dragolich"
    - should not affect monsters Special Summoned from locations other than the Main or Extra Deck
    - modernization updates

--- official/c88724332.lua
+++ official/c88724332.lua
@@ -2,28 +2,18 @@
 --Night Dragolich
 local s,id=GetID()
 function s.initial_effect(c)
-	--position change
+	--Change all non-Wyrm Attack Position monsters that were Special Summoned from the Main or Extra Deck to Defense Position
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SET_POSITION)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTarget(s.target)
+	e1:SetTarget(function(e,c) return c:IsFaceup() and not c:IsRace(RACE_WYRM) and c:IsSpecialSummoned() and c:IsSummonLocation(LOCATION_DECK|LOCATION_EXTRA) end)
 	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e1:SetValue(POS_FACEUP_DEFENSE)
 	c:RegisterEffect(e1)
-	--defense decrease
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
+	--All non-Wyrm monsters that were Special Summoned from the Main or Extra Deck lose DEF equal to their original DEF
+	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENSE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(s.target)
-	e2:SetValue(s.defval)
+	e2:SetValue(function(e,c) return -c:GetBaseDefense() end)
 	c:RegisterEffect(e2)
-end
-function s.target(e,c)
-	return c:IsFaceup() and not c:IsRace(RACE_WYRM) and c:IsSpecialSummoned() and (c:GetSummonLocation()&LOCATION_DECK|LOCATION_EXTRA)~=0
-end
-function s.defval(e,c)
-	return -c:GetBaseDefense()
 end

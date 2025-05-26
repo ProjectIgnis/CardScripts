f546cb30a5ac3ecabd951705f77ee6a9761c5e73
yoshiduels <yoshi.katreuvin@gmail.com>
2025-05-26 13:06:42 +0200

    added Botanical Lion

---
+++ rush/c160402052.lua
@@ -0,0 +1,27 @@
+--ボタニカル・ライオ
+--Botanical Lion
+local s,id=GetID()
+function s.initial_effect(c)
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
+	--control
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+	e2:SetCondition(s.condition)
+	c:RegisterEffect(e2)
+end
+function s.val(e,c)
+	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsRace,RACE_PLANT),c:GetControler(),LOCATION_MZONE,0,nil)*300
+end
+function s.condition(e)
+	return Duel.GetCurrentPhase()==PHASE_MAIN1
+end

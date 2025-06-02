f8818f059edc1f7468ca7c9deeda1f941e5040d2
Naim <naimsantos45@gmail.com>
2025-06-02 13:45:05 -0300

    "Battle Royal Mode - Joining" fix
    Fixed a bug in which the player would still be prompted to Special Summon a monster even when they did not have a zone available

--- official/c65433790.lua
+++ official/c65433790.lua
@@ -5,7 +5,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate by targeting 1 monster
 	aux.AddPersistentProcedure(c,PLAYER_ALL,aux.FaceupFilter(Card.IsType,TYPE_EFFECT))
-	--Cannot be destroyed by battle the first two times
+	--The first two times that monster would be destroyed by battle each turn, it is not destroyed
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
@@ -15,14 +15,14 @@ function s.initial_effect(c)
 	e1:SetTarget(aux.PersistentTargetFilter)
 	e1:SetValue(function(_,_,r) return r&REASON_BATTLE==REASON_BATTLE end)
 	c:RegisterEffect(e1)
-	--Destroying player gains 2000 LP
+	--When it is destroyed by battle, the player that destroyed it gains 2000 LP
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_LEAVE_FIELD)
 	e2:SetOperation(s.lpop)
 	c:RegisterEffect(e2)
-	--Special Summon 1 Level 4 or lower monster
+	--Special Summon 1 Level 4 or lower monster from your hand or Deck, but lose 2000 LP
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -44,9 +44,10 @@ function s.spfilter(c,e,tp)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e)
-		or not Duel.IsExistingMatchingCard(s.spfilter,ep,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,ep)
-		or not Duel.SelectYesNo(ep,aux.Stringid(id,1)) then return end
+	if not (c:IsRelateToEffect(e)
+		and Duel.GetLocationCount(ep,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,ep,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,ep)
+		and Duel.SelectYesNo(ep,aux.Stringid(id,1)) then return end
 	Duel.Hint(HINT_SELECTMSG,ep,HINTMSG_SPSUMMON)
 	local sg=Duel.SelectMatchingCard(ep,s.spfilter,ep,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,ep)
 	if #sg>0 and Duel.SpecialSummon(sg,0,ep,ep,false,false,POS_FACEUP)>0 then

fa402ee59c2d476c35d49d669322cf69648d815b
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-06-10 13:27:57 -0700

    Update "Tank Corps"
    -Tokens should be Level 4
    -Incorrect function order caused issues with activation

--- unofficial/c511000881.lua
+++ unofficial/c511000881.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCondition(function(e) return Duel.IsExistingMatchingCard(e:GetHandlerPlayer(),aux.FaceupFilter(Card.IsCode,511000880),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end)
+	e1:SetCondition(function(e) return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,511000880),e:GetHandlerPlayer(),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
@@ -15,13 +15,13 @@ end
 s.listed_names={511000880,id+1} --"KC-1 Crayton", "Tank Token"
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>2 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) 
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,500,1200,3,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE) end
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,500,1200,4,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,3,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<3 or 
-		not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,500,1200,3,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE)then return end
+		not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,500,1200,4,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE)then return end
 	for i=1,3 do
 		local token=Duel.CreateToken(tp,id+1)
 		if Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then

a301cb2b31a0eeae364048659f2fc5da739c4391
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-06-09 21:31:55 -0700

    Update "Tank Corps"
    - Should not target a "KC-1 Crayton", only check if it is on the field.
    - Defense parameter missing from special summon checks
    - Tokens should not be Tributable for a Tribute Summon
    - Script modernization update

--- unofficial/c511000881.lua
+++ unofficial/c511000881.lua
@@ -1,38 +1,40 @@
+--歴戦の戦車部隊
 --Tank Corps
 local s,id=GetID()
 function s.initial_effect(c)
-	--
+	--Special Summon 3 "Tank Tokens" while you control "KC-1 Crayton"
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(function(e) return Duel.IsExistingMatchingCard(e:GetHandlerPlayer(),aux.FaceupFilter(Card.IsCode,511000880),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-s.listed_names={511000880}
-function s.filter(c)
-	return c:IsFaceup() and c:IsCode(511000880)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) 
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>2 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) 
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,800,1200,3,RACE_MACHINE,ATTRIBUTE_EARTH) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+s.listed_names={511000880,id+1} --"KC-1 Crayton", "Tank Token"
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>2 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,500,1200,3,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,3,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>2 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) 
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,800,1200,3,RACE_MACHINE,ATTRIBUTE_EARTH) then
-		for i=1,3 do
-			local token=Duel.CreateToken(tp,id+1)
-			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<3 or 
+		not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,500,1200,3,RACE_MACHINE,ATTRIBUTE_EARTH,POS_FACEUP_DEFENSE)then return end
+	for i=1,3 do
+		local token=Duel.CreateToken(tp,id+1)
+		if Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+			--Cannot be Tributed for a Tribute Summon
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetDescription(3304)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+			e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+			e1:SetValue(1)
+			token:RegisterEffect(e1,true)
 		end
-		Duel.SpecialSummonComplete()
 	end
+	Duel.SpecialSummonComplete()
 end

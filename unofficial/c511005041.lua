3652ec70e9eb40da82b38a8bd807eb696bb6d5e2
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-06-11 09:04:18 -0700

    Update "Traplin"
    -Modified monster zone check to prevent being able to prompt player to Special Summon when all Main Monster Zones are full.
    -Slight script cleanup/polish

--- unofficial/c511005041.lua
+++ unofficial/c511005041.lua
@@ -3,7 +3,7 @@
 --By Shad3
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon itself from hand
+	--Special Summon itself from your hand by Tributing 1 Continuous Trap you control
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -14,16 +14,14 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
-function s.spfilter(c,tp)
-	return (c:IsTrap() and c:IsType(TYPE_CONTINUOUS)) and c:IsReleasable() and (not tp or Duel.GetMZoneCount(tp,c)>0)
-end
 function s.spcon(e,c)
 	if c==nil then return true end
-	local g=Duel.GetMatchingGroup(s.spfilter,c:GetControler(),LOCATION_ONFIELD,0,nil,tp)
-	return not Duel.IsPlayerAffectedByEffect(c:GetControler(),EFFECT_CANNOT_RELEASE) and #g>0
+	local g=Duel.GetMatchingGroup(aux.AND(Card.IsContinuousTrap,Card.IsReleasable),c:GetControler(),LOCATION_ONFIELD,0,nil)
+	return not Duel.IsPlayerAffectedByEffect(c:GetControler(),EFFECT_CANNOT_RELEASE) and #g>0 
+		and Duel.GetMZoneCount(e:GetHandlerPlayer())>0
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD,0,nil)
+	local g=Duel.GetMatchingGroup(aux.AND(Card.IsContinuousTrap,Card.IsReleasable),tp,LOCATION_ONFIELD,0,nil)
 	local sg=aux.SelectUnselectGroup(g,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_RELEASE,nil,nil,true)
 	local dg=sg:Filter(Card.IsFacedown,nil)
     	if #dg>0 then

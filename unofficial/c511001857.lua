374fbf12b30206869d8f389e4d65ca4e5ded4985
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-06-28 12:50:09 -0700

    Update c511001857.lua
    -Should only activate in response to the Spell Card in question
    -Script polish/modernization update

--- unofficial/c511001857.lua
+++ unofficial/c511001857.lua
@@ -2,36 +2,28 @@
 --Last Trick
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Activate
+	--Add 1 Spell Card your opponent activated to your hand instead of it going to the GY
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_DRAW_PHASE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return rp==1-tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsSpellCard()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ec=re:GetHandler()
+	if chk==0 then return ec:IsStatus(STATUS_LEAVE_CONFIRMED) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,ec,1,0,0)
+end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_CHAIN_ACTIVATING)
-	e1:SetOperation(s.regop)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local rc=re:GetHandler()
-	if rp==tp or not re:IsHasType(EFFECT_TYPE_ACTIVATE) or not re:IsActiveType(TYPE_SPELL) then return end
-	local e1=Effect.CreateEffect(e:GetOwner())
-	e1:SetCode(EVENT_LEAVE_FIELD_P)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCondition(function(e)return e:GetHandler():GetDestination()==LOCATION_GRAVE end)
-	e1:SetOperation(s.repop)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	rc:RegisterEffect(e1)
-end
-function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local p=e:GetOwner():GetOwner()
-	Duel.SendtoHand(c,p,REASON_EFFECT)
-	Duel.ConfirmCards(1-p,c)
+	local ec=re:GetHandler()
+	if ec and not ec:IsStatus(STATUS_DESTROY_CONFIRMED) then
+		ec:CancelToGrave()
+		Duel.SendtoHand(ec,tp,REASON_EFFECT)
+	end
 end

ce2146672dcecb3ad7f536aa8d6f855eb5db60ee
Naim <naimsantos45@gmail.com>
2025-06-29 16:21:08 -0300

    Update "Concours de Cuisine (Culinary Confrontation)"
    Fixed a bug where the player would be prevented for using materials for contact-like summons (which are not treated as the types listed)

--- official/c14283055.lua
+++ official/c14283055.lua
@@ -57,25 +57,16 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	--Cannot use monsters as material for a Fusion/Synchro/Xyz/Link Summon, except "Nouvelles" and "Patissciel" monsters
+	--You cannot use monsters as material for a Fusion, Synchro, Xyz, or Link Summon, except "Nouvelles" and "Patissciel" monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+	e1:SetCode(EFFECT_CANNOT_BE_MATERIAL)
 	e1:SetTargetRange(LOCATION_ALL,LOCATION_ALL)
 	e1:SetTarget(function(e,c) return not c:IsSetCard({SET_NOUVELLES,SET_PATISSCIEL}) end)
 	e1:SetValue(s.sumlimit)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
-	Duel.RegisterEffect(e2,tp)
-	local e3=e1:Clone()
-	e3:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
-	Duel.RegisterEffect(e3,tp)
-	local e4=e1:Clone()
-	e4:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
-	Duel.RegisterEffect(e4,tp)
 	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,2))
 	--Special Summon 1 "Nouvelles" monster and 1 "Patissciel" monster
 	local g1=Duel.GetMatchingGroup(s.cfilter,tp,locs,0,nil,SET_NOUVELLES)
@@ -91,9 +82,10 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SpecialSummonStep(sc2,0,tp,1-tp,false,false,POS_FACEUP)
 	Duel.SpecialSummonComplete()
 end
-function s.sumlimit(e,c)
-	if not c then return false end
-	return c:IsControler(e:GetHandlerPlayer())
+function s.sumlimit(e,sumc,sumtype,sumplayer)
+	if not sumc then return false end
+	local tp=e:GetHandlerPlayer()
+	return sumplayer==tp and sumc:IsControler(tp) and (sumtype&(SUMMON_TYPE_FUSION|SUMMON_TYPE_SYNCHRO|SUMMON_TYPE_XYZ|SUMMON_TYPE_LINK))>0
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local ct=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_GRAVE,LOCATION_GRAVE,nil,SET_RECIPE)

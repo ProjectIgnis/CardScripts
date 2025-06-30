5bc59cc4915c0a1c9ad9d3753c3846d74002e865
Hatter <47074795+that-hatter@users.noreply.github.com>
2025-07-01 04:32:47 +0800

    update "Tachyon Unit"
    * use new function for checking effects with a detach cost
    * modernization

--- unofficial/c511002571.lua
+++ unofficial/c511002571.lua
@@ -3,83 +3,57 @@
 Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Activate 1 "Tachyon" monster's effect that is activated by detaching its own Xyz Material(s)
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetCost(Cost.PayLP(500))
+	e1:SetTarget(s.efftg)
+	e1:SetOperation(s.effop)
 	c:RegisterEffect(e1)
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,500) end
-	Duel.PayLPCost(tp,500)
+function s.runfn(fn,eff,re,tp,chk)
+	return not fn or fn(eff,tp,Group.CreateGroup(),PLAYER_NONE,0,re,REASON_EFFECT,PLAYER_NONE,chk)
 end
-function s.filter(c,e,tp)
-	if c:IsFacedown() or not c:IsTachyon() or not c:IsHasEffect(id) then return false end
-	local eff={c:GetCardEffect(id)}
-	for _,teh in ipairs(eff) do
-		local te=teh:GetLabelObject()
-		if te:GetCode()&511001822==511001822 or te:GetLabel()==511001822 then te=te:GetLabelObject() end
-		local con=te:GetCondition()
-		local tg=te:GetTarget()
-		if (not con or con(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE))
-			and (not tg or tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then return true
-		end
+function s.efffilter(c,e,tp)
+	if c:IsFacedown() or not c:IsTachyon() then return false end
+	for _,eff in ipairs({c:GetOwnEffects()}) do
+		if eff:HasDetachCost() and s.runfn(eff:GetCondition(),eff,e,tp) and s.runfn(eff:GetTarget(),eff,e,tp,0) then return true end
 	end
 	return false
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.efffilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.efffilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SelectTarget(tp,s.efffilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		local eff={tc:GetCardEffect(id)}
-		local te=nil
-		local acd={}
-		local ac={}
-		for _,teh in ipairs(eff) do
-			local temp=teh:GetLabelObject()
-			if temp:GetCode()&511001822==511001822 or temp:GetLabel()==511001822 then temp=temp:GetLabelObject() end
-			local con=temp:GetCondition()
-			local tg=temp:GetTarget()
-			if (not con or con(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE))
-				and (not tg or tg(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,0)) then
-				table.insert(ac,teh)
-				table.insert(acd,temp:GetDescription())
-			end
-		end
-		if #ac==1 then te=ac[1] elseif #ac>1 then
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
-			op=Duel.SelectOption(tp,table.unpack(acd))
-			op=op+1
-			te=ac[op]
-		end
-		if not te then return end
-		local teh=te
-		te=teh:GetLabelObject()
-		if te:GetCode()&511001822==511001822 or te:GetLabel()==511001822 then te=te:GetLabelObject() end
-		local tg=te:GetTarget()
-		if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
-		Duel.BreakEffect()
-		tc:CreateEffectRelation(te)
-		Duel.BreakEffect()
-		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-		for etc in aux.Next(g) do
-			etc:CreateEffectRelation(te)
-		end
-		local operation=te:GetOperation()
-		if operation then operation(te,tp,Group.CreateGroup(),PLAYER_NONE,0,teh,REASON_EFFECT,PLAYER_NONE,1) end
-		tc:ReleaseEffectRelation(te)
-		for etc in aux.Next(g) do
-			etc:ReleaseEffectRelation(te)
+	if not tc:IsRelateToEffect(e) then return end
+	local effs={}
+	local options={}
+	for _,eff in ipairs({tc:GetOwnEffects()}) do
+		if eff:HasDetachCost() then
+			table.insert(effs,eff)
+			local eff_chk=s.runfn(eff:GetCondition(),eff,e,tp) and s.runfn(eff:GetTarget(),eff,e,tp,0)
+			table.insert(options,{eff_chk,eff:GetDescription()})
 		end
 	end
+	local op=#options==1 and 1 or Duel.SelectEffect(tp,table.unpack(options))
+	if not op then return end
+	local te=effs[op]
+	if not te then return end
+	Duel.ClearTargetCard()
+	s.runfn(te:GetTarget(),te,e,tp,1)
+	Duel.BreakEffect()
+	tc:CreateEffectRelation(te)
+	Duel.BreakEffect()
+	local tg=Duel.GetTargetCards(te)
+	tg:ForEach(Card.CreateEffectRelation,te)
+	s.runfn(te:GetOperation(),te,e,tp,1)
+	tg:ForEach(Card.ReleaseEffectRelation,te)
 end

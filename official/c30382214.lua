5899b2120f66cd7d9f7943bb32a46a9a323b703c
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-06-30 23:13:30 +0300

    "Flower Cardian" updates

--- official/c30382214.lua
+++ official/c30382214.lua
@@ -3,24 +3,24 @@
 --Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon itself from hand
+	--Special Summon this card from your hand
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
-	c:RegisterEffect(e1,false,EFFECT_MARKER_CARDIAN)
-	--Draw 1 and search or Special Summon
+	c:RegisterEffect(e1)
+	--Draw 1 card, and if you do, show it, then, if it is a "Flower Cardian" monster, you can take 1 "Flower Cardian" monster from your Deck, except "Flower Cardian Cherry Blossom", and either add it to your hand or Special Summon it
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DRAW+CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetCountLimit(1)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
 	e2:SetCost(s.drcost)
 	e2:SetTarget(s.drtg)
 	e2:SetOperation(s.drop)
@@ -28,42 +28,40 @@ function s.initial_effect(c)
 end
 s.listed_names={id}
 s.listed_series={SET_FLOWER_CARDIAN}
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_FLOWER_CARDIAN) and c:IsLevelBelow(2)
+function s.spconfilter(c)
+	return c:IsLevelBelow(2) and c:IsSetCard(SET_FLOWER_CARDIAN) and c:IsFaceup()
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(s.spconfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
+	--You cannot Normal or Special Summon monsters for the rest of this turn, except "Flower Cardian" monsters
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(s.splimit)
+	e1:SetTarget(function(e,c) return not c:IsSetCard(SET_FLOWER_CARDIAN) end)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	local e2=e1:Clone()
-	e2:SetCode(EFFECT_CANNOT_SUMMON)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	Duel.RegisterEffect(e2,tp)
-	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,2),nil)
 end
-function s.splimit(e,c)
-	return not c:IsSetCard(SET_FLOWER_CARDIAN)
-end
-s.cfilter=aux.FaceupFilter(Card.IsSetCard,SET_FLOWER_CARDIAN)
 function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsSetCard,1,false,nil,nil,SET_FLOWER_CARDIAN) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsSetCard,1,1,false,nil,nil,SET_FLOWER_CARDIAN)
 	Duel.Release(g,REASON_COST)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -75,32 +73,35 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 end
-function s.spfilter(c)
-	return c:IsSetCard(SET_FLOWER_CARDIAN) and not c:IsCode(id)
+function s.thspfilter(c,e,tp,mmz_chk)
+	return c:IsSetCard(SET_FLOWER_CARDIAN) and c:IsMonster() and not c:IsCode(id) and (c:IsAbleToHand()
+		or (mmz_chk and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	if Duel.Draw(p,d,REASON_EFFECT)~=0 then
-		local tc=Duel.GetOperatedGroup():GetFirst()
-		Duel.ConfirmCards(1-tp,tc)
-		if tc:IsMonster() and tc:IsSetCard(SET_FLOWER_CARDIAN) then
-			local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
-			if #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-				Duel.BreakEffect()
+	if Duel.Draw(p,d,REASON_EFFECT)>0 then
+		local dc=Duel.GetOperatedGroup():GetFirst()
+		Duel.ConfirmCards(1-tp,dc)
+		if dc:IsSetCard(SET_FLOWER_CARDIAN) and dc:IsMonster() then
+			local mmz_chk=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			local g=Duel.GetMatchingGroup(s.thspfilter,tp,LOCATION_DECK,0,nil,e,tp,mmz_chk)
+			if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-				local sc=sg:Select(tp,1,1,nil):GetFirst()
+				local sc=g:Select(tp,1,1,nil):GetFirst()
+				Duel.BreakEffect()
 				aux.ToHandOrElse(sc,tp,
-					function(c)
-						return sc:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+					function()
+						return mmz_chk and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
 					end,
-					function(c)
+					function()
 						return Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
 					end,
-					aux.Stringid(id,0)
+					aux.Stringid(id,4)
 				)
 			end
 		else
-			Duel.SendtoGrave(tc,REASON_EFFECT)
+			Duel.BreakEffect()
+			Duel.SendtoGrave(dc,REASON_EFFECT)
 		end
 		Duel.ShuffleHand(tp)
 	end
--- official/c89818984.lua
+++ official/c89818984.lua
@@ -3,76 +3,80 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--spsummon from hand
+	--Must first be Special Summoned (from your hand) by Tributing 1 Level 11 "Flower Cardian" monster, except "Flower Cardian Willow with Calligrapher"
+	local e0=Effect.CreateEffect(c)
+	e0:SetDescription(aux.Stringid(id,0))
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCondition(s.selfspcon)
+	e0:SetTarget(s.selfsptg)
+	e0:SetOperation(s.selfspop)
+	c:RegisterEffect(e0)
+	--Draw 1 card, and if you do, show it, then you can Special Summon it if it is a "Flower Cardian" monster
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetCondition(s.hspcon)
-	e1:SetTarget(s.hsptg)
-	e1:SetOperation(s.hspop)
-	c:RegisterEffect(e1,false,EFFECT_MARKER_CARDIAN)
-	--draw
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetTarget(s.drsptg)
+	e1:SetOperation(s.drspop)
+	c:RegisterEffect(e1)
+	--When this card is used as Synchro Material, you can treat it and all other Synchro Materials (that have a Level) as Level 2 monsters
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_DRAW+CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_SYNCHRO_MATERIAL_CUSTOM)
+	e2:SetOperation(s.synop)
 	c:RegisterEffect(e2)
-	--synchro
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_SYNCHRO_MATERIAL_CUSTOM)
-	e3:SetOperation(s.synop)
-	c:RegisterEffect(e3)
 end
 s.listed_series={SET_FLOWER_CARDIAN}
 s.listed_names={id}
-function s.hspfilter(c,tp)
-	return c:IsSetCard(SET_FLOWER_CARDIAN) and c:GetLevel()==11 and not c:IsCode(id)
+function s.selfspfilter(c)
+	return c:IsSetCard(SET_FLOWER_CARDIAN) and c:IsLevel(11) and not c:IsCode(id)
 end
-function s.hspcon(e,c)
+function s.selfspcon(e,c)
 	if c==nil then return true end
-	return Duel.CheckReleaseGroup(c:GetControler(),s.hspfilter,1,false,1,true,c,c:GetControler(),nil,false,nil)
+	local tp=c:GetControler()
+	return Duel.CheckReleaseGroup(tp,s.selfspfilter,1,false,1,true,c,tp,nil,false,nil)
 end
-function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.SelectReleaseGroup(tp,s.hspfilter,1,1,false,true,true,c,nil,nil,false,nil)
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local g=Duel.SelectReleaseGroup(tp,s.selfspfilter,1,1,false,true,true,c,nil,nil,false,nil)
 	if g then
 		g:KeepAlive()
 		e:SetLabelObject(g)
-	return true
+		return true
 	end
 	return false
 end
-function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.Release(g,REASON_COST)
 	g:DeleteGroup()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.drsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.drspop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	if Duel.Draw(p,d,REASON_EFFECT)~=0 then
-		local tc=Duel.GetOperatedGroup():GetFirst()
-		Duel.ConfirmCards(1-tp,tc)
-		Duel.BreakEffect()
-		if tc:IsMonster() and tc:IsSetCard(SET_FLOWER_CARDIAN) then
-			if tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-				and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-				and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	if Duel.Draw(p,d,REASON_EFFECT)>0 then
+		local dc=Duel.GetOperatedGroup():GetFirst()
+		Duel.ConfirmCards(1-tp,dc)
+		if dc:IsSetCard(SET_FLOWER_CARDIAN) and dc:IsMonster() then
+			if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and dc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+				and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+				Duel.BreakEffect()
+				Duel.SpecialSummon(dc,0,tp,tp,false,false,POS_FACEUP)
 			end
 		else
-			Duel.SendtoGrave(tc,REASON_EFFECT)
+			Duel.BreakEffect()
+			Duel.SendtoGrave(dc,REASON_EFFECT)
 		end
 		Duel.ShuffleHand(tp)
 	end

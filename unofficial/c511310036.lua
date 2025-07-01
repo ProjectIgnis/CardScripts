91cd664c8b32674fed40b2c1ca3d765661b11182
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-07-01 13:23:15 +0300

    "Allure Queen" updates

--- unofficial/c511310036.lua
+++ unofficial/c511310036.lua
@@ -2,109 +2,109 @@
 --Allure Palace
 --Scripted by AlphaKretin
 local s,id=GetID()
+local EFFECT_ALLURE_PALACE_ANIME=511310036
 function s.initial_effect(c)
-	--workaround for e4
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e0:SetCode(EVENT_ADJUST)
-	e0:SetCountLimit(1)
-	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_NO_TURN_RESET)
-	e0:SetRange(LOCATION_ALL)
-	e0:SetOperation(s.regop)
-	c:RegisterEffect(e0)
 	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
-	--atk&def
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetRange(LOCATION_FZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_ALLURE_QUEEN))
-	e2:SetValue(500)
-	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_UPDATE_DEFENSE)
-	c:RegisterEffect(e3)
-	--become ignition
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetCode(511310036) --hardcode
-	e4:SetRange(LOCATION_FZONE)
-	e4:SetTargetRange(LOCATION_MZONE,0)
-	e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_ALLURE_QUEEN))
-	c:RegisterEffect(e4)
-	--special summon
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(122520,0))
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e5:SetCode(EVENT_SUMMON_SUCCESS)
-	e5:SetRange(LOCATION_FZONE)
-	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e5:SetCondition(s.spcon)
-	e5:SetTarget(s.sptg)
-	e5:SetOperation(s.spop)
-	c:RegisterEffect(e5)
-	local e6=e5:Clone()
-	e6:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e6)
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--All "Allure Queen" monsters you control gain 500 ATK/DEF
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_FIELD)
+	e1a:SetCode(EFFECT_UPDATE_ATTACK)
+	e1a:SetRange(LOCATION_FZONE)
+	e1a:SetTargetRange(LOCATION_MZONE,0)
+	e1a:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_ALLURE_QUEEN))
+	e1a:SetValue(500)
+	c:RegisterEffect(e1a)
+	local e1b=e1a:Clone()
+	e1b:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e1b)
+	--Also, you can activate their effects that activate by sending themselves to the GY during your Main Phase
+	local e2a=Effect.CreateEffect(c)
+	e2a:SetType(EFFECT_TYPE_FIELD)
+	e2a:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2a:SetCode(EFFECT_ALLURE_PALACE_ANIME)
+	e2a:SetRange(LOCATION_FZONE)
+	e2a:SetTargetRange(1,0)
+	c:RegisterEffect(e2a)
+	local e2b=Effect.CreateEffect(c)
+	e2b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2b:SetCode(EVENT_ADJUST)
+	e2b:SetRange(LOCATION_FZONE)
+	e2b:SetOperation(s.regop)
+	c:RegisterEffect(e2b)
+	--Special Summon to your opponent's field, 1 monster with the same name as an "Allure Queen" monster that was Normal or Special Summoned to your field
+	local e3a=Effect.CreateEffect(c)
+	e3a:SetDescription(aux.Stringid(id,0))
+	e3a:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3a:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e3a:SetCode(EVENT_SUMMON_SUCCESS)
+	e3a:SetRange(LOCATION_FZONE)
+	e3a:SetCondition(s.spcon)
+	e3a:SetTarget(s.sptg)
+	e3a:SetOperation(s.spop)
+	c:RegisterEffect(e3a)
+	local e3b=e3a:Clone()
+	e3b:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3b)
 end
 s.listed_series={SET_ALLURE_QUEEN}
-function s.regop(e)
-	if s.global_check then return end
-	s.global_check = true
-	local g=Duel.GetMatchingGroup(Card.IsOriginalSetCard,0,LOCATION_ALL,LOCATION_ALL,nil,SET_ALLURE_QUEEN)
-	for tc in g:Iter() do
-		local effs={tc:GetCardEffect(511310036)}
-		for _,teh in ipairs(effs) do
-			local eff=teh:GetLabelObject()
-			local e1=eff:Clone()
-			e1:SetType(EFFECT_TYPE_IGNITION)
-			e1:SetCode(0)
-			e1:SetCondition(s.igcon)
-			tc:RegisterEffect(e1)
+function s.regop(e,tp)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_ALLURE_QUEEN),tp,LOCATION_MZONE,0,nil):Match(aux.NOT(Card.HasFlagEffect),nil,id)
+	if #g==0 then return end
+	for allure_card in g:Iter() do
+		local effs={allure_card:GetOwnEffects()}
+		for _,eff in ipairs(effs) do
+			if eff:GetCost()==Cost.SelfToGrave and eff:GetCode()&(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)>0 then
+				allure_card:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+				local condition=eff:GetCondition()
+				--Ignition Effect version of the "Allure Queen" monster's Trigger Effect
+				local e1=eff:Clone()
+				e1:SetType(EFFECT_TYPE_IGNITION)
+				e1:SetCode(0)
+				e1:SetCondition(function(e,tp) return Duel.IsPlayerAffectedByEffect(tp,EFFECT_ALLURE_PALACE_ANIME) and condition(e,tp) end)
+				e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+				allure_card:RegisterEffect(e1)
+			end
 		end
 	end
 end
-function s.eqfilter(c,code)
-	return c:GetFlagEffect(code)~=0
-end
-function s.igcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:IsHasEffect(511310036) and #c:GetEquipGroup():Filter(s.eqfilter,nil,c:GetOriginalCode())>0
-end
-function s.cfilter(c,tp)
-	return c:IsFaceup() and c:IsSetCard(SET_ALLURE_QUEEN) and c:IsControler(tp)
+function s.spconfilter(c,tp)
+	return c:IsSetCard(SET_ALLURE_QUEEN) and c:IsControler(tp) and c:IsFaceup()
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+	return eg:IsExists(s.spconfilter,1,nil,tp)
+end
+function s.tgfilter(c,e,tp)
+	return c:IsCanBeEffectTarget(e) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
 end
 function s.spfilter(c,e,tp,code)
 	return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,1-tp)
 end
-function s.tgfilter(c,e,tp,eg)
-	return c:IsFaceup() and eg:IsContains(c) and 
-		Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetCode())
-end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.tgfilter(chkc,e,tp,eg) end
-	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and 
-		Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,eg) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp,eg)
+	local g=eg:Filter(s.spconfilter,nil,tp):Match(s.tgfilter,nil,e,tp)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and g:IsContains(chkc) and s.tgfilter(chkc,e,tp) end
+	if chk==0 then return #g>0 and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 end
+	local tg=nil
+	if #g==1 then
+		tg=g
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		tg=g:Select(tp,1,1,nil)
+	end
+	Duel.SetTargetCard(tg)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if not tc:IsFaceup() or not tc:IsRelateToEffect(e) or
-		Duel.GetLocationCount(1-tp,LOCATION_MZONE)<1 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc:GetCode())
-	if #g>0 then
-		 Duel.SpecialSummon(g,0,tp,1-tp,false,false,POS_FACEUP)
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,tc:GetCode())
+		if #g>0 then
+			 Duel.SpecialSummon(g,0,tp,1-tp,false,false,POS_FACEUP)
+		end
 	end
 end

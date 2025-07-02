82fdec18d4335e8431c7963a92fbd736f7621038
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-07-02 13:48:18 +0300

    "tellarknight" updates

--- official/c38667773.lua
+++ official/c38667773.lua
@@ -2,38 +2,38 @@
 --Satellarknight Vega
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon 1 "tellarknight" monster from the hand
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
-	c:RegisterEffect(e1,false,EFFECT_MARKER_TELLAR)
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	c:RegisterEffect(e2)
-	local e3=e1:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e3)
+	--Special Summon 1 "tellarknight" monster from your hand, except "Satellarknight Vega"
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetDescription(aux.Stringid(id,0))
+	e1a:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1a:SetProperty(EFFECT_FLAG_DELAY)
+	e1a:SetCode(EVENT_SUMMON_SUCCESS)
+	e1a:SetCountLimit(1,id)
+	e1a:SetTarget(s.sptg)
+	e1a:SetOperation(s.spop)
+	c:RegisterEffect(e1a)
+	local e1b=e1a:Clone()
+	e1b:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e1b)
+	local e1c=e1a:Clone()
+	e1c:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e1c)
 end
 s.listed_series={SET_TELLARKNIGHT}
 s.listed_names={id}
-function s.filter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsSetCard(SET_TELLARKNIGHT) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end

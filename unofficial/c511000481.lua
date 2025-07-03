7ccc13e65f5a33fec2f70128712617d5a998c82f
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-02 21:18:34 -0700

    Update "Mirror of Duality"
    Should destroy all monsters on the field, and updated it to allow for destruction attempts on monsters that cannot be destroyed by card effects.

--- unofficial/c511000481.lua
+++ unofficial/c511000481.lua
@@ -1,32 +1,26 @@
+--闇の神
 --Mirror of Duality
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Pay 1000 LP to destroy all monsters on the field and inflict damage when an opponent's monster declares an attack
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetCost(s.cost)
-	e1:SetCondition(s.condition)
+	e1:SetCost(Cost.PayLP(1000))
+	e1:SetCondition(function(e) Duel.GetAttacker():IsControler(1-e:GetHandlerPlayer()) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,1000) end
-	Duel.PayLPCost(tp,1000)
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return tp~=Duel.GetTurnPlayer()
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,0,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,#g*100)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION,LOCATION_MZONE,nil)
 	if #g>0 then
 		local ct=Duel.Destroy(g,REASON_EFFECT)
 		Duel.Damage(1-tp,ct*500,REASON_EFFECT,true)

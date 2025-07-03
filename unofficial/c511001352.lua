13ec5769cd48f971d5d6545432bebd4797dea041
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-03 12:58:57 -0700

    Update "Insect Garden"
    Reworked code to better change control of monsters.

--- unofficial/c511001352.lua
+++ unofficial/c511001352.lua
@@ -1,3 +1,4 @@
+--虫媒花の園
 --Insect Garden
 local s,id=GetID()
 function s.initial_effect(c)
@@ -5,20 +6,21 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(function() Duel.AdjustInstantly() end)
 	c:RegisterEffect(e1)
-	--control
+	--Give control of all Level 4 or lower Insect monsters you control to your opponent
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_SET_CONTROL)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e2:SetTarget(s.filter)
-	e2:SetValue(s.ctval)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetOperation(s.ctrlop)
 	c:RegisterEffect(e2)
 end
-function s.filter(e,c)
-	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_INSECT) and c:GetOwner()==e:GetHandlerPlayer()
+function s.ctrlfilter(c)
+	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_INSECT) and c:IsControlerCanBeChanged()
 end
-function s.ctval(e,c)
-	return 1-e:GetHandlerPlayer() 
+function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.ctrlfilter,tp,LOCATION_MZONE,0,nil)
+	Duel.GetControl(g,1-tp)
 end

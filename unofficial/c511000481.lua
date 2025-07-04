83376ef955163d197954c77f3cb0d615733445c7
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-04 13:06:25 -0700

    Update "Mirror of Duality"
    Only opponent should take the damage.

--- unofficial/c511000481.lua
+++ unofficial/c511000481.lua
@@ -17,14 +17,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION,LOCATION_MZONE,1,nil) end
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,#g*100)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,#g*500)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION,LOCATION_MZONE,nil)
 	if #g>0 then
 		local ct=Duel.Destroy(g,REASON_EFFECT)
 		Duel.Damage(1-tp,ct*500,REASON_EFFECT,true)
-		Duel.Damage(tp,ct*100,REASON_EFFECT,true)
-		Duel.RDComplete()
 	end
 end

60806fa7cba02a9735162162c4da71e2207fe445
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-04 13:14:22 -0700

    Update "Explosive Urchin (Anime)"
    - Should damage in the next Standby Phase after activation, if it happens to be chained to a Trap Card activated by the opponent during the Standby Phase of the triggering player's turn
    - General script clean-up/modernization

--- unofficial/c511002915.lua
+++ unofficial/c511002915.lua
@@ -1,4 +1,5 @@
---爆弾ウニ－ボム・アーチン－
+--爆弾ウニ－ボム・アーチン－ (Anime)
+--Explosive Urchin (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,26 +7,24 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCondition(s.condition)
+	e1:SetOperation(function(e) e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY,0,1,Duel.GetTurnCount()) end)
 	c:RegisterEffect(e1)
-	--damage
+	--Inflict 1000 damage to your opponent during your next Standby Phase
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(52140003,0))
 	e2:SetCategory(CATEGORY_DAMAGE)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetCondition(s.damcon)
+	e2:SetCondition(function(e) return Duel.IsTurnPlayer(e:GetHandlerPlayer()) and e:GetHandler():GetFlagEffectLabel(id)~=Duel.GetTurnCount() end)
 	e2:SetTarget(s.damtg)
 	e2:SetOperation(s.damop)
 	c:RegisterEffect(e2)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsActiveType(TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE) and rp~=tp
-end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return tp==Duel.GetTurnPlayer()
+	return rp==1-tp and re:GetHandler():IsTrap() and re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

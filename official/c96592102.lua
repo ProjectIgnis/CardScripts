105dbc4c85302e2ebffa2a3856ae41f4691edbed
Hatter <47074795+that-hatter@users.noreply.github.com>
2025-07-05 22:21:03 +0800

    update some official cards to use Cost.Detach

--- official/c96592102.lua
+++ official/c96592102.lua
@@ -1,18 +1,18 @@
---RR－ブレード・バーナー・ファルコン
+--ＲＲ－ブレード・バーナー・ファルコン
 --Raidraptor - Blade Burner Falcon
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Xyz Summon Procedure
+	--Xyz Summon procedure: 2 Level 4 Winged Beast monsters
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WINGEDBEAST),4,2)
 	--Make this card gain 3000 ATK
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCondition(s.atkcon)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(function(e,tp) return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ) and Duel.GetLP(tp)<=Duel.GetLP(1-tp)-3000 end)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
 	--Destroy monsters your opponent controls
@@ -22,35 +22,20 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BATTLE_DESTROYING)
 	e2:SetCondition(aux.bdocon)
-	e2:SetCost(s.descost)
+	e2:SetCost(Cost.Detach(1,function(e,tp) return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE) end,function(e,og) e:SetLabel(#og) end))
 	e2:SetTarget(s.destg)
 	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 end
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetLP(tp)<=Duel.GetLP(1-tp)-3000 and e:GetHandler():GetSummonType()==SUMMON_TYPE_XYZ
-end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(3000)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
+		c:UpdateAttack(3000)
 	end
 end
-function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	local rt=math.min(Duel.GetMatchingGroupCount(nil,tp,0,LOCATION_MZONE,nil),c:GetOverlayCount())
-	local ct=c:RemoveOverlayCard(tp,1,rt,REASON_COST)
-	e:SetLabel(ct)
-end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return true end
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,e:GetLabel(),0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)

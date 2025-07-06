7f5e9c01f113a1444265b5230fc0cb3e44908915
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-07-06 20:36:54 +0300

    Update c85692042.lua

--- official/c85692042.lua
+++ official/c85692042.lua
@@ -8,42 +8,37 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,nil,4,2,nil,nil,Xyz.InfiniteMats)
 	--Special Summon 1 Level 4 "Mathmech" monster from your hand or GY
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,4))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCountLimit(1,{id,1})
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
 	e1:SetCost(s.spcost)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--Activate the appropriate effect based on the number of detached materials
+	--Activate the appropriate effect based on the number of materials detached
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCountLimit(1,id)
+	e2:SetCountLimit(1,{id,1})
 	e2:SetCondition(function(e) return e:GetHandler():IsXyzSummoned() end)
 	e2:SetCost(Cost.Choice(
-		{Cost.Detach(2),aux.Stringid(id,1),s.thcheck(s.mmfilter)},
-		{Cost.Detach(3),aux.Stringid(id,2),s.thcheck(s.l4filter)},
-		{Cost.Detach(4),aux.Stringid(id,3),s.thcheck(s.stfilter)}
+		{Cost.Detach(2),aux.Stringid(id,2),s.thcheck(s.mmfilter)},
+		{Cost.Detach(3),aux.Stringid(id,3),s.thcheck(s.l4filter)},
+		{Cost.Detach(4),aux.Stringid(id,4),s.thcheck(s.stfilter)}
 	))
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2,false,EFFECT_MARKER_DETACH_XMAT)
-
+	c:RegisterEffect(e2)
 end
 s.listed_series={SET_MATHMECH}
-function s.spcostfilter(c,ft,tp)
-	return (c:IsControler(tp) and Duel.GetMZoneCount(tp,c) or ft)>0
-end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.spcostfilter,1,false,nil,nil,ft,tp) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.spcostfilter,1,1,false,nil,nil,ft,tp)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,nil,1,false,aux.ReleaseCheckMMZ,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,nil,1,1,false,aux.ReleaseCheckMMZ,nil)
 	Duel.Release(g,REASON_COST)
 end
 function s.spfilter(c,e,tp)
@@ -81,7 +76,9 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local op=e:GetLabel()
-	local filter=op==1 and s.mmfilter or (op==2 and s.l4filter or s.stfilter)
+	local filter=(op==1 and s.mmfilter)
+		or (op==2 and s.l4filter)
+		or s.stfilter
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,filter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then

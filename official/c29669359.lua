57eb5a176edb9669d8cadd10710228d122c0906f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-07-06 23:24:40 +0300

    Some Xyz updates

--- official/c29669359.lua
+++ official/c29669359.lua
@@ -2,56 +2,51 @@
 --Number 61: Volcasaurus
 local s,id=GetID()
 function s.initial_effect(c)
-	--Must be properly summoned before reviving
 	c:EnableReviveLimit()
-	--Xyz summon procedure
+	--Xyz Summon procedure: 2 Level 5 monsters
 	Xyz.AddProcedure(c,nil,5,2)
-	--Destroy 1 of opponent's monsters, and if you do, inflict damage equal to its original ATK
+	--Destroy 1 face-up monster your opponent controls, and if you do, inflict damage to your opponent equal to the destroyed monster's original ATK
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetCountLimit(1)
+	e1:SetCost(Cost.AND(Cost.Detach(1),s.descost))
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 end
 s.xyz_number=61
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST)
-		and not e:GetHandler():IsDirectAttacked() end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-	--Cannot attack directly
-	local e1=Effect.CreateEffect(e:GetHandler())
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsDirectAttacked() end
+	--This card cannot attack your opponent directly the turn you activate this effect
+	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(3207)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
 	e1:SetReset(RESETS_STANDARD_PHASE_END)
-	e:GetHandler():RegisterEffect(e1)
+	c:RegisterEffect(e1)
 end
-function s.filter(c)
-	return c:IsFaceup()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-	local atk=g:GetFirst():GetTextAttack()
+	local tc=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	local atk=tc:GetTextAttack()
 	if atk<0 then atk=0 end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsControler(1-tp) then
+	if tc:IsRelateToEffect(e) and tc:IsMonster() and tc:IsControler(1-tp) then
 		local atk=tc:GetTextAttack()
 		if atk<0 then atk=0 end
-		if Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		if Duel.Destroy(tc,REASON_EFFECT)>0 then
 			Duel.Damage(1-tp,atk,REASON_EFFECT)
 		end
 	end

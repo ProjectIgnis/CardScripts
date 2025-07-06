b5a9588a1fd9fb40891c557364f74b7ccaad90ca
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-07-06 11:02:17 +0300

    Added new card scripts

---
+++ pre-release/c101302030.lua
@@ -0,0 +1,64 @@
+--斎キ狭依姫
+--Ichiki Sayori Hime
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:AddCannotBeSpecialSummoned()
+	Spirit.AddProcedure(c,EVENT_SUMMON_SUCCESS,EVENT_FLIP)
+	--Immediately after this effect resolves, Normal Summon this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.nscon)
+	e1:SetTarget(s.nstg)
+	e1:SetOperation(s.nsop)
+	c:RegisterEffect(e1)
+	--Add 1 Level 4 LIGHT or DARK monster with 800 ATK or DEF from your Deck to your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={id}
+function s.nsconfilter(c)
+	return (c:IsAttack(800) or c:IsDefense(800)) and c:IsFaceup()
+end
+function s.nscon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.nsconfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.nstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsSummonable(true,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,c,1,tp,0)
+end
+function s.nsop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.Summon(tp,c,true,nil)
+	end
+end
+function s.thfilter(c)
+	return c:IsLevel(4) and c:IsAttribute(ATTRIBUTE_LIGHT|ATTRIBUTE_DARK) and (c:IsAttack(800) or c:IsDefense(800))
+		and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

46d1a3f0d222c1f98a45f9bfe26de8b50b405115
Naim <naimsantos45@gmail.com>
2025-07-08 13:07:56 -0300

    "Synchro Fellows" update
    - It should not be able to target a level 1 monster
    - the player should only gain the additional Normal Summon if the monster had its level decreased

--- pre-release/c100348005.lua
+++ pre-release/c100348005.lua
@@ -52,7 +52,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.lvfilter(c)
-	return c:IsType(TYPE_SYNCHRO) and c:HasLevel() and c:IsFaceup()
+	return c:IsType(TYPE_SYNCHRO) and c:HasLevel() and c:IsLevelAbove(2) and c:IsFaceup()
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.lvfilter(chkc) end
@@ -65,7 +65,7 @@ end
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsLevelAbove(2) and not tc:IsImmuneToEffect(e) then
 		--Decrease its Level by 1
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
@@ -74,14 +74,15 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(-1)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		tc:RegisterEffect(e1)
+		Duel.BreakEffect()
+		--You can Normal Summon 1 "Synchron" monster during your Main Phase this turn, in addition to your Normal Summon/Set (you can only gain this effect once per turn)
+		aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,2))
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+		e2:SetTargetRange(LOCATION_HAND|LOCATION_MZONE,0)
+		e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_SYNCHRON))
+		e2:SetReset(RESET_PHASE|PHASE_END)
+		Duel.RegisterEffect(e2,tp)
 	end
-	aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,2))
-	--You can Normal Summon 1 "Synchron" monster during your Main Phase this turn, in addition to your Normal Summon/Set (you can only gain this effect once per turn)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-	e2:SetTargetRange(LOCATION_HAND|LOCATION_MZONE,0)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_SYNCHRON))
-	e2:SetReset(RESET_PHASE|PHASE_END)
-	Duel.RegisterEffect(e2,tp)
 end

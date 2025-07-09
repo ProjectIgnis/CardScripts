1ae19e5046d020528b58ac1298b21293858ec537
Naim <naimsantos45@gmail.com>
2025-07-09 09:08:58 -0300

    "Zexal Field" fix
    Fixed a bug where the effect reventing activations would be applied even when the Xyz monster targeted was not on the field

--- official/c95856586.lua
+++ official/c95856586.lua
@@ -8,14 +8,14 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--act limit
+	--Your opponent cannot activate cards or effects in response to the activation of your cards and effects that target an Xyz Monster you control
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetOperation(s.chainop)
 	c:RegisterEffect(e2)
-	--attach
+	--Attach 1 Xyz Monster from your Extra Deck or GY to a Xyz Monster(s) is Special Summoned to your field
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 	local g=Group.CreateGroup()
 	g:KeepAlive()
 	e3:SetLabelObject(g)
-	--Mass register
+	--Keep track of Xyz Monster(s) Special Summoned to your field
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -38,7 +38,7 @@ function s.initial_effect(c)
 	e4:SetLabelObject(e3)
 	e4:SetOperation(s.regop)
 	c:RegisterEffect(e4)
-	--excavate
+	--You can place 1 "Shining Draw" from your Deck on top of your Deck
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,1))
 	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -48,9 +48,9 @@ function s.initial_effect(c)
 	e5:SetOperation(s.operation)
 	c:RegisterEffect(e5)
 end
-s.listed_names={35906693}
+s.listed_names={35906693} --"Shining Draw" 
 function s.chfilter(c,tp)
-	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsControler(tp)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
 end
 function s.chainop(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
@@ -68,7 +68,7 @@ end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local tg=eg:Filter(s.mcfilter,nil,e,tp)
 	if #tg>0 then
-		for tc in aux.Next(tg) do
+		for tc in tg:Iter() do
 			tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
 		end
 		local g=e:GetLabelObject():GetLabelObject()
@@ -82,7 +82,8 @@ end
 function s.mtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=e:GetLabelObject():Filter(s.mcfilter,nil,e,tp)
 	if chkc then return g:IsContains(chkc) and s.mcfilter(chkc,e,tp) end
-	if chk==0 then return #g>0 and Duel.GetFlagEffect(tp,id)==0 and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE|LOCATION_EXTRA,0,1,nil,TYPE_XYZ) end
+	if chk==0 then return #g>0 and Duel.GetFlagEffect(tp,id)==0
+		and Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_GRAVE|LOCATION_EXTRA,0,1,nil,TYPE_XYZ) end
 	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
 	if #g==1 then
 		Duel.SetTargetCard(g:GetFirst())
@@ -94,7 +95,7 @@ function s.mtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.mop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if e:GetHandler():IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
 		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(Card.IsType),tp,LOCATION_GRAVE|LOCATION_EXTRA,0,1,1,tc,TYPE_XYZ)
 		if #g>0 then
@@ -106,7 +107,6 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsTurnPlayer(tp) and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_DECK,0,1,nil,35906693)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstMatchingCard(Card.IsCode,tp,LOCATION_DECK,0,nil,35906693)
 	if tc then
 		Duel.ShuffleDeck(tp)

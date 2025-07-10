26e999e83876b3c935efc9cbf7de3fdfbd4561ae
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-07-10 11:32:09 +0300

    Some more Xyz updates

--- official/c10389142.lua
+++ official/c10389142.lua
@@ -1,84 +1,52 @@
---No.42 スターシップ・ギャラクシー・トマホーク
+--Ｎｏ．４２ スターシップ・ギャラクシー・トマホーク
 --Number 42: Galaxy Tomahawk
 local s,id=GetID()
+local TOKEN_BATTLE_EAGLE=id+1
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,7,2)
 	c:EnableReviveLimit()
-	--token
+	--Xyz Summon procedure: 2 Level 7 monsters
+	Xyz.AddProcedure(c,nil,7,2)
+	--Special Summon as many "Battle Eagle Tokens" (Machine-Type/WIND/Level 6/ATK 2000/DEF 0) as possible, but destroy them during the End Phase of this turn, also your opponent takes no further battle damage this turn
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCountLimit(1)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(s.spcost)
+	e1:SetCountLimit(1)
+	e1:SetCost(Cost.Detach(2))
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
-s.listed_names={10389143}
 s.xyz_number=42
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
+s.listed_names={TOKEN_BATTLE_EAGLE}
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_BATTLE_EAGLE,0,TYPES_TOKEN,2000,0,6,RACE_MACHINE,ATTRIBUTE_WIND) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,TOKEN_BATTLE_EAGLE,0,TYPES_TOKEN,2000,0,6,RACE_MACHINE,ATTRIBUTE_WIND) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local sg=Group.CreateGroup()
+	for i=1,ft do
+		local token=Duel.CreateToken(tp,TOKEN_BATTLE_EAGLE)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		sg:AddCard(token)
+	end
+	--Destroy them during the End Phase of this turn
+	aux.DelayedOperation(sg,PHASE_END,id,e,tp,function(ag) Duel.Destroy(ag,REASON_EFFECT) end,nil,nil,1,aux.Stringid(id,1))
+	Duel.SpecialSummonComplete()
+	--Your opponent takes no further battle damage this turn
 	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetTargetRange(0,1)
 	e1:SetValue(1)
 	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,2000,0,6,RACE_MACHINE,ATTRIBUTE_WIND) end
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,ft,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ft,tp,0)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,2000,0,6,RACE_MACHINE,ATTRIBUTE_WIND) then return end
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	local fid=e:GetHandler():GetFieldID()
-	local g=Group.CreateGroup()
-	for i=1,ft do
-		local token=Duel.CreateToken(tp,id+1)
-		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
-		token:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1,fid)
-		g:AddCard(token)
-	end
-	Duel.SpecialSummonComplete()
-	g:KeepAlive()
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetReset(RESET_PHASE|PHASE_END)
-	e1:SetCountLimit(1)
-	e1:SetLabel(fid)
-	e1:SetLabelObject(g)
-	e1:SetCondition(s.descon)
-	e1:SetOperation(s.desop)
-	Duel.RegisterEffect(e1,tp)
-end
-function s.desfilter(c,fid)
-	return c:GetFlagEffectLabel(id)==fid
-end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetLabelObject()
-	if not g:IsExists(s.desfilter,1,nil,e:GetLabel()) then
-		g:DeleteGroup()
-		e:Reset()
-		return false
-	else return true end
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetLabelObject()
-	local tg=g:Filter(s.desfilter,nil,e:GetLabel())
-	g:DeleteGroup()
-	Duel.Destroy(tg,REASON_EFFECT)
 end

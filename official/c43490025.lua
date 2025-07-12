f473f97187310a7dbec3f01b1f20d2be37d695f8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-07-12 12:33:29 +0300

    Update c43490025.lua

--- official/c43490025.lua
+++ official/c43490025.lua
@@ -3,15 +3,15 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Xyz summon procedure: 2 Xyz Monsters with the same Rank, except "Number" monsters
-	Xyz.AddProcedure(c,s.xyzfilter,nil,2,s.ovfilter,aux.Stringid(id,1),nil,nil,false,s.xyzcheck)
-	--Gains 500 ATK per "Number" Xyz monsters in the GYs
+	--Xyz Summon procedure: 2 Xyz Monsters with the same Rank, except "Number" monsters OR 1 "Utopia" monster or "Number F0: Utopic Future" you control
+	Xyz.AddProcedure(c,s.xyzfilter,nil,2,s.ovfilter,aux.Stringid(id,0),nil,nil,false,s.xyzcheck)
+	--Gains 500 ATK for each "Number" Xyz Monster in the GYs
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(function(e,c) return Duel.GetMatchingGroupCount(s.atkfilter,c:GetControler(),LOCATION_GRAVE,LOCATION_GRAVE,nil)*500 end)
+	e1:SetValue(function(e,c) return Duel.GetMatchingGroupCount(function(c) return c:IsSetCard(SET_NUMBER) and c:IsType(TYPE_XYZ) end,0,LOCATION_GRAVE,LOCATION_GRAVE,nil)*500 end)
 	c:RegisterEffect(e1)
 	--Cannot be destroyed by battle
 	local e2=Effect.CreateEffect(c)
@@ -19,21 +19,21 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--Make this card able to make a second attack
+	--Make this card able to make a second attack during each Battle Phase this turn
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1)
 	e3:SetCondition(function() return Duel.IsAbleToEnterBP() end)
 	e3:SetCost(Cost.Detach(1))
-	e3:SetTarget(s.atktg)
-	e3:SetOperation(s.atkop)
+	e3:SetTarget(s.secondatktg)
+	e3:SetOperation(s.secondatkop)
 	c:RegisterEffect(e3)
 end
-s.listed_series={SET_NUMBER,SET_UTOPIA}
 s.xyz_number=0
-s.listed_names={65305468}
+s.listed_series={SET_NUMBER,SET_UTOPIA}
+s.listed_names={65305468} --"Number F0: Utopic Future"
 function s.xyzfilter(c,xyz,sumtype,tp)
 	return c:IsType(TYPE_XYZ,xyz,sumtype,tp) and not c:IsSetCard(SET_NUMBER,xyz,sumtype,tp)
 end
@@ -41,16 +41,16 @@ function s.xyzcheck(g,tp,xyz)
 	local mg=g:Filter(function(c) return not c:IsHasEffect(EFFECT_EQUIP_SPELL_XYZ_MAT) end,nil)
 	return mg:GetClassCount(Card.GetRank)==1
 end
-function s.ovfilter(c,tp,lc)
-	return c:IsFaceup() and (c:IsSetCard(SET_UTOPIA,lc,SUMMON_TYPE_XYZ,tp) or c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,65305468))
+function s.ovfilter(c,tp,xyzc)
+	return (c:IsSetCard(SET_UTOPIA,xyzc,SUMMON_TYPE_XYZ,tp) or c:IsSummonCode(xyzc,SUMMON_TYPE_XYZ,tp,65305468)) and c:IsFaceup()
 end
-function s.atkfilter(c)
-	return c:IsType(TYPE_XYZ) and c:IsSetCard(SET_NUMBER)
+function s.secondatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local c=e:GetHandler()
+		return c:CanAttack() and c:GetEffectCount(EFFECT_EXTRA_ATTACK)==0
+	end
 end
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetEffectCount(EFFECT_EXTRA_ATTACK)==0 end
-end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.secondatkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		--This card can make a second attack during each Battle Phase this turn

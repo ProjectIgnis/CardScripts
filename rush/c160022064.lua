51ba2ba5e4000eac2fab3222d4d1eb9c32b9c1a4
yoshiduels <yoshi.katreuvin@gmail.com>
2025-07-14 19:21:13 +0200

    fixed Partial Destruction of the Sacred Tower

--- rush/c160022064.lua
+++ rush/c160022064.lua
@@ -24,7 +24,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.cfilter,1,nil,tp)
 end
 function s.costfilter(c)
-	return c:IsFaceup() and c:IsAbleToGraveAsCost() and not c:IsMaximumModeSide()
+	return c:IsAbleToGraveAsCost() and not c:IsMaximumModeSide()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE,0,1,nil) 

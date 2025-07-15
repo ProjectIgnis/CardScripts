fbf2e6d17fcb900a8944b5a02c4603ffc141c0d1
Naim <naimsantos45@gmail.com>
2025-07-15 14:57:30 -0300

    "Speedlift" update
    Fixed a bug where the player(s) would be unable to activate cards and effects in a new chain, in response to a summon, even if the monster summoned by "Speedlift" was summoned in a chain link higher than 1

--- official/c36730805.lua
+++ official/c36730805.lua
@@ -30,33 +30,17 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	local tc=g:GetFirst()
-	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		e1:SetLabelObject(tc)
-		e1:SetOperation(s.sumop)
-		Duel.RegisterEffect(e1,tp)
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e2:SetCode(EVENT_CHAIN_END)
-		e2:SetLabelObject(e1)
-		e2:SetOperation(s.cedop)
-		Duel.RegisterEffect(e2,tp)
+	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if tc then
+		if Duel.GetCurrentChain()==1 then
+			--Neither player can activate cards or effects when it is summoned
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_CHAIN_END)
+			e1:SetCountLimit(1)
+			e1:SetOperation(function() Duel.SetChainLimitTillChainEnd(aux.FALSE) end)
+			Duel.RegisterEffect(e1,tp)
+		end
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
-	Duel.SpecialSummonComplete()
-end
-function s.sumop(e,tp,eg,ep,ev,re,r,rp)
-	if eg:IsContains(e:GetLabelObject()) then
-		e:SetLabel(1)
-		e:Reset()
-	else e:SetLabel(0) end
-end
-function s.cedop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.CheckEvent(EVENT_SPSUMMON_SUCCESS) and e:GetLabelObject():GetLabel()==1 then
-		Duel.SetChainLimitTillChainEnd(aux.FALSE)
-	end
-	e:Reset()
 end

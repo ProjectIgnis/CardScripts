582dd644945e301cd01f86f329947c2600a58bf7
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-15 19:07:46 -0700

    Update "Odd-Eyes Raging Dragon (Anime)"
    - Slight script polish
    - Missing delay flag
    - Missing category/opinfo in negation/destruction effect

--- unofficial/c511009340.lua
+++ unofficial/c511009340.lua
@@ -3,9 +3,10 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Pendulum.AddProcedure(c,false)
 	--Xyz Summon procedure: 2 Level 7 Dragon monstes
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_DRAGON),7,2)
+	--Pendulum procedure
+	Pendulum.AddProcedure(c,false)
 	--Place 1 Pendulum Monster from your Deck in your Pendulum Zone
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -30,7 +31,7 @@ function s.initial_effect(c)
 	--Negate the effects of all face-up Spells/Traps on the field, and if you do, destroy as many other cards on the field as possible, then this card gains 200 ATK for each card destroyed until the end of this turn
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+	e4:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
@@ -42,8 +43,8 @@ function s.initial_effect(c)
 	e5:SetRange(LOCATION_ONFIELD)
 	e5:SetTargetRange(LOCATION_ONFIELD,0)
 	e5:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e5:SetCondition(s.efcon)
-	e5:SetTarget(s.eftg)
+	e5:SetCondition(function(e) return e:GetHandler():IsXyzSummoned() and e:GetHandler():HasFlagEffect(id) end)
+	e5:SetTarget(function(e,c) return c=e:GetHandler() end)
 	e5:SetLabelObject(e3)
 	c:RegisterEffect(e5)
 	local e6=e5:Clone()
@@ -53,9 +54,9 @@ function s.initial_effect(c)
 	local e7=Effect.CreateEffect(c)
 	e7:SetDescription(aux.Stringid(id,2))
 	e7:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
-	e7:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e7:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e7:SetCode(EVENT_LEAVE_FIELD)
-	e7:SetCondition(s.pencon)
+	e7:SetCondition(function(e) local c=e:GetHandler() return c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousLocation(LOCATION_MZONE) end)
 	e7:SetTarget(s.pentg)
 	e7:SetOperation(s.penop)
 	c:RegisterEffect(e7)
@@ -76,35 +77,28 @@ function s.pcop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end
-function s.negfilter(c)
-	return c:IsSpellTrap() and c:IsFaceup() and not c:IsDisabled()
+end
+function s.matfilter(c,sc)
+	return c:IsType(TYPE_XYZ,sc,SUMMON_TYPE_XYZ,sc:GetSummonPlayer()) and c:IsXyzLevel(sc,7)
+end
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(s.matfilter,1,nil,c) then
+		c:RegisterFlagEffect(id,(RESET_EVENT|RESETS_STANDARD_DISABLE)&~RESET_TOFIELD,0,1)
+	end
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.negfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsNegatableSpellTrap),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if chk==0 then return #g>0 end
+	local dg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.negfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	for tc in aux.Next(g) do
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e2)
-		if tc:IsType(TYPE_TRAPMONSTER) then
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc:RegisterEffect(e3)
-		end
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsNegatableSpellTrap),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	for tc in g:Iter() do
+		tc:NegateEffects(c,nil,true)
 	end
 	local dg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,c)
 	local ct=Duel.Destroy(dg,REASON_EFFECT)
@@ -113,28 +107,10 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 		local e4=Effect.CreateEffect(c)
 		e4:SetType(EFFECT_TYPE_SINGLE)
 		e4:SetCode(EFFECT_UPDATE_ATTACK)
-		e4:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		e4:SetReset(RESETS_STANDARD_DISABLE_PHASE_END)
 		e4:SetValue(ct*200)
 		c:RegisterEffect(e4)
 	end
-end
-function s.matfilter(c,sc)
-	return c:IsType(TYPE_XYZ,sc,SUMMON_TYPE_XYZ,sc:GetSummonPlayer()) and c:IsXyzLevel(sc,7)
-end
-function s.valcheck(e,c)
-	local g=c:GetMaterial()
-	if g:IsExists(s.matfilter,1,nil,c) then
-		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD,0,1)
-	end
-end
-function s.efcon(e)
-	return e:GetHandler():IsXyzSummoned() and e:GetHandler():GetFlagEffect(id)>0
-end
-function s.eftg(e,c)
-	return c==e:GetHandler()
-end
-function s.pencon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousPosition(POS_FACEUP) and e:GetHandler():IsPreviousLocation(LOCATION_MZONE)
 end
 function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
@@ -143,7 +119,7 @@ function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.penop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetFieldGroup(tp,LOCATION_PZONE,0)
-	if Duel.Destroy(g,REASON_EFFECT)~=0 and e:GetHandler():IsRelateToEffect(e) then
+	if Duel.Destroy(g,REASON_EFFECT)>0 and e:GetHandler():IsRelateToEffect(e) then
 		Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
 end

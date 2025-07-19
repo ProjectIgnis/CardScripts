ff989c5c550c07f2b31f2648b2a9266d63aad011
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-19 15:16:34 -0700

    Fix "Dimension Dice (Anime)"
    - Field check was preventing card from being able to be activated
    - Script clean-up/polishing

--- unofficial/c316000124.lua
+++ unofficial/c316000124.lua
@@ -1,5 +1,5 @@
---ディメンジョン・ダイス
---Dimension Dice
+--ディメンジョン・ダイス (Anime)
+--Dimension Dice (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -7,17 +7,13 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMING_BATTLE_START)
-	e1:SetCondition(s.con)
+	e1:SetCondition(function(e) return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,11808215),e:GetHandlerPlayer(),LOCATION_FZONE,LOCATION_FZONE,1,nil) end)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={15744417,511001152}
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsEnvironment(511001152)
-end
+s.listed_names={11808215,15744417} --"Dice Dungeon,Orgoth the Relentless"
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
 	if chk==0 then return Duel.CheckReleaseGroupCost(tp,nil,2,nil,aux.ReleaseCheckMMZ,nil) end
@@ -25,7 +21,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(sg,REASON_COST)
 end
 function s.spfilter(c,e,tp)
-	return c:IsCode(15744417) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+	return c:IsCode(15744417) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
@@ -33,15 +29,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		e:SetLabel(0)
 		return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_DECK|LOCATION_HAND)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_HAND|LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc then
-		Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
-		tc:CompleteProcedure()
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

d9efa04f79fd007d9c80dc6e538011a331a16d8c
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-18 18:02:01 -0700

    Update "Cross Heart"
    -Should allow for control to be changed, regardless of battle damage infliction
    - Slight script modernization update

--- unofficial/c511001870.lua
+++ unofficial/c511001870.lua
@@ -1,27 +1,25 @@
+--交差する心
 --Cross Heart
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Take control of 1 opponent's monster that attacked you directly 
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_CONTROL)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_BATTLE_DAMAGE)
-	e1:SetCondition(s.condition)
+	e1:SetCode(EVENT_DAMAGE_STEP_END)
+	e1:SetCondition(function(e) return Duel.GetAttackTarget()==nil and Duel.GetAttacker():IsControler(1-e:GetHandlerPlayer()) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=eg:GetFirst()
+	local tc=Duel.GetAttacker()
 	if chk==0 then return tc:IsControlerCanBeChanged() and tc:IsControler(1-tp) end
 	Duel.SetTargetCard(tc)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,tc,1,0,0)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
+	local tc=Duel.GetAttacker()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.GetControl(tc,tp)
 	end

5369941225d8b4343f0dadfeb4d702f810af7bbc
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-24 20:09:04 -0700

    Update "Kasha (Manga)"
    - Missing special summon procedure
    - Slight script notation update

--- unofficial/c511002371.lua
+++ unofficial/c511002371.lua
@@ -3,35 +3,52 @@
 Duel.EnableUnofficialRace(RACE_YOKAI)
 local s,id=GetID()
 function s.initial_effect(c)
-	--todeck
+	--Special Summon this card from your hand if you control both "Mezuki" and "Gozuki"
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCondition(s.spcon)
+	c:RegisterEffect(e0)
+	--Return all other monsters on the field to the Deck, then this card's ATK becomes 1000 times the number of Yokai monsters returned by this effect
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TODECK)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetTarget(s.tdtg)
 	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)
 end
+s.listed_names={52467217,92826944} --"Gozuki, Mezuki"
+function s.spcon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,52467217),tp,LOCATION_MZONE,0,1,nil)
+	and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,92826944),tp,LOCATION_MZONE,0,1,nil)
+end
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
 end
-function s.rfilter(c)
-	return c:IsLocation(LOCATION_DECK|LOCATION_EXTRA) and c:IsRace(RACE_YOKAI) and (c:GetPreviousPosition()&POS_FACEUP)~=0
+function s.retfilter(c)
+	return c:IsLocation(LOCATION_DECK|LOCATION_EXTRA) and c:IsRace(RACE_YOKAI) and (c:GetPreviousPosition()&POS_FACEUP)>0
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
-	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-	local c=e:GetHandler()
-	local rt=Duel.GetOperatedGroup():FilterCount(s.rfilter,nil)
-	if rt>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetValue(rt*1000)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
+	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then
+		local c=e:GetHandler()
+		local retct=Duel.GetOperatedGroup():FilterCount(s.retfilter,nil)
+		if retct>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetValue(retct*1000)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+			c:RegisterEffect(e1)
+		end
 	end
 end

964ba7d2dc3fb96f10eb4910925e04b6060889b7
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-26 15:04:38 -0700

    Update "Clay Wrap"
    - Should send 1 Spell/Trap an opponent controls to the GY instead of destroying it. 
    - Effect of leaving the field should have the monster still on the field when it does so (following the rulings of Attachment Cybern, Archfiend Staff of Despair, Smoke Grenade of the Thief, etc)

--- unofficial/c511000998.lua
+++ unofficial/c511000998.lua
@@ -1,35 +1,37 @@
+--クレイラップ
 --Clay Wrap
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c,0,aux.FilterBoolFunction(Card.IsCode,84327329))
-	--destroy
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetCondition(s.descon)
-	e3:SetTarget(s.destg)
-	e3:SetOperation(s.desop)
-	c:RegisterEffect(e3)
+	--Send 1 Spell/Trap card your opponent controls to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetCondition(s.tgcon)
+	e1:SetTarget(s.tgtg)
+	e1:SetOperation(s.tgop)
+	c:RegisterEffect(e1)
 end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local ec=e:GetHandler():GetPreviousEquipTarget()
-	return e:GetHandler():IsReason(REASON_LOST_TARGET) and ec:IsLocation(LOCATION_GRAVE)
+s.listed_names={84327329} --"Elemental HERO Clayman"
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetPreviousLocation()==LOCATION_SZONE and not c:IsReason(REASON_LOST_TARGET)
 end
-function s.dfilter(c)
-	return c:IsSpellTrap() and c:IsDestructable()
+function s.tgfilter(c)
+	return c:IsSpellTrap() and c:IsAbleToGrave()
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(s.dfilter,tp,0,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	local g=Duel.GetMatchingGroup(s.tgfilter,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,tp,0)
 end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,s.dfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
 	if #g>0 then
 		Duel.HintSelection(g)
-		Duel.Destroy(g,REASON_EFFECT)
+		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
 end

b6168af0940bb5e4881a53a56e368aef6860ccf1
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-01 15:15:55 -0700

    Update "Common Sacrifice"
    -Added loop handling to more optimally (and correctly) select monsters to send to GY
    - Monsters should be sent via effect, not as cost
    - Script modernization update/cleanup

--- unofficial/c511007018.lua
+++ unofficial/c511007018.lua
@@ -4,11 +4,10 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
@@ -16,34 +15,32 @@ end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsTurnPlayer(tp) and Duel.IsMainPhase() and Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>=3
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g1=Duel.GetFieldGroup(tp,0,LOCATION_MZONE):GetMinGroup(Card.GetAttack)
-	local g2
-	if #g1==1 then g2=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,g1:GetFirst()):GetMinGroup(Card.GetAttack) end
-	if chk==0 then return g1 and ((#g1>1 and g1:IsExists(Card.IsAbleToGraveAsCost,2,nil))
-		or (#g1==1 and g2 and g1:GetFirst():IsAbleToGraveAsCost() and g2:GetFirst():IsAbleToGraveAsCost())) end
-	local g=g1
-	if #g1>2 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		g=g1:Select(tp,2,2,nil)
-	elseif #g1==1 then
-		g:Merge(g2)
-	end
-	Duel.SendtoGrave(g,REASON_COST)
+function s.resconfilter(c,atk)
+	return atk and c:GetAttack()<atk
 end
-function s.filter(c,e,tp)
+function s.rescon(sg,e,tp,mg)
+	local _,atk=sg:GetMaxGroup(Card.GetAttack)
+	return #sg==2 and not mg:IsExists(s.resconfilter,1,sg,atk)
+end
+function s.spfilter(c,e,tp)
 	return c:IsLevelAbove(7) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsAbleToGrave),tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsAbleToGraveAsCost),tp,0,LOCATION_MZONE,nil)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or #g==0 then return end
+	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE)
+	if Duel.SendtoGrave(tg,REASON_EFFECT)==2 and tg:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+		if #sg>0 then
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 end

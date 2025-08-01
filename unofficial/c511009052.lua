d6bcf4eb3adfbc567af1adb76ec64fa9cda3ba62
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-07-31 20:25:22 -0700

    Fix "D-Pressure"
    - Missing category/operation info
    - Cleaned up summon check/fixed being able to Summon monsters with '?' ATK
    - Script nomenclature update

--- unofficial/c511009052.lua
+++ unofficial/c511009052.lua
@@ -1,13 +1,14 @@
+--Ｄ－プレッシャー
 --D - Pressure
---fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 	aux.GlobalCheck(s,function()
 		local ge1=Effect.CreateEffect(c)
@@ -18,28 +19,28 @@ function s.initial_effect(c)
 	end)
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=eg:GetFirst()
-	while tc do
-		if tc:GetFlagEffect(id)==0 then
-			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	local g=eg:Filter(Card.IsSummonPlayer,nil,tp)
+	for tc in g:Iter() do
+		if not tc:HasFlagEffect(id) then 
+			tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
 		end
-		tc=eg:GetNext()
 	end
 end
-function s.filter(c,e,tp)
-	return c:IsFaceup() and c:IsSetCard(SET_DESTINY_HERO) and c:GetFlagEffect(id)>0 
+function s.sumtgfilter(c,e,tp)
+	return c:IsFaceup() and c:IsSetCard(SET_DESTINY_HERO) and c:HasFlagEffect(id) 
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,c:GetAttack(),e,tp)
 end
 function s.spfilter(c,atk,e,tp)
-	return c:IsSetCard(SET_DESTINY_HERO) and c:GetAttack()<=atk and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(SET_DESTINY_HERO) and c:IsAttackBelow(atk) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.sumtgfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.sumtgfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SelectTarget(tp,s.sumtgfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
 	if not tc or not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end

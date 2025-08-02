32033cf313937381ebb841f2bcf368750934b527
Naim <naimsantos45@gmail.com>
2025-08-02 18:24:25 -0300

    "Music Princess's Substitute" fix
    Fixed a bug where it could be activated even if it was the only card in the player's hand

--- rush/c160022043.lua
+++ rush/c160022043.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,e:GetHandler()) end
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end

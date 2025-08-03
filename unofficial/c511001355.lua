dc246db82517d64033d6001aff7dc9fbd5503a6c
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-03 13:35:24 -0700

    Update "Relationship"
    - Should only check for monsters destroy on the player's field
    - Cleaned up ATK check to determine appropriate ATK values
    - Script modernization update

--- unofficial/c511001355.lua
+++ unofficial/c511001355.lua
@@ -1,9 +1,8 @@
 --リレーションシップ
 --Relationship
---fixed by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--If a monster(s) you control was destroyed this turn: Special Summon 1 "Elemental HERO" monster from your Deck with ATK equal to the ATK 1 of those destroyed monsters had on the field
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -12,55 +11,45 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 	aux.GlobalCheck(s,function()
-		s[0]=Group.CreateGroup()
-		s[0]:KeepAlive()
-		s[1]=Group.CreateGroup()
-		s[1]:KeepAlive()
+		s[0]={}
+		s[1]={}
+		--Check for monsters destroyed during this turn
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 		ge1:SetCode(EVENT_DESTROYED)
 		ge1:SetOperation(s.checkop)
 		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetOperation(s.clear)
-		Duel.RegisterEffect(ge2,0)
+		aux.AddValuesReset(function()
+			s[0]={}
+			s[1]={}
+		end)
 	end)
 end
-function s.cfilter(c,atk)
-	return c:GetTextAttack()==atk
-end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(Card.IsMonster,nil)
-	local tc=g:GetFirst()
-	while tc do
-		local sg=Duel.GetMatchingGroup(s.cfilter,tp,0xff,0xff,nil,tc:GetTextAttack())
-		s[tc:GetPreviousControler()]:Merge(sg)
-		tc=g:GetNext()
+	local g=eg:Filter(Card.IsPreviousLocation,nil,LOCATION_MZONE)
+	for tc in g:Iter() do
+		if tc:IsPreviousControler(0) then
+        		table.insert(s[0],tc:GetPreviousAttackOnField())
+		end
+		if tc:IsPreviousControler(1) then
+			table.insert(s[1],tc:GetPreviousAttackOnField())
+		end
 	end
 end
-function s.clear(e,tp,eg,ep,ev,re,r,rp)
-	s[0]:Clear()
-	s[1]:Clear()
-end
-function s.filter(c,e,tp)
-	return c:IsControler(tp) and c:IsLocation(LOCATION_DECK) and c:IsSetCard(SET_ELEMENTAL_HERO) 
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter(c,e,tp,atk)
+	return c:IsSetCard(SET_ELEMENTAL_HERO) and c:IsAttack(s[tp]) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=s[tp]:Filter(s.filter,nil,e,tp)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp,s[tp])
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and #g>0 end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local g=s[tp]:Filter(s.filter,nil,e,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sg=g:Select(tp,1,1,nil)
+	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,s[tp])
 	if #sg>0 then
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end

bba71f38e99985783fa739a3962ba04534947cdc
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-04 10:53:42 -0700

    Update "Supreme King Z-ARC (Anime)"
    - Should summon up to 2 "Supreme King Dragon" monsters, not exactly 2
    - Fixed zone evaluation to properly handle new MR/Linked Zones for summoning evaluation
    - Script notation update
    - Slight script polish/modernization

--- unofficial/c511009441.lua
+++ unofficial/c511009441.lua
@@ -3,8 +3,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	--Pendulum Procedure
 	Pendulum.AddProcedure(c,false)
-	--fusion proc
+	--Fusion procedure - Treated as a Fusion Monster
 	local fe=Effect.CreateEffect(c)
 	fe:SetType(EFFECT_TYPE_SINGLE)
 	fe:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
@@ -13,30 +14,30 @@ function s.initial_effect(c)
 	c:RegisterEffect(fe)
 	s.min_material_count=0
 	s.max_material_count=0
-	--level/rank
+	--Treated as an Xyz Monster and has a Rank equal to its Level
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e0:SetCode(EFFECT_RANK_LEVEL_S)
 	c:RegisterEffect(e0)
-	--splimit
+	--Must be Special Summoned by the effect of "Astrograph Sorcerer"
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(s.splimit)
 	c:RegisterEffect(e1)
-	--Special Summon
+	--Special Summon this card from your Pendulum Zone by Tributing 1 "Supreme King" monster
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+    	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_PZONE)
-	e2:SetCost(s.spcost)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetCost(s.selfspcost)
+	e2:SetTarget(s.selfsptg)
+	e2:SetOperation(s.selfspop)
 	c:RegisterEffect(e2)
-	--destroy and damage
+	--If this card is Special Summoned: Destroy as many monsters your opponent controls as possible, then inflict damage to the controllers of the destroyed monsters.
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
@@ -47,7 +48,7 @@ function s.initial_effect(c)
 	e3:SetTarget(s.damtg)
 	e3:SetOperation(s.damop)
 	c:RegisterEffect(e3)
-	--indestructable/immune
+	--Cannot be destroyed by battle or the effects of your opponent's Fusion, Synchro or Xyz Monsters
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -57,14 +58,16 @@ function s.initial_effect(c)
 	e4:SetValue(1)
 	c:RegisterEffect(e4)
 	local e5=e4:Clone()
+	--Unaffected by card effects that would make it leave the field
 	e5:SetCode(EFFECT_IMMUNE_EFFECT)
 	e5:SetValue(s.imfilter)
 	c:RegisterEffect(e5)
+	--Cannot be used as Fusion or Ritual material (to better replicate total immunity)
 	local e6=e3:Clone()
 	e6:SetCode(EFFECT_CANNOT_BE_MATERIAL)
 	e6:SetValue(aux.cannotmatfilter(SUMMON_TYPE_FUSION,SUMMON_TYPE_RITUAL))
 	c:RegisterEffect(e6)
-	--immune to Fusion/Synchro/Xyz
+	--Monsters you control are unaffected by the effect of your opponent's Fusion, Synchro or Xyz Monsters
 	local e7=Effect.CreateEffect(c)
 	e7:SetType(EFFECT_TYPE_FIELD)
 	e7:SetCode(EFFECT_IMMUNE_EFFECT)
@@ -72,32 +75,32 @@ function s.initial_effect(c)
 	e7:SetTargetRange(LOCATION_MZONE,0)
 	e7:SetValue(s.efilter)
 	c:RegisterEffect(e7)
-	--special summon SK dragon
+	--If this card destroys an opponent's monster by battle: Special Summon up to 2 "Supreme King Dragon" monsters from your Extra Deck
 	local e8=Effect.CreateEffect(c)
 	e8:SetDescription(aux.Stringid(id,2))
 	e8:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e8:SetProperty(EFFECT_FLAG_DELAY)
 	e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e8:SetProperty(EFFECT_FLAG_DELAY)	
 	e8:SetCode(EVENT_BATTLE_DESTROYING)
 	e8:SetCondition(aux.bdocon)
-	e8:SetTarget(s.sptg2)
-	e8:SetOperation(s.spop2)
+	e8:SetTarget(s.spfromextg)
+	e8:SetOperation(s.spfromexop)
 	c:RegisterEffect(e8)
-	--destroy drawn
+	--If a card(s) is added to the hand, except during the Draw Phase: You can destroy that card(s)
 	local e9=Effect.CreateEffect(c)
 	e9:SetDescription(aux.Stringid(id,3))
 	e9:SetCategory(CATEGORY_DESTROY)
-	e9:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e9:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e9:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)	
 	e9:SetCode(EVENT_TO_HAND)
 	e9:SetRange(LOCATION_MZONE)
-	e9:SetCondition(s.descon)
+	e9:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return eg:IsExists(Card.IsControler,1,nil,1-tp) and not Duel.IsPhase(PHASE_DRAW) end)
 	e9:SetTarget(s.destg)
 	e9:SetOperation(s.desop)
 	c:RegisterEffect(e9)
 end
 s.listed_series={SET_SUPREME_KING,SET_SUPREME_KING_DRAGON}
-s.listed_names={76794549}
+s.listed_names={76794549} --"Astrograph Sorcerer"
 function s.fscon(e,g,gc,chkfnf)
 	if g==nil then return true end
 	return false
@@ -106,20 +109,18 @@ function s.splimit(e,se,sp,st)
 	local code=Duel.GetChainInfo(0,CHAININFO_TRIGGERING_CODE)
 	return se:GetHandler():IsCode(76794549) or code==76794549
 end
-function s.cfilter(c,ft,tp)
-	return c:IsSetCard(SET_SUPREME_KING) and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5)) and (c:IsControler(tp) or c:IsFaceup())
-end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.selfspcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsSetCard,1,false,aux.ReleaseCheckMMZ,nil,SET_SUPREME_KING) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local g=Duel.SelectReleaseGroupCost(tp,Card.IsSetCard,1,1,false,aux.ReleaseCheckMMZ,nil,SET_SUPREME_KING)
 	Duel.Release(g,REASON_COST)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
@@ -128,16 +129,18 @@ end
 function s.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:GetAttackAnnouncedCount()==0 end
+	--Cannot attack the turn you activate this effect
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(3206)
 	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH)
-	e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+	e1:SetReset(RESETS_STANDARD_PHASE_END)
 	c:RegisterEffect(e1,true)
 end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
-	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,e:GetHandler())
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	local sg=Duel.GetMatchingGroup(aux.TRUE,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,PLAYER_ALL,0)
 end
@@ -169,40 +172,65 @@ function s.imfilter(e,te)
 		or s.leaveChk(c,CATEGORY_TODECK) or s.leaveChk(c,CATEGORY_RELEASE) or s.leaveChk(c,CATEGORY_TOGRAVE))
 end
 function s.efilter(e,te)
-	return te:IsActiveType(TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+	return te:IsActiveType(TYPE_FUSION|TYPE_SYNCHRO|TYPE_XYZ) and te:GetOwnerPlayer()~=e:GetHandlerPlayer()
 end
-function s.spfilter(c,e,tp,rp)
-	if c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp,rp,nil,c)<=0 then return false end
-	return c:IsSetCard(SET_SUPREME_KING_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfromexfilter(c,e,tp)
+	if not (c:IsSetCard(SET_SUPREME_KING_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)) then return false end
+	return c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
 end
-function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return loc~=0 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,2,nil,e,tp,rp)
+function s.spfromextg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfromexfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,rp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.exmmzfilter(c)
+	return c:IsLocation(LOCATION_EXTRA) and c:IsFacedown() and c:IsType(TYPE_FUSION|TYPE_SYNCHRO|TYPE_XYZ)
+end
+function s.exlinkmzfilter(c)
+	return c:IsLocation(LOCATION_EXTRA) and (c:IsType(TYPE_LINK) or (c:IsFaceup() and c:IsType(TYPE_PENDULUM)))
+end
+function s.rescon(ft2,ft3,ft4,ft)
+	return function(sg,e,tp,mg)
+		local exnpct=sg:FilterCount(s.exmmzfilter,nil,LOCATION_EXTRA)
+		local expct=sg:FilterCount(s.exlinkmzfilter,nil,LOCATION_EXTRA)
+		local exct=sg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+		local res=ft3>=exnpct and ft4>=expct
+		return res,not res
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_EXTRA)
 end
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,2,2,nil,e,tp,rp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+function s.spfromexop(e,tp,eg,ep,ev,re,r,rp)
+	local ft2=Duel.GetLocationCountFromEx(tp)
+	local ft3=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_FUSION|TYPE_SYNCHRO|TYPE_XYZ)
+	local ft4=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_PENDULUM|TYPE_LINK)
+	local ft=math.min(Duel.GetMZoneCount(tp),2)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then
+		if ft2>0 then ft2=1 end
+		if ft3>0 then ft3=1 end
+		if ft4>0 then ft4=1 end
+		ft=1
 	end
-end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()~=PHASE_DRAW and eg:IsExists(Card.IsControler,1,nil,1-tp)
+	local ect=aux.CheckSummonGate(tp)
+	if ect then
+		ft2=math.min(ect,ft2)
+		ft3=math.min(ect,ft3)
+		ft4=math.min(ect,ft4)
+	end
+	local loc=0
+	if ft2>0 or ft3>0 or ft4>0 then loc=loc|LOCATION_EXTRA end
+	if loc==0 then return end
+	local sg=Duel.GetMatchingGroup(s.spfromexfilter,tp,loc,0,nil,e,tp)
+	if #sg==0 then return end
+	local rg=aux.SelectUnselectGroup(sg,e,tp,1,ft,s.rescon(ft2,ft3,ft4,ft),1,tp,HINTMSG_SPSUMMON)
+	Duel.SpecialSummon(rg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetTargetCard(eg)
 	local g=eg:Filter(Card.IsControler,nil,1-tp)
+	if chk==0 then return #g>0 end
+	Duel.SetTargetCard(g)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
-function s.desfilter(c,e,tp)
-	return c:IsRelateToEffect(e) and c:IsControler(1-tp)
-end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=eg:Filter(s.desfilter,nil,e,tp)
+	local g=eg:Filter(Card.IsControler,nil,1-tp):Match(Card.IsRelateToEffect,nil,e)
 	if #g>0 then
-		Duel.Destroy(g,REASON_EFFECT)
+        	Duel.Destroy(g,REASON_EFFECT)
 	end
 end

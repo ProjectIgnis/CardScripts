b22d6cc08f7de84f262f4314317b0d795f675d00
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-05 17:50:26 -0700

    Update "Performapal Curtain Call"
    - Should be activatable even if no "Performapal" monsters were destroyed prior
    - Script clean-up and modernization

--- unofficial/c511001254.lua
+++ unofficial/c511001254.lua
@@ -1,97 +1,76 @@
+--ＥＭアフター・エンドロール
 --Performapal Curtain Call
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Target 1 monster you control; apply the following effects
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_DAMAGE_STEP)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function() return not (Duel.IsPhase(PHASE_DAMAGE) or Duel.IsDamageCalculated()) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--Global register for "Performapal" monsters destroyed this turn
 	aux.GlobalCheck(s,function()
 		s[0]=0
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 		ge1:SetCode(EVENT_DESTROYED)
 		ge1:SetOperation(s.regop)
 		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetOperation(s.clear)
-		Duel.RegisterEffect(ge2,0)
+		aux.AddValuesReset(function()
+			s[0]=0
+		end)
 	end)
 end
-function s.filter(c)
+function s.regfilter(c)
 	return c:IsMonster() and c:IsSetCard(SET_PERFORMAPAL) and c:IsPreviousLocation(LOCATION_MZONE)
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	if not eg then return end
-	local ct=eg:FilterCount(s.filter,nil)
-	s[0]=s[0]+ct
-end
-function s.clear(e,tp,eg,ep,ev,re,r,rp)
-	s[0]=0
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+	local regct=eg:FilterCount(s.regfilter,nil)
+	if regct==0 then return end
+	s[0]=s[0]+regct
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) 
-		and s[0]>0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetValue(s[0]*600)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-		e2:SetValue(1)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e2)
-		local e3=e2:Clone()
-		e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		--Gains 600 ATK for each "Performapal" monster destroyed this turn
+		tc:UpdateAttack(s[0]*600,RESETS_STANDARD_PHASE_END)
+		--Cannot be destroyed by battle or card effects
+		local e2a=Effect.CreateEffect(c)
+		e2a:SetType(EFFECT_TYPE_SINGLE)
+		e2a:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e2a:SetValue(1)
+		e2a:SetReset(RESETS_STANDARD_PHASE_END)
+		tc:RegisterEffect(e2a)
+		local e2b=e2a:Clone()
+		e2b:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		tc:RegisterEffect(e2b)
+		--If this card attacks, negate the effects of all cards your opponent controls
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+		e3:SetOperation(s.atkop)
+		e3:SetReset(RESETS_STANDARD_PHASE_END)
 		tc:RegisterEffect(e3)
-		local e4=Effect.CreateEffect(c)
-		e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-		e4:SetCode(EVENT_ATTACK_ANNOUNCE)
-		e4:SetOperation(s.atkop)
-		e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e4)
 	end
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,id)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_ONFIELD,nil)
-	local tc=g:GetFirst()
+	local g=Duel.GetMatchingGroup(Card.IsNegatable,tp,0,LOCATION_ONFIELD,nil)
 	local c=e:GetHandler()
-	while tc do
-		local e1=Effect.CreateEffect(e:GetOwner())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(e:GetOwner())
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e2)
-		tc=g:GetNext()
+	for tc in g:Iter() do
+		tc:NegateEffects(c)
 	end
 end

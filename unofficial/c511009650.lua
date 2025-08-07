890a842ce76b70109e7f32b21c68dae7c997e431
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-06 19:28:11 -0700

    Update "Devil Mirage"
    - Was incorrectly filtering only the triggering player's cards
    - Script modernization/clean-up
    - Fixed opinfo to accurately pass information

--- unofficial/c511009650.lua
+++ unofficial/c511009650.lua
@@ -1,7 +1,8 @@
+--魔鏡の呪い
 --Devil Mirage
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--If a card or effect is activated that would destroy a card(s): Negate that effect, then destroy all cards your opponent controls
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -11,30 +12,26 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x572}
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x572)
-end
-function s.filter(c,p)
-	return c:GetControler()==p and c:IsOnField()
-end
+s.listed_series={0x572} --"Mirror Imagine" archetype
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.IsChainNegatable(ev) then return false end
+	if re:IsHasCategory(CATEGORY_NEGATE) and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
 	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
-	return ex and tg~=nil and tc+tg:FilterCount(s.filter,nil,tp)-#tg>0
-	and Duel.IsExistingMatchingCard(s.cfilter,e:GetHandler():GetControler(),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x572),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+		local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=re:GetHandler()
-	if not tc:IsDisabled() then
-		if Duel.NegateEffect(ev) and tc:IsRelateToEffect(re) then
+	local rc=re:GetHandler()
+	if not rc:IsDisabled() then
+		if Duel.NegateEffect(ev) and rc:IsRelateToEffect(re) then
 			local g=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD)
 			Duel.Destroy(g,REASON_EFFECT)
 		end

0cf47f1d4b93768cebe22aead540a48d1836451e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-07 18:17:35 -0700

    Update "Light Dragon @Ignister (Anime)"
    Script modernization update and some slight error corrections.

--- unofficial/c511600298.lua
+++ unofficial/c511600298.lua
@@ -3,29 +3,30 @@
 --Scripted by Larry126
 local s,id,alias=GetID()
 function s.initial_effect(c)
-	alias=c:GetOriginalCodeRule()
-	--xyz summon
-	Xyz.AddProcedure(c,nil,4,2,nil,nil,nil,nil,false,s.xyzcheck)
 	c:EnableReviveLimit()
-	--destroy
+	--Alias to interact with the OCG version of "Light Dragon @Ignister" count limit
+	alias=c:GetOriginalCodeRule()
+	--Xyz Summon procedure: 2 Level 4 monsters, including a LIGHT monster
+	Xyz.AddProcedure(c,nil,4,2,nil,nil,nil,nil,false,s.xyzcheck)
+	--Destroy monsters your opponent controls, up to the number of other "@Ignister" monsters you control
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(alias,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,alias)
 	e1:SetCost(Cost.DetachFromSelf(1))
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
-	--summon
+	--Special Summon 1 "@Ignister" Link Monster from your GY when another "@Ignister" monster you control inflicts damage to your opponent
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(alias,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_BATTLE_DAMAGE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_BATTLE_DAMAGE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{alias,1})
 	e2:SetCondition(s.spcon)
@@ -33,7 +34,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x135}
+s.listed_series={SET_IGNISTER}
 function s.xyzfilter(c,xyz,tp)
 	return c:IsAttribute(ATTRIBUTE_LIGHT,xyz,SUMMON_TYPE_XYZ,tp)
 end
@@ -41,34 +42,31 @@ function s.xyzcheck(g,tp,xyz)
 	local mg=g:Filter(function(c) return not c:IsHasEffect(EFFECT_EQUIP_SPELL_XYZ_MAT) end,nil)
 	return mg:IsExists(s.xyzfilter,1,nil,xyz,tp)
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_IGNISTER)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,0,nil),nil)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsSetCard,SET_IGNISTER),tp,LOCATION_MZONE,0,nil),nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,1-tp,LOCATION_MZONE)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	Duel.Destroy(g,REASON_EFFECT)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
-	return ep~=tp and tc:IsControler(tp) and tc:IsSetCard(SET_IGNISTER) and tc~=e:GetHandler()
+	return ep==1-tp and tc:IsControler(tp) and tc:IsSetCard(SET_IGNISTER) and tc~=e:GetHandler()
 end
 function s.spfilter(c,e,tp)
 	return c:IsLinkMonster() and c:IsSetCard(SET_IGNISTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()

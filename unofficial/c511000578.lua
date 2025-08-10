52bcd2104952057d76985893af60abbc448cd43d
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-10 14:46:24 -0700

    Update "Necro Shot"
    - Should be able to banish from either GY
    - Script modernization update

--- unofficial/c511000578.lua
+++ unofficial/c511000578.lua
@@ -1,30 +1,29 @@
 --ネクロショット
 --Necro Shot
---fixed by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	--equip
-	aux.AddEquipProcedure(c,nil,s.filter)
-	--banish
+	--Equip only to a DARK Warrior monster
+	aux.AddEquipProcedure(c,nil,s.eqfilter)
+	--Banish 1 monster from either GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(10691144,2))
 	e1:SetCategory(CATEGORY_REMOVE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetTarget(s.rmtg)
 	e1:SetOperation(s.rmop)
 	c:RegisterEffect(e1)
-	--selfdes
+	--Destroy this card after using the above effect 3 times
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EFFECT_SELF_DESTROY)
+	e2:SetRange(LOCATION_SZONE)
 	e2:SetCondition(s.descon)
 	e2:SetLabelObject(e1)
 	c:RegisterEffect(e2)
-	--to grave
+	--When this card is sent to the GY: Send 1 monster from your Deck to the GY
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(27632240,0))
 	e3:SetCategory(CATEGORY_TOGRAVE)
@@ -34,19 +33,19 @@ function s.initial_effect(c)
 	e3:SetOperation(s.tgop)
 	c:RegisterEffect(e3)
 end
-function s.filter(c)
+function s.eqfilter(c)
 	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_WARRIOR)
 end
 function s.rmfilter(c)
-	return c:IsMonster() and c:IsAbleToRemove() and aux.SpElimFilter(c)
+	return c:IsMonster() and c:IsAbleToRemove() and aux.SpElimFilter(c,true)
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE|LOCATION_GRAVE) and chkc:IsControler(1-tp) and s.rmfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,0,LOCATION_MZONE|LOCATION_GRAVE,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE|LOCATION_GRAVE) and s.rmfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,s.rmfilter,tp,0,LOCATION_MZONE|LOCATION_GRAVE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.rmfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
-	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
 end
 function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
@@ -55,10 +54,8 @@ function s.rmop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.descon(e)
-	return e:GetHandler():GetFlagEffect(id)>=3 and not e:GetHandler():IsRelateToEffect(e:GetLabelObject())
-end
-function s.tgfilter(c)
-	return c:IsMonster() and c:IsAbleToGrave()
+	local c=e:GetHandler()
+	return c:HasFlagEffect(id,3) and not c:IsRelateToEffect(e:GetLabelObject())
 end
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -66,7 +63,7 @@ function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,aux.AND(Card.IsMonster,Card.IsAbleToGrave),tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end

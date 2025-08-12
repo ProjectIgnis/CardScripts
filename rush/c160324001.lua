550b4a28328dcead8c234fa3f3f3a79004b18a24
yoshiduels <yoshi.katreuvin@gmail.com>
2025-08-12 09:01:08 +0200

    fixed Caius the Shadow Monarch

--- rush/c160324001.lua
+++ rush/c160324001.lua
@@ -40,5 +40,17 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		if tc:IsPreviousPosition(POS_FACEUP) and tc:GetOriginalAttribute()==ATTRIBUTE_DARK then
 			Duel.Damage(1-tp,1000,REASON_EFFECT)
 		end
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(aux.Stringid(id,1))
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(s.aclimit)
+		e1:SetReset(RESET_PHASE|PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
+end
+function s.aclimit(e,re,tp)
+	return re:GetHandler():IsCode(9748752)
 end

2a0364c3335cf4958afe195127e1bf4b9f067dd4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-15 12:03:51 -0700

    Update "Joruri-P.U.N.K. Dangerous Gabu"
    -Prevent script errors when targeting a monster that cannot have its effects negated (i.e. "Lunalight Liger Dancer")
    -General script polish/modernization

--- official/c43685562.lua
+++ official/c43685562.lua
@@ -1,57 +1,42 @@
 --ジョウルリ－パンクデンジャラス・ガブ
 --Joruri-P.U.N.K. Dangerous Gabu
---scripted by Rundas
+--Scripted by Rundas
 local s,id=GetID()
 function s.initial_effect(c)
-	--Negate Effect
+	--Negate the effects of 1 monster your opponent controls, then gain LP if you control a "P.U.N.K." monster
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DISABLE+CATEGORY_RECOVER)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	e1:SetTarget(s.negtg)
-	e1:SetOperation(s.negop)
+	e1:SetTarget(s.disrectg)
+	e1:SetOperation(s.disrecop)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_PUNK}
---Negate Effect
-function s.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and not c:IsDisabled()
+function s.disrecfilter(c)
+	return c:IsNegatableMonster() and c:IsEffectMonster()
 end
-function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+function s.disrectg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.disrecfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.disrecfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local g=Duel.SelectTarget(tp,s.disrecfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,g:GetFirst():GetBaseAttack())
 end
-function s.recfilter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_PUNK)
-end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
+function s.disrecop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsDisabled() then
-		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESETS_STANDARD_PHASE_END)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetValue(RESET_TURN_SET)
-		e2:SetReset(RESETS_STANDARD_PHASE_END)
-		tc:RegisterEffect(e2)
-		local atk=tc:GetTextAttack()
-		if atk>0 and Duel.IsExistingMatchingCard(s.recfilter,tp,LOCATION_MZONE,0,1,nil)
-			and not tc:IsImmuneToEffect(e1)
-			and not tc:IsImmuneToEffect(e2) then
-			Duel.BreakEffect()
-			Duel.Recover(tp,atk,REASON_EFFECT)
-		end
+	if not (tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsCanBeDisabledByEffect(e)) then return end
+	local c=e:GetHandler()
+	tc:NegateEffects(c,RESETS_STANDARD_PHASE_END)
+	Duel.AdjustInstantly(tc)
+	if tc:IsDisabled() and tc:GetBaseAttack()>0 and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_PUNK),tp,LOCATION_MZONE,0,1,nil) then
+		Duel.BreakEffect()
+		Duel.Recover(tp,tc:GetBaseAttack(),REASON_EFFECT)
 	end
 end

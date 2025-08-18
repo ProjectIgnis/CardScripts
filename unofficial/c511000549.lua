2551984cc5baaec1e93438c580cc8459ae0093ed
ClaireStanfield <hirakimitsu1123@gmail.com>
2025-08-18 13:45:55 -0400

    Add SetDescription

--- unofficial/c511000549.lua
+++ unofficial/c511000549.lua
@@ -4,6 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Inflict damage to your opponent equal to that monster's ATK
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)

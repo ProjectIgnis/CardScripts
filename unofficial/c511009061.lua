db33a4a47120b3d55e8cdd3269d78f6b80b092ca
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-19 19:52:11 -0700

    Update "Jumbo Drill (Anime)"
    - ATK was evaluating itself when it should have excluded it.
    - Minor script polish/optimization.

--- unofficial/c511009061.lua
+++ unofficial/c511009061.lua
@@ -1,33 +1,32 @@
---Jumbo Drill (anime)
+--穿孔重機ドリルジャンボ (Anime)
+--Jumbo Drill (Anime)
 Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--to defence
+	--Gains 300 ATK for each other "Heavy Industry" monster you control
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetCondition(s.poscon)
-	e1:SetOperation(s.posop)
+	e1:SetValue(function(e,c) return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsHeavyIndustry),c:GetControler(),LOCATION_MZONE,0,e:GetHandler())*300 end)
 	c:RegisterEffect(e1)
-	--pierce
+	--If this card attacks a Defense Position monster, inflict piercing damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e2)
-	--atkup
+	--If this card attacks, it is changed to Defense Position at the end of the Battle Phase
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_PHASE+PHASE_BATTLE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetValue(s.val)
-	c:RegisterEffect(e3)
+	e3:SetCountLimit(1)
+	e3:SetCondition(function(e) return e:GetHandler():GetAttackedCount()>0 end)
+	e3:SetOperation(s.posop)
+	c:RegisterEffect(e3)	
 end
-function s.poscon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetAttackedCount()>0
-end
+s.listed_series={0x529} --"Heavy Industry" archetype
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsAttackPos() then
@@ -37,12 +36,6 @@ function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_COPY_INHERIT)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
+	e1:SetReset(RESETS_STANDARD_PHASE_END,3)
 	c:RegisterEffect(e1)
-end
-function s.val(e,c)
-	return Duel.GetMatchingGroupCount(s.filter,c:GetControler(),LOCATION_MZONE,0,nil)*300
-end
-function s.filter(c)
-	return c:IsFaceup() and c:IsHeavyIndustry()
 end

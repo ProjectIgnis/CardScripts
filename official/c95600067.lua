2c935a62a7c714715facd80ee2f05f0e4c7ecfac
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-23 20:19:09 -0700

    Update "Crystal Beast Topaz Tiger"
    - Script clean-up and modernization

--- official/c95600067.lua
+++ official/c95600067.lua
@@ -2,41 +2,39 @@
 --Crystal Beast Topaz Tiger
 local s,id=GetID()
 function s.initial_effect(c)
-	--send replace
+	--If this card attacks an opponent's monster, it gains 400 ATK during the Damage Step only
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT_CB)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCondition(s.repcon)
-	e1:SetOperation(s.repop)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.atkcon)
+	e1:SetValue(400)
 	c:RegisterEffect(e1)
-	--atkup
-	local e2=Effect.CreateEffect(c)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.condition)
-	e2:SetValue(400)
-	c:RegisterEffect(e2)
+	--Place this card in your Spell & Trap Zone instead of sending it to the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT_CB)
+	e1:SetCondition(s.replacecon)
+	e1:SetOperation(s.replaceop)
+	c:RegisterEffect(e1)
 end
-function s.repcon(e)
+function s.atkcon(e)
+	return Duel.GetAttacker()==e:GetHandler() and Duel.GetAttackTarget()~=nil and aux.StatChangeDamageStepCondition()
+end
+function s.replacecon(e)
 	local c=e:GetHandler()
 	return c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY)
 end
-function s.repop(e,tp,eg,ep,ev,re,r,rp)
+function s.replaceop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetCode(EFFECT_CHANGE_TYPE)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
-	e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+	e1:SetReset((RESET_EVENT|RESETS_STANDARD)&~RESET_TURN_SET)
+	e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
 	c:RegisterEffect(e1)
 	Duel.RaiseEvent(c,EVENT_CUSTOM+CARD_CRYSTAL_TREE,e,0,tp,0,0)
-end
-function s.condition(e)
-	local phase=Duel.GetCurrentPhase()
-	return (phase==PHASE_DAMAGE or phase==PHASE_DAMAGE_CAL)
-		and Duel.GetAttacker()==e:GetHandler() and Duel.GetAttackTarget()~=nil
 end

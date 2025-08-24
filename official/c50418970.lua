0565f57a708577daaeae047eba13da7ea6f6fd5f
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-24 12:19:18 -0700

    Update "Spirit Burner"
    -Missing check that the equipped monster was previously on the field
    - Minor script modernization update

--- official/c50418970.lua
+++ official/c50418970.lua
@@ -3,51 +3,52 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c)
-	--pos
+	--Change the position of the equipped monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.postg)
+	e1:SetOperation(s.posop)
+	c:RegisterEffect(e1)
+	--When the equipped monster is returned from the field to the hand and this card is sent to the GY, inflict 600 damage to your opponent
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCondition(s.damcon)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
+	--Add this card to your hand instead of conducting your normal draw
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_POSITION)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCountLimit(1)
-	e3:SetTarget(s.postg)
-	e3:SetOperation(s.posop)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PREDRAW)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCondition(s.retcon)
+	e3:SetTarget(s.rettg)
+	e3:SetOperation(s.retop)
 	c:RegisterEffect(e3)
-	--damage
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e4:SetCategory(CATEGORY_DAMAGE)
-	e4:SetCode(EVENT_TO_GRAVE)
-	e4:SetCondition(s.damcon)
-	e4:SetTarget(s.damtg)
-	e4:SetOperation(s.damop)
-	c:RegisterEffect(e4)
-	--
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,2))
-	e5:SetCategory(CATEGORY_TOHAND)
-	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e5:SetCode(EVENT_PREDRAW)
-	e5:SetRange(LOCATION_GRAVE)
-	e5:SetCondition(s.retcon)
-	e5:SetTarget(s.rettg)
-	e5:SetOperation(s.retop)
-	c:RegisterEffect(e5)
 end
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ec=e:GetHandler():GetEquipTarget()
 	if chk==0 then return ec and ec:IsAttackPos() and ec:IsCanChangePosition() end
 end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	Duel.ChangePosition(e:GetHandler():GetEquipTarget(),POS_FACEUP_DEFENSE)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.ChangePosition(c:GetEquipTarget(),POS_FACEUP_DEFENSE)
 end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local ec=c:GetPreviousEquipTarget()
-	return c:IsReason(REASON_LOST_TARGET) and ec:IsLocation(LOCATION_HAND)
+	return c:IsReason(REASON_LOST_TARGET) and ec and ec:IsPreviousLocation(LOCATION_MZONE) and ec:IsLocation(LOCATION_HAND)
 end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -64,12 +65,13 @@ function s.retcon(e,tp,eg,ep,ev,re,r,rp)
 		and Duel.GetDrawCount(tp)>0
 end
 function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToHand() end
 	local dt=Duel.GetDrawCount(tp)
 	if dt~=0 then
 		_replace_count=0
 		_replace_max=dt
-		local e1=Effect.CreateEffect(e:GetHandler())
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetCode(EFFECT_DRAW_COUNT)
@@ -78,7 +80,7 @@ function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
 		e1:SetValue(0)
 		Duel.RegisterEffect(e1,tp)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,0,0)
 end
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()

5b6194ad23b784556cb4b758c4b5e82defc59c11
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-28 19:23:25 -0700

    Update "Mirror Labyrinth"
    - Added check for Level 5 or higher monsters
    - Script modernization update

--- unofficial/c511000840.lua
+++ unofficial/c511000840.lua
@@ -1,24 +1,23 @@
+--鏡の迷宮－ミラー・ラビリンス
 --Mirror Labyrinth
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--multi attack
+	--If you control no Level 5 or higher monsters, and the only monster you control is a Level 4 or lower monster: That monster can attack twice during each Battle Phase
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_EXTRA_ATTACK)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetCondition(s.condition)
+	e2:SetCondition(s.extraattackcon)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
 end
-function s.condition(e)
+function s.extraattackcon(e)
 	local g=Duel.GetFieldGroup(e:GetHandlerPlayer(),LOCATION_MZONE,0)
-	local ct=#g
-	local tg=g:GetFirst()
-	return ct==1 and tg:IsFaceup() and tg:IsLevelBelow(4)
+	return #g==1 and g:IsExists(aux.FaceupFilter(Card.IsLevelBelow,4),1,nil) and g:FilterCount(aux.FaceupFilter(Card.IsLevelAbove,5),nil)==0
 end

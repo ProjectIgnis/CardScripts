3b2c98bddd4468e025faf486a9ebce7a0eeb038f
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-08-31 15:22:01 -0700

    Update "Nitro Warrior"
    - Reworked script to allow gain of ATK even if this card wasn't the first to attack
    - Added handling to ensure the ATK gain occurs only once
    - General script polish/modernization

--- official/c18013090.lua
+++ official/c18013090.lua
@@ -2,80 +2,84 @@
 --Nitro Warrior
 local s,id=GetID()
 function s.initial_effect(c)
-	--synchro summon
-	Synchro.AddProcedure(c,s.tfilter,1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
-	--atkup
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.atcon)
-	e1:SetOperation(aux.chainreg)
-	c:RegisterEffect(e1)
+	--Synchro Summon procedure: "Nitro Synchron" + 1 or more non-Tuner monsters
+	Synchro.AddProcedure(c,s.tunerfilter,1,1,Synchro.NonTuner(nil),1,99)
+	--Register Spell card activations
+	local e0a=Effect.CreateEffect(c)
+	e0a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e0a:SetCode(EVENT_CHAINING)
+	e0a:SetRange(LOCATION_MZONE)
+	e0a:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return ep==tp and Duel.IsTurnPlayer(tp) end)
+	e0a:SetOperation(aux.chainreg)
+	c:RegisterEffect(e0a)
+	local e0b=Effect.CreateEffect(c)
+	e0b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0b:SetCode(EVENT_CHAIN_SOLVED)
+	e0b:SetRange(LOCATION_MZONE)
+	e0b:SetOperation(s.regop)
+	c:RegisterEffect(e0b)
+	--Once during each of your turns, if you activate a Spell Card, this card gains 1000 ATK during the next attack involving this card, during damage calculation only
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_SINGLE)
+	e1a:SetCode(EFFECT_UPDATE_ATTACK)
+	e1a:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1a:SetRange(LOCATION_MZONE)
+	e1a:SetCondition(s.atkcon)
+	e1a:SetValue(1000)
+	c:RegisterEffect(e1a)
+	--Reset the flag effect above if this card attacks again 
+	local e1b=Effect.CreateEffect(c)
+	e1b:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1b:SetCode(EVENT_BATTLED)
+	e1b:SetCondition(function(e) return e:GetHandler():HasFlagEffect(id) end)
+	e1b:SetOperation(function(e) e:GetHandler():ResetFlagEffect(id) end)
+	c:RegisterEffect(e1b)
+	--Change 1 Defense Position monster your opponent controls to Attack Position, then this card can make a second attack against that monster
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_CHAIN_SOLVED)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetOperation(s.atop)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_POSITION)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_BATTLED)
+	e2:SetCondition(s.chainattackposcon)
+	e2:SetTarget(s.chainattackpostg)
+	e2:SetOperation(s.chainattackposop)
 	c:RegisterEffect(e2)
-	--chain attack
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_POSITION)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_BATTLED)
-	e3:SetCondition(s.cacon)
-	e3:SetTarget(s.catg)
-	e3:SetOperation(s.caop)
-	c:RegisterEffect(e3)
 end
-s.material={96182448}
-s.listed_names={96182448}
+s.material={96182448} --"Nitro Synchron"
+s.listed_names={96182448} --"Nitro Synchron"
 s.material_setcode=SET_SYNCHRON
-function s.tfilter(c,lc,stype,tp)
-	return c:IsSummonCode(lc,stype,tp,96182448) or c:IsHasEffect(20932152)
+function s.tunerfilter(c,lc,stype,tp)
+	return c:IsSummonCode(lc,stype,tp,96182448) or c:IsHasEffect(20932152) --"Quickdraw Synchron" check
 end
-function s.atcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and Duel.IsTurnPlayer(tp)
-		and re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsSpellEffect()
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsSpellEffect() and e:GetHandler():GetFlagEffect(1)>0 then
+		e:GetHandler():RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
+	end
 end
-function s.atop(e,tp,eg,ep,ev,re,r,rp)
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:GetFlagEffect(1)==0 or c:GetFlagEffect(id)~=0 then return end
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.atkcon)
-	e2:SetValue(1000)
-	e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_DAMAGE_CAL|PHASE_END)
-	c:RegisterEffect(e2)
-	c:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1)
+	return Duel.GetAttacker()==c and c:HasFlagEffect(id) and Duel.IsPhase(PHASE_DAMAGE_CAL) 
 end
-function s.atkcon(e)
-	local c=e:GetHandler()
-	return Duel.IsPhase(PHASE_DAMAGE_CAL) and c==Duel.GetAttacker()
-end
-function s.cacon(e,tp,eg,ep,ev,re,r,rp)
+function s.chainattackposcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
 	return bc and bc:IsStatus(STATUS_BATTLE_DESTROYED) and c:CanChainAttack()
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsDefensePos() and not c:IsStatus(STATUS_BATTLE_DESTROYED)
+function s.chainattackposfilter(c)
+	return c:IsFaceup() and c:IsDefensePos() and not c:IsStatus(STATUS_BATTLE_DESTROYED) and c:IsCanChangePosition()
 end
-function s.catg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+function s.chainattackpostg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.chainattackposfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.chainattackposfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.chainattackposfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
-function s.caop(e,tp,eg,ep,ev,re,r,rp)
+function s.chainattackposop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.ChangePosition(tc,POS_FACEUP_ATTACK)

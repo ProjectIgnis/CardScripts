467506fce27fe439bf2189ec2bfa9d89300903c8
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-01 13:45:32 -0700

    Update "Numeron" cards
    - "Numeron Gate (Anime)" monster script modernization
    - Fixed "Numeron Network (Anime)" to properly allow "Numeron" monsters to activate effects without detaching materials

--- unofficial/c511000277.lua
+++ unofficial/c511000277.lua
@@ -1,46 +1,41 @@
---ＣＮｏ．１ ゲート・オブ・カオス・ヌメロン－シニューニャ
+--ＣＮｏ．１ ゲート・オブ・カオス・ヌメロン－シニューニャ (Anime)
 --Number C1: Numeron Chaos Gate Sunya (anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--Xyz summon
-	Xyz.AddProcedure(c,nil,2,4,s.ovfilter,aux.Stringid(id,0))
 	c:EnableReviveLimit()
-	--Destroy itself
+	--Xyz Summon procedure: 4 Level 2 monsters or 1 "Number 1: Numeron Gate Ekam"
+	Xyz.AddProcedure(c,nil,2,4,s.ovfilter,aux.Stringid(id,0))
+	--Cannot be destroyed by battle, except with "Number" monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetCode(EFFECT_SELF_DESTROY)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.descon)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
 	c:RegisterEffect(e1)
-	--Remove monsters
+	--If "Numeron Network" is not on the field, destory this card
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCondition(s.bancon)
-	e2:SetTarget(s.bantg)
-	e2:SetOperation(s.banop)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_SELF_DESTROY)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(function(e) return not Duel.IsEnvironment(CARD_NUMERON_NETWORK) end)
 	c:RegisterEffect(e2)
-	--Battle indestructable
+	--When this card is Xyz Summoned, banish all monsters on the field
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e3:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(function(e) return e:GetHandler():IsXyzSummoned() end)
+	e3:SetTarget(s.bantg)
+	e3:SetOperation(s.banop)
 	c:RegisterEffect(e3)
+
 end
-s.listed_series={0x48}
-s.listed_names={CARD_NUMERON_NETWORK}
+s.listed_series={SET_NUMBER}
+s.listed_names={CARD_NUMERON_NETWORK,15232745} --"Number 1: Numeron Gate Ekam"
 s.xyz_number=1
 function s.ovfilter(c,tp,lc)
 	return c:IsFaceup() and c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,15232745) and Duel.IsEnvironment(CARD_NUMERON_NETWORK)
-end
-function s.descon(e)
-	return not Duel.IsEnvironment(CARD_NUMERON_NETWORK)
-end
-function s.bancon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsXyzSummoned()
 end
 function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -49,15 +44,14 @@ function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local chk=c:IsRelateToEffect(e)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
 	local atk=0
-	for tc in aux.Next(g) do
-		local a=tc:GetAttack()
-		if a<0 or tc:IsFacedown() then a=0 end
-		atk=atk+a
+	for tc in g:Iter() do
+		local tcatk=tc:GetAttack()
+		if tcatk<0 or tc:IsFacedown() then tcatk=0 end
+		atk=atk+tcatk
 	end
-	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 and chk then
+	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
 		local e3=Effect.CreateEffect(c)
 		e3:SetDescription(aux.Stringid(id,2))
 		e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -68,14 +62,14 @@ function s.banop(e,tp,eg,ep,ev,re,r,rp)
 		e3:SetTarget(s.sptg)
 		e3:SetOperation(s.spop)
 		e3:SetLabel(atk)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
+		e3:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY|RESET_SELF_TURN)
 		c:RegisterEffect(e3)
-		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN,0,1)
+		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY|RESET_SELF_TURN,0,1)
 	end
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:GetTurnID()~=Duel.GetTurnCount() and tp==Duel.GetTurnPlayer() and c:GetFlagEffect(id)>0
+	return c:GetTurnID()~=Duel.GetTurnCount() and Duel.IsTurnPlayer(tp) and c:HasFlagEffect(id)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
@@ -92,18 +86,15 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		e1:SetCondition(s.damcon)
+		e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+1) end)
 		e1:SetCost(Cost.DetachFromSelf(1))
 		e1:SetTarget(s.damtg)
 		e1:SetOperation(s.damop)
 		e1:SetLabel(e:GetLabel())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+		e1:SetReset((RESET_EVENT|RESETS_STANDARD)&~RESET_TOFIELD)
 		c:RegisterEffect(e1)
 		Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)
 	end
-end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL+1)
 end
 function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

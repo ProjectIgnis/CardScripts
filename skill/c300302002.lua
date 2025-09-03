bbaaf06a35cea7457f1419fa70d930694e1180e2
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-02 21:31:09 -0700

    Update "Ritual Ceremony"
    - Prevent script errors
    - Added checks for non-revealed cards
    - Cleaned up card search functionality to filter for cards listed on the revealed ritual monster.

--- skill/c300302002.lua
+++ skill/c300302002.lua
@@ -4,33 +4,36 @@ function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.cffilter,tp,LOCATION_HAND,0,1,nil)
-	and Duel.GetFlagEffect(tp,id)==0
+	--Condition check
+	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.ritualrevealfilter,tp,LOCATION_HAND,0,1,nil,tp) and not Duel.HasFlagEffect(tp,id)
 end
-function s.cffilter(c)
-	return c:IsRitualMonster()
+function s.ritualrevealfilter(c,tp)
+	return c:IsRitualMonster() and not c:IsPublic() and Duel.IsExistingMatchingCard(s.ritspellthfilter,c:GetControler(),LOCATION_DECK,0,1,nil,c)
 end
-function s.ritfilter(c,tc)
-	return c:IsRitualSpell() and Ritual.GetMatchingFilterFunction(c)(tc)
+function s.ritspellthfilter(c,tc)
+	return c:IsRitualSpell() and tc:ListsCode(c:GetCode()) and c:IsAbleToHand()
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--opd register
+	--OPD register
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
-	local g=Duel.SelectMatchingCard(tp,s.cffilter,tp,LOCATION_HAND,0,1,1,e:GetHandler())
+	--Reveal 1 Ritual Monster in your hand
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,s.ritualrevealfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
 	Duel.ConfirmCards(1-tp,g)
 	local tc=g:GetFirst()
+	--Add 1 Ritual Spell listed on that Ritual Monster
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local sg=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_DECK,0,1,1,e:GetHandler(),tc)
-	if #sg>0 then
-		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	local sg=Duel.SelectMatchingCard(tp,s.ritspellthfilter,tp,LOCATION_DECK,0,1,1,nil,tc)
+	if #sg>0 and Duel.SendtoHand(sg,nil,REASON_EFFECT)>0 then
 		Duel.ConfirmCards(1-tp,sg)
 		Duel.ShuffleDeck(tp)
+		--Place 1 card from your hand on the bottom of your Deck
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 		local rg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
 		if #rg>0 then
+			Duel.BreakEffect()
 			Duel.SendtoDeck(rg,tp,SEQ_DECKBOTTOM,REASON_EFFECT)
 		end
 	end

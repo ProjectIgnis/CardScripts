68c8fc6ee26f2f9784b934a6d3c55879d54d8790
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-04 21:44:18 -0700

    Update "Doppelganger Shark"
    - Slight script clean-up

--- unofficial/c810000030.lua
+++ unofficial/c810000030.lua
@@ -1,28 +1,28 @@
--- Doppel Shark
--- scripted by: UnknownGuest
+--ッペル・シャーク
+--Doppelganger Shark
+--Scripted by: UnknownGuest
 local s,id=GetID()
 function s.initial_effect(c)
-	-- copy name
+	--This card's name becomes the name of 1 WATER monster you control
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(s.copytg)
-	e1:SetOperation(s.copyop)
+	e1:SetTarget(s.copynametg)
+	e1:SetOperation(s.copynameop)
 	c:RegisterEffect(e1)
 end
-function s.filter(c)
-	return c:IsAttribute(ATTRIBUTE_WATER)
+function s.copynamefilter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
 end
-function s.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+function s.copynametg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.copynamefilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.copynamefilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetTargetCard(g)
+	Duel.SelectTarget(tp,s.copynamefilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.copyop(e,tp,eg,ep,ev,re,r,rp)
+function s.copynameop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and c:IsRelateToEffect(e) and c:IsFaceup() then
@@ -30,14 +30,14 @@ function s.copyop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e1:SetCode(EFFECT_CHANGE_CODE)
 		e1:SetValue(code)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 		c:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(e:GetHandler())
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DIRECT_ATTACK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
 		tc:RegisterEffect(e2)
 	end
 end

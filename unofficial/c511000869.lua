3a343876770f6e3d8fb0834222014f611a414f27
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-05 20:31:50 -0700

    Update "Wild Heart"
    - Self-destruction effect was triggering on battle damage dealt, not on if it attacked directly with its own effect
    - Fixed condition check for direct attack/added handling for multiple direct attack effects on it/added check for direct attacks with its own effect
    - General script polish/modernization

--- unofficial/c511000869.lua
+++ unofficial/c511000869.lua
@@ -1,93 +1,116 @@
+--ワイルド・ハート
 --Wild Heart
 local s,id=GetID()
 function s.initial_effect(c)
-	--sp summon
+	--Special Summon this card from your if a monster card is sent from the hand to the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_TO_GRAVE)
-	e1:SetCondition(s.spcon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spfromhandcon)
+	e1:SetTarget(s.spselftg)
+	e1:SetOperation(s.spfromhandop)
 	c:RegisterEffect(e1)
-	--direct atk
+	--This card can attack directly
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_DIRECT_ATTACK)
 	c:RegisterEffect(e2)
-	--destroy
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_BATTLE_DAMAGE)
-	e3:SetCondition(s.descon)
-	e3:SetTarget(s.destg)
-	e3:SetOperation(s.desop)
-	c:RegisterEffect(e3)
-	--sp summon again
+	--Register flag if this card attacked directly with its own effect
+	local e3a=Effect.CreateEffect(c)
+	e3a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3a:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3a:SetCondition(s.regcon)
+	e3a:SetOperation(s.regop)
+	c:RegisterEffect(e3a)
+	--If this card attacked directly with its own effect, destroy it at the end of the Damage Step
+	local e3b=Effect.CreateEffect(c)
+	e3b:SetDescription(aux.Stringid(id,1))
+	e3b:SetCategory(CATEGORY_DESTROY)
+	e3b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3b:SetCode(EVENT_DAMAGE_STEP_END)
+	e3b:SetRange(LOCATION_MZONE)
+	e3b:SetCountLimit(1)
+	e3b:SetTarget(s.destg)
+	e3b:SetOperation(s.desop)
+	c:RegisterEffect(e3b)
+	--If this card on the field is destroyed by a card effect, except its own, Special Summon it from the GY
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_DESTROYED)
-	e4:SetCondition(s.spcon2)
-	e4:SetTarget(s.sptg)
-	e4:SetOperation(s.spop2)
+	e4:SetCondition(s.spfromgycon)
+	e4:SetTarget(s.spselftg)
+	e4:SetOperation(s.spfromgyop)
 	c:RegisterEffect(e4)
 end
-function s.filter(c)
+function s.spfromhandconfilter(c)
 	return c:IsPreviousLocation(LOCATION_HAND) and c:IsMonster()
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.filter,1,nil)
+function s.spfromhandcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.spfromhandconfilter,1,nil)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+function s.spselftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spfromhandop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and Duel.GetAttackTarget()==nil
-		and Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_MZONE)>0
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttackTarget()==nil and e:GetHandler():IsHasEffect(EFFECT_DIRECT_ATTACK)
+		and Duel.IsExistingMatchingCard(aux.NOT(Card.IsHasEffect),tp,0,LOCATION_MZONE,1,nil,EFFECT_IGNORE_BATTLE_TARGET)
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local effs={c:GetCardEffect(EFFECT_DIRECT_ATTACK)}
+	local eg=Group.CreateGroup()
+	for _,eff in ipairs(effs) do
+		eg:AddCard(eff:GetOwner())
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
+	local ec = #eg==1 and eg:GetFirst() or eg:Select(tp,1,1,nil):GetFirst()
+	if c==ec then
+		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE,0,1)
 	end
 end
-function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	return not c:IsReason(REASON_BATTLE) and re and re:GetOwner()~=c and c:IsLocation(LOCATION_GRAVE)
+	if chk==0 then return c:HasFlagEffect(id) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,c,1,0,0)
 end
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.Destroy(c,REASON_EFFECT)
+	end
+end
+function s.spfromgycon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_EFFECT) and re and re:GetOwner()~=c and c:IsLocation(LOCATION_GRAVE)
+end
+function s.spfromgyop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EVENT_PHASE+PHASE_END)
 		e1:SetRange(LOCATION_MZONE)
 		e1:SetCountLimit(1)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetOperation(s.rmop)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		e1:SetOperation(function(e) Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT) end)
 		c:RegisterEffect(e1)
 	end
-end
-function s.rmop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
 end

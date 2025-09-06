44307f7fc3ed3ff0a6b840fcb01354689ee67748
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-06 15:21:01 -0700

    Update "Conduction Warrior Linear Magnum ± (Anime)"
    - Text does not indicate that monster is a form of Nomi, so special summon limit was removed
    - ATK gain appears to be listed as a continuous effect that applies entirely on the field at all times instead of during battle, updated effect text to reflect it and implemented script changes. 
    - Cleaned up summon cost check
    - General script polish/modernization updates.

--- unofficial/c170000116.lua
+++ unofficial/c170000116.lua
@@ -1,60 +1,60 @@
---超電導戦士 リニア・マグナム±
---Conduction Warrior Linear Magnum ±
+--超電導戦士 リニア・マグナム± (Anime)
+--Conduction Warrior Linear Magnum ± (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	c:EnableReviveLimit()
-	--special summon
+	--Special Summon this card from  your hand by sending 1 "Magnet Warrior Σ＋" and 1 "Magnet Warrior Ω－" from your hand or field to the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--Increase ATK
+	--Gains ATK equal to half the total ATK of all Plus and Minus monsters the field 
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetTarget(s.atkgtg)
-	e2:SetOperation(s.atkgop)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.atkval)
 	c:RegisterEffect(e2)
 end
-function s.spfilter(c,tpe)
-	return c:IsType(tpe) and c:IsAbleToGraveAsCost()
+s.listed_names={170000111,170000112} --"Magnet Warrior Σ＋", "Magnet Warrior Ω－"
+function s.chk(c,sg,g,code,...)
+	if not c:IsCode(code) then return false end
+	local res
+	if ... then
+		g:AddCard(c)
+		res=sg:IsExists(s.chk,1,g,sg,g,...)
+		g:RemoveCard(c)
+	else
+		res=true
+	end
+	return res
 end
 function s.rescon(sg,e,tp,mg)
-	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:IsExists(s.chk,1,nil,sg)
-end
-function s.chk(c,sg)
-	return c:IsType(TYPE_PLUS) and sg:IsExists(Card.IsType,1,c,TYPE_MINUS)
+	return aux.ChkfMMZ(1)(sg,e,tp,mg) and sg:IsExists(s.chk,1,nil,sg,Group.CreateGroup(),170000111,170000112)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
-	local g1=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil,TYPE_PLUS)
-	local g2=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil,TYPE_MINUS)
+	local rg=Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND|LOCATION_MZONE,0,nil)
+	local g1=rg:Filter(Card.IsCode,nil,170000111)
+	local g2=rg:Filter(Card.IsCode,nil,170000112)
 	local g=g1:Clone()
 	g:Merge(g2)
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2 and #g1>0 and #g2>0 and #g>1
-		and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-2 and #g1>0 and #g2>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil,TYPE_PLUS+TYPE_MINUS)
-	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE,nil,nil,true)
-	local cg=sg:Filter(Card.IsFacedown,nil)
-	if #cg>0 then
-		Duel.ConfirmCards(1-tp,cg)
-	end
-	if #sg>0 then
-		sg:KeepAlive()
-		e:SetLabelObject(sg)
-	return true
+	local rg=Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND|LOCATION_MZONE,0,nil):Filter(Card.IsCode,nil,170000111,170000112)
+	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
 	end
 	return false
 end
@@ -62,29 +62,9 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.SendtoGrave(g,REASON_COST)
-	c:SetMaterial(g)
 	g:DeleteGroup()
 end
-function s.filter(c)
-	return c:IsFaceup() and c:IsType(TYPE_PLUS+TYPE_MINUS)
-end
-function s.atkgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) and chkc~=e:GetHandler() end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler())
-end
-function s.atkgop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() and c:IsRelateToEffect(e) and c:IsFaceup() then
-		local atk=tc:GetAttack()/2
-		if atk<0 then return end
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(atk)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
-	end
+function s.atkval(e,c)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsType,TYPE_PLUS|TYPE_MINUS),c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,c)
+	return g:GetSum(Card.GetBaseAttack)/2
 end

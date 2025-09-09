aefede774ff2fe253c2feb174c6b0ca04408215a
yoshiduels <yoshi.katreuvin@gmail.com>
2025-09-09 20:29:55 +0200

    added Dedicated Gaia the Fierce Knight

---
+++ rush/c160216032.lua
@@ -0,0 +1,50 @@
+--洗礼の暗黒騎士ガイア
+--Dedicated Gaia the Fierce Knight
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Name becomes "Gaia the Fierce Knight" in the hand or Graveyard
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_CHANGE_CODE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e0:SetValue(6368038)
+	c:RegisterEffect(e0)
+	--Add 1 card from the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={55761792}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonPhaseMain() and c:IsStatus(STATUS_SUMMON_TURN+STATUS_SPSUMMON_TURN)
+end
+function s.thfilter(c)
+	return c:IsCode(55761792) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		if c:HasLevel() and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			c:UpdateLevel(1,RESETS_STANDARD_PHASE_END,c)
+		end
+	end
+end

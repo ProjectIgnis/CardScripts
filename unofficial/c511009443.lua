43ca0c4a71851ff3de386c8408d109aa79405a0b
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-10 21:07:16 -0700

    Update "Supreme King Gate Zero (Anime)"
    - Effect to prevent Pendulum Summoning should be negatable. 
    - General script polish/modernization update

--- unofficial/c511009443.lua
+++ unofficial/c511009443.lua
@@ -1,63 +1,53 @@
---覇王門零
+--覇王門零 (Anime)
+--Supreme King Gate Zero (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
+	--Pendulum procedure
 	Pendulum.AddProcedure(c)
-	--splimit
+	--While you control a monster, you cannot Pendulum Summon
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetRange(LOCATION_PZONE)
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e0:SetRange(LOCATION_PZONE)
 	e0:SetTargetRange(1,0)
-	e0:SetCondition(s.splimcon)
-	e0:SetTarget(s.splimit)
+	e0:SetCondition(function(e) return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)>0 end)
+	e0:SetTarget(function(e,c,sump,sumtype,sumpos,targetp) return (sumtype&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM end)
 	c:RegisterEffect(e0)
-	--avoid damage
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_DAMAGE)
-	e1:SetRange(LOCATION_PZONE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetLabel(0)
-	e1:SetCondition(s.damcon)
-	e1:SetValue(s.damval)
-	c:RegisterEffect(e1)
-	--copy	
+	--While you control a "Supreme King" monster, you take no damage
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_FIELD)
+	e1a:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1a:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1a:SetRange(LOCATION_PZONE)
+	e1a:SetTargetRange(1,0)
+	e1a:SetLabel(0)
+	e1a:SetCondition(function(e) return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xf8),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil) end)
+	e1a:SetValue(s.damval)
+	c:RegisterEffect(e1a)
+	--Handling for "Supreme King Gate Infinity (Anime)"	for above effect application
+	local e1b=Effect.CreateEffect(c)
+	e1b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1b:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1b:SetCode(EVENT_ADJUST)
+	e1b:SetRange(LOCATION_PZONE)
+	e1b:SetLabelObject(e1a)
+	e1b:SetOperation(s.trig)
+	c:RegisterEffect(e1b)
+	--Add 1 "Supreme King Gate Infinity" from your Deck to your hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_ADJUST)
-	e2:SetRange(LOCATION_PZONE)
-	e2:SetLabelObject(e1)
-	e2:SetOperation(s.trig)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_MZONE) and e:GetHandler():IsFaceup() end)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	--tohand
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_DESTROYED)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCondition(s.thcon)
-	e3:SetTarget(s.thtg)
-	e3:SetOperation(s.thop)
-	c:RegisterEffect(e3)
 end
-s.listed_series={0xf8}
-s.listed_names={22211622}
-function s.splimcon(e)
-	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)>0
-end
-function s.splimit(e,c,sump,sumtype,sumpos,targetp)
-	return (sumtype&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
-end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xf8)
-end
-function s.damcon(e)
-	return Duel.IsExistingMatchingCard(s.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
-end
+s.listed_series={0xf8} --"Supreme King" archetype
+s.listed_names={22211622} --"Supreme King Gate Infinity"
 function s.damval(e,re,val,r,rp,rc)
 	local tp=e:GetHandlerPlayer()
 	if val~=0 then
@@ -72,20 +62,16 @@ function s.trig(e,tp,eg,ep,ev,re,r,rp)
 		e:GetLabelObject():SetLabel(0)
 	end
 end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
-end
-function s.filter(c)
+function s.thfilter(c)
 	return c:IsCode(22211622) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)

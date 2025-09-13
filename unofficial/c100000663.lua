0b9a3a21c7412f66e5bce56e93c5b760d3562f93
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-13 08:34:13 -0700

    Update cards related to "Chaos Distill"
    Added official code for "Chaos Distill" to allow proper functionality/added notes to scripts/polished as needed.

--- unofficial/c100000663.lua
+++ unofficial/c100000663.lua
@@ -1,4 +1,5 @@
  --白の過程－アルベド
+ --White Process - Albedo
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -11,25 +12,22 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={27408609,100000650}
+s.listed_names={27408609,6500778} --"Golden Homunculus", "Chaos Distill"
 function s.spfilter(c,e,tp)
 	return c:IsCode(27408609) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsCode(100000650)
-end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
-	and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK|LOCATION_HAND,0,1,nil,e,tp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,6500778),tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_DECK|LOCATION_HAND)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_HAND|LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)

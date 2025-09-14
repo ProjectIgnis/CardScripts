05ebf0654ee423823368cc38e31ce9e94ce99f36
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-14 15:40:10 -0700

    Update "Missile Knight"
    - Was coded as an Ignition instead of a Quick Effect
    - Minor script polish

--- unofficial/c511009727.lua
+++ unofficial/c511009727.lua
@@ -1,18 +1,21 @@
+--ミサイル・ナイト
 --Missile Knight
 local s,id=GetID()
 function s.initial_effect(c)
-	--destroy
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCondition(s.descon)
-	e2:SetCost(Cost.SelfBanish)
-	e2:SetTarget(s.destg)
-	e2:SetOperation(s.desop)
-	c:RegisterEffect(e2)
+	--Destroy 1 monster on the field if you destroyed a monster this turn
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_GRAVE)	
+	e1:SetCondition(function(e,tp) return s[tp]>0 end)
+	e1:SetCost(Cost.SelfBanish)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	--Check for monsters destroyed during a given turn
 	aux.GlobalCheck(s,function()
 		s[0]=0
 		s[1]=0
@@ -27,23 +30,20 @@ function s.initial_effect(c)
 		end)
 	end)
 end
-function s.spfilter(c,tp)
+function s.chkfilter(c,tp)
 	return c:IsPreviousLocation(LOCATION_MZONE) and (c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==tp)
 end
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-	if eg:IsExists(s.spfilter,1,nil,tp) then
+	if eg:IsExists(s.chkfilter,1,nil,tp) then
 		s[tp]=s[tp]+1
 	end
 end
-function s.descon(e,tp)
-	return s[tp]>0
-end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()

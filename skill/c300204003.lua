170286529b4e5e666d1670e85bab6c95944a7c1e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-09-14 22:22:18 +0300

    "Thousand-Eyes Spell" script update
    Updated it to use the Ritual and Fusion procedures.

--- skill/c300204003.lua
+++ skill/c300204003.lua
@@ -3,153 +3,68 @@ local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
+s.listed_names={64631466,63519819} --"Relinquished", "Thousand-Eyes Restrict"
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return aux.CanActivateSkill(tp) and (s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0) or s.ritTarget(e,tp,eg,ep,ev,re,r,rp,0))
-		and Duel.GetFlagEffect(tp,id)==0
+	return aux.CanActivateSkill(tp) and not Duel.HasFlagEffect(tp,id) and s.cost(e,tp,eg,ep,ev,re,r,rp,0)
 end
-s.listed_names={64631466,63519819}
+function s.costfilter(c,e,tp,eg,ep,ev,re,r,rp)
+	if not c:IsDiscardable() then return false end
+	local rit_params={filter=function(ritual_c) return ritual_c:IsCode(64631466) and ritual_c~=c end,lvtype=RITPROC_GREATER,forcedselection=s.rcheck(c)}
+	local fus_params={fusfilter=aux.FilterBoolFunction(Card.IsCode,63519819),extrafil=s.fextra(c)}
+	return Ritual.Target(rit_params)(e,tp,eg,ep,ev,re,r,rp,0)
+		or Fusion.SummonEffTG(fus_params)(e,tp,eg,ep,ev,re,r,rp,0)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil,e,tp,eg,ep,ev,re,r,rp) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST|REASON_DISCARD,nil,e,tp,eg,ep,ev,re,r,rp)
+end
+function s.fextra(exc)
+	return function(e,tp,mg)
+		return nil,s.fcheck(exc)
+	end
+end
+function s.fcheck(exc)
+	return function(tp,sg,fc)
+		return not (exc and sg:IsContains(exc))
+	end
+end
+function s.rcheck(exc)
+	return function(e,tp,sg,rc)
+		return not (exc and sg:IsContains(exc))
+	end
+end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--ritual
-	local g1=s.ritTarget(e,tp,eg,ep,ev,re,r,rp,0)
-	--fusion
-	local g2=s.fusTarget(e,tp,eg,ep,ev,re,r,rp,0)
-	local opt=0
-	if g1 and g2 then
-		opt=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-	elseif g1 then
-		opt=Duel.SelectOption(tp,aux.Stringid(id,1))
-	elseif g2 then
-		opt=Duel.SelectOption(tp,aux.Stringid(id,2))+1
-	else return end
-	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-	--cannot conduct BP this turn
+	--Soft OPT
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	--Discard 1 card
+	s.cost(e,tp,eg,ep,ev,re,r,rp,1)
+	local rit_params={filter=aux.FilterBoolFunction(Card.IsCode,64631466),lvtype=RITPROC_GREATER,forcedselection=s.rcheck(c)}
+	local fus_params={fusfilter=aux.FilterBoolFunction(Card.IsCode,63519819),extrafil=s.fextra(c)}
+	local b1=Ritual.Target(rit_params)(e,tp,eg,ep,ev,re,r,rp,0)
+	local b2=Fusion.SummonEffTG(fus_params)(e,tp,eg,ep,ev,re,r,rp,0)
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	if op==1 then
+		--Ritual Summon 1 "Relinquished" from your hand by Tributing a monster from your hand or field whose Level is 1 or more, also flip this card over
+		Ritual.Target(rit_params)(e,tp,eg,ep,ev,re,r,rp,1)
+		Ritual.Operation(rit_params)(e,tp,eg,ep,ev,re,r,rp)
+	elseif op==2 then
+		--Fusion Summon 1 "Thousand-Eyes Restrict" from your Extra Deck, using monsters from your hand or field as Fusion Material, also flip this card over
+		Fusion.SummonEffTG(fus_params)(e,tp,eg,ep,ev,re,r,rp,1)
+		Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp)
+	end
+	--You cannot conduct your Battle Phase this turn
 	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,3))
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 	e1:SetCode(EFFECT_CANNOT_BP)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
 	e1:SetTargetRange(1,0)
-	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetReset(RESET_PHASE|PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,3),nil)
-	if opt==0 then
-		s.ritTarget(e,tp,eg,ep,ev,re,r,rp,1)
-		local mg=Duel.GetRitualMaterial(tp)
-		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tc=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp,mg,ft):GetFirst()
-		if tc then
-			mg:Match(Card.IsCanBeRitualMaterial,tc,tc)
-			local mat=nil
-			if ft>0 then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-				mat=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
-			else
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-				mat=mg:FilterSelect(tp,s.mfilterf,1,1,nil,tp,mg,tc)
-				Duel.SetSelectedCard(mat)
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-				local mat2=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
-				mat:Merge(mat2)
-			end
-			tc:SetMaterial(mat)
-			Duel.ReleaseRitualMaterial(mat)
-			Duel.BreakEffect()
-			Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
-			tc:CompleteProcedure()
-		end
-	else
-		s.fusTarget(e,tp,eg,ep,ev,re,r,rp,1)
-		local chkf=tp
-		local mg1=Duel.GetFusionMaterial(tp):Filter(aux.NOT(Card.IsImmuneToEffect),nil,e)
-		local sg1=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-		local mg2=nil
-		local sg2=nil
-		local ce=Duel.GetChainMaterial(tp)
-		if ce~=nil then
-			local fgroup=ce:GetTarget()
-			mg2=fgroup(ce,e,tp)
-			local mf=ce:GetValue()
-			sg2=Duel.GetMatchingGroup(s.fusfilter,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,chkf)
-		end
-		if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
-			local sg=sg1:Clone()
-			if sg2 then sg:Merge(sg2) end
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local tg=sg:Select(tp,1,1,nil)
-			local tc=tg:GetFirst()
-			if sg1:IsContains(tc) and (sg2==nil or not sg2:IsContains(tc) or not Duel.SelectYesNo(tp,ce:GetDescription())) then
-				local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-				tc:SetMaterial(mat1)
-				Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-				Duel.BreakEffect()
-				Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-			else
-				local mat2=Duel.SelectFusionMaterial(tp,tc,mg2,nil,chkf)
-				local fop=ce:GetOperation()
-				fop(ce,e,tp,tc,mat2)
-			end
-			tc:CompleteProcedure()
-		end
-	end
+	--Flip this card over
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
-end
---ritual
-function s.ritDiscardFilter(c,e,tp,m,ft)
-	return c:IsDiscardable() and Duel.IsExistingMatchingCard(s.ritfilter,tp,LOCATION_HAND,0,1,c,e,tp,m-c,ft)
-end
-function s.ritual_filter(c)
-	return c:IsRitualMonster() and c:IsCode(64631466)
-end
-function s.ritfilter(c,e,tp,m,ft)
-	if not s.ritual_filter(c) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
-	if ft>0 then
-		return mg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
-	else
-		return mg:IsExists(s.mfilterf,1,nil,tp,mg,c)
-	end
-end
-function s.mfilterf(c,tp,mg,rc)
-	if c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) then
-		Duel.SetSelectedCard(c)
-		return mg:CheckWithSumGreater(Card.GetRitualLevel,rc:GetLevel(),rc)
-	else
-		return false
-	end
-end
-function s.ritTarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	local mg=Duel.GetRitualMaterial(tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return ft>-1 and Duel.IsExistingMatchingCard(s.ritDiscardFilter,tp,LOCATION_HAND,0,1,nil,e,tp,mg,ft) end
-	Duel.DiscardHand(tp,s.ritDiscardFilter,1,1,REASON_COST+REASON_DISCARD,nil,e,tp,mg,ft)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-end
---fusion
-function s.cfilter(c,e,tp)
-	if not c:IsDiscardable() then return false end
-	local chkf=tp
-	local mg1=Duel.GetFusionMaterial(tp)
-	local res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg1-c,nil,chkf)
-	if not res then
-		local ce=Duel.GetChainMaterial(tp)
-		if ce~=nil then
-			local fgroup=ce:GetTarget()
-			local mg2=fgroup(ce,e,tp)
-			local mf=ce:GetValue()
-			res=Duel.IsExistingMatchingCard(s.fusfilter,tp,LOCATION_EXTRA,0,1,c,e,tp,mg2-c,mf,chkf)
-		end
-	end
-	return res
-end
-function s.fusfilter(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and (not f or f(c)) and c:IsCode(63519819)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-function s.fusTarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.DiscardHand(tp,s.cfilter,1,1,REASON_COST+REASON_DISCARD,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end

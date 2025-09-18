eb444e8718f285ba6d804ef86e584686febfa58f
Naim <naimsantos45@gmail.com>
2025-09-18 19:56:11 -0300

    "Darklord Uprising" fix
    Fixed a bug where the player would not be able to activate it sometimes when their monster zones were all occupied

--- official/c54527349.lua
+++ official/c54527349.lua
@@ -3,65 +3,60 @@
 --Scripted by edo9300
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),extrafil=s.extrafil,stage2=s.stage2})
-	local tg=e1:GetTarget()
-	local op=e1:GetOperation()
-	e1:SetTarget(s.target(tg))
-	e1:SetOperation(s.operation(op))
-	e1:SetCost(s.cost(tg))
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON+CATEGORY_RECOVER)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_DARKLORD}
-function s.target(target)
-	return function(e,tp,eg,ep,ev,re,r,rp,chk)
-		if chk==0 then
-			if e:GetLabel()==0 then
-				return target(e,tp,eg,ep,ev,re,r,rp,0)
-			end
-			e:SetLabel(0)
-			return true
-		end
-		e:SetLabel(0)
-		return target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.fextra(exc)
+	return function(e,tp,mg)
+		return nil,s.costcardcheck(exc)
 	end
 end
-function s.operation(operation)
-	return function(e,...)
-		e:SetLabel(1)
-		local res=operation(e,...)
-		e:SetLabel(0)
-		return res
-	end
-end
-function s.check(e)
+function s.costcardcheck(exc)
 	return function(tp,sg,fc)
-		return e:GetLabel()==1 or (not e:GetLabelObject() or not sg:IsContains(e:GetLabelObject()))
+		return not (exc and sg:IsContains(exc))
 	end
 end
-function s.extrafil(e,tp,mg1)
-	return nil,s.check(e)
+function s.costfilter(c,e,tp,eg,ep,ev,re,r,rp)
+	if not (c:IsSetCard(SET_DARKLORD) and c:IsMonster() and (c:IsLocation(LOCATION_HAND) or c:IsFaceup()) and c:IsAbleToGraveAsCost()) then return false end
+	local params={fusfilter=aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),extrafil=s.fextra(c)}
+	return Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
 end
-function s.costfilter(c,target,e,tp,eg,ep,ev,re,r,rp,chk)
-	e:SetLabelObject(c)
-	local res=c:IsSetCard(SET_DARKLORD) and c:IsMonster() and (c:IsLocation(LOCATION_HAND) or c:IsFaceup())
-				and c:IsAbleToGraveAsCost() and target(e,tp,eg,ep,ev,re,r,rp,0)
-	e:SetLabelObject(nil)
-	return res
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,nil,e,tp,eg,ep,ev,re,r,rp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,nil,e,tp,eg,ep,ev,re,r,rp)
+	Duel.SendtoGrave(g,REASON_COST)
+	e:SetLabelObject(g:GetFirst())
 end
-function s.cost(target)
-	return function(e,tp,eg,ep,ev,re,r,rp,chk)
-		if chk==0 then
-			e:SetLabel(0)
-			local res=Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,nil,target,e,tp,eg,ep,ev,re,r,rp,chk)
-			if res then e:SetLabel(1) end
-			return res
-		end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,nil,target,e,tp,eg,ep,ev,re,r,rp,chk)
-		Duel.SendtoGrave(g,REASON_COST)
-		e:SetLabelObject(g:GetFirst())
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local params={fusfilter=aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK)}
+		return e:GetLabel()==100 or Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
 	end
+	local cost_card=e:GetLabelObject()
+	if cost_card then
+		Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,1,tp,cost_card:GetBaseAttack())
+	end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local cost_card=e:GetLabelObject()
+	local params={}
+	if cost_card then
+		params={fusfilter=aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),extrafil=s.fextra(cost_card),stage2=s.stage2}
+	else
+		params={fusfilter=aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK)}
+	end
+	Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp,1)
 end
 function s.stage2(e,tc,tp,sg,chk)
 	if chk==1 and e:IsHasType(EFFECT_TYPE_ACTIVATE) and e:GetLabelObject() then
@@ -70,7 +65,6 @@ function s.stage2(e,tc,tp,sg,chk)
 			Duel.BreakEffect()
 			Duel.Recover(tp,lp,REASON_EFFECT)
 		end
-		e:SetLabel(0)
 		e:SetLabelObject(nil)
 	end
 end

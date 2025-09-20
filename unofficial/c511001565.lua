4b18f6696282adc580d8371612db7f00ac2800a0
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-09-20 16:35:00 -0700

    Update "Performage Mirror Conductor (Anime)"
    - Should not make Link Monster ATK 0 (they technically have no defense)
    - Other minor script polishing and updates for modernization purposes.

--- unofficial/c511001565.lua
+++ unofficial/c511001565.lua
@@ -1,68 +1,64 @@
---Performage Mirror Conductor
+--Ｅｍミラー・コンダクター (Anime)
+--Performage Mirror Conductor (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--pendulum summon
+	--Pendulum Summon procedure
 	Pendulum.AddProcedure(c)
-	--lowest value
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SET_BASE_ATTACK)
-	e1:SetRange(LOCATION_PZONE)
-	e1:SetTargetRange(0,LOCATION_MZONE)
-	e1:SetTarget(s.target)
-	e1:SetValue(s.val)
+	--The original ATK and DEF of each Special Summoned monster your opponent controls become equal to the lowest value 
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_FIELD)
+	e1a:SetCode(EFFECT_SET_BASE_ATTACK)
+	e1a:SetRange(LOCATION_PZONE)
+	e1a:SetTargetRange(0,LOCATION_MZONE)
+	e1a:SetTarget(aux.TargetBoolFunction(Card.IsSpecialSummoned))
+	e1a:SetValue(s.atkdefval)
 	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+	local e1b=e1a:Clone()
+	e1b:SetCode(EFFECT_SET_BASE_DEFENSE)
+	c:RegisterEffect(e1b)
+	--When this card battle an opponent's monster: Target that monster, then switch its ATK/DEF during that damage calculation only
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetCondition(s.adswapcon)
+	e2:SetTarget(s.adswaptg)
+	e2:SetOperation(s.adswapop)
 	c:RegisterEffect(e2)
-	--switch
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
-	e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
-	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetCondition(s.con)
-	e3:SetTarget(s.tg)
-	e3:SetOperation(s.op)
-	c:RegisterEffect(e3)
 end
-function s.val(e,c)
-	if c:GetBaseAttack()<=c:GetBaseDefense() then
+function s.atkdefval(e,c)
+	if c:GetBaseAttack()<=c:GetBaseDefense() or not c:HasDefense() then
 		return c:GetBaseAttack()
 	else
 		return c:GetBaseDefense()
 	end
 end
-function s.target(e,c)
-	return (c:GetSummonType()&SUMMON_TYPE_SPECIAL)~=0
-end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
+function s.adswapcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local bc=c:GetBattleTarget()
-	return bc
+	return c:IsRelateToBattle() and bc and bc:IsControler(1-tp) and bc:IsRelateToBattle() and bc:IsFaceup()
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.adswaptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local bc=e:GetHandler():GetBattleTarget()
 	if chkc then return chkc==bc end
 	if chk==0 then return bc:IsOnField() and bc:IsCanBeEffectTarget(e) end
 	Duel.SetTargetCard(bc)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.adswapop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local atk=tc:GetAttack()
-		local def=tc:GetDefense()
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
-		e1:SetValue(def)
+		e1:SetReset(RESET_PHASE|PHASE_DAMAGE_CAL)
+		e1:SetValue(tc:GetDefense())
 		tc:RegisterEffect(e1)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
-		e2:SetValue(atk)
+		e2:SetValue(tc:GetAttack())
 		tc:RegisterEffect(e2)
 	end
 end

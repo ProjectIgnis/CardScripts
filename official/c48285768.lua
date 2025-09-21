69f3d0258a000a10e7ef9b4bc449bdaf2454dbe8
Naim <naimsantos45@gmail.com>
2025-09-21 05:16:50 -0300

    "Springans Merrymaker" fix
    It was not considered a card that lists "Fallen of Albaz"

--- official/c48285768.lua
+++ official/c48285768.lua
@@ -1,14 +1,13 @@
 --スプリガンズ・メリーメイカ
 --Springans Merrymaker
 --Logical Nonsense
---Substitute ID
 local s,id=GetID()
 function s.initial_effect(c)
 	--Xyz summon procedure
 	Xyz.AddProcedure(c,nil,4,2)
 	--Must be properly summoned before reviving
 	c:EnableReviveLimit()
-	--Send 1 "Sprigguns" monster from deck to GY
+	--Send 1 "Springans" monster from your Deck to GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOGRAVE)
@@ -16,14 +15,14 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(function(e)return e:GetHandler():IsPreviousLocation(LOCATION_EXTRA)end)
+	e1:SetCondition(function(e) return e:GetHandler():IsPreviousLocation(LOCATION_EXTRA) end)
 	e1:SetTarget(s.tgtg)
 	e1:SetOperation(s.tgop)
 	c:RegisterEffect(e1)
-	--Banish itself temporarily
+	--Banish this card until the End Phase
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_TOGRAVE)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetHintTiming(0,TIMING_MAIN_END|TIMING_BATTLE_START|TIMING_BATTLE_END)
@@ -34,18 +33,15 @@ function s.initial_effect(c)
 	e2:SetOperation(s.banop)
 	c:RegisterEffect(e2)
 end
-	--Lists "Sprigguns" archetype
+s.listed_names={CARD_ALBAZ}
 s.listed_series={SET_SPRINGANS}
-	--Check for a "Sprigguns" monster
 function s.tgfilter(c)
 	return c:IsMonster() and c:IsSetCard(SET_SPRINGANS) and c:IsAbleToGrave()
 end
-	--Activation legality
 function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
-	--Send 1 "Sprigguns" monster from deck to GY
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
@@ -53,19 +49,17 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
 end
-	--Check if current phase is opponent's main phase or battle phase
 function s.bancon(e,tp,eg,ep,ev,re,r,rp)
-	return (Duel.IsMainPhase() or Duel.IsBattlePhase()) and Duel.GetTurnPlayer()~=tp
+	return Duel.IsTurnPlayer(1-tp) and (Duel.IsMainPhase() or Duel.IsBattlePhase())
 end
 function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return e:GetHandler():IsAbleToRemove() end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 end
-	--Check for fusion monster that lists "Fallen of Albaz"
 function s.edfilter(c)
 	return c:IsType(TYPE_FUSION) and c:ListsCodeAsMaterial(CARD_ALBAZ)
 end
-	--Banish itself temporarily
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
@@ -79,12 +73,12 @@ function s.banop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCountLimit(1)
 		e1:SetOperation(s.retop)
 		Duel.RegisterEffect(e1,tp)
-		--Send 1 fusion monster that lists "Fallen of Albaz" from extra deck to GY
+		--Send 1 Fusion monster that lists "Fallen of Albaz" from your Extra Deck to GY
 		if ct>=2 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			Duel.BreakEffect()
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 			local g=Duel.SelectMatchingCard(tp,s.edfilter,tp,LOCATION_EXTRA,0,1,1,nil)
 			if #g>0 then
+				Duel.BreakEffect()
 				Duel.SendtoGrave(g,REASON_EFFECT)
 			end
 		end

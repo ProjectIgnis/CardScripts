f7a43af4085240c294910c8ef4067dfcbe79a37c
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-10-03 21:38:00 -0700

    Update "Dark Magic Curtain (Anime)"
    - Should apply a lingering effect in the vein of Last Will
    - Reworked script to properly apply cost/summoning

--- unofficial/c511002532.lua
+++ unofficial/c511002532.lua
@@ -1,6 +1,5 @@
 --黒魔術のカーテン (Anime)
 --Dark Magic Curtain (Anime)
---fixed by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -8,51 +7,48 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.spfilter(c,e,tp)
-	return c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
-	local p=tp
-	if Duel.IsExistingMatchingCard(s.spfilter,1-tp,LOCATION_DECK,0,1,nil,e,1-tp) 
-		and Duel.SelectYesNo(1-tp,aux.Stringid(102380,0)) then
-		Duel.PayLPCost(1-tp,math.floor(Duel.GetLP(1-tp)/2))
-		p=tp+1
-	end
-	e:SetLabel(p)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) 
-		and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSpecialSummon(1-tp) end
-	local p=e:GetLabel()
-	if p~=tp then
-		p=PLAYER_ALL
-	end
-	Duel.SetTargetPlayer(p)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,p==PLAYER_ALL and 2 or 1,p,LOCATION_DECK)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,PLAYER_EITHER,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-		if #g>0 then
-			Duel.SpecialSummonStep(g:GetFirst(),0,tp,tp,false,false,POS_FACEUP)
-		end
+	--You can Special Summon 1 DARK Spellcaster-Type monster from your Deck once this turn
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	--Your opponent can Special Summon 1 DARK Spellcaster-Type monster from their Deck once this turn
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.spcon)
+	e2:SetOperation(s.spop)
+	e2:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e2,1-tp)
+end
+function s.spfilter(c,e,sp)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_SPELLCASTER) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+end
+function s.spcon(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.GetMZoneCount(tp)>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+end
+function s.spop(e)
+	local tp=e:GetHandlerPlayer()
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.PayLPCost(tp,Duel.GetLP(tp)/2)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
-	if p==PLAYER_ALL and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(1-tp,s.spfilter,1-tp,LOCATION_DECK,0,1,1,nil,e,1-tp)
-		if #g>0 then
-			Duel.SpecialSummonStep(g:GetFirst(),0,1-tp,1-tp,false,false,POS_FACEUP)
-		end
-	end
-	Duel.SpecialSummonComplete()
 end

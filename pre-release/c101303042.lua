d739afec11635a426130c3bbcc3cd77acf2e0051
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-10-15 00:43:46 +0300

    Added new card scripts

---
+++ pre-release/c101303042.lua
@@ -0,0 +1,52 @@
+--鎖縛竜ザレン
+--Zaren the Chainbound Dragon
+--Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Synchro Summon procedure: 1 Tuner + 1+ Synchro Monsters
+	Synchro.AddProcedure(c,nil,1,1,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SYNCHRO),1,99)
+	--If this card you control would be used as Synchro Material, you can treat it as a non-Tuner
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_NONTUNER)
+	e1:SetRange(LOCATION_MZONE)
+	c:RegisterEffect(e1)
+	--Activate 1 of these effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(function() return Duel.GetCurrentChain()>1 end)
+	e2:SetTarget(s.distg)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsChainDisablable(ev)
+	local b2=Duel.IsChainDisablable(ev-1)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	local ch=ev-op+1
+	e:SetLabel(ch)
+	local eff=Duel.GetChainInfo(ch,CHAININFO_TRIGGERING_EFFECT)
+	local rc=eff:GetHandler()
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,rc,1,tp,0)
+	if rc:IsDestructable() and rc:IsRelateToEffect(eff) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,rc,1,tp,0)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local ch=e:GetLabel()
+	local eff=Duel.GetChainInfo(ch,CHAININFO_TRIGGERING_EFFECT)
+	local rc=eff:GetHandler()
+	if Duel.NegateEffect(ch) and rc:IsRelateToEffect(eff) then
+		Duel.Destroy(rc,REASON_EFFECT)
+	end
+end

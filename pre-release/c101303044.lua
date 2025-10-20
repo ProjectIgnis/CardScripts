28386ff2dc8cdcfc75e2c59dcc2ad7a6a1ed1f80
pyrQ <30827955+pyrQ@users.noreply.github.com>
2025-10-21 00:23:03 +0300

    Added new card scripts

---
+++ pre-release/c101303044.lua
@@ -0,0 +1,82 @@
+--エクソシスター・カルマエル
+--Exosister Karmael
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Xyz Summon procedure: 2 Level 4 "Exosister" monsters
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_EXOSISTER),4,2)
+	--Apply a "during the End Phase of this turn, Set 1 "Exosister" Spell/Trap from your Deck, GY, or banishment" effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function(e) return e:GetHandler():IsXyzSummoned() end)
+	e1:SetOperation(s.regop)
+	c:RegisterEffect(e1)
+	--Detach all materials from this card, and if you do, the activated effect becomes "Add 1 card from your opponent's GY to their hand"
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.chcon)
+	e2:SetTarget(s.chtg)
+	e2:SetOperation(s.chop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_EXOSISTER}
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	--During the End Phase of this turn, Set 1 "Exosister" Spell/Trap from your Deck, GY, or banishment
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.setop)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.setfilter(c)
+	return c:IsSetCard(SET_EXOSISTER) and c:IsSpellTrap() and c:IsSSetable() and (c:IsFaceup() or c:IsLocation(LOCATION_DECK))
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.setfilter),tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
+end
+function s.chcon(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re:GetHandler()
+	return rp==1-tp and (re:IsMonsterEffect() or (rc:IsNormalSpellTrap() and re:IsHasType(EFFECT_TYPE_ACTIVATE)))
+end
+function s.chtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local c=e:GetHandler()
+		local mat_ct=c:GetOverlayCount()
+		return mat_ct>0 and c:CheckRemoveOverlayCard(tp,mat_ct,REASON_EFFECT)
+			and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,LOCATION_GRAVE,0,1,nil)
+	end
+end
+function s.chop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local mg=c:GetOverlayGroup()
+	if c:IsRelateToEffect(e) and #mg>0 and Duel.SendtoGrave(mg,REASON_EFFECT)>0 then
+		local g=Group.CreateGroup()
+		Duel.ChangeTargetCard(ev,g)
+		Duel.ChangeChainOperation(ev,s.repop)
+	end
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,0,LOCATION_GRAVE,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end

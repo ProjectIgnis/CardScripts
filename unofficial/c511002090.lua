90b8609f71726eb0bd1990d246163442786b5220
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-11-03 23:57:59 -0800

    Update "Number 6: Chronomaly Atlandis (Anime)"
    - Misnamed filter was allowing non-Number monsters in GY to be targeted
    - General script polish

--- unofficial/c511002090.lua
+++ unofficial/c511002090.lua
@@ -1,67 +1,67 @@
+--Ｎｏ．６ 先史遺産－アトランタル (Anime)
 --Number 6: Chronomaly Atlandis (Anime)
 Duel.LoadCardScript("c9161357.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,6,2)
 	c:EnableReviveLimit()
-	--equip
+	--Xyz Summon procedure: 2 Level 6 monsters
+	Xyz.AddProcedure(c,nil,6,2)
+	--Cannot be destroyed by battle, except with "Number" monsters
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(9161357,0))
-	e1:SetCategory(CATEGORY_LEAVE_GRAVE+CATEGORY_EQUIP)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCondition(s.eqcon)
-	e1:SetTarget(s.eqtg)
-	e1:SetOperation(s.eqop)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
 	c:RegisterEffect(e1)
-	aux.AddEREquipLimit(c,s.eqcon,s.eqval,s.equipop,e1)
-	--lp
+	--When this card is Xyz Summoned: You can target 1 "Number" monster in your GY; equip it to this card.
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(9161357,1))
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetCountLimit(1)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCost(Cost.DetachFromSelf(1))
-	e2:SetOperation(s.lpop)
+	e2:SetDescription(aux.Stringid(9161357,0))
+	e2:SetCategory(CATEGORY_LEAVE_GRAVE+CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.eqcon)
+	e2:SetTarget(s.eqtg)
+	e2:SetOperation(s.eqop)
 	c:RegisterEffect(e2)
-	--lp - 0 materials
+	aux.AddEREquipLimit(c,s.eqcon,s.eqval,s.equipop,e2)
+	--Once per turn: You can detach 1 Xyz Material from this card; halve your opponent's LP
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(52090844,0))
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e3:SetRange(LOCATION_MZONE)
+	e3:SetDescription(aux.Stringid(9161357,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetCountLimit(1)
-	e3:SetCondition(s.hlpcon)
-	e3:SetOperation(s.hlpop)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCost(Cost.DetachFromSelf(1))
+	e3:SetOperation(function(e) local tp=e:GetHandlerPlayer() Duel.SetLP(1-tp,Duel.GetLP(1-tp)/2))
 	c:RegisterEffect(e3)
-	--indes
+	--If this card has no materials: Halve both players' LP during your Standby Phase
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e4:SetCondition(s.indcon)
-	e4:SetValue(1)
+	e4:SetDescription(aux.Stringid(52090844,0))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) and e:GetHandler():GetOverlayCount()==0 end)
+	e4:SetOperation(s.hlpop)
 	c:RegisterEffect(e4)
-	--avoid battle damage
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_FIELD)
-	e5:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e5:SetTargetRange(1,0)
-	e5:SetCondition(s.indcon)
-	c:RegisterEffect(e5)
-	--battle indestructable
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_SINGLE)
-	e6:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e6:SetValue(s.indes)
-	c:RegisterEffect(e6)
+	--This card cannot be destroyed by battle while your LP are 1000 or less by the above effect, also you take no battle damage
+	local e5a=Effect.CreateEffect(c)
+	e5a:SetType(EFFECT_TYPE_SINGLE)
+	e5a:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e5a:SetCondition(function(e) return Duel.HasFlagEffect(e:GetHandlerPlayer(),id) end)
+	e5a:SetValue(1)
+	c:RegisterEffect(e5a)
+	local e5b=Effect.CreateEffect(c)
+	e5b:SetType(EFFECT_TYPE_FIELD)
+	e5b:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5b:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e5b:SetCondition(function(e) return Duel.HasFlagEffect(e:GetHandlerPlayer(),id) end)
+	e5b:SetRange(LOCATION_MZONE)
+	e5b:SetTargetRange(1,0)
+	c:RegisterEffect(e5b)
+	--Check for LP to register battle indestructibility/damage avoidance
 	aux.GlobalCheck(s,function()
 		s[0]=0
 		s[1]=0
-		--lpcheck
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_ADJUST)
@@ -69,22 +69,35 @@ function s.initial_effect(c)
 		Duel.RegisterEffect(ge1,0)
 	end)
 end
+s.listed_series={SET_NUMBER}
 s.xyz_number=6
+function s.lpchk(e,tp,eg,ep,ev,re,r,rp)
+	local prevlp1=s[tp]
+	local prevlp2=s[1-tp]
+	if prevlp1<=1000 and Duel.GetLP(tp)>1000 then
+		Duel.ResetFlagEffect(tp,id)
+	end
+	if prevlp2<=1000 and Duel.GetLP(1-tp)>1000 then
+		Duel.ResetFlagEffect(1-tp,id)
+	end
+	s[tp]=Duel.GetLP(tp)
+	s[1-tp]=Duel.GetLP(1-tp)
+end
 function s.eqval(ec,c,tp)
 	return ec:IsControler(tp) and ec:IsSetCard(SET_NUMBER)
 end
 function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsXyzSummoned()
 end
-function s.filter(c)
+function s.eqfilter(c)
 	return c:IsSetCard(SET_NUMBER) and c:IsMonster()
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.eqfilter(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
+		and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,c9161357.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
@@ -96,7 +109,7 @@ function s.equipop(c,e,tp,tc)
 		e2:SetType(EFFECT_TYPE_EQUIP)
 		e2:SetProperty(EFFECT_FLAG_OWNER_RELATE+EFFECT_FLAG_IGNORE_IMMUNE)
 		e2:SetCode(EFFECT_UPDATE_ATTACK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
 		e2:SetValue(atk)
 		tc:RegisterEffect(e2)
 	end
@@ -109,35 +122,11 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 		s.equipop(c,e,tp,tc)
 	end
 end
-function s.lpop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetLP(1-tp,Duel.GetLP(1-tp)/2)
-end
-function s.hlpcon(e,tp,eg,ep,ev,re,r,rp)
-	return tp==Duel.GetTurnPlayer() and e:GetHandler():GetOverlayCount()==0
-end
-function s.hlpop(e,tp,eg,ep,ev,re,r,rp)
+function s.halvelpop(e,tp,eg,ep,ev,re,r,rp)
 	local lp=Duel.GetLP(tp)
 	Duel.SetLP(tp,lp/2)
 	Duel.SetLP(1-tp,Duel.GetLP(1-tp)/2)
 	if lp>1000 and Duel.GetLP(tp)<=1000 then
-		Duel.RegisterFlagEffect(tp,51102090,0,0,0)
+		Duel.RegisterFlagEffect(tp,id,0,0,0)
 	end
-end
-function s.indcon(e)
-	return Duel.GetFlagEffect(e:GetHandlerPlayer(),51102090)>0
-end
-function s.lpchk(e,tp,eg,ep,ev,re,r,rp)
-	local prevlp1=s[tp]
-	local prevlp2=s[1-tp]
-	if prevlp1<=1000 and Duel.GetLP(tp)>1000 then
-		Duel.ResetFlagEffect(tp,51102090)
-	end
-	if prevlp2<=1000 and Duel.GetLP(1-tp)>1000 then
-		Duel.ResetFlagEffect(1-tp,51102090)
-	end
-	s[tp]=Duel.GetLP(tp)
-	s[1-tp]=Duel.GetLP(1-tp)
-end
-function s.indes(e,c)
-	return not c:IsSetCard(SET_NUMBER)
 end

ac672e9a69da4a4fadbb14f1cab0fcb9412a5444
yoshiduels <yoshi.katreuvin@gmail.com>
2025-11-13 18:42:12 +0100

    added By Order of the Monarch

---
+++ rush/c160023060.lua
@@ -0,0 +1,30 @@
+--帝王の采配
+--By Order of the Monarch
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add 1 LIGHT/DARK Effect monster with 800 ATK and 1000 DEF
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.thfilter(c)
+	return c:IsMonster() and c:IsType(TYPE_EFFECT) and c:IsAttribute(ATTRIBUTE_LIGHT|ATTRIBUTE_DARK) and c:IsAttack(800) and c:IsDefense(1000) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

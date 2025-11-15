0be43b8cb5d8ea4643fa836c9d2fea2608b4765d
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-11-14 16:46:24 -0800

    Update "Speed Spell - Last Will"
    - Update script to match that of OCG "Last Will"/slight polishing

--- unofficial/c100100002.lua
+++ unofficial/c100100002.lua
@@ -1,4 +1,5 @@
 --Ｓｐ－遺言状
+--Speed Spell - Last Will
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -6,8 +7,27 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+	--Checks if a monster you control was sent to your GY this turn
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_TO_GRAVE)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+function s.checkfilter(c,tp)
+	return c:IsControler(tp) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	for p=0,1 do
+		if eg:IsExists(s.checkfilter,1,nil,p) then
+			Duel.RegisterFlagEffect(p,id,RESET_PHASE|PHASE_END,0,1)
+		end
+	end
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.GetFieldCard(tp,LOCATION_FZONE,0)
@@ -15,67 +35,46 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 	tc:RemoveCounter(tp,0x91,5,REASON_COST)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	--spsummon
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_TO_GRAVE)
-	e1:SetCondition(s.check)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCondition(s.spcon)
-	e2:SetOperation(s.spop)
-	e2:SetLabelObject(e1)
-	e2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
-	local e3=e2:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCondition(s.spcon2)
-	Duel.RegisterEffect(e3,tp)
-	local e4=e2:Clone()
-	e4:SetCode(EVENT_CHAIN_END)
-	Duel.RegisterEffect(e4,tp)
-	local e5=e2:Clone()
-	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
-	Duel.RegisterEffect(e5,tp)
-	local e6=e2:Clone()
-	e6:SetCode(EVENT_DAMAGE_STEP_END)
-	Duel.RegisterEffect(e6,tp)
-	local e7=e2:Clone()
-	e7:SetCode(EVENT_PHASE+PHASE_END)
-	Duel.RegisterEffect(e7,tp)
-end
-function s.cfilter(c,tp)
-	return c:IsControler(tp) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_MZONE)
-end
-function s.check(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==2 then return end
-	if eg:IsExists(s.cfilter,1,nil,tp) then
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if Duel.HasFlagEffect(tp,id) then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 		e:SetLabel(1)
+	else
+		e:SetCategory(0)
+		e:SetLabel(0)
 	end
 end
 function s.spfilter(c,e,tp)
 	return c:IsAttackBelow(1500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():GetLabel()==1
-end
-function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():GetLabel()==1 and Duel.GetCurrentChain()==0
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
 		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.Hint(HINT_CARD,0,id)
-		Duel.Hint(HINT_OPSELECTED,tp,HINTMSG_SPSUMMON)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		e:GetLabelObject():SetLabel(2)
 	else
-		e:GetLabelObject():SetLabel(0)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetCountLimit(1)
+		e1:SetCondition(s.spcon)
+		e1:SetOperation(s.spop)
+		e1:SetReset(RESET_PHASE|PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.HasFlagEffect(tp,id) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 end

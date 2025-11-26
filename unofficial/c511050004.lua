fa7df476f473c3fe84040420eef0375346ca1897
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-11-25 20:07:46 -0800

    Update "Frightfur Sheep (Anime)"
    - Should only be able to use effect when this card attacks
    - Should be a trigger, not quick
    - Minor script polish/optimization clean-up

--- unofficial/c511050004.lua
+++ unofficial/c511050004.lua
@@ -3,36 +3,31 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--Fusion Summon Procedure
+	--Fusion Materials: "Fluffal Sheep" + "Edge Imp Chain"
 	Fusion.AddProcMix(c,true,true,98280324,61173621)
-	--Your opponent cannot activate Spell/Trap cards and their effects until the end of the Battle Phase
+	--Your opponent cannot activate Spell/Trap cards until the end of the Battle Phase
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetHintTiming(TIMING_BATTLE_PHASE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
-	e1:SetCondition(s.accon)
-	e1:SetOperation(s.acop)
+	e1:SetCondition(function(e) return Duel.GetAttacker()==e:GetHandler() and not e:GetHandler():HasFlagEffect(id) end)
+	e1:SetOperation(s.actop)
 	c:RegisterEffect(e1)
 end
-function s.accon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetAttackTarget()==e:GetHandler() and not e:GetHandler():HasFlagEffect(id)
-end
-function s.acop(e,tp,eg,ep,ev,re,r,rp)
+s.material_setcode={SET_FLUFFAL,SET_EDGE_IMP}
+function s.actop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE,0,1)
-	--Prevent activations until the end of the Battle Phase
+	--Prevent Spell/Trap Card activations until the end of the Battle Phase
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
 	e1:SetTargetRange(0,1)
-	e1:SetValue(s.aclimit)
+	e1:SetValue(function(e,re,tp) return re:IsHasType(EFFECT_TYPE_ACTIVATE) end)
 	e1:SetReset(RESET_PHASE|PHASE_BATTLE)
 	Duel.RegisterEffect(e1,tp)
-end
-function s.aclimit(e,re,tp)
-	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
-end
+end	

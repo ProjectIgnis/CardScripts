640f2d03ecd02d32559b755653e04e179942a9dd
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-12-09 21:41:17 -0800

    Update "Magic Elf Order"
    -Incorrect category
    -Missing function call/serial declaration for unofficial archetype
    -Target missing legality check
    -Minor script polish

--- unofficial/c511002038.lua
+++ unofficial/c511002038.lua
@@ -1,35 +1,36 @@
 --デモンズ・チェーン
+--Magic Elf Order
+Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddPersistentProcedure(c,nil,aux.FilterBoolFunction(Card.IsFaceup),CATEGORY_DISABLE+CATEGORY_POSITION,nil,nil,TIMINGS_CHECK_MONSTER,s.condition,nil,s.target)
-	--disable
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_DISABLE)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetTarget(aux.PersistentTargetFilter)
-	c:RegisterEffect(e1)
-	--cannot change pos
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	aux.AddPersistentProcedure(c,nil,aux.FilterBoolFunction(Card.IsFaceup),CATEGORY_DISABLE,nil,nil,TIMINGS_CHECK_MONSTER,s.condition,nil,s.target)
+	--Targeted monster has its effects negated
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_FIELD)
+	e1a:SetCode(EFFECT_DISABLE)
+	e1a:SetRange(LOCATION_SZONE)
+	e1a:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1a:SetTarget(aux.PersistentTargetFilter)
+	c:RegisterEffect(e1a)
+	--Also it cannot change its position
+	local e1b=e1a:Clone()
+	e1b:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	c:RegisterEffect(e1b)
+	--Destroy this card if the targeted monster is destroyed
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(s.descon)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
-	--Destroy
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetCode(EVENT_LEAVE_FIELD)
-	e3:SetCondition(s.descon)
-	e3:SetOperation(s.desop)
-	c:RegisterEffect(e3)
 end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x151b)
-end
+s.listed_series={0x151b} --"Magic Elf" archetype
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x151b),tp,LOCATION_MZONE,0,1,nil)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,tc)
+function s.target(e,tp,eg,ep,ev,re,r,rp,tc,chk)
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,tc,1,0,0)
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)

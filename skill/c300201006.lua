1d1044b2d2d1f0f1478d69f4378fdf0980e6f97a
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-01-06 17:46:39 -0800

    Update "Millennium Necklace"
    - Should not be able to activate the first turn of the Duel as there is no Draw Phase/ability to conduct a normal draw.

--- skill/c300201006.lua
+++ skill/c300201006.lua
@@ -5,17 +5,18 @@ function s.initial_effect(c)
 	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--opd check
-	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--OPD check
+	if Duel.GetFlagEffect(tp,id)>0 then return end
 	--condition
-	return Duel.GetCurrentChain()==0 and Duel.GetTurnPlayer()~=tp
+	return Duel.GetCurrentChain()==0 and Duel.IsTurnPlayer(1-tp) and Duel.GetTurnCount()>1
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	--ask if you want to activate the skill or not
+	--Ask if you want to activate the skill or not
 	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--opd register
-	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	--OPD register
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	--Sort the top 3 cards of your opponnet's Deck
 	Duel.SortDecktop(tp,1-tp,3)
 end

1d244f8f5d1b9bc28e4df24a4ad99c17a92e1a2e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-02-21 10:02:34 -0800

    Update "Number iC1000: Numeronius Numeronia (Anime)"
    Script polish

--- unofficial/c511000296.lua
+++ unofficial/c511000296.lua
@@ -2,34 +2,41 @@
 --Number iC1000: Numeronius Numeronia
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,nil,13,5)
 	c:EnableReviveLimit()
-	--special summon
+	--Xyz Summon procedure: 5 Level 13 monsters
+	Xyz.AddProcedure(c,nil,13,5)
+	--Cannot be destroyed by battle, except with a "Number" monster
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e0:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
+	c:RegisterEffect(e8)
+	--When a "Number C1000: Numeronius" you control is destroyed: You can Xyz Summon this card from your Extra Deck, by using it as the Xyz Material 
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetRange(LOCATION_EXTRA)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetRange(LOCATION_EXTRA)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--indes effect
+	--Cannot be destroyed by card effects 
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--cannot attack
+	--Cannot declare an attack
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
 	c:RegisterEffect(e3)
+	--Your opponent must only attack this card while it is on the field
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_ONLY_BE_ATTACKED)
@@ -40,37 +47,33 @@ function s.initial_effect(c)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetTargetRange(0,LOCATION_MZONE)
 	c:RegisterEffect(e5)
-	--win
+	--If your opponent did not attack this card, you win the Duel during the End Phase
 	local e6=Effect.CreateEffect(c)
 	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY)
-	e6:SetRange(LOCATION_MZONE)
+	e6:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY)
 	e6:SetCode(EVENT_PHASE+PHASE_END)
+	e6:SetRange(LOCATION_MZONE)
 	e6:SetCountLimit(1)
-	e6:SetCondition(s.wincon)
-	e6:SetOperation(s.winop)
+	e6:SetCondition(function(e,tp) return Duel.IsTurnPlayer(1-tp) and not Duel.HasFlagEffect(tp,id) end)
+	e6:SetOperation(function(e) Duel.Win(e:GetHandlerPlayer(),WIN_REASON_NUMBER_iC1000) end)
 	c:RegisterEffect(e6)
-	--negate attack
+	--Once per turn, when this card is attacked: You can detach 1 material from this card; negate the attack, then gain LP equal to the attacking monster's ATK
 	local e7=Effect.CreateEffect(c)
 	e7:SetCategory(CATEGORY_RECOVER)
 	e7:SetDescription(aux.Stringid(id,1))
 	e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e7:SetCode(EVENT_BE_BATTLE_TARGET)
 	e7:SetRange(LOCATION_MZONE)
-	e7:SetCondition(s.nacon)
+	e7:SetCondition(s.negatkcon)
 	e7:SetCost(Cost.DetachFromSelf(1))
-	e7:SetTarget(s.natg)
-	e7:SetOperation(s.naop)
+	e7:SetTarget(s.negatktg)
+	e7:SetOperation(s.negatkop)
 	c:RegisterEffect(e7)
-	--number generic effect
-	local e8=Effect.CreateEffect(c)
-	e8:SetType(EFFECT_TYPE_SINGLE)
-	e8:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e8:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,SET_NUMBER)))
-	c:RegisterEffect(e8)
+	--Check for attacks performed by the opponent
 	aux.GlobalCheck(s,function()
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 		ge1:SetCode(EVENT_ATTACK_ANNOUNCE)
 		ge1:SetOperation(s.check)
 		Duel.RegisterEffect(ge1,0)
@@ -78,11 +81,11 @@ function s.initial_effect(c)
 		Duel.RegisterEffect(ge2,1)
 	end)
 end
-s.listed_series={0x48}
-s.listed_names={89477759}
+s.listed_series={SET_NUMBER}
+s.listed_names={89477759} --"Number C1000: Numeronious"
 s.xyz_number=1000
 function s.check(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.IsTurnPlayer(tp) or Duel.GetFlagEffect(tp,id)~=0 then return end
+	if Duel.IsTurnPlayer(tp) or Duel.GetFlagEffect(tp,id)>0 then return end
 	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
 end
 function s.cfilter(c,e,tp,xyz)
@@ -114,21 +117,15 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		c:CompleteProcedure()
 	end
 end
-function s.wincon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsTurnPlayer(1-tp) and Duel.GetFlagEffect(tp,id)==0
-end
-function s.winop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Win(tp,WIN_REASON_NUMBER_iC1000)
-end
-function s.nacon(e,tp,eg,ep,ev,re,r,rp)
+function s.negatkacon(e,tp,eg,ep,ev,re,r,rp)
 	local d=Duel.GetAttackTarget()
 	return d and d:IsControler(tp)
 end
-function s.natg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.negatktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetAttacker():IsOnField() end
 	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,Duel.GetAttacker():GetAttack())
 end
-function s.naop(e,tp,eg,ep,ev,re,r,rp)
+function s.negatkop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateAttack() then
 		Duel.Recover(tp,Duel.GetAttacker():GetAttack(),REASON_EFFECT)
 	end

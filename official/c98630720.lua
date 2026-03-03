39df2000fe07586ede84869261f4752a68ca816a
Naim <naimsantos45@gmail.com>
2026-03-03 19:22:19 -0300

    "Borrelend Dragon" fix
    Fixed a bug in which its effect that prevent it from being destroyed by card effects would apply while it was in the Spell/Trap Zone

--- official/c98630720.lua
+++ official/c98630720.lua
@@ -4,26 +4,29 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	--Link Summon procedure: 3+ Effect Monsters
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3)
-	--Cannot be targeted
+	--Neither player can target this card with monster effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(s.etg)
+	e1:SetValue(function (e,re,rp) return re:IsMonsterEffect() end)
 	c:RegisterEffect(e1)
-	--Cannot be destroyed
+	--Cannot be destroyed by battle
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
+	--Cannot be destroyed by card effects
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	c:RegisterEffect(e3)
-	--Attack all monsters
+	--This card can attack all monsters your opponent controls
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_ATTACK_ALL)
@@ -44,9 +47,6 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 s.listed_series={SET_ROKKET}
-function s.etg(e,re,rp)
-	return re:IsMonsterEffect()
-end
 function s.disfilter(c)
 	return c:IsFaceup() and not c:IsDisabled() and c:IsType(TYPE_EFFECT)
 end

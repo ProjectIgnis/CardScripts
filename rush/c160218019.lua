95112e0abd6eeb833dd321d68ed550e5fb4565b7
yoshiduels <yoshi.katreuvin@gmail.com>
2026-03-03 22:13:57 +0100

    fixed Handbook to the Spiritual Arts

--- rush/c160218019.lua
+++ rush/c160218019.lua
@@ -9,9 +9,26 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_CHAIN_SOLVED)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+s.listed_names={160218019}
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if re:GetHandler():IsCode(160218019) then
+		Duel.RegisterFlagEffect(rp,id,RESET_PHASE|PHASE_END,0,1)
+	end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFlagEffect(tp,id)==0
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5 end

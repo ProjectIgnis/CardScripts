10193ac04c36547bff37bab390cbe28e05f2cbf0
Hatter <47074795+that-hatter@users.noreply.github.com>
2026-03-23 20:55:51 +0800

    update "Scareclaw Straddle"
    - added hint prompt on damage step

--- official/c41619242.lua
+++ official/c41619242.lua
@@ -3,20 +3,20 @@
 --Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	--Boost ATK of 1 "Scareclaw" monster or "Visas Stafrost"
+	--Target 1 "Scareclaw" monster or 1 "Visas Starfrost" in your Monster Zone, and 1 face-up monster your opponent controls; your monster gains ATK/DEF equal to your opponent's monster's ATK or DEF (whichever is higher)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E|TIMING_MAIN_END)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E|TIMING_MAIN_END|TIMING_DAMAGE_STEP)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetCondition(aux.StatChangeDamageStepCondition)
 	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
-	--Negate an effect that targets "Scareclaw" monster(s) and/or "Visas Stafrost"
+	--When a card or effect is activated that targets a "Scareclaw" monster or "Visas Starfrost" you control: Negate that effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DISABLE)
@@ -30,16 +30,15 @@ function s.initial_effect(c)
 end
 s.listed_names={CARD_VISAS_STARFROST}
 s.listed_series={SET_SCARECLAW}
-function s.atkfilter(c,e,tp)
-	return c:IsFaceup() and c:IsCanBeEffectTarget(e) 
-		and (c:IsSetCard(SET_SCARECLAW) or c:IsCode(CARD_VISAS_STARFROST) or c:IsControler(1-tp))
+function s.atkfilter(c,tp)
+	return c:IsFaceup() and ((c:IsSetCard(SET_SCARECLAW) or c:IsCode(CARD_VISAS_STARFROST)) or c:IsControler(1-tp))
 end
 function s.atkrescon(sg,e,tp,mg)
 	return sg:GetClassCount(Card.GetControler)==2
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e,tp)
+	local g=Duel.GetTargetGroup(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,tp)
 	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.atkrescon,0) end
 	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,s.atkrescon,1,tp,HINTMSG_FACEUP)
 	Duel.SetTargetCard(tg)
@@ -50,16 +49,11 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc,ac=g:GetFirst(),g:GetNext()
 	if tc:IsControler(1-tp) then tc,ac=ac,tc end
 	if tc:IsFaceup() and tc:IsControler(tp) and ac:IsFaceup() and ac:IsControler(1-tp) then
-		--Increase ATK/DEF
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-		e1:SetValue(math.max(ac:GetAttack(),ac:GetDefense()))
-		tc:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		tc:RegisterEffect(e2)
+		local c=e:GetHandler()
+		local val=math.max(ac:GetAttack(),ac:GetDefense())
+		--Your monster gains ATK/DEF equal to your opponent's monster's ATK or DEF (whichever is higher)
+		tc:UpdateAttack(val,nil,c)
+		tc:UpdateDefense(val,nil,c)
 	end
 end
 function s.disconfilter(c,tp)

504827e9b4185fe93338a4e9a2f7cafcfdc616e8
Hatter <47074795+that-hatter@users.noreply.github.com>
2026-04-02 08:33:27 +0800

    fix "Number C104: Umbral Horror Masquerade"
    - second effect should not halve the opponent's LP if it failed to send the random card from their hand to the GY
    - modernized script

--- official/c49456901.lua
+++ official/c49456901.lua
@@ -2,10 +2,10 @@
 --Number C104: Umbral Horror Masquerade
 local s,id=GetID()
 function s.initial_effect(c)
-	--Xyz summon
-	Xyz.AddProcedure(c,nil,5,4)
 	c:EnableReviveLimit()
-	--Destroy
+	--Xyz Summon procedure: 4 Level 5 monsters
+	Xyz.AddProcedure(c,nil,5,4)
+	--When this card is Special Summoned: You can target 1 Spell/Trap Card on the field; destroy that target
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
@@ -15,55 +15,54 @@ function s.initial_effect(c)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
-	--Negate activate
+	--Once per turn, during either player's turn, when a monster effect is activated on your opponent's side of the field: You can detach 1 Xyz Material from this card; negate the activation, then you can send 1 random card from your opponent's hand to the Graveyard, and if you do, halve your opponent's Life Points
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_HANDES)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetCondition(s.condition)
+	e2:SetCondition(s.negcon)
 	e2:SetCost(Cost.DetachFromSelf(1))
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
 	aux.DoubleSnareValidity(c,LOCATION_MZONE)
 end
 s.xyz_number=104
-s.listed_names={2061963}
-function s.desfilter(c)
-	return c:IsSpellTrap()
-end
+s.listed_names={2061963} --"Number 104: Masquerade"
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and s.desfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	if chkc then return chkc:IsOnField() and chkc:IsSpellTrap() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local g=Duel.SelectTarget(tp,Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.negcon(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
-	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep==1-tp
+	return not c:IsStatus(STATUS_BATTLE_DESTROYED) and ep==1-tp
 		and loc==LOCATION_MZONE and re:IsMonsterEffect() and Duel.IsChainNegatable(ev)
-		and e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode,1,nil,2061963)
+		and c:GetOverlayGroup():IsExists(Card.IsCode,1,nil,2061963)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.NegateActivation(ev) and Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)>0
-		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-		local g=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0):RandomSelect(1-tp,1)
-		Duel.SendtoGrave(g,REASON_EFFECT)
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.NegateActivation(ev) then return end
+	local g=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
+	if #g==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,2)) then return end
+	local sc=g:RandomSelect(1-tp,1):GetFirst()
+	if sc and Duel.SendtoGrave(sc,REASON_EFFECT)>0 and sc:IsLocation(LOCATION_GRAVE) then
 		Duel.SetLP(1-tp,Duel.GetLP(1-tp)/2)
 	end
 end

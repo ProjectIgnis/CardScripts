4d714db24c36cffe5fec4153a3bc7955d9f6ea69
Naim <naimsantos45@gmail.com>
2026-04-04 19:09:31 -0300

    "Trap Gatherer" fix
    Fixed a bug where it would be possible to activate its effect when any monster inflicted battle damage, not only the equip target

--- official/c24839398.lua
+++ official/c24839398.lua
@@ -38,8 +38,8 @@ function s.setcon(e,tp,eg,ep,ev,re,r,rp)
 	local ec=e:GetHandler():GetEquipTarget()
 	if not ec then return false end
 	local bc=ec:GetBattleTarget()
-	local ex,_,damp=Duel.CheckEvent(EVENT_BATTLE_DAMAGE,true)
-	return (bc and bc:IsStatus(STATUS_BATTLE_DESTROYED) and bc:IsControler(1-tp)) or (ex and damp==1-tp)
+	local ex,damg,damp=Duel.CheckEvent(EVENT_BATTLE_DAMAGE,true)
+	return (bc and bc:IsStatus(STATUS_BATTLE_DESTROYED) and bc:IsControler(1-tp)) or (ex and damp==1-tp and damg:GetFirst()==ec)
 end
 function s.setfilter(c,ignore_zones)
 	return c:IsTrap() and c:IsSSetable(ignore_zones)

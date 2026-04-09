007408c06e1deb566cdb9be04f30e7bd91dd47b2
Hatter <47074795+that-hatter@users.noreply.github.com>
2026-04-09 19:40:59 +0800

    fix "Gunkan Suship Shirauo"
    - was not able to place "Gunkan Suship Shari" to the top of the Deck if the only copies of Shari are already in the Deck, due to the recent change in Card.IsAbleToDeck (https://github.com/ProjectIgnis/CardScripts/commit/ce95c9c941483b0579b5a4554af4f9d73088dd97)
    - modernized script

--- official/c78362751.lua
+++ official/c78362751.lua
@@ -1,86 +1,77 @@
 --しらうおの軍貫
 --Gunkan Suship Shirauo
 --Logical Nonsense
---Substitute ID
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special summon itself from hand
+	--If you control a "Gunkan Suship Shari", or an Xyz Monster that has "Gunkan Suship Shari" as material: You can Special Summon this card from your hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetCondition(s.selfspcon)
+	e1:SetTarget(s.selfsptg)
+	e1:SetOperation(s.selfspop)
 	c:RegisterEffect(e1)
-	--Special summon 1 "Suship" monster from hand
+	--During your Main Phase: You can Special Summon 1 "Gunkan" monster from your hand, except "Gunkan Suship Shirauo", then you can take any number of "Gunkan Suship Shari" from your Deck or GY and place them on top of your Deck in any order
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetTarget(s.sstg)
-	e2:SetOperation(s.ssop)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
-	--Lists "Gunkan" archetype
 s.listed_series={SET_GUNKAN}
-	--Specifically lists itself and "Gunkan Suship Shari"
 s.listed_names={id,CARD_SUSHIP_SHARI}
-	--Check for a "Gunkan Suship Shari" you control (in MZ or as overlay material)
-function s.xyzfilter(c)
-	return c:IsFaceup() and (c:IsCode(CARD_SUSHIP_SHARI) or (c:GetOverlayCount()>0 and c:GetOverlayGroup():IsExists(Card.IsCode,1,nil,CARD_SUSHIP_SHARI)))
+function s.selfspconfilter(c)
+	return c:IsFaceup() and (c:IsCode(CARD_SUSHIP_SHARI) or c:GetOverlayGroup():IsExists(Card.IsCode,1,nil,CARD_SUSHIP_SHARI))
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil)
+function s.selfspcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.selfspconfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-	--Activation legality
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
-	--Special summon itself from hand
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
-	--Check for a "Gunkan" monster, except "Gunkan Suship Shirauo"
-function s.ssfilter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsSetCard(SET_GUNKAN) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-	--Activation legality
-function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.ssfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
 end
-	--Check for "Gunkan Sushup Shari"
 function s.tdfilter(c)
-	return c:IsCode(CARD_SUSHIP_SHARI) and c:IsAbleToDeck()
+	return c:IsCode(CARD_SUSHIP_SHARI) and (c:IsLocation(LOCATION_DECK) or c:IsAbleToDeck())
 end
-	--Special summon 1 "Gunkan" monster from hand, except "Gunkan Suship Shirauo"
-function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.ssfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,nil)
-		if #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			--Move any number of "Gunkan Suship Shari" from deck/GY to top of deck
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-			local tg=sg:Select(tp,1,#sg,nil)
-			Duel.HintSelection(tg)
-			Duel.SendtoDeck(tg,nil,SEQ_DECKTOP,REASON_EFFECT)
-			Duel.MoveToDeckTop(tg)
-			if #tg<=1 then return end
-			Duel.SortDecktop(tp,tp,#tg)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g==0 or Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)==0 then return end
+	local dg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.tdfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,nil)
+	if #dg==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,2)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=dg:Select(tp,1,#dg,nil)
+	if #sg>0 then
+		Duel.BreakEffect()
+		Duel.HintSelection(sg)
+		Duel.SendtoDeck(sg,nil,SEQ_DECKTOP,REASON_EFFECT)
+		Duel.MoveToDeckTop(sg)
+		if #sg>1 then
+			Duel.SortDecktop(tp,tp,#sg)
 		end
 	end
 end

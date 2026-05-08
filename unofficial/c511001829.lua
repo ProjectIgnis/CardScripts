9f43e90df51ad691c5012911d1017c80bf05e26a
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-08 07:41:33 -0700

    Update "Photon Hurricane"
    - Should target the cards to be returned

--- unofficial/c511001829.lua
+++ unofficial/c511001829.lua
@@ -1,3 +1,4 @@
+--フォトン・ハリケーン
 --Photon Hurricane
 local s,id=GetID()
 function s.initial_effect(c)
@@ -5,6 +6,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
@@ -14,19 +16,17 @@ function s.filter(c)
 	return c:IsSpellTrap() and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) and s.filter(chkc) end
 	local c=e:GetHandler()
 	local ct=Duel.GetMatchingGroupCount(nil,tp,LOCATION_HAND,0,e:GetHandler())
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_ONFIELD,1,c) 
-		and ct>0 end
-	local sg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_ONFIELD,c)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,sg,1,0,0)
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_ONFIELD,1,c) and ct>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_ONFIELD,1,ct,c)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.GetMatchingGroupCount(nil,tp,LOCATION_HAND,0,e:GetHandler())
-	local rg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_ONFIELD,nil)
-	if ct<=0 then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local sg=rg:Select(tp,1,ct,nil)
-	Duel.HintSelection(sg)
-	Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	local sg=Duel.GetTargetCards(e)
+	if #sg>0 then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	end
 end

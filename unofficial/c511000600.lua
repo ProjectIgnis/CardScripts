d07e1819f054d8d7db31cb0cba876778f9683664
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-07 18:54:40 -0700

    Update "Intervention of Fate"
    - Should only trigger on Normal Summons

--- unofficial/c511000600.lua
+++ unofficial/c511000600.lua
@@ -1,3 +1,4 @@
+--運命の介在
 --Intervention of Fate
 local s,id=GetID()
 function s.initial_effect(c)
@@ -5,26 +6,13 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetRange(LOCATION_HAND)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(s.con)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(function(e,tp,eg) return eg:IsExists(aux.FilterEqualFunction(Card.GetSummonPlayer,1-tp),1,nil) end)
+	e1:SetCost(Cost.SelfToGrave)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	c:RegisterEffect(e2)
-	local e3=e1:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e3)
-end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(aux.FilterEqualFunction(Card.GetSummonPlayer,1-tp),1,nil)
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function s.filter(c,tp)
 	local te=c:GetActivateEffect()
@@ -36,12 +24,12 @@ function s.filter(c,tp)
 			if type(prev)~='function' or prev(eff,te,tp) then return false end
 		end
 	end
-	return c:GetType()==TYPE_SPELL and c:CheckActivateEffect(false,false,false)~=nil
+	return c:IsNormalSpell() and c:CheckActivateEffect(false,false,false)~=nil
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,tp) end
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,tp):GetFirst()
 	local tpe=tc:GetType()
 	local te=tc:GetActivateEffect()

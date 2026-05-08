63991910a2bc561ab304383cd693ea3f98b1c396
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-08 12:16:09 -0700

    Update "Seven Kid Goats"
    - Effect to destroy isn't optional and doesn't target
    - Should only activate when it is Normal or Flip Summoned

--- unofficial/c511000438.lua
+++ unofficial/c511000438.lua
@@ -1,44 +1,35 @@
+--七匹の子ヤギ
 --Seven Kid Goats
---Jackpro 1.3
 local s,id=GetID()
 function s.initial_effect(c)
-	--destroy
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(s.descon)
-	e1:SetTarget(s.destg)
-	e1:SetOperation(s.desop)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	c:RegisterEffect(e2)
-	local e3=e1:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e3)
-end
-function s.descon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:IsFaceup()
-end
-function s.filter(c)
-	return c:IsFaceup() and c:IsDestructable() and c:IsRace(RACE_BEAST)
+	--When this card is Normal or Flip Summoned, destroy 1 face-up Beast-Type monster your opponent controls and inflict damage to your opponent equal to the ATK of the destroyed monster.
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1a:SetCode(EVENT_SUMMON_SUCCESS)
+	e1a:SetTarget(s.destg)
+	e1a:SetOperation(s.desop)
+	c:RegisterEffect(e1a)
+	local e1b=e1a:Clone()
+	e1b:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e1b)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsRace,RACE_BEAST),tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if not tc then return end
-	local atk=tc:GetAttack()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
-		Duel.Damage(1-tp,atk,REASON_EFFECT)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsRace,RACE_BEAST),tp,0,LOCATION_MZONE,1,1,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		local tc=g:GetFirst()
+		local atk=tc:GetAttack()
+		if atk<0 then atk=0 end
+		if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
+			Duel.Damage(1-tp,atk,REASON_EFFECT)
+		end
 	end
 end

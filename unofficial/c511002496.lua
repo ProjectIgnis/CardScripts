86531ea34aedfeb3a213e142e064dbbc72d06e9d
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-10 09:24:26 -0700

    Update "Pixie Gong"
    - Monsters should only be able to Summoned in Attack Position

--- unofficial/c511002496.lua
+++ unofficial/c511002496.lua
@@ -1,22 +1,21 @@
+--妖精のゴング
 --Pixie Gong
 Duel.LoadScript("c420.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--When you take battle damage from a direct attack: Special Summon 1 Level 3 or lower "Pixie" or "Fairy" monster from your Deck in face-up Attack Position.
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetCode(EVENT_BATTLE_DAMAGE)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(function(e,tp,eg,ep,ev,re,r,rp)	return ep==tp and Duel.GetAttackTarget()==nil end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and Duel.GetAttackTarget()==nil
-end
+s.listed_series={0x53e,0x51d} --"Pixie", "Fairy" archetypes 
 function s.spfilter(c,e,tp)
-	return (c:IsFairy() or c:IsPixie()) and c:IsLevelBelow(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:IsFairy() or c:IsPixie()) and c:IsLevelBelow(3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -28,6 +27,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_ATTACK)
 	end
 end

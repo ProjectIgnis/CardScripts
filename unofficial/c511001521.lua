4cca236fc4c30fc8de73f98fc20e970c914e7c96
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-10 20:23:39 -0700

    Update "Cyber Petit Angel (Anime)"
    -Effect is a forced trigger
    -Should only activate on Normal or Flip Summon

--- unofficial/c511001521.lua
+++ unofficial/c511001521.lua
@@ -1,33 +1,31 @@
---Cyber Petit Angel
+--サイバー・プチ・エンジェル (Anime)
+--Cyber Petit Angel (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--search
+	--When this card is Normal or Flip Summoned: Add 1 "Machine Angel Ritual" from your Deck to your hand.
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	local e3=e1:Clone()
-	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-	c:RegisterEffect(e3)
 end
-s.listed_names={39996157}
-function s.filter(c)
+s.listed_names={39996157} --"Machine Angel Ritual"
+function s.thfilter(c)
 	return c:IsCode(39996157) and c:IsAbleToHand()
 end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)

22918fec16c45e2b53e0720774a23b44a026320c
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-12 12:02:35 -0700

    Update "Command Angel (Anime)"
    - Missing the effect to prevent it being targeted for attacks.
    - Script clean-up

--- unofficial/c511000653.lua
+++ unofficial/c511000653.lua
@@ -1,16 +1,23 @@
---Command Angel
+--コマンド・エンジェル (Anime)
+--Command Angel (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--atk def
+	--If you control another monster, monsters your opponent controls cannot target this card for attacks.
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetTarget(s.tg)
-	e1:SetValue(400)
+	e1:SetCondition(function(e) return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)>1 end)
+	e1:SetValue(aux.imval1)
 	c:RegisterEffect(e1)
-end
-function s.tg(e,c)
-	return c:IsRace(RACE_FAIRY)
+	--All Fairy-Type monsters gain 400 ATK while this card is face-up on the field
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(function(e,c) return c:IsFaceup() and c:IsRace(RACE_FAIRY) end)
+	e2:SetValue(400)
+	c:RegisterEffect(e2)
 end

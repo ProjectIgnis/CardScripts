c2df436dccf38a89644ab3ef3314187d9d6f32f0
Naim <naimsantos45@gmail.com>
2026-05-15 17:18:40 -0300

    "Witchcrafter Terracottan" update
    Fixed a bug in which it would be possible to Special Summon this card even when the target was not sucessfully sent to the hand

--- pre-release/c100455023.lua
+++ pre-release/c100455023.lua
@@ -45,7 +45,7 @@ end
 function s.thspop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
+	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) and c:IsRelateToEffect(e) then
 		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 	--You cannot Special Summon from the Extra Deck for the rest of this turn, except "Witchcrafter" monsters

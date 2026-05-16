c1fd942de514b0ef1056f53467b72f8cdfceb160
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-16 08:08:46 -0700

    Update "Necro Cycle"
    - Should be able to Special Summon "Necro Mannequin" from hand as well as Deck
    - Added effect to destroy itself
    - Condition moved to activation as text specifies

--- unofficial/c511000158.lua
+++ unofficial/c511000158.lua
@@ -1,47 +1,56 @@
+--ネクリサイクル
 --Necro Cycle
 --scripted by UnknownGuest
 local s,id=GetID()
 function s.initial_effect(c)
-	-- activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCondition(s.condition)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
 	c:RegisterEffect(e1)
-	-- special summon
+	--Special Summon 1 "Necro Mannquein" from your hand or Deck
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCountLimit(1)
-	e2:SetRange(LOCATION_SZONE)
 	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetCondition(s.spcon)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
+	--Destroy this card if you do not control a "Necro Mannequin"
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_SELF_DESTROY)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCondition(s.sdcon)
+	c:RegisterEffect(e1)
 end
-s.listed_names={511000157}
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsCode(511000157)
+s.listed_names={511000157} --"Necro Mannequin"
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,511000157),tp,LOCATION_MZONE,0,1,nil)
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
-end
-function s.filter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsCode(511000157) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
+end
+function s.sdcon(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,511000157),tp,LOCATION_MZONE,0,1,nil)
 end

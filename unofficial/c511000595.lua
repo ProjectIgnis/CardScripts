eada7a7c370747e7478855e79fc6554ea6c7034f
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-17 09:38:03 -0700

    Update "White Knight Lord (Anime)"
    - First effect should apply during each End Phase, and if the monster didn't battle.
    - Tribute restriction is general, not just for Tribute Summons
    - Fixed incorrect category/damage information
    - General script polish/modernization

--- unofficial/c511000595.lua
+++ unofficial/c511000595.lua
@@ -1,85 +1,100 @@
---White Knight Lord
+--白騎士団のロード (Anime)
+--White Knight Lord (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--
+	--During each End Phase, if this card did battle this turn: Take 800 damage.
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UNRELEASABLE_SUM)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCategory(CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_PHASE|PHASE_END)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(1)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(e) return e:GetHandler():GetBattledGroupCount()==0 end)
+	e1:SetTarget(s.selfepdamtg)
+	e1:SetOperation(s.selfepdamop)
 	c:RegisterEffect(e1)
-	--damage LP
+	--Cannot be destroyed by battle
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1)
-	e2:SetOperation(s.retop)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
-	--
+	--You take no battle damage from battles involving this card
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e4:SetValue(1)
-	c:RegisterEffect(e4)
-	--damage
+	--This card cannot be Tributed
+	local e4a=Effect.CreateEffect(c)
+	e4a:SetType(EFFECT_TYPE_SINGLE)
+	e4a:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4a:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e4a:SetRange(LOCATION_MZONE)
+	e4a:SetValue(1)
+	c:RegisterEffect(e4a)
+	local e4b=e4a:Clone()
+	e4b:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e4b)
+	--When this card destroys a monster by battle: Inflict 300 damage to your opponent
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,1))
 	e5:SetCategory(CATEGORY_DAMAGE)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e5:SetCode(EVENT_BATTLE_DESTROYING)
-	e5:SetCondition(s.damcon)
-	e5:SetTarget(s.damtg)
-	e5:SetOperation(s.damop)
+	e5:SetCondition(s.batdesdamcon)
+	e5:SetTarget(s.batdestg)
+	e5:SetOperation(s.batdesop)
 	c:RegisterEffect(e5)
-	--special summon
+	--When this card is destroyed by an opponent's card effect: Inflict 1000 damage to your opponent.
 	local e6=Effect.CreateEffect(c)
 	e6:SetDescription(aux.Stringid(id,2))
-	e6:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e6:SetCategory(CATEGORY_DAMAGE)
 	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e6:SetCode(EVENT_DESTROY)
-	e6:SetCondition(s.damcon2)
-	e6:SetTarget(s.damtg2)
-	e6:SetOperation(s.damop2)
+	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e6:SetCode(EVENT_DESTROYED)
+	e6:SetCondition(s.oppeffdesdamcon)
+	e6:SetTarget(s.oppeffdesdamtg)
+	e6:SetOperation(s.oppeffdesdamop)
 	c:RegisterEffect(e6)
 end
-function s.retop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.IsTurnPlayer(tp) and c:GetAttackedCount()==0 then
-		Duel.Damage(tp,800,REASON_EFFECT)
-	end
+function s.selfepdamdamtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(800)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,0,0,tp,800)
 end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+function s.selfepdamdamop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
+function s.batdesdamcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsRelateToBattle() and c:GetBattleTarget():IsMonster()
 end
-function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.batdesdamtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(300)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,300)
 end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
+function s.batdesdamop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end
-function s.damcon2(e,tp,eg,ep,ev,re,r,rp)
-	return rp~=tp and (r&0x41)==0x41
+function s.oppeffdesdamcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return rp==1-tp and c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp)
 end
-function s.damtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.oppeffdesdamtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(1000)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
 end
-function s.damop2(e,tp,eg,ep,ev,re,r,rp)
+function s.oppeffdesdamop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
 end

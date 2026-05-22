9b42303b96b498f99aa2b8dfa59badd89c92cbff
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-22 12:44:23 -0700

    Update "Invincible Demise Lord (Anime)"
    - Reworked effect to include once per duel limit.
    - General script polish

--- unofficial/c810000001.lua
+++ unofficial/c810000001.lua
@@ -1,61 +1,51 @@
--- Invincible Demise Lord
--- scripted by: UnknownGuest
+--不死のデスロード (Anime)
+--Invincible Demise Lord (Anime)
+--Scripted by: UnknownGuest
 local s,id=GetID()
 function s.initial_effect(c)
-	--to grave
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_BATTLE_DESTROYED)
-	e2:SetCountLimit(1,id)
-	e2:SetCost(s.spcost)
-	e2:SetOperation(s.regop)
-	c:RegisterEffect(e2)
-	--atk up and cannot be destroyed
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetCondition(s.upcon)
-	e3:SetOperation(s.upop)
-	c:RegisterEffect(e3)
+	--If this card is destroyed by battle and sent to the Graveyard: it will be Special Summoned during the End Phase with ATK of 3000, also it cannot be destroyed by card effects. You can only use this effect once in the Duel. 
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(function(e) return e:GetHandler():IsLocation(LOCATION_GRAVE) and e:GetHandler():IsReason(REASON_BATTLE))
+	e1:SetCost(s.regcost)
+	e1:SetOperation(s.regop)
+	c:RegisterEffect(e1)
 end
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+function s.regcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)
-end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFlagEffect(tp,888000025)==0 end
-	Duel.RegisterFlagEffect(tp,888000025,0,0,0)
+	if chk==0 then return not c:HasFlagEffect(id) end
+	c:RegisterFlagEffect(id,0,0,1)
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	-- reborn
+	--Special Summon this card from the GY during the End Phase
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetReset(RESET_PHASE+PHASE_END)
 	e2:SetCountLimit(1)
-	e2:SetOperation(s.spop2)
+	e2:SetOperation(s.selfspop)
+	e2:SetReset(RESET_PHASE|PHASE_END)
 	c:RegisterEffect(e2)
 end
-function s.upcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
-end
-function s.upop(e,tp,eg,ep,ev,re,r,rp)
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
 	local c=e:GetHandler()
-	if c:IsFaceup() and c:IsRelateToEffect(e) then
+	if c and c:IsLocation(LOCATION_GRAVE) and Duel.SpecialSummon(c,1,tp,tp,false,false,POS_FACEUP)>0 then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK)
 		e1:SetValue(3000)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(3001)
 		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
 		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
 		e2:SetValue(1)
 		c:RegisterEffect(e2)
 	end

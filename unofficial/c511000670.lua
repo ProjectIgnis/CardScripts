82d9fde067407439abc951abbbae2e7819e07049
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-30 11:25:58 -0700

    Update "Red-Eyes Darkness Metal Dragon (Anime)"
    -Is a full Nomi monster
    -Script modernization update

--- unofficial/c511000670.lua
+++ unofficial/c511000670.lua
@@ -1,63 +1,62 @@
+--レッドアイズ・ダークネスメタルドラゴン (Anime)
 --Red-Eyes Darkness Metal Dragon (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	--special summon
+	c:AddMustBeSpecialSummoned()
+	--Special Summon this card by Tributing 1 "Red-Eyes Darkness Dragon" equipped with "Metal Plus"
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_HAND)
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetCondition(s.hspcon)
+	e2:SetTarget(s.hsptg)
+	e2:SetOperation(s.hspop)
 	c:RegisterEffect(e2)
-	--atkup
+	--Gains 400 ATK for each Dragon-Type monster in your Graveyard
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetValue(s.val)
+	e3:SetValue(function(e,c) return Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_GRAVE,0,nil,RACE_DRAGON)*400 end)
 	c:RegisterEffect(e3)
-	--negate
+	--When a Spell Card or effect that targets this card is activated: You can negate that effect.
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetCategory(CATEGORY_DISABLE)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
-	e4:SetCode(EVENT_CHAINING)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e4:SetCode(EVENT_CHAINING)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCondition(s.discon)
 	e4:SetTarget(s.distg)
-	e4:SetOperation(s.disop)
+	e4:SetOperation(function(e,tp,eg,ep,ev,re,r,rp) Duel.NegateEffect(ev) end)
 	c:RegisterEffect(e4)
-	--Negate
+	--When your opponent activates a Spell Card: You can discard 1 card; negate the activation, and if you do, destroy it.
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(id,1))
 	e5:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
 	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e5:SetCode(EVENT_CHAINING)
 	e5:SetRange(LOCATION_MZONE)
 	e5:SetCondition(s.negcon)
-	e5:SetCost(s.negcost)
+	e5:SetCost(Cost.Discard())
 	e5:SetTarget(s.negtg)
 	e5:SetOperation(s.negop)
 	c:RegisterEffect(e5)
 end
-s.listed_names={96561011,511000669}
-function s.val(e,c)
-	return Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_GRAVE,0,nil,RACE_DRAGON)*400
-end
+s.listed_names={96561011,511000669} --"Red-Eyes Darkness Dragon", "Metal Plus"
 function s.hspfilter(c,tp)
 	return c:IsCode(96561011) and c:GetEquipGroup():IsExists(Card.IsCode,1,nil,511000669) 
 end
-function s.spcon(e,c)
+function s.hspcon(e,c)
 	if c==nil then return true end
 	return Duel.CheckReleaseGroup(c:GetControler(),s.hspfilter,1,false,1,true,c,c:GetControler(),nil,false,nil)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=Duel.SelectReleaseGroup(tp,s.hspfilter,1,1,false,true,true,c,nil,nil,false,nil)
 	if g then
 		g:KeepAlive()
@@ -66,7 +65,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
 	end
 	return false
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
 	Duel.Release(g,REASON_COST)
@@ -78,21 +77,16 @@ function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return end
 	local loc,tg=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION,CHAININFO_TARGET_CARDS)
 	if not tg or not tg:IsContains(c) then return false end
-	return re:IsActiveType(TYPE_SPELL) and Duel.IsChainDisablable(ev) and loc~=LOCATION_DECK
+	return re:IsSpellEffect() and Duel.IsChainDisablable(ev) and loc~=LOCATION_DECK
 end
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 end
-function s.disop(e,tp,eg,ep,ev,re,r,rp,chk)
-	Duel.NegateEffect(ev)
-end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and re:IsActiveType(TYPE_SPELL) and Duel.IsChainNegatable(ev) and rp~=tp
-end
-function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,e:GetHandler())
+	local c=e:GetHandler()
+	if c:IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	return re:IsSpellEffect() and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev) and rp==1-tp
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

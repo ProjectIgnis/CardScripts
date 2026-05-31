7314ba3a5e010598b88dc3114aa613f1b300597a
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-05-31 07:33:35 -0700

    Update "Damage Off Zone"
    -Second effect should apply to either player.

--- unofficial/c511000465.lua
+++ unofficial/c511000465.lua
@@ -7,40 +7,35 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--Half damage
+	--Halve any Battle Damage either player takes.
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
 	e2:SetTargetRange(1,1)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetValue(HALF_DAMAGE)
 	c:RegisterEffect(e2)
-	--Reduce Damage to 0
+	--If a player(s) would take Battle Damage, you can pay half your Life Points and send this card to the Graveyard to make that damage become 0.
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCondition(s.damcon)
-	e3:SetCost(s.damcost)
+	e3:SetCost(Cost.AND(Cost.PayLP(1/2),Cost.SelfToGrave))
 	e3:SetOperation(s.damop)
 	c:RegisterEffect(e3)
 end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetBattleDamage(tp)>0
-end
-function s.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+	return Duel.GetBattleDamage(tp)>0 or Duel.GetBattleDamage(1-tp)>0
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
+	e1:SetTargetRange(1,1)
 	e1:SetReset(RESET_PHASE|PHASE_DAMAGE)
 	Duel.RegisterEffect(e1,tp)
 end

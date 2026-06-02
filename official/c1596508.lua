bc493e39bcaa91220db195061c9a2043eb387a61
Naim <naimsantos45@gmail.com>
2026-06-02 19:42:15 -0300

    "Dimension Fortress Weapon" and "Stellarknight Constellar Diamond" fix
    Fixed a bug in which they would make cards that tribute from the Deck to be unable to send the card(s) to the GY, which should not happen.

--- official/c1596508.lua
+++ official/c1596508.lua
@@ -2,14 +2,15 @@
 --Dimension Fortress Weapon
 local s,id=GetID()
 function s.initial_effect(c)
-	--
+	--Cards cannot be sent from the Deck to the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(LOCATION_DECK,LOCATION_DECK)
+	e1:SetTargetRange(1,1)
+	e1:SetTarget(s.tgfilter)
 	c:RegisterEffect(e1)
-	--
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
@@ -17,4 +18,7 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(1,1)
 	c:RegisterEffect(e2)
+end
+function s.tgfilter(e,c,rp,r)
+	return c:IsLocation(LOCATION_DECK) and (r&REASON_RELEASE)==0
 end
--- official/c9272381.lua
+++ official/c9272381.lua
@@ -2,18 +2,19 @@
 --Stellarknight Constellar Diamond
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),5,3,s.ovfilter,aux.Stringid(id,0),Xyz.InfiniteMats)
 	c:EnableReviveLimit()
-	--
+	--Xyz Summon procedure: 3+ Level 5 LIGHT monsters OR During your Main Phase 2, you can also Xyz Summon this card by using a "tellarknight" Xyz Monster you control as material, except "Stellarknight Constellar Diamond"
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),5,3,s.ovfilter,aux.Stringid(id,0),Xyz.InfiniteMats)
+	--While this card has material, neither player can send cards from the Deck to the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_TO_GRAVE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(LOCATION_DECK,LOCATION_DECK)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,1)
 	e1:SetCondition(s.effcon)
+	e1:SetTarget(s.tgfilter)
 	c:RegisterEffect(e1)
-	--
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_DISCARD_DECK)
@@ -22,7 +23,7 @@ function s.initial_effect(c)
 	e2:SetTargetRange(1,1)
 	e2:SetCondition(s.effcon)
 	c:RegisterEffect(e2)
-	--
+	--Any card that returns from the GY to the hand is banished instead
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_TO_HAND_REDIRECT)
@@ -31,7 +32,7 @@ function s.initial_effect(c)
 	e3:SetValue(LOCATION_REMOVED)
 	e3:SetCondition(s.effcon)
 	c:RegisterEffect(e3)
-	--Negate
+	--When an opponent's DARK monster's effect is activated (Quick Effect): You can detach 1 material from this card; negate that activation, and if you do, destroy it
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
@@ -49,11 +50,14 @@ function s.ovfilter(c,tp,xyzc)
 	return c:IsFaceup() and c:IsSetCard(SET_TELLARKNIGHT,xyzc,SUMMON_TYPE_XYZ,tp) and c:IsType(TYPE_XYZ,xyzc,SUMMON_TYPE_XYZ,tp)
 		and not c:IsSummonCode(xyzc,SUMMON_TYPE_XYZ,tp,id) and Duel.IsPhase(PHASE_MAIN2)
 end
+function s.tgfilter(e,c,rp,r)
+	return c:IsLocation(LOCATION_DECK) and (r&REASON_RELEASE)==0
+end
 function s.effcon(e)
 	return e:GetHandler():GetOverlayCount()>0
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and re:IsMonsterEffect() and re:GetHandler():IsAttribute(ATTRIBUTE_DARK) and Duel.IsChainNegatable(ev)
+	return ep==1-tp and re:IsMonsterEffect() and re:GetHandler():IsAttribute(ATTRIBUTE_DARK) and Duel.IsChainNegatable(ev)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end

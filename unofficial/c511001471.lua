baf7a680dc04deb275da20278f2bf2cfeee04e02
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-06-05 07:26:52 -0700

    Update "Cursed Dollhouse"
    - Was missing effect to allow choice of attack targets
    - Script modernization/clean-up

--- unofficial/c511001471.lua
+++ unofficial/c511001471.lua
@@ -1,3 +1,4 @@
+--呪われたドールハウス
 --Cursed Dollhouse
 local s,id=GetID()
 function s.initial_effect(c)
@@ -6,35 +7,44 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--sp summon
+	--When your opponent takes control of "Alice the Wandering Doll", you can Special Summon 1 "Doll Part" monster from your Deck. Also, you select the targets of your opponent's attacks.
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetCode(EVENT_CONTROL_CHANGED)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCondition(s.condition)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.operation)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
+	--You choose the targets for your opponent's attacks
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_PATRICIAN_OF_DARKNESS)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(0,1)
+	c:RegisterEffect(e3)
 end
-s.listed_names={511001466}
-function s.cfilter(c,tp)
+s.listed_names={511001466} --"Alice the Wandering Doll"
+s.listed_series={0x2517} --"Doll Part"
+function s.spcfilter(c,tp)
 	return c:IsCode(511001466) and c:IsControler(1-tp) and c:IsPreviousControler(tp)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.spcfilter,1,nil,tp)
 end
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x2517) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)

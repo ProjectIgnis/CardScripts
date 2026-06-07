38c21e50b5ae4901d2a7e70c321dd1aac4bbefa5
Edoardo Lolletti <edoardo762@gmail.com>
2026-06-07 13:36:51 +0200

    Update Mitsurugi Ritual to the new definition of Duel.ReleaseRitualMaterial

--- official/c81560239.lua
+++ official/c81560239.lua
@@ -20,25 +20,26 @@ end
 function s.extragroup(e,tp,eg,ep,ev,re,r,rp,chk)
 	return Duel.GetMatchingGroup(s.deckmatfilter,tp,LOCATION_DECK,0,nil)
 end
+function s.extraop(mat,e,tp,eg,ep,ev,re,r,rp,tc)
+	Duel.ReleaseRitualMaterial(mat,true)
+end
 function s.tributelimit(e,tp,g,sc)
 	return #g<=2,#g>2
 end
+local ritual_params1={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_DECK,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE)}
+local ritual_params2={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,extraop=s.extraop,forcedselection=s.tributelimit}
 function s.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(-100)
-	local params1={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_DECK,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE)}
-	local params2={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,forcedselection=s.tributelimit}
-	local b1=not Duel.HasFlagEffect(tp,id) and Ritual.Target(params1)(e,tp,eg,ep,ev,re,r,rp,0)
-	local b2=not Duel.HasFlagEffect(tp,id+1) and Ritual.Target(params2)(e,tp,eg,ep,ev,re,r,rp,0)
+	local b1=not Duel.HasFlagEffect(tp,id) and Ritual.Target(ritual_params1)(e,tp,eg,ep,ev,re,r,rp,0)
+	local b2=not Duel.HasFlagEffect(tp,id+1) and Ritual.Target(ritual_params2)(e,tp,eg,ep,ev,re,r,rp,0)
 	if chk==0 then return b1 or b2 end
 end
 function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local cost_skip=e:GetLabel()~=-100
-	local params1={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_DECK,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE)}
-	local params2={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,forcedselection=s.tributelimit}
 	local b1=(cost_skip or not Duel.HasFlagEffect(tp,id))
-		and Ritual.Target(params1)(e,tp,eg,ep,ev,re,r,rp,0)
+		and Ritual.Target(ritual_params1)(e,tp,eg,ep,ev,re,r,rp,0)
 	local b2=(cost_skip or not Duel.HasFlagEffect(tp,id+1))
-		and Ritual.Target(params2)(e,tp,eg,ep,ev,re,r,rp,0)
+		and Ritual.Target(ritual_params2)(e,tp,eg,ep,ev,re,r,rp,0)
 	if chk==0 then e:SetLabel(0) return b1 or b2 end
 	local op=Duel.SelectEffect(tp,
 		{b1,aux.Stringid(id,1)},
@@ -57,10 +58,8 @@ end
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local op=e:GetLabel()
 	if op==1 then
-		local params1={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),location=LOCATION_DECK,matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE)}
-		Ritual.Operation(params1)(e,tp,eg,ep,ev,re,r,rp)
+		Ritual.Operation(ritual_params1)(e,tp,eg,ep,ev,re,r,rp)
 	elseif op==2 then
-		local params2={lvtype=RITPROC_EQUAL,filter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),matfilter=aux.FilterBoolFunction(Card.IsRace,RACE_REPTILE),extrafil=s.extragroup,forcedselection=s.tributelimit}
-		Ritual.Operation(params2)(e,tp,eg,ep,ev,re,r,rp)
+		Ritual.Operation(ritual_params2)(e,tp,eg,ep,ev,re,r,rp)
 	end
 end

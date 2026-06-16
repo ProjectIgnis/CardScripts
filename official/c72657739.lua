3ac66a4b17e16d2ba2f88dbe0e1d2d3b46d4bfc4
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-06-16 11:42:05 -0700

    Update "Malice Doll of Demise"
    - Prevent script errors

--- official/c72657739.lua
+++ official/c72657739.lua
@@ -2,7 +2,7 @@
 --Malice Doll of Demise
 local s,id=GetID()
 function s.initial_effect(c)
-	--register
+	--Register if this card is sent to the GY by a Continuous Spell effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_TO_GRAVE)
@@ -11,8 +11,9 @@ function s.initial_effect(c)
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local rc=re:GetHandler()
 	if c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT)
-		and re:GetActiveType()==TYPE_CONTINUOUS+TYPE_SPELL then
+		and re:IsSpellEffect() and rc:IsContinuousSpell() then
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(aux.Stringid(id,0))
 		e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -20,23 +21,22 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EVENT_PHASE|PHASE_STANDBY)
 		e1:SetCountLimit(1)
 		e1:SetRange(LOCATION_GRAVE)
-		e1:SetCondition(s.spcon)
+		e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
 		e1:SetTarget(s.sptg)
 		e1:SetOperation(s.spop)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_STANDBY|RESET_SELF_TURN)
 		c:RegisterEffect(e1)
 	end
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsTurnPlayer(tp)
-end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end

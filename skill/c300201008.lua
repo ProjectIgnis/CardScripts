66a5b8e7e25ed71f0fa3b3cb2f105bb6aef10b8e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-06-21 10:51:46 -0700

    Update "Mind Scan"
    - Updated the check effect to use EFFECT_PUBLIC
    - Minor script polish/clean-up

--- skill/c300201008.lua
+++ skill/c300201008.lua
@@ -5,50 +5,34 @@ function s.initial_effect(c)
 	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
 	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==3 and Duel.GetLP(tp)>=3000
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	--ask if you want to activate the skill or not
+	local c=e:GetHandler()
+	--Ask if you want to activate this Skill
 	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
-	--look at hand
-	--skill is active flag
+	--Flag registration for active Skill
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
-	--mind scan
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_ADJUST)
-	e1:SetCondition(s.con1)
-	e1:SetOperation(s.op1)
-	Duel.RegisterEffect(e1,tp)
-	--flip back if LP<3000
+	--You can look at your opponent's Set Spell/Traps at any time
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetCode(EFFECT_PUBLIC)
+	e1:SetRange(0x5f)
+	e1:SetCondition(function(e,tp) return Duel.HasFlagEffect(tp,id) and Duel.GetLP(tp)>=3000 end)
+	e1:SetTargetRange(0,LOCATION_SZONE)
+	Duel.RegisterFlagEffect(e1,tp)
+	--If your LP fall below 3000, flip this card face-down.
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_ADJUST)
-	e2:SetCondition(s.con2)
-	e2:SetOperation(s.op2)
+	e2:SetCondition(function(e,tp) return Duel.HasFlagEffect(tp,id) and Duel.GetLP(tp)<3000 end)
+	e2:SetOperation(s.flipdownop)
 	Duel.RegisterEffect(e2,tp)
 end
-function s.con1(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id)~=0
-end
-function s.op1(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsFacedown,tp,0,LOCATION_SZONE,nil)
-	if g and #g>0 then
-		for tc in aux.Next(g) do
-			if tc:GetFlagEffect(id)==0 then
-				Duel.ConfirmCards(tp,tc)
-				tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
-			end
-		end
-	end
-end
-function s.con2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id)~=0 and Duel.GetLP(tp)<3000
-end
-function s.op2(e,tp,eg,ep,ev,re,r,rp)
+function s.flipdownop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
 	Duel.ResetFlagEffect(tp,id)
 end

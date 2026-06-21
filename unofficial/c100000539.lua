5f371564cd4f978782f3b8d1f2ac413904bbfd69
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-06-20 17:23:08 -0700

    Update "Rainbow Bridge Bifrost (Anime)"
    -Should only gain ATK from opponent's banished monsters
    -General script rework to account for new card text

--- unofficial/c100000539.lua
+++ unofficial/c100000539.lua
@@ -1,40 +1,31 @@
---英雄の掛橋－ビヴロスト
---Rainbow Bridge Bifrost
+--英雄の掛橋－ビヴロスト (Anime)
+--Rainbow Bridge Bifrost (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x122}
-function s.filter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_VALKYRIE)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) 
-		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
-end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsMonster()
+s.listed_series={SET_VALKYRIE}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_VALKYRIE),tp,LOCATION_MZONE,0,1,nil) 
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsMonster),tp,0,LOCATION_REMOVED,1,nil) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	local atk=Duel.GetMatchingGroupCount(s.cfilter,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)*500
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(atk)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
+	local c=e:GetHandler()
+	local mg=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_VALKYRIE),tp,LOCATION_MZONE,0,nil)
+	local rg=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsMonster),tp,0,LOCATION_REMOVED,nil)
+	if #mg==0 or #rg==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local sg=mg:Select(tp,1,1,nil)
+	local tc=sg:GetFirst()
+	if tc and tc:IsFaceup() then
+		Duel.HintSelection(tc)
+		tc:UpdateAttack(#rg*500,RESETS_STANDARD_PHASE_END,c)
 	end
 end

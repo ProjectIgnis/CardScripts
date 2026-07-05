5d6ba18d361a8926ce6c36a867e6011a8ed6c664
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-07-05 09:30:01 -0700

    Update "Power Charger"
    -Should be able to equip to any monster
    -Script polish
    -Handling added for ATK reset for ATK gained by the spells effect

--- unofficial/c511000489.lua
+++ unofficial/c511000489.lua
@@ -1,31 +1,69 @@
+--パワー・チャージャー
 --Power Charger
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddEquipProcedure(c,0,aux.FilterBoolFunction(Card.IsRace,RACE_WARRIOR))
-	--gain atk
+	aux.AddEquipProcedure(c)
+	--If the equipped monster destroys an opponent's monster by battle, it gains ATK equal to that monster's ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_BATTLE_DESTROYING)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCondition(s.atkcon)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+	--During the End Phase, reduce the ATK gained by this effect to 0.
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetCode(EVENT_BATTLE_DESTROYING)
+	e2:SetCode(EVENT_PHASE|PHASE_END)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCondition(s.atkcon)
-	e2:SetOperation(s.atkop)
+	e2:SetCountLimit(1)
+	e2:SetLabelObject(e1)
+	e2:SetTarget(s.rettg)
+	e2:SetOperation(s.retop)
 	c:RegisterEffect(e2)
 end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local ec=eg:GetFirst()
 	local bc=ec:GetBattleTarget()
-	return e:GetHandler():GetEquipTarget()==eg:GetFirst() and bc:IsReason(REASON_BATTLE)
+	return c:GetEquipTarget()==ec and bc:IsReason(REASON_BATTLE)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local ec=eg:GetFirst()
+	local c=e:GetHandler()
+	local ec=c:GetEquipTarget()
 	local bc=ec:GetBattleTarget()
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(bc:GetAttack())
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	e1:SetLabelObject(e)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 	ec:RegisterEffect(e1)
+end
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local eqc=c:GetEquipTarget()
+	if chk==0 then
+		local effs={eqc:GetCardEffect(EFFECT_UPDATE_ATTACK)}
+		for _,eff in ipairs(effs) do
+			if eff:GetLabelObject()==e:GetLabelObject() then
+				return true
+			end
+		end
+		return false
+	end
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local eqc=c:GetEquipTarget()
+	local chk=false
+	local effs={eqc:GetCardEffect(EFFECT_UPDATE_ATTACK)}
+	for _,eff in ipairs(effs) do
+		if eff:GetLabelObject()==e:GetLabelObject() then
+			eff:Reset()
+			chk=true
+		end
+	end
 end

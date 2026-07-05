b8da02c9e03166c963004d1d09910f565c6d6035
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-07-05 13:16:23 -0700

    Update "Last Tusk Mammoth (Anime)"
    - Should have both players take battle damage

--- unofficial/c100000316.lua
+++ unofficial/c100000316.lua
@@ -1,10 +1,10 @@
---ラスオブタクス・マンモス
+--ラスオブタクス・マンモス (Anime)
+--Last Tusk Mammoth (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
-	--reflect battle dam
+	--Both players take battle damage from this card
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
-	e1:SetValue(1)
+	e1:SetCode(EFFECT_BOTH_BATTLE_DAMAGE)
 	c:RegisterEffect(e1)
 end

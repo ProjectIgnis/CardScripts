8b019d96c587ed87162159860d35b74328e18700
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-07-05 15:00:33 -0700

    Update "Ring of Fiendish Power"
    -Mild script modernization/clean-up

--- unofficial/c511001377.lua
+++ unofficial/c511001377.lua
@@ -1,3 +1,4 @@
+--リング・オブ・デビルパワー
 --Ring of Fiendish Power
 local s,id=GetID()
 function s.initial_effect(c)
@@ -6,7 +7,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--
+	--Your opponent can only attack the face-up Fiend-Type monster you control with the highest original ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
@@ -14,32 +15,32 @@ function s.initial_effect(c)
 	e2:SetTargetRange(0,LOCATION_MZONE)
 	e2:SetValue(s.atlimit)
 	c:RegisterEffect(e2)
-	--damage
+	--If a Fiend-Type monster you control destroys an opponent's monster by battle, inflict damage to your opponent equal to the DEF of the destroyed monster.
 	local e3=Effect.CreateEffect(c)
 	e3:SetCategory(CATEGORY_DAMAGE)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e3:SetCode(EVENT_BATTLE_DESTROYING)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetCondition(s.damcon)
 	e3:SetTarget(s.damtg)
 	e3:SetOperation(s.damop)
 	c:RegisterEffect(e3)
-	--destroy
+	--If you control no face-up Fiend-Type monsters, destroy this card.
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetRange(LOCATION_SZONE)
 	e4:SetCode(EFFECT_SELF_DESTROY)
-	e4:SetCondition(s.descon)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCondition(function(e) return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_FIEND),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 	c:RegisterEffect(e4)
 end
-function s.filter(c,atk)
+function s.atfilter(c,atk)
 	return c:IsFaceup() and c:IsRace(RACE_FIEND) and c:GetBaseAttack()>atk
 end
 function s.atlimit(e,c)
 	return c:IsFacedown() or not c:IsRace(RACE_FIEND) 
-		or Duel.IsExistingMatchingCard(s.filter,e:GetHandler():GetControler(),LOCATION_MZONE,0,1,c,c:GetBaseAttack())
+		or Duel.IsExistingMatchingCard(s.atfilter,e:GetHandler():GetControler(),LOCATION_MZONE,0,1,c,c:GetBaseAttack())
 end
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=eg:GetFirst()
@@ -58,10 +59,4 @@ end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
-end
-function s.desfilter(c)
-	return c:IsFaceup() and c:IsRace(RACE_FIEND)
-end
-function s.descon(e)
-	return not Duel.IsExistingMatchingCard(s.desfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end

82f44cfa1f9b657d06a8cab7349285079d548d51
pyrQ <30827955+pyrQ@users.noreply.github.com>
2026-07-16 14:38:05 +0300

    "Sennet Per Akh" fix
    - Prevent the non-Effect Fusion Monster summoned as a Normal Monster Card from being returned to the hand or Main Deck as cost.
    - Add another timing to reset its type to make it return to the Extra Deck instead of the hand or Main Deck by an effect.

--- pre-release/c100458007.lua
+++ pre-release/c100458007.lua
@@ -27,6 +27,27 @@ function s.initial_effect(c)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
+	--Prevent the non-Effect Fusion Monster Special Summoned as a Normal Monster Card from being returned to the hand or Main Deck as cost
+	aux.GlobalCheck(s,function()
+		Card.IsAbleToHandAsCost=(function()
+			local oldfunc=Card.IsAbleToHandAsCost
+			return function(c,...)
+				if c:HasFlagEffect(id) then
+					return false
+				end
+				return oldfunc(c,...)
+			end
+		end)()
+		Card.IsAbleToDeckAsCost=(function()
+			local oldfunc=Card.IsAbleToDeckAsCost
+			return function(c,...)
+				if c:HasFlagEffect(id) then
+					return false
+				end
+				return oldfunc(c,...)
+			end
+		end)()
+	end)
 end
 s.listed_series={SET_SENNET}
 function s.thfilter(c)
@@ -69,20 +90,35 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
 		local original_type=sc:GetType()
 		sc:Type((original_type&~TYPE_FUSION)|TYPE_NORMAL)
-		sc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,2))
+		sc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,original_type,aux.Stringid(id,2))
 		--Reset its type back to the original when it stops being face-up in the Monster Zone
-		local e0=Effect.CreateEffect(c)
-		e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e0:SetCode(EVENT_ADJUST)
-		e0:SetCondition(function()
+		local e0a=Effect.CreateEffect(c)
+		e0a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e0a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e0a:SetCode(EVENT_ADJUST)
+		e0a:SetCondition(function()
 			return not sc:HasFlagEffect(id)
 		end)
-		e0:SetOperation(function()
+		e0a:SetOperation(function()
 			sc:Type(original_type)
-			e0:Reset()
+			e0a:Reset()
 		end)
-		Duel.RegisterEffect(e0,tp)
+		Duel.RegisterEffect(e0a,tp)
+		local e0b=Effect.CreateEffect(c)
+		e0b:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e0b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e0b:SetCode(EFFECT_SEND_REPLACE)
+		e0b:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk)
+			local g=eg:Filter(Card.HasFlagEffect,nil,id)
+			if chk==0 then return #g>0 end
+			for tc in g:Iter() do
+				tc:Type(tc:GetFlagEffectLabel(id))
+			end
+			e0b:Reset()
+			return true
+		end)
+		e0b:SetValue(aux.FALSE)
+		Duel.RegisterEffect(e0b,tp)
 		--But for the rest of this turn, it cannot attack directly
 		local e1=Effect.CreateEffect(c)
 		e1:SetDescription(3207)

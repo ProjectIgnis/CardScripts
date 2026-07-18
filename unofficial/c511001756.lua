b11c233502fd75fc06b281f473937fbb27910e41
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-07-18 15:35:43 -0700

    Update "Big Eater"

--- unofficial/c511001756.lua
+++ unofficial/c511001756.lua
@@ -1,11 +1,12 @@
+--ビッグ・イーター
 --Big Eater
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--You can send 2 cards from your hand to the Graveyard to Special Summon this card from your hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
@@ -15,11 +16,11 @@ end
 function s.spcon(e,c)
 	if c==nil then return true end
 	local rg=Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,c:GetControler(),LOCATION_HAND,0,e:GetHandler())
-	return aux.SelectUnselectGroup(rg,e,c:GetControler(),1,2,aux.ChkfMMZ(1),0)
+	return aux.SelectUnselectGroup(rg,e,c:GetControler(),2,2,aux.ChkfMMZ(1),0)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
 	local rg=Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,e:GetHandler())
-	local g=aux.SelectUnselectGroup(rg,e,tp,1,2,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+	local g=aux.SelectUnselectGroup(rg,e,tp,2,2,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
 	if #g>0 then
 		g:KeepAlive()
 		e:SetLabelObject(g)

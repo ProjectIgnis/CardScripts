0127953b9f7b20fe452d6bb34958a534a3dbd4ee
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-07-19 14:10:18 -0700

    Update "D/D/D Duo-Dawn King Kali Yuga (Anime)"
    Script clean-up/polish

--- unofficial/c511001376.lua
+++ unofficial/c511001376.lua
@@ -3,28 +3,28 @@
 --fixed by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,SET_DD),8,2)
 	c:EnableReviveLimit()
-	--cannot disable
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetValue(s.cnvalue)
-	e0:SetCode(EFFECT_CANNOT_DISEFFECT)
-	c:RegisterEffect(e0)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_DISABLE)
-	c:RegisterEffect(e1)
-	--negate
+	--Xyz Summon procedure: 2 Level 8 "D/D" monsters
+	Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,SET_DD),8,2)
+	--This card's effects cannot be negated
+	local e1a=Effect.CreateEffect(c)
+	e1a:SetType(EFFECT_TYPE_SINGLE)
+	e1a:SetCode(EFFECT_CANNOT_DISABLE)
+	c:RegisterEffect(e1a)
+	local e1b=Effect.CreateEffect(c)
+	e1b:SetType(EFFECT_TYPE_FIELD
+	e1b:SetCode(EFFECT_CANNOT_DISEFFECT)
+	e1b:SetRange(LOCATION_MZONE)
+	e1b:SetValue(function(e,ct) return Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT):GetHandler()==e:GetHandler() end)
+	c:RegisterEffect(e1b)
+	--If this card is Special Summoned: Negate the effects of all other face-up cards currently on the field, until the end of this turn. 
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(55888045,0))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetOperation(s.negop)
 	c:RegisterEffect(e2)
-	--destroy
+	--(Quick Effect): You can detach 1 Xyz Material from this card; destroy all Spells/Traps on the field.
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(15939229,0))
 	e3:SetCategory(CATEGORY_DESTROY)
@@ -35,11 +35,11 @@ function s.initial_effect(c)
 	e3:SetTarget(s.destg)
 	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
-	--return
+	--If a card(s) were destroyed by the above effect: You can detach 1 Xyz Material from this card; return all cards that were destroyed on your side of the field to the same zone and postion they were in when they were destroyed.
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_DESTROYED)
 	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_DESTROYED)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetLabelObject(e3)
 	e4:SetCondition(s.retcon)
@@ -47,7 +47,7 @@ function s.initial_effect(c)
 	e4:SetTarget(s.rettg)
 	e4:SetOperation(s.retop)
 	c:RegisterEffect(e4)
-	--special summon
+	--Register a "Double Snare" check on Special Summon
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
 	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -55,63 +55,53 @@ function s.initial_effect(c)
 	e5:SetOperation(s.regop)
 	c:RegisterEffect(e5)
 end
-s.listed_series={0xaf}
-function s.cnvalue(e,ct)
-	return Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT):GetHandler()==e:GetHandler()
-end
+s.listed_series={SET_DD}
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(3682106)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetReset(RESETS_STANDARD_PHASE_END)
 	e:GetHandler():RegisterEffect(e1)
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,c)
-	for tc in aux.Next(g) do
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e2)
+	for tc in g:Iter() do
+		tc:NegateEffects(c,RESETS_STANDARD_PHASE_END)
 	end
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,TYPE_SPELL+TYPE_TRAP) end
-	local sg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,TYPE_SPELL+TYPE_TRAP)
+	local sg=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if chk==0 then return #sg>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,LOCATION_ONFIELD)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local sg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,TYPE_SPELL+TYPE_TRAP)
-	Duel.Destroy(sg,REASON_EFFECT)
+	local sg=Duel.GetMatchingGroup(Card.IsSpellTrap,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if #sg>0 then
+		Duel.Destroy(sg,REASON_EFFECT)
+	end
 end
-function s.retcfilter(c,tp,e)
+function s.retcardfilter(c,tp,e)
 	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousControler(tp)
 		and c:IsReason(REASON_EFFECT) and c:GetReasonEffect()==e
 end
-function s.retcfilter2(c,tp)
+function s.retlocfilter(c,tp)
 	return Duel.CheckLocation(tp,c:GetPreviousLocation(),c:GetPreviousSequence())
 end
 function s.retcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.retcfilter,1,nil,tp,e:GetLabelObject())
+	return eg:IsExists(s.retcardfilter,1,nil,tp,e:GetLabelObject())
 end
 function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=eg:Filter(s.retcfilter,nil,tp,e:GetLabelObject())
-	if chk==0 then return not g:IsExists(aux.NOT(s.retcfilter2),1,nil,tp) end
+	local g=eg:Filter(s.retcardfilter,nil,tp,e:GetLabelObject())
+	if chk==0 then return not g:IsExists(aux.NOT(s.retlocfilter),1,nil,tp) end
 	Duel.SetTargetCard(g)
 end
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
-	if #g<=0 or g:IsExists(aux.NOT(s.retcfilter2),1,nil,tp) then return end
-	for tc in aux.Next(g) do
+	if #g<=0 or g:IsExists(aux.NOT(s.retlocfilter),1,nil,tp) then return end
+	for tc in g:Iter() do
 		if tc:IsPreviousLocation(LOCATION_PZONE) then
 			local seq=0
 			if tc:GetPreviousSequence()==7 or tc:GetPreviousSequence()==4 then seq=1 end

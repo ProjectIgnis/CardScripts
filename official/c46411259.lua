3506e3959733cbc3f86968710794d80bf6f67825
Naim <naimsantos45@gmail.com>
2026-07-22 18:15:52 -0300

    "Metamorphosis" fix
    Fixed a bug where it would not be able to tribute an opponent's monster under the presence of "Lair of Darkness"

--- official/c46411259.lua
+++ official/c46411259.lua
@@ -2,8 +2,9 @@
 --Metamorphosis
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Tribute 1 monster. Special Summon 1 Fusion Monster from your Extra Deck with the same Level as the Tributed monster.
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -17,28 +18,28 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(100)
 	return true
 end
-function s.filter1(c,e,tp)
+function s.costfilter(c,e,tp)
 	local lv=c:GetLevel()
-	return lv>0 and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,lv,e,tp,c)
+	return lv>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,lv,e,tp,c)
 end
-function s.filter2(c,lv,e,tp,mc)
-	return c:IsType(TYPE_FUSION) and c:GetLevel()==lv and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spfilter(c,lv,e,tp,mc)
+	return c:IsFusionMonster() and c:IsLevel(lv) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		if e:GetLabel()~=100 then return false end
 		e:SetLabel(0)
-		return Duel.CheckReleaseGroup(tp,s.filter1,1,nil,e,tp)
+		return Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,nil,nil,e,tp)
 	end
-	local rg=Duel.SelectReleaseGroup(tp,s.filter1,1,1,nil,e,tp)
-	e:SetLabel(rg:GetFirst():GetLevel())
+	local rg=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,nil,nil,e,tp)
+	e:GetChainData().level_of_the_tribute=rg:GetFirst():GetLevel()
 	Duel.Release(rg,REASON_COST)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local lv=e:GetLabel()
+	local lv=e:GetChainData().level_of_the_tribute
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil,lv,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,lv,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end

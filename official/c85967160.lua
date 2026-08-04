9e621968649107817e65cb66af7f972a42ef6b02
Naim <naimsantos45@gmail.com>
2026-08-04 17:46:21 -0300

    "Aromage Bergamot" update
    Now its script will correctly call the string associated with the trigger effect

--- official/c85967160.lua
+++ official/c85967160.lua
@@ -2,7 +2,7 @@
 --Aromage Bergamot
 local s,id=GetID()
 function s.initial_effect(c)
-	--pierce
+	--While your LP is higher than your opponent's, if a Plant-Type monster you control attacks a Defense Position monster, inflict piercing battle damage to your opponent
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_PIERCE)
@@ -11,14 +11,15 @@ function s.initial_effect(c)
 	e1:SetCondition(s.pccon)
 	e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_PLANT))
 	c:RegisterEffect(e1)
-	--atk & def
+	--Once per turn, if you gain LP: This card gains 1000 ATK and DEF until the end of your opponent's turn.
 	local e2=Effect.CreateEffect(c)
+	e:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_RECOVER)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetCondition(s.adcon)
+	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return ep==tp end)
 	e2:SetTarget(s.adtg)
 	e2:SetOperation(s.adop)
 	c:RegisterEffect(e2)
@@ -26,9 +27,6 @@ end
 function s.pccon(e)
 	local tp=e:GetHandlerPlayer()
 	return Duel.GetLP(tp)>Duel.GetLP(1-tp)
-end
-function s.adcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp
 end
 function s.adtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return ep==tp and e:GetHandler():IsRelateToEffect(e) end

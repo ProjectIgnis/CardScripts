608388404c044524e5e0e2122df106631d9283e0
yoshiduels <yoshi.katreuvin@gmail.com>
2026-08-22 16:38:19 +0200

    updated Neo-Spacian Grand Mole

--- rush/c160026067.lua
+++ rush/c160026067.lua
@@ -40,4 +40,17 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 	Duel.SkipPhase(Duel.GetTurnPlayer(),Duel.GetCurrentPhase(),RESET_PHASE|PHASE_END,1)
+	--Prevents cards activations during "End of Main"
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(s.aclimit)
+	e1:SetCondition(function() return Duel.IsMainPhase() end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.aclimit(e,re,tp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
 end

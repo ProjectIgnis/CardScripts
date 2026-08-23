36b20bd20c8b1cbb3f42aff8774812d89f39711e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-08-23 14:42:37 -0700

    Update "Heraldry Catastrophe"
    -Added possible opinfo for destruction/damage
    -Should destroy all monsters with ATK lower than the target
    -General script polishing

--- unofficial/c511001452.lua
+++ unofficial/c511001452.lua
@@ -1,8 +1,10 @@
+--メダリオン・カタストロフィー
 --Heraldry Catastrophe
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -10,45 +12,39 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.filter(c,tp)
-	return c:IsFaceup() and c:GetOwner()==tp
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() and chkc:IsOwner(tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.FaceupFilter(Card.IsOwner,tp),tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil,tp)
+	local g=Duel.SelectTarget(tp,aux.FaceupFilter(Card.IsOwner,tp),tp,0,LOCATION_MZONE,1,1,nil,tp)
+	local dg=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsAttackBelow,g:GetFirst():GetAttack()),tp,0,LOCATION_MZONE,nil)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,dg,#dg,tp,LOCATION_MZONE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
 		e1:SetCode(EVENT_TURN_END)
 		e1:SetCountLimit(1)
-		e1:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
-		e1:SetOperation(s.op)
+		e1:SetOperation(s.desop)
 		e1:SetLabelObject(tc)
 		Duel.RegisterEffect(e1,tp)
 		tc:CreateEffectRelation(e1)
 	end
 end
-function s.desfilter(c,atk)
-	return c:IsFaceup() and c:GetAttack()<=atk and c:IsDestructable()
-end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
 	local tc=e:GetLabelObject()
-	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,tc,tc:GetAttack())
-	if tc:IsRelateToEffect(e) and tc:IsControler(1-tp) and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(68535320,0)) then
-		Duel.Hint(HINT_CARD,0,id)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-		local sg=g:Select(tp,1,#g,nil)
-		Duel.HintSelection(sg)
-		if Duel.Destroy(sg,REASON_EFFECT)>0 then
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsAttackBelow,tc:GetAttack()),tp,0,LOCATION_MZONE,nil)
+	if tc:IsRelateToEffect(e) and tc:IsControler(1-tp) and tc:IsOnField() and #g>0 then
+		if Duel.Destroy(g,REASON_EFFECT)>0 then
 			Duel.BreakEffect()
 			local dg=Duel.GetOperatedGroup()
-			local sum=dg:GetSum(Card.GetAttack)
-			Duel.Damage(1-tp,sum,REASON_EFFECT)
+			local dam=dg:GetSum(Card.GetPreviousAttackOnField)
+			Duel.Damage(1-tp,dam,REASON_EFFECT)
 		end
 	end
 end

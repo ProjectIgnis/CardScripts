d22c38a72ffd64517df75f6090ee175ea5668d08
Naim <naimsantos45@gmail.com>
2026-08-24 18:26:06 -0300

    Update c160220032.lua

--- rush/c160220032.lua
+++ rush/c160220032.lua
@@ -34,7 +34,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 	--Prevent non-Cyberse from attacking
-	local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_ATTACK)
 	e1:SetProperty(EFFECT_FLAG_OATH)

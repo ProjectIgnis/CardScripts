62d111ff371aee4a954c23550fd297b5456ab4c3
pyrQ <30827955+pyrQ@users.noreply.github.com>
2026-08-30 20:27:31 +0300

    Delete pre-release directory

--- pre-release/c101402144.lua
+++
@@ -1,3 +0,0 @@
---ＣＸ 冀望皇龍カオス・バリアン・ドラゴン
---CXyz Chaos Barian Hope Dragon
-Duel.LoadCardScriptAlias(101402044)
--- pre-release/c101403130.lua
+++
@@ -1,3 +0,0 @@
---暗黒の太陽神－ラーの翼神竜
---The Sun God of Darkness - The Winged Dragon of Ra
-Duel.LoadCardScriptAlias(101403030)

c4faac1c5d75929f6f54751117952c074bddf7e8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2026-09-03 21:47:48 +0300

    Added new match winner scripts

---
+++ unofficial/c501000096.lua
@@ -0,0 +1,22 @@
+--JP name
+--Moonlight at Dawn
+local s,id=GetID()
+function s.initial_effect(c)
+	c:AddCannotBeSpecialSummoned()
+	--Requires 3 Warrior Tributes to Normal Summon (cannot be Normal Set)
+	aux.AddNormalSummonProcedure(c,true,false,3,3)
+	aux.AddNormalSetProcedure(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_TRIBUTE_LIMIT)
+	e1:SetValue(function(e,c)
+		return not c:IsRace(RACE_WARRIOR)
+	end)
+	c:RegisterEffect(e1)
+	--If this card attacks your opponent directly and reduces their LP to 0, you win the Match
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_MATCH_KILL)
+	c:RegisterEffect(e2)
+end

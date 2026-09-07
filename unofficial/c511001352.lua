5a4507c4eab0be2132c1f8b3e7f4404bdfea07e2
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-07 11:28:51 -0700

    Update "Insect Garden"
    -Effect should be optional
    -Improved handling to account for zone availability/filter selection/card prompts

--- unofficial/c511001352.lua
+++ unofficial/c511001352.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetOperation(function() Duel.AdjustInstantly() end)
 	c:RegisterEffect(e1)
-	--Give control of all Level 4 or lower Insect monsters you control to your opponent
+	--Give control of any Level 4 or lower Insect monster you control to your opponent
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_ADJUST)
@@ -18,9 +18,19 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.ctrlfilter(c)
-	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_INSECT) and c:IsControlerCanBeChanged()
+	return c:IsFaceup() and c:IsLevelBelow(4) and c:IsRace(RACE_INSECT) and c:IsControlerCanBeChanged() 
 end
 function s.ctrlop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.ctrlfilter,tp,LOCATION_MZONE,0,nil)
-	Duel.GetControl(g,1-tp)
+	local oft=Duel.GetMZoneCount(1-tp)
+	if #g==0 or oft<=0 then return end 
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+		local sg=g:FilterSelect(tp,s.ctrlfilter,1,oft,nil)
+		if #sg>0 then
+			Duel.HintSelection(sg)
+			Duel.GetControl(sg,1-tp)
+		end
+	end
 end

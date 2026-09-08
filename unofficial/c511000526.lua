33354676d8288a58fb4e2d8c90483f59ba797edb
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-07 20:21:25 -0700

    Update "Dowsing Burn"
    -Should be able to destroy monsters with Levels less than or equal to the banished monster's Level

--- unofficial/c511000526.lua
+++ unofficial/c511000526.lua
@@ -16,7 +16,7 @@ function s.banfilter(c)
 		and Duel.IsExistingTarget(s.desfilter,0,LOCATION_MZONE,LOCATION_MZONE,1,c,c:GetLevel(),c:GetRace())
 end
 function s.desfilter(c,lv,race)
-	return c:IsFaceup() and c:HasLevel() and c:GetLevel()<lv and c:IsRace(race)
+	return c:IsFaceup() and c:HasLevel() and c:GetLevel()<=lv and c:IsRace(race)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.banfilter,tp,0,LOCATION_MZONE|LOCATION_GRAVE,1,nil) end

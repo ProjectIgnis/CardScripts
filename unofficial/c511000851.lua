4978897493eea712cff371186cf70fb7ed2be66f
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-09 22:36:29 -0700

    Update "Synchro Destructor"
    -Should apply the damage twice, not double the damage.

--- unofficial/c511000851.lua
+++ unofficial/c511000851.lua
@@ -1,38 +1,41 @@
+--シンクロ・デストラクター
 --Synchro Destructor
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_BATTLE_DESTROYED)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCondition(s.condition)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(function(e,tp,eg) return eg:IsExists(s.confilter,1,nil,tp) end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c,tp)
+function s.confilter(c,tp)
 	local rc=c:GetReasonCard()
 	return c:IsReason(REASON_BATTLE) and rc:IsType(TYPE_SYNCHRO) and rc:IsControler(tp) and rc:IsRelateToBattle()
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local g=eg:Filter(s.cfilter,nil,tp)
+	local g=eg:Filter(s.confilter,nil,tp)
 	local tc=g:GetFirst()
 	local dam=tc:GetTextAttack()/2
 	if dam<0 then dam=0 end
+	Duel.SetTargetCard(tc)
 	Duel.SetTargetPlayer(1-tp)
-	if tc:IsType(TYPE_SYNCHRO) then
-		Duel.SetTargetParam(dam*2)
-	else
-		Duel.SetTargetParam(dam)
-	end
+	Duel.SetTargetParam(dam)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+	if tc:IsSynchroMonster() then
+		Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+	end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Damage(p,d,REASON_EFFECT)
+	if tc:IsSynchroMonster() then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.Damage(p,d,REASON_EFFECT)
+	end
 end

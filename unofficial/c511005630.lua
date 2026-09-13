9b4c0ed72e2ed62508f133f5e11dd914e0e3e710
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-13 16:41:53 -0700

    Update "Freedom Bird"
    -Should activate when a monster is Normal or Special Summoned to the opponent's field, not when an opponent Normal or Special Summons a monster.

--- unofficial/c511005630.lua
+++ unofficial/c511005630.lua
@@ -1,16 +1,16 @@
+--自由鳥
 --Freedom Bird
---scripted by GameMaster (GM)
---fixed by MLD
+--Scripted by GameMaster (GM)
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special summon Freedom bird from you deck
+	--When a monster(s) is Normal or Special Summoned to your opponent's side of the field: You can Special Summon 1 "Freedom Bird" from your Deck.Special Summon "Freedom Bird from you deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
-	e1:SetCondition(s.spcon)	
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(function(e,tp,eg) return eg:IsExists(Card.IsControler,1,nil,1-tp) end)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
@@ -18,23 +18,20 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 end
-function s.cfilter(c,tp)
-	return c:GetSummonPlayer()~=tp
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
-end
-function s.filter(c,e,tp)
+s.listed_names={id} --"Freedom Bird"
+function s.spfilter(c,e,tp)
 	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end

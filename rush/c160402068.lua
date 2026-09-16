c3164ee770bbb1cb237a03da2079baef72a75b29
yoshiduels <yoshi.katreuvin@gmail.com>
2026-09-16 15:04:32 +0200

    added Lostream Nereides

---
+++ rush/c160402068.lua
@@ -0,0 +1,75 @@
+--雲海竜 ネーレイディス
+--Lostream Nereides
+--scripted by YoshiDuels
+local s,id=GetID()
+function s.initial_effect(c)
+	--Summon with 1 tribute
+	local e1=aux.AddNormalSummonProcedure(c,true,true,1,1,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0),s.filter)
+	--Equip
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.filter(c)
+	return c:IsAttribute(ATTRIBUTE_WIND)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,2) end
+end
+function s.equipfilter(c)
+	return c:IsFaceup()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.equipfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_SEASERPENT) and c:IsAttribute(ATTRIBUTE_WIND) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	if Duel.DiscardDeck(tp,2,REASON_COST)<1 then return end
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectMatchingCard(tp,s.equipfilter,tp,LOCATION_MZONE,0,1,1,c)
+	Duel.HintSelection(g)
+	local tc=g:GetFirst()
+	Duel.Equip(tp,c,tc,true)
+	s.eqeffect(c,tc)
+end
+function s.eqeffect(c,tc)
+	--Add Equip limit
+	local e1=Effect.CreateEffect(tc)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	e1:SetValue(s.eqlimit)
+	c:RegisterEffect(e1)
+	--Indestructable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_ONFIELD,0)
+	e2:SetTarget(s.indtg)
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
+end
+function s.eqlimit(e,c)
+	return c:IsFaceup()
+end
+function s.indtg(e,c)
+	return c:IsSpellTrap() and c~=e:GetHandler()
+end
+function s.efilter(e,te)
+	return te:GetOwnerPlayer()~=e:GetOwnerPlayer()
+end

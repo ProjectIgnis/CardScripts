31e31fc7123579c989a6c77f484286cd6abce56e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-19 09:58:57 -0700

    Update "Parasite Maneuver"
    - Should only affect monsters equipped with "Fusion Parasite" that already attacked

--- unofficial/c511009341.lua
+++ unofficial/c511009341.lua
@@ -3,34 +3,37 @@
 --Fixed by MLD
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--During the Battle Phase, if 1 monster equipped with "Fusion Parasite" attacked: That monster can make a second attack.
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetHintTiming(TIMING_BATTLE_PHASE)
+	e1:SetCondition(function() return Duel.IsBattlePhase() end)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={6205579}
-function s.filter(c)
-	return c:GetEquipGroup():IsExists(Card.IsCode,1,nil,6205579)
+s.listed_names={6205579} --"Fusion Parasite"
+function s.exatkfilter(c)
+	return c:IsFaceup() and c:GetEquipGroup():IsExists(Card.IsCode,1,nil,6205579) and c:GetAttackedCount()==1
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.exatkfilter,tp,LOCATION_MZONE,0,1,nil) end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_EXTRA_ATTACK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e2:SetValue(1)
-		tc:RegisterEffect(e2)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,s.exatkfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and tc:IsFaceup() then
+		--Can make a second attack
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3201)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_EXTRA_ATTACK)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE)
+		tc:RegisterEffect(e1)
 	end
 end

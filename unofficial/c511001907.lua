ed8c07a3c3294d9fc542c19a3d879e1395f6ba8e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-19 15:17:26 -0700

    Update "Amazoness Trainer"
    Should be able to inflict piercing.

--- unofficial/c511001907.lua
+++ unofficial/c511001907.lua
@@ -1,3 +1,4 @@
+--アマゾネスの調教師
 --Amazoness Trainer
 local s,id=GetID()
 function s.initial_effect(c)
@@ -10,27 +11,31 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={10979723}
+s.listed_names={10979723} --"Amazoness Tiger"
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-	if not d then return false end
-	if d:IsControler(tp) or a:IsControler(1-tp) then return false end
-	return a:IsCode(10979723) and a:IsRelateToBattle() and d:IsLocation(LOCATION_ONFIELD)
+	local ac=Duel.GetAttacker()
+	local bc=ac:GetBattleTarget()
+	if not bc then return false end
+	if bc:IsControler(tp) or ac:IsControler(1-tp) then return false end
+	return ac and ac:IsCode(10979723) and bc and bc:IsRelateToBattle() and ac:CanChainAttack()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local a=Duel.GetAttacker()
-	if chk==0 then return a:CanChainAttack() end
+	local ac=Duel.GetAttacker()
+	if chk==0 then return ac:CanChainAttack() end
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	if a and a:IsRelateToBattle() and a:IsFaceup() then
-		local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	local ac=Duel.GetAttacker()
+	local bc=ac:GetBattleTarget()	
+	if ac and ac:IsRelateToBattle() and ac:IsFaceup() and bc:IsOnField() then
+		ac:UpdateAttack(400)
+		Duel.ChainAttack(bc) then
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(400)
-		a:RegisterEffect(e1)
-		Duel.ChainAttack()
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetCode(EFFECT_PIERCE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_BATTLE|PHASE_DAMAGE_CAL)
+		ac:RegisterEffect(e1,true)
 	end
 end

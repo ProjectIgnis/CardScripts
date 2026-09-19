2b57b56e95bab65b7a1e1d7a6ba9a5340e4ee4bb
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-18 23:58:48 -0700

    Re-script "Hero Soul"
    -Should activate when the player reaches 0 LP, not if they take damage that would reduce their LP to 0.

--- unofficial/c511000317.lua
+++ unofficial/c511000317.lua
@@ -1,30 +1,61 @@
+--ヒーロー・ソウル
 --Hero Soul
+Duel.EnableUnofficialProc(PROC_EVENT_LP0)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e1:SetCondition(s.condition)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_LP0)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCondition(function(_,tp) return s[tp] end)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	aux.LP0ActivationValidity(e1)
+	--Check for "HERO" monsters destroyed this turn
+	aux.GlobalCheck(s,function()
+		s[0]=true
+		s[1]=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_DESTROYED)
+		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+		aux.AddValuesReset(function()
+			s[0]=false
+			s[1]=false
+		end)
+	end)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local p=e:GetHandler():GetControler()
-	local at=Duel.GetAttackTarget()
-	local a=Duel.GetAttacker()
-	return Duel.GetBattleDamage(p)>=Duel.GetLP(p) and ((at and at:IsSetCard(SET_HERO) and at:IsControler(p)) or (a and a:IsSetCard(SET_HERO) and a:IsControler(p))) 
+s.listed_series={SET_HERO}
+function s.checkfilter(c)
+	return c:IsSetCard(SET_HERO) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP)
+		and (c:GetReason()&REASON_DESTROY)==REASON_DESTROY
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.checkfilter,nil)
+	g:ForEach(function(tc)
+		s[tc:GetPreviousControler()]=true
+	end)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return true end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_LOSE_LP)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,1,tp,100)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local p=e:GetHandler():GetControler()
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e1:SetOperation(s.damop)
-	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
-	Duel.RegisterEffect(e1,p)
-	Duel.SetLP(p,100,REASON_EFFECT)
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ChangeBattleDamage(tp,0)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Recover(p,100,REASON_EFFECT)
 end

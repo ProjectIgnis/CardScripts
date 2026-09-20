2653fcb3bcc63fd46636fe0d6fef2221086babd0
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-19 21:22:30 -0700

    Update "Crime and Punishment"
    -Should only destroy opponent's monsters
    -Script clean-up/modernization

--- unofficial/c511001389.lua
+++ unofficial/c511001389.lua
@@ -1,37 +1,36 @@
+--罪と罰
 --Crime and Punishment
 local s,id=GetID()
 function s.initial_effect(c)
-	--special summon
+	--Activate only when you take Battle Damage by a direct attack. Destroy 1 monster your opponent controls. At the end of the Battle Phase, you can Special Summon 1 Level 4 or lower monster from your hand.
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_BATTLE_DAMAGE)
-	e1:SetCondition(s.con)
-	e1:SetTarget(s.tg)
-	e1:SetOperation(s.op)
+	e1:SetCondition(function(_,tp,_,ep) return ep==tp and Duel.GetAttackTarget()==nil end)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.con(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and Duel.GetAttackTarget()==nil
-end
-function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(nil,tp,0,LOCATION_MZONE,nil)
+	if chk==0 then return #g>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_MZONE,1,1,nil)
 	if #g>0 then
 		Duel.HintSelection(g)
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE)
+	e1:SetCode(EVENT_PHASE|PHASE_BATTLE)
 	e1:SetCountLimit(1)
 	e1:SetOperation(s.spop)
-	e1:SetReset(RESET_PHASE+PHASE_BATTLE)
+	e1:SetReset(RESET_PHASE|PHASE_BATTLE)
 	Duel.RegisterEffect(e1,tp)
 end
 function s.spfilter(c,e,tp)
@@ -39,11 +38,12 @@ function s.spfilter(c,e,tp)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
-	if #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(96148285,0))then
+	if #g==0 or Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	if Duel.SelectYesNo(tp,aux.Stringid(96148285,0)) then
 		Duel.Hint(HINT_CARD,0,id)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local sg=g:Select(tp,1,1,nil)
-		if #sg~=0 then
+		if #sg>0 then
 			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end

259d0c55a52d76d7f2d947448b4e7947a65aab1e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-25 11:56:28 -0700

    Update "Eternal Reverse"
    -Does not target
    -Should not be able to select Pendulums with its effect
    -Cleaned up destruction replace effect

--- unofficial/c100000364.lua
+++ unofficial/c100000364.lua
@@ -3,45 +3,55 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c)
-	--Flip 1 face-up Spell/Trap your opponent controls face-down
+	--Once per turn, you can Set 1 Spell/Trap Card on your opponent's side of the field face-down. That Set card cannot be activated for the rest of this turn.
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SET)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_SZONE)
 	e1:SetCountLimit(1)
 	e1:SetTarget(s.settg)
 	e1:SetOperation(s.setop)
 	c:RegisterEffect(e1)
-	--Send this card to the GY to prevent battle destruction
+	--If the equipped monster would be destroyed by battle, you can send this card to the GY instead
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_EQUIP)
+	e2:SetType(EFFECT_TYPE_EQUIP+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e2:SetCode(EFFECT_DESTROY_REPLACE)
-	e2:SetTarget(s.destg)
-	e2:SetOperation(s.desop)
+	e2:SetTarget(s.replacetg)
 	c:RegisterEffect(e2)
 end
 function s.setfilter(c)
 	return c:IsFaceup() and c:IsSpellTrap() and c:IsCanTurnSet() and not c:IsType(TYPE_PENDULUM)
 end
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and c:IsControler(1-tp) and s.setfilter(chkc) end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,s.setfilter,tp,0,LOCATION_SZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.setfilter,tp,0,LOCATION_ONFIELD,nil)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local tc=Duel.SelectMatchingCard(tp,s.setfilter,tp,0,LOCATION_ONFIELD,1,1,nil):GetFirst()
+	Duel.HintSelection(tc)
+	if tc and tc:IsFaceup() then
+		tc:CancelToGrave()
 		Duel.ChangePosition(tc,POS_FACEDOWN)
+		tc:SetStatus(STATUS_SET_TURN,false)
+		Duel.RaiseEvent(tc,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_TRIGGER)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		tc:RegisterEffect(e1)
 	end
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.replacetg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local tg=c:GetEquipTarget()
-	if chk==0 then return c and not c:IsStatus(STATUS_DESTROY_CONFIRMED) and tg and tg:IsReason(REASON_BATTLE) end
-	return Duel.SelectEffectYesNo(tp,c,96)
-end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE)
+	local ec=c:GetEquipTarget()
+	if chk==0 then return ec:IsReason(REASON_BATTLE) and not ec:IsReason(REASON_REPLACE)
+		and c:IsAbleToGrave() and not c:IsStatus(STATUS_DESTROY_CONFIRMED) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+		return true
+	else return false end
 end

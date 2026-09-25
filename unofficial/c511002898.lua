c761ace6cc86f83fdf3ce3164897577a8cb82411
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2026-09-24 17:34:50 -0700

    Update "Battle Eater"
    -Should trigger off potential battle damage, not the ATK of the monster.
    -Other script polishings

--- unofficial/c511002898.lua
+++ unofficial/c511002898.lua
@@ -2,34 +2,28 @@
 --Battle Eater
 local s,id=GetID()
 function s.initial_effect(c)
-	--end battle phase
+	--If you would take battle damage less than or equal to this card's ATK, you can send this card to the Graveyard to negate the attack and end the Battle Phase. Then, take damage equal to this card's ATK.
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(18964575,0))
 	e1:SetCategory(CATEGORY_DAMAGE)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.condition)
-	e1:SetCost(s.cost)
+	e1:SetCondition(function(e,tp) return Duel.GetBattleDamage(tp)<=e:GetHandler():GetAttack() end)
+	e1:SetCost(Cost.SelfToGrave)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local at=Duel.GetAttacker()
-	return at:IsControler(1-tp) and Duel.GetAttackTarget()==nil and at:GetAttack()<e:GetHandler():GetAttack()
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,e:GetHandler():GetAttack())
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,e:GetHandler():GetAttack())
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	if Duel.NegateAttack() then
-		Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
+		Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE|PHASE_BATTLE_STEP,1)
 		Duel.BreakEffect()
 		Duel.Damage(tp,e:GetHandler():GetAttack(),REASON_EFFECT)
 	end
